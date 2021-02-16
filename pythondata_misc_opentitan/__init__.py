789418337caca74679c699934b180d01d84c5a6e
LiteX Robot <robot@timvideos.us>
2021-02-16 20:13:20 +0000

    Updating pythondata-misc-opentitan to 0.0.post5016
    Updated data to v0.0-4925-g30d23802d based on 30d23802d054518738cbaa78393995274d5602ac from https://github.com/lowRISC/opentitan.
    > commit 30d23802d054518738cbaa78393995274d5602ac
    > Author: Pirmin Vogel <vogelpi@lowrisc.org>
    > Date:   Tue Feb 16 10:11:21 2021 +0100
    >
    >     [top] Re-align top_englishbreakfast with top_earlgrey
    >
    >     The most prominent changes are:
    >     - Move pinmux from main crossbar to peripheral crossbar.
    >     - Re-align instance names (uart -> uart0, add _aon suffixes).
    >     - Re-align base addresses (ram_ret_aon, usbdev, sensor_ctrl_aon,
    >       ast_wrapper).
    >     - Re-align top_englishbreakfast.hjson to ease future diffs.
    >
    >     This resolves lowRISC/OpenTitan#5242.
    >
    >     Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
    >
    
    Updated using 0.0.post91 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,41 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5014"
-version_tuple = (0, 0, 5014)
+version_str = "0.0.post5016"
+version_tuple = (0, 0, 5016)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5014")
+    pversion = V("0.0.post5016")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post4923"
-data_version_tuple = (0, 0, 4923)
+data_version_str = "0.0.post4925"
+data_version_tuple = (0, 0, 4925)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post4923")
+    pdata_version = V("0.0.post4925")
 except ImportError:
     pass
-data_git_hash = "acde96f378b9412ce7919737f11ec0614f03cb8a"
-data_git_describe = "v0.0-4923-gacde96f37"
+data_git_hash = "30d23802d054518738cbaa78393995274d5602ac"
+data_git_describe = "v0.0-4925-g30d23802d"
 data_git_msg = """\
-commit acde96f378b9412ce7919737f11ec0614f03cb8a
-Author: Weicai Yang <weicai@google.com>
-Date:   Fri Feb 12 13:41:05 2021 -0800
+commit 30d23802d054518738cbaa78393995274d5602ac
+Author: Pirmin Vogel <vogelpi@lowrisc.org>
+Date:   Tue Feb 16 10:11:21 2021 +0100
 
-    [keymgr/dv] add lc_disable into stress_all
+    [top] Re-align top_englishbreakfast with top_earlgrey
     
-    Signed-off-by: Weicai Yang <weicai@google.com>
+    The most prominent changes are:
+    - Move pinmux from main crossbar to peripheral crossbar.
+    - Re-align instance names (uart -> uart0, add _aon suffixes).
+    - Re-align base addresses (ram_ret_aon, usbdev, sensor_ctrl_aon,
+      ast_wrapper).
+    - Re-align top_englishbreakfast.hjson to ease future diffs.
+    
+    This resolves lowRISC/OpenTitan#5242.
+    
+    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
 
 """
 
