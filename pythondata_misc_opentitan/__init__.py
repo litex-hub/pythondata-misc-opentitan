78522e365aec0962b0a3bd96079831d327016fbb
LiteX Robot <robot@timvideos.us>
2021-02-17 17:26:40 +0000

    Updating pythondata-misc-opentitan to 0.0.post5024
    Updated data to v0.0-4933-g9dffa1b32 based on 9dffa1b320ff70f1fd724a0cfb67f418c6caf437 from https://github.com/lowRISC/opentitan.
    > commit 9dffa1b320ff70f1fd724a0cfb67f418c6caf437
    > Author: Tom Roberts <tomroberts@lowrisc.org>
    > Date:   Wed Feb 17 11:45:30 2021 +0000
    >
    >     [reggen] Minor fix to REGWEN error message
    >
    >     Relates to #5267
    >
    >     Signed-off-by: Tom Roberts <tomroberts@lowrisc.org>
    >
    
    Updated using 0.0.post91 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,42 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5023"
-version_tuple = (0, 0, 5023)
+version_str = "0.0.post5024"
+version_tuple = (0, 0, 5024)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5023")
+    pversion = V("0.0.post5024")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post4932"
-data_version_tuple = (0, 0, 4932)
+data_version_str = "0.0.post4933"
+data_version_tuple = (0, 0, 4933)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post4932")
+    pdata_version = V("0.0.post4933")
 except ImportError:
     pass
-data_git_hash = "81ada4f0c26389e4175e0236eabe1124203d6850"
-data_git_describe = "v0.0-4932-g81ada4f0c"
+data_git_hash = "9dffa1b320ff70f1fd724a0cfb67f418c6caf437"
+data_git_describe = "v0.0-4933-g9dffa1b32"
 data_git_msg = """\
-commit 81ada4f0c26389e4175e0236eabe1124203d6850
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Tue Feb 16 15:59:39 2021 +0000
+commit 9dffa1b320ff70f1fd724a0cfb67f418c6caf437
+Author: Tom Roberts <tomroberts@lowrisc.org>
+Date:   Wed Feb 17 11:45:30 2021 +0000
 
-    [ci] Remove --fork-point from git merge-base command
+    [reggen] Minor fix to REGWEN error message
     
-    The --fork-point argument enables a complicated mode for git
-    merge-base, which tries to cope with situations where the branch from
-    which we forked (origin/master) has had rewrites.
+    Relates to #5267
     
-    This uses the reflog when searching for ancestors, which isn't
-    appropriate in CI (where we're working on fresh checkouts). It seems
-    that this fails occasionally, leading to rather mysterious CI errors.
-    
-    Since we don't rewrite the master branch, just use vanilla merge-base.
-    
-    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    Signed-off-by: Tom Roberts <tomroberts@lowrisc.org>
 
 """
 
