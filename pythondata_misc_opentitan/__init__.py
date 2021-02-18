dd4a719a360128fc4bbcac6afaedfbfb319adcd8
LiteX Robot <robot@timvideos.us>
2021-02-18 16:31:48 +0000

    Updating pythondata-misc-opentitan to 0.0.post5042
    Updated data to v0.0-4951-g789ea02bb based on 789ea02bbe3a60c883e25a1cfa2f8d50a950ac43 from https://github.com/lowRISC/opentitan.
    > commit 789ea02bbe3a60c883e25a1cfa2f8d50a950ac43
    > Author: Mark Branstad <mark.branstad@wdc.com>
    > Date:   Fri Feb 12 14:35:42 2021 -0800
    >
    >     [entropy_src/rtl] add fatal alert
    >
    >     ERR_CODE is RO and can be tested with a register.
    >     Re-ran top level to make updates to interconnections.
    >     Re-ran the reg script.
    >     Re-ran top level again.
    >
    >     Signed-off-by: Mark Branstad <mark.branstad@wdc.com>
    >
    
    Updated using 0.0.post91 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,37 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5041"
-version_tuple = (0, 0, 5041)
+version_str = "0.0.post5042"
+version_tuple = (0, 0, 5042)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5041")
+    pversion = V("0.0.post5042")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post4950"
-data_version_tuple = (0, 0, 4950)
+data_version_str = "0.0.post4951"
+data_version_tuple = (0, 0, 4951)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post4950")
+    pdata_version = V("0.0.post4951")
 except ImportError:
     pass
-data_git_hash = "588819ef9a4df185741ae2b59a830d20c9a494ae"
-data_git_describe = "v0.0-4950-g588819ef9"
+data_git_hash = "789ea02bbe3a60c883e25a1cfa2f8d50a950ac43"
+data_git_describe = "v0.0-4951-g789ea02bb"
 data_git_msg = """\
-commit 588819ef9a4df185741ae2b59a830d20c9a494ae
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Wed Feb 10 16:27:53 2021 +0000
+commit 789ea02bbe3a60c883e25a1cfa2f8d50a950ac43
+Author: Mark Branstad <mark.branstad@wdc.com>
+Date:   Fri Feb 12 14:35:42 2021 -0800
 
-    [regtool] Remove support for alternate-style headers
+    [entropy_src/rtl] add fatal alert
     
-    This is unused, and seems not to have been used since the start of
-    development history.
+    ERR_CODE is RO and can be tested with a register.
+    Re-ran top level to make updates to interconnections.
+    Re-ran the reg script.
+    Re-ran top level again.
     
-    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    Signed-off-by: Mark Branstad <mark.branstad@wdc.com>
 
 """
 
