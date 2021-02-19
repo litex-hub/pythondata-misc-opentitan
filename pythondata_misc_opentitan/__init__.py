3d5a6d4ee9de8c5d26efaabc0a0b2a4591e14c77
LiteX Robot <robot@timvideos.us>
2021-02-19 04:15:50 +0000

    Updating pythondata-misc-opentitan to 0.0.post5048
    Updated data to v0.0-4957-gbd80aed96 based on bd80aed965f06a2a2b74e2d469f057c7c88a68bc from https://github.com/lowRISC/opentitan.
    > commit bd80aed965f06a2a2b74e2d469f057c7c88a68bc
    > Author: Weicai Yang <weicai@google.com>
    > Date:   Thu Feb 18 15:51:37 2021 -0800
    >
    >     [top/dv] Fix uart CSR failure
    >
    >     uart RX pin may not be connected. When loopback is set, TX pin may
    >     toggle unexpectedly. Disable monitor TX pin in CSR test
    >
    >     Signed-off-by: Weicai Yang <weicai@google.com>
    >
    
    Updated using 0.0.post91 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5047"
-version_tuple = (0, 0, 5047)
+version_str = "0.0.post5048"
+version_tuple = (0, 0, 5048)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5047")
+    pversion = V("0.0.post5048")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post4956"
-data_version_tuple = (0, 0, 4956)
+data_version_str = "0.0.post4957"
+data_version_tuple = (0, 0, 4957)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post4956")
+    pdata_version = V("0.0.post4957")
 except ImportError:
     pass
-data_git_hash = "771d397f79a0fc80d9cc36bb003fc06fabdccb97"
-data_git_describe = "v0.0-4956-g771d397f7"
+data_git_hash = "bd80aed965f06a2a2b74e2d469f057c7c88a68bc"
+data_git_describe = "v0.0-4957-gbd80aed96"
 data_git_msg = """\
-commit 771d397f79a0fc80d9cc36bb003fc06fabdccb97
-Author: Cindy Chen <chencindy@google.com>
-Date:   Fri Feb 12 16:39:37 2021 -0800
+commit bd80aed965f06a2a2b74e2d469f057c7c88a68bc
+Author: Weicai Yang <weicai@google.com>
+Date:   Thu Feb 18 15:51:37 2021 -0800
 
-    [dv/otp_ctrl] update regwen access policy
+    [top/dv] Fix uart CSR failure
     
-    This PR follows the change from `W1C` to `W0C`, so we change the random
-    distribution to follow the change.
+    uart RX pin may not be connected. When loopback is set, TX pin may
+    toggle unexpectedly. Disable monitor TX pin in CSR test
     
-    Signed-off-by: Cindy Chen <chencindy@google.com>
+    Signed-off-by: Weicai Yang <weicai@google.com>
 
 """
 
