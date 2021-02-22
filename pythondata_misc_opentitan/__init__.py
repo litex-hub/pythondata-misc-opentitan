cce1894a768148c50c17f4ee34207ab14d26bbb4
LiteX Robot <robot@timvideos.us>
2021-02-22 19:10:38 +0000

    Updating pythondata-misc-opentitan to 0.0.post5101
    Updated data to v0.0-5010-g87285a2db based on 87285a2db401b932579a44b43d9691a8d04768b8 from https://github.com/lowRISC/opentitan.
    > commit 87285a2db401b932579a44b43d9691a8d04768b8
    > Author: Udi Jonnalagadda <udij@google.com>
    > Date:   Thu Feb 18 15:57:05 2021 -0800
    >
    >     [rtl/kmac] fix `right_encode(output_len)` for KDF
    >
    >     this PR fixes the output length encoding that is padded by the
    >     kmac_keymgr module.
    >     The values should be exactly the same as the encoded keylength in
    >     kmac_core.sv, except the byte that contains the byte-length of the
    >     encoded value should be in the MSB position for right encoding instead
    >     of in the LSB position for left encoding.
    >
    >     Signed-off-by: Udi Jonnalagadda <udij@google.com>
    >
    
    Updated using 0.0.post91 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,39 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5100"
-version_tuple = (0, 0, 5100)
+version_str = "0.0.post5101"
+version_tuple = (0, 0, 5101)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5100")
+    pversion = V("0.0.post5101")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5009"
-data_version_tuple = (0, 0, 5009)
+data_version_str = "0.0.post5010"
+data_version_tuple = (0, 0, 5010)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5009")
+    pdata_version = V("0.0.post5010")
 except ImportError:
     pass
-data_git_hash = "6a0baac71378994feca7154c19dac66b0aef3998"
-data_git_describe = "v0.0-5009-g6a0baac71"
+data_git_hash = "87285a2db401b932579a44b43d9691a8d04768b8"
+data_git_describe = "v0.0-5010-g87285a2db"
 data_git_msg = """\
-commit 6a0baac71378994feca7154c19dac66b0aef3998
-Author: Srikrishna Iyer <sriyer@google.com>
-Date:   Fri Feb 19 12:36:12 2021 -0800
+commit 87285a2db401b932579a44b43d9691a8d04768b8
+Author: Udi Jonnalagadda <udij@google.com>
+Date:   Thu Feb 18 15:57:05 2021 -0800
 
-    [dvsim] lint fixes to FlowCfg
+    [rtl/kmac] fix `right_encode(output_len)` for KDF
     
-    Signed-off-by: Srikrishna Iyer <sriyer@google.com>
+    this PR fixes the output length encoding that is padded by the
+    kmac_keymgr module.
+    The values should be exactly the same as the encoded keylength in
+    kmac_core.sv, except the byte that contains the byte-length of the
+    encoded value should be in the MSB position for right encoding instead
+    of in the LSB position for left encoding.
+    
+    Signed-off-by: Udi Jonnalagadda <udij@google.com>
 
 """
 
