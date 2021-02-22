087b70667e23f4250d71c886105067395f176ad8
LiteX Robot <robot@timvideos.us>
2021-02-22 20:13:46 +0000

    Updating pythondata-misc-opentitan to 0.0.post5102
    Updated data to v0.0-5011-g5bc27bd66 based on 5bc27bd66d61777a25fedc31b5233038be78f009 from https://github.com/lowRISC/opentitan.
    > commit 5bc27bd66d61777a25fedc31b5233038be78f009
    > Author: Udi Jonnalagadda <udij@google.com>
    > Date:   Fri Feb 12 10:59:47 2021 -0800
    >
    >     [dv/kmac] kmac_burst_write test
    >
    >     This PR implements the kmac_burst_wr test as described in the testplan.
    >
    >     Signed-off-by: Udi Jonnalagadda <udij@google.com>
    >
    
    Updated using 0.0.post91 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,37 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5101"
-version_tuple = (0, 0, 5101)
+version_str = "0.0.post5102"
+version_tuple = (0, 0, 5102)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5101")
+    pversion = V("0.0.post5102")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5010"
-data_version_tuple = (0, 0, 5010)
+data_version_str = "0.0.post5011"
+data_version_tuple = (0, 0, 5011)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5010")
+    pdata_version = V("0.0.post5011")
 except ImportError:
     pass
-data_git_hash = "87285a2db401b932579a44b43d9691a8d04768b8"
-data_git_describe = "v0.0-5010-g87285a2db"
+data_git_hash = "5bc27bd66d61777a25fedc31b5233038be78f009"
+data_git_describe = "v0.0-5011-g5bc27bd66"
 data_git_msg = """\
-commit 87285a2db401b932579a44b43d9691a8d04768b8
+commit 5bc27bd66d61777a25fedc31b5233038be78f009
 Author: Udi Jonnalagadda <udij@google.com>
-Date:   Thu Feb 18 15:57:05 2021 -0800
+Date:   Fri Feb 12 10:59:47 2021 -0800
 
-    [rtl/kmac] fix `right_encode(output_len)` for KDF
+    [dv/kmac] kmac_burst_write test
     
-    this PR fixes the output length encoding that is padded by the
-    kmac_keymgr module.
-    The values should be exactly the same as the encoded keylength in
-    kmac_core.sv, except the byte that contains the byte-length of the
-    encoded value should be in the MSB position for right encoding instead
-    of in the LSB position for left encoding.
+    This PR implements the kmac_burst_wr test as described in the testplan.
     
     Signed-off-by: Udi Jonnalagadda <udij@google.com>
 
