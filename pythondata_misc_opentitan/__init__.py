f2665b8c753c8d60b79229d0857fdae87850d768
LiteX Robot <robot@timvideos.us>
2021-02-23 23:13:57 +0000

    Updating pythondata-misc-opentitan to 0.0.post5128
    Updated data to v0.0-5037-gf55676252 based on f556762523a0e46af61a0a725a0bae2124021d6a from https://github.com/lowRISC/opentitan.
    > commit f556762523a0e46af61a0a725a0bae2124021d6a
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Tue Feb 23 10:14:59 2021 -0800
    >
    >     [top] Auto generate regfiles
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post91 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,43 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5126"
-version_tuple = (0, 0, 5126)
+version_str = "0.0.post5128"
+version_tuple = (0, 0, 5128)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5126")
+    pversion = V("0.0.post5128")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5035"
-data_version_tuple = (0, 0, 5035)
+data_version_str = "0.0.post5037"
+data_version_tuple = (0, 0, 5037)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5035")
+    pdata_version = V("0.0.post5037")
 except ImportError:
     pass
-data_git_hash = "51651f83e1795d99d86eb222829714b1c7567198"
-data_git_describe = "v0.0-5035-g51651f83e"
+data_git_hash = "f556762523a0e46af61a0a725a0bae2124021d6a"
+data_git_describe = "v0.0-5037-gf55676252"
 data_git_msg = """\
-commit 51651f83e1795d99d86eb222829714b1c7567198
-Author: Cindy Chen <chencindy@google.com>
-Date:   Mon Feb 22 18:45:26 2021 -0800
+commit f556762523a0e46af61a0a725a0bae2124021d6a
+Author: Timothy Chen <timothytim@google.com>
+Date:   Tue Feb 23 10:14:59 2021 -0800
 
-    [dv/otp_ctrl] enable checking for interrupts and collect coverage
+    [top] Auto generate regfiles
     
-    This PR is related to interrupts:
-    1. Support checking for interrupt related registers (except intr_state
-    waiting for designer's confirmation)
-    2. Ignored checking for status field `dai_idle` during dai operation,
-    because scb does not accurately predict which cycle dai operation
-    finishes.
-    Also move status prediction to addr_read_phase instead of
-    data_read_phase.
-    3. Change status `enum` from value to index because it is easier to use
-    index in scb.
-    
-    Signed-off-by: Cindy Chen <chencindy@google.com>
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
