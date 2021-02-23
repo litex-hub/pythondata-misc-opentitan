7708bfae5c5702624dd8bd46408600235bb81f7a
LiteX Robot <robot@timvideos.us>
2021-02-23 03:14:05 +0000

    Updating pythondata-misc-opentitan to 0.0.post5111
    Updated data to v0.0-5020-gd861bea12 based on d861bea1215bb42d3b07984a9f4fb8a4b7c0a82c from https://github.com/lowRISC/opentitan.
    > commit d861bea1215bb42d3b07984a9f4fb8a4b7c0a82c
    > Author: Cindy Chen <chencindy@google.com>
    > Date:   Mon Feb 22 10:00:58 2021 -0800
    >
    >     [dv/otp_ctrl] update testplan to fix the unmapped test
    >
    >     This PR updates the test sequence names and description to match the
    >     name of the sequences and avoid unmapped tests in regression results.
    >
    >     Signed-off-by: Cindy Chen <chencindy@google.com>
    >
    
    Updated using 0.0.post91 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5110"
-version_tuple = (0, 0, 5110)
+version_str = "0.0.post5111"
+version_tuple = (0, 0, 5111)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5110")
+    pversion = V("0.0.post5111")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5019"
-data_version_tuple = (0, 0, 5019)
+data_version_str = "0.0.post5020"
+data_version_tuple = (0, 0, 5020)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5019")
+    pdata_version = V("0.0.post5020")
 except ImportError:
     pass
-data_git_hash = "40c21eb946682b665112223d2b299df3c9385037"
-data_git_describe = "v0.0-5019-g40c21eb94"
+data_git_hash = "d861bea1215bb42d3b07984a9f4fb8a4b7c0a82c"
+data_git_describe = "v0.0-5020-gd861bea12"
 data_git_msg = """\
-commit 40c21eb946682b665112223d2b299df3c9385037
-Author: Timothy Chen <timothytim@google.com>
-Date:   Mon Feb 22 15:58:34 2021 -0800
+commit d861bea1215bb42d3b07984a9f4fb8a4b7c0a82c
+Author: Cindy Chen <chencindy@google.com>
+Date:   Mon Feb 22 10:00:58 2021 -0800
 
-    [top] Correct top level parameters for sram execution
+    [dv/otp_ctrl] update testplan to fix the unmapped test
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    This PR updates the test sequence names and description to match the
+    name of the sequences and avoid unmapped tests in regression results.
+    
+    Signed-off-by: Cindy Chen <chencindy@google.com>
 
 """
 
