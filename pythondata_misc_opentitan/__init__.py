c76aa0f63420927f859558263a3a75670ed0457c
LiteX Robot <robot@timvideos.us>
2021-02-23 11:13:01 +0000

    Updating pythondata-misc-opentitan to 0.0.post5121
    Updated data to v0.0-5030-gc40f2db4b based on c40f2db4b87ad811e4b50b1c3825c744df9c6838 from https://github.com/lowRISC/opentitan.
    > commit c40f2db4b87ad811e4b50b1c3825c744df9c6838
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Mon Feb 22 08:37:37 2021 +0000
    >
    >     [prim_packer] Silence verilator width warnings
    >
    >     There are two changes here. The first is to explicitly zero-extend
    >     wdata_i from InW up to OutW when computing wdata_shifted in pack mode.
    >     The second is to slice out just the bits we need from MaxW / MinW when
    >     comparing depth_q against it (otherwise Verilator complains that we're
    >     comparing a small bitvector against a 32-bit int).
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post91 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5120"
-version_tuple = (0, 0, 5120)
+version_str = "0.0.post5121"
+version_tuple = (0, 0, 5121)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5120")
+    pversion = V("0.0.post5121")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5029"
-data_version_tuple = (0, 0, 5029)
+data_version_str = "0.0.post5030"
+data_version_tuple = (0, 0, 5030)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5029")
+    pdata_version = V("0.0.post5030")
 except ImportError:
     pass
-data_git_hash = "1b001576ba32555b5260617585cf0a25f3714f30"
-data_git_describe = "v0.0-5029-g1b001576b"
+data_git_hash = "c40f2db4b87ad811e4b50b1c3825c744df9c6838"
+data_git_describe = "v0.0-5030-gc40f2db4b"
 data_git_msg = """\
-commit 1b001576ba32555b5260617585cf0a25f3714f30
+commit c40f2db4b87ad811e4b50b1c3825c744df9c6838
 Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Mon Feb 22 12:53:54 2021 +0000
+Date:   Mon Feb 22 08:37:37 2021 +0000
 
-    [otbn] Inline set_half_word_unsigned into BN.MULQACC.SO in ISS
+    [prim_packer] Silence verilator width warnings
     
-    This should have no change on behaviour, but inlining the one and only
-    call site gets rid of another state method from the generated
-    documentation.
+    There are two changes here. The first is to explicitly zero-extend
+    wdata_i from InW up to OutW when computing wdata_shifted in pack mode.
+    The second is to slice out just the bits we need from MaxW / MinW when
+    comparing depth_q against it (otherwise Verilator complains that we're
+    comparing a small bitvector against a 32-bit int).
     
     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
