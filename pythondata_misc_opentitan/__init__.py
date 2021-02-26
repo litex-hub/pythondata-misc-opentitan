57d29fc4fdf618dce8239c3c5defb9029982f69b
LiteX Robot <robot@timvideos.us>
2021-02-26 21:12:05 +0000

    Updating pythondata-misc-opentitan to 0.0.post5163
    Updated data to v0.0-5072-g6387a20a7 based on 6387a20a70c8c4f8ae10a92f4db34f7a95a2afbc from https://github.com/lowRISC/opentitan.
    > commit 6387a20a70c8c4f8ae10a92f4db34f7a95a2afbc
    > Author: Michael Schaffner <msf@opentitan.org>
    > Date:   Fri Feb 26 09:42:45 2021 -0800
    >
    >     [lint] Remove comportable waivers from non-comportable IPs
    >
    >     Signed-off-by: Michael Schaffner <msf@opentitan.org>
    >
    
    Updated using 0.0.post91 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5161"
-version_tuple = (0, 0, 5161)
+version_str = "0.0.post5163"
+version_tuple = (0, 0, 5163)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5161")
+    pversion = V("0.0.post5163")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5070"
-data_version_tuple = (0, 0, 5070)
+data_version_str = "0.0.post5072"
+data_version_tuple = (0, 0, 5072)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5070")
+    pdata_version = V("0.0.post5072")
 except ImportError:
     pass
-data_git_hash = "f11e29f066aa3601b1152aa5c35db23986619d63"
-data_git_describe = "v0.0-5070-gf11e29f06"
+data_git_hash = "6387a20a70c8c4f8ae10a92f4db34f7a95a2afbc"
+data_git_describe = "v0.0-5072-g6387a20a7"
 data_git_msg = """\
-commit f11e29f066aa3601b1152aa5c35db23986619d63
-Author: Tom Roberts <tomroberts@lowrisc.org>
-Date:   Thu Feb 11 09:04:35 2021 +0000
+commit 6387a20a70c8c4f8ae10a92f4db34f7a95a2afbc
+Author: Michael Schaffner <msf@opentitan.org>
+Date:   Fri Feb 26 09:42:45 2021 -0800
 
-    [usbdev] Add ASSERT_KNOWN on outputs
+    [lint] Remove comportable waivers from non-comportable IPs
     
-    This is required for D1 signoff.
-    
-    Signed-off-by: Tom Roberts <tomroberts@lowrisc.org>
+    Signed-off-by: Michael Schaffner <msf@opentitan.org>
 
 """
 
