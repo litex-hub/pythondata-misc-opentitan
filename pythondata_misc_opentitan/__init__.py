cc326ea7d44d6e1621aafa28a655ecd7a4e2ddcd
LiteX Robot <robot@timvideos.us>
2021-02-26 17:28:08 +0000

    Updating pythondata-misc-opentitan to 0.0.post5161
    Updated data to v0.0-5070-gf11e29f06 based on f11e29f066aa3601b1152aa5c35db23986619d63 from https://github.com/lowRISC/opentitan.
    > commit f11e29f066aa3601b1152aa5c35db23986619d63
    > Author: Tom Roberts <tomroberts@lowrisc.org>
    > Date:   Thu Feb 11 09:04:35 2021 +0000
    >
    >     [usbdev] Add ASSERT_KNOWN on outputs
    >
    >     This is required for D1 signoff.
    >
    >     Signed-off-by: Tom Roberts <tomroberts@lowrisc.org>
    >
    
    Updated using 0.0.post91 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,83 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5159"
-version_tuple = (0, 0, 5159)
+version_str = "0.0.post5161"
+version_tuple = (0, 0, 5161)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5159")
+    pversion = V("0.0.post5161")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5068"
-data_version_tuple = (0, 0, 5068)
+data_version_str = "0.0.post5070"
+data_version_tuple = (0, 0, 5070)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5068")
+    pdata_version = V("0.0.post5070")
 except ImportError:
     pass
-data_git_hash = "d812401e0220ecc4f6471b9b3d54175ded8d0dd3"
-data_git_describe = "v0.0-5068-gd812401e0"
+data_git_hash = "f11e29f066aa3601b1152aa5c35db23986619d63"
+data_git_describe = "v0.0-5070-gf11e29f06"
 data_git_msg = """\
-commit d812401e0220ecc4f6471b9b3d54175ded8d0dd3
-Author: Michael Munday <mike.munday@lowrisc.org>
-Date:   Wed Jan 20 16:37:27 2021 +0000
+commit f11e29f066aa3601b1152aa5c35db23986619d63
+Author: Tom Roberts <tomroberts@lowrisc.org>
+Date:   Thu Feb 11 09:04:35 2021 +0000
 
-    [sw, pmp] Add new enhanced PMP C API
+    [usbdev] Add ASSERT_KNOWN on outputs
     
-    Add an API to create, configure and commit entries to the PMP
-    control registers. Updates are performed using 'transactions'.
-    Transactions must be initialized before use with the number of
-    regions that are to be configured. This will then be checked
-    when the transaction is finalized and the CSRs are updated.
+    This is required for D1 signoff.
     
-    Unlike the existing PMP API we specify all region types the same
-    way using a struct containing `start` and `end` addresses. This
-    makes it easy to be explicit about the region boundaries as shown
-    in the example below.
-    
-    The new EPMP API assumes that most `mseccfg` updates will occur in
-    assembly and that the API will only be used to disable the Rule
-    Locking Bypass (`msecffg.RLB`) feature. In particular this API
-    assumes that Machine Mode Lockdown (`mseccfg.MML`) will be
-    disabled (though that can be verified by a call to `epmp_check`.)
-    
-    The API has been designed so that Machine Mode Lockdown support
-    can be added at a later date if required by adding an additional
-    set of `Lockdown` permission enum values. It would make the API
-    clunkier though.
-    
-    Example (configure and set in a single transaction):
-    
-      ```
-      // Initialize the library with the current state of the PMP
-      // CSRs.
-      res0 = epmp_init();
-    
-      // Enable Rule Locking Bypass (RLB).
-      res1  = epmp_set_rule_locking_bypass(kEpmpRlbToggleEnabled);
-    
-      // Update 4 PMP entries in a single transaction.
-      res2 = epmp_transaction_start(kEpmpEntryCount4);
-      res3 = epmp_entry_configure_na4(kEpmpEntry0,
-                 (epmp_region_t){ .start = 0x0, .end = 0x4 },
-                 kEpmpPermLockedReadWrite);
-      res4 = epmp_entry_configure_napot(kEpmpEntry1,
-                 (epmp_region_t){ .start = 0x40, .end = 0x48 },
-                 kEpmpPermLockedReadExecute);
-      // pmpaddr2 used as base address for entry 3
-      res5 = epmp_entry_configure_tor(kEpmpEntry3,
-                 (epmp_region_t){ .start = 0x100, .end = 0x200 },
-                 kEpmpPermLockedReadWrite);
-      res6 = epmp_entry_configure_tor(kEpmpEntry4,
-                 (epmp_region_t){ .start = 0x200, .end = 0x300 },
-                 kEpmpPermLockedReadOnly);
-      res7 = epmp_transaction_end(kEpmpEntryCount4); // update CSRs
-    
-      ```
-    
-    Signed-off-by: Michael Munday <mike.munday@lowrisc.org>
+    Signed-off-by: Tom Roberts <tomroberts@lowrisc.org>
 
 """
 
