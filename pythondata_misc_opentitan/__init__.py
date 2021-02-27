4106972644f21896b73f7876d8211816aaaf1d9e
LiteX Robot <robot@timvideos.us>
2021-02-27 01:40:38 +0000

    Updating pythondata-misc-opentitan to 0.0.post5166
    Updated data to v0.0-5075-g363f5c196 based on 363f5c1967786a96c59d5c9aa797c7a027f9a306 from https://github.com/lowRISC/opentitan.
    > commit 363f5c1967786a96c59d5c9aa797c7a027f9a306
    > Author: Cindy Chen <chencindy@google.com>
    > Date:   Thu Feb 25 12:26:52 2021 -0800
    >
    >     [dv/shadow_reg] Fix aes shadow reg error
    >
    >     This PR support aes shadow reg with the new feature reported in
    >     PR #4895 :
    >     AES shadow reg fatal error will lock this register's write access.
    >
    >     This PR updates the testbench regarding this feature:
    >     1. Add a `shadow_fatal_lock` local variable to indicate if the shadow
    >     reg is locked due to fatal error.
    >     2. Temp unlock the `shadow_fatal_lock` if a backdoor write is issued.
    >     Because tesbench can still update the shadow reg value.
    >
    >     Signed-off-by: Cindy Chen <chencindy@google.com>
    >
    
    Updated using 0.0.post91 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,40 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5165"
-version_tuple = (0, 0, 5165)
+version_str = "0.0.post5166"
+version_tuple = (0, 0, 5166)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5165")
+    pversion = V("0.0.post5166")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5074"
-data_version_tuple = (0, 0, 5074)
+data_version_str = "0.0.post5075"
+data_version_tuple = (0, 0, 5075)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5074")
+    pdata_version = V("0.0.post5075")
 except ImportError:
     pass
-data_git_hash = "ff5a23bd27248e958ac2a48061d7406684daefb7"
-data_git_describe = "v0.0-5074-gff5a23bd2"
+data_git_hash = "363f5c1967786a96c59d5c9aa797c7a027f9a306"
+data_git_describe = "v0.0-5075-g363f5c196"
 data_git_msg = """\
-commit ff5a23bd27248e958ac2a48061d7406684daefb7
+commit 363f5c1967786a96c59d5c9aa797c7a027f9a306
 Author: Cindy Chen <chencindy@google.com>
-Date:   Fri Feb 26 10:16:10 2021 -0800
+Date:   Thu Feb 25 12:26:52 2021 -0800
 
-    [fpv] TLUL_pkg compile error
+    [dv/shadow_reg] Fix aes shadow reg error
     
-    This PR fixes a small compile error due to enum casting in tlul_pkg.
+    This PR support aes shadow reg with the new feature reported in
+    PR #4895 :
+    AES shadow reg fatal error will lock this register's write access.
+    
+    This PR updates the testbench regarding this feature:
+    1. Add a `shadow_fatal_lock` local variable to indicate if the shadow
+    reg is locked due to fatal error.
+    2. Temp unlock the `shadow_fatal_lock` if a backdoor write is issued.
+    Because tesbench can still update the shadow reg value.
     
     Signed-off-by: Cindy Chen <chencindy@google.com>
 
