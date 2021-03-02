8320c9cf580b7afe8bf913433c004cbc5160ef18
LiteX Robot <robot@timvideos.us>
2021-03-02 10:12:52 +0000

    Updating pythondata-misc-opentitan to 0.0.post5195
    Updated data to v0.0-5104-g1db6fcd8f based on 1db6fcd8f909543a08a67bf6c354898be258612d from https://github.com/lowRISC/opentitan.
    > commit 1db6fcd8f909543a08a67bf6c354898be258612d
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Thu Feb 11 14:56:20 2021 +0000
    >
    >     [reggen] Define a class to represent the registers in a block
    >
    >     This is used to represent obj['registers'], which was previously a
    >     list of registers, multiregs, windows, reserved and skipto entries.
    >     The commit doesn't replace the top-level object (obj).
    >
    >     Other than removing genwennames and gennextoffset from (h)json export,
    >     the only other change is that we no longer distinguish between
    >     reserved and skipto, which means we have to pick one format or the
    >     other on export. This commit picks reserved (because it is more
    >     "relocatable"), but skipto could work instead.
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post91 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,33 +4,40 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5192"
-version_tuple = (0, 0, 5192)
+version_str = "0.0.post5195"
+version_tuple = (0, 0, 5195)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5192")
+    pversion = V("0.0.post5195")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5101"
-data_version_tuple = (0, 0, 5101)
+data_version_str = "0.0.post5104"
+data_version_tuple = (0, 0, 5104)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5101")
+    pdata_version = V("0.0.post5104")
 except ImportError:
     pass
-data_git_hash = "bf2bbda2285a2faa7a8b4edd423de0401cd60d2f"
-data_git_describe = "v0.0-5101-gbf2bbda22"
+data_git_hash = "1db6fcd8f909543a08a67bf6c354898be258612d"
+data_git_describe = "v0.0-5104-g1db6fcd8f"
 data_git_msg = """\
-commit bf2bbda2285a2faa7a8b4edd423de0401cd60d2f
+commit 1db6fcd8f909543a08a67bf6c354898be258612d
 Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Mon Mar 1 13:37:19 2021 +0000
+Date:   Thu Feb 11 14:56:20 2021 +0000
 
-    [reggen] Fix check_name() regex in reggen/lib.py
+    [reggen] Define a class to represent the registers in a block
     
-    Also fix the one example where we had something that's supposed to
-    look like a symbol and in fact contained a space.
+    This is used to represent obj['registers'], which was previously a
+    list of registers, multiregs, windows, reserved and skipto entries.
+    The commit doesn't replace the top-level object (obj).
+    
+    Other than removing genwennames and gennextoffset from (h)json export,
+    the only other change is that we no longer distinguish between
+    reserved and skipto, which means we have to pick one format or the
+    other on export. This commit picks reserved (because it is more
+    "relocatable"), but skipto could work instead.
     
     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
