1b76f7b1f7851fef632846ef646e4f24fa404fe7
LiteX Robot <robot@timvideos.us>
2021-03-03 23:14:28 +0000

    Updating pythondata-misc-opentitan to 0.0.post5226
    Updated data to v0.0-5135-gefaa97eb0 based on efaa97eb056324fec332981139a3370db290679a from https://github.com/lowRISC/opentitan.
    > commit efaa97eb056324fec332981139a3370db290679a
    > Author: Michael Schaffner <msf@opentitan.org>
    > Date:   Tue Mar 2 12:00:02 2021 -0800
    >
    >     [ast] AscenLint fixes and new analog typedef
    >
    >     Co-authored-by: Jacob Levy <jacob.levy@opentitan.org>
    >     Signed-off-by: Michael Schaffner <msf@opentitan.org>
    >     Signed-off-by: Jacob Levy <jacob.levy@opentitan.org>
    >
    
    Updated using 0.0.post91 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5225"
-version_tuple = (0, 0, 5225)
+version_str = "0.0.post5226"
+version_tuple = (0, 0, 5226)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5225")
+    pversion = V("0.0.post5226")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5134"
-data_version_tuple = (0, 0, 5134)
+data_version_str = "0.0.post5135"
+data_version_tuple = (0, 0, 5135)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5134")
+    pdata_version = V("0.0.post5135")
 except ImportError:
     pass
-data_git_hash = "11e4d89943abecc79361d3b46d38b0a64cbd5b84"
-data_git_describe = "v0.0-5134-g11e4d8994"
+data_git_hash = "efaa97eb056324fec332981139a3370db290679a"
+data_git_describe = "v0.0-5135-gefaa97eb0"
 data_git_msg = """\
-commit 11e4d89943abecc79361d3b46d38b0a64cbd5b84
+commit efaa97eb056324fec332981139a3370db290679a
 Author: Michael Schaffner <msf@opentitan.org>
 Date:   Tue Mar 2 12:00:02 2021 -0800
 
-    [otp_ctrl] Always attempt to program all LC words in presence of errors
+    [ast] AscenLint fixes and new analog typedef
     
+    Co-authored-by: Jacob Levy <jacob.levy@opentitan.org>
     Signed-off-by: Michael Schaffner <msf@opentitan.org>
+    Signed-off-by: Jacob Levy <jacob.levy@opentitan.org>
 
 """
 
