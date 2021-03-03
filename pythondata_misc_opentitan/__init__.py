a2436ec74659cf9f22051fe5216f21f49e525ed4
LiteX Robot <robot@timvideos.us>
2021-03-03 03:12:28 +0000

    Updating pythondata-misc-opentitan to 0.0.post5209
    Updated data to v0.0-5118-gc8b3333ec based on c8b3333ec9a5fd550198d2772926227005e8f4e0 from https://github.com/lowRISC/opentitan.
    > commit c8b3333ec9a5fd550198d2772926227005e8f4e0
    > Author: Cindy Chen <chencindy@google.com>
    > Date:   Tue Mar 2 17:19:52 2021 -0800
    >
    >     [rtl/lc_ctrl] Fix jtag d_error
    >
    >     When I try to use DMI to access LC_CTRL CSR, I always get a `d_error`.
    >     The error is coming from this check: https://github.com/lowRISC/opentitan/blob/master/hw/ip/tlul/rtl/tlul_pkg.sv#L142
    >     I believe it is related to default value for tap's tl_i.a_user.
    >
    >     Signed-off-by: Cindy Chen <chencindy@google.com>
    >
    
    Updated using 0.0.post91 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5208"
-version_tuple = (0, 0, 5208)
+version_str = "0.0.post5209"
+version_tuple = (0, 0, 5209)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5208")
+    pversion = V("0.0.post5209")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5117"
-data_version_tuple = (0, 0, 5117)
+data_version_str = "0.0.post5118"
+data_version_tuple = (0, 0, 5118)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5117")
+    pdata_version = V("0.0.post5118")
 except ImportError:
     pass
-data_git_hash = "03bcc2f0975e0748ce46e6b21c85415dedb0c688"
-data_git_describe = "v0.0-5117-g03bcc2f09"
+data_git_hash = "c8b3333ec9a5fd550198d2772926227005e8f4e0"
+data_git_describe = "v0.0-5118-gc8b3333ec"
 data_git_msg = """\
-commit 03bcc2f0975e0748ce46e6b21c85415dedb0c688
+commit c8b3333ec9a5fd550198d2772926227005e8f4e0
 Author: Cindy Chen <chencindy@google.com>
-Date:   Mon Mar 1 15:13:25 2021 -0800
+Date:   Tue Mar 2 17:19:52 2021 -0800
 
-    [dv/jtag] Fix two small jtag issue
+    [rtl/lc_ctrl] Fix jtag d_error
     
-    These PR fixes two small issues in jtag:
-    1. Add a clock cycle delay in between clock_en and drive_tms.
-       This is to make sure the clock is stable before driving any signals.
-    2. Fix a port declaration from output to ref.
+    When I try to use DMI to access LC_CTRL CSR, I always get a `d_error`.
+    The error is coming from this check: https://github.com/lowRISC/opentitan/blob/master/hw/ip/tlul/rtl/tlul_pkg.sv#L142
+    I believe it is related to default value for tap's tl_i.a_user.
     
     Signed-off-by: Cindy Chen <chencindy@google.com>
 
