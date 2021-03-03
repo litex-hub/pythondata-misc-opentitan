b2d50f76afc9b0f7936ddc019a4e60bcb2e7802b
LiteX Robot <robot@timvideos.us>
2021-03-03 12:18:27 +0000

    Updating pythondata-misc-opentitan to 0.0.post5214
    Updated data to v0.0-5123-g53721b5ef based on 53721b5ef9d7c655a5f9934ba7e08ccba17c8250 from https://github.com/lowRISC/opentitan.
    > commit 53721b5ef9d7c655a5f9934ba7e08ccba17c8250
    > Author: Felix Miller <felix.miller@gi-de.com>
    > Date:   Thu Feb 18 02:48:15 2021 +0100
    >
    >     [otbn] add optimized 384 bit Barrett mult for P-384
    >
    >     Adds a variant for the 384 bit Barrett redcution
    >     based modular multiplication algorithm optimized
    >     for and limited to the NIST P-384 curve.
    >
    >     Differences to generic version:
    >     - The multiplication sequences are inline to reduce
    >       move instructions and enbale different configurations
    >       for the multiplications
    >     - Use optimized multiplication sequences exploiting the
    >       special form of the P-384 modulus
    >     - Only a single conditional subtract of the modulus at
    >       the end
    >
    >     Signed-off-by: Felix Miller <felix.miller@gi-de.com>
    >
    
    Updated using 0.0.post91 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,45 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5213"
-version_tuple = (0, 0, 5213)
+version_str = "0.0.post5214"
+version_tuple = (0, 0, 5214)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5213")
+    pversion = V("0.0.post5214")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5122"
-data_version_tuple = (0, 0, 5122)
+data_version_str = "0.0.post5123"
+data_version_tuple = (0, 0, 5123)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5122")
+    pdata_version = V("0.0.post5123")
 except ImportError:
     pass
-data_git_hash = "6cc5c164ba96d339f06cbcede0d17d2c96ce3c05"
-data_git_describe = "v0.0-5122-g6cc5c164b"
+data_git_hash = "53721b5ef9d7c655a5f9934ba7e08ccba17c8250"
+data_git_describe = "v0.0-5123-g53721b5ef"
 data_git_msg = """\
-commit 6cc5c164ba96d339f06cbcede0d17d2c96ce3c05
-Author: Srikrishna Iyer <sriyer@google.com>
-Date:   Wed Mar 3 01:10:09 2021 -0800
+commit 53721b5ef9d7c655a5f9934ba7e08ccba17c8250
+Author: Felix Miller <felix.miller@gi-de.com>
+Date:   Thu Feb 18 02:48:15 2021 +0100
 
-    [dv] Add SV_FCOV_SVA back
+    [otbn] add optimized 384 bit Barrett mult for P-384
     
-    Signed-off-by: Srikrishna Iyer <sriyer@google.com>
+    Adds a variant for the 384 bit Barrett redcution
+    based modular multiplication algorithm optimized
+    for and limited to the NIST P-384 curve.
+    
+    Differences to generic version:
+    - The multiplication sequences are inline to reduce
+      move instructions and enbale different configurations
+      for the multiplications
+    - Use optimized multiplication sequences exploiting the
+      special form of the P-384 modulus
+    - Only a single conditional subtract of the modulus at
+      the end
+    
+    Signed-off-by: Felix Miller <felix.miller@gi-de.com>
 
 """
 
