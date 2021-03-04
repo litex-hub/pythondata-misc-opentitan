54990ecc38e32c5ebc896eedebf312ac1d5e8762
LiteX Robot <robot@timvideos.us>
2021-03-04 09:14:39 +0000

    Updating pythondata-misc-opentitan to 0.0.post5237
    Updated data to v0.0-5146-g6940d94cd based on 6940d94cd03b5f75aec543086182c4b8151e49fe from https://github.com/lowRISC/opentitan.
    > commit 6940d94cd03b5f75aec543086182c4b8151e49fe
    > Author: Silvestrs Timofejevs <silvestrst@lowrisc.org>
    > Date:   Thu Feb 4 16:29:33 2021 +0000
    >
    >     [sw, rom_ext_signer] Add signing and remaining image update API
    >
    >     - Retrieving exponent is work-in-progress, and at the moment
    >       dummy hardcoded vector of `0xA5` is used.
    >     - Retrieving modulus is work-in-progress, and at the moment
    >       dummy hardcoded vector of `0xA5` is used.
    >     - Peripheral Lockdown Info encodingis work in progress, and at
    >       the moment dummy hardcoded vector of `0xA5` is used.
    >     - It is not clear yet how the device_usage_value is encoded in
    >       the 256-bit usage_constraints blob, and at the moment dummy
    >       hardcoded vector of `0xA5` is used.
    >
    >     This change adds image signing functionality through Mundane.
    >
    >     * There was some refactoring done, missing fields
    >       `extension{0,1,2,3}_offset` have been added.
    >
    >     * `image_length` is no longer passed in the config, as it is
    >       set automatically in the manifest assembly file.
    >
    >     * `image_timestamp` is no longer passed in the config, and is
    >       calculated at runtime.
    >
    >     Signed-off-by: Silvestrs Timofejevs <silvestrst@lowrisc.org>
    >
    
    Updated using 0.0.post91 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,53 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5234"
-version_tuple = (0, 0, 5234)
+version_str = "0.0.post5237"
+version_tuple = (0, 0, 5237)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5234")
+    pversion = V("0.0.post5237")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5143"
-data_version_tuple = (0, 0, 5143)
+data_version_str = "0.0.post5146"
+data_version_tuple = (0, 0, 5146)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5143")
+    pdata_version = V("0.0.post5146")
 except ImportError:
     pass
-data_git_hash = "b2576aa8428a553bdbcb8f6ad34377270ea899a8"
-data_git_describe = "v0.0-5143-gb2576aa84"
+data_git_hash = "6940d94cd03b5f75aec543086182c4b8151e49fe"
+data_git_describe = "v0.0-5146-g6940d94cd"
 data_git_msg = """\
-commit b2576aa8428a553bdbcb8f6ad34377270ea899a8
-Author: Cindy Chen <chencindy@google.com>
-Date:   Thu Feb 11 19:12:13 2021 -0800
+commit 6940d94cd03b5f75aec543086182c4b8151e49fe
+Author: Silvestrs Timofejevs <silvestrst@lowrisc.org>
+Date:   Thu Feb 4 16:29:33 2021 +0000
 
-    [dv/lock_reg] Update IPs to adopt the lock_reg changes
+    [sw, rom_ext_signer] Add signing and remaining image update API
     
-    Following the lock_reg changes that support reg field, this PR updates
-    naming changes to the rest of the DV testbenches.
+    - Retrieving exponent is work-in-progress, and at the moment
+      dummy hardcoded vector of `0xA5` is used.
+    - Retrieving modulus is work-in-progress, and at the moment
+      dummy hardcoded vector of `0xA5` is used.
+    - Peripheral Lockdown Info encodingis work in progress, and at
+      the moment dummy hardcoded vector of `0xA5` is used.
+    - It is not clear yet how the device_usage_value is encoded in
+      the 256-bit usage_constraints blob, and at the moment dummy
+      hardcoded vector of `0xA5` is used.
     
-    Signed-off-by: Cindy Chen <chencindy@google.com>
+    This change adds image signing functionality through Mundane.
+    
+    * There was some refactoring done, missing fields
+      `extension{0,1,2,3}_offset` have been added.
+    
+    * `image_length` is no longer passed in the config, as it is
+      set automatically in the manifest assembly file.
+    
+    * `image_timestamp` is no longer passed in the config, and is
+      calculated at runtime.
+    
+    Signed-off-by: Silvestrs Timofejevs <silvestrst@lowrisc.org>
 
 """
 
