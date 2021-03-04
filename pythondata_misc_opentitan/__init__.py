b281db44570e7b0cffad690ab11194311465b522
LiteX Robot <robot@timvideos.us>
2021-03-04 06:14:53 +0000

    Updating pythondata-misc-opentitan to 0.0.post5231
    Updated data to v0.0-5140-g8a1726fb8 based on 8a1726fb81612e4abdcc447170eced896c07b9dc from https://github.com/lowRISC/opentitan.
    > commit 8a1726fb81612e4abdcc447170eced896c07b9dc
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Wed Mar 3 18:48:23 2021 -0800
    >
    >     [top] Expand rom to 40b for slightly improved security properties
    >
    >     - Now all Rom nibbles will be fed through the 4-bit sbox, even if
    >       only 1 round of permutation is done for scrambling
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post91 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,38 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5230"
-version_tuple = (0, 0, 5230)
+version_str = "0.0.post5231"
+version_tuple = (0, 0, 5231)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5230")
+    pversion = V("0.0.post5231")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5139"
-data_version_tuple = (0, 0, 5139)
+data_version_str = "0.0.post5140"
+data_version_tuple = (0, 0, 5140)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5139")
+    pdata_version = V("0.0.post5140")
 except ImportError:
     pass
-data_git_hash = "bc4df4bb63165bfd60c7037291e4e41d20c5d4bd"
-data_git_describe = "v0.0-5139-gbc4df4bb6"
+data_git_hash = "8a1726fb81612e4abdcc447170eced896c07b9dc"
+data_git_describe = "v0.0-5140-g8a1726fb8"
 data_git_msg = """\
-commit bc4df4bb63165bfd60c7037291e4e41d20c5d4bd
-Author: Udi Jonnalagadda <udij@google.com>
-Date:   Wed Feb 3 16:36:03 2021 -0800
+commit 8a1726fb81612e4abdcc447170eced896c07b9dc
+Author: Timothy Chen <timothytim@google.com>
+Date:   Wed Mar 3 18:48:23 2021 -0800
 
-    [dv/chip] Update mem_bkdr_if in chip level TB
+    [top] Expand rom to 40b for slightly improved security properties
     
-    This commit updates the usage of `mem_bkdr_if` throughout the chip level
-    testbench.
+    - Now all Rom nibbles will be fed through the 4-bit sbox, even if
+      only 1 round of permutation is done for scrambling
     
-    The OTP testbencch also has minor updates to enable ECC in the
-    mem_bkdr_if.
-    
-    Signed-off-by: Udi Jonnalagadda <udij@google.com>
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
