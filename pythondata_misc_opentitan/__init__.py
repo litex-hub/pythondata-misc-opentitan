d728dfd75cbccec59aad41e8564217f09aaf4768
LiteX Robot <robot@timvideos.us>
2021-03-04 08:16:26 +0000

    Updating pythondata-misc-opentitan to 0.0.post5234
    Updated data to v0.0-5143-gb2576aa84 based on b2576aa8428a553bdbcb8f6ad34377270ea899a8 from https://github.com/lowRISC/opentitan.
    > commit b2576aa8428a553bdbcb8f6ad34377270ea899a8
    > Author: Cindy Chen <chencindy@google.com>
    > Date:   Thu Feb 11 19:12:13 2021 -0800
    >
    >     [dv/lock_reg] Update IPs to adopt the lock_reg changes
    >
    >     Following the lock_reg changes that support reg field, this PR updates
    >     naming changes to the rest of the DV testbenches.
    >
    >     Signed-off-by: Cindy Chen <chencindy@google.com>
    >
    
    Updated using 0.0.post91 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5231"
-version_tuple = (0, 0, 5231)
+version_str = "0.0.post5234"
+version_tuple = (0, 0, 5234)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5231")
+    pversion = V("0.0.post5234")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5140"
-data_version_tuple = (0, 0, 5140)
+data_version_str = "0.0.post5143"
+data_version_tuple = (0, 0, 5143)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5140")
+    pdata_version = V("0.0.post5143")
 except ImportError:
     pass
-data_git_hash = "8a1726fb81612e4abdcc447170eced896c07b9dc"
-data_git_describe = "v0.0-5140-g8a1726fb8"
+data_git_hash = "b2576aa8428a553bdbcb8f6ad34377270ea899a8"
+data_git_describe = "v0.0-5143-gb2576aa84"
 data_git_msg = """\
-commit 8a1726fb81612e4abdcc447170eced896c07b9dc
-Author: Timothy Chen <timothytim@google.com>
-Date:   Wed Mar 3 18:48:23 2021 -0800
+commit b2576aa8428a553bdbcb8f6ad34377270ea899a8
+Author: Cindy Chen <chencindy@google.com>
+Date:   Thu Feb 11 19:12:13 2021 -0800
 
-    [top] Expand rom to 40b for slightly improved security properties
+    [dv/lock_reg] Update IPs to adopt the lock_reg changes
     
-    - Now all Rom nibbles will be fed through the 4-bit sbox, even if
-      only 1 round of permutation is done for scrambling
+    Following the lock_reg changes that support reg field, this PR updates
+    naming changes to the rest of the DV testbenches.
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Cindy Chen <chencindy@google.com>
 
 """
 
