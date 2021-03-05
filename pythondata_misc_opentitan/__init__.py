69f41fe9c655703cbca0b643772e2f0e9ffd16d4
LiteX Robot <robot@timvideos.us>
2021-03-05 19:54:08 +0000

    Updating pythondata-misc-opentitan to 0.0.post5259
    Updated data to v0.0-5167-ga63640ec0 based on a63640ec01fbac7a8c43d75ca461faf97675654f from https://github.com/lowRISC/opentitan.
    > commit a63640ec01fbac7a8c43d75ca461faf97675654f
    > Author: Tung Hoang <hoang.tung@wdc.com>
    > Date:   Sun Feb 28 15:20:22 2021 -0800
    >
    >     [i2c, dv] Update i2c_testplan to include tests required to verify i2c target rtl
    >
    >     Signed-off-by: Tung Hoang <hoang.tung@wdc.com>
    >
    
    Updated using 0.0.post92 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,47 +4,41 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5257"
-version_tuple = (0, 0, 5257)
+version_str = "0.0.post5259"
+version_tuple = (0, 0, 5259)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5257")
+    pversion = V("0.0.post5259")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5166"
-data_version_tuple = (0, 0, 5166)
+data_version_str = "0.0.post5167"
+data_version_tuple = (0, 0, 5167)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5166")
+    pdata_version = V("0.0.post5167")
 except ImportError:
     pass
-data_git_hash = "4c609f8ad262174d4c802cb3f8624840488ec38b"
-data_git_describe = "v0.0-5166-g4c609f8ad"
+data_git_hash = "a63640ec01fbac7a8c43d75ca461faf97675654f"
+data_git_describe = "v0.0-5167-ga63640ec0"
 data_git_msg = """\
-commit 4c609f8ad262174d4c802cb3f8624840488ec38b
-Author: Michael Schaffner <msf@google.com>
-Date:   Thu Mar 4 18:20:43 2021 -0800
+commit a63640ec01fbac7a8c43d75ca461faf97675654f
+Author: Tung Hoang <hoang.tung@wdc.com>
+Date:   Sun Feb 28 15:20:22 2021 -0800
 
-    [chip/dv] Fix OTP preloading issues in chip DV environment
+    [i2c, dv] Update i2c_testplan to include tests required to verify i2c target rtl
     
-    This moves OTP preloading into the chip_base_vseq such that all tests
-    correctly preload the OTP with the correct LC state.
-    
-    This also adds some more CSR exclusions, as now the OTP and LC
-    controllers populate these CSRs with nonzero values upon init.
-    
-    Signed-off-by: Michael Schaffner <msf@google.com>
+    Signed-off-by: Tung Hoang <hoang.tung@wdc.com>
 
 """
 
 # Tool version info
-tool_version_str = "0.0.post91"
-tool_version_tuple = (0, 0, 91)
+tool_version_str = "0.0.post92"
+tool_version_tuple = (0, 0, 92)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post91")
+    ptool_version = V("0.0.post92")
 except ImportError:
     pass
 
