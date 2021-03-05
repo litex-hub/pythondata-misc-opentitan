9a4dd1c9ef9f13b522bf04d5dbe263a0227de11a
LiteX Robot <robot@timvideos.us>
2021-03-05 19:10:17 +0000

    Updating pythondata-misc-opentitan to 0.0.post5257
    Updated data to v0.0-5166-g4c609f8ad based on 4c609f8ad262174d4c802cb3f8624840488ec38b from https://github.com/lowRISC/opentitan.
    > commit 4c609f8ad262174d4c802cb3f8624840488ec38b
    > Author: Michael Schaffner <msf@google.com>
    > Date:   Thu Mar 4 18:20:43 2021 -0800
    >
    >     [chip/dv] Fix OTP preloading issues in chip DV environment
    >
    >     This moves OTP preloading into the chip_base_vseq such that all tests
    >     correctly preload the OTP with the correct LC state.
    >
    >     This also adds some more CSR exclusions, as now the OTP and LC
    >     controllers populate these CSRs with nonzero values upon init.
    >
    >     Signed-off-by: Michael Schaffner <msf@google.com>
    >
    
    Updated using 0.0.post91 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,38 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5254"
-version_tuple = (0, 0, 5254)
+version_str = "0.0.post5257"
+version_tuple = (0, 0, 5257)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5254")
+    pversion = V("0.0.post5257")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5163"
-data_version_tuple = (0, 0, 5163)
+data_version_str = "0.0.post5166"
+data_version_tuple = (0, 0, 5166)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5163")
+    pdata_version = V("0.0.post5166")
 except ImportError:
     pass
-data_git_hash = "c88e97fe0634880a8e617a8594e1a3e883394569"
-data_git_describe = "v0.0-5163-gc88e97fe0"
+data_git_hash = "4c609f8ad262174d4c802cb3f8624840488ec38b"
+data_git_describe = "v0.0-5166-g4c609f8ad"
 data_git_msg = """\
-commit c88e97fe0634880a8e617a8594e1a3e883394569
-Author: Tom Roberts <tomroberts@lowrisc.org>
-Date:   Thu Mar 4 13:38:20 2021 +0000
+commit 4c609f8ad262174d4c802cb3f8624840488ec38b
+Author: Michael Schaffner <msf@google.com>
+Date:   Thu Mar 4 18:20:43 2021 -0800
 
-    [ibex] Connect up crash dump output
+    [chip/dv] Fix OTP preloading issues in chip DV environment
     
-    Fixes #4618
+    This moves OTP preloading into the chip_base_vseq such that all tests
+    correctly preload the OTP with the correct LC state.
     
-    Signed-off-by: Tom Roberts <tomroberts@lowrisc.org>
+    This also adds some more CSR exclusions, as now the OTP and LC
+    controllers populate these CSRs with nonzero values upon init.
+    
+    Signed-off-by: Michael Schaffner <msf@google.com>
 
 """
 
