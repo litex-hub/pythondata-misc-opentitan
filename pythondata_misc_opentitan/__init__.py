7b614f45f564d7da71759338df8c5fbaa8c43233
LiteX Robot <robot@timvideos.us>
2021-03-06 00:33:07 +0000

    Updating pythondata-misc-opentitan to 0.0.post5266
    Updated data to v0.0-5171-g12cce14e3 based on 12cce14e3355f5df1c1d2c533a9686b88cc84b93 from https://github.com/lowRISC/opentitan.
    > commit 12cce14e3355f5df1c1d2c533a9686b88cc84b93
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Tue Mar 2 18:11:01 2021 -0800
    >
    >     [tlul] Add memory transmission integrity checks
    >
    >     - Add optional integrity generation / checks into tlul_adapter_sram
    >     - Update sram_scr to make use of the integrity error information
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    >     [top] Fix for englishbreakfast
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    >     [all] update for intg_error_o port
    >
    >     - Create alert from sram controllers
    >     - Unused for all other modules
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    >     [top] Auto generate files
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    >     [sram dv] Add extra alert
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,54 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5264"
-version_tuple = (0, 0, 5264)
+version_str = "0.0.post5266"
+version_tuple = (0, 0, 5266)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5264")
+    pversion = V("0.0.post5266")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5169"
-data_version_tuple = (0, 0, 5169)
+data_version_str = "0.0.post5171"
+data_version_tuple = (0, 0, 5171)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5169")
+    pdata_version = V("0.0.post5171")
 except ImportError:
     pass
-data_git_hash = "f77e60c36955b16daa20fccfc91589a1f21ecde9"
-data_git_describe = "v0.0-5169-gf77e60c36"
+data_git_hash = "12cce14e3355f5df1c1d2c533a9686b88cc84b93"
+data_git_describe = "v0.0-5171-g12cce14e3"
 data_git_msg = """\
-commit f77e60c36955b16daa20fccfc91589a1f21ecde9
-Author: Cindy Chen <chencindy@google.com>
-Date:   Fri Mar 5 10:44:42 2021 -0800
+commit 12cce14e3355f5df1c1d2c533a9686b88cc84b93
+Author: Timothy Chen <timothytim@google.com>
+Date:   Tue Mar 2 18:11:01 2021 -0800
 
-    [rtl/otp_ctrl] Report ECC correctable error in consistency check
+    [tlul] Add memory transmission integrity checks
     
-    This PR adds some logic to report ECC correctable error in consistency
-    check. Previous logic will silently fix the ecc correctable error
-    without reporting it to status and error bit.
+    - Add optional integrity generation / checks into tlul_adapter_sram
+    - Update sram_scr to make use of the integrity error information
     
-    Signed-off-by: Cindy Chen <chencindy@google.com>
+    Signed-off-by: Timothy Chen <timothytim@google.com>
+    
+    [top] Fix for englishbreakfast
+    
+    Signed-off-by: Timothy Chen <timothytim@google.com>
+    
+    [all] update for intg_error_o port
+    
+    - Create alert from sram controllers
+    - Unused for all other modules
+    
+    Signed-off-by: Timothy Chen <timothytim@google.com>
+    
+    [top] Auto generate files
+    
+    Signed-off-by: Timothy Chen <timothytim@google.com>
+    
+    [sram dv] Add extra alert
+    
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
