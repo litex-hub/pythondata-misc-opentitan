9f8173913291556d1d9f2581eb9f591e02826040
LiteX Robot <robot@timvideos.us>
2021-03-08 12:18:12 +0000

    Updating pythondata-misc-opentitan to 0.0.post5280
    Updated data to v0.0-5185-gf5fb61f2a based on f5fb61f2a41734365c039024a355630725a5b15c from https://github.com/lowRISC/opentitan.
    > commit f5fb61f2a41734365c039024a355630725a5b15c
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Wed Mar 3 12:20:55 2021 +0000
    >
    >     [reggen] Add typing annotations to gen_html.py
    >
    >     Also remove the (unused) toc and toclvl arguments. We don't have any
    >     typing checks in CI at the moment, but adding this sort of thing makes
    >     it much easier to spot if we miss something when refactoring (relevant
    >     for the multi-interface work that I'm doing at the moment).
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,37 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5279"
-version_tuple = (0, 0, 5279)
+version_str = "0.0.post5280"
+version_tuple = (0, 0, 5280)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5279")
+    pversion = V("0.0.post5280")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5184"
-data_version_tuple = (0, 0, 5184)
+data_version_str = "0.0.post5185"
+data_version_tuple = (0, 0, 5185)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5184")
+    pdata_version = V("0.0.post5185")
 except ImportError:
     pass
-data_git_hash = "2a1589419e8fd8c80f21939b810698295f73d05c"
-data_git_describe = "v0.0-5184-g2a1589419"
+data_git_hash = "f5fb61f2a41734365c039024a355630725a5b15c"
+data_git_describe = "v0.0-5185-gf5fb61f2a"
 data_git_msg = """\
-commit 2a1589419e8fd8c80f21939b810698295f73d05c
-Author: Felix Miller <felix.miller@gi-de.com>
-Date:   Sun Mar 7 18:05:39 2021 +0100
+commit f5fb61f2a41734365c039024a355630725a5b15c
+Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
+Date:   Wed Mar 3 12:20:55 2021 +0000
 
-    [otbn] add P-384 curve point addition
+    [reggen] Add typing annotations to gen_html.py
     
-    Adds routine for P-384 point addition in projective space plus
-    simple standalone test.
+    Also remove the (unused) toc and toclvl arguments. We don't have any
+    typing checks in CI at the moment, but adding this sort of thing makes
+    it much easier to spot if we miss something when refactoring (relevant
+    for the multi-interface work that I'm doing at the moment).
     
-    Signed-off-by: Felix Miller <felix.miller@gi-de.com>
+    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
 """
 
