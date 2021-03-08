b8ca1342e7146d67c8eaf0564b92210659574680
LiteX Robot <robot@timvideos.us>
2021-03-08 20:14:39 +0000

    Updating pythondata-misc-opentitan to 0.0.post5285
    Updated data to v0.0-5190-g0481a821c based on 0481a821c01424bb2271964104ce8849c258474b from https://github.com/lowRISC/opentitan.
    > commit 0481a821c01424bb2271964104ce8849c258474b
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Fri Mar 5 14:30:17 2021 -0800
    >
    >     [aon_timer] Change escalate_en control to cpu_en
    >
    >     * The issue with using escalate_en is that if the software configures
    >       the alert handling to skip from normal directly to scrap, we may not
    >       halt the watchdog timer.  There are also similar issues with other
    >       invalid states.
    >
    >     * By using cpu_en, we align the behavior of the aon_timer with that of
    >       the processing element (its main consumer) and thus is more consistent
    >       overall.  If the cpu is running, then the watchdog / wake timer should
    >       be in effect.  If the cpu is halted due to life cycle reasons, there
    >       would be no one to consume the output anyways.
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,43 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5284"
-version_tuple = (0, 0, 5284)
+version_str = "0.0.post5285"
+version_tuple = (0, 0, 5285)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5284")
+    pversion = V("0.0.post5285")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5189"
-data_version_tuple = (0, 0, 5189)
+data_version_str = "0.0.post5190"
+data_version_tuple = (0, 0, 5190)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5189")
+    pdata_version = V("0.0.post5190")
 except ImportError:
     pass
-data_git_hash = "cd3ddb33bc56d9d80bbf2da6f5020f8d03f21b21"
-data_git_describe = "v0.0-5189-gcd3ddb33b"
+data_git_hash = "0481a821c01424bb2271964104ce8849c258474b"
+data_git_describe = "v0.0-5190-g0481a821c"
 data_git_msg = """\
-commit cd3ddb33bc56d9d80bbf2da6f5020f8d03f21b21
-Author: Udi Jonnalagadda <udij@google.com>
-Date:   Thu Mar 4 20:34:43 2021 -0800
+commit 0481a821c01424bb2271964104ce8849c258474b
+Author: Timothy Chen <timothytim@google.com>
+Date:   Fri Mar 5 14:30:17 2021 -0800
 
-    [dv/sram] bijection test
+    [aon_timer] Change escalate_en control to cpu_en
     
-    this PR implements the bijection test as described in the testplan.
+    * The issue with using escalate_en is that if the software configures
+      the alert handling to skip from normal directly to scrap, we may not
+      halt the watchdog timer.  There are also similar issues with other
+      invalid states.
     
-    Signed-off-by: Udi Jonnalagadda <udij@google.com>
+    * By using cpu_en, we align the behavior of the aon_timer with that of
+      the processing element (its main consumer) and thus is more consistent
+      overall.  If the cpu is running, then the watchdog / wake timer should
+      be in effect.  If the cpu is halted due to life cycle reasons, there
+      would be no one to consume the output anyways.
+    
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
