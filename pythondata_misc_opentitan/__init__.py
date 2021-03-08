7cd441a609b3b78bd5daa29144193568265b38de
LiteX Robot <robot@timvideos.us>
2021-03-08 18:16:23 +0000

    Updating pythondata-misc-opentitan to 0.0.post5283
    Updated data to v0.0-5188-gd55eaeb0e based on d55eaeb0e51b9e2fa79c406fa6328b99f382796a from https://github.com/lowRISC/opentitan.
    > commit d55eaeb0e51b9e2fa79c406fa6328b99f382796a
    > Author: Eunchan Kim <eunchan@opentitan.org>
    > Date:   Thu Feb 11 12:38:15 2021 -0800
    >
    >     [spi_device] Command Process block
    >
    >     This commit implements SPI Flash command process block. It decodes first
    >     8bit of received data and triggers proper downstream module to handle
    >     the received commands.
    >
    >     Signed-off-by: Eunchan Kim <eunchan@opentitan.org>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,37 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5280"
-version_tuple = (0, 0, 5280)
+version_str = "0.0.post5283"
+version_tuple = (0, 0, 5283)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5280")
+    pversion = V("0.0.post5283")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5185"
-data_version_tuple = (0, 0, 5185)
+data_version_str = "0.0.post5188"
+data_version_tuple = (0, 0, 5188)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5185")
+    pdata_version = V("0.0.post5188")
 except ImportError:
     pass
-data_git_hash = "f5fb61f2a41734365c039024a355630725a5b15c"
-data_git_describe = "v0.0-5185-gf5fb61f2a"
+data_git_hash = "d55eaeb0e51b9e2fa79c406fa6328b99f382796a"
+data_git_describe = "v0.0-5188-gd55eaeb0e"
 data_git_msg = """\
-commit f5fb61f2a41734365c039024a355630725a5b15c
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Wed Mar 3 12:20:55 2021 +0000
+commit d55eaeb0e51b9e2fa79c406fa6328b99f382796a
+Author: Eunchan Kim <eunchan@opentitan.org>
+Date:   Thu Feb 11 12:38:15 2021 -0800
 
-    [reggen] Add typing annotations to gen_html.py
+    [spi_device] Command Process block
     
-    Also remove the (unused) toc and toclvl arguments. We don't have any
-    typing checks in CI at the moment, but adding this sort of thing makes
-    it much easier to spot if we miss something when refactoring (relevant
-    for the multi-interface work that I'm doing at the moment).
+    This commit implements SPI Flash command process block. It decodes first
+    8bit of received data and triggers proper downstream module to handle
+    the received commands.
     
-    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>
 
 """
 
