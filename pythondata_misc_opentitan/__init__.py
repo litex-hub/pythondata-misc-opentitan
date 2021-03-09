f78ff6c2f4afd72cf440772497fc559bef374634
LiteX Robot <robot@timvideos.us>
2021-03-09 06:15:09 +0000

    Updating pythondata-misc-opentitan to 0.0.post5296
    Updated data to v0.0-5201-g915df6982 based on 915df6982ec6094bd351fa9db15ee9d22a2c1cd7 from https://github.com/lowRISC/opentitan.
    > commit 915df6982ec6094bd351fa9db15ee9d22a2c1cd7
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Fri Mar 5 13:16:36 2021 -0800
    >
    >     [tlul] Enable tlul integrity check across the design
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    >     [tlul] Minor enhancements
    >
    >     - remove commented out code after placing tlul_host_adapter inside lc_ctrl
    >     - add full width parameters to tlul_pkg.sv
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    >     [dv] Minor hacks to get tlul integrity checks going
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    >     [top] Auto generate files
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    >     [tlul] correct comment typo
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,49 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5295"
-version_tuple = (0, 0, 5295)
+version_str = "0.0.post5296"
+version_tuple = (0, 0, 5296)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5295")
+    pversion = V("0.0.post5296")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5200"
-data_version_tuple = (0, 0, 5200)
+data_version_str = "0.0.post5201"
+data_version_tuple = (0, 0, 5201)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5200")
+    pdata_version = V("0.0.post5201")
 except ImportError:
     pass
-data_git_hash = "e17f6b5826dae013e7d29b088c665578618426e8"
-data_git_describe = "v0.0-5200-ge17f6b582"
+data_git_hash = "915df6982ec6094bd351fa9db15ee9d22a2c1cd7"
+data_git_describe = "v0.0-5201-g915df6982"
 data_git_msg = """\
-commit e17f6b5826dae013e7d29b088c665578618426e8
+commit 915df6982ec6094bd351fa9db15ee9d22a2c1cd7
 Author: Timothy Chen <timothytim@google.com>
-Date:   Mon Mar 8 13:04:22 2021 -0800
+Date:   Fri Mar 5 13:16:36 2021 -0800
 
-    [aon_timer] Add basic dv tb
+    [tlul] Enable tlul integrity check across the design
     
-    - only thing that is tested is CSR
+    Signed-off-by: Timothy Chen <timothytim@google.com>
+    
+    [tlul] Minor enhancements
+    
+    - remove commented out code after placing tlul_host_adapter inside lc_ctrl
+    - add full width parameters to tlul_pkg.sv
+    
+    Signed-off-by: Timothy Chen <timothytim@google.com>
+    
+    [dv] Minor hacks to get tlul integrity checks going
+    
+    Signed-off-by: Timothy Chen <timothytim@google.com>
+    
+    [top] Auto generate files
+    
+    Signed-off-by: Timothy Chen <timothytim@google.com>
+    
+    [tlul] correct comment typo
     
     Signed-off-by: Timothy Chen <timothytim@google.com>
 
