40552699e6a7ed851837f5e96a19a7876ddc6124
LiteX Robot <robot@timvideos.us>
2021-03-10 12:18:27 +0000

    Updating pythondata-misc-opentitan to 0.0.post5318
    Updated data to v0.0-5223-g7247d8aa6 based on 7247d8aa62c7fa1878cf91c7bc8a1b2b8dd3fc9e from https://github.com/lowRISC/opentitan.
    > commit 7247d8aa62c7fa1878cf91c7bc8a1b2b8dd3fc9e
    > Author: Greg Chadwick <gac@lowrisc.org>
    > Date:   Fri Mar 5 18:07:38 2021 +0000
    >
    >     [otbn] Fix read/write enables in tracer
    >
    >     * `insn_valid` must be factored in with the read enable for the bignum
    >       register file as it uses the raw decoder signal.
    >     * `rf_base_wr_commit` must be combined with the write enable for the
    >       base register file to determine if the write occurred.
    >
    >     Signed-off-by: Greg Chadwick <gac@lowrisc.org>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,46 +4,37 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5317"
-version_tuple = (0, 0, 5317)
+version_str = "0.0.post5318"
+version_tuple = (0, 0, 5318)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5317")
+    pversion = V("0.0.post5318")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5222"
-data_version_tuple = (0, 0, 5222)
+data_version_str = "0.0.post5223"
+data_version_tuple = (0, 0, 5223)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5222")
+    pdata_version = V("0.0.post5223")
 except ImportError:
     pass
-data_git_hash = "cfcfbce85e182c127b8c4be5cd8bf531e0a4d927"
-data_git_describe = "v0.0-5222-gcfcfbce85"
+data_git_hash = "7247d8aa62c7fa1878cf91c7bc8a1b2b8dd3fc9e"
+data_git_describe = "v0.0-5223-g7247d8aa6"
 data_git_msg = """\
-commit cfcfbce85e182c127b8c4be5cd8bf531e0a4d927
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Tue Mar 9 14:14:30 2021 +0000
+commit 7247d8aa62c7fa1878cf91c7bc8a1b2b8dd3fc9e
+Author: Greg Chadwick <gac@lowrisc.org>
+Date:   Fri Mar 5 18:07:38 2021 +0000
 
-    [reggen] Promote FOO_SIZE to an int unsigned in *_reg_pkg.sv
+    [otbn] Fix read/write enables in tracer
     
-    With the previous version of the code, there was a problem if the last
-    window happened to finish on the same power of 2 boundary as the
-    block's address space. In that case, we ended up with something like
+    * `insn_valid` must be factored in with the read enable for the bignum
+      register file as it uses the raw decoder signal.
+    * `rf_base_wr_commit` must be combined with the write enable for the
+      base register file to determine if the write occurred.
     
-        parameter logic [9:0] BLOCK_FOO_SIZE = 10'd1024;
-    
-    which is, of course, zero.
-    
-    It turns out that the only code in the tree that uses these _SIZE
-    parameters is using them to compute int parameters (rather than
-    something involving the address width). As such, we can just promote
-    the parameter to an int unsigned, fixing various width mismatches as a
-    bonus!
-    
-    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    Signed-off-by: Greg Chadwick <gac@lowrisc.org>
 
 """
 
