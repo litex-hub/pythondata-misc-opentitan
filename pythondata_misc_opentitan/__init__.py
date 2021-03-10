b5682d630c3d1d2a20ca45b9a05008a79c137294
LiteX Robot <robot@timvideos.us>
2021-03-10 02:28:17 +0000

    Updating pythondata-misc-opentitan to 0.0.post5314
    Updated data to v0.0-5219-g44ab47bf0 based on 44ab47bf0c323a02d59c39ce6c98162338db819e from https://github.com/lowRISC/opentitan.
    > commit 44ab47bf0c323a02d59c39ce6c98162338db819e
    > Author: Cindy Chen <chencindy@google.com>
    > Date:   Mon Mar 8 12:22:02 2021 -0800
    >
    >     [dv/alert] Add fatal alert check if scb is disabled
    >
    >     This PR addes a fatal alert check (check if fatal alert continuously
    >     triggered until reset in a nonblocking task).
    >     This can be used in aes shadow reg tests and keymgr tests where the scb
    >     is disabled.
    >
    >     Signed-off-by: Cindy Chen <chencindy@google.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5313"
-version_tuple = (0, 0, 5313)
+version_str = "0.0.post5314"
+version_tuple = (0, 0, 5314)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5313")
+    pversion = V("0.0.post5314")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5218"
-data_version_tuple = (0, 0, 5218)
+data_version_str = "0.0.post5219"
+data_version_tuple = (0, 0, 5219)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5218")
+    pdata_version = V("0.0.post5219")
 except ImportError:
     pass
-data_git_hash = "93daf962b2b90b42879ac0f6ea8b0a17a9a0cb50"
-data_git_describe = "v0.0-5218-g93daf962b"
+data_git_hash = "44ab47bf0c323a02d59c39ce6c98162338db819e"
+data_git_describe = "v0.0-5219-g44ab47bf0"
 data_git_msg = """\
-commit 93daf962b2b90b42879ac0f6ea8b0a17a9a0cb50
+commit 44ab47bf0c323a02d59c39ce6c98162338db819e
 Author: Cindy Chen <chencindy@google.com>
-Date:   Sun Mar 7 15:16:46 2021 -0800
+Date:   Mon Mar 8 12:22:02 2021 -0800
 
-    [dv/otp_ctrl] check timeout failure
+    [dv/alert] Add fatal alert check if scb is disabled
     
-    This PR supports one kind of check failure - the timeout failure.
-    The timoeut failure is triggered by setting the timeout value to a small
-    value. The timeout failure will send out a fatal alert and status
-    timeout error bit will be set.
+    This PR addes a fatal alert check (check if fatal alert continuously
+    triggered until reset in a nonblocking task).
+    This can be used in aes shadow reg tests and keymgr tests where the scb
+    is disabled.
     
     Signed-off-by: Cindy Chen <chencindy@google.com>
 
