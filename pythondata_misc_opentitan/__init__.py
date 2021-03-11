4a3814e21ec75af2574ead127ac42d10d0869298
LiteX Robot <robot@timvideos.us>
2021-03-11 17:31:20 +0000

    Updating pythondata-misc-opentitan to 0.0.post5349
    Updated data to v0.0-5254-g4abf56b97 based on 4abf56b97dcfef7f74883fad73012a285983ebc1 from https://github.com/lowRISC/opentitan.
    > commit 4abf56b97dcfef7f74883fad73012a285983ebc1
    > Author: Tom Roberts <tomroberts@lowrisc.org>
    > Date:   Thu Mar 11 14:17:30 2021 +0000
    >
    >     [aon_timer/lint] Minor lint fixes
    >
    >     Signed-off-by: Tom Roberts <tomroberts@lowrisc.org>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5345"
-version_tuple = (0, 0, 5345)
+version_str = "0.0.post5349"
+version_tuple = (0, 0, 5349)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5345")
+    pversion = V("0.0.post5349")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5250"
-data_version_tuple = (0, 0, 5250)
+data_version_str = "0.0.post5254"
+data_version_tuple = (0, 0, 5254)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5250")
+    pdata_version = V("0.0.post5254")
 except ImportError:
     pass
-data_git_hash = "e0d36964c4a56cd4dfb43900550590a45848f140"
-data_git_describe = "v0.0-5250-ge0d36964c"
+data_git_hash = "4abf56b97dcfef7f74883fad73012a285983ebc1"
+data_git_describe = "v0.0-5254-g4abf56b97"
 data_git_msg = """\
-commit e0d36964c4a56cd4dfb43900550590a45848f140
-Author: Greg Chadwick <gac@lowrisc.org>
-Date:   Thu Mar 11 11:47:45 2021 +0000
+commit 4abf56b97dcfef7f74883fad73012a285983ebc1
+Author: Tom Roberts <tomroberts@lowrisc.org>
+Date:   Thu Mar 11 14:17:30 2021 +0000
 
-    [otbn] Expand OTBN DMem/IMem width
+    [aon_timer/lint] Minor lint fixes
     
-    Extra upper bits are ignored. This increase is required to aid area
-    estimations for an OTBN design including integrity features.
-    
-    Signed-off-by: Greg Chadwick <gac@lowrisc.org>
+    Signed-off-by: Tom Roberts <tomroberts@lowrisc.org>
 
 """
 
