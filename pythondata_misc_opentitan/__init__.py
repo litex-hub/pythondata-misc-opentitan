0cd6acfc447ca7c7c5a39def4fa4ebbbafcf9bd5
LiteX Robot <robot@timvideos.us>
2021-03-11 10:15:33 +0000

    Updating pythondata-misc-opentitan to 0.0.post5342
    Updated data to v0.0-5247-gcc0dd2a76 based on cc0dd2a769f5852b7301848a050531d91f347b41 from https://github.com/lowRISC/opentitan.
    > commit cc0dd2a769f5852b7301848a050531d91f347b41
    > Author: Greg Chadwick <gac@lowrisc.org>
    > Date:   Wed Mar 10 15:55:58 2021 +0000
    >
    >     [otbn] Add second EDN connection
    >
    >     OTBN needs two connections, one for RND and URND. This purely adds the
    >     second EDN connection, RND and URND are not yet implemented.
    >
    >     Fixes #5523
    >
    >     Signed-off-by: Greg Chadwick <gac@lowrisc.org>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,49 +4,37 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5340"
-version_tuple = (0, 0, 5340)
+version_str = "0.0.post5342"
+version_tuple = (0, 0, 5342)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5340")
+    pversion = V("0.0.post5342")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5245"
-data_version_tuple = (0, 0, 5245)
+data_version_str = "0.0.post5247"
+data_version_tuple = (0, 0, 5247)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5245")
+    pdata_version = V("0.0.post5247")
 except ImportError:
     pass
-data_git_hash = "13cdb23996c6548494b5b41069592798486e1767"
-data_git_describe = "v0.0-5245-g13cdb2399"
+data_git_hash = "cc0dd2a769f5852b7301848a050531d91f347b41"
+data_git_describe = "v0.0-5247-gcc0dd2a76"
 data_git_msg = """\
-commit 13cdb23996c6548494b5b41069592798486e1767
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Tue Mar 9 15:34:53 2021 +0000
+commit cc0dd2a769f5852b7301848a050531d91f347b41
+Author: Greg Chadwick <gac@lowrisc.org>
+Date:   Wed Mar 10 15:55:58 2021 +0000
 
-    [topgen] Remove alert_module and interrupt_module from top_*.hjson
+    [otbn] Add second EDN connection
     
-    These lists actually had code to populate them in topgen/merge.py's
-    amend_alert function, but this didn't ever run because it already has
-    a value. That value means that you need to remember to update it
-    whenever you add a new instance of an IP block.
+    OTBN needs two connections, one for RND and URND. This purely adds the
+    second EDN connection, RND and URND are not yet implemented.
     
-    The original motivation behind listing these explicitly was that the
-    top-level might not want to wire up all the alerts / interrupts from
-    the modules that it instantiates. We're not doing that at the moment
-    and, if we start doing that again, it's probably cleaner to explicitly
-    disable things rather than to have to add everything twice.
+    Fixes #5523
     
-    With this patch, we fix the code in amend_interrupt and amend_alert to
-    add everything. Now the only difference between that and the manual
-    list is the exact ordering of the bits in the signal. Since we access
-    these bits through auto-generated named constants anyway, we can
-    dispense with the manual step entirely.
-    
-    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    Signed-off-by: Greg Chadwick <gac@lowrisc.org>
 
 """
 
