c51f2934695923167c705b8c7cf2f759ff97b49b
LiteX Robot <robot@timvideos.us>
2021-03-11 18:17:29 +0000

    Updating pythondata-misc-opentitan to 0.0.post5350
    Updated data to v0.0-5255-gee80c35e1 based on ee80c35e1cfb3a175bee2d60b4d2691176cf2e2d from https://github.com/lowRISC/opentitan.
    > commit ee80c35e1cfb3a175bee2d60b4d2691176cf2e2d
    > Author: Cindy Chen <chencindy@google.com>
    > Date:   Tue Mar 9 15:43:23 2021 -0800
    >
    >     [dv/otp_ctrl] support err_code checking
    >
    >     This PR adds support for err_code register checking
    >
    >     Signed-off-by: Cindy Chen <chencindy@google.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5349"
-version_tuple = (0, 0, 5349)
+version_str = "0.0.post5350"
+version_tuple = (0, 0, 5350)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5349")
+    pversion = V("0.0.post5350")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5254"
-data_version_tuple = (0, 0, 5254)
+data_version_str = "0.0.post5255"
+data_version_tuple = (0, 0, 5255)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5254")
+    pdata_version = V("0.0.post5255")
 except ImportError:
     pass
-data_git_hash = "4abf56b97dcfef7f74883fad73012a285983ebc1"
-data_git_describe = "v0.0-5254-g4abf56b97"
+data_git_hash = "ee80c35e1cfb3a175bee2d60b4d2691176cf2e2d"
+data_git_describe = "v0.0-5255-gee80c35e1"
 data_git_msg = """\
-commit 4abf56b97dcfef7f74883fad73012a285983ebc1
-Author: Tom Roberts <tomroberts@lowrisc.org>
-Date:   Thu Mar 11 14:17:30 2021 +0000
+commit ee80c35e1cfb3a175bee2d60b4d2691176cf2e2d
+Author: Cindy Chen <chencindy@google.com>
+Date:   Tue Mar 9 15:43:23 2021 -0800
 
-    [aon_timer/lint] Minor lint fixes
+    [dv/otp_ctrl] support err_code checking
     
-    Signed-off-by: Tom Roberts <tomroberts@lowrisc.org>
+    This PR adds support for err_code register checking
+    
+    Signed-off-by: Cindy Chen <chencindy@google.com>
 
 """
 
