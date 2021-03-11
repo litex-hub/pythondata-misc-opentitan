340c1480032e58da40881fe323d9325fb8424b4b
LiteX Robot <robot@timvideos.us>
2021-03-11 08:16:35 +0000

    Updating pythondata-misc-opentitan to 0.0.post5340
    Updated data to v0.0-5245-g13cdb2399 based on 13cdb23996c6548494b5b41069592798486e1767 from https://github.com/lowRISC/opentitan.
    > commit 13cdb23996c6548494b5b41069592798486e1767
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Tue Mar 9 15:34:53 2021 +0000
    >
    >     [topgen] Remove alert_module and interrupt_module from top_*.hjson
    >
    >     These lists actually had code to populate them in topgen/merge.py's
    >     amend_alert function, but this didn't ever run because it already has
    >     a value. That value means that you need to remember to update it
    >     whenever you add a new instance of an IP block.
    >
    >     The original motivation behind listing these explicitly was that the
    >     top-level might not want to wire up all the alerts / interrupts from
    >     the modules that it instantiates. We're not doing that at the moment
    >     and, if we start doing that again, it's probably cleaner to explicitly
    >     disable things rather than to have to add everything twice.
    >
    >     With this patch, we fix the code in amend_interrupt and amend_alert to
    >     add everything. Now the only difference between that and the manual
    >     list is the exact ordering of the bits in the signal. Since we access
    >     these bits through auto-generated named constants anyway, we can
    >     dispense with the manual step entirely.
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,49 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5339"
-version_tuple = (0, 0, 5339)
+version_str = "0.0.post5340"
+version_tuple = (0, 0, 5340)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5339")
+    pversion = V("0.0.post5340")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5244"
-data_version_tuple = (0, 0, 5244)
+data_version_str = "0.0.post5245"
+data_version_tuple = (0, 0, 5245)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5244")
+    pdata_version = V("0.0.post5245")
 except ImportError:
     pass
-data_git_hash = "a0c880dff8b1a941c4e7dcf0f60342b0a7ec4075"
-data_git_describe = "v0.0-5244-ga0c880dff"
+data_git_hash = "13cdb23996c6548494b5b41069592798486e1767"
+data_git_describe = "v0.0-5245-g13cdb2399"
 data_git_msg = """\
-commit a0c880dff8b1a941c4e7dcf0f60342b0a7ec4075
-Author: Eric Shiu <eshiu@google.com>
-Date:   Tue Mar 9 15:45:07 2021 -0800
+commit 13cdb23996c6548494b5b41069592798486e1767
+Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
+Date:   Tue Mar 9 15:34:53 2021 +0000
 
-    [dcd] Added new IP to top lint config
+    [topgen] Remove alert_module and interrupt_module from top_*.hjson
     
-    Signed-off-by: Eric Shiu <eshiu@google.com>
+    These lists actually had code to populate them in topgen/merge.py's
+    amend_alert function, but this didn't ever run because it already has
+    a value. That value means that you need to remember to update it
+    whenever you add a new instance of an IP block.
+    
+    The original motivation behind listing these explicitly was that the
+    top-level might not want to wire up all the alerts / interrupts from
+    the modules that it instantiates. We're not doing that at the moment
+    and, if we start doing that again, it's probably cleaner to explicitly
+    disable things rather than to have to add everything twice.
+    
+    With this patch, we fix the code in amend_interrupt and amend_alert to
+    add everything. Now the only difference between that and the manual
+    list is the exact ordering of the bits in the signal. Since we access
+    these bits through auto-generated named constants anyway, we can
+    dispense with the manual step entirely.
+    
+    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
 """
 
