7c29c6722ccb7acd8470904ddb3b69bfbbd20553
LiteX Robot <robot@timvideos.us>
2021-03-12 12:18:25 +0000

    Updating pythondata-misc-opentitan to 0.0.post5363
    Updated data to v0.0-5268-g73b67d7b0 based on 73b67d7b026f2cb2cb5e2392f7f2c15f70fca3a6 from https://github.com/lowRISC/opentitan.
    > commit 73b67d7b026f2cb2cb5e2392f7f2c15f70fca3a6
    > Author: Rasmus Madsen <rasmus.madsen@wdc.com>
    > Date:   Thu Mar 4 02:18:24 2021 -0800
    >
    >     [aes/cov] added refine file to aes
    >
    >     Signed-off-by: Rasmus Madsen <rasmus.madsen@wdc.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5360"
-version_tuple = (0, 0, 5360)
+version_str = "0.0.post5363"
+version_tuple = (0, 0, 5363)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5360")
+    pversion = V("0.0.post5363")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5265"
-data_version_tuple = (0, 0, 5265)
+data_version_str = "0.0.post5268"
+data_version_tuple = (0, 0, 5268)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5265")
+    pdata_version = V("0.0.post5268")
 except ImportError:
     pass
-data_git_hash = "4d8438cfc7769fbc4a5c1ea7f25e5c306c8e01b3"
-data_git_describe = "v0.0-5265-g4d8438cfc"
+data_git_hash = "73b67d7b026f2cb2cb5e2392f7f2c15f70fca3a6"
+data_git_describe = "v0.0-5268-g73b67d7b0"
 data_git_msg = """\
-commit 4d8438cfc7769fbc4a5c1ea7f25e5c306c8e01b3
-Author: Matute <maturana@google.com>
-Date:   Thu Mar 11 14:32:08 2021 -0800
+commit 73b67d7b026f2cb2cb5e2392f7f2c15f70fca3a6
+Author: Rasmus Madsen <rasmus.madsen@wdc.com>
+Date:   Thu Mar 4 02:18:24 2021 -0800
 
-    [doc/dv] Fixes typos and formatting changes.
+    [aes/cov] added refine file to aes
     
-    Signed-off-by: Matute <maturana@google.com>
+    Signed-off-by: Rasmus Madsen <rasmus.madsen@wdc.com>
 
 """
 
