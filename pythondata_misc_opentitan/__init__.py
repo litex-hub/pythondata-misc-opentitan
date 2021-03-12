b1c4611b64fb1d724573127d2d69d8a4d424d8a1
LiteX Robot <robot@timvideos.us>
2021-03-12 09:13:56 +0000

    Updating pythondata-misc-opentitan to 0.0.post5360
    Updated data to v0.0-5265-g4d8438cfc based on 4d8438cfc7769fbc4a5c1ea7f25e5c306c8e01b3 from https://github.com/lowRISC/opentitan.
    > commit 4d8438cfc7769fbc4a5c1ea7f25e5c306c8e01b3
    > Author: Matute <maturana@google.com>
    > Date:   Thu Mar 11 14:32:08 2021 -0800
    >
    >     [doc/dv] Fixes typos and formatting changes.
    >
    >     Signed-off-by: Matute <maturana@google.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,50 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5358"
-version_tuple = (0, 0, 5358)
+version_str = "0.0.post5360"
+version_tuple = (0, 0, 5360)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5358")
+    pversion = V("0.0.post5360")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5263"
-data_version_tuple = (0, 0, 5263)
+data_version_str = "0.0.post5265"
+data_version_tuple = (0, 0, 5265)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5263")
+    pdata_version = V("0.0.post5265")
 except ImportError:
     pass
-data_git_hash = "11017d6bb5b510fbdd8d6c91dcb1260ee38d0fe1"
-data_git_describe = "v0.0-5263-g11017d6bb"
+data_git_hash = "4d8438cfc7769fbc4a5c1ea7f25e5c306c8e01b3"
+data_git_describe = "v0.0-5265-g4d8438cfc"
 data_git_msg = """\
-commit 11017d6bb5b510fbdd8d6c91dcb1260ee38d0fe1
-Author: Timothy Chen <timothytim@google.com>
-Date:   Wed Mar 10 15:07:53 2021 -0800
+commit 4d8438cfc7769fbc4a5c1ea7f25e5c306c8e01b3
+Author: Matute <maturana@google.com>
+Date:   Thu Mar 11 14:32:08 2021 -0800
 
-    [dcd] add auto generated tb
+    [doc/dv] Fixes typos and formatting changes.
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
-    
-    [dcd] update fifo depth
-    
-    pending #5028
-    
-    Signed-off-by: Timothy Chen <timothytim@google.com>
-    
-    [dv] add dcd csr tests to smoke regression
-    
-    Signed-off-by: Timothy Chen <timothytim@google.com>
-    
-    [dcd] updates to address comments
-    
-    Signed-off-by: Timothy Chen <timothytim@google.com>
-    
-    [dcd] Add doc placeholder
-    
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Matute <maturana@google.com>
 
 """
 
