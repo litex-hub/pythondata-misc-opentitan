123142296935cd009db776d07cfd3d6f9c13a3f5
LiteX Robot <robot@timvideos.us>
2021-03-13 01:44:45 +0000

    Updating pythondata-misc-opentitan to 0.0.post5373
    Updated data to v0.0-5278-g9bb75e906 based on 9bb75e906b09e887cacb0450d17d103e19e17afc from https://github.com/lowRISC/opentitan.
    > commit 9bb75e906b09e887cacb0450d17d103e19e17afc
    > Author: Michael Schaffner <msf@opentitan.org>
    > Date:   Fri Mar 12 15:13:31 2021 -0800
    >
    >     [ibex] Hook up ram_cfg to Ibex
    >
    >     Signed-off-by: Michael Schaffner <msf@opentitan.org>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,41 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5370"
-version_tuple = (0, 0, 5370)
+version_str = "0.0.post5373"
+version_tuple = (0, 0, 5373)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5370")
+    pversion = V("0.0.post5373")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5275"
-data_version_tuple = (0, 0, 5275)
+data_version_str = "0.0.post5278"
+data_version_tuple = (0, 0, 5278)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5275")
+    pdata_version = V("0.0.post5278")
 except ImportError:
     pass
-data_git_hash = "534416961ec3bdb9c5ac09ebc62acfecc7165300"
-data_git_describe = "v0.0-5275-g534416961"
+data_git_hash = "9bb75e906b09e887cacb0450d17d103e19e17afc"
+data_git_describe = "v0.0-5278-g9bb75e906"
 data_git_msg = """\
-commit 534416961ec3bdb9c5ac09ebc62acfecc7165300
-Author: Tom Roberts <tomroberts@lowrisc.org>
-Date:   Fri Mar 12 17:30:38 2021 +0000
+commit 9bb75e906b09e887cacb0450d17d103e19e17afc
+Author: Michael Schaffner <msf@opentitan.org>
+Date:   Fri Mar 12 15:13:31 2021 -0800
 
-    Update lowrisc_ibex to lowRISC/ibex@42827fc9
+    [ibex] Hook up ram_cfg to Ibex
     
-    Update code from upstream repository
-    https://github.com/lowRISC/ibex.git to revision
-    42827fc9cd0b2043d5d179cae46b0238a55d3652
-    
-    * [rtl/icache] Switch ECC granularity to 32bits (Tom Roberts)
-    * Update lowrisc_ip to lowRISC/opentitan@1ae03937f (Tom Roberts)
-    * [rtl] Fix lint issues (Greg Chadwick)
-    * [dv/ibex] filter out tests on a per-config basis (Udi Jonnalagadda)
-    
-    Signed-off-by: Tom Roberts <tomroberts@lowrisc.org>
+    Signed-off-by: Michael Schaffner <msf@opentitan.org>
 
 """
 
