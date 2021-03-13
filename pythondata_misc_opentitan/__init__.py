48206bd92b88453ae2921824479572085c180f01
LiteX Robot <robot@timvideos.us>
2021-03-13 02:28:40 +0000

    Updating pythondata-misc-opentitan to 0.0.post5375
    Updated data to v0.0-5280-g8e56cfc06 based on 8e56cfc0688e07bac071b46126b2317e202e6bd2 from https://github.com/lowRISC/opentitan.
    > commit 8e56cfc0688e07bac071b46126b2317e202e6bd2
    > Author: Cindy Chen <chencindy@google.com>
    > Date:   Thu Mar 11 15:31:10 2021 -0800
    >
    >     [otp_ctrl] Disable assertion due to esc_en failure
    >
    >     This PR disables an assertion from `prim_arbiter_tree`. The assertion
    >     tries to check if req should stay high until grant signal is set.
    >     However, it does not work if `lc_escalate_en` signal is set.
    >     Please feel free to discard this PR if you prefer some other ways to fix
    >     it.
    >
    >     Signed-off-by: Cindy Chen <chencindy@google.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,38 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5373"
-version_tuple = (0, 0, 5373)
+version_str = "0.0.post5375"
+version_tuple = (0, 0, 5375)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5373")
+    pversion = V("0.0.post5375")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5278"
-data_version_tuple = (0, 0, 5278)
+data_version_str = "0.0.post5280"
+data_version_tuple = (0, 0, 5280)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5278")
+    pdata_version = V("0.0.post5280")
 except ImportError:
     pass
-data_git_hash = "9bb75e906b09e887cacb0450d17d103e19e17afc"
-data_git_describe = "v0.0-5278-g9bb75e906"
+data_git_hash = "8e56cfc0688e07bac071b46126b2317e202e6bd2"
+data_git_describe = "v0.0-5280-g8e56cfc06"
 data_git_msg = """\
-commit 9bb75e906b09e887cacb0450d17d103e19e17afc
-Author: Michael Schaffner <msf@opentitan.org>
-Date:   Fri Mar 12 15:13:31 2021 -0800
+commit 8e56cfc0688e07bac071b46126b2317e202e6bd2
+Author: Cindy Chen <chencindy@google.com>
+Date:   Thu Mar 11 15:31:10 2021 -0800
 
-    [ibex] Hook up ram_cfg to Ibex
+    [otp_ctrl] Disable assertion due to esc_en failure
     
-    Signed-off-by: Michael Schaffner <msf@opentitan.org>
+    This PR disables an assertion from `prim_arbiter_tree`. The assertion
+    tries to check if req should stay high until grant signal is set.
+    However, it does not work if `lc_escalate_en` signal is set.
+    Please feel free to discard this PR if you prefer some other ways to fix
+    it.
+    
+    Signed-off-by: Cindy Chen <chencindy@google.com>
 
 """
 
