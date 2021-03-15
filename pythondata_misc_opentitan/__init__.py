a3c9b9dd5e3a5d6e85d28d97da8851aa4cfaa446
LiteX Robot <robot@timvideos.us>
2021-03-15 13:16:21 +0000

    Updating pythondata-misc-opentitan to 0.0.post5383
    Updated data to v0.0-5288-g4fe07f960 based on 4fe07f960e57d0381005de430a896502c7fedb8e from https://github.com/lowRISC/opentitan.
    > commit 4fe07f960e57d0381005de430a896502c7fedb8e
    > Author: Philipp Wagner <phw@lowrisc.org>
    > Date:   Mon Mar 15 11:06:11 2021 +0000
    >
    >     Display error message from topgen in topgen-fusesoc
    >
    >     Signed-off-by: Philipp Wagner <phw@lowrisc.org>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,40 +4,30 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5381"
-version_tuple = (0, 0, 5381)
+version_str = "0.0.post5383"
+version_tuple = (0, 0, 5383)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5381")
+    pversion = V("0.0.post5383")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5286"
-data_version_tuple = (0, 0, 5286)
+data_version_str = "0.0.post5288"
+data_version_tuple = (0, 0, 5288)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5286")
+    pdata_version = V("0.0.post5288")
 except ImportError:
     pass
-data_git_hash = "0949e6de79510494050e5aa39121162a0be7aa3b"
-data_git_describe = "v0.0-5286-g0949e6de7"
+data_git_hash = "4fe07f960e57d0381005de430a896502c7fedb8e"
+data_git_describe = "v0.0-5288-g4fe07f960"
 data_git_msg = """\
-commit 0949e6de79510494050e5aa39121162a0be7aa3b
+commit 4fe07f960e57d0381005de430a896502c7fedb8e
 Author: Philipp Wagner <phw@lowrisc.org>
-Date:   Mon Mar 8 17:01:01 2021 +0000
+Date:   Mon Mar 15 11:06:11 2021 +0000
 
-    [lint_commits] Propose a command line which signs the commit
-    
-    We do some basic checks on the commit author to check if it is likely a
-    "real name." When this check fails we give a git command line to fix it.
-    However in that command line we do not include the `--signoff` flag to add a
-    `Signed-off-by` line, meaning users following our guidance will then be
-    facing another lint failure. This is fixed in this commit, hopefully
-    making our developer experience slightly nicer.
-    
-    Also replace all uses of `-s` in this doc with `--signoff` to make it
-    clearer to users what this flag actually means.
+    Display error message from topgen in topgen-fusesoc
     
     Signed-off-by: Philipp Wagner <phw@lowrisc.org>
 
