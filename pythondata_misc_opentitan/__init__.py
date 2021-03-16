dbe1a10731c104bb404fb3a72ab66820a86e8f53
LiteX Robot <robot@timvideos.us>
2021-03-16 18:16:32 +0000

    Updating pythondata-misc-opentitan to 0.0.post5410
    Updated data to v0.0-5315-g58f329710 based on 58f3297109e4c51b6c68386f14323ac441287082 from https://github.com/lowRISC/opentitan.
    > commit 58f3297109e4c51b6c68386f14323ac441287082
    > Author: Philipp Wagner <phw@lowrisc.org>
    > Date:   Tue Mar 16 13:57:23 2021 +0000
    >
    >     [doc] Make URLs in prj.hjson files relative to the containing file
    >
    >     Paths to documentation content, such as the test plan, the design
    >     specification, etc., were given in the ipname.prj.hjson file as relative
    >     path, which was resolved relative to REPO_TOP. This makes IP blocks
    >     non-relocateable.
    >
    >     With this PR the paths become relative to the file they are written in,
    >     i.e. relative to the ipname.prj.hjson file. Absolute paths are resolved
    >     absolute to REPO_TOP, effectively producing the previous behavior.
    >
    >     Signed-off-by: Philipp Wagner <phw@lowrisc.org>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,49 +4,41 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5408"
-version_tuple = (0, 0, 5408)
+version_str = "0.0.post5410"
+version_tuple = (0, 0, 5410)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5408")
+    pversion = V("0.0.post5410")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5313"
-data_version_tuple = (0, 0, 5313)
+data_version_str = "0.0.post5315"
+data_version_tuple = (0, 0, 5315)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5313")
+    pdata_version = V("0.0.post5315")
 except ImportError:
     pass
-data_git_hash = "25468b4a8a6b3dc53f87a0ccdfb7fde34c1ef657"
-data_git_describe = "v0.0-5313-g25468b4a8"
+data_git_hash = "58f3297109e4c51b6c68386f14323ac441287082"
+data_git_describe = "v0.0-5315-g58f329710"
 data_git_msg = """\
-commit 25468b4a8a6b3dc53f87a0ccdfb7fde34c1ef657
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Thu Mar 11 15:20:10 2021 +0000
+commit 58f3297109e4c51b6c68386f14323ac441287082
+Author: Philipp Wagner <phw@lowrisc.org>
+Date:   Tue Mar 16 13:57:23 2021 +0000
 
-    [dv] Get blocks with multiple device interfaces working with chip DV
+    [doc] Make URLs in prj.hjson files relative to the containing file
     
-    The big change here is that we might now generate several *_ral_pkg.sv
-    files for a block: one per device interface. To support this,
-    gen_dv.py now has a loop over device interfaces (just like gen_rtl.py
-    and gen_fpv.py).
+    Paths to documentation content, such as the test plan, the design
+    specification, etc., were given in the ipname.prj.hjson file as relative
+    path, which was resolved relative to REPO_TOP. This makes IP blocks
+    non-relocateable.
     
-    What's more, we have the same problem as gen_fpv where we need to list
-    the generated files in the core file properly. To get that right, we
-    pull core file generation out of ralgen.py and put it into gen_fpv,
-    where we know what files we've created.
+    With this PR the paths become relative to the file they are written in,
+    i.e. relative to the ipname.prj.hjson file. Absolute paths are resolved
+    absolute to REPO_TOP, effectively producing the previous behavior.
     
-    Finally (and this is the biggest part of the patch), we split up the
-    top-level and block-level versions of uvm_reg.sv.tpl. The top-level
-    and block-level templates share functions from uvm_reg_base.sv.tpl,
-    and I think the result is a bit easier to follow. There's also a
-    liberal sprinking of big "##"-style comments to try to make the
-    template code a bit easier to follow.
-    
-    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    Signed-off-by: Philipp Wagner <phw@lowrisc.org>
 
 """
 
