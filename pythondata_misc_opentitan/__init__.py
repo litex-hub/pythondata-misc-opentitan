6d30bacaa968ecc8a23bddbaa1528c8b58a263e2
LiteX Robot <robot@timvideos.us>
2021-03-17 23:13:58 +0000

    Updating pythondata-misc-opentitan to 0.0.post5444
    Updated data to v0.0-5349-ga4e2d7f2e based on a4e2d7f2e1d7d76455558187a7792fa9176f6ee4 from https://github.com/lowRISC/opentitan.
    > commit a4e2d7f2e1d7d76455558187a7792fa9176f6ee4
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Thu Mar 11 22:34:21 2021 -0800
    >
    >     [keymgr] Actually increase salt / sw binding sizes
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,47 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5442"
-version_tuple = (0, 0, 5442)
+version_str = "0.0.post5444"
+version_tuple = (0, 0, 5444)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5442")
+    pversion = V("0.0.post5444")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5347"
-data_version_tuple = (0, 0, 5347)
+data_version_str = "0.0.post5349"
+data_version_tuple = (0, 0, 5349)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5347")
+    pdata_version = V("0.0.post5349")
 except ImportError:
     pass
-data_git_hash = "4829d28a852c38fe8db7726b6f5343de1556e6e9"
-data_git_describe = "v0.0-5347-g4829d28a8"
+data_git_hash = "a4e2d7f2e1d7d76455558187a7792fa9176f6ee4"
+data_git_describe = "v0.0-5349-ga4e2d7f2e"
 data_git_msg = """\
-commit 4829d28a852c38fe8db7726b6f5343de1556e6e9
-Author: Srikrishna Iyer <sriyer@google.com>
-Date:   Wed Mar 17 00:18:26 2021 -0700
+commit a4e2d7f2e1d7d76455558187a7792fa9176f6ee4
+Author: Timothy Chen <timothytim@google.com>
+Date:   Thu Mar 11 22:34:21 2021 -0800
 
-    [dvsim] Cosmetic updates to launcher methods
+    [keymgr] Actually increase salt / sw binding sizes
     
-    - Rearranged APIs to make them uniform
-    - Updated `_has_passed()`
-      - Renamed to `_check_status()`
-      - Returns a tuple as opposed to bool
-        - Tuple returned is status, err_msg, where status is "P" or "F"
-        - Makes it easy to pick the 'right' error message to report
-        especially when there are multiple points of failure in more complex
-        launcher system such as LSF
-    - _post_finish() now takes err_msg as additional arg to reuse more code
-    - LsfLauncher:
-      - Renamed some vars / methods
-      - Removed the bsub output file existence check since it adds runtime
-        overhead and is not needed.
-      - Some more cosmetic changes
-    
-    Signed-off-by: Srikrishna Iyer <sriyer@google.com>
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
