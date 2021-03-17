489cf1aa139b8adc68dd4f12c93c6e654f981ca3
LiteX Robot <robot@timvideos.us>
2021-03-17 20:15:05 +0000

    Updating pythondata-misc-opentitan to 0.0.post5436
    Updated data to v0.0-5341-g0a49c7e5d based on 0a49c7e5da806b8c202e446114f371c20ce29d01 from https://github.com/lowRISC/opentitan.
    > commit 0a49c7e5da806b8c202e446114f371c20ce29d01
    > Author: Eunchan Kim <eunchan@opentitan.org>
    > Date:   Wed Mar 17 10:21:36 2021 -0700
    >
    >     [spi_device] Remove cmdparse until read command path implemented
    >
    >     Removing cmdparse temporary until readcmd path is implemented and
    >     checked in the synthesis tool.
    >
    >     Signed-off-by: Eunchan Kim <eunchan@opentitan.org>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5435"
-version_tuple = (0, 0, 5435)
+version_str = "0.0.post5436"
+version_tuple = (0, 0, 5436)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5435")
+    pversion = V("0.0.post5436")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5340"
-data_version_tuple = (0, 0, 5340)
+data_version_str = "0.0.post5341"
+data_version_tuple = (0, 0, 5341)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5340")
+    pdata_version = V("0.0.post5341")
 except ImportError:
     pass
-data_git_hash = "6cc21b3666e2863e80067079ba96f6b5968d4c29"
-data_git_describe = "v0.0-5340-g6cc21b366"
+data_git_hash = "0a49c7e5da806b8c202e446114f371c20ce29d01"
+data_git_describe = "v0.0-5341-g0a49c7e5d"
 data_git_msg = """\
-commit 6cc21b3666e2863e80067079ba96f6b5968d4c29
-Author: Timothy Chen <timothytim@google.com>
-Date:   Thu Mar 11 22:04:55 2021 -0800
+commit 0a49c7e5da806b8c202e446114f371c20ce29d01
+Author: Eunchan Kim <eunchan@opentitan.org>
+Date:   Wed Mar 17 10:21:36 2021 -0700
 
-    [tlul] Two minor adapter_reg fixes
+    [spi_device] Remove cmdparse until read command path implemented
     
-    - return all 1's (similar to error case) on rdata for write transactions
-    - correct the data used for integrity generation
+    Removing cmdparse temporary until readcmd path is implemented and
+    checked in the synthesis tool.
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>
 
 """
 
