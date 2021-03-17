8358e5532b5f7f8ebdf29852f5f2905986527b08
LiteX Robot <robot@timvideos.us>
2021-03-17 08:16:35 +0000

    Updating pythondata-misc-opentitan to 0.0.post5424
    Updated data to v0.0-5329-g998b3fabc based on 998b3fabcfbf56ec093d6c13a145e245f19aba52 from https://github.com/lowRISC/opentitan.
    > commit 998b3fabcfbf56ec093d6c13a145e245f19aba52
    > Author: Philipp Wagner <phw@lowrisc.org>
    > Date:   Tue Mar 16 21:55:25 2021 +0000
    >
    >     [rv_plic] Clean up core files
    >
    >     * Only depend primitives which we actually need.
    >     * Depend on the tlul code (which is used in the SV files produced by
    >       reggen) in the core which actually includes the relevant source files.
    >     * Improve the description of core files a bit.
    >
    >     Signed-off-by: Philipp Wagner <phw@lowrisc.org>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,38 +4,37 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5421"
-version_tuple = (0, 0, 5421)
+version_str = "0.0.post5424"
+version_tuple = (0, 0, 5424)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5421")
+    pversion = V("0.0.post5424")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5326"
-data_version_tuple = (0, 0, 5326)
+data_version_str = "0.0.post5329"
+data_version_tuple = (0, 0, 5329)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5326")
+    pdata_version = V("0.0.post5329")
 except ImportError:
     pass
-data_git_hash = "b5c684b86340ac4502fd34d8b8554575669b174d"
-data_git_describe = "v0.0-5326-gb5c684b86"
+data_git_hash = "998b3fabcfbf56ec093d6c13a145e245f19aba52"
+data_git_describe = "v0.0-5329-g998b3fabc"
 data_git_msg = """\
-commit b5c684b86340ac4502fd34d8b8554575669b174d
-Author: Udi Jonnalagadda <udij@google.com>
-Date:   Thu Mar 11 14:44:55 2021 -0800
+commit 998b3fabcfbf56ec093d6c13a145e245f19aba52
+Author: Philipp Wagner <phw@lowrisc.org>
+Date:   Tue Mar 16 21:55:25 2021 +0000
 
-    [dv/sram] lc_escalation test
+    [rv_plic] Clean up core files
     
-    this PR adds the lifecycle escalation test as described by the testplan.
+    * Only depend primitives which we actually need.
+    * Depend on the tlul code (which is used in the SV files produced by
+      reggen) in the core which actually includes the relevant source files.
+    * Improve the description of core files a bit.
     
-    in this test we randomly inject an escalation request, then expect that
-    the SRAM ignore all following memory requests until a system reset is
-    issued.
-    
-    Signed-off-by: Udi Jonnalagadda <udij@google.com>
+    Signed-off-by: Philipp Wagner <phw@lowrisc.org>
 
 """
 
