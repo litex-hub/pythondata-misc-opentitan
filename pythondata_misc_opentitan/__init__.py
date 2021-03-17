935ccc54d2da13f4d725e7cee01be9addb988b3a
LiteX Robot <robot@timvideos.us>
2021-03-17 22:14:57 +0000

    Updating pythondata-misc-opentitan to 0.0.post5442
    Updated data to v0.0-5347-g4829d28a8 based on 4829d28a852c38fe8db7726b6f5343de1556e6e9 from https://github.com/lowRISC/opentitan.
    > commit 4829d28a852c38fe8db7726b6f5343de1556e6e9
    > Author: Srikrishna Iyer <sriyer@google.com>
    > Date:   Wed Mar 17 00:18:26 2021 -0700
    >
    >     [dvsim] Cosmetic updates to launcher methods
    >
    >     - Rearranged APIs to make them uniform
    >     - Updated `_has_passed()`
    >       - Renamed to `_check_status()`
    >       - Returns a tuple as opposed to bool
    >         - Tuple returned is status, err_msg, where status is "P" or "F"
    >         - Makes it easy to pick the 'right' error message to report
    >         especially when there are multiple points of failure in more complex
    >         launcher system such as LSF
    >     - _post_finish() now takes err_msg as additional arg to reuse more code
    >     - LsfLauncher:
    >       - Renamed some vars / methods
    >       - Removed the bsub output file existence check since it adds runtime
    >         overhead and is not needed.
    >       - Some more cosmetic changes
    >
    >     Signed-off-by: Srikrishna Iyer <sriyer@google.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,52 +4,47 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5437"
-version_tuple = (0, 0, 5437)
+version_str = "0.0.post5442"
+version_tuple = (0, 0, 5442)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5437")
+    pversion = V("0.0.post5442")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5342"
-data_version_tuple = (0, 0, 5342)
+data_version_str = "0.0.post5347"
+data_version_tuple = (0, 0, 5347)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5342")
+    pdata_version = V("0.0.post5347")
 except ImportError:
     pass
-data_git_hash = "6f98f3534462701e3bdbe0585782087a3f7a6dcc"
-data_git_describe = "v0.0-5342-g6f98f3534"
+data_git_hash = "4829d28a852c38fe8db7726b6f5343de1556e6e9"
+data_git_describe = "v0.0-5347-g4829d28a8"
 data_git_msg = """\
-commit 6f98f3534462701e3bdbe0585782087a3f7a6dcc
-Author: Timothy Chen <timothytim@google.com>
-Date:   Wed Mar 10 16:27:29 2021 -0800
+commit 4829d28a852c38fe8db7726b6f5343de1556e6e9
+Author: Srikrishna Iyer <sriyer@google.com>
+Date:   Wed Mar 17 00:18:26 2021 -0700
 
-    [top, dcd] top and dcd updates for integration
+    [dvsim] Cosmetic updates to launcher methods
     
-    - tie off dcd unused signals
-    - slight rename of inter-module ports
-    - hook-up in top_earlgrey.hjson
-    - hook-up to ast
+    - Rearranged APIs to make them uniform
+    - Updated `_has_passed()`
+      - Renamed to `_check_status()`
+      - Returns a tuple as opposed to bool
+        - Tuple returned is status, err_msg, where status is "P" or "F"
+        - Makes it easy to pick the 'right' error message to report
+        especially when there are multiple points of failure in more complex
+        launcher system such as LSF
+    - _post_finish() now takes err_msg as additional arg to reuse more code
+    - LsfLauncher:
+      - Renamed some vars / methods
+      - Removed the bsub output file existence check since it adds runtime
+        overhead and is not needed.
+      - Some more cosmetic changes
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
-    
-    [sw] updates to plic / pwrmgr dif unit tests
-    
-    - increase interrupt number
-    - update wakeup reasons
-    
-    Signed-off-by: Timothy Chen <timothytim@google.com>
-    
-    [top] Auto generate files
-    
-    Signed-off-by: Timothy Chen <timothytim@google.com>
-    
-    [dcd] remove non-ascii characters
-    
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Srikrishna Iyer <sriyer@google.com>
 
 """
 
