9e6df020e54fd2f9d7cfe5b3f6d6127decfe1161
LiteX Robot <robot@timvideos.us>
2021-03-18 14:14:24 +0000

    Updating pythondata-misc-opentitan to 0.0.post5460
    Updated data to v0.0-5365-g7fa334247 based on 7fa33424764f4f6b8baeb84c5440db6200b0fc16 from https://github.com/lowRISC/opentitan.
    > commit 7fa33424764f4f6b8baeb84c5440db6200b0fc16
    > Author: Silvestrs Timofejevs <silvestrst@lowrisc.org>
    > Date:   Tue Mar 9 12:11:59 2021 +0000
    >
    >     [test, systemtest] Add MaskROM + ROM_EXT apps selfchecking
    >
    >     This change allows to run both set of tests in CI (boot_rom and Silicon
    >     Creator). At the moment only a single smoke test (UART) has been added
    >     to run via Silicon Creator code.
    >
    >     Rationale:
    >     The main idea is to add some intial MaskROM + ROM_EXT testing without
    >     disturbing the existing test set-up.
    >
    >     The new added test has passed, checked locally:
    >     ```
    >     cd $REPO_TOP
    >     mkdir build-bin/hw
    >
    >     ln -sf \
    >     `pwd`/build/lowrisc_systems_top_earlgrey_verilator_0.1/sim-verilator \
    >     build-bin/hw/top_earlgrey
    >
    >     pytest test/systemtest/earlgrey/test_sim_verilator.py
    >     ```
    >
    >     Signed-off-by: Silvestrs Timofejevs <silvestrst@lowrisc.org>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,37 +4,52 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5456"
-version_tuple = (0, 0, 5456)
+version_str = "0.0.post5460"
+version_tuple = (0, 0, 5460)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5456")
+    pversion = V("0.0.post5460")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5361"
-data_version_tuple = (0, 0, 5361)
+data_version_str = "0.0.post5365"
+data_version_tuple = (0, 0, 5365)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5361")
+    pdata_version = V("0.0.post5365")
 except ImportError:
     pass
-data_git_hash = "b1e4b6d9f02e98546200ca88a147a370f3ecc8ef"
-data_git_describe = "v0.0-5361-gb1e4b6d9f"
+data_git_hash = "7fa33424764f4f6b8baeb84c5440db6200b0fc16"
+data_git_describe = "v0.0-5365-g7fa334247"
 data_git_msg = """\
-commit b1e4b6d9f02e98546200ca88a147a370f3ecc8ef
-Author: Tom Roberts <tomroberts@lowrisc.org>
-Date:   Tue Mar 16 15:43:44 2021 +0000
+commit 7fa33424764f4f6b8baeb84c5440db6200b0fc16
+Author: Silvestrs Timofejevs <silvestrst@lowrisc.org>
+Date:   Tue Mar 9 12:11:59 2021 +0000
 
-    [aon_timer/rtl] Fix bugs found by smoke test
+    [test, systemtest] Add MaskROM + ROM_EXT apps selfchecking
     
-    Lifecycle input signals are the wrong way around.
-    Only clock the prescale counter when enabled.
+    This change allows to run both set of tests in CI (boot_rom and Silicon
+    Creator). At the moment only a single smoke test (UART) has been added
+    to run via Silicon Creator code.
     
-    Relates to #5624
+    Rationale:
+    The main idea is to add some intial MaskROM + ROM_EXT testing without
+    disturbing the existing test set-up.
     
-    Signed-off-by: Tom Roberts <tomroberts@lowrisc.org>
+    The new added test has passed, checked locally:
+    ```
+    cd $REPO_TOP
+    mkdir build-bin/hw
+    
+    ln -sf \
+    `pwd`/build/lowrisc_systems_top_earlgrey_verilator_0.1/sim-verilator \
+    build-bin/hw/top_earlgrey
+    
+    pytest test/systemtest/earlgrey/test_sim_verilator.py
+    ```
+    
+    Signed-off-by: Silvestrs Timofejevs <silvestrst@lowrisc.org>
 
 """
 
