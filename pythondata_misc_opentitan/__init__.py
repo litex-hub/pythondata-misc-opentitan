cca77d8447b402d4a1b272806ae4d9f124aab933
LiteX Robot <robot@timvideos.us>
2021-03-18 16:23:03 +0000

    Updating pythondata-misc-opentitan to 0.0.post5464
    Updated data to v0.0-5369-gd0ca7e97f based on d0ca7e97ffd988d9d9ec27a73d876ff009b29b79 from https://github.com/lowRISC/opentitan.
    > commit d0ca7e97ffd988d9d9ec27a73d876ff009b29b79
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Mon Mar 15 13:00:32 2021 +0000
    >
    >     [ci] Be more informative if install-package-dependencies fails
    >
    >     Also, use a temporary directory to avoid polluting the checkout and
    >     move the logic to a shell script (no real change from the yaml file,
    >     but this script has got big enough that it doesn't feel like it should
    >     be inlined any more).
    >
    >     Splitting out into a shell script means a bit of extra work, because
    >     we have to pass Azure Pipeline variables through to the script. Use
    >     proper command line arguments, rather than environment variables, to
    >     make it a bit more obvious what's going on.
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,52 +4,42 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5460"
-version_tuple = (0, 0, 5460)
+version_str = "0.0.post5464"
+version_tuple = (0, 0, 5464)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5460")
+    pversion = V("0.0.post5464")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5365"
-data_version_tuple = (0, 0, 5365)
+data_version_str = "0.0.post5369"
+data_version_tuple = (0, 0, 5369)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5365")
+    pdata_version = V("0.0.post5369")
 except ImportError:
     pass
-data_git_hash = "7fa33424764f4f6b8baeb84c5440db6200b0fc16"
-data_git_describe = "v0.0-5365-g7fa334247"
+data_git_hash = "d0ca7e97ffd988d9d9ec27a73d876ff009b29b79"
+data_git_describe = "v0.0-5369-gd0ca7e97f"
 data_git_msg = """\
-commit 7fa33424764f4f6b8baeb84c5440db6200b0fc16
-Author: Silvestrs Timofejevs <silvestrst@lowrisc.org>
-Date:   Tue Mar 9 12:11:59 2021 +0000
+commit d0ca7e97ffd988d9d9ec27a73d876ff009b29b79
+Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
+Date:   Mon Mar 15 13:00:32 2021 +0000
 
-    [test, systemtest] Add MaskROM + ROM_EXT apps selfchecking
+    [ci] Be more informative if install-package-dependencies fails
     
-    This change allows to run both set of tests in CI (boot_rom and Silicon
-    Creator). At the moment only a single smoke test (UART) has been added
-    to run via Silicon Creator code.
+    Also, use a temporary directory to avoid polluting the checkout and
+    move the logic to a shell script (no real change from the yaml file,
+    but this script has got big enough that it doesn't feel like it should
+    be inlined any more).
     
-    Rationale:
-    The main idea is to add some intial MaskROM + ROM_EXT testing without
-    disturbing the existing test set-up.
+    Splitting out into a shell script means a bit of extra work, because
+    we have to pass Azure Pipeline variables through to the script. Use
+    proper command line arguments, rather than environment variables, to
+    make it a bit more obvious what's going on.
     
-    The new added test has passed, checked locally:
-    ```
-    cd $REPO_TOP
-    mkdir build-bin/hw
-    
-    ln -sf \
-    `pwd`/build/lowrisc_systems_top_earlgrey_verilator_0.1/sim-verilator \
-    build-bin/hw/top_earlgrey
-    
-    pytest test/systemtest/earlgrey/test_sim_verilator.py
-    ```
-    
-    Signed-off-by: Silvestrs Timofejevs <silvestrst@lowrisc.org>
+    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
 """
 
