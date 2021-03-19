c4e62e0b16be2936254813f8d259b5fd500169a1
LiteX Robot <robot@timvideos.us>
2021-03-19 15:14:33 +0000

    Updating pythondata-misc-opentitan to 0.0.post5486
    Updated data to v0.0-5391-g967319ded based on 967319ded21fa93734dcc8a2756099dd370b8071 from https://github.com/lowRISC/opentitan.
    > commit 967319ded21fa93734dcc8a2756099dd370b8071
    > Author: Tom Roberts <tomroberts@lowrisc.org>
    > Date:   Fri Mar 19 09:18:42 2021 +0000
    >
    >     [aon_timer] Lint fixes
    >
    >     Ascentlint doesn't pick up the unused_* bits from inside a struct and so
    >     is failing on the intr_state bits. This commit lists them out explicitly
    >     which will hopefully silence the warning.
    >
    >     Signed-off-by: Tom Roberts <tomroberts@lowrisc.org>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,38 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5484"
-version_tuple = (0, 0, 5484)
+version_str = "0.0.post5486"
+version_tuple = (0, 0, 5486)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5484")
+    pversion = V("0.0.post5486")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5389"
-data_version_tuple = (0, 0, 5389)
+data_version_str = "0.0.post5391"
+data_version_tuple = (0, 0, 5391)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5389")
+    pdata_version = V("0.0.post5391")
 except ImportError:
     pass
-data_git_hash = "0e1e8aad9e3065fa7cb8354d211fe6e0a6fdc419"
-data_git_describe = "v0.0-5389-g0e1e8aad9"
+data_git_hash = "967319ded21fa93734dcc8a2756099dd370b8071"
+data_git_describe = "v0.0-5391-g967319ded"
 data_git_msg = """\
-commit 0e1e8aad9e3065fa7cb8354d211fe6e0a6fdc419
-Author: Hoang Tung <Hoang.Tung@wdc.com>
-Date:   Sun Mar 14 23:41:18 2021 -0700
+commit 967319ded21fa93734dcc8a2756099dd370b8071
+Author: Tom Roberts <tomroberts@lowrisc.org>
+Date:   Fri Mar 19 09:18:42 2021 +0000
 
-    [pwm, dv] Add WiP DV for PWM, all *csr tests pass
+    [aon_timer] Lint fixes
     
-      - add pwm_agent (device mode) with empty driver
-        (pwm does not require response from agent)
-      - add env dv supports *csr tests
-      - add tb.sv with 2 dependent clock and reset sources (core_clk_freq <= bus_clk_freq)
-      - add pwm_testplan.hjson to the list in build_docs.py script
+    Ascentlint doesn't pick up the unused_* bits from inside a struct and so
+    is failing on the intr_state bits. This commit lists them out explicitly
+    which will hopefully silence the warning.
     
-    Signed-off-by: Hoang Tung <Hoang.Tung@wdc.com>
+    Signed-off-by: Tom Roberts <tomroberts@lowrisc.org>
 
 """
 
