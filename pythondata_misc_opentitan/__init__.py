ce2bdc225a5c1925616b80eb7cfe493dd26e7110
LiteX Robot <robot@timvideos.us>
2021-03-22 20:16:18 +0000

    Updating pythondata-misc-opentitan to 0.0.post5515
    Updated data to v0.0-5420-g4c2b5f433 based on 4c2b5f4338a93c7b48d33e48fb75478ee558772a from https://github.com/lowRISC/opentitan.
    > commit 4c2b5f4338a93c7b48d33e48fb75478ee558772a
    > Author: Weicai Yang <weicai@google.com>
    > Date:   Fri Mar 19 14:25:13 2021 -0700
    >
    >     [dv] Update driver to process item at active edge for 1st item
    >
    >     Driver should process items at active edge. For 1st item, we didn't wait
    >     for clock edge
    >
    >     Signed-off-by: Weicai Yang <weicai@google.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,37 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5514"
-version_tuple = (0, 0, 5514)
+version_str = "0.0.post5515"
+version_tuple = (0, 0, 5515)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5514")
+    pversion = V("0.0.post5515")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5419"
-data_version_tuple = (0, 0, 5419)
+data_version_str = "0.0.post5420"
+data_version_tuple = (0, 0, 5420)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5419")
+    pdata_version = V("0.0.post5420")
 except ImportError:
     pass
-data_git_hash = "1cbcce42ed2a2583eccb96c64df78468f40b0c40"
-data_git_describe = "v0.0-5419-g1cbcce42e"
+data_git_hash = "4c2b5f4338a93c7b48d33e48fb75478ee558772a"
+data_git_describe = "v0.0-5420-g4c2b5f433"
 data_git_msg = """\
-commit 1cbcce42ed2a2583eccb96c64df78468f40b0c40
-Author: Cindy Chen <chencindy@google.com>
-Date:   Thu Mar 18 19:38:03 2021 -0700
+commit 4c2b5f4338a93c7b48d33e48fb75478ee558772a
+Author: Weicai Yang <weicai@google.com>
+Date:   Fri Mar 19 14:25:13 2021 -0700
 
-    [dv/otp_ctrl] test_access sequence
+    [dv] Update driver to process item at active edge for 1st item
     
-    This PR adds a test_access sequence to access otp_ctrl's test_access.
-    For open-sourced testbench, this is a dummy check to ensure the
-    connection is correct. The real output should be checked in
-    proprietary env.
+    Driver should process items at active edge. For 1st item, we didn't wait
+    for clock edge
     
-    Signed-off-by: Cindy Chen <chencindy@google.com>
+    Signed-off-by: Weicai Yang <weicai@google.com>
 
 """
 
