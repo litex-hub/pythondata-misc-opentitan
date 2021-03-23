08d31af478ad1607dcebb8a37812b07a2f66c221
LiteX Robot <robot@timvideos.us>
2021-03-23 18:18:10 +0000

    Updating pythondata-misc-opentitan to 0.0.post5533
    Updated data to v0.0-5438-gf89627f29 based on f89627f2929fa7fa38db8f9bf21279e549612a17 from https://github.com/lowRISC/opentitan.
    > commit f89627f2929fa7fa38db8f9bf21279e549612a17
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Tue Mar 23 14:03:29 2021 +0000
    >
    >     [pwrmgr] Waive Verilator lint warning about "abort" name
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,30 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5532"
-version_tuple = (0, 0, 5532)
+version_str = "0.0.post5533"
+version_tuple = (0, 0, 5533)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5532")
+    pversion = V("0.0.post5533")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5437"
-data_version_tuple = (0, 0, 5437)
+data_version_str = "0.0.post5438"
+data_version_tuple = (0, 0, 5438)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5437")
+    pdata_version = V("0.0.post5438")
 except ImportError:
     pass
-data_git_hash = "04b1c25924dfc22247a80f44a98577e95c54f994"
-data_git_describe = "v0.0-5437-g04b1c2592"
+data_git_hash = "f89627f2929fa7fa38db8f9bf21279e549612a17"
+data_git_describe = "v0.0-5438-gf89627f29"
 data_git_msg = """\
-commit 04b1c25924dfc22247a80f44a98577e95c54f994
+commit f89627f2929fa7fa38db8f9bf21279e549612a17
 Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Thu Mar 18 12:22:34 2021 +0000
+Date:   Tue Mar 23 14:03:29 2021 +0000
 
-    [otbn] Use DpiMemutil / MemArea to access memory for otbn_model.cc
-    
-    This removes the duplicated calls to simutil_get_mem and
-    simutil_set_mem. It also avoids duplicating the SV paths to the guts
-    of the memories: they were in otbn_memutil.cc and otbn.sv and
-    otbn_top_sim.sv; now they're just in otbn_memutil.cc.
+    [pwrmgr] Waive Verilator lint warning about "abort" name
     
     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
