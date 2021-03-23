7efbada46d4cdd308f2093d2458001f9586096c8
LiteX Robot <robot@timvideos.us>
2021-03-23 23:15:36 +0000

    Updating pythondata-misc-opentitan to 0.0.post5539
    Updated data to v0.0-5444-g02eaac710 based on 02eaac710afb2080e64706e73b1b5d0f8d017e1b from https://github.com/lowRISC/opentitan.
    > commit 02eaac710afb2080e64706e73b1b5d0f8d017e1b
    > Author: Eunchan Kim <eunchan@opentitan.org>
    > Date:   Tue Mar 23 10:54:25 2021 -0700
    >
    >     [kmac] Moving KDF struct to KMAC IP
    >
    >     Previously KeyMgr KDF interface was defined in KeyMgr IP. The interface
    >     has been used for KeyMgr to initiate KMAC operation via side channel
    >     interface.
    >
    >     As rom_ctrl and otp_ctrl plan to use KMAC as signature verification
    >     function, KMAC needs to have more general application interface now.
    >
    >     This commit is to move the KDF struct into KMAC and renames it to
    >     `app_{req/rsp}_t`.
    >
    >     Signed-off-by: Eunchan Kim <eunchan@opentitan.org>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,42 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5538"
-version_tuple = (0, 0, 5538)
+version_str = "0.0.post5539"
+version_tuple = (0, 0, 5539)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5538")
+    pversion = V("0.0.post5539")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5443"
-data_version_tuple = (0, 0, 5443)
+data_version_str = "0.0.post5444"
+data_version_tuple = (0, 0, 5444)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5443")
+    pdata_version = V("0.0.post5444")
 except ImportError:
     pass
-data_git_hash = "a96975a5f4a551d804bbd60871712e38eae86248"
-data_git_describe = "v0.0-5443-ga96975a5f"
+data_git_hash = "02eaac710afb2080e64706e73b1b5d0f8d017e1b"
+data_git_describe = "v0.0-5444-g02eaac710"
 data_git_msg = """\
-commit a96975a5f4a551d804bbd60871712e38eae86248
-Author: Miguel Osorio <miguelosorio@google.com>
-Date:   Tue Mar 23 13:26:09 2021 -0700
+commit 02eaac710afb2080e64706e73b1b5d0f8d017e1b
+Author: Eunchan Kim <eunchan@opentitan.org>
+Date:   Tue Mar 23 10:54:25 2021 -0700
 
-    [sw/dif] Entropy DIF updates
+    [kmac] Moving KDF struct to KMAC IP
     
-    * Documentation updates
-    * Switch constants to enum values
+    Previously KeyMgr KDF interface was defined in KeyMgr IP. The interface
+    has been used for KeyMgr to initiate KMAC operation via side channel
+    interface.
     
-    Signed-off-by: Miguel Osorio <miguelosorio@google.com>
+    As rom_ctrl and otp_ctrl plan to use KMAC as signature verification
+    function, KMAC needs to have more general application interface now.
+    
+    This commit is to move the KDF struct into KMAC and renames it to
+    `app_{req/rsp}_t`.
+    
+    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>
 
 """
 
