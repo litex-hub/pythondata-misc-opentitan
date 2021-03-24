2d9d9e6978c518fdcdb6d9bce451bd7d8bbabcc2
LiteX Robot <robot@timvideos.us>
2021-03-24 04:05:51 +0000

    Updating pythondata-misc-opentitan to 0.0.post5542
    Updated data to v0.0-5447-g37b0c9944 based on 37b0c994439615f70b45202b1f90e15dc5510dd1 from https://github.com/lowRISC/opentitan.
    > commit 37b0c994439615f70b45202b1f90e15dc5510dd1
    > Author: Srikrishna Iyer <sriyer@google.com>
    > Date:   Tue Mar 23 16:27:26 2021 -0700
    >
    >     [dvsim] Spot fixes for LSF and internal launcher
    >
    >     Over the past few weeks, the scheduling and launching mechanism were
    >     refactored to fully support LSF and our Google cloud which is used
    >     internally.
    >
    >     The fixes made in this PR are minor ones to support our internal cloud
    >     launcher (which will not be committed to our open repo).
    >
    >     The fixes are as follows:
    >     - Updates to commands and env vars in the HJson data
    >     - Addition of `input_dirs` and `output_dirs` variables in `Deploy`
    >       - This is needed only for the cloud launcher because jobs with cloud
    >       run in a hermetically sealed VM which needs to explicitly indicate
    >       what the input and output data is.
    >       - We will refactor this again in future when we will flip the Deploy
    >       and Launcher hierarchy.
    >     - Fixes to command sub-strings (ensure they are quoted properly)
    >     - Allow remote launchers to set their own python virtualenv region
    >     - Some cosmetic changes
    >
    >     Signed-off-by: Srikrishna Iyer <sriyer@google.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,51 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5541"
-version_tuple = (0, 0, 5541)
+version_str = "0.0.post5542"
+version_tuple = (0, 0, 5542)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5541")
+    pversion = V("0.0.post5542")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5446"
-data_version_tuple = (0, 0, 5446)
+data_version_str = "0.0.post5447"
+data_version_tuple = (0, 0, 5447)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5446")
+    pdata_version = V("0.0.post5447")
 except ImportError:
     pass
-data_git_hash = "5c6da15f1b279a95d189974795edcb510c0e2323"
-data_git_describe = "v0.0-5446-g5c6da15f1"
+data_git_hash = "37b0c994439615f70b45202b1f90e15dc5510dd1"
+data_git_describe = "v0.0-5447-g37b0c9944"
 data_git_msg = """\
-commit 5c6da15f1b279a95d189974795edcb510c0e2323
-Author: Cindy Chen <chencindy@google.com>
-Date:   Tue Mar 23 16:44:40 2021 -0700
+commit 37b0c994439615f70b45202b1f90e15dc5510dd1
+Author: Srikrishna Iyer <sriyer@google.com>
+Date:   Tue Mar 23 16:27:26 2021 -0700
 
-    [fpv/rstmgr] FPV compile error
+    [dvsim] Spot fixes for LSF and internal launcher
     
-    This PR fixes a compile error in FPV. I will contact the AE and see if
-    they can fix it. Right now the prim_lc_sync syntax works for simulation
-    but not FPV.
+    Over the past few weeks, the scheduling and launching mechanism were
+    refactored to fully support LSF and our Google cloud which is used
+    internally.
     
-    Signed-off-by: Cindy Chen <chencindy@google.com>
+    The fixes made in this PR are minor ones to support our internal cloud
+    launcher (which will not be committed to our open repo).
+    
+    The fixes are as follows:
+    - Updates to commands and env vars in the HJson data
+    - Addition of `input_dirs` and `output_dirs` variables in `Deploy`
+      - This is needed only for the cloud launcher because jobs with cloud
+      run in a hermetically sealed VM which needs to explicitly indicate
+      what the input and output data is.
+      - We will refactor this again in future when we will flip the Deploy
+      and Launcher hierarchy.
+    - Fixes to command sub-strings (ensure they are quoted properly)
+    - Allow remote launchers to set their own python virtualenv region
+    - Some cosmetic changes
+    
+    Signed-off-by: Srikrishna Iyer <sriyer@google.com>
 
 """
 
