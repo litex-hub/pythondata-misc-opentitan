6deba5a5a61d188f7917e6537caad83b6b9a324d
LiteX Robot <robot@timvideos.us>
2021-03-24 08:05:44 +0000

    Updating pythondata-misc-opentitan to 0.0.post5543
    Updated data to v0.0-5448-gf2b7ef068 based on f2b7ef068d6414314c2f0f51ea73d8d1c339f1dd from https://github.com/lowRISC/opentitan.
    > commit f2b7ef068d6414314c2f0f51ea73d8d1c339f1dd
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Mon Mar 22 11:19:10 2021 +0000
    >
    >     [otbn] Add a "multi" vseq to run binaries back-to-back
    >
    >     This should catch errors where we don't re-initialise enough of the
    >     design when starting a new operation. It intentionally doesn't do a
    >     reset, which would hide that sort of thing.
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,51 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5542"
-version_tuple = (0, 0, 5542)
+version_str = "0.0.post5543"
+version_tuple = (0, 0, 5543)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5542")
+    pversion = V("0.0.post5543")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5447"
-data_version_tuple = (0, 0, 5447)
+data_version_str = "0.0.post5448"
+data_version_tuple = (0, 0, 5448)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5447")
+    pdata_version = V("0.0.post5448")
 except ImportError:
     pass
-data_git_hash = "37b0c994439615f70b45202b1f90e15dc5510dd1"
-data_git_describe = "v0.0-5447-g37b0c9944"
+data_git_hash = "f2b7ef068d6414314c2f0f51ea73d8d1c339f1dd"
+data_git_describe = "v0.0-5448-gf2b7ef068"
 data_git_msg = """\
-commit 37b0c994439615f70b45202b1f90e15dc5510dd1
-Author: Srikrishna Iyer <sriyer@google.com>
-Date:   Tue Mar 23 16:27:26 2021 -0700
+commit f2b7ef068d6414314c2f0f51ea73d8d1c339f1dd
+Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
+Date:   Mon Mar 22 11:19:10 2021 +0000
 
-    [dvsim] Spot fixes for LSF and internal launcher
+    [otbn] Add a "multi" vseq to run binaries back-to-back
     
-    Over the past few weeks, the scheduling and launching mechanism were
-    refactored to fully support LSF and our Google cloud which is used
-    internally.
+    This should catch errors where we don't re-initialise enough of the
+    design when starting a new operation. It intentionally doesn't do a
+    reset, which would hide that sort of thing.
     
-    The fixes made in this PR are minor ones to support our internal cloud
-    launcher (which will not be committed to our open repo).
-    
-    The fixes are as follows:
-    - Updates to commands and env vars in the HJson data
-    - Addition of `input_dirs` and `output_dirs` variables in `Deploy`
-      - This is needed only for the cloud launcher because jobs with cloud
-      run in a hermetically sealed VM which needs to explicitly indicate
-      what the input and output data is.
-      - We will refactor this again in future when we will flip the Deploy
-      and Launcher hierarchy.
-    - Fixes to command sub-strings (ensure they are quoted properly)
-    - Allow remote launchers to set their own python virtualenv region
-    - Some cosmetic changes
-    
-    Signed-off-by: Srikrishna Iyer <sriyer@google.com>
+    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
 """
 
