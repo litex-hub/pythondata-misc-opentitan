505af04b9a069fb524e8ab716d2ceb607a6c9ec6
LiteX Robot <robot@timvideos.us>
2021-03-24 21:04:56 +0000

    Updating pythondata-misc-opentitan to 0.0.post5569
    Updated data to v0.0-5474-g5f1d304ad based on 5f1d304ad5c63a428219d1881479707727a559de from https://github.com/lowRISC/opentitan.
    > commit 5f1d304ad5c63a428219d1881479707727a559de
    > Author: Eric Shiu <eshiu@google.com>
    > Date:   Wed Mar 17 17:24:11 2021 -0700
    >
    >     [adc_ctrl] rename dcd to adc_ctrl
    >
    >     Signed-off-by: Eric Shiu <eshiu@google.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5566"
-version_tuple = (0, 0, 5566)
+version_str = "0.0.post5569"
+version_tuple = (0, 0, 5569)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5566")
+    pversion = V("0.0.post5569")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5471"
-data_version_tuple = (0, 0, 5471)
+data_version_str = "0.0.post5474"
+data_version_tuple = (0, 0, 5474)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5471")
+    pdata_version = V("0.0.post5474")
 except ImportError:
     pass
-data_git_hash = "7444b1113cd3177570dc4efccf70abc2bdc5c919"
-data_git_describe = "v0.0-5471-g7444b1113"
+data_git_hash = "5f1d304ad5c63a428219d1881479707727a559de"
+data_git_describe = "v0.0-5474-g5f1d304ad"
 data_git_msg = """\
-commit 7444b1113cd3177570dc4efccf70abc2bdc5c919
-Author: Philipp Wagner <phw@lowrisc.org>
-Date:   Wed Mar 24 18:26:35 2021 +0000
+commit 5f1d304ad5c63a428219d1881479707727a559de
+Author: Eric Shiu <eshiu@google.com>
+Date:   Wed Mar 17 17:24:11 2021 -0700
 
-    [ast] Remove whitespace in core file
+    [adc_ctrl] rename dcd to adc_ctrl
     
-    Signed-off-by: Philipp Wagner <phw@lowrisc.org>
+    Signed-off-by: Eric Shiu <eshiu@google.com>
 
 """
 
