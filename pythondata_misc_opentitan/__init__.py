8368635f6b14e88f86a7b1120b009b49a40e97a9
LiteX Robot <robot@timvideos.us>
2021-03-24 20:05:12 +0000

    Updating pythondata-misc-opentitan to 0.0.post5566
    Updated data to v0.0-5471-g7444b1113 based on 7444b1113cd3177570dc4efccf70abc2bdc5c919 from https://github.com/lowRISC/opentitan.
    > commit 7444b1113cd3177570dc4efccf70abc2bdc5c919
    > Author: Philipp Wagner <phw@lowrisc.org>
    > Date:   Wed Mar 24 18:26:35 2021 +0000
    >
    >     [ast] Remove whitespace in core file
    >
    >     Signed-off-by: Philipp Wagner <phw@lowrisc.org>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5565"
-version_tuple = (0, 0, 5565)
+version_str = "0.0.post5566"
+version_tuple = (0, 0, 5566)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5565")
+    pversion = V("0.0.post5566")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5470"
-data_version_tuple = (0, 0, 5470)
+data_version_str = "0.0.post5471"
+data_version_tuple = (0, 0, 5471)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5470")
+    pdata_version = V("0.0.post5471")
 except ImportError:
     pass
-data_git_hash = "4ca4a2f07bce8912d6adc10a56584319cea67491"
-data_git_describe = "v0.0-5470-g4ca4a2f07"
+data_git_hash = "7444b1113cd3177570dc4efccf70abc2bdc5c919"
+data_git_describe = "v0.0-5471-g7444b1113"
 data_git_msg = """\
-commit 4ca4a2f07bce8912d6adc10a56584319cea67491
-Author: Timothy Chen <timothytim@google.com>
-Date:   Fri Mar 19 13:51:39 2021 -0700
+commit 7444b1113cd3177570dc4efccf70abc2bdc5c919
+Author: Philipp Wagner <phw@lowrisc.org>
+Date:   Wed Mar 24 18:26:35 2021 +0000
 
-    [sw] Prototype hardware memory init
+    [ast] Remove whitespace in core file
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Philipp Wagner <phw@lowrisc.org>
 
 """
 
