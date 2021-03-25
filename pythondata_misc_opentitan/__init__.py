f3e471a0b5c3570c51ef63081d54658a5c671507
LiteX Robot <robot@timvideos.us>
2021-03-25 10:05:07 +0000

    Updating pythondata-misc-opentitan to 0.0.post5579
    Updated data to v0.0-5484-gda490e7b1 based on da490e7b187050ddad51bd093a3df9fc63abf8af from https://github.com/lowRISC/opentitan.
    > commit da490e7b187050ddad51bd093a3df9fc63abf8af
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Thu Mar 25 08:29:42 2021 +0000
    >
    >     [otbn] Document the idle_o signal
    >
    >     As Sri pointed out, I've just added some DV code that checks the
    >     idle_o signal does what I think it should do, but this isn't actually
    >     specced anywhere. Fix that!
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,49 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5577"
-version_tuple = (0, 0, 5577)
+version_str = "0.0.post5579"
+version_tuple = (0, 0, 5579)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5577")
+    pversion = V("0.0.post5579")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5482"
-data_version_tuple = (0, 0, 5482)
+data_version_str = "0.0.post5484"
+data_version_tuple = (0, 0, 5484)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5482")
+    pdata_version = V("0.0.post5484")
 except ImportError:
     pass
-data_git_hash = "f49bee189f77c731ad00b92627c1b70d905b2a90"
-data_git_describe = "v0.0-5482-gf49bee189"
+data_git_hash = "da490e7b187050ddad51bd093a3df9fc63abf8af"
+data_git_describe = "v0.0-5484-gda490e7b1"
 data_git_msg = """\
-commit f49bee189f77c731ad00b92627c1b70d905b2a90
-Author: Srikrishna Iyer <sriyer@google.com>
-Date:   Wed Mar 24 17:55:16 2021 -0700
+commit da490e7b187050ddad51bd093a3df9fc63abf8af
+Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
+Date:   Thu Mar 25 08:29:42 2021 +0000
 
-    [dvsim] Prevent command echo suppression
+    [otbn] Document the idle_o signal
     
-    At some point in the past, I had refactored the way SW images were
-    handled. I had added `.ONESHELL:` to the `sw_build` target, assuming
-    that the `.ONESHELL` would only affect that target (cause all recipes of
-    a target to execute in the same shell). Turns out, its more of a global
-    setting - it affects ALL recipes of all targets.
+    As Sri pointed out, I've just added some DV code that checks the
+    idle_o signal does what I think it should do, but this isn't actually
+    specced anywhere. Fix that!
     
-    This had an unintended consequence:
-    For all targets, the first recipe is an echo command printing the name
-    of that target, the echoing of which is suppressed with an `@`. Because the
-    rest of the recipes get invoked in the same shell, their echoing is also
-    suppressed. This makes issues hard to debug.
-    
-    In this change, the `.ONESHELL` is removed, and all recipes of
-    `sw_build` are invoked one the same line with a trailing `\`. The first
-    command is `set -e` which is the equivalent of all commands chained with
-    `&&` (which is already what we want).
-    
-    Signed-off-by: Srikrishna Iyer <sriyer@google.com>
+    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
 """
 
