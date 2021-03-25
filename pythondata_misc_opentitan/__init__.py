f6c9524158b3593471c85bc84a9c81e9b8b5c90f
LiteX Robot <robot@timvideos.us>
2021-03-25 23:06:42 +0000

    Updating pythondata-misc-opentitan to 0.0.post5597
    Updated data to v0.0-5502-g08adf5b3c based on 08adf5b3c38731c39a8bed1b40d7c67b8d8244e6 from https://github.com/lowRISC/opentitan.
    > commit 08adf5b3c38731c39a8bed1b40d7c67b8d8244e6
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Thu Mar 25 14:38:36 2021 -0700
    >
    >     [otbn] Wrap non-synthesizeable code with defines
    >
    >     Some dv code in otbn is included as part of the design core files
    >     to do side by side comparisons.  These files need to be wrapped with
    >     the SYNTHESIS flag to ensure they do not get parsed for synthesis.
    >
    >     Applying this fix to otbn_memutil_pkg.sv (the same fix is already applied
    >     to various other model files).
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,39 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5591"
-version_tuple = (0, 0, 5591)
+version_str = "0.0.post5597"
+version_tuple = (0, 0, 5597)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5591")
+    pversion = V("0.0.post5597")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5496"
-data_version_tuple = (0, 0, 5496)
+data_version_str = "0.0.post5502"
+data_version_tuple = (0, 0, 5502)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5496")
+    pdata_version = V("0.0.post5502")
 except ImportError:
     pass
-data_git_hash = "2a142ffbbe2f65367e03852f75bb18be4e79e75c"
-data_git_describe = "v0.0-5496-g2a142ffbb"
+data_git_hash = "08adf5b3c38731c39a8bed1b40d7c67b8d8244e6"
+data_git_describe = "v0.0-5502-g08adf5b3c"
 data_git_msg = """\
-commit 2a142ffbbe2f65367e03852f75bb18be4e79e75c
-Author: Michael Schaffner <msf@opentitan.org>
-Date:   Thu Mar 25 11:31:16 2021 -0700
+commit 08adf5b3c38731c39a8bed1b40d7c67b8d8244e6
+Author: Timothy Chen <timothytim@google.com>
+Date:   Thu Mar 25 14:38:36 2021 -0700
 
-    [prim_prince] Reverse the k0||k1 mapping to match with the paper
+    [otbn] Wrap non-synthesizeable code with defines
     
-    This aligns the logical mapping of the key into the k0/k1 halves.
-    The implementation and golden model mapped the LSBs of the key to k0,
-    and the MSBs to k1, whereas they are actually reversed in the paper.
+    Some dv code in otbn is included as part of the design core files
+    to do side by side comparisons.  These files need to be wrapped with
+    the SYNTHESIS flag to ensure they do not get parsed for synthesis.
     
-    Signed-off-by: Michael Schaffner <msf@opentitan.org>
+    Applying this fix to otbn_memutil_pkg.sv (the same fix is already applied
+    to various other model files).
+    
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
