3768a0bd4cbb0c77ca227b606a791c74aabbf76c
LiteX Robot <robot@timvideos.us>
2021-03-26 01:19:56 +0000

    Updating pythondata-misc-opentitan to 0.0.post5600
    Updated data to v0.0-5505-gb506aa33a based on b506aa33a6db353d73f2a98e24604c4ce6381558 from https://github.com/lowRISC/opentitan.
    > commit b506aa33a6db353d73f2a98e24604c4ce6381558
    > Author: Mark Branstad <mark.branstad@wdc.com>
    > Date:   Thu Mar 25 15:04:54 2021 -0700
    >
    >     [csnrg/rtl] remove incorrect TODO
    >
    >     The TODO says that the GEN command does not update working state, but it does.
    >
    >     Signed-off-by: Mark Branstad <mark.branstad@wdc.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,39 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5597"
-version_tuple = (0, 0, 5597)
+version_str = "0.0.post5600"
+version_tuple = (0, 0, 5600)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5597")
+    pversion = V("0.0.post5600")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5502"
-data_version_tuple = (0, 0, 5502)
+data_version_str = "0.0.post5505"
+data_version_tuple = (0, 0, 5505)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5502")
+    pdata_version = V("0.0.post5505")
 except ImportError:
     pass
-data_git_hash = "08adf5b3c38731c39a8bed1b40d7c67b8d8244e6"
-data_git_describe = "v0.0-5502-g08adf5b3c"
+data_git_hash = "b506aa33a6db353d73f2a98e24604c4ce6381558"
+data_git_describe = "v0.0-5505-gb506aa33a"
 data_git_msg = """\
-commit 08adf5b3c38731c39a8bed1b40d7c67b8d8244e6
-Author: Timothy Chen <timothytim@google.com>
-Date:   Thu Mar 25 14:38:36 2021 -0700
+commit b506aa33a6db353d73f2a98e24604c4ce6381558
+Author: Mark Branstad <mark.branstad@wdc.com>
+Date:   Thu Mar 25 15:04:54 2021 -0700
 
-    [otbn] Wrap non-synthesizeable code with defines
+    [csnrg/rtl] remove incorrect TODO
     
-    Some dv code in otbn is included as part of the design core files
-    to do side by side comparisons.  These files need to be wrapped with
-    the SYNTHESIS flag to ensure they do not get parsed for synthesis.
+    The TODO says that the GEN command does not update working state, but it does.
     
-    Applying this fix to otbn_memutil_pkg.sv (the same fix is already applied
-    to various other model files).
-    
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Mark Branstad <mark.branstad@wdc.com>
 
 """
 
