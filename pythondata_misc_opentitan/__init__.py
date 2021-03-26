c8df7aa67ca1b00e5ed7226994b5f1dd3086ac5d
LiteX Robot <robot@timvideos.us>
2021-03-26 13:11:03 +0000

    Updating pythondata-misc-opentitan to 0.0.post5603
    Updated data to v0.0-5508-g26215746e based on 26215746ec72774c540e4e8076f29e3d1a918eb1 from https://github.com/lowRISC/opentitan.
    > commit 26215746ec72774c540e4e8076f29e3d1a918eb1
    > Author: Mark Branstad <mark.branstad@wdc.com>
    > Date:   Wed Mar 17 16:47:09 2021 -0700
    >
    >     [entropy_src/rtl] sha3 block added as conditioner
    >
    >     Replacing the placeholder FIFO with the kmac sha3 module.
    >     Updated documentation for window going from 1024 to 2048 samples.
    >     Update the main state machine will new flow.
    >     Uniquified the local parameters related to the sha3 block.
    >
    >     Signed-off-by: Mark Branstad <mark.branstad@wdc.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,37 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5601"
-version_tuple = (0, 0, 5601)
+version_str = "0.0.post5603"
+version_tuple = (0, 0, 5603)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5601")
+    pversion = V("0.0.post5603")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5506"
-data_version_tuple = (0, 0, 5506)
+data_version_str = "0.0.post5508"
+data_version_tuple = (0, 0, 5508)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5506")
+    pdata_version = V("0.0.post5508")
 except ImportError:
     pass
-data_git_hash = "fa60a60581561cfd5f616f95ee8af3ec84ce30d2"
-data_git_describe = "v0.0-5506-gfa60a6058"
+data_git_hash = "26215746ec72774c540e4e8076f29e3d1a918eb1"
+data_git_describe = "v0.0-5508-g26215746e"
 data_git_msg = """\
-commit fa60a60581561cfd5f616f95ee8af3ec84ce30d2
-Author: Timothy Chen <timothytim@google.com>
-Date:   Tue Mar 23 14:29:40 2021 -0700
+commit 26215746ec72774c540e4e8076f29e3d1a918eb1
+Author: Mark Branstad <mark.branstad@wdc.com>
+Date:   Wed Mar 17 16:47:09 2021 -0700
 
-    [top, clkmgr] Add external clock switch support
+    [entropy_src/rtl] sha3 block added as conditioner
     
-    Address #5566
+    Replacing the placeholder FIFO with the kmac sha3 module.
+    Updated documentation for window going from 1024 to 2048 samples.
+    Update the main state machine will new flow.
+    Uniquified the local parameters related to the sha3 block.
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Mark Branstad <mark.branstad@wdc.com>
 
 """
 
