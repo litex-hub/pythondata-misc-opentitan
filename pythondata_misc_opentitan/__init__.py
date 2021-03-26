86845afb4357feaa3ccb08e3a95bf776ae2c2746
LiteX Robot <robot@timvideos.us>
2021-03-26 14:04:58 +0000

    Updating pythondata-misc-opentitan to 0.0.post5604
    Updated data to v0.0-5509-g44fe217af based on 44fe217af660b1abbe22fa00e70583482f096871 from https://github.com/lowRISC/opentitan.
    > commit 44fe217af660b1abbe22fa00e70583482f096871
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Fri Mar 26 11:22:05 2021 +0000
    >
    >     [csrng] Fix some width mismatches
    >
    >     These cause lint errors from Verilator. Most of the changes are
    >     mechanical, but I've changed the left shift that computes
    >     concat_outblk_shifted_value to add the BlkLen zeros at the bottom
    >     explicitly (I think we might have been silently dropping the top block
    >     before).
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,37 +4,38 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5603"
-version_tuple = (0, 0, 5603)
+version_str = "0.0.post5604"
+version_tuple = (0, 0, 5604)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5603")
+    pversion = V("0.0.post5604")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5508"
-data_version_tuple = (0, 0, 5508)
+data_version_str = "0.0.post5509"
+data_version_tuple = (0, 0, 5509)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5508")
+    pdata_version = V("0.0.post5509")
 except ImportError:
     pass
-data_git_hash = "26215746ec72774c540e4e8076f29e3d1a918eb1"
-data_git_describe = "v0.0-5508-g26215746e"
+data_git_hash = "44fe217af660b1abbe22fa00e70583482f096871"
+data_git_describe = "v0.0-5509-g44fe217af"
 data_git_msg = """\
-commit 26215746ec72774c540e4e8076f29e3d1a918eb1
-Author: Mark Branstad <mark.branstad@wdc.com>
-Date:   Wed Mar 17 16:47:09 2021 -0700
+commit 44fe217af660b1abbe22fa00e70583482f096871
+Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
+Date:   Fri Mar 26 11:22:05 2021 +0000
 
-    [entropy_src/rtl] sha3 block added as conditioner
+    [csrng] Fix some width mismatches
     
-    Replacing the placeholder FIFO with the kmac sha3 module.
-    Updated documentation for window going from 1024 to 2048 samples.
-    Update the main state machine will new flow.
-    Uniquified the local parameters related to the sha3 block.
+    These cause lint errors from Verilator. Most of the changes are
+    mechanical, but I've changed the left shift that computes
+    concat_outblk_shifted_value to add the BlkLen zeros at the bottom
+    explicitly (I think we might have been silently dropping the top block
+    before).
     
-    Signed-off-by: Mark Branstad <mark.branstad@wdc.com>
+    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
 """
 
