3f95ca807bc08d01fcda437dce13633368941742
LiteX Robot <robot@timvideos.us>
2021-03-28 15:04:43 +0000

    Updating pythondata-misc-opentitan to 0.0.post5609
    Updated data to v0.0-5514-g0c49992bc based on 0c49992bc0b713f91bb7ba01cd78eaf89f49d04a from https://github.com/lowRISC/opentitan.
    > commit 0c49992bc0b713f91bb7ba01cd78eaf89f49d04a
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Fri Mar 26 11:05:20 2021 +0000
    >
    >     [kmac] Fix app_config_t to work with Verilator
    >
    >     Verilator doesn't have proper support for unpacked structs. Since we
    >     don't need one here, we can just declare app_config_t to be packed.
    >
    >     Also, it seems to have rather strange behaviour with int enums when
    >     used as fields in a struct. I've reported this on the Verilator bug
    >     tracker as issue #2855 but there's an easy workaround here, which is
    >     probably what we'd want for synthesizable code anyway.
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,59 +4,40 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5608"
-version_tuple = (0, 0, 5608)
+version_str = "0.0.post5609"
+version_tuple = (0, 0, 5609)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5608")
+    pversion = V("0.0.post5609")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5513"
-data_version_tuple = (0, 0, 5513)
+data_version_str = "0.0.post5514"
+data_version_tuple = (0, 0, 5514)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5513")
+    pdata_version = V("0.0.post5514")
 except ImportError:
     pass
-data_git_hash = "c7a5044e87da31cf4b4d8bb62a9c0f7ac639bdab"
-data_git_describe = "v0.0-5513-gc7a5044e8"
+data_git_hash = "0c49992bc0b713f91bb7ba01cd78eaf89f49d04a"
+data_git_describe = "v0.0-5514-g0c49992bc"
 data_git_msg = """\
-commit c7a5044e87da31cf4b4d8bb62a9c0f7ac639bdab
-Author: Srikrishna Iyer <sriyer@google.com>
-Date:   Thu Mar 25 21:47:56 2021 -0700
+commit 0c49992bc0b713f91bb7ba01cd78eaf89f49d04a
+Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
+Date:   Fri Mar 26 11:05:20 2021 +0000
 
-    [dvsim] Keep dependencies list
+    [kmac] Fix app_config_t to work with Verilator
     
-    This set of changes is aimed at retaining the dependency order across
-    targets, even if the dependency is not scheduled to be run. The
-    Deploy::dependencies list once constructed, remains untouched. We
-    instead change the way the FlowCfg::deploy list is created - if targets
-    are not required to run (for example, when --build-only or --run-only
-    switch is passed), then they are not added to the deploy list. This
-    means that the deploy list needs to be constructed correctly - the
-    scheduler relies on it to know what to run.
+    Verilator doesn't have proper support for unpacked structs. Since we
+    don't need one here, we can just declare app_config_t to be packed.
     
-    The scheduler previously recursively went through the dependencies to
-    determine what all needed to be run - this required the FlowCfg to
-    delete dependencies after the fact, if flow modifier switches were
-    passed. This is no longer needed because we now explicitly provide it
-    the list of things to run instead. This also means when checking an
-    item's eligibility to be enqueued based on its dependencies' statuses,
-    it needs to ignore the deps that were not a part of the original deploy
-    list.
+    Also, it seems to have rather strange behaviour with int enums when
+    used as fields in a struct. I've reported this on the Verilator bug
+    tracker as issue #2855 but there's an easy workaround here, which is
+    probably what we'd want for synthesizable code anyway.
     
-    The reason for this change is our internal Google Cloud based launching
-    system, which runs each job (input -> process -> output) in an isolated
-    VM instance. The job's input and output are tarballs that flit between
-    the user's workstation, Google Cloud storage, and the VM instance. To
-    support --run-only for example, in our environment, the run deploy
-    object needs to be able to provide a pointer to its build dependency
-    (which would have run in the past) so that the built simulation
-    executable can be tarballed and uploaded as the run-job's input.
-    
-    Signed-off-by: Srikrishna Iyer <sriyer@google.com>
+    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
 """
 
