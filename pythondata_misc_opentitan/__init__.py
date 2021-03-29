a2e59bb870f6bce08e492c06cf2776235aa132b0
LiteX Robot <robot@timvideos.us>
2021-03-29 23:04:51 +0000

    Updating pythondata-misc-opentitan to 0.0.post5619
    Updated data to v0.0-5524-g3800c6dc1 based on 3800c6dc1f36b4134bc2b3a9eb0a90889e980f9c from https://github.com/lowRISC/opentitan.
    > commit 3800c6dc1f36b4134bc2b3a9eb0a90889e980f9c
    > Author: Cindy Chen <chencindy@google.com>
    > Date:   Mon Mar 29 15:00:04 2021 -0700
    >
    >     [dv/otp] fix number of edn required for sram
    >
    >     The num of EDN required for SRAM should be 12 instead of 10.
    >
    >     Signed-off-by: Cindy Chen <chencindy@google.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,37 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5618"
-version_tuple = (0, 0, 5618)
+version_str = "0.0.post5619"
+version_tuple = (0, 0, 5619)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5618")
+    pversion = V("0.0.post5619")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5523"
-data_version_tuple = (0, 0, 5523)
+data_version_str = "0.0.post5524"
+data_version_tuple = (0, 0, 5524)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5523")
+    pdata_version = V("0.0.post5524")
 except ImportError:
     pass
-data_git_hash = "e051f4c7c73ee5de7e3f8cd0ddb40a6368b2cb7c"
-data_git_describe = "v0.0-5523-ge051f4c7c"
+data_git_hash = "3800c6dc1f36b4134bc2b3a9eb0a90889e980f9c"
+data_git_describe = "v0.0-5524-g3800c6dc1"
 data_git_msg = """\
-commit e051f4c7c73ee5de7e3f8cd0ddb40a6368b2cb7c
+commit 3800c6dc1f36b4134bc2b3a9eb0a90889e980f9c
 Author: Cindy Chen <chencindy@google.com>
-Date:   Tue Mar 23 17:08:15 2021 -0700
+Date:   Mon Mar 29 15:00:04 2021 -0700
 
-    [formal/conn] Support connectivity test locally
+    [dv/otp] fix number of edn required for sram
     
-    This PR adds basic configs to run connecivity test locally.
-    This is the first step to support running connecivity test using dvsim.
-    The reason we are adding this local option is because we can temporarily
-    using this script to run with GUI.
-    
-    Next PR will support running the connectivity test on dvsim.
+    The num of EDN required for SRAM should be 12 instead of 10.
     
     Signed-off-by: Cindy Chen <chencindy@google.com>
 
