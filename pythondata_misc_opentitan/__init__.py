e78798ea5a3b5cc659d130362ab573a7b7269128
LiteX Robot <robot@timvideos.us>
2021-04-01 18:15:31 +0000

    Updating pythondata-misc-opentitan to 0.0.post5670
    Updated data to v0.0-5575-gb398b1181 based on b398b1181febe5acbdfd084dbe31818f520ecabd from https://github.com/lowRISC/opentitan.
    > commit b398b1181febe5acbdfd084dbe31818f520ecabd
    > Author: Eitan Shapira <eitan.shapira@nuvoton.com>
    > Date:   Wed Mar 31 18:42:57 2021 +0300
    >
    >     [dv/flash] Small changes to improve integration with partner env
    >
    >     Signed-off-by: Eitan Shapira <eitan.shapira@nuvoton.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5666"
-version_tuple = (0, 0, 5666)
+version_str = "0.0.post5670"
+version_tuple = (0, 0, 5670)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5666")
+    pversion = V("0.0.post5670")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5571"
-data_version_tuple = (0, 0, 5571)
+data_version_str = "0.0.post5575"
+data_version_tuple = (0, 0, 5575)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5571")
+    pdata_version = V("0.0.post5575")
 except ImportError:
     pass
-data_git_hash = "2799bf0de69b3d7878375a51901fba96aeadac45"
-data_git_describe = "v0.0-5571-g2799bf0de"
+data_git_hash = "b398b1181febe5acbdfd084dbe31818f520ecabd"
+data_git_describe = "v0.0-5575-gb398b1181"
 data_git_msg = """\
-commit 2799bf0de69b3d7878375a51901fba96aeadac45
-Author: Timothy Chen <timothytim@google.com>
-Date:   Thu Mar 18 14:48:47 2021 -0700
+commit b398b1181febe5acbdfd084dbe31818f520ecabd
+Author: Eitan Shapira <eitan.shapira@nuvoton.com>
+Date:   Wed Mar 31 18:42:57 2021 +0300
 
-    [tlul] Add support for data integrity passthrough
+    [dv/flash] Small changes to improve integration with partner env
     
-    - Top level/otbn rams are now fully connected without dropping or pading bits
-    - The integrity is fully passed through, however, integrity recalculation on byte writes for top level memories will be done in a separate PR.
-    - ROM handling will also be separately done as it needs to be padded out to byte alignment
-    
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Eitan Shapira <eitan.shapira@nuvoton.com>
 
 """
 
