0eec264870a107df81064c6cc21c228b56d83b30
LiteX Robot <robot@timvideos.us>
2021-04-02 00:20:21 +0000

    Updating pythondata-misc-opentitan to 0.0.post5674
    Updated data to v0.0-5579-g62dabf7c5 based on 62dabf7c512b09d6e185df52752d82393338c0b0 from https://github.com/lowRISC/opentitan.
    > commit 62dabf7c512b09d6e185df52752d82393338c0b0
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Wed Mar 24 12:09:27 2021 -0700
    >
    >     [reggen, util] Add support for data integrity passthrough
    >
    >     - When a module has no window or has windows with no integrity passthrough,
    >       data integrity can be generated at a common location.
    >
    >     - When a module window has data integrity passthrough, the regfile
    >       registers must separately generate data integrity so as to not
    >       duplicate.
    >
    >     - When a module has a mix of windows with and without integrity, common
    >       generation is not possible, and data integrity is selectively generated
    >       for those windows that do not directly pass them in.
    >
    >     - The only module that is currently impacted is otbn (and soon rom_ctrl)
    >       Otbn dmem / imem each contain their own integrity, and thus can directly
    >       pass them through.  The regfile for otbn thus no longer generates data
    >       integrity at a common location, and instead generates it only for data
    >       passed through tlul_adapter_reg.  The data integrity for the windows are
    >       fed through directly.
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    >     [top] Auto generate files
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,54 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5673"
-version_tuple = (0, 0, 5673)
+version_str = "0.0.post5674"
+version_tuple = (0, 0, 5674)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5673")
+    pversion = V("0.0.post5674")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5578"
-data_version_tuple = (0, 0, 5578)
+data_version_str = "0.0.post5579"
+data_version_tuple = (0, 0, 5579)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5578")
+    pdata_version = V("0.0.post5579")
 except ImportError:
     pass
-data_git_hash = "4653df9a423b5c13ea33da5bd0d50a756aee9667"
-data_git_describe = "v0.0-5578-g4653df9a4"
+data_git_hash = "62dabf7c512b09d6e185df52752d82393338c0b0"
+data_git_describe = "v0.0-5579-g62dabf7c5"
 data_git_msg = """\
-commit 4653df9a423b5c13ea33da5bd0d50a756aee9667
-Author: Cindy Chen <chencindy@google.com>
-Date:   Thu Apr 1 11:21:27 2021 -0700
+commit 62dabf7c512b09d6e185df52752d82393338c0b0
+Author: Timothy Chen <timothytim@google.com>
+Date:   Wed Mar 24 12:09:27 2021 -0700
 
-    [fpv] Fix connectivity script folder name
+    [reggen, util] Add support for data integrity passthrough
     
-    This PR fixes connectivity script folder name.
-    Also small fix on auto-exit in gui mode.
+    - When a module has no window or has windows with no integrity passthrough,
+      data integrity can be generated at a common location.
     
-    Signed-off-by: Cindy Chen <chencindy@google.com>
+    - When a module window has data integrity passthrough, the regfile
+      registers must separately generate data integrity so as to not
+      duplicate.
+    
+    - When a module has a mix of windows with and without integrity, common
+      generation is not possible, and data integrity is selectively generated
+      for those windows that do not directly pass them in.
+    
+    - The only module that is currently impacted is otbn (and soon rom_ctrl)
+      Otbn dmem / imem each contain their own integrity, and thus can directly
+      pass them through.  The regfile for otbn thus no longer generates data
+      integrity at a common location, and instead generates it only for data
+      passed through tlul_adapter_reg.  The data integrity for the windows are
+      fed through directly.
+    
+    Signed-off-by: Timothy Chen <timothytim@google.com>
+    
+    [top] Auto generate files
+    
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
