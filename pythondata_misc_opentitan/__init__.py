9c1b6626620c432bdeadfa72e740115bcb29aff6
LiteX Robot <robot@timvideos.us>
2021-04-02 02:28:02 +0000

    Updating pythondata-misc-opentitan to 0.0.post5683
    Updated data to v0.0-5588-g76eb883eb based on 76eb883ebbb734764bf81f8531d02f1aa9f207f0 from https://github.com/lowRISC/opentitan.
    > commit 76eb883ebbb734764bf81f8531d02f1aa9f207f0
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Thu Mar 25 16:49:58 2021 -0700
    >
    >     [top] Auto generate files
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5681"
-version_tuple = (0, 0, 5681)
+version_str = "0.0.post5683"
+version_tuple = (0, 0, 5683)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5681")
+    pversion = V("0.0.post5683")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5586"
-data_version_tuple = (0, 0, 5586)
+data_version_str = "0.0.post5588"
+data_version_tuple = (0, 0, 5588)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5586")
+    pdata_version = V("0.0.post5588")
 except ImportError:
     pass
-data_git_hash = "10c6e5bfd6bbd2255a2f043ef34106b529eed481"
-data_git_describe = "v0.0-5586-g10c6e5bfd"
+data_git_hash = "76eb883ebbb734764bf81f8531d02f1aa9f207f0"
+data_git_describe = "v0.0-5588-g76eb883eb"
 data_git_msg = """\
-commit 10c6e5bfd6bbd2255a2f043ef34106b529eed481
-Author: Cindy Chen <chencindy@google.com>
-Date:   Thu Apr 1 11:18:54 2021 -0700
+commit 76eb883ebbb734764bf81f8531d02f1aa9f207f0
+Author: Timothy Chen <timothytim@google.com>
+Date:   Thu Mar 25 16:49:58 2021 -0700
 
-    [dv/otp_ctrl] Support ecc correctable error in otp check
+    [top] Auto generate files
     
-    This PR supports ECC correctable error in otp_checks.
-    This error not trigger any alert, but just trigger an interrupt and
-    update error code accordingly.
-    
-    Signed-off-by: Cindy Chen <chencindy@google.com>
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
