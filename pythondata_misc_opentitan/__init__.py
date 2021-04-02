f265696f80fa266ec8556d8891a4916935824afe
LiteX Robot <robot@timvideos.us>
2021-04-02 18:14:57 +0000

    Updating pythondata-misc-opentitan to 0.0.post5697
    Updated data to v0.0-5602-ge96574134 based on e96574134f987a91f05cd2c6e48782796f4101fd from https://github.com/lowRISC/opentitan.
    > commit e96574134f987a91f05cd2c6e48782796f4101fd
    > Author: Michael Schaffner <msf@opentitan.org>
    > Date:   Thu Apr 1 23:14:46 2021 -0700
    >
    >     [top/arty] Remove artys7 target
    >
    >     This deprecates and removes the artys7 target since it
    >     has become out of sync and it will not be maintained anymore
    >     going forward.
    >
    >     Signed-off-by: Michael Schaffner <msf@opentitan.org>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,41 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5686"
-version_tuple = (0, 0, 5686)
+version_str = "0.0.post5697"
+version_tuple = (0, 0, 5697)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5686")
+    pversion = V("0.0.post5697")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5591"
-data_version_tuple = (0, 0, 5591)
+data_version_str = "0.0.post5602"
+data_version_tuple = (0, 0, 5602)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5591")
+    pdata_version = V("0.0.post5602")
 except ImportError:
     pass
-data_git_hash = "5bf75e9351e9eef509a967f80eb3e37015cc9ca0"
-data_git_describe = "v0.0-5591-g5bf75e935"
+data_git_hash = "e96574134f987a91f05cd2c6e48782796f4101fd"
+data_git_describe = "v0.0-5602-ge96574134"
 data_git_msg = """\
-commit 5bf75e9351e9eef509a967f80eb3e37015cc9ca0
-Author: Miguel Osorio <miguelosorio@google.com>
-Date:   Wed Mar 31 11:33:08 2021 -0700
+commit e96574134f987a91f05cd2c6e48782796f4101fd
+Author: Michael Schaffner <msf@opentitan.org>
+Date:   Thu Apr 1 23:14:46 2021 -0700
 
-    [sw/dif] CSRNG Initial Implementation
+    [top/arty] Remove artys7 target
     
-    This commits adds the following dif_csrng functions:
+    This deprecates and removes the artys7 target since it
+    has become out of sync and it will not be maintained anymore
+    going forward.
     
-    *   `dif_csrng_init()`
-    *   `dif_csrng_configure()`
-    *   `dif_csrng_get_cmd_interface()`
-    *   `dif_csrng_get_output_status()`
-    
-    Added unittest and smoketest boilerplate.
-    
-    Signed-off-by: Miguel Osorio <miguelosorio@google.com>
+    Signed-off-by: Michael Schaffner <msf@opentitan.org>
 
 """
 
