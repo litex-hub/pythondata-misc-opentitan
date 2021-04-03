fdb2548571637c835899ad5dd0a8dd8f90adc131
LiteX Robot <robot@timvideos.us>
2021-04-03 18:14:06 +0000

    Updating pythondata-misc-opentitan to 0.0.post5709
    Updated data to v0.0-5614-g414f7a74d based on 414f7a74d775b88c842e508c4e036ea539cd3355 from https://github.com/lowRISC/opentitan.
    > commit 414f7a74d775b88c842e508c4e036ea539cd3355
    > Author: Cindy Chen <chencindy@google.com>
    > Date:   Fri Apr 2 13:58:57 2021 -0700
    >
    >     [dv/otp_ctrl] otp init failure test
    >
    >     This PR adds a otp_init failure test. This test will cause otp init
    >     failure by creating init check errors. Scb is disabled in this test.
    >
    >     Signed-off-by: Cindy Chen <chencindy@google.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5707"
-version_tuple = (0, 0, 5707)
+version_str = "0.0.post5709"
+version_tuple = (0, 0, 5709)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5707")
+    pversion = V("0.0.post5709")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5612"
-data_version_tuple = (0, 0, 5612)
+data_version_str = "0.0.post5614"
+data_version_tuple = (0, 0, 5614)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5612")
+    pdata_version = V("0.0.post5614")
 except ImportError:
     pass
-data_git_hash = "4c519c5563c3da902c7170b46b119a054bc0ac80"
-data_git_describe = "v0.0-5612-g4c519c556"
+data_git_hash = "414f7a74d775b88c842e508c4e036ea539cd3355"
+data_git_describe = "v0.0-5614-g414f7a74d"
 data_git_msg = """\
-commit 4c519c5563c3da902c7170b46b119a054bc0ac80
-Author: Miguel Osorio <miguelosorio@google.com>
-Date:   Fri Apr 2 09:39:12 2021 -0700
+commit 414f7a74d775b88c842e508c4e036ea539cd3355
+Author: Cindy Chen <chencindy@google.com>
+Date:   Fri Apr 2 13:58:57 2021 -0700
 
-    [sw/dif] Fix dif_hmac.h clang-format
+    [dv/otp_ctrl] otp init failure test
     
-    Also small compile fixes to dif_hmac_smoketest.c
+    This PR adds a otp_init failure test. This test will cause otp init
+    failure by creating init check errors. Scb is disabled in this test.
     
-    Signed-off-by: Miguel Osorio <miguelosorio@google.com>
+    Signed-off-by: Cindy Chen <chencindy@google.com>
 
 """
 
