f00af54f3d8bce60ce5d583ea2b7e8a1047b3328
LiteX Robot <robot@timvideos.us>
2021-04-05 20:04:58 +0000

    Updating pythondata-misc-opentitan to 0.0.post5714
    Updated data to v0.0-5619-g551695c50 based on 551695c501766bcc10a64fff1ba714720e62e349 from https://github.com/lowRISC/opentitan.
    > commit 551695c501766bcc10a64fff1ba714720e62e349
    > Author: Jake Ke <jackieke724@hotmail.com>
    > Date:   Fri Jan 22 14:22:38 2021 +0800
    >
    >     [util/simplespi] PyFtdi updated, remove '.tobytes()' for spi read
    >
    >     Signed-off-by: Jake Ke <jackieke724@hotmail.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5713"
-version_tuple = (0, 0, 5713)
+version_str = "0.0.post5714"
+version_tuple = (0, 0, 5714)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5713")
+    pversion = V("0.0.post5714")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5618"
-data_version_tuple = (0, 0, 5618)
+data_version_str = "0.0.post5619"
+data_version_tuple = (0, 0, 5619)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5618")
+    pdata_version = V("0.0.post5619")
 except ImportError:
     pass
-data_git_hash = "b02ea11fd3a8845b9ff5ab2d892ae85b8eba7774"
-data_git_describe = "v0.0-5618-gb02ea11fd"
+data_git_hash = "551695c501766bcc10a64fff1ba714720e62e349"
+data_git_describe = "v0.0-5619-g551695c50"
 data_git_msg = """\
-commit b02ea11fd3a8845b9ff5ab2d892ae85b8eba7774
-Author: Weicai Yang <weicai@google.com>
-Date:   Mon Mar 29 16:40:06 2021 -0700
+commit 551695c501766bcc10a64fff1ba714720e62e349
+Author: Jake Ke <jackieke724@hotmail.com>
+Date:   Fri Jan 22 14:22:38 2021 +0800
 
-    [dv] Document how to reproduce CI (Private) failure
+    [util/simplespi] PyFtdi updated, remove '.tobytes()' for spi read
     
-    related to #5786
-    
-    Signed-off-by: Weicai Yang <weicai@google.com>
+    Signed-off-by: Jake Ke <jackieke724@hotmail.com>
 
 """
 
