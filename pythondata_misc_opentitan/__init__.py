9d93e5fe737a5decd072515f4b763ed8d5dd596e
LiteX Robot <robot@timvideos.us>
2021-04-06 08:04:50 +0000

    Updating pythondata-misc-opentitan to 0.0.post5720
    Updated data to v0.0-5625-g8235b73df based on 8235b73df356a92b7fb99e29e864c7086b6860b1 from https://github.com/lowRISC/opentitan.
    > commit 8235b73df356a92b7fb99e29e864c7086b6860b1
    > Author: Silvestrs Timofejevs <silvestrst@lowrisc.org>
    > Date:   Wed Mar 31 15:37:52 2021 +0100
    >
    >     [sw, dif_aon_timer] Minor naming and documentation fixups
    >
    >     Signed-off-by: Silvestrs Timofejevs <silvestrst@lowrisc.org>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5719"
-version_tuple = (0, 0, 5719)
+version_str = "0.0.post5720"
+version_tuple = (0, 0, 5720)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5719")
+    pversion = V("0.0.post5720")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5624"
-data_version_tuple = (0, 0, 5624)
+data_version_str = "0.0.post5625"
+data_version_tuple = (0, 0, 5625)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5624")
+    pdata_version = V("0.0.post5625")
 except ImportError:
     pass
-data_git_hash = "070d956d1d5191074df7e8815f300e9ff0267adc"
-data_git_describe = "v0.0-5624-g070d956d1"
+data_git_hash = "8235b73df356a92b7fb99e29e864c7086b6860b1"
+data_git_describe = "v0.0-5625-g8235b73df"
 data_git_msg = """\
-commit 070d956d1d5191074df7e8815f300e9ff0267adc
-Author: Cindy Chen <chencindy@google.com>
-Date:   Mon Apr 5 11:50:29 2021 -0700
+commit 8235b73df356a92b7fb99e29e864c7086b6860b1
+Author: Silvestrs Timofejevs <silvestrst@lowrisc.org>
+Date:   Wed Mar 31 15:37:52 2021 +0100
 
-    [dv/otp_ctrl] Add macro failure to otp init test
+    [sw, dif_aon_timer] Minor naming and documentation fixups
     
-    The otp_init_fail can also be triggered by ECC failure.
-    ECC correctable failure will pass OTP init with an error.
-    ECC uncorrectable failure will fail OTP init with an alert.
-    
-    Signed-off-by: Cindy Chen <chencindy@google.com>
+    Signed-off-by: Silvestrs Timofejevs <silvestrst@lowrisc.org>
 
 """
 
