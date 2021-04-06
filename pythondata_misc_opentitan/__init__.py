fc73a2e7e5a811b75ac3cb2a4a11aeedd3b41ca8
LiteX Robot <robot@timvideos.us>
2021-04-06 15:04:41 +0000

    Updating pythondata-misc-opentitan to 0.0.post5727
    Updated data to v0.0-5632-g77106b793 based on 77106b793f5b60d6323d53c885f1d47d1efff76d from https://github.com/lowRISC/opentitan.
    > commit 77106b793f5b60d6323d53c885f1d47d1efff76d
    > Author: Colin O'Flynn <coflynn@newae.com>
    > Date:   Tue Apr 6 10:14:22 2021 -0300
    >
    >     [doc] Remove separate doc build package list
    >
    >     Signed-off-by: Colin O'Flynn <coflynn@newae.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5725"
-version_tuple = (0, 0, 5725)
+version_str = "0.0.post5727"
+version_tuple = (0, 0, 5727)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5725")
+    pversion = V("0.0.post5727")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5630"
-data_version_tuple = (0, 0, 5630)
+data_version_str = "0.0.post5632"
+data_version_tuple = (0, 0, 5632)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5630")
+    pdata_version = V("0.0.post5632")
 except ImportError:
     pass
-data_git_hash = "ffb1921fcb519cb7a72700c38deb7b4622b22a07"
-data_git_describe = "v0.0-5630-gffb1921fc"
+data_git_hash = "77106b793f5b60d6323d53c885f1d47d1efff76d"
+data_git_describe = "v0.0-5632-g77106b793"
 data_git_msg = """\
-commit ffb1921fcb519cb7a72700c38deb7b4622b22a07
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Tue Apr 6 08:53:48 2021 +0100
+commit 77106b793f5b60d6323d53c885f1d47d1efff76d
+Author: Colin O'Flynn <coflynn@newae.com>
+Date:   Tue Apr 6 10:14:22 2021 -0300
 
-    [otbn] Remove unused bits waiver
+    [doc] Remove separate doc build package list
     
-    Commit 2799bf0 wired them up to the bus, so the waiver is no longer
-    needed.
-    
-    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    Signed-off-by: Colin O'Flynn <coflynn@newae.com>
 
 """
 
