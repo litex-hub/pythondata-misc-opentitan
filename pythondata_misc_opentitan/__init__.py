282c94b050942517cf2a2e1c777184b18e17cb68
LiteX Robot <robot@timvideos.us>
2021-04-08 02:26:52 +0000

    Updating pythondata-misc-opentitan to 0.0.post5767
    Updated data to v0.0-5672-g8961a8026 based on 8961a8026331fcf39899d73e10a9e5a3c69193b2 from https://github.com/lowRISC/opentitan.
    > commit 8961a8026331fcf39899d73e10a9e5a3c69193b2
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Wed Mar 31 19:56:10 2021 -0700
    >
    >     [sw] Minor software updates for dif_entropy_src and test
    >
    >     - This change should now ensure the new seed is used and
    >       the entropy reads should be different between reads.
    >     - Lastly, a few auxillary functions are added to poll for specific
    >       status.  These likely should be replaced long term by something
    >       more robust
    >     - unittests are also udpated for the slightly tweaked sequence
    >     - At the moment, since we disable / re-enable entropy_src, the
    >       timing differences between platforms (dv vs verilator) causes
    >       the test to spit out different results.  For the time being,
    >       favor verilator until #5941 is in.
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,42 +4,43 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5765"
-version_tuple = (0, 0, 5765)
+version_str = "0.0.post5767"
+version_tuple = (0, 0, 5767)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5765")
+    pversion = V("0.0.post5767")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5670"
-data_version_tuple = (0, 0, 5670)
+data_version_str = "0.0.post5672"
+data_version_tuple = (0, 0, 5672)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5670")
+    pdata_version = V("0.0.post5672")
 except ImportError:
     pass
-data_git_hash = "74c4ff235ba08caef2e970f98e0efd1318daea4a"
-data_git_describe = "v0.0-5670-g74c4ff235"
+data_git_hash = "8961a8026331fcf39899d73e10a9e5a3c69193b2"
+data_git_describe = "v0.0-5672-g8961a8026"
 data_git_msg = """\
-commit 74c4ff235ba08caef2e970f98e0efd1318daea4a
-Author: Michael Schaffner <msf@google.com>
-Date:   Tue Mar 30 15:43:46 2021 -0700
+commit 8961a8026331fcf39899d73e10a9e5a3c69193b2
+Author: Timothy Chen <timothytim@google.com>
+Date:   Wed Mar 31 19:56:10 2021 -0700
 
-    [topgen] Rework pinmux datastructure and templatize tops
+    [sw] Minor software updates for dif_entropy_src and test
     
-    This is a complete overhaul of the pinmux / padctrl configuration
-    datastructure in the top Hjson.
+    - This change should now ensure the new seed is used and
+      the entropy reads should be different between reads.
+    - Lastly, a few auxillary functions are added to poll for specific
+      status.  These likely should be replaced long term by something
+      more robust
+    - unittests are also udpated for the slightly tweaked sequence
+    - At the moment, since we disable / re-enable entropy_src, the
+      timing differences between platforms (dv vs verilator) causes
+      the test to spit out different results.  For the time being,
+      favor verilator until #5941 is in.
     
-    The new datastructures are used to provide more control over the
-    DIO/MIO configuration, pinout and target-specific differences
-    (ASIC vs FPGA vs Verilator).
-    
-    The chip-level files are now generated from one common template,
-    and all regular DIO/MIO connections are made automatically.
-    
-    Signed-off-by: Michael Schaffner <msf@google.com>
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
