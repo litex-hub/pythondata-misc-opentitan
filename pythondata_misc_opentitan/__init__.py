943352512e67d7843c73c54cf2f369cbfb7e3d66
LiteX Robot <robot@timvideos.us>
2021-04-09 15:04:54 +0000

    Updating pythondata-misc-opentitan to 0.0.post5804
    Updated data to v0.0-5709-g6abe0ff01 based on 6abe0ff0129c6e24ae170e2162869c9bd0c64902 from https://github.com/lowRISC/opentitan.
    > commit 6abe0ff0129c6e24ae170e2162869c9bd0c64902
    > Author: Pirmin Vogel <vogelpi@lowrisc.org>
    > Date:   Tue Apr 6 20:32:10 2021 +0200
    >
    >     [kmac, sha3] Add separate Verilator lint waiver files, fix some warnings
    >
    >     Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5800"
-version_tuple = (0, 0, 5800)
+version_str = "0.0.post5804"
+version_tuple = (0, 0, 5804)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5800")
+    pversion = V("0.0.post5804")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5705"
-data_version_tuple = (0, 0, 5705)
+data_version_str = "0.0.post5709"
+data_version_tuple = (0, 0, 5709)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5705")
+    pdata_version = V("0.0.post5709")
 except ImportError:
     pass
-data_git_hash = "e1bc36198309dcbe9a993d7cc4067a1011deb455"
-data_git_describe = "v0.0-5705-ge1bc36198"
+data_git_hash = "6abe0ff0129c6e24ae170e2162869c9bd0c64902"
+data_git_describe = "v0.0-5709-g6abe0ff01"
 data_git_msg = """\
-commit e1bc36198309dcbe9a993d7cc4067a1011deb455
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Thu Apr 8 13:00:14 2021 +0100
+commit 6abe0ff0129c6e24ae170e2162869c9bd0c64902
+Author: Pirmin Vogel <vogelpi@lowrisc.org>
+Date:   Tue Apr 6 20:32:10 2021 +0200
 
-    [edn,lint] Fix width mismatch in the definition of cmd_sent
+    [kmac, sha3] Add separate Verilator lint waiver files, fix some warnings
     
-    cmd_fifo_cnt_q is actually 4 bits wide in the current design. The
-    mismatch between that and 13'h01 causes Verilator width warnings.
-    
-    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
 
 """
 
