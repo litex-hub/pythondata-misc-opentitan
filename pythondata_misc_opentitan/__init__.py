7d80b0923e8f4e786b181acb6b28cb768d558d72
LiteX Robot <robot@timvideos.us>
2021-04-12 07:11:33 +0000

    Updating pythondata-misc-opentitan to 0.0.post5818
    Updated data to v0.0-5723-g1def47b25 based on 1def47b2507b73b66d045695c03946e5349f738d from https://github.com/lowRISC/opentitan.
    > commit 1def47b2507b73b66d045695c03946e5349f738d
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Fri Apr 9 10:07:55 2021 +0100
    >
    >     [keymgr] Define an intermediate signal for setting cfg_regwen
    >
    >     This avoids an UNOPTFLAT warning from Verilator, caused because
    >     Verilator doesn't automatically split the hw2reg structure when
    >     topologically sorting the processes that read to and write from it.
    >     Another way to address this is to use a "split_var" Verilator-specific
    >     hint, but I think the code is also probably clearer for humans when
    >     rewritten this way.
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,39 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5815"
-version_tuple = (0, 0, 5815)
+version_str = "0.0.post5818"
+version_tuple = (0, 0, 5818)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5815")
+    pversion = V("0.0.post5818")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5720"
-data_version_tuple = (0, 0, 5720)
+data_version_str = "0.0.post5723"
+data_version_tuple = (0, 0, 5723)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5720")
+    pdata_version = V("0.0.post5723")
 except ImportError:
     pass
-data_git_hash = "28047086d5d14a00c1feac17c5616fde871d0a31"
-data_git_describe = "v0.0-5720-g28047086d"
+data_git_hash = "1def47b2507b73b66d045695c03946e5349f738d"
+data_git_describe = "v0.0-5723-g1def47b25"
 data_git_msg = """\
-commit 28047086d5d14a00c1feac17c5616fde871d0a31
-Author: Timothy Chen <timothytim@google.com>
-Date:   Thu Apr 8 09:18:13 2021 -0700
+commit 1def47b2507b73b66d045695c03946e5349f738d
+Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
+Date:   Fri Apr 9 10:07:55 2021 +0100
 
-    [ci] Remove dif_entropy_smoketest completely from CI
+    [keymgr] Define an intermediate signal for setting cfg_regwen
     
-    - The test will be reintroduced when #5941 is ready
-      and can produce reliable results.
+    This avoids an UNOPTFLAT warning from Verilator, caused because
+    Verilator doesn't automatically split the hw2reg structure when
+    topologically sorting the processes that read to and write from it.
+    Another way to address this is to use a "split_var" Verilator-specific
+    hint, but I think the code is also probably clearer for humans when
+    rewritten this way.
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
 """
 
