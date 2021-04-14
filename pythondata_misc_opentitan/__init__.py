eb9cce40017b406bafcdac077e783de824e341d1
LiteX Robot <robot@timvideos.us>
2021-04-14 04:04:48 +0000

    Updating pythondata-misc-opentitan to 0.0.post5859
    Updated data to v0.0-5764-g9312b9e52 based on 9312b9e529636114dc33dbd2937b489fe338af98 from https://github.com/lowRISC/opentitan.
    > commit 9312b9e529636114dc33dbd2937b489fe338af98
    > Author: Michael Schaffner <msf@opentitan.org>
    > Date:   Tue Apr 13 17:51:01 2021 -0700
    >
    >     [lint/docs] Update ascentlint dvsim command in readme
    >
    >     Signed-off-by: Michael Schaffner <msf@opentitan.org>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,38 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5858"
-version_tuple = (0, 0, 5858)
+version_str = "0.0.post5859"
+version_tuple = (0, 0, 5859)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5858")
+    pversion = V("0.0.post5859")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5763"
-data_version_tuple = (0, 0, 5763)
+data_version_str = "0.0.post5764"
+data_version_tuple = (0, 0, 5764)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5763")
+    pdata_version = V("0.0.post5764")
 except ImportError:
     pass
-data_git_hash = "07302adc818271b9d2d7e1c3c353053955183121"
-data_git_describe = "v0.0-5763-g07302adc8"
+data_git_hash = "9312b9e529636114dc33dbd2937b489fe338af98"
+data_git_describe = "v0.0-5764-g9312b9e52"
 data_git_msg = """\
-commit 07302adc818271b9d2d7e1c3c353053955183121
-Author: Timothy Chen <timothytim@google.com>
-Date:   Tue Apr 13 11:50:31 2021 -0700
+commit 9312b9e529636114dc33dbd2937b489fe338af98
+Author: Michael Schaffner <msf@opentitan.org>
+Date:   Tue Apr 13 17:51:01 2021 -0700
 
-    [pwrmgr] Create an early indication of pwr_clamp
+    [lint/docs] Update ascentlint dvsim command in readme
     
-    The pwr_clamp_early signal asserts before pwr_clamp and de-asserts
-    after pwr_clamp.
-    
-    This gives some modules an early indication that pwr_clamp is about
-    to assert envelopes the pwr_clamp signal in timing.
-    
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Michael Schaffner <msf@opentitan.org>
 
 """
 
