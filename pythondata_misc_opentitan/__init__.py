e0bb54f36589f6b3c8c7fbbb82f9f71cb2a271d0
LiteX Robot <robot@timvideos.us>
2021-04-14 00:12:41 +0000

    Updating pythondata-misc-opentitan to 0.0.post5856
    Updated data to v0.0-5761-g338f58bd3 based on 338f58bd34d81870ab0b63aff1fc087ab5607f3d from https://github.com/lowRISC/opentitan.
    > commit 338f58bd34d81870ab0b63aff1fc087ab5607f3d
    > Author: Mark Branstad <mark.branstad@wdc.com>
    > Date:   Sat Apr 10 05:22:45 2021 -0700
    >
    >     [sha3/lint] added waivers for sha3 lint msgs
    >
    >     Signed-off-by: Mark Branstad <mark.branstad@wdc.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5855"
-version_tuple = (0, 0, 5855)
+version_str = "0.0.post5856"
+version_tuple = (0, 0, 5856)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5855")
+    pversion = V("0.0.post5856")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5760"
-data_version_tuple = (0, 0, 5760)
+data_version_str = "0.0.post5761"
+data_version_tuple = (0, 0, 5761)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5760")
+    pdata_version = V("0.0.post5761")
 except ImportError:
     pass
-data_git_hash = "fa9fc4d120c974e8ebfbcee435120afed4ba4b91"
-data_git_describe = "v0.0-5760-gfa9fc4d12"
+data_git_hash = "338f58bd34d81870ab0b63aff1fc087ab5607f3d"
+data_git_describe = "v0.0-5761-g338f58bd3"
 data_git_msg = """\
-commit fa9fc4d120c974e8ebfbcee435120afed4ba4b91
-Author: Udi Jonnalagadda <udij@google.com>
-Date:   Tue Apr 13 14:39:02 2021 -0700
+commit 338f58bd34d81870ab0b63aff1fc087ab5607f3d
+Author: Mark Branstad <mark.branstad@wdc.com>
+Date:   Sat Apr 10 05:22:45 2021 -0700
 
-    [dv/kmac] fix kmac_smoke nightly failures
+    [sha3/lint] added waivers for sha3 lint msgs
     
-    Signed-off-by: Udi Jonnalagadda <udij@google.com>
+    Signed-off-by: Mark Branstad <mark.branstad@wdc.com>
 
 """
 
