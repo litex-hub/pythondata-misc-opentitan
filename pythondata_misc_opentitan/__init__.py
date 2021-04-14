0503445655d1b0be751c6eb53d14c053c0541e4b
LiteX Robot <robot@timvideos.us>
2021-04-14 19:08:09 +0000

    Updating pythondata-misc-opentitan to 0.0.post5866
    Updated data to v0.0-5771-gcc5e3e02b based on cc5e3e02b6321423daa5c578aa3cd77ff6fce57b from https://github.com/lowRISC/opentitan.
    > commit cc5e3e02b6321423daa5c578aa3cd77ff6fce57b
    > Author: Udi Jonnalagadda <udij@google.com>
    > Date:   Thu Apr 8 16:07:29 2021 -0700
    >
    >     [dv/sram] check sram initialization in scb
    >
    >     this PR adds support for checking `CTRL.init`in the scoreboard,
    >     using a simple timing model to do so.
    >
    >     Signed-off-by: Udi Jonnalagadda <udij@google.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,40 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5865"
-version_tuple = (0, 0, 5865)
+version_str = "0.0.post5866"
+version_tuple = (0, 0, 5866)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5865")
+    pversion = V("0.0.post5866")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5770"
-data_version_tuple = (0, 0, 5770)
+data_version_str = "0.0.post5771"
+data_version_tuple = (0, 0, 5771)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5770")
+    pdata_version = V("0.0.post5771")
 except ImportError:
     pass
-data_git_hash = "952b714368b8af072c7b3376c9f593633c72a157"
-data_git_describe = "v0.0-5770-g952b71436"
+data_git_hash = "cc5e3e02b6321423daa5c578aa3cd77ff6fce57b"
+data_git_describe = "v0.0-5771-gcc5e3e02b"
 data_git_msg = """\
-commit 952b714368b8af072c7b3376c9f593633c72a157
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Wed Apr 14 13:51:33 2021 +0100
+commit cc5e3e02b6321423daa5c578aa3cd77ff6fce57b
+Author: Udi Jonnalagadda <udij@google.com>
+Date:   Thu Apr 8 16:07:29 2021 -0700
 
-    [otbn,dv] Use "done" directly in the idle checker module
+    [dv/sram] check sram initialization in scb
     
-    We were reconstructing "done" by looking at the hw2reg signals. This
-    is sort of nice, because you can do it by just looking at OTBN's
-    output ports. Unfortunately, it doesn't quite work because these
-    signals are also triggered by writes to the INTR_TEST register.
+    this PR adds support for checking `CTRL.init`in the scoreboard,
+    using a simple timing model to do so.
     
-    Since we're binding otbn_idle_checker into otbn anyway, just snoop on
-    the internal "done" signal that we actually want.
-    
-    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    Signed-off-by: Udi Jonnalagadda <udij@google.com>
 
 """
 
