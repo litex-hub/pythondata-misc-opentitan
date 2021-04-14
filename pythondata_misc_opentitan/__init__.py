4c83a721b328d327c03fa578e8656433c1aa08f7
LiteX Robot <robot@timvideos.us>
2021-04-14 01:27:37 +0000

    Updating pythondata-misc-opentitan to 0.0.post5857
    Updated data to v0.0-5762-g071e62a3a based on 071e62a3a221357a033dec34f5854958f1f2b91e from https://github.com/lowRISC/opentitan.
    > commit 071e62a3a221357a033dec34f5854958f1f2b91e
    > Author: Weicai Yang <weicai@google.com>
    > Date:   Mon Apr 12 18:14:22 2021 -0700
    >
    >     [dv] Update tl_agent to not always allow abort a_valid
    >
    >     Most likely our host won't abort a_valid. Don't always allow abort, so
    >     that if device doesn't respond, let it hang rather than abort and re-send
    >     request
    >
    >     Signed-off-by: Weicai Yang <weicai@google.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5856"
-version_tuple = (0, 0, 5856)
+version_str = "0.0.post5857"
+version_tuple = (0, 0, 5857)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5856")
+    pversion = V("0.0.post5857")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5761"
-data_version_tuple = (0, 0, 5761)
+data_version_str = "0.0.post5762"
+data_version_tuple = (0, 0, 5762)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5761")
+    pdata_version = V("0.0.post5762")
 except ImportError:
     pass
-data_git_hash = "338f58bd34d81870ab0b63aff1fc087ab5607f3d"
-data_git_describe = "v0.0-5761-g338f58bd3"
+data_git_hash = "071e62a3a221357a033dec34f5854958f1f2b91e"
+data_git_describe = "v0.0-5762-g071e62a3a"
 data_git_msg = """\
-commit 338f58bd34d81870ab0b63aff1fc087ab5607f3d
-Author: Mark Branstad <mark.branstad@wdc.com>
-Date:   Sat Apr 10 05:22:45 2021 -0700
+commit 071e62a3a221357a033dec34f5854958f1f2b91e
+Author: Weicai Yang <weicai@google.com>
+Date:   Mon Apr 12 18:14:22 2021 -0700
 
-    [sha3/lint] added waivers for sha3 lint msgs
+    [dv] Update tl_agent to not always allow abort a_valid
     
-    Signed-off-by: Mark Branstad <mark.branstad@wdc.com>
+    Most likely our host won't abort a_valid. Don't always allow abort, so
+    that if device doesn't respond, let it hang rather than abort and re-send
+    request
+    
+    Signed-off-by: Weicai Yang <weicai@google.com>
 
 """
 
