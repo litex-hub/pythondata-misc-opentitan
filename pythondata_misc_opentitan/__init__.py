17f9ca1148e1325e247839539594904ce33e1a51
LiteX Robot <robot@timvideos.us>
2021-04-17 07:07:27 +0000

    Updating pythondata-misc-opentitan to 0.0.post5939
    Updated data to v0.0-5844-gdcee03a96 based on dcee03a96bea3246a99de80c03055312015843ca from https://github.com/lowRISC/opentitan.
    > commit dcee03a96bea3246a99de80c03055312015843ca
    > Author: Guillermo Maturana <maturana@google.com>
    > Date:   Fri Apr 16 14:52:24 2021 -0700
    >
    >     [dv/dv_macros] Fix DV_PRINT_ARR_CONTENTS
    >
    >     For array `arr` with a single value 42, DV_PRINT_ARR_CONTENTS
    >     should print `arr[0] = 42 (0x2a)`; it currently prints
    >     `arr[0] = 0x42[0x2a]`.
    >
    >     Signed-off-by: Guillermo Maturana <maturana@google.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5938"
-version_tuple = (0, 0, 5938)
+version_str = "0.0.post5939"
+version_tuple = (0, 0, 5939)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5938")
+    pversion = V("0.0.post5939")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5843"
-data_version_tuple = (0, 0, 5843)
+data_version_str = "0.0.post5844"
+data_version_tuple = (0, 0, 5844)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5843")
+    pdata_version = V("0.0.post5844")
 except ImportError:
     pass
-data_git_hash = "d5c17ccdf0ce03438350cf6792c1bc1f07060e99"
-data_git_describe = "v0.0-5843-gd5c17ccdf"
+data_git_hash = "dcee03a96bea3246a99de80c03055312015843ca"
+data_git_describe = "v0.0-5844-gdcee03a96"
 data_git_msg = """\
-commit d5c17ccdf0ce03438350cf6792c1bc1f07060e99
-Author: Michael Schaffner <msf@google.com>
-Date:   Fri Apr 16 17:12:36 2021 -0700
+commit dcee03a96bea3246a99de80c03055312015843ca
+Author: Guillermo Maturana <maturana@google.com>
+Date:   Fri Apr 16 14:52:24 2021 -0700
 
-    [sdc] Correct the clock group constraint
+    [dv/dv_macros] Fix DV_PRINT_ARR_CONTENTS
     
-    Signed-off-by: Michael Schaffner <msf@google.com>
+    For array `arr` with a single value 42, DV_PRINT_ARR_CONTENTS
+    should print `arr[0] = 42 (0x2a)`; it currently prints
+    `arr[0] = 0x42[0x2a]`.
+    
+    Signed-off-by: Guillermo Maturana <maturana@google.com>
 
 """
 
