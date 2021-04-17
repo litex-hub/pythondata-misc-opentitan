d1fb063cb33272871c1a8c5ac8be7f1dd0cc773a
LiteX Robot <robot@timvideos.us>
2021-04-17 02:20:00 +0000

    Updating pythondata-misc-opentitan to 0.0.post5938
    Updated data to v0.0-5843-gd5c17ccdf based on d5c17ccdf0ce03438350cf6792c1bc1f07060e99 from https://github.com/lowRISC/opentitan.
    > commit d5c17ccdf0ce03438350cf6792c1bc1f07060e99
    > Author: Michael Schaffner <msf@google.com>
    > Date:   Fri Apr 16 17:12:36 2021 -0700
    >
    >     [sdc] Correct the clock group constraint
    >
    >     Signed-off-by: Michael Schaffner <msf@google.com>
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post5937"
-version_tuple = (0, 0, 5937)
+version_str = "0.0.post5938"
+version_tuple = (0, 0, 5938)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post5937")
+    pversion = V("0.0.post5938")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post5842"
-data_version_tuple = (0, 0, 5842)
+data_version_str = "0.0.post5843"
+data_version_tuple = (0, 0, 5843)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post5842")
+    pdata_version = V("0.0.post5843")
 except ImportError:
     pass
-data_git_hash = "45b996fbabb7c2a51e96825e9a78a9fdb4694e76"
-data_git_describe = "v0.0-5842-g45b996fba"
+data_git_hash = "d5c17ccdf0ce03438350cf6792c1bc1f07060e99"
+data_git_describe = "v0.0-5843-gd5c17ccdf"
 data_git_msg = """\
-commit 45b996fbabb7c2a51e96825e9a78a9fdb4694e76
-Author: Felix Miller <felix.miller@gi-de.com>
-Date:   Thu Apr 15 23:11:02 2021 +0200
+commit d5c17ccdf0ce03438350cf6792c1bc1f07060e99
+Author: Michael Schaffner <msf@google.com>
+Date:   Fri Apr 16 17:12:36 2021 -0700
 
-    [otbn] add test for RSA verify with exp=3
+    [sdc] Correct the clock group constraint
     
-    Adds a test for RSA signature verification including
-    an appropriate test key.
-    
-    Signed-off-by: Felix Miller <felix.miller@gi-de.com>
+    Signed-off-by: Michael Schaffner <msf@google.com>
 
 """
 
