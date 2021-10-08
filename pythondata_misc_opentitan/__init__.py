b7cb90a6340159829596ef4ed31f72d32fef1d56
LiteX Robot <robot@timvideos.us>
2021-10-08 16:02:41 +0000

    Updating pythondata-misc-opentitan to 0.0.post8178
    Updated data to v0.0-8066-gb106151c1 based on b106151c112f62def7c9bba0234f0bdb6638afea from https://github.com/lowRISC/opentitan.
    > commit b106151c112f62def7c9bba0234f0bdb6638afea
    > Author: Jade Philipoom <jadep@google.com>
    > Date:   Fri Oct 8 09:48:48 2021 +0100
    >
    >     [otbn] Update comment to describe generic functionality
    >
    >     This fixes the wording of a comment to show that the function it
    >     documents works for any OTBN application, not just RSA.
    >
    >     Signed-off-by: Jade Philipoom <jadep@google.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,39 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8177"
-version_tuple = (0, 0, 8177)
+version_str = "0.0.post8178"
+version_tuple = (0, 0, 8178)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8177")
+    pversion = V("0.0.post8178")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8065"
-data_version_tuple = (0, 0, 8065)
+data_version_str = "0.0.post8066"
+data_version_tuple = (0, 0, 8066)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8065")
+    pdata_version = V("0.0.post8066")
 except ImportError:
     pass
-data_git_hash = "9e18b46e0a96355c3c163629c8a90b70b805f24e"
-data_git_describe = "v0.0-8065-g9e18b46e0"
+data_git_hash = "b106151c112f62def7c9bba0234f0bdb6638afea"
+data_git_describe = "v0.0-8066-gb106151c1"
 data_git_msg = """\
-commit 9e18b46e0a96355c3c163629c8a90b70b805f24e
-Author: Philipp Wagner <phw@lowrisc.org>
-Date:   Fri Oct 8 12:09:28 2021 +0100
+commit b106151c112f62def7c9bba0234f0bdb6638afea
+Author: Jade Philipoom <jadep@google.com>
+Date:   Fri Oct 8 09:48:48 2021 +0100
 
-    Use pycryptodome from upstream
+    [otbn] Update comment to describe generic functionality
     
-    A new versin of pycryptodome has been released which includes support
-    for cSHAKE.
+    This fixes the wording of a comment to show that the function it
+    documents works for any OTBN application, not just RSA.
     
-    Release notes: https://www.pycryptodome.org/en/latest/src/changelog.html#october-2021
-    
-    Fixes #6694
-    
-    Signed-off-by: Philipp Wagner <phw@lowrisc.org>
+    Signed-off-by: Jade Philipoom <jadep@google.com>
 
 """
 
