bc8b25cc9669da0d911396933d9527b4f0e3ac5c
LiteX Robot <robot@timvideos.us>
2021-10-11 22:02:25 +0000

    Updating pythondata-misc-opentitan to 0.0.post8208
    Updated data to v0.0-8096-gd0ae0ebb1 based on d0ae0ebb12ad4a3e29985913802606704360c6ac from https://github.com/lowRISC/opentitan.
    > commit d0ae0ebb12ad4a3e29985913802606704360c6ac
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Thu Sep 30 00:08:30 2021 -0700
    >
    >     [dv/shadow_reg] Fix aes shadow reg failure
    >
    >     This PR fixes aes shadow reg failure:
    >     1). AES ctrl shadow reg write needs to update its write value before
    >     predicting the shadow reg.
    >     2). AES added a few more field in shadow reg.
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,37 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8204"
-version_tuple = (0, 0, 8204)
+version_str = "0.0.post8208"
+version_tuple = (0, 0, 8208)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8204")
+    pversion = V("0.0.post8208")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8092"
-data_version_tuple = (0, 0, 8092)
+data_version_str = "0.0.post8096"
+data_version_tuple = (0, 0, 8096)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8092")
+    pdata_version = V("0.0.post8096")
 except ImportError:
     pass
-data_git_hash = "16049df3f36306b8d02f640f7e96223223202bd8"
-data_git_describe = "v0.0-8092-g16049df3f"
+data_git_hash = "d0ae0ebb12ad4a3e29985913802606704360c6ac"
+data_git_describe = "v0.0-8096-gd0ae0ebb1"
 data_git_msg = """\
-commit 16049df3f36306b8d02f640f7e96223223202bd8
-Author: Michael Schaffner <msf@google.com>
-Date:   Mon Oct 4 16:29:55 2021 -0700
+commit d0ae0ebb12ad4a3e29985913802606704360c6ac
+Author: Cindy Chen <chencindy@opentitan.org>
+Date:   Thu Sep 30 00:08:30 2021 -0700
 
-    [alert_handler/doc] Update documentation with LPG feature
+    [dv/shadow_reg] Fix aes shadow reg failure
     
-    Signed-off-by: Michael Schaffner <msf@google.com>
+    This PR fixes aes shadow reg failure:
+    1). AES ctrl shadow reg write needs to update its write value before
+    predicting the shadow reg.
+    2). AES added a few more field in shadow reg.
+    
+    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
 """
 
