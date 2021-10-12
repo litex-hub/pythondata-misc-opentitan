726efe2d499c24349a05f24dd1e63ebb89c7aea6
LiteX Robot <robot@timvideos.us>
2021-10-12 10:02:37 +0000

    Updating pythondata-misc-opentitan to 0.0.post8214
    Updated data to v0.0-8102-g366c41832 based on 366c41832947783734e8026757903c7188e2d79a from https://github.com/lowRISC/opentitan.
    > commit 366c41832947783734e8026757903c7188e2d79a
    > Author: Philipp Wagner <phw@lowrisc.org>
    > Date:   Fri Oct 8 09:37:21 2021 +0100
    >
    >     [rstmgr] Add missing dependency on alert_handler_component
    >
    >     `rstmgr.sv` uses `alert_pkg`. The package containing this file needs to
    >     be listed as dependency.
    >
    >     Signed-off-by: Philipp Wagner <phw@lowrisc.org>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,49 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8213"
-version_tuple = (0, 0, 8213)
+version_str = "0.0.post8214"
+version_tuple = (0, 0, 8214)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8213")
+    pversion = V("0.0.post8214")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8101"
-data_version_tuple = (0, 0, 8101)
+data_version_str = "0.0.post8102"
+data_version_tuple = (0, 0, 8102)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8101")
+    pdata_version = V("0.0.post8102")
 except ImportError:
     pass
-data_git_hash = "1a6053dc5b759da3d5523889ada1b994de1c892d"
-data_git_describe = "v0.0-8101-g1a6053dc5"
+data_git_hash = "366c41832947783734e8026757903c7188e2d79a"
+data_git_describe = "v0.0-8102-g366c41832"
 data_git_msg = """\
-commit 1a6053dc5b759da3d5523889ada1b994de1c892d
-Author: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
-Date:   Mon Oct 11 03:50:07 2021 -0700
+commit 366c41832947783734e8026757903c7188e2d79a
+Author: Philipp Wagner <phw@lowrisc.org>
+Date:   Fri Oct 8 09:37:21 2021 +0100
 
-    [ spi_host, rtl ] Simplify SPI_HOST FSM stall behavior
+    [rstmgr] Add missing dependency on alert_handler_component
     
-    - Reverts changes to core state variables made in #7377
-        - No more "actual" or "prestall" state
+    `rstmgr.sv` uses `alert_pkg`. The package containing this file needs to
+    be listed as dependency.
     
-    Still correctly solves errors noted in #7281 with the following changes:
-    - Adds signal command_ready_int to internally track non-stall-condition values
-      for command_ready_o
-    - Internal signal new_command now depends on command_ready_int, not command_ready_o
-    - command_ready_o is masked to always output 0 during a stall condition.
-    - All registers and output signals are now enabled only when !stall
-    - The name of the state register has been renamed to simply "state_d/q"
-    
-    This results in a far simpler scheme for managing the FSM, with fewer coverage points
-    to worry about.  The previous version, though functional, left some uncertainty as
-    to how to establish coverage points for the "actual" and "prestall" states.
-    
-    - Removes corresponding TODOs from the documentation
-    
-    Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
+    Signed-off-by: Philipp Wagner <phw@lowrisc.org>
 
 """
 
