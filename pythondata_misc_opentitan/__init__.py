0d3380de03bdfb1b5350286e3aacc15c1a495419
LiteX Robot <robot@timvideos.us>
2021-10-13 20:02:26 +0000

    Updating pythondata-misc-opentitan to 0.0.post8249
    Updated data to v0.0-8137-gfebdee3ea based on febdee3ea9134ebfbaf12fe276cdf6e747129ee1 from https://github.com/lowRISC/opentitan.
    > commit febdee3ea9134ebfbaf12fe276cdf6e747129ee1
    > Author: Srikrishna Iyer <sriyer@google.com>
    > Date:   Tue Oct 5 17:45:09 2021 -0700
    >
    >     [sw/clkmgr_testutils] Add clkmgr testutils
    >
    >     Added these to make `aes_idle.c` test in PR #8451 a bit more
    >     streamlined. These utils will be needed in other idle tests.
    >
    >     Signed-off-by: Srikrishna Iyer <sriyer@google.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,48 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8248"
-version_tuple = (0, 0, 8248)
+version_str = "0.0.post8249"
+version_tuple = (0, 0, 8249)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8248")
+    pversion = V("0.0.post8249")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8136"
-data_version_tuple = (0, 0, 8136)
+data_version_str = "0.0.post8137"
+data_version_tuple = (0, 0, 8137)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8136")
+    pdata_version = V("0.0.post8137")
 except ImportError:
     pass
-data_git_hash = "e56b147144d2d9c2a412e7a52d90cd39f4a9274a"
-data_git_describe = "v0.0-8136-ge56b14714"
+data_git_hash = "febdee3ea9134ebfbaf12fe276cdf6e747129ee1"
+data_git_describe = "v0.0-8137-gfebdee3ea"
 data_git_msg = """\
-commit e56b147144d2d9c2a412e7a52d90cd39f4a9274a
-Author: Timothy Trippel <ttrippel@google.com>
-Date:   Wed Oct 6 19:01:01 2021 +0000
+commit febdee3ea9134ebfbaf12fe276cdf6e747129ee1
+Author: Srikrishna Iyer <sriyer@google.com>
+Date:   Tue Oct 5 17:45:09 2021 -0700
 
-    [hw/aon_timer] Make IRQ and signal names consistent.
+    [sw/clkmgr_testutils] Add clkmgr testutils
     
-    This fixes #8533.
+    Added these to make `aes_idle.c` test in PR #8451 a bit more
+    streamlined. These utils will be needed in other idle tests.
     
-    Every IP that can raise IRQs has an `interrupt_list` field in its HJSON
-    file that lists the names (in snake case) and brief descriptions (in a
-    full sentence/phrase) of each interrupt it produces.
-    
-    Additionally, for IPs that raise IRQs, their HJSON usually defines the
-    following three registers: INTR_STATE, INTR_ENABLE, and INTR_TEST.
-    Typically, the bit fields within each of those three registers map 1:1
-    with the interrupts listed above in the `interrupt_list` field, and
-    **_therefore their names match_**.
-    
-    However, in the aon_timer, the `wdog_timer_bark` interrupt was also
-    referred to as `wdog_timer_expired`. This fixes this naming
-    irregularity.
-    
-    Signed-off-by: Timothy Trippel <ttrippel@google.com>
+    Signed-off-by: Srikrishna Iyer <sriyer@google.com>
 
 """
 
