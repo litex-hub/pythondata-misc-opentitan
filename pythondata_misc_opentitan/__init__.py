af5f145e1b1e2778c5f4500a64a6a9f4b0a1a226
LiteX Robot <robot@timvideos.us>
2021-10-13 00:14:53 +0000

    Updating pythondata-misc-opentitan to 0.0.post8235
    Updated data to v0.0-8123-g6c7ce7683 based on 6c7ce7683b3a1ad82496db7cfd67f174164b6502 from https://github.com/lowRISC/opentitan.
    > commit 6c7ce7683b3a1ad82496db7cfd67f174164b6502
    > Author: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
    > Date:   Tue Oct 12 12:43:06 2021 -0700
    >
    >     [ spi_host, rtl ] Fix CSAAT behavior
    >
    >     When ending a segment forces SPI HOST FSM to look at CSAAT for the
    >     currently running segment, not one that is potentially pending at
    >     the FSM inputs.
    >
    >     Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8233"
-version_tuple = (0, 0, 8233)
+version_str = "0.0.post8235"
+version_tuple = (0, 0, 8235)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8233")
+    pversion = V("0.0.post8235")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8121"
-data_version_tuple = (0, 0, 8121)
+data_version_str = "0.0.post8123"
+data_version_tuple = (0, 0, 8123)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8121")
+    pdata_version = V("0.0.post8123")
 except ImportError:
     pass
-data_git_hash = "52dac1e6cf8aedc8f8df3b0fe948081cde1262a8"
-data_git_describe = "v0.0-8121-g52dac1e6c"
+data_git_hash = "6c7ce7683b3a1ad82496db7cfd67f174164b6502"
+data_git_describe = "v0.0-8123-g6c7ce7683"
 data_git_msg = """\
-commit 52dac1e6cf8aedc8f8df3b0fe948081cde1262a8
-Author: Chris Frantz <cfrantz@google.com>
-Date:   Tue Sep 21 08:11:22 2021 -0700
+commit 6c7ce7683b3a1ad82496db7cfd67f174164b6502
+Author: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
+Date:   Tue Oct 12 12:43:06 2021 -0700
 
-    [opentitantool] Primitive bootstrap implementation
+    [ spi_host, rtl ] Fix CSAAT behavior
     
-    1. Implement the primitive bootstrap protocol used by the opentitan boot_rom.
-    2. Add a bootstrap command to the opentitantool CLI.
+    When ending a segment forces SPI HOST FSM to look at CSAAT for the
+    currently running segment, not one that is potentially pending at
+    the FSM inputs.
     
-    Signed-off-by: Chris Frantz <cfrantz@google.com>
+    Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
 
 """
 
