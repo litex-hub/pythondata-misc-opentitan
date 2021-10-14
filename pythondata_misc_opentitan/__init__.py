7de5c82da5b83deafbb56ab9661f7a69c7b2d877
LiteX Robot <robot@timvideos.us>
2021-10-14 09:02:36 +0000

    Updating pythondata-misc-opentitan to 0.0.post8259
    Updated data to v0.0-8147-g22af1e8d9 based on 22af1e8d98b1a1753af0e34ef37ed333fc082c4a from https://github.com/lowRISC/opentitan.
    > commit 22af1e8d98b1a1753af0e34ef37ed333fc082c4a
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Wed Oct 13 15:46:39 2021 +0100
    >
    >     [otbn,dv] Sample RTL signals on first cycle of instruction
    >
    >     We sample various internal RTL signals in order to do coverage
    >     collection. These are supposed to be "the value of the state just
    >     before the instruction ran". Most of the time, this is the same as
    >     "the value of the state just before the last cycle of running the
    >     instruction", but that's not quite always true.
    >
    >     In particular, consider the following instruction:
    >
    >       BN.LID x1, 0(x0)
    >
    >     This reads from the call stack (x1) in order to get a destination
    >     address. With the current RTL implementation, the call stack pop
    >     happens on the first cycle which means that the previous code was
    >     sampling too late and saw an empty call stack. We wouldn't have
    >     noticed, except that the checks on "lockl_x1_uflow" in otbn_env_cov
    >     were failing (because we thought we should have seen a call stack
    >     underflow and the RTL disagreed).
    >
    >     Fixes #8587.
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,43 +4,52 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8258"
-version_tuple = (0, 0, 8258)
+version_str = "0.0.post8259"
+version_tuple = (0, 0, 8259)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8258")
+    pversion = V("0.0.post8259")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8146"
-data_version_tuple = (0, 0, 8146)
+data_version_str = "0.0.post8147"
+data_version_tuple = (0, 0, 8147)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8146")
+    pdata_version = V("0.0.post8147")
 except ImportError:
     pass
-data_git_hash = "4a8e2ce55209660d86d89f4cb054165a362292b8"
-data_git_describe = "v0.0-8146-g4a8e2ce55"
+data_git_hash = "22af1e8d98b1a1753af0e34ef37ed333fc082c4a"
+data_git_describe = "v0.0-8147-g22af1e8d9"
 data_git_msg = """\
-commit 4a8e2ce55209660d86d89f4cb054165a362292b8
-Author: Timothy Trippel <ttrippel@google.com>
-Date:   Wed Oct 6 00:19:34 2021 +0000
+commit 22af1e8d98b1a1753af0e34ef37ed333fc082c4a
+Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
+Date:   Wed Oct 13 15:46:39 2021 +0100
 
-    [dif/aon_timer] Integrate autogen'd IRQ DIFs into test code.
+    [otbn,dv] Sample RTL signals on first cycle of instruction
     
-    This commit partially addresses #8142. Specifically it:
-    1. deprecates existing (manually implemented) **Always-On Timer** IRQ DIFs,
-    2. integrates the auto-generated **Always-On Timer** IRQ DIFs into meson build
-       targets, and
-    3. refactors all existing source code to make use of the new auto-genenerated
-       **Always-On Timer** IRQ DIFs, and supporting shared DIF typedefs and
-       error codes.
+    We sample various internal RTL signals in order to do coverage
+    collection. These are supposed to be "the value of the state just
+    before the instruction ran". Most of the time, this is the same as
+    "the value of the state just before the last cycle of running the
+    instruction", but that's not quite always true.
     
-    This continues the long-term goal of auto-generating all IRQ DIFs across
-    all IPs, as described in #8142.
+    In particular, consider the following instruction:
     
-    Signed-off-by: Timothy Trippel <ttrippel@google.com>
+      BN.LID x1, 0(x0)
+    
+    This reads from the call stack (x1) in order to get a destination
+    address. With the current RTL implementation, the call stack pop
+    happens on the first cycle which means that the previous code was
+    sampling too late and saw an empty call stack. We wouldn't have
+    noticed, except that the checks on "lockl_x1_uflow" in otbn_env_cov
+    were failing (because we thought we should have seen a call stack
+    underflow and the RTL disagreed).
+    
+    Fixes #8587.
+    
+    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
 """
 
