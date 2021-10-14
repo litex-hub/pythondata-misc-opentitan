5e1a58c6c915980d33394bce3b8775bfb67033f1
LiteX Robot <robot@timvideos.us>
2021-10-14 22:01:58 +0000

    Updating pythondata-misc-opentitan to 0.0.post8282
    Updated data to v0.0-8170-g126c22a6d based on 126c22a6d4e5da97c25c26429c6529bac68671c6 from https://github.com/lowRISC/opentitan.
    > commit 126c22a6d4e5da97c25c26429c6529bac68671c6
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Thu Sep 30 15:51:46 2021 -0700
    >
    >     [top] Convert to mubi usage in some areas
    >
    >     - Convert to mubi usage for tlul interface
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    >     [dv] corresponding dv changes
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,47 +4,38 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8279"
-version_tuple = (0, 0, 8279)
+version_str = "0.0.post8282"
+version_tuple = (0, 0, 8282)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8279")
+    pversion = V("0.0.post8282")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8167"
-data_version_tuple = (0, 0, 8167)
+data_version_str = "0.0.post8170"
+data_version_tuple = (0, 0, 8170)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8167")
+    pdata_version = V("0.0.post8170")
 except ImportError:
     pass
-data_git_hash = "936db9a03080ca0a42ac56a094c1eb90e11bfd50"
-data_git_describe = "v0.0-8167-g936db9a03"
+data_git_hash = "126c22a6d4e5da97c25c26429c6529bac68671c6"
+data_git_describe = "v0.0-8170-g126c22a6d"
 data_git_msg = """\
-commit 936db9a03080ca0a42ac56a094c1eb90e11bfd50
-Author: Timothy Trippel <ttrippel@google.com>
-Date:   Wed Oct 13 19:55:11 2021 +0000
+commit 126c22a6d4e5da97c25c26429c6529bac68671c6
+Author: Timothy Chen <timothytim@google.com>
+Date:   Thu Sep 30 15:51:46 2021 -0700
 
-    [dif/alert_handler] Remove need to pass in HW params.
+    [top] Convert to mubi usage in some areas
     
-    This partially addresses #8409, with respect to the Alert Handler.
+    - Convert to mubi usage for tlul interface
     
-    Templated IPs (which have the `templated` attribute in the `<toplevel>.hjson`
-    file) **_may_** have DIFs that require extra bits of information related to
-    the specific toplevel instantiation of said IP for DIF arg-checking purposes.
-    This toplevel instantiation specific information was most recently
-    encapsulated in the `dif_<ip>_params_t` struct, which was manually defined
-    in the Alert Handler's DIF header file, and passed in as an argument to
-    various DIFs. However, the information contained in this struct is
-    already automatically generated in the `alert_handler_regs.h` header
-    file. To reduce usage complexity, the `dif_alert_handler_params_t`
-    struct was deprecated, and the required parameter information is used
-    directly from the `alert_handler_regs.h` header file throughout the
-    various Alert Handler DIFs that require this information.
+    Signed-off-by: Timothy Chen <timothytim@google.com>
     
-    Signed-off-by: Timothy Trippel <ttrippel@google.com>
+    [dv] corresponding dv changes
+    
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
