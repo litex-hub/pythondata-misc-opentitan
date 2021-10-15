1c1c4fd5990b46d22d42b76616d80905cc721123
LiteX Robot <robot@timvideos.us>
2021-10-15 18:02:29 +0000

    Updating pythondata-misc-opentitan to 0.0.post8299
    Updated data to v0.0-8187-g2db51d244 based on 2db51d2441bb2c9e643774432c0ae45f6b09e33d from https://github.com/lowRISC/opentitan.
    > commit 2db51d2441bb2c9e643774432c0ae45f6b09e33d
    > Author: Mark Branstad <mark.branstad@wdc.com>
    > Date:   Thu Oct 14 07:21:01 2021 -0700
    >
    >     [entropy_src/rtl] fix rng bit mode
    >
    >     RNG bit mode is a diagnostic mode that needed a bug fix.
    >
    >     Signed-off-by: Mark Branstad <mark.branstad@wdc.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8298"
-version_tuple = (0, 0, 8298)
+version_str = "0.0.post8299"
+version_tuple = (0, 0, 8299)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8298")
+    pversion = V("0.0.post8299")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8186"
-data_version_tuple = (0, 0, 8186)
+data_version_str = "0.0.post8187"
+data_version_tuple = (0, 0, 8187)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8186")
+    pdata_version = V("0.0.post8187")
 except ImportError:
     pass
-data_git_hash = "b9bc4878f97b9258435b106f840db27488fe0374"
-data_git_describe = "v0.0-8186-gb9bc4878f"
+data_git_hash = "2db51d2441bb2c9e643774432c0ae45f6b09e33d"
+data_git_describe = "v0.0-8187-g2db51d244"
 data_git_msg = """\
-commit b9bc4878f97b9258435b106f840db27488fe0374
-Author: Timothy Chen <timothytim@google.com>
-Date:   Wed Oct 13 15:54:02 2021 -0700
+commit 2db51d2441bb2c9e643774432c0ae45f6b09e33d
+Author: Mark Branstad <mark.branstad@wdc.com>
+Date:   Thu Oct 14 07:21:01 2021 -0700
 
-    [csrng] Mubi updates
+    [entropy_src/rtl] fix rng bit mode
     
-    - Use mubi instead of custom defined types.
+    RNG bit mode is a diagnostic mode that needed a bug fix.
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Mark Branstad <mark.branstad@wdc.com>
 
 """
 
