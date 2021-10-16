fa748d5b5708a54e0957c8be846e7f2b93ca20b7
LiteX Robot <robot@timvideos.us>
2021-10-16 22:02:18 +0000

    Updating pythondata-misc-opentitan to 0.0.post8312
    Updated data to v0.0-8200-g4ac735964 based on 4ac735964069019021cab557d39f363691b38820 from https://github.com/lowRISC/opentitan.
    > commit 4ac735964069019021cab557d39f363691b38820
    > Author: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
    > Date:   Mon Oct 11 18:04:41 2021 -0700
    >
    >     [spi_host, rtl] Fix/Refactor state wait counters in SPI_HOST FSM
    >
    >     - Fixes a bug in which there was no wait counter associated with the
    >     CSBSwitch state (it was using the wait_idle counter, but this
    >     counter was not being appropriately reset on entering CSBSwitch)
    >     - Refactors the wait_idle, wait_lead, and wait_trail counters
    >     into a single wait_cntr that can be commonly managed
    >     - wait_cntr is properly reset up entering the WaitIdle, WaitLead,
    >     WaitTrail and CSBSwitch states
    >
    >     Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,40 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8311"
-version_tuple = (0, 0, 8311)
+version_str = "0.0.post8312"
+version_tuple = (0, 0, 8312)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8311")
+    pversion = V("0.0.post8312")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8199"
-data_version_tuple = (0, 0, 8199)
+data_version_str = "0.0.post8200"
+data_version_tuple = (0, 0, 8200)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8199")
+    pdata_version = V("0.0.post8200")
 except ImportError:
     pass
-data_git_hash = "727f1312ea6602e586ff0b34029dd674c22e7ab6"
-data_git_describe = "v0.0-8199-g727f1312e"
+data_git_hash = "4ac735964069019021cab557d39f363691b38820"
+data_git_describe = "v0.0-8200-g4ac735964"
 data_git_msg = """\
-commit 727f1312ea6602e586ff0b34029dd674c22e7ab6
-Author: Chris Frantz <cfrantz@google.com>
-Date:   Wed Oct 13 09:43:09 2021 -0700
+commit 4ac735964069019021cab557d39f363691b38820
+Author: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
+Date:   Mon Oct 11 18:04:41 2021 -0700
 
-    [opentitantool] Support bitstream loading on the CW310 board
+    [spi_host, rtl] Fix/Refactor state wait counters in SPI_HOST FSM
     
-    1. Add an fpga_program function to the Transport trait.
-    2. Add the bitstream programming procedure to the CW310 backend.
-    3. Add a CLI command to program the bitstream.
+    - Fixes a bug in which there was no wait counter associated with the
+    CSBSwitch state (it was using the wait_idle counter, but this
+    counter was not being appropriately reset on entering CSBSwitch)
+    - Refactors the wait_idle, wait_lead, and wait_trail counters
+    into a single wait_cntr that can be commonly managed
+    - wait_cntr is properly reset up entering the WaitIdle, WaitLead,
+    WaitTrail and CSBSwitch states
     
-    Signed-off-by: Chris Frantz <cfrantz@google.com>
+    Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
 
 """
 
