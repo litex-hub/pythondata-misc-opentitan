61cea7d69a358451a72fb1ee6cb20d573721f798
LiteX Robot <robot@timvideos.us>
2021-10-18 09:02:17 +0000

    Updating pythondata-misc-opentitan to 0.0.post8313
    Updated data to v0.0-8201-g59ce35158 based on 59ce35158fe4ea004c7c5cc355b41c3fb12b7b1f from https://github.com/lowRISC/opentitan.
    > commit 59ce35158fe4ea004c7c5cc355b41c3fb12b7b1f
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Wed Sep 29 12:05:18 2021 +0100
    >
    >     [otbn,dv] Allow the testbench to trigger IMEM errors in ISS
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,40 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8312"
-version_tuple = (0, 0, 8312)
+version_str = "0.0.post8313"
+version_tuple = (0, 0, 8313)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8312")
+    pversion = V("0.0.post8313")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8200"
-data_version_tuple = (0, 0, 8200)
+data_version_str = "0.0.post8201"
+data_version_tuple = (0, 0, 8201)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8200")
+    pdata_version = V("0.0.post8201")
 except ImportError:
     pass
-data_git_hash = "4ac735964069019021cab557d39f363691b38820"
-data_git_describe = "v0.0-8200-g4ac735964"
+data_git_hash = "59ce35158fe4ea004c7c5cc355b41c3fb12b7b1f"
+data_git_describe = "v0.0-8201-g59ce35158"
 data_git_msg = """\
-commit 4ac735964069019021cab557d39f363691b38820
-Author: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
-Date:   Mon Oct 11 18:04:41 2021 -0700
+commit 59ce35158fe4ea004c7c5cc355b41c3fb12b7b1f
+Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
+Date:   Wed Sep 29 12:05:18 2021 +0100
 
-    [spi_host, rtl] Fix/Refactor state wait counters in SPI_HOST FSM
+    [otbn,dv] Allow the testbench to trigger IMEM errors in ISS
     
-    - Fixes a bug in which there was no wait counter associated with the
-    CSBSwitch state (it was using the wait_idle counter, but this
-    counter was not being appropriately reset on entering CSBSwitch)
-    - Refactors the wait_idle, wait_lead, and wait_trail counters
-    into a single wait_cntr that can be commonly managed
-    - wait_cntr is properly reset up entering the WaitIdle, WaitLead,
-    WaitTrail and CSBSwitch states
-    
-    Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
+    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
 """
 
