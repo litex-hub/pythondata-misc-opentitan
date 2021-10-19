34911cd55d9d4ffc3ce0cee6f4882cb35b3cd242
LiteX Robot <robot@timvideos.us>
2021-10-19 21:02:08 +0000

    Updating pythondata-misc-opentitan to 0.0.post8343
    Updated data to v0.0-8231-ge6e9fb2de based on e6e9fb2de76e92aeed4673e13d2c87ab7d5a6abd from https://github.com/lowRISC/opentitan.
    > commit e6e9fb2de76e92aeed4673e13d2c87ab7d5a6abd
    > Author: Timothy Trippel <ttrippel@google.com>
    > Date:   Wed Oct 13 00:51:52 2021 +0000
    >
    >     [rv_timer] Make interrupt names more descriptive.
    >
    >     The RV timer can raise and IRQ for each timer within each hart. In the
    >     current configuration of the RV timer, we implement one timer and one
    >     hart, so the name of the (single) resulting IRQ signal is:
    >     `timer_expired_0_0`, where the `0_0` portion signifies hart-0, timer-0.
    >
    >     This commit fixes #8681 by changing the IRQ name above to:
    >     `timer_expired_hart0_timer0`.
    >
    >     Signed-off-by: Timothy Trippel <ttrippel@google.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,40 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8342"
-version_tuple = (0, 0, 8342)
+version_str = "0.0.post8343"
+version_tuple = (0, 0, 8343)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8342")
+    pversion = V("0.0.post8343")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8230"
-data_version_tuple = (0, 0, 8230)
+data_version_str = "0.0.post8231"
+data_version_tuple = (0, 0, 8231)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8230")
+    pdata_version = V("0.0.post8231")
 except ImportError:
     pass
-data_git_hash = "61644b942f77ddb5f98714091562b4d769b1ee01"
-data_git_describe = "v0.0-8230-g61644b942"
+data_git_hash = "e6e9fb2de76e92aeed4673e13d2c87ab7d5a6abd"
+data_git_describe = "v0.0-8231-ge6e9fb2de"
 data_git_msg = """\
-commit 61644b942f77ddb5f98714091562b4d769b1ee01
-Author: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
-Date:   Sat Oct 16 15:34:36 2021 -0700
+commit e6e9fb2de76e92aeed4673e13d2c87ab7d5a6abd
+Author: Timothy Trippel <ttrippel@google.com>
+Date:   Wed Oct 13 00:51:52 2021 +0000
 
-    [ spi_host ] AUTOGEN
+    [rv_timer] Make interrupt names more descriptive.
     
-    Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
+    The RV timer can raise and IRQ for each timer within each hart. In the
+    current configuration of the RV timer, we implement one timer and one
+    hart, so the name of the (single) resulting IRQ signal is:
+    `timer_expired_0_0`, where the `0_0` portion signifies hart-0, timer-0.
+    
+    This commit fixes #8681 by changing the IRQ name above to:
+    `timer_expired_hart0_timer0`.
+    
+    Signed-off-by: Timothy Trippel <ttrippel@google.com>
 
 """
 
