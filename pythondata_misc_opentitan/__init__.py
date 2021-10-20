b1586eb0fbaf0a4fd93c053bb74357ba9f055577
LiteX Robot <robot@timvideos.us>
2021-10-20 19:02:12 +0000

    Updating pythondata-misc-opentitan to 0.0.post8360
    Updated data to v0.0-8248-gf0f3426ba based on f0f3426ba5a49d728bb623e428130941028c88cb from https://github.com/lowRISC/opentitan.
    > commit f0f3426ba5a49d728bb623e428130941028c88cb
    > Author: Timothy Trippel <ttrippel@google.com>
    > Date:   Wed Oct 20 00:22:51 2021 +0000
    >
    >     [dif/rv_timer] Prepare for `dif_rv_timer_init()` autogen.
    >
    >     The RV Timer `dif_rv_timer_init()` function contained reset logic that was
    >     inconsistent with other IP's DIF APIs. In order to auto-generate the
    >     `dif_<ip>_init()` function across all IPs (as stated in #8409), this reset
    >     logic must be separated into a separate DIF/DIF invocation.
    >
    >     Signed-off-by: Timothy Trippel <ttrippel@google.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,44 +4,37 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8356"
-version_tuple = (0, 0, 8356)
+version_str = "0.0.post8360"
+version_tuple = (0, 0, 8360)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8356")
+    pversion = V("0.0.post8360")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8244"
-data_version_tuple = (0, 0, 8244)
+data_version_str = "0.0.post8248"
+data_version_tuple = (0, 0, 8248)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8244")
+    pdata_version = V("0.0.post8248")
 except ImportError:
     pass
-data_git_hash = "6386c757419ca1cdf24004a1bc830931a827850b"
-data_git_describe = "v0.0-8244-g6386c7574"
+data_git_hash = "f0f3426ba5a49d728bb623e428130941028c88cb"
+data_git_describe = "v0.0-8248-gf0f3426ba"
 data_git_msg = """\
-commit 6386c757419ca1cdf24004a1bc830931a827850b
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Fri Oct 15 15:14:14 2021 +0100
+commit f0f3426ba5a49d728bb623e428130941028c88cb
+Author: Timothy Trippel <ttrippel@google.com>
+Date:   Wed Oct 20 00:22:51 2021 +0000
 
-    [otbn] Flop STATUS register
+    [dif/rv_timer] Prepare for `dif_rv_timer_init()` autogen.
     
-    This ensures that the STATUS register switches from IDLE to
-    BUSY_EXECUTE at the start of an operation on the same cycle as we zero
-    out INSN_CNT.
+    The RV Timer `dif_rv_timer_init()` function contained reset logic that was
+    inconsistent with other IP's DIF APIs. In order to auto-generate the
+    `dif_<ip>_init()` function across all IPs (as stated in #8409), this reset
+    logic must be separated into a separate DIF/DIF invocation.
     
-    The STATUS change is also delayed by a cycle at the other end of the
-    run. Thus we rename the derived "done" signal in the model from
-    done_r_o to done_rr_o, showing that it's now delayed by two cycles.
-    
-    The only other change is to add an expected stall cycle to the count
-    in stats_test.py, since there really is one extra stalled cycle that
-    we count.
-    
-    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    Signed-off-by: Timothy Trippel <ttrippel@google.com>
 
 """
 
