a5577b9cd4ea13385ff71eff432eabc0f11bc8d9
LiteX Robot <robot@timvideos.us>
2021-10-21 09:02:20 +0000

    Updating pythondata-misc-opentitan to 0.0.post8372
    Updated data to v0.0-8260-g8d876f109 based on 8d876f10990d516ed83e3b058d089fd3e7176380 from https://github.com/lowRISC/opentitan.
    > commit 8d876f10990d516ed83e3b058d089fd3e7176380
    > Author: Jade Philipoom <jadep@google.com>
    > Date:   Wed Oct 20 17:10:55 2021 +0100
    >
    >     [otbn/util] Update RISC-V toolchain search to look in default location.
    >
    >     Signed-off-by: Jade Philipoom <jadep@google.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,48 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8369"
-version_tuple = (0, 0, 8369)
+version_str = "0.0.post8372"
+version_tuple = (0, 0, 8372)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8369")
+    pversion = V("0.0.post8372")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8257"
-data_version_tuple = (0, 0, 8257)
+data_version_str = "0.0.post8260"
+data_version_tuple = (0, 0, 8260)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8257")
+    pdata_version = V("0.0.post8260")
 except ImportError:
     pass
-data_git_hash = "bf7faa427a5adf377a4245ad4689be831021934a"
-data_git_describe = "v0.0-8257-gbf7faa427"
+data_git_hash = "8d876f10990d516ed83e3b058d089fd3e7176380"
+data_git_describe = "v0.0-8260-g8d876f109"
 data_git_msg = """\
-commit bf7faa427a5adf377a4245ad4689be831021934a
-Author: Srikrishna Iyer <sriyer@google.com>
-Date:   Tue Oct 19 22:22:19 2021 -0700
+commit 8d876f10990d516ed83e3b058d089fd3e7176380
+Author: Jade Philipoom <jadep@google.com>
+Date:   Wed Oct 20 17:10:55 2021 +0100
 
-    [dvsim] Enabling glob-style patterns for -i switch
+    [otbn/util] Update RISC-V toolchain search to look in default location.
     
-    This commit enables the user to pass glob-style patterns instead of
-    fully resolved test/regression names to `-i|--items` switch.
-    
-    The following are some examples:
-    - Run all CSR tests: `-i *csr*`
-    - Run all sleep tests: `-i chip_sw_sleep*`
-    
-    As before, it matches the input patterns with the list of available
-    regrssions first. For example, `-i *smoke*` will add all tests enabled
-    for the smoke regressions AND then, it will add all tests containing the
-    pattern `*smoke*` in the name that are not added to the smoke
-    regression. If multiple regressions match the input patterns, and there
-    is an overlap of tests, then the subsequent regressions are dropped with
-    a warning message. If an input pattern does not match any available
-    regression or test name, then a warning is thrown.
-    
-    Signed-off-by: Srikrishna Iyer <sriyer@google.com>
+    Signed-off-by: Jade Philipoom <jadep@google.com>
 
 """
 
