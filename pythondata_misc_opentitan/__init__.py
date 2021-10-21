3fa89e0f5963d06fcae705923dad394442832891
LiteX Robot <robot@timvideos.us>
2021-10-21 16:17:08 +0000

    Updating pythondata-misc-opentitan to 0.0.post8384
    Updated data to v0.0-8272-g45faebcae based on 45faebcae0fcbf5c34d2d8fbc49869d95af60bb8 from https://github.com/lowRISC/opentitan.
    > commit 45faebcae0fcbf5c34d2d8fbc49869d95af60bb8
    > Author: Chris Frantz <cfrantz@google.com>
    > Date:   Wed Oct 20 16:13:41 2021 -0700
    >
    >     [opentitantool] Run `cargo fmt`
    >
    >     1. Apply standard formatting via `cargo fmt`.
    >
    >     Signed-off-by: Chris Frantz <cfrantz@google.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8379"
-version_tuple = (0, 0, 8379)
+version_str = "0.0.post8384"
+version_tuple = (0, 0, 8384)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8379")
+    pversion = V("0.0.post8384")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8267"
-data_version_tuple = (0, 0, 8267)
+data_version_str = "0.0.post8272"
+data_version_tuple = (0, 0, 8272)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8267")
+    pdata_version = V("0.0.post8272")
 except ImportError:
     pass
-data_git_hash = "d726f959ed37eadc2fc4a6a8747350a8c66157a6"
-data_git_describe = "v0.0-8267-gd726f959e"
+data_git_hash = "45faebcae0fcbf5c34d2d8fbc49869d95af60bb8"
+data_git_describe = "v0.0-8272-g45faebcae"
 data_git_msg = """\
-commit d726f959ed37eadc2fc4a6a8747350a8c66157a6
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Wed Oct 20 17:50:53 2021 +0100
+commit 45faebcae0fcbf5c34d2d8fbc49869d95af60bb8
+Author: Chris Frantz <cfrantz@google.com>
+Date:   Wed Oct 20 16:13:41 2021 -0700
 
-    [otbn,dv] Add otbn_imem_err to the "core" regression
+    [opentitantool] Run `cargo fmt`
     
-    These sequences are the intended to be the ones that run code and we
-    forgot to add otbn_imem_err when defining it.
+    1. Apply standard formatting via `cargo fmt`.
     
-    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    Signed-off-by: Chris Frantz <cfrantz@google.com>
 
 """
 
