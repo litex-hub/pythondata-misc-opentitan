6567dd0052f1b8151966b5173d33debc8e4b19fb
LiteX Robot <robot@timvideos.us>
2021-10-21 22:02:27 +0000

    Updating pythondata-misc-opentitan to 0.0.post8395
    Updated data to v0.0-8283-gdab021e1b based on dab021e1b7932ae2b9f7ea0f1930807c24eb2cf2 from https://github.com/lowRISC/opentitan.
    > commit dab021e1b7932ae2b9f7ea0f1930807c24eb2cf2
    > Author: Weicai Yang <weicai@google.com>
    > Date:   Wed Oct 20 17:12:45 2021 -0700
    >
    >     [dv] Update kmac_app_monitor to remove glitch of ok_to_end
    >
    >     The update doesn't change any functionality but this will remove 0 delay
    >     glich of ok_to_end
    >     Signed-off-by: Weicai Yang <weicai@google.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8391"
-version_tuple = (0, 0, 8391)
+version_str = "0.0.post8395"
+version_tuple = (0, 0, 8395)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8391")
+    pversion = V("0.0.post8395")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8279"
-data_version_tuple = (0, 0, 8279)
+data_version_str = "0.0.post8283"
+data_version_tuple = (0, 0, 8283)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8279")
+    pdata_version = V("0.0.post8283")
 except ImportError:
     pass
-data_git_hash = "0d9c43a552498ee2bbfbbe80e3f767066e572223"
-data_git_describe = "v0.0-8279-g0d9c43a55"
+data_git_hash = "dab021e1b7932ae2b9f7ea0f1930807c24eb2cf2"
+data_git_describe = "v0.0-8283-gdab021e1b"
 data_git_msg = """\
-commit 0d9c43a552498ee2bbfbbe80e3f767066e572223
-Author: Timothy Trippel <ttrippel@google.com>
-Date:   Wed Oct 20 21:13:29 2021 +0000
+commit dab021e1b7932ae2b9f7ea0f1930807c24eb2cf2
+Author: Weicai Yang <weicai@google.com>
+Date:   Wed Oct 20 17:12:45 2021 -0700
 
-    [dif/sysrst_ctrl] Auto-generate portion of DIFs.
+    [dv] Update kmac_app_monitor to remove glitch of ok_to_end
     
-    Currently, the DIF library for the System Reset Control IP is
-    unimplemented. This begins the implementation of this DIF library by
-    first checking in the auto-generated DIFs (init and IRQ DIFs).
-    
-    Signed-off-by: Timothy Trippel <ttrippel@google.com>
+    The update doesn't change any functionality but this will remove 0 delay
+    glich of ok_to_end
+    Signed-off-by: Weicai Yang <weicai@google.com>
 
 """
 
