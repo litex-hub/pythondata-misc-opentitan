d4d9a8ec000e283e9b51a1d329475ea90241e8fb
LiteX Robot <robot@timvideos.us>
2021-10-21 14:02:19 +0000

    Updating pythondata-misc-opentitan to 0.0.post8375
    Updated data to v0.0-8263-gca82314d8 based on ca82314d866d4f0e41243081d30c958ed6a4dad8 from https://github.com/lowRISC/opentitan.
    > commit ca82314d866d4f0e41243081d30c958ed6a4dad8
    > Author: Tomasz Gorochowik <tgorochowik@antmicro.com>
    > Date:   Tue Oct 19 13:54:46 2021 +0200
    >
    >     Setup verible-linter github action
    >
    >     Signed-off-by: Tomasz Gorochowik <tgorochowik@antmicro.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8374"
-version_tuple = (0, 0, 8374)
+version_str = "0.0.post8375"
+version_tuple = (0, 0, 8375)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8374")
+    pversion = V("0.0.post8375")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8262"
-data_version_tuple = (0, 0, 8262)
+data_version_str = "0.0.post8263"
+data_version_tuple = (0, 0, 8263)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8262")
+    pdata_version = V("0.0.post8263")
 except ImportError:
     pass
-data_git_hash = "5feafbe44e8fc9e19c280a5b1183dac67ac90cea"
-data_git_describe = "v0.0-8262-g5feafbe44"
+data_git_hash = "ca82314d866d4f0e41243081d30c958ed6a4dad8"
+data_git_describe = "v0.0-8263-gca82314d8"
 data_git_msg = """\
-commit 5feafbe44e8fc9e19c280a5b1183dac67ac90cea
-Author: Alphan Ulusoy <alphan@google.com>
-Date:   Wed Oct 20 17:17:52 2021 -0400
+commit ca82314d866d4f0e41243081d30c958ed6a4dad8
+Author: Tomasz Gorochowik <tgorochowik@antmicro.com>
+Date:   Tue Oct 19 13:54:46 2021 +0200
 
-    [sw/silicon_creator] Fix typo in bootstrap module enum name
+    Setup verible-linter github action
     
-    Signed-off-by: Alphan Ulusoy <alphan@google.com>
+    Signed-off-by: Tomasz Gorochowik <tgorochowik@antmicro.com>
 
 """
 
