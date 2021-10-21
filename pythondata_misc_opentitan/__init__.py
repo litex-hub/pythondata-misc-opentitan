9eb11c592e3549c32dfdd7ea5224e22d66249dce
LiteX Robot <robot@timvideos.us>
2021-10-21 10:02:29 +0000

    Updating pythondata-misc-opentitan to 0.0.post8373
    Updated data to v0.0-8261-g37c78bd29 based on 37c78bd29b25851f78178d632e078d6ee4ae9d15 from https://github.com/lowRISC/opentitan.
    > commit 37c78bd29b25851f78178d632e078d6ee4ae9d15
    > Author: Vladimir Rozic <vrozic@lowrisc.org>
    > Date:   Mon Oct 4 13:17:49 2021 +0100
    >
    >     [otbn] Added RTL for internal secure wipe
    >
    >     At the end of the OTBN operation the internal state is wiped by
    >     randomizing (using URNG data) and zeroing:
    >       -Wide Data registers
    >       -General purpose registers
    >       -The accumulator register
    >       -The modulus
    >
    >     Call and loop stack pointers are reset.
    >     Flags are set to zero.
    >
    >     This operation can be switched on/off via parameter SecWipeEn
    >     at otbn_core.sv (default SecWipeEn = 1'b0)
    >
    >     Added internal secure wipe.
    >
    >     Signed-off-by: Vladimir Rozic <vrozic@lowrisc.org>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,47 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8372"
-version_tuple = (0, 0, 8372)
+version_str = "0.0.post8373"
+version_tuple = (0, 0, 8373)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8372")
+    pversion = V("0.0.post8373")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8260"
-data_version_tuple = (0, 0, 8260)
+data_version_str = "0.0.post8261"
+data_version_tuple = (0, 0, 8261)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8260")
+    pdata_version = V("0.0.post8261")
 except ImportError:
     pass
-data_git_hash = "8d876f10990d516ed83e3b058d089fd3e7176380"
-data_git_describe = "v0.0-8260-g8d876f109"
+data_git_hash = "37c78bd29b25851f78178d632e078d6ee4ae9d15"
+data_git_describe = "v0.0-8261-g37c78bd29"
 data_git_msg = """\
-commit 8d876f10990d516ed83e3b058d089fd3e7176380
-Author: Jade Philipoom <jadep@google.com>
-Date:   Wed Oct 20 17:10:55 2021 +0100
+commit 37c78bd29b25851f78178d632e078d6ee4ae9d15
+Author: Vladimir Rozic <vrozic@lowrisc.org>
+Date:   Mon Oct 4 13:17:49 2021 +0100
 
-    [otbn/util] Update RISC-V toolchain search to look in default location.
+    [otbn] Added RTL for internal secure wipe
     
-    Signed-off-by: Jade Philipoom <jadep@google.com>
+    At the end of the OTBN operation the internal state is wiped by
+    randomizing (using URNG data) and zeroing:
+      -Wide Data registers
+      -General purpose registers
+      -The accumulator register
+      -The modulus
+    
+    Call and loop stack pointers are reset.
+    Flags are set to zero.
+    
+    This operation can be switched on/off via parameter SecWipeEn
+    at otbn_core.sv (default SecWipeEn = 1'b0)
+    
+    Added internal secure wipe.
+    
+    Signed-off-by: Vladimir Rozic <vrozic@lowrisc.org>
 
 """
 
