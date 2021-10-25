8e93998ccdb8505d6c289295b775362a39f72b61
LiteX Robot <robot@timvideos.us>
2021-10-25 16:03:47 +0000

    Updating pythondata-misc-opentitan to 0.0.post8423
    Updated data to v0.0-8311-g241a82c66 based on 241a82c665d1823ac70f8a63a6943a2cd8dd8ac9 from https://github.com/lowRISC/opentitan.
    > commit 241a82c665d1823ac70f8a63a6943a2cd8dd8ac9
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Thu Sep 23 16:32:54 2021 -0700
    >
    >     [fpv] prim_counter_fpv
    >
    >     1).Set up an initial FPV testbench on prim_counter.
    >     2).Move around some assertions in design to avoid conditional disable
    >     assertions in FPV.
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8419"
-version_tuple = (0, 0, 8419)
+version_str = "0.0.post8423"
+version_tuple = (0, 0, 8423)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8419")
+    pversion = V("0.0.post8423")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8307"
-data_version_tuple = (0, 0, 8307)
+data_version_str = "0.0.post8311"
+data_version_tuple = (0, 0, 8311)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8307")
+    pdata_version = V("0.0.post8311")
 except ImportError:
     pass
-data_git_hash = "3e25bcd12336f774069a2b2d037d864da007c3cc"
-data_git_describe = "v0.0-8307-g3e25bcd12"
+data_git_hash = "241a82c665d1823ac70f8a63a6943a2cd8dd8ac9"
+data_git_describe = "v0.0-8311-g241a82c66"
 data_git_msg = """\
-commit 3e25bcd12336f774069a2b2d037d864da007c3cc
-Author: Greg Chadwick <gac@lowrisc.org>
-Date:   Mon Oct 25 12:05:36 2021 +0100
+commit 241a82c665d1823ac70f8a63a6943a2cd8dd8ac9
+Author: Cindy Chen <chencindy@opentitan.org>
+Date:   Thu Sep 23 16:32:54 2021 -0700
 
-    [otbn] Suppress bad_insn_addr error on other software errors
+    [fpv] prim_counter_fpv
     
-    Fixes #8645
+    1).Set up an initial FPV testbench on prim_counter.
+    2).Move around some assertions in design to avoid conditional disable
+    assertions in FPV.
     
-    Signed-off-by: Greg Chadwick <gac@lowrisc.org>
+    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
 """
 
