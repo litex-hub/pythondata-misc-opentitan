e1deed018f27bcc8de859bd09ae30bb5c173b46d
LiteX Robot <robot@timvideos.us>
2021-10-26 12:03:44 +0000

    Updating pythondata-misc-opentitan to 0.0.post8455
    Updated data to v0.0-8343-g7857ffea4 based on 7857ffea4cb85a76bd820a41a721fb9ecb33ac70 from https://github.com/lowRISC/opentitan.
    > commit 7857ffea4cb85a76bd820a41a721fb9ecb33ac70
    > Author: Pirmin Vogel <vogelpi@lowrisc.org>
    > Date:   Mon Oct 25 16:13:45 2021 +0200
    >
    >     [aes/rtl] Cause lint errors if Sec parameters get non-default values
    >
    >     We want to avoid that the features controlled by these parameters get
    >     accidentally enabled in the final design.
    >
    >     Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,37 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8450"
-version_tuple = (0, 0, 8450)
+version_str = "0.0.post8455"
+version_tuple = (0, 0, 8455)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8450")
+    pversion = V("0.0.post8455")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8338"
-data_version_tuple = (0, 0, 8338)
+data_version_str = "0.0.post8343"
+data_version_tuple = (0, 0, 8343)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8338")
+    pdata_version = V("0.0.post8343")
 except ImportError:
     pass
-data_git_hash = "4100db3899c8a6b26bcf377f825b5725af8c8091"
-data_git_describe = "v0.0-8338-g4100db389"
+data_git_hash = "7857ffea4cb85a76bd820a41a721fb9ecb33ac70"
+data_git_describe = "v0.0-8343-g7857ffea4"
 data_git_msg = """\
-commit 4100db3899c8a6b26bcf377f825b5725af8c8091
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Thu Oct 21 16:45:02 2021 +0100
+commit 7857ffea4cb85a76bd820a41a721fb9ecb33ac70
+Author: Pirmin Vogel <vogelpi@lowrisc.org>
+Date:   Mon Oct 25 16:13:45 2021 +0200
 
-    [pwrmgr,lint] Fix some width mismatches in pwrmgr_pkg
+    [aes/rtl] Cause lint errors if Sec parameters get non-default values
     
-    These are all of the form "wide0 = wide1 + narrow" where wide* are
-    integers and narrow is an enum value that's just a couple of bits
-    wide. Make the widening to int explicit to silence the Verilator
-    warning.
+    We want to avoid that the features controlled by these parameters get
+    accidentally enabled in the final design.
     
-    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
 
 """
 
