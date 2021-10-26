bf17311ba47a604cc33ca9dd50e38e56e6f4aeb8
LiteX Robot <robot@timvideos.us>
2021-10-26 01:46:04 +0000

    Updating pythondata-misc-opentitan to 0.0.post8445
    Updated data to v0.0-8333-g3400408d9 based on 3400408d95acfc5852285e1b91914773b674520c from https://github.com/lowRISC/opentitan.
    > commit 3400408d95acfc5852285e1b91914773b674520c
    > Author: Weicai Yang <weicai@google.com>
    > Date:   Mon Oct 25 15:32:42 2021 -0700
    >
    >     [dv] Move alert init to post_apply_reset
    >
    >     This is needed for stess_all with rand reset, which invokes apply_resets_concurrently
    >     and post_apply_reset.
    >     This will also fix uart regression failures
    >
    >     Signed-off-by: Weicai Yang <weicai@google.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8443"
-version_tuple = (0, 0, 8443)
+version_str = "0.0.post8445"
+version_tuple = (0, 0, 8445)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8443")
+    pversion = V("0.0.post8445")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8331"
-data_version_tuple = (0, 0, 8331)
+data_version_str = "0.0.post8333"
+data_version_tuple = (0, 0, 8333)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8331")
+    pdata_version = V("0.0.post8333")
 except ImportError:
     pass
-data_git_hash = "a502032a3ecc8e9f3c0b9c935d935d8c78571f04"
-data_git_describe = "v0.0-8331-ga502032a3"
+data_git_hash = "3400408d95acfc5852285e1b91914773b674520c"
+data_git_describe = "v0.0-8333-g3400408d9"
 data_git_msg = """\
-commit a502032a3ecc8e9f3c0b9c935d935d8c78571f04
-Author: Srikrishna Iyer <sriyer@google.com>
-Date:   Sat Oct 23 14:51:37 2021 -0700
+commit 3400408d95acfc5852285e1b91914773b674520c
+Author: Weicai Yang <weicai@google.com>
+Date:   Mon Oct 25 15:32:42 2021 -0700
 
-    [chip, dv] Update autogen SW PLIC test
+    [dv] Move alert init to post_apply_reset
     
-    - All peripherals (that emit IRQs) are now enabled
-    - Removed helper macros to keep everything inline
-    - Added workaround for rv_timer and aon_timer
+    This is needed for stess_all with rand reset, which invokes apply_resets_concurrently
+    and post_apply_reset.
+    This will also fix uart regression failures
     
-    Signed-off-by: Srikrishna Iyer <sriyer@google.com>
+    Signed-off-by: Weicai Yang <weicai@google.com>
 
 """
 
