5427c0dbcada63edf2b1a8a4b26b9915c3598c28
LiteX Robot <robot@timvideos.us>
2021-10-26 00:15:13 +0000

    Updating pythondata-misc-opentitan to 0.0.post8443
    Updated data to v0.0-8331-ga502032a3 based on a502032a3ecc8e9f3c0b9c935d935d8c78571f04 from https://github.com/lowRISC/opentitan.
    > commit a502032a3ecc8e9f3c0b9c935d935d8c78571f04
    > Author: Srikrishna Iyer <sriyer@google.com>
    > Date:   Sat Oct 23 14:51:37 2021 -0700
    >
    >     [chip, dv] Update autogen SW PLIC test
    >
    >     - All peripherals (that emit IRQs) are now enabled
    >     - Removed helper macros to keep everything inline
    >     - Added workaround for rv_timer and aon_timer
    >
    >     Signed-off-by: Srikrishna Iyer <sriyer@google.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8440"
-version_tuple = (0, 0, 8440)
+version_str = "0.0.post8443"
+version_tuple = (0, 0, 8443)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8440")
+    pversion = V("0.0.post8443")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8328"
-data_version_tuple = (0, 0, 8328)
+data_version_str = "0.0.post8331"
+data_version_tuple = (0, 0, 8331)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8328")
+    pdata_version = V("0.0.post8331")
 except ImportError:
     pass
-data_git_hash = "116abc28855890a28010dc1a386dcbf270850083"
-data_git_describe = "v0.0-8328-g116abc288"
+data_git_hash = "a502032a3ecc8e9f3c0b9c935d935d8c78571f04"
+data_git_describe = "v0.0-8331-ga502032a3"
 data_git_msg = """\
-commit 116abc28855890a28010dc1a386dcbf270850083
-Author: Chris Frantz <cfrantz@google.com>
-Date:   Fri Oct 22 13:44:43 2021 -0700
+commit a502032a3ecc8e9f3c0b9c935d935d8c78571f04
+Author: Srikrishna Iyer <sriyer@google.com>
+Date:   Sat Oct 23 14:51:37 2021 -0700
 
-    [spi_host] shift in correct bit
+    [chip, dv] Update autogen SW PLIC test
     
-    In Single mode, we should be shifting in sd_i[1] (nee MISO).
+    - All peripherals (that emit IRQs) are now enabled
+    - Removed helper macros to keep everything inline
+    - Added workaround for rv_timer and aon_timer
     
-    Signed-off-by: Chris Frantz <cfrantz@google.com>
+    Signed-off-by: Srikrishna Iyer <sriyer@google.com>
 
 """
 
