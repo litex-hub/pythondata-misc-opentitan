3512f74c978a04b5da242fe69291bd9670095443
LiteX Robot <robot@timvideos.us>
2021-10-27 18:02:50 +0000

    Updating pythondata-misc-opentitan to 0.0.post8481
    Updated data to v0.0-8369-g0836ef34d based on 0836ef34d6ab6912702683d68fe2c81cc147ade2 from https://github.com/lowRISC/opentitan.
    > commit 0836ef34d6ab6912702683d68fe2c81cc147ade2
    > Author: Guillermo Maturana <maturana@google.com>
    > Date:   Tue Oct 26 17:14:54 2021 -0700
    >
    >     [doc/sw] Add specific example to build documentation
    >
    >     Signed-off-by: Guillermo Maturana <maturana@google.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8479"
-version_tuple = (0, 0, 8479)
+version_str = "0.0.post8481"
+version_tuple = (0, 0, 8481)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8479")
+    pversion = V("0.0.post8481")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8367"
-data_version_tuple = (0, 0, 8367)
+data_version_str = "0.0.post8369"
+data_version_tuple = (0, 0, 8369)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8367")
+    pdata_version = V("0.0.post8369")
 except ImportError:
     pass
-data_git_hash = "5adfa4eea91d66503220e29dd3b4e92faba83871"
-data_git_describe = "v0.0-8367-g5adfa4eea"
+data_git_hash = "0836ef34d6ab6912702683d68fe2c81cc147ade2"
+data_git_describe = "v0.0-8369-g0836ef34d"
 data_git_msg = """\
-commit 5adfa4eea91d66503220e29dd3b4e92faba83871
-Author: Timothy Chen <timothytim@google.com>
-Date:   Wed Oct 20 12:57:50 2021 -0700
+commit 0836ef34d6ab6912702683d68fe2c81cc147ade2
+Author: Guillermo Maturana <maturana@google.com>
+Date:   Tue Oct 26 17:14:54 2021 -0700
 
-    [util, top] Correct top level ast reset mapping
+    [doc/sw] Add specific example to build documentation
     
-    - Also fix a minor issue in `merge.py` that added
-      unnecessary domains to resets.
-    
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Guillermo Maturana <maturana@google.com>
 
 """
 
