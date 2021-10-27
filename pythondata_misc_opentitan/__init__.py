97e6d9561ccf4eee073e5763bd249204f58c4ec2
LiteX Robot <robot@timvideos.us>
2021-10-27 16:03:08 +0000

    Updating pythondata-misc-opentitan to 0.0.post8478
    Updated data to v0.0-8366-g76cb99e40 based on 76cb99e40384216db61c87ab40d0aa66829ca9db from https://github.com/lowRISC/opentitan.
    > commit 76cb99e40384216db61c87ab40d0aa66829ca9db
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Sat Oct 16 00:29:45 2021 -0700
    >
    >     [sram_ctrl] Various fixes
    >
    >     - Address issues listed in #8706
    >     - Swap the order of sram_byte and rsp_gen
    >     - Add read qualification to integrity data
    >     - Add outstanding transaction handling
    >     - Retain workarounds for #7461, that will be addressed separately.
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,39 +4,38 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8475"
-version_tuple = (0, 0, 8475)
+version_str = "0.0.post8478"
+version_tuple = (0, 0, 8478)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8475")
+    pversion = V("0.0.post8478")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8363"
-data_version_tuple = (0, 0, 8363)
+data_version_str = "0.0.post8366"
+data_version_tuple = (0, 0, 8366)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8363")
+    pdata_version = V("0.0.post8366")
 except ImportError:
     pass
-data_git_hash = "b51084435ad3a7de7a7719899172ec597d130698"
-data_git_describe = "v0.0-8363-gb51084435"
+data_git_hash = "76cb99e40384216db61c87ab40d0aa66829ca9db"
+data_git_describe = "v0.0-8366-g76cb99e40"
 data_git_msg = """\
-commit b51084435ad3a7de7a7719899172ec597d130698
-Author: Prajwala Puttappa <prajwalaputtappa@lowrisc.org>
-Date:   Thu Oct 21 17:47:33 2021 +0100
+commit 76cb99e40384216db61c87ab40d0aa66829ca9db
+Author: Timothy Chen <timothytim@google.com>
+Date:   Sat Oct 16 00:29:45 2021 -0700
 
-    [otbn, dv] Generate instructions with bad CSR / WSR addresses.
+    [sram_ctrl] Various fixes
     
-    Following additions/ modifications are done in this commit:
-    1.bad_ispr.py: A new snippet generator that picks up instructions only
-    related to CSR/ WSR registers and replaces the good register address
-    with bad address.
-    2.known_mem.py: A method called pick_bad_addr is added to pick an
-    address from the gaps in known memory.
+    - Address issues listed in #8706
+    - Swap the order of sram_byte and rsp_gen
+    - Add read qualification to integrity data
+    - Add outstanding transaction handling
+    - Retain workarounds for #7461, that will be addressed separately.
     
-    Signed-off-by: Prajwala Puttappa <prajwalaputtappa@lowrisc.org>
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
