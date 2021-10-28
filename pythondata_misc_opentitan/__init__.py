3e2bcfeaadce6b7e318a7d0ed5698516485ebe20
LiteX Robot <robot@timvideos.us>
2021-10-28 17:10:46 +0000

    Updating pythondata-misc-opentitan to 0.0.post8504
    Updated data to v0.0-8392-g4418f7d1b based on 4418f7d1b91f515dffe4b8ee13dc77d3fce98ecd from https://github.com/lowRISC/opentitan.
    > commit 4418f7d1b91f515dffe4b8ee13dc77d3fce98ecd
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Thu Oct 28 17:48:10 2021 +0100
    >
    >     [otbn,dv] Fix bad type in bad_ispr.py
    >
    >     This happened because I merged two PRs that broke each other. Oops!
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,49 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8500"
-version_tuple = (0, 0, 8500)
+version_str = "0.0.post8504"
+version_tuple = (0, 0, 8504)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8500")
+    pversion = V("0.0.post8504")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8388"
-data_version_tuple = (0, 0, 8388)
+data_version_str = "0.0.post8392"
+data_version_tuple = (0, 0, 8392)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8388")
+    pdata_version = V("0.0.post8392")
 except ImportError:
     pass
-data_git_hash = "255c02843e7ded785130b86b76af0daa3a800158"
-data_git_describe = "v0.0-8388-g255c02843"
+data_git_hash = "4418f7d1b91f515dffe4b8ee13dc77d3fce98ecd"
+data_git_describe = "v0.0-8392-g4418f7d1b"
 data_git_msg = """\
-commit 255c02843e7ded785130b86b76af0daa3a800158
+commit 4418f7d1b91f515dffe4b8ee13dc77d3fce98ecd
 Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Mon Oct 25 16:20:35 2021 +0100
+Date:   Thu Oct 28 17:48:10 2021 +0100
 
-    [otbn,dv] Fix timeout when we inject an IMEM error early in a run
+    [otbn,dv] Fix bad type in bad_ispr.py
     
-    Before commit a85aae3, we only signalled an expected alert when we saw
-    a change to STATUS. That didn't work once we'd flopped the STATUS
-    register because it changed at the same time as the alert going out.
-    
-    Unfortunately, there's also a problem with the "fix" in that commit.
-    It changed to sending the "expect an alert now" signal as soon as we
-    injected an error. However, if we inject an IMEM error just after
-    starting OTBN then it might be a while before we get URND data and
-    start trying to fetch stuff (and see that IMEM is invalid). In that
-    case, it's a while before we see the alert.
-    
-    Calling set_exp_alert has two effects. Firstly, it allows an alert to
-    come in without causing an error. Secondly, it sets a timeout,
-    requiring an alert to come in by then in order to avoid an error.
-    Here, we set max_delay to some large number of cycles (10k) to
-    essentially disable the second of these effects. However, our ISS will
-    stop soon afterwards, so we can tighten things up again by requiring
-    that an alert goes out at most 10 cycles after that happens.
+    This happened because I merged two PRs that broke each other. Oops!
     
     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
