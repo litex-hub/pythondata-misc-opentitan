8e062a531dda234a5a8dbafb18d7dd37a00aaab8
LiteX Robot <robot@timvideos.us>
2021-10-28 02:19:23 +0000

    Updating pythondata-misc-opentitan to 0.0.post8488
    Updated data to v0.0-8376-g5e04deddd based on 5e04deddd4b79a925fe031b8f212beafd7b2132a from https://github.com/lowRISC/opentitan.
    > commit 5e04deddd4b79a925fe031b8f212beafd7b2132a
    > Author: Philipp Wagner <phw@lowrisc.org>
    > Date:   Wed Oct 27 22:55:59 2021 +0100
    >
    >     Revert "[ spi_host ] Flag non-allowed write events as errors"
    >
    >     This reverts commit 3193d73abd2f41dca02f376d70c0e070d7cfcec9.
    >
    >     This commit broke private CI:
    >
    >     ```
    >     * `UVM_ERROR (csr_utils_pkg.sv:466) [csr_utils::csr_rd_check] Check failed obs == exp (* [*] vs * [*]) Regname: chip_reg_block.spi_host*.error_status` has 1 failures:
    >         * Test chip_csr_rw has 1 failures.
    >             * 0.chip_csr_rw.1\
    >               Line 130, in log /azp/agent/_work/1/s/scratch/HEAD/chip_earlgrey_asic-sim-vcs/0.chip_csr_rw/out/run.log
    >
    >                     UVM_ERROR @ 1081370000 ps: (csr_utils_pkg.sv:466) [csr_utils::csr_rd_check] Check failed obs == exp (8 [0x8] vs 0 [0x0]) Regname: chip_reg_block.spi_host1.error_status
    >                     UVM_INFO @ 1081370000 ps: (uvm_report_server.svh:901) [UVM/REPORT/SERVER]
    >                     --- UVM Report Summary ---
    >
    >                     Quit count reached!
    >     ```
    >
    >     The PR itself was green, so probably a mid-air collision.
    >
    >     Signed-off-by: Philipp Wagner <phw@lowrisc.org>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,39 +4,51 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8487"
-version_tuple = (0, 0, 8487)
+version_str = "0.0.post8488"
+version_tuple = (0, 0, 8488)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8487")
+    pversion = V("0.0.post8488")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8375"
-data_version_tuple = (0, 0, 8375)
+data_version_str = "0.0.post8376"
+data_version_tuple = (0, 0, 8376)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8375")
+    pdata_version = V("0.0.post8376")
 except ImportError:
     pass
-data_git_hash = "351ff1a4dfb698171f0c4d684c2309d75961318e"
-data_git_describe = "v0.0-8375-g351ff1a4d"
+data_git_hash = "5e04deddd4b79a925fe031b8f212beafd7b2132a"
+data_git_describe = "v0.0-8376-g5e04deddd"
 data_git_msg = """\
-commit 351ff1a4dfb698171f0c4d684c2309d75961318e
-Author: Eunchan Kim <eunchan@opentitan.org>
-Date:   Wed Oct 27 20:50:19 2021 +0000
+commit 5e04deddd4b79a925fe031b8f212beafd7b2132a
+Author: Philipp Wagner <phw@lowrisc.org>
+Date:   Wed Oct 27 22:55:59 2021 +0100
 
-    [spi_device] CmdInfo to have valid field
+    Revert "[ spi_host ] Flag non-allowed write events as errors"
     
-    Previous command info list does not have the entry valid field. So the
-    HW logic always assumes the info config is valid. It means from the SW
-    point of view, SW shall program entire lists regardless of its use.
+    This reverts commit 3193d73abd2f41dca02f376d70c0e070d7cfcec9.
     
-    This PR introduces valid field in the command info entry and HW only
-    use valid entries.
+    This commit broke private CI:
     
-    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>
+    ```
+    * `UVM_ERROR (csr_utils_pkg.sv:466) [csr_utils::csr_rd_check] Check failed obs == exp (* [*] vs * [*]) Regname: chip_reg_block.spi_host*.error_status` has 1 failures:
+        * Test chip_csr_rw has 1 failures.
+            * 0.chip_csr_rw.1\
+              Line 130, in log /azp/agent/_work/1/s/scratch/HEAD/chip_earlgrey_asic-sim-vcs/0.chip_csr_rw/out/run.log
+    
+                    UVM_ERROR @ 1081370000 ps: (csr_utils_pkg.sv:466) [csr_utils::csr_rd_check] Check failed obs == exp (8 [0x8] vs 0 [0x0]) Regname: chip_reg_block.spi_host1.error_status
+                    UVM_INFO @ 1081370000 ps: (uvm_report_server.svh:901) [UVM/REPORT/SERVER]
+                    --- UVM Report Summary ---
+    
+                    Quit count reached!
+    ```
+    
+    The PR itself was green, so probably a mid-air collision.
+    
+    Signed-off-by: Philipp Wagner <phw@lowrisc.org>
 
 """
 
