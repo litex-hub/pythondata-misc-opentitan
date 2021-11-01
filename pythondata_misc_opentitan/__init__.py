f18383a557ef358b1fde4b3fbdcc26e8f46f9d16
LiteX Robot <robot@timvideos.us>
2021-11-01 17:13:26 +0000

    Updating pythondata-misc-opentitan to 0.0.post8546
    Updated data to v0.0-8434-gddcfe93f1 based on ddcfe93f1f669f192ff4a33f1c19d827663a921f from https://github.com/lowRISC/opentitan.
    > commit ddcfe93f1f669f192ff4a33f1c19d827663a921f
    > Author: Michael Schaffner <msf@opentitan.org>
    > Date:   Tue Oct 26 15:24:23 2021 -0700
    >
    >     Update pulp_riscv_dbg to pulp-platform/riscv-dbg@4befe83
    >
    >     Update code from upstream repository https://github.com/pulp-
    >     platform/riscv-dbg to revision
    >     4befe83b03f43cef72486e0078cca0126e2680a0
    >
    >     Signed-off-by: Michael Schaffner <msf@opentitan.org>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8542"
-version_tuple = (0, 0, 8542)
+version_str = "0.0.post8546"
+version_tuple = (0, 0, 8546)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8542")
+    pversion = V("0.0.post8546")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8430"
-data_version_tuple = (0, 0, 8430)
+data_version_str = "0.0.post8434"
+data_version_tuple = (0, 0, 8434)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8430")
+    pdata_version = V("0.0.post8434")
 except ImportError:
     pass
-data_git_hash = "10eb39442fdaa80e5ff5c46edfb4d9aa7ea9474d"
-data_git_describe = "v0.0-8430-g10eb39442"
+data_git_hash = "ddcfe93f1f669f192ff4a33f1c19d827663a921f"
+data_git_describe = "v0.0-8434-gddcfe93f1"
 data_git_msg = """\
-commit 10eb39442fdaa80e5ff5c46edfb4d9aa7ea9474d
-Author: Douglas Reis <doreis@lowrisc.org>
-Date:   Tue Oct 26 14:28:49 2021 +0100
+commit ddcfe93f1f669f192ff4a33f1c19d827663a921f
+Author: Michael Schaffner <msf@opentitan.org>
+Date:   Tue Oct 26 15:24:23 2021 -0700
 
-    [dif] Implement the AES OFB and CFB modes on the DIF API
+    Update pulp_riscv_dbg to pulp-platform/riscv-dbg@4befe83
     
-    Signed-off-by: Douglas Reis <doreis@lowrisc.org>
+    Update code from upstream repository https://github.com/pulp-
+    platform/riscv-dbg to revision
+    4befe83b03f43cef72486e0078cca0126e2680a0
+    
+    Signed-off-by: Michael Schaffner <msf@opentitan.org>
 
 """
 
