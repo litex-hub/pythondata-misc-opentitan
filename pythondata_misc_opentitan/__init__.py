7c2da9db27f55cfea0b81c9edd5237bb610214a3
LiteX Robot <robot@timvideos.us>
2021-11-02 21:02:16 +0000

    Updating pythondata-misc-opentitan to 0.0.post8571
    Updated data to v0.0-8459-g0a345f933 based on 0a345f9331a47c2179b353259e288d12eab13cc1 from https://github.com/lowRISC/opentitan.
    > commit 0a345f9331a47c2179b353259e288d12eab13cc1
    > Author: Weicai Yang <weicai@google.com>
    > Date:   Mon Nov 1 11:44:58 2021 -0700
    >
    >     [dv] Update TL intg testplan
    >
    >     Merged tl_intg_err_mem_subword_cg into tl_intg_err_cg as memory doesn't
    >     exist in all IPs. This cg is only enabled when the IP contains memory.
    >     No need to have a separate entry for it.
    >
    >     Signed-off-by: Weicai Yang <weicai@google.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,49 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8569"
-version_tuple = (0, 0, 8569)
+version_str = "0.0.post8571"
+version_tuple = (0, 0, 8571)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8569")
+    pversion = V("0.0.post8571")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8457"
-data_version_tuple = (0, 0, 8457)
+data_version_str = "0.0.post8459"
+data_version_tuple = (0, 0, 8459)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8457")
+    pdata_version = V("0.0.post8459")
 except ImportError:
     pass
-data_git_hash = "3f2a1c1223d3ca969b726d0ca982e936c5a42e4b"
-data_git_describe = "v0.0-8457-g3f2a1c122"
+data_git_hash = "0a345f9331a47c2179b353259e288d12eab13cc1"
+data_git_describe = "v0.0-8459-g0a345f933"
 data_git_msg = """\
-commit 3f2a1c1223d3ca969b726d0ca982e936c5a42e4b
-Author: Guillermo Maturana <maturana@google.com>
-Date:   Wed Oct 27 14:13:42 2021 -0700
+commit 0a345f9331a47c2179b353259e288d12eab13cc1
+Author: Weicai Yang <weicai@google.com>
+Date:   Mon Nov 1 11:44:58 2021 -0700
 
-    [clkmgr, pwrmgr] Add clock handshaking for usb
+    [dv] Update TL intg testplan
     
-    Since usb clocks can be turned on/off outside of normal
-    low power routines, it creates some corner cases depending
-    on how it is used.
+    Merged tl_intg_err_mem_subword_cg into tl_intg_err_cg as memory doesn't
+    exist in all IPs. This cg is only enabled when the IP contains memory.
+    No need to have a separate entry for it.
     
-    This PR fixes [sw, dif pwrmgr smoketest] Deadlock when disabling USB clock in active power #6504 by handshaking usb oscillator disables.
-    Specifically, before the usb oscillator can be disabled, it must
-    first request downstream gating to assert. Only when this is done,
-    can the usb oscillator be turned off. This ensures there is no
-    low power entry / exit confusion.
-    
-    Note this is not the ideal fix, but this is done to minimize the amount of disruption
-    on the overall design and eventually dv.
-    
-    Some changes in DV code are required by the clkmgr change.
-    
-    RTL changes by tjaychen, DV changes by matutem.
-    
-    Signed-off-by: Guillermo Maturana <maturana@google.com>
+    Signed-off-by: Weicai Yang <weicai@google.com>
 
 """
 
