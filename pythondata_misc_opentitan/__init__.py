e64b9704697cb9992767e9408b60adf49636a3a0
LiteX Robot <robot@timvideos.us>
2021-11-02 23:02:36 +0000

    Updating pythondata-misc-opentitan to 0.0.post8572
    Updated data to v0.0-8460-g7d6d53d11 based on 7d6d53d11f21675a558da1eb51fc93c1329ca7c9 from https://github.com/lowRISC/opentitan.
    > commit 7d6d53d11f21675a558da1eb51fc93c1329ca7c9
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Mon Nov 1 15:17:08 2021 -0700
    >
    >     [sram_ctrl] Fix sram error checking and byte sequencing
    >
    >     - Should fix #8905
    >     - Previously, the error checking was mostly done downstream of
    >       sram_byte. This meant that illegal write transactions would
    >       become correctly transformed by the sram_byte module, and thus
    >       the would-be error silenced.
    >     - This PR moves all the error checking upstream of the sram byte
    >       module and passes the error through if it is observed. This is
    >       more consistent error behavior.
    >     - The documentation is also updated with a high level overview on
    >       how the adapater sram behaves.
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,43 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8571"
-version_tuple = (0, 0, 8571)
+version_str = "0.0.post8572"
+version_tuple = (0, 0, 8572)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8571")
+    pversion = V("0.0.post8572")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8459"
-data_version_tuple = (0, 0, 8459)
+data_version_str = "0.0.post8460"
+data_version_tuple = (0, 0, 8460)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8459")
+    pdata_version = V("0.0.post8460")
 except ImportError:
     pass
-data_git_hash = "0a345f9331a47c2179b353259e288d12eab13cc1"
-data_git_describe = "v0.0-8459-g0a345f933"
+data_git_hash = "7d6d53d11f21675a558da1eb51fc93c1329ca7c9"
+data_git_describe = "v0.0-8460-g7d6d53d11"
 data_git_msg = """\
-commit 0a345f9331a47c2179b353259e288d12eab13cc1
-Author: Weicai Yang <weicai@google.com>
-Date:   Mon Nov 1 11:44:58 2021 -0700
+commit 7d6d53d11f21675a558da1eb51fc93c1329ca7c9
+Author: Timothy Chen <timothytim@google.com>
+Date:   Mon Nov 1 15:17:08 2021 -0700
 
-    [dv] Update TL intg testplan
+    [sram_ctrl] Fix sram error checking and byte sequencing
     
-    Merged tl_intg_err_mem_subword_cg into tl_intg_err_cg as memory doesn't
-    exist in all IPs. This cg is only enabled when the IP contains memory.
-    No need to have a separate entry for it.
+    - Should fix #8905
+    - Previously, the error checking was mostly done downstream of
+      sram_byte. This meant that illegal write transactions would
+      become correctly transformed by the sram_byte module, and thus
+      the would-be error silenced.
+    - This PR moves all the error checking upstream of the sram byte
+      module and passes the error through if it is observed. This is
+      more consistent error behavior.
+    - The documentation is also updated with a high level overview on
+      how the adapater sram behaves.
     
-    Signed-off-by: Weicai Yang <weicai@google.com>
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
