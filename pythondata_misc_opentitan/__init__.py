c3903001cb1ecb78ad6e42c7c8f89447d15121dd
LiteX Robot <robot@timvideos.us>
2021-11-04 07:02:16 +0000

    Updating pythondata-misc-opentitan to 0.0.post8582
    Updated data to v0.0-8470-g4607d1c63 based on 4607d1c63afab73e716671f129c32bf98e69ff9a from https://github.com/lowRISC/opentitan.
    > commit 4607d1c63afab73e716671f129c32bf98e69ff9a
    > Author: Weicai Yang <weicai@google.com>
    > Date:   Tue Nov 2 16:31:23 2021 -0700
    >
    >     [dv] Fix rstmgr intg test
    >
    >     1. Added super.post_apply_reset as Cindy did in #8997
    >     2. Added associated err_code for intg error in cfg
    >
    >     Signed-off-by: Weicai Yang <weicai@google.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8581"
-version_tuple = (0, 0, 8581)
+version_str = "0.0.post8582"
+version_tuple = (0, 0, 8582)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8581")
+    pversion = V("0.0.post8582")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8469"
-data_version_tuple = (0, 0, 8469)
+data_version_str = "0.0.post8470"
+data_version_tuple = (0, 0, 8470)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8469")
+    pdata_version = V("0.0.post8470")
 except ImportError:
     pass
-data_git_hash = "f5d8bbc5ac76df9e0fcbb1aa234f3c2043b2a3bf"
-data_git_describe = "v0.0-8469-gf5d8bbc5a"
+data_git_hash = "4607d1c63afab73e716671f129c32bf98e69ff9a"
+data_git_describe = "v0.0-8470-g4607d1c63"
 data_git_msg = """\
-commit f5d8bbc5ac76df9e0fcbb1aa234f3c2043b2a3bf
-Author: Michael Schaffner <msf@google.com>
-Date:   Wed Nov 3 14:08:35 2021 -0700
+commit 4607d1c63afab73e716671f129c32bf98e69ff9a
+Author: Weicai Yang <weicai@google.com>
+Date:   Tue Nov 2 16:31:23 2021 -0700
 
-    [clkmgr/dif] Align DIF to use mubi type
+    [dv] Fix rstmgr intg test
     
-    Signed-off-by: Michael Schaffner <msf@google.com>
+    1. Added super.post_apply_reset as Cindy did in #8997
+    2. Added associated err_code for intg error in cfg
+    
+    Signed-off-by: Weicai Yang <weicai@google.com>
 
 """
 
