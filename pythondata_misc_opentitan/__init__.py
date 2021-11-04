072e092b78d51565bf9a15d4d3cd0f7d7c188c4d
LiteX Robot <robot@timvideos.us>
2021-11-04 00:15:36 +0000

    Updating pythondata-misc-opentitan to 0.0.post8581
    Updated data to v0.0-8469-gf5d8bbc5a based on f5d8bbc5ac76df9e0fcbb1aa234f3c2043b2a3bf from https://github.com/lowRISC/opentitan.
    > commit f5d8bbc5ac76df9e0fcbb1aa234f3c2043b2a3bf
    > Author: Michael Schaffner <msf@google.com>
    > Date:   Wed Nov 3 14:08:35 2021 -0700
    >
    >     [clkmgr/dif] Align DIF to use mubi type
    >
    >     Signed-off-by: Michael Schaffner <msf@google.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,49 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8580"
-version_tuple = (0, 0, 8580)
+version_str = "0.0.post8581"
+version_tuple = (0, 0, 8581)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8580")
+    pversion = V("0.0.post8581")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8468"
-data_version_tuple = (0, 0, 8468)
+data_version_str = "0.0.post8469"
+data_version_tuple = (0, 0, 8469)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8468")
+    pdata_version = V("0.0.post8469")
 except ImportError:
     pass
-data_git_hash = "755ea76e80583f4bb41aa0b02ad2a40a60eaa48f"
-data_git_describe = "v0.0-8468-g755ea76e8"
+data_git_hash = "f5d8bbc5ac76df9e0fcbb1aa234f3c2043b2a3bf"
+data_git_describe = "v0.0-8469-gf5d8bbc5a"
 data_git_msg = """\
-commit 755ea76e80583f4bb41aa0b02ad2a40a60eaa48f
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Fri Oct 22 15:21:23 2021 +0100
+commit f5d8bbc5ac76df9e0fcbb1aa234f3c2043b2a3bf
+Author: Michael Schaffner <msf@google.com>
+Date:   Wed Nov 3 14:08:35 2021 -0700
 
-    [dv] Massively shorten FOO_csr_mem_rw_with_rand_reset tests
+    [clkmgr/dif] Align DIF to use mubi type
     
-    These tests are intended to mess around with memory and then suddenly
-    pull the reset line at unexpected times. Since the device is quiescent
-    otherwise, the "time constant" for the sort of state we're trying to
-    hit edge cases in is only a few TL transactions' worth. Since a TL
-    transaction only takes ~10 cycles, there's no real need to delay
-    millions of cycles before injecting the reset.
-    
-    However, we don't also want to shorten the delay to reset for the
-    stress_all_with_rand_reset tests, because that would mean we hardly
-    did any transactions between each reset.
-    
-    So we leave the existing constraint unchanged (except slightly
-    changing the name to make it describe what's going on better), but
-    allow a caller of run_stress_all_with_rand_reset_vseq() to pass an
-    argument called reset_delay_bound. This defaults to a massive number
-    so will have no effect unless provided.
-    
-    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    Signed-off-by: Michael Schaffner <msf@google.com>
 
 """
 
