697230b6fec828493d650cf268ac25af949ddaed
LiteX Robot <robot@timvideos.us>
2021-11-05 17:11:45 +0000

    Updating pythondata-misc-opentitan to 0.0.post8608
    Updated data to v0.0-8496-g3d156ce35 based on 3d156ce35e0ba559b5246c96286c0568cfa559c0 from https://github.com/lowRISC/opentitan.
    > commit 3d156ce35e0ba559b5246c96286c0568cfa559c0
    > Author: Timothy Trippel <ttrippel@google.com>
    > Date:   Wed Nov 3 03:11:04 2021 +0000
    >
    >     [doc] Update DIF dev and style guide.
    >
    >     After auto-generating a portion of the DIFs, as described in #8142, this
    >     commit cleans up the DIF developement and style guide on the website to
    >     match what is now implemented.
    >
    >     Signed-off-by: Timothy Trippel <ttrippel@google.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8604"
-version_tuple = (0, 0, 8604)
+version_str = "0.0.post8608"
+version_tuple = (0, 0, 8608)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8604")
+    pversion = V("0.0.post8608")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8492"
-data_version_tuple = (0, 0, 8492)
+data_version_str = "0.0.post8496"
+data_version_tuple = (0, 0, 8496)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8492")
+    pdata_version = V("0.0.post8496")
 except ImportError:
     pass
-data_git_hash = "e7615f5074daeb38a4aaf99368ec11ff2879469c"
-data_git_describe = "v0.0-8492-ge7615f507"
+data_git_hash = "3d156ce35e0ba559b5246c96286c0568cfa559c0"
+data_git_describe = "v0.0-8496-g3d156ce35"
 data_git_msg = """\
-commit e7615f5074daeb38a4aaf99368ec11ff2879469c
-Author: Miguel Osorio <miguelosorio@google.com>
-Date:   Wed Nov 3 17:51:28 2021 -0700
+commit 3d156ce35e0ba559b5246c96286c0568cfa559c0
+Author: Timothy Trippel <ttrippel@google.com>
+Date:   Wed Nov 3 03:11:04 2021 +0000
 
-    [sw/rom_ext] Rename rom_ext folder.
+    [doc] Update DIF dev and style guide.
     
-    sw/device/silicon_creator/rom_exts to sw/device/silicon_creator/rom_ext
-    to avoid ambiguity.
+    After auto-generating a portion of the DIFs, as described in #8142, this
+    commit cleans up the DIF developement and style guide on the website to
+    match what is now implemented.
     
-    Signed-off-by: Miguel Osorio <miguelosorio@google.com>
+    Signed-off-by: Timothy Trippel <ttrippel@google.com>
 
 """
 
