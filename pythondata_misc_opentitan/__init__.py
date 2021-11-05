ea77e1789f51bdfba6a331e06bfb43fe7c395fa9
LiteX Robot <robot@timvideos.us>
2021-11-05 05:02:07 +0000

    Updating pythondata-misc-opentitan to 0.0.post8604
    Updated data to v0.0-8492-ge7615f507 based on e7615f5074daeb38a4aaf99368ec11ff2879469c from https://github.com/lowRISC/opentitan.
    > commit e7615f5074daeb38a4aaf99368ec11ff2879469c
    > Author: Miguel Osorio <miguelosorio@google.com>
    > Date:   Wed Nov 3 17:51:28 2021 -0700
    >
    >     [sw/rom_ext] Rename rom_ext folder.
    >
    >     sw/device/silicon_creator/rom_exts to sw/device/silicon_creator/rom_ext
    >     to avoid ambiguity.
    >
    >     Signed-off-by: Miguel Osorio <miguelosorio@google.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8603"
-version_tuple = (0, 0, 8603)
+version_str = "0.0.post8604"
+version_tuple = (0, 0, 8604)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8603")
+    pversion = V("0.0.post8604")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8491"
-data_version_tuple = (0, 0, 8491)
+data_version_str = "0.0.post8492"
+data_version_tuple = (0, 0, 8492)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8491")
+    pdata_version = V("0.0.post8492")
 except ImportError:
     pass
-data_git_hash = "ba3f6f9f1cebc380d85ceffe02215aa36db3238e"
-data_git_describe = "v0.0-8491-gba3f6f9f1"
+data_git_hash = "e7615f5074daeb38a4aaf99368ec11ff2879469c"
+data_git_describe = "v0.0-8492-ge7615f507"
 data_git_msg = """\
-commit ba3f6f9f1cebc380d85ceffe02215aa36db3238e
-Author: Alphan Ulusoy <alphan@google.com>
-Date:   Wed Nov 3 15:41:12 2021 -0400
+commit e7615f5074daeb38a4aaf99368ec11ff2879469c
+Author: Miguel Osorio <miguelosorio@google.com>
+Date:   Wed Nov 3 17:51:28 2021 -0700
 
-    [sw/silicon_creator] Simplify flash_ctrl_prog loop
+    [sw/rom_ext] Rename rom_ext folder.
     
-    Signed-off-by: Alphan Ulusoy <alphan@google.com>
+    sw/device/silicon_creator/rom_exts to sw/device/silicon_creator/rom_ext
+    to avoid ambiguity.
+    
+    Signed-off-by: Miguel Osorio <miguelosorio@google.com>
 
 """
 
