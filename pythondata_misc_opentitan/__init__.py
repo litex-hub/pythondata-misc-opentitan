2c800af737a77dfff004484d2f610ac11c806000
LiteX Robot <robot@timvideos.us>
2021-11-05 21:02:09 +0000

    Updating pythondata-misc-opentitan to 0.0.post8611
    Updated data to v0.0-8499-gae4a31f9f based on ae4a31f9fa20ee80875cb9582678c93f449d5102 from https://github.com/lowRISC/opentitan.
    > commit ae4a31f9fa20ee80875cb9582678c93f449d5102
    > Author: Mark Branstad <mark.branstad@wdc.com>
    > Date:   Wed Nov 3 15:13:48 2021 -0700
    >
    >     [edn/rtl] prim counter replacing standard counter
    >
    >     A counter used to track long generate commands has been replaced with the hardened version.
    >
    >     Signed-off-by: Mark Branstad <mark.branstad@wdc.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8609"
-version_tuple = (0, 0, 8609)
+version_str = "0.0.post8611"
+version_tuple = (0, 0, 8611)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8609")
+    pversion = V("0.0.post8611")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8497"
-data_version_tuple = (0, 0, 8497)
+data_version_str = "0.0.post8499"
+data_version_tuple = (0, 0, 8499)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8497")
+    pdata_version = V("0.0.post8499")
 except ImportError:
     pass
-data_git_hash = "2c275eba7be688e9a3d9716b8b91d56e35ce1298"
-data_git_describe = "v0.0-8497-g2c275eba7"
+data_git_hash = "ae4a31f9fa20ee80875cb9582678c93f449d5102"
+data_git_describe = "v0.0-8499-gae4a31f9f"
 data_git_msg = """\
-commit 2c275eba7be688e9a3d9716b8b91d56e35ce1298
-Author: Michael Schaffner <msf@google.com>
-Date:   Thu Nov 4 11:35:54 2021 -0700
+commit ae4a31f9fa20ee80875cb9582678c93f449d5102
+Author: Mark Branstad <mark.branstad@wdc.com>
+Date:   Wed Nov 3 15:13:48 2021 -0700
 
-    [syn/aes/otbn] Minor fixes to fix block level synthesis
+    [edn/rtl] prim counter replacing standard counter
     
-    Signed-off-by: Michael Schaffner <msf@google.com>
+    A counter used to track long generate commands has been replaced with the hardened version.
+    
+    Signed-off-by: Mark Branstad <mark.branstad@wdc.com>
 
 """
 
