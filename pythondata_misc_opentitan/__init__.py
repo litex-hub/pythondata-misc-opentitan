3cbf9575d28dbe80df94b78466f00b679c65c59f
LiteX Robot <robot@timvideos.us>
2021-11-06 08:02:23 +0000

    Updating pythondata-misc-opentitan to 0.0.post8619
    Updated data to v0.0-8507-g7dd8ba4dc based on 7dd8ba4dc8e5d77f3b77b4ba16af57b0a4804591 from https://github.com/lowRISC/opentitan.
    > commit 7dd8ba4dc8e5d77f3b77b4ba16af57b0a4804591
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Fri Nov 5 17:14:34 2021 -0700
    >
    >     [fpv/secded] Fix FPV auto-gen script
    >
    >     In previous PR where we renamed the FPV testbench to `_tb` suffix. I
    >     manually changed the FPV files, but did not change the template.
    >     This PR updates the FPV template for gen_secded.py
    >
    >     Thanks Tim for finding the issue.
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,38 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8618"
-version_tuple = (0, 0, 8618)
+version_str = "0.0.post8619"
+version_tuple = (0, 0, 8619)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8618")
+    pversion = V("0.0.post8619")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8506"
-data_version_tuple = (0, 0, 8506)
+data_version_str = "0.0.post8507"
+data_version_tuple = (0, 0, 8507)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8506")
+    pdata_version = V("0.0.post8507")
 except ImportError:
     pass
-data_git_hash = "8316200f4533b9edf692fd94f59ad13312f06944"
-data_git_describe = "v0.0-8506-g8316200f4"
+data_git_hash = "7dd8ba4dc8e5d77f3b77b4ba16af57b0a4804591"
+data_git_describe = "v0.0-8507-g7dd8ba4dc"
 data_git_msg = """\
-commit 8316200f4533b9edf692fd94f59ad13312f06944
-Author: Timothy Chen <timothytim@google.com>
-Date:   Fri Nov 5 19:58:07 2021 -0700
+commit 7dd8ba4dc8e5d77f3b77b4ba16af57b0a4804591
+Author: Cindy Chen <chencindy@opentitan.org>
+Date:   Fri Nov 5 17:14:34 2021 -0700
 
-    [csrng / edn] Add required assertions from prim_count testing
+    [fpv/secded] Fix FPV auto-gen script
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    In previous PR where we renamed the FPV testbench to `_tb` suffix. I
+    manually changed the FPV files, but did not change the template.
+    This PR updates the FPV template for gen_secded.py
+    
+    Thanks Tim for finding the issue.
+    
+    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
 """
 
