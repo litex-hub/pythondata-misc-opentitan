4ac584838168851c7481ff1ceda276c716801874
LiteX Robot <robot@timvideos.us>
2021-11-08 06:03:00 +0000

    Updating pythondata-misc-opentitan to 0.0.post8623
    Updated data to v0.0-8511-g40f724779 based on 40f72477995d57ef4e53b1568271e6414fd9d786 from https://github.com/lowRISC/opentitan.
    > commit 40f72477995d57ef4e53b1568271e6414fd9d786
    > Author: Udi Jonnalagadda <udij@google.com>
    > Date:   Tue Jun 22 15:19:20 2021 -0700
    >
    >     [dv/sram] Enable multi-ral
    >
    >     This PR enables full multi-ral functionality in the SRAM testbench, and
    >     to that end does a couple of related things:
    >
    >     - Adds a custom reg model for the memory primitive, this is needed since
    >       different parameterizations of the TB will be reused at the
    >       chip-level.
    >     - General updates to the scoreboard and rest of the environment to make
    >       use of the built-in TL arrays in the base env_cfg classes, and remove
    >       the explicitly defined tl_agent initially used for the memory
    >       interface.
    >     - Remove the `mem_tl_errors` test as we can now rely on the automated
    >       test suite.
    >     - Parameterize the entire SRAM testbench accordingly to allow reuse at
    >       the chip-level.
    >
    >     Signed-off-by: Udi Jonnalagadda <udij@google.com>
    >     Signed-off-by: Weicai Yang <weicai@google.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,48 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8622"
-version_tuple = (0, 0, 8622)
+version_str = "0.0.post8623"
+version_tuple = (0, 0, 8623)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8622")
+    pversion = V("0.0.post8623")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8510"
-data_version_tuple = (0, 0, 8510)
+data_version_str = "0.0.post8511"
+data_version_tuple = (0, 0, 8511)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8510")
+    pdata_version = V("0.0.post8511")
 except ImportError:
     pass
-data_git_hash = "c26089ae313a0627c44ef77c4ad0e3f35f213a87"
-data_git_describe = "v0.0-8510-gc26089ae3"
+data_git_hash = "40f72477995d57ef4e53b1568271e6414fd9d786"
+data_git_describe = "v0.0-8511-g40f724779"
 data_git_msg = """\
-commit c26089ae313a0627c44ef77c4ad0e3f35f213a87
-Author: Mark Branstad <mark.branstad@wdc.com>
-Date:   Tue Nov 2 13:32:28 2021 -0700
+commit 40f72477995d57ef4e53b1568271e6414fd9d786
+Author: Udi Jonnalagadda <udij@google.com>
+Date:   Tue Jun 22 15:19:20 2021 -0700
 
-    [csrng/rtl] sw port data attack check added
+    [dv/sram] Enable multi-ral
     
-    For the software port only, the returned genbits will have a repeated data check before genbits are returned to the register interface.
+    This PR enables full multi-ral functionality in the SRAM testbench, and
+    to that end does a couple of related things:
     
-    Signed-off-by: Mark Branstad <mark.branstad@wdc.com>
+    - Adds a custom reg model for the memory primitive, this is needed since
+      different parameterizations of the TB will be reused at the
+      chip-level.
+    - General updates to the scoreboard and rest of the environment to make
+      use of the built-in TL arrays in the base env_cfg classes, and remove
+      the explicitly defined tl_agent initially used for the memory
+      interface.
+    - Remove the `mem_tl_errors` test as we can now rely on the automated
+      test suite.
+    - Parameterize the entire SRAM testbench accordingly to allow reuse at
+      the chip-level.
+    
+    Signed-off-by: Udi Jonnalagadda <udij@google.com>
+    Signed-off-by: Weicai Yang <weicai@google.com>
 
 """
 
