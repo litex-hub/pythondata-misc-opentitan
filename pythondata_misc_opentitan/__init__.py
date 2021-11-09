d06521821a14e8afa379a4ee505f1ace96621327
LiteX Robot <robot@timvideos.us>
2021-11-09 20:02:37 +0000

    Updating pythondata-misc-opentitan to 0.0.post8651
    Updated data to v0.0-8539-g89df3eb3a based on 89df3eb3a3736ac08410758631ed1ca10884db1b from https://github.com/lowRISC/opentitan.
    > commit 89df3eb3a3736ac08410758631ed1ca10884db1b
    > Author: Timothy Trippel <ttrippel@google.com>
    > Date:   Fri Nov 5 06:32:48 2021 +0000
    >
    >     [dif/rom_ctrl] Add DIF header and implementation.
    >
    >     This completes the header and implementation of all ROM Controller DIFs.
    >     At this time, the ROM Controller is a very simple IP, so there are only
    >     three DIFs.
    >
    >     Signed-off-by: Timothy Trippel <ttrippel@google.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,37 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8649"
-version_tuple = (0, 0, 8649)
+version_str = "0.0.post8651"
+version_tuple = (0, 0, 8651)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8649")
+    pversion = V("0.0.post8651")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8537"
-data_version_tuple = (0, 0, 8537)
+data_version_str = "0.0.post8539"
+data_version_tuple = (0, 0, 8539)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8537")
+    pdata_version = V("0.0.post8539")
 except ImportError:
     pass
-data_git_hash = "9f7d8ccc6ab98c080e62c79e191dbed759490920"
-data_git_describe = "v0.0-8537-g9f7d8ccc6"
+data_git_hash = "89df3eb3a3736ac08410758631ed1ca10884db1b"
+data_git_describe = "v0.0-8539-g89df3eb3a"
 data_git_msg = """\
-commit 9f7d8ccc6ab98c080e62c79e191dbed759490920
-Author: Guillermo Maturana <maturana@google.com>
-Date:   Fri Nov 5 23:17:56 2021 -0700
+commit 89df3eb3a3736ac08410758631ed1ca10884db1b
+Author: Timothy Trippel <ttrippel@google.com>
+Date:   Fri Nov 5 06:32:48 2021 +0000
 
-    [dv/pwrmgr] Enhance wakeup test
+    [dif/rom_ctrl] Add DIF header and implementation.
     
-    Randomize enabling wake_info capture.
-    Fix handling enables portion of `control` CSR.
-    Fix control coverage.
-    Enable the test in the testplan.
+    This completes the header and implementation of all ROM Controller DIFs.
+    At this time, the ROM Controller is a very simple IP, so there are only
+    three DIFs.
     
-    Signed-off-by: Guillermo Maturana <maturana@google.com>
+    Signed-off-by: Timothy Trippel <ttrippel@google.com>
 
 """
 
