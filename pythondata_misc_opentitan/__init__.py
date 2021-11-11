3687ca00d4e05e1267f3460b5d3db2e8b9fc3a4d
LiteX Robot <robot@timvideos.us>
2021-11-11 04:02:50 +0000

    Updating pythondata-misc-opentitan to 0.0.post8674
    Updated data to v0.0-8562-gdc440e33b based on dc440e33b494229eb6cb7dbd7da5105c1bf87b93 from https://github.com/lowRISC/opentitan.
    > commit dc440e33b494229eb6cb7dbd7da5105c1bf87b93
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Wed Nov 10 12:15:33 2021 -0800
    >
    >     [dv/prim_alert_receiver] Fix assertion that consumes large mem
    >
    >     This PR fixes an assertion sequence with large unbounded delays that
    >     consumes large memory runtime.
    >     If I understand correctly, we do not need to wait every clock cycle in
    >     the init state for `init_trig_i` to change to True. We only need the
    >     condition that we are in the init state and `init_trig_i` changes from
    >     true to false, and in which we have an alert signal integrity error.
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,39 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8673"
-version_tuple = (0, 0, 8673)
+version_str = "0.0.post8674"
+version_tuple = (0, 0, 8674)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8673")
+    pversion = V("0.0.post8674")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8561"
-data_version_tuple = (0, 0, 8561)
+data_version_str = "0.0.post8562"
+data_version_tuple = (0, 0, 8562)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8561")
+    pdata_version = V("0.0.post8562")
 except ImportError:
     pass
-data_git_hash = "928a45f94807a9ccdc3fe01a08e7cc808ff1e2e5"
-data_git_describe = "v0.0-8561-g928a45f94"
+data_git_hash = "dc440e33b494229eb6cb7dbd7da5105c1bf87b93"
+data_git_describe = "v0.0-8562-gdc440e33b"
 data_git_msg = """\
-commit 928a45f94807a9ccdc3fe01a08e7cc808ff1e2e5
-Author: Alphan Ulusoy <alphan@google.com>
-Date:   Mon Nov 8 14:32:13 2021 -0500
+commit dc440e33b494229eb6cb7dbd7da5105c1bf87b93
+Author: Cindy Chen <chencindy@opentitan.org>
+Date:   Wed Nov 10 12:15:33 2021 -0800
 
-    [sw/silicon_creator] Add individual error codes for data and info read, write, and erase operations
+    [dv/prim_alert_receiver] Fix assertion that consumes large mem
     
-    Signed-off-by: Alphan Ulusoy <alphan@google.com>
+    This PR fixes an assertion sequence with large unbounded delays that
+    consumes large memory runtime.
+    If I understand correctly, we do not need to wait every clock cycle in
+    the init state for `init_trig_i` to change to True. We only need the
+    condition that we are in the init state and `init_trig_i` changes from
+    true to false, and in which we have an alert signal integrity error.
+    
+    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
 """
 
