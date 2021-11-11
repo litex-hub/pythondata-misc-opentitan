21e89b378fc937ec1ab76b6a4a3a90dfb9d9ded1
LiteX Robot <robot@timvideos.us>
2021-11-11 06:02:56 +0000

    Updating pythondata-misc-opentitan to 0.0.post8675
    Updated data to v0.0-8563-g69cb0a249 based on 69cb0a249e40399c89dabdd8c44db77612ec63f5 from https://github.com/lowRISC/opentitan.
    > commit 69cb0a249e40399c89dabdd8c44db77612ec63f5
    > Author: Weicai Yang <weicai@google.com>
    > Date:   Wed Nov 10 00:08:44 2021 -0800
    >
    >     [keymgr/dv] Fix shadow_reg_update_error_with_csr_rw
    >
    >     Don't do operation in shadow_reg_errors_with_csr_rw, as the
    >     csr_rw_seq runs in parallel and issueing an operation affects CSR access.
    >
    >     Signed-off-by: Weicai Yang <weicai@google.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,39 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8674"
-version_tuple = (0, 0, 8674)
+version_str = "0.0.post8675"
+version_tuple = (0, 0, 8675)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8674")
+    pversion = V("0.0.post8675")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8562"
-data_version_tuple = (0, 0, 8562)
+data_version_str = "0.0.post8563"
+data_version_tuple = (0, 0, 8563)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8562")
+    pdata_version = V("0.0.post8563")
 except ImportError:
     pass
-data_git_hash = "dc440e33b494229eb6cb7dbd7da5105c1bf87b93"
-data_git_describe = "v0.0-8562-gdc440e33b"
+data_git_hash = "69cb0a249e40399c89dabdd8c44db77612ec63f5"
+data_git_describe = "v0.0-8563-g69cb0a249"
 data_git_msg = """\
-commit dc440e33b494229eb6cb7dbd7da5105c1bf87b93
-Author: Cindy Chen <chencindy@opentitan.org>
-Date:   Wed Nov 10 12:15:33 2021 -0800
+commit 69cb0a249e40399c89dabdd8c44db77612ec63f5
+Author: Weicai Yang <weicai@google.com>
+Date:   Wed Nov 10 00:08:44 2021 -0800
 
-    [dv/prim_alert_receiver] Fix assertion that consumes large mem
+    [keymgr/dv] Fix shadow_reg_update_error_with_csr_rw
     
-    This PR fixes an assertion sequence with large unbounded delays that
-    consumes large memory runtime.
-    If I understand correctly, we do not need to wait every clock cycle in
-    the init state for `init_trig_i` to change to True. We only need the
-    condition that we are in the init state and `init_trig_i` changes from
-    true to false, and in which we have an alert signal integrity error.
+    Don't do operation in shadow_reg_errors_with_csr_rw, as the
+    csr_rw_seq runs in parallel and issueing an operation affects CSR access.
     
-    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
+    Signed-off-by: Weicai Yang <weicai@google.com>
 
 """
 
