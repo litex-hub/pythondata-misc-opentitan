b69d559625cf2803e3e42116cdeb2c3f3ce6fea3
LiteX Robot <robot@timvideos.us>
2021-11-12 20:02:11 +0000

    Updating pythondata-misc-opentitan to 0.0.post8706
    Updated data to v0.0-8594-g0310540ed based on 0310540ed5e4e25c3b695db326027726ff5b434f from https://github.com/lowRISC/opentitan.
    > commit 0310540ed5e4e25c3b695db326027726ff5b434f
    > Author: Jade Philipoom <jadep@google.com>
    > Date:   Thu Nov 4 09:50:04 2021 +0000
    >
    >     [sw] Add message encoding to RSA-3072 interface.
    >
    >     This will allow us to implement a more typical verification interface,
    >     and easily run test vectors that haven't pre-encoded the message.
    >
    >     Signed-off-by: Jade Philipoom <jadep@google.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8705"
-version_tuple = (0, 0, 8705)
+version_str = "0.0.post8706"
+version_tuple = (0, 0, 8706)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8705")
+    pversion = V("0.0.post8706")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8593"
-data_version_tuple = (0, 0, 8593)
+data_version_str = "0.0.post8594"
+data_version_tuple = (0, 0, 8594)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8593")
+    pdata_version = V("0.0.post8594")
 except ImportError:
     pass
-data_git_hash = "1ac30bea3b2502e46137e30794349e5452618bf2"
-data_git_describe = "v0.0-8593-g1ac30bea3"
+data_git_hash = "0310540ed5e4e25c3b695db326027726ff5b434f"
+data_git_describe = "v0.0-8594-g0310540ed"
 data_git_msg = """\
-commit 1ac30bea3b2502e46137e30794349e5452618bf2
-Author: Chris Frantz <cfrantz@google.com>
-Date:   Thu Nov 11 16:33:13 2021 -0800
+commit 0310540ed5e4e25c3b695db326027726ff5b434f
+Author: Jade Philipoom <jadep@google.com>
+Date:   Thu Nov 4 09:50:04 2021 +0000
 
-    [sw, cleanup, opentitantool] Minor code formatting cleanups
+    [sw] Add message encoding to RSA-3072 interface.
     
-    1. Run `cargo clippy`.  Heed its advice.
-    2. Run `cargo fmt`.
+    This will allow us to implement a more typical verification interface,
+    and easily run test vectors that haven't pre-encoded the message.
     
-    Signed-off-by: Chris Frantz <cfrantz@google.com>
+    Signed-off-by: Jade Philipoom <jadep@google.com>
 
 """
 
