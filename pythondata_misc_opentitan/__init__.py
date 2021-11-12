88227f096ad099551799752a4d520ee4a5386450
LiteX Robot <robot@timvideos.us>
2021-11-12 10:02:38 +0000

    Updating pythondata-misc-opentitan to 0.0.post8697
    Updated data to v0.0-8585-g40e87bc3c based on 40e87bc3ca98090b9bafd87968995ff414de77f7 from https://github.com/lowRISC/opentitan.
    > commit 40e87bc3ca98090b9bafd87968995ff414de77f7
    > Author: Canberk Topal <ctopal@lowrisc.org>
    > Date:   Mon Oct 25 13:07:00 2021 +0100
    >
    >     [dv] Support Multiple EDN Interfaces in OpenTitan
    >
    >     This commit includes changes in CIP to support multiple EDN IFs with
    >     setting NUM_EDN parameter in *env_pkg.sv and num_edn in *env_cfg.sv
    >     Also includes changes in:
    >     - OTBN testbench to allow connecting two EDN interfaces
    >     - In order to support EDN IF changes, DV enviroments of:
    >       - AES, Key Manager, KMAC, OTP Controller, Alert Handler
    >
    >     Signed-off-by: Canberk Topal <ctopal@lowrisc.org>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,39 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8695"
-version_tuple = (0, 0, 8695)
+version_str = "0.0.post8697"
+version_tuple = (0, 0, 8697)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8695")
+    pversion = V("0.0.post8697")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8583"
-data_version_tuple = (0, 0, 8583)
+data_version_str = "0.0.post8585"
+data_version_tuple = (0, 0, 8585)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8583")
+    pdata_version = V("0.0.post8585")
 except ImportError:
     pass
-data_git_hash = "dd3a05bc5abc9dd2874896f171d12c6eaf1576f3"
-data_git_describe = "v0.0-8583-gdd3a05bc5"
+data_git_hash = "40e87bc3ca98090b9bafd87968995ff414de77f7"
+data_git_describe = "v0.0-8585-g40e87bc3c"
 data_git_msg = """\
-commit dd3a05bc5abc9dd2874896f171d12c6eaf1576f3
-Author: Vladimir Rozic <vrozic@lowrisc.org>
-Date:   Thu Nov 11 12:11:46 2021 +0000
+commit 40e87bc3ca98090b9bafd87968995ff414de77f7
+Author: Canberk Topal <ctopal@lowrisc.org>
+Date:   Mon Oct 25 13:07:00 2021 +0100
 
-    [otbn] Updated uRNG in OTBN
+    [dv] Support Multiple EDN Interfaces in OpenTitan
     
-    Replaced OTBN's LFSR-based PRNG with xoshiro256pp.
+    This commit includes changes in CIP to support multiple EDN IFs with
+    setting NUM_EDN parameter in *env_pkg.sv and num_edn in *env_cfg.sv
+    Also includes changes in:
+    - OTBN testbench to allow connecting two EDN interfaces
+    - In order to support EDN IF changes, DV enviroments of:
+      - AES, Key Manager, KMAC, OTP Controller, Alert Handler
     
-    Signed-off-by: Vladimir Rozic <vrozic@lowrisc.org>
+    Signed-off-by: Canberk Topal <ctopal@lowrisc.org>
 
 """
 
