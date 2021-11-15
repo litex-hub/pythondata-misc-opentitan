cfdf9d48820e7ed191e755b165d2b5821629fdae
LiteX Robot <robot@timvideos.us>
2021-11-15 13:13:26 +0000

    Updating pythondata-misc-opentitan to 0.0.post8713
    Updated data to v0.0-8601-g8872015a0 based on 8872015a077af8abc279852f40e0a4f2c1b84c06 from https://github.com/lowRISC/opentitan.
    > commit 8872015a077af8abc279852f40e0a4f2c1b84c06
    > Author: Luís Marques <luismarques@lowrisc.org>
    > Date:   Sun Nov 14 22:28:36 2021 +0000
    >
    >     [sw] Fix inline functions without linking location
    >
    >     When creating builds with optimizations disabled (`-O0`) or with just
    >     inlining disabled (e.g. `-fno-inline`) there would be linking errors
    >     due to inline functions missing the corresponding extern declaration
    >     and thus not having linking locations. This fixes such errors, by
    >     adding the missing declarations and library dependencies. It also
    >     improves the consistency of those declarations, e.g. by adding the
    >     usual explanatory comment where it was missing.
    >
    >     Signed-off-by: Luís Marques <luismarques@lowrisc.org>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,40 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8712"
-version_tuple = (0, 0, 8712)
+version_str = "0.0.post8713"
+version_tuple = (0, 0, 8713)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8712")
+    pversion = V("0.0.post8713")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8600"
-data_version_tuple = (0, 0, 8600)
+data_version_str = "0.0.post8601"
+data_version_tuple = (0, 0, 8601)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8600")
+    pdata_version = V("0.0.post8601")
 except ImportError:
     pass
-data_git_hash = "4c0e61fd2ee6cbca3fb59e0c542409b53aeac606"
-data_git_describe = "v0.0-8600-g4c0e61fd2"
+data_git_hash = "8872015a077af8abc279852f40e0a4f2c1b84c06"
+data_git_describe = "v0.0-8601-g8872015a0"
 data_git_msg = """\
-commit 4c0e61fd2ee6cbca3fb59e0c542409b53aeac606
-Author: Timothy Chen <timothytim@google.com>
-Date:   Thu Nov 11 14:32:53 2021 -0800
+commit 8872015a077af8abc279852f40e0a4f2c1b84c06
+Author: Luís Marques <luismarques@lowrisc.org>
+Date:   Sun Nov 14 22:28:36 2021 +0000
 
-    [util] Add alternate hjson paths for reggen_only modules
+    [sw] Fix inline functions without linking location
     
-    - Fixes #8207
+    When creating builds with optimizations disabled (`-O0`) or with just
+    inlining disabled (e.g. `-fno-inline`) there would be linking errors
+    due to inline functions missing the corresponding extern declaration
+    and thus not having linking locations. This fixes such errors, by
+    adding the missing declarations and library dependencies. It also
+    improves the consistency of those declarations, e.g. by adding the
+    usual explanatory comment where it was missing.
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Luís Marques <luismarques@lowrisc.org>
 
 """
 
