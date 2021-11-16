ea9e8f9db15963fe3b125ebc944711e437262d8a
LiteX Robot <robot@timvideos.us>
2021-11-16 12:04:43 +0000

    Updating pythondata-misc-opentitan to 0.0.post8723
    Updated data to v0.0-8611-g2238c51f8 based on 2238c51f8fe92556213a061d19a8c233fe5c666e from https://github.com/lowRISC/opentitan.
    > commit 2238c51f8fe92556213a061d19a8c233fe5c666e
    > Author: Silvestrs Timofejevs <silvestrst@lowrisc.org>
    > Date:   Fri Nov 12 17:01:48 2021 +0000
    >
    >     [sw, tests] Main SRAM exec minor refactor
    >
    >     - Cosmetic changes (kRam..Bytes => kRam..Addr, function comments from
    >       `/* text */` => `/** text */`)
    >     - Minor bug fix (addresses are inclusive, so the check whether the
    >       instruction buffer resides within Main SRAM must include the
    >       `kRamStartAddr`)
    >
    >     Signed-off-by: Silvestrs Timofejevs <silvestrst@lowrisc.org>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,38 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8722"
-version_tuple = (0, 0, 8722)
+version_str = "0.0.post8723"
+version_tuple = (0, 0, 8723)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8722")
+    pversion = V("0.0.post8723")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8610"
-data_version_tuple = (0, 0, 8610)
+data_version_str = "0.0.post8611"
+data_version_tuple = (0, 0, 8611)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8610")
+    pdata_version = V("0.0.post8611")
 except ImportError:
     pass
-data_git_hash = "3ab2d2557de984053698378b50e753d9f39d015f"
-data_git_describe = "v0.0-8610-g3ab2d2557"
+data_git_hash = "2238c51f8fe92556213a061d19a8c233fe5c666e"
+data_git_describe = "v0.0-8611-g2238c51f8"
 data_git_msg = """\
-commit 3ab2d2557de984053698378b50e753d9f39d015f
-Author: Jade Philipoom <jadep@google.com>
-Date:   Mon Nov 15 16:56:06 2021 +0000
+commit 2238c51f8fe92556213a061d19a8c233fe5c666e
+Author: Silvestrs Timofejevs <silvestrst@lowrisc.org>
+Date:   Fri Nov 12 17:01:48 2021 +0000
 
-    [doc] Update OTBN style guide.
+    [sw, tests] Main SRAM exec minor refactor
     
-    Add some advice on inline data, and update the variable-time example to
-    show good practice with regard to the OTBN loop stack.
+    - Cosmetic changes (kRam..Bytes => kRam..Addr, function comments from
+      `/* text */` => `/** text */`)
+    - Minor bug fix (addresses are inclusive, so the check whether the
+      instruction buffer resides within Main SRAM must include the
+      `kRamStartAddr`)
     
-    Signed-off-by: Jade Philipoom <jadep@google.com>
+    Signed-off-by: Silvestrs Timofejevs <silvestrst@lowrisc.org>
 
 """
 
