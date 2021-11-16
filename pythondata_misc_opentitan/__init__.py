4e2720f72204264e9d51fda3238cceaad6975938
LiteX Robot <robot@timvideos.us>
2021-11-16 21:02:18 +0000

    Updating pythondata-misc-opentitan to 0.0.post8738
    Updated data to v0.0-8626-g84291ebf7 based on 84291ebf7c2a740ffdd4954de05431facbd63369 from https://github.com/lowRISC/opentitan.
    > commit 84291ebf7c2a740ffdd4954de05431facbd63369
    > Author: Timothy Trippel <ttrippel@google.com>
    > Date:   Sat Nov 13 00:53:30 2021 +0000
    >
    >     [sw/ottf] Restructure meson build targets for OTTF.
    >
    >     For testing the OTTF, a single meson build target was constructed with
    >     an example test built in. This refactors the build targets to build the
    >     OTTF as a library that is linked with tests that are also defined as
    >     libraries. This makes building test binaries that use the OTTF similar
    >     to those that use the existing test_main.c test framework.
    >
    >     Additionally, two example OTTF tests (one bare-metal and one concurrency)
    >     are added to provided examples of how to write on-device tests using the
    >     OTTF.
    >
    >     This partially addresses a larger effort of refactoring the on-device
    >     test framework, as described in #8015.
    >
    >     Signed-off-by: Timothy Trippel <ttrippel@google.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,45 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8735"
-version_tuple = (0, 0, 8735)
+version_str = "0.0.post8738"
+version_tuple = (0, 0, 8738)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8735")
+    pversion = V("0.0.post8738")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8623"
-data_version_tuple = (0, 0, 8623)
+data_version_str = "0.0.post8626"
+data_version_tuple = (0, 0, 8626)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8623")
+    pdata_version = V("0.0.post8626")
 except ImportError:
     pass
-data_git_hash = "7e5b0deb1c24089adeed5396eca473f60449d45c"
-data_git_describe = "v0.0-8623-g7e5b0deb1"
+data_git_hash = "84291ebf7c2a740ffdd4954de05431facbd63369"
+data_git_describe = "v0.0-8626-g84291ebf7"
 data_git_msg = """\
-commit 7e5b0deb1c24089adeed5396eca473f60449d45c
-Author: Michael Schaffner <msf@opentitan.org>
-Date:   Tue Nov 16 10:19:32 2021 -0800
+commit 84291ebf7c2a740ffdd4954de05431facbd63369
+Author: Timothy Trippel <ttrippel@google.com>
+Date:   Sat Nov 13 00:53:30 2021 +0000
 
-    [doc] Minor dev stages cleanup
+    [sw/ottf] Restructure meson build targets for OTTF.
     
-    Signed-off-by: Michael Schaffner <msf@opentitan.org>
+    For testing the OTTF, a single meson build target was constructed with
+    an example test built in. This refactors the build targets to build the
+    OTTF as a library that is linked with tests that are also defined as
+    libraries. This makes building test binaries that use the OTTF similar
+    to those that use the existing test_main.c test framework.
+    
+    Additionally, two example OTTF tests (one bare-metal and one concurrency)
+    are added to provided examples of how to write on-device tests using the
+    OTTF.
+    
+    This partially addresses a larger effort of refactoring the on-device
+    test framework, as described in #8015.
+    
+    Signed-off-by: Timothy Trippel <ttrippel@google.com>
 
 """
 
