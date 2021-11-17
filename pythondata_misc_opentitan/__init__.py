7b78906638b19653f8c70f00047081a5f15b9f61
LiteX Robot <robot@timvideos.us>
2021-11-17 17:09:03 +0000

    Updating pythondata-misc-opentitan to 0.0.post8753
    Updated data to v0.0-8641-g6a2713ba3 based on 6a2713ba38efc0f0026858aa93076f53a8ab9bbc from https://github.com/lowRISC/opentitan.
    > commit 6a2713ba38efc0f0026858aa93076f53a8ab9bbc
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Tue Nov 16 15:06:34 2021 -0800
    >
    >     [dv/alert] Support LPG in alert_sender/receiver pair
    >
    >     This PR supports LPG in prim_alert testbench by randomly issue
    >     init_trigger_i in alert_receiver side when alert handshake is on-going.
    >     This PR implements part of the TODO in issue #8814.
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,49 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8751"
-version_tuple = (0, 0, 8751)
+version_str = "0.0.post8753"
+version_tuple = (0, 0, 8753)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8751")
+    pversion = V("0.0.post8753")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8639"
-data_version_tuple = (0, 0, 8639)
+data_version_str = "0.0.post8641"
+data_version_tuple = (0, 0, 8641)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8639")
+    pdata_version = V("0.0.post8641")
 except ImportError:
     pass
-data_git_hash = "0beb25531c1bbf75a269608f225c4c4a49b3caf8"
-data_git_describe = "v0.0-8639-g0beb25531"
+data_git_hash = "6a2713ba38efc0f0026858aa93076f53a8ab9bbc"
+data_git_describe = "v0.0-8641-g6a2713ba3"
 data_git_msg = """\
-commit 0beb25531c1bbf75a269608f225c4c4a49b3caf8
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Tue Nov 16 20:22:35 2021 +0000
+commit 6a2713ba38efc0f0026858aa93076f53a8ab9bbc
+Author: Cindy Chen <chencindy@opentitan.org>
+Date:   Tue Nov 16 15:06:34 2021 -0800
 
-    [otbn,rtl] Weaken assertion about imem_addr_o
+    [dv/alert] Support LPG in alert_sender/receiver pair
     
-    The core can sometimes send X on this signal, but it doesn't matter
-    because imem_req_o is false.
+    This PR supports LPG in prim_alert testbench by randomly issue
+    init_trigger_i in alert_receiver side when alert handshake is on-going.
+    This PR implements part of the TODO in issue #8814.
     
-    To see this happen, run:
-    
-        util/dvsim/dvsim.py hw/ip/otbn/dv/uvm/otbn_sim_cfg.hjson \
-            -i otbn_multi_err --fixed-seed 3982135834
-    
-    this is running the "jalr-0" test, which does a
-    
-        jalr x0, x1, 0
-    
-    with an empty callstack. I think the X's in the uninitialised call
-    stack are appearing as the next address to load. Of course, this
-    doesn't matter because we're also spotting the error and squashing the
-    request.
-    
-    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
 """
 
