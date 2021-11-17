897707c5e18c5cf80c8e6d54e0e94e99be6bcb14
LiteX Robot <robot@timvideos.us>
2021-11-17 02:20:02 +0000

    Updating pythondata-misc-opentitan to 0.0.post8746
    Updated data to v0.0-8634-ga601c7a6c based on a601c7a6ca50b8e53800ba89c3f13b17d3271db0 from https://github.com/lowRISC/opentitan.
    > commit a601c7a6ca50b8e53800ba89c3f13b17d3271db0
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Thu Nov 11 09:28:22 2021 -0800
    >
    >     [fpv] Fix compile warning regarding types
    >
    >     The output type of prim_mubi_sync is an array of mubis, so we change the
    >     output signal to an array of size 1.
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,53 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8744"
-version_tuple = (0, 0, 8744)
+version_str = "0.0.post8746"
+version_tuple = (0, 0, 8746)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8744")
+    pversion = V("0.0.post8746")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8632"
-data_version_tuple = (0, 0, 8632)
+data_version_str = "0.0.post8634"
+data_version_tuple = (0, 0, 8634)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8632")
+    pdata_version = V("0.0.post8634")
 except ImportError:
     pass
-data_git_hash = "e10dfaf312614ae9ebcdfb8c79840de4cf45be7e"
-data_git_describe = "v0.0-8632-ge10dfaf31"
+data_git_hash = "a601c7a6ca50b8e53800ba89c3f13b17d3271db0"
+data_git_describe = "v0.0-8634-ga601c7a6c"
 data_git_msg = """\
-commit e10dfaf312614ae9ebcdfb8c79840de4cf45be7e
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Tue Nov 9 17:28:56 2021 +0000
+commit a601c7a6ca50b8e53800ba89c3f13b17d3271db0
+Author: Cindy Chen <chencindy@opentitan.org>
+Date:   Thu Nov 11 09:28:22 2021 -0800
 
-    [otbn,dv] Teach otbn_model to pass validity bits to and from the ISS
+    [fpv] Fix compile warning regarding types
     
-    The binary file format has changed and now consists of 5 bytes for
-    each 32-bit word. The first byte is a "validity byte", which should
-    have value 0 (for invalid) or 1 (for valid). The other 4 bytes are a
-    little-endian encoded 32-bit word.
+    The output type of prim_mubi_sync is an array of mubis, so we change the
+    output signal to an array of size 1.
     
-    The first change in otbn_model is to use Ecc32MemArea's new
-    ReadWithIntegrity method to get the integrity bits as well as the raw
-    data. We then pass this to the ISS in the new format. In the other
-    direction (reading data from the ISS), we have to parse the new format
-    and also need to tweak our correctness check.
-    
-    Note that there's a temporary hack (with TODO message) here, which
-    will go away once we've implemented integrity bits properly on the ISS
-    side.
-    
-    On the ISS side, the only work is to read and write the new file
-    format (with struct format string "<BI") and fix up the various bits
-    of packing logic. There's a corresponding hack (also with TODO
-    message), reminding us to handle validity bits properly when loading
-    DMEM.
-    
-    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
 """
 
