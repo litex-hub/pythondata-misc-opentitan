d27c4b8e14e1df7c5792aa890243aa44fd7642f2
LiteX Robot <robot@timvideos.us>
2021-11-18 17:12:08 +0000

    Updating pythondata-misc-opentitan to 0.0.post8762
    Updated data to v0.0-8650-g0e0c1a99f based on 0e0c1a99f5a45584017d42bc8cba0aa376745908 from https://github.com/lowRISC/opentitan.
    > commit 0e0c1a99f5a45584017d42bc8cba0aa376745908
    > Author: Eunchan Kim <eunchan@opentitan.org>
    > Date:   Wed Nov 17 22:10:34 2021 +0000
    >
    >     [kmac] Use Empty signal from FIFO
    >
    >     The issue has been discussed in #9202
    >
    >     KMAC MsgFIFO sees the FIFO depth to calculate the empty status of SYNC
    >     FIFO. When the logic writes data and the receiver acked the read data at
    >     the same time and the FIFO has been empty already, the empty status
    >     won't be de-asserted as the `depth` wont be increased.
    >
    >     The issue is that SW will not get the fifo empty event due to the
    >     behavior described above if the receiver logic is faster than the SW.
    >
    >     This commit revises the msgfifo logic to see the `rvalid` as an
    >     inversion of the empty status. The scenario above will lower the `empty`
    >     signal with the fix.
    >
    >     Co-Authored-by: Cindy Chen <chencindy@opentitan.org>
    >     Signed-off-by: Eunchan Kim <eunchan@opentitan.org>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,47 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8761"
-version_tuple = (0, 0, 8761)
+version_str = "0.0.post8762"
+version_tuple = (0, 0, 8762)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8761")
+    pversion = V("0.0.post8762")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8649"
-data_version_tuple = (0, 0, 8649)
+data_version_str = "0.0.post8650"
+data_version_tuple = (0, 0, 8650)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8649")
+    pdata_version = V("0.0.post8650")
 except ImportError:
     pass
-data_git_hash = "085cef5b6b2b7dc2044cb4679623999871d561c3"
-data_git_describe = "v0.0-8649-g085cef5b6"
+data_git_hash = "0e0c1a99f5a45584017d42bc8cba0aa376745908"
+data_git_describe = "v0.0-8650-g0e0c1a99f"
 data_git_msg = """\
-commit 085cef5b6b2b7dc2044cb4679623999871d561c3
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Wed Nov 17 17:24:09 2021 +0000
+commit 0e0c1a99f5a45584017d42bc8cba0aa376745908
+Author: Eunchan Kim <eunchan@opentitan.org>
+Date:   Wed Nov 17 22:10:34 2021 +0000
 
-    [prim] Fix prim_packer_fifo when ClearOnRead is false
+    [kmac] Use Empty signal from FIFO
     
-    When we take the first part of an output word, we need to zero the
-    existing contents.
+    The issue has been discussed in #9202
     
-    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    KMAC MsgFIFO sees the FIFO depth to calculate the empty status of SYNC
+    FIFO. When the logic writes data and the receiver acked the read data at
+    the same time and the FIFO has been empty already, the empty status
+    won't be de-asserted as the `depth` wont be increased.
+    
+    The issue is that SW will not get the fifo empty event due to the
+    behavior described above if the receiver logic is faster than the SW.
+    
+    This commit revises the msgfifo logic to see the `rvalid` as an
+    inversion of the empty status. The scenario above will lower the `empty`
+    signal with the fix.
+    
+    Co-Authored-by: Cindy Chen <chencindy@opentitan.org>
+    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>
 
 """
 
