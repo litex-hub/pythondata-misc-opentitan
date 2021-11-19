571f4a415e0a2542c47cac0432da08bd61154703
LiteX Robot <robot@timvideos.us>
2021-11-19 22:02:30 +0000

    Updating pythondata-misc-opentitan to 0.0.post8786
    Updated data to v0.0-8674-ga5e3223e8 based on a5e3223e85d4715ab7949c069f9eab2c414ef53e from https://github.com/lowRISC/opentitan.
    > commit a5e3223e85d4715ab7949c069f9eab2c414ef53e
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Thu Nov 11 15:16:36 2021 -0800
    >
    >     [mubi] Enhance mubi_sync with stability check
    >
    >     - Stability check is not needed for most of the design and defaults
    >       to 0.
    >
    >     - With stability enabled, a 3rd stage is compared to the sychronized
    >       output.  If the output is the same, the 3rd stage results are used,
    >       if not, the ResetValue is used.
    >
    >     - This addresses some of the concerns raised with the original mubi_sync
    >       in #8848.
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,42 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8784"
-version_tuple = (0, 0, 8784)
+version_str = "0.0.post8786"
+version_tuple = (0, 0, 8786)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8784")
+    pversion = V("0.0.post8786")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8672"
-data_version_tuple = (0, 0, 8672)
+data_version_str = "0.0.post8674"
+data_version_tuple = (0, 0, 8674)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8672")
+    pdata_version = V("0.0.post8674")
 except ImportError:
     pass
-data_git_hash = "74d7e11098b51183c948a263fa8db6f7c489fe4c"
-data_git_describe = "v0.0-8672-g74d7e1109"
+data_git_hash = "a5e3223e85d4715ab7949c069f9eab2c414ef53e"
+data_git_describe = "v0.0-8674-ga5e3223e8"
 data_git_msg = """\
-commit 74d7e11098b51183c948a263fa8db6f7c489fe4c
-Author: Douglas Reis <doreis@lowrisc.org>
-Date:   Tue Nov 16 18:25:49 2021 +0000
+commit a5e3223e85d4715ab7949c069f9eab2c414ef53e
+Author: Timothy Chen <timothytim@google.com>
+Date:   Thu Nov 11 15:16:36 2021 -0800
 
-    [sw] Clean up the mtvec loading code
+    [mubi] Enhance mubi_sync with stability check
     
-    Signed-off-by: Douglas Reis <doreis@lowrisc.org>
+    - Stability check is not needed for most of the design and defaults
+      to 0.
+    
+    - With stability enabled, a 3rd stage is compared to the sychronized
+      output.  If the output is the same, the 3rd stage results are used,
+      if not, the ResetValue is used.
+    
+    - This addresses some of the concerns raised with the original mubi_sync
+      in #8848.
+    
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
