f3f7e5d66d1c01c988add5adca97606587896240
LiteX Robot <robot@timvideos.us>
2021-11-19 13:12:33 +0000

    Updating pythondata-misc-opentitan to 0.0.post8771
    Updated data to v0.0-8659-geecccf35a based on eecccf35aa9a8d94bc6acb4ebd876e08bffcdf86 from https://github.com/lowRISC/opentitan.
    > commit eecccf35aa9a8d94bc6acb4ebd876e08bffcdf86
    > Author: Greg Chadwick <gac@lowrisc.org>
    > Date:   Fri Nov 19 12:01:51 2021 +0000
    >
    >     Revert "[kmac] Use Empty signal from FIFO"
    >
    >     This reverts commit 0e0c1a99f5a45584017d42bc8cba0aa376745908.
    >
    >     The change appears to have broken the CW310 CI tests
    >
    >     Signed-off-by: Greg Chadwick <gac@lowrisc.org>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8770"
-version_tuple = (0, 0, 8770)
+version_str = "0.0.post8771"
+version_tuple = (0, 0, 8771)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8770")
+    pversion = V("0.0.post8771")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8658"
-data_version_tuple = (0, 0, 8658)
+data_version_str = "0.0.post8659"
+data_version_tuple = (0, 0, 8659)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8658")
+    pdata_version = V("0.0.post8659")
 except ImportError:
     pass
-data_git_hash = "2a78a93ac3226c2ba8343c863023f036eda8f179"
-data_git_describe = "v0.0-8658-g2a78a93ac"
+data_git_hash = "eecccf35aa9a8d94bc6acb4ebd876e08bffcdf86"
+data_git_describe = "v0.0-8659-geecccf35a"
 data_git_msg = """\
-commit 2a78a93ac3226c2ba8343c863023f036eda8f179
-Author: Weicai Yang <weicai@google.com>
-Date:   Thu Nov 18 15:59:59 2021 -0800
+commit eecccf35aa9a8d94bc6acb4ebd876e08bffcdf86
+Author: Greg Chadwick <gac@lowrisc.org>
+Date:   Fri Nov 19 12:01:51 2021 +0000
 
-    [dv] Fix intg_err test
+    Revert "[kmac] Use Empty signal from FIFO"
     
-    This addressed the regression failures in all the intg_err test which is
-    due to the update from #9243
+    This reverts commit 0e0c1a99f5a45584017d42bc8cba0aa376745908.
     
-    Signed-off-by: Weicai Yang <weicai@google.com>
+    The change appears to have broken the CW310 CI tests
+    
+    Signed-off-by: Greg Chadwick <gac@lowrisc.org>
 
 """
 
