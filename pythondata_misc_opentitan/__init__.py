d9f6fd4184149cdf588d1f93be8829826da09e0c
LiteX Robot <robot@timvideos.us>
2021-11-22 22:02:46 +0000

    Updating pythondata-misc-opentitan to 0.0.post8818
    Updated data to v0.0-8706-g686deb7fd based on 686deb7fd40e73798fd434dbbde05b02840e7c78 from https://github.com/lowRISC/opentitan.
    > commit 686deb7fd40e73798fd434dbbde05b02840e7c78
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Mon Nov 22 16:35:37 2021 +0000
    >
    >     [rom_ctrl] Update checklist to match current status
    >
    >     Everything is done for D2 except the planning/implementation of
    >     FI hardening and countermeasures.
    >
    >     The LINT_PASS item includes a review of lint waivers. There's exactly
    >     one such waiver, which silences AscentLint about a signal that's
    >     intentionally driven to a constant, one cycle after reset (the world's
    >     simplest FSM!)
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,40 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8813"
-version_tuple = (0, 0, 8813)
+version_str = "0.0.post8818"
+version_tuple = (0, 0, 8818)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8813")
+    pversion = V("0.0.post8818")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8701"
-data_version_tuple = (0, 0, 8701)
+data_version_str = "0.0.post8706"
+data_version_tuple = (0, 0, 8706)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8701")
+    pdata_version = V("0.0.post8706")
 except ImportError:
     pass
-data_git_hash = "bd51d1852e1da8ee8d3687be196bcb0bd3421b12"
-data_git_describe = "v0.0-8701-gbd51d1852"
+data_git_hash = "686deb7fd40e73798fd434dbbde05b02840e7c78"
+data_git_describe = "v0.0-8706-g686deb7fd"
 data_git_msg = """\
-commit bd51d1852e1da8ee8d3687be196bcb0bd3421b12
-Author: Timothy Trippel <ttrippel@google.com>
-Date:   Fri Nov 19 21:14:43 2021 +0000
+commit 686deb7fd40e73798fd434dbbde05b02840e7c78
+Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
+Date:   Mon Nov 22 16:35:37 2021 +0000
 
-    [sw/ottf] Fix typos in example OTTF test names.
+    [rom_ctrl] Update checklist to match current status
     
-    OTTF example test names accidentally had the word "lib" tacked on the
-    end.
+    Everything is done for D2 except the planning/implementation of
+    FI hardening and countermeasures.
     
-    Signed-off-by: Timothy Trippel <ttrippel@google.com>
+    The LINT_PASS item includes a review of lint waivers. There's exactly
+    one such waiver, which silences AscentLint about a signal that's
+    intentionally driven to a constant, one cycle after reset (the world's
+    simplest FSM!)
+    
+    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
 """
 
