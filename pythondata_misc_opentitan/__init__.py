feebd64080114814f7c47d08be0bb8dc5dc0db61
LiteX Robot <robot@timvideos.us>
2021-11-22 20:03:14 +0000

    Updating pythondata-misc-opentitan to 0.0.post8806
    Updated data to v0.0-8694-gd8cc3a18e based on d8cc3a18e6bdbe4ffe160bd92e0d750cf6853c61 from https://github.com/lowRISC/opentitan.
    > commit d8cc3a18e6bdbe4ffe160bd92e0d750cf6853c61
    > Author: Eitan Shapira <eitanshapira89@gmail.com>
    > Date:   Wed Nov 17 12:02:55 2021 +0200
    >
    >     [flash_ctrl/dv] Checking fix of bug #8934 and small addition to seq_cfg
    >
    >     Signed-off-by: Eitan Shapira <eitanshapira89@gmail.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8805"
-version_tuple = (0, 0, 8805)
+version_str = "0.0.post8806"
+version_tuple = (0, 0, 8806)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8805")
+    pversion = V("0.0.post8806")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8693"
-data_version_tuple = (0, 0, 8693)
+data_version_str = "0.0.post8694"
+data_version_tuple = (0, 0, 8694)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8693")
+    pdata_version = V("0.0.post8694")
 except ImportError:
     pass
-data_git_hash = "cc80879227cf914cb313599c380916113f812bfa"
-data_git_describe = "v0.0-8693-gcc8087922"
+data_git_hash = "d8cc3a18e6bdbe4ffe160bd92e0d750cf6853c61"
+data_git_describe = "v0.0-8694-gd8cc3a18e"
 data_git_msg = """\
-commit cc80879227cf914cb313599c380916113f812bfa
-Author: Jade Philipoom <jadep@google.com>
-Date:   Fri Nov 19 13:28:49 2021 +0000
+commit d8cc3a18e6bdbe4ffe160bd92e0d750cf6853c61
+Author: Eitan Shapira <eitanshapira89@gmail.com>
+Date:   Wed Nov 17 12:02:55 2021 +0200
 
-    [sw/otbn] Move crypto assembly files into new folder.
+    [flash_ctrl/dv] Checking fix of bug #8934 and small addition to seq_cfg
     
-    OTBN crypto assembly files are a bit of a different category than the
-    other small example programs in code-snippets; move these to their own,
-    new folder under sw/otbn/crypto.
-    
-    Signed-off-by: Jade Philipoom <jadep@google.com>
+    Signed-off-by: Eitan Shapira <eitanshapira89@gmail.com>
 
 """
 
