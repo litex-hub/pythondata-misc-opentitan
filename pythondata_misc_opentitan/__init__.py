8fefb37d25271bc11d4b4db151488d02f9dd7db6
LiteX Robot <robot@timvideos.us>
2021-11-24 10:02:18 +0000

    Updating pythondata-misc-opentitan to 0.0.post8853
    Updated data to v0.0-8741-ga6e6c8f1f based on a6e6c8f1f097e56bf682b6f9966596eba76d0718 from https://github.com/lowRISC/opentitan.
    > commit a6e6c8f1f097e56bf682b6f9966596eba76d0718
    > Author: Jade Philipoom <jadep@google.com>
    > Date:   Mon Nov 15 16:39:23 2021 +0000
    >
    >     [sw/otbn] Create a static check for OTBN loops.
    >
    >     Checks for common mistakes that can cause LOOP errors or loop stack
    >     issues.
    >
    >     Signed-off-by: Jade Philipoom <jadep@google.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,40 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8851"
-version_tuple = (0, 0, 8851)
+version_str = "0.0.post8853"
+version_tuple = (0, 0, 8853)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8851")
+    pversion = V("0.0.post8853")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8739"
-data_version_tuple = (0, 0, 8739)
+data_version_str = "0.0.post8741"
+data_version_tuple = (0, 0, 8741)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8739")
+    pdata_version = V("0.0.post8741")
 except ImportError:
     pass
-data_git_hash = "36cbccbefa66530bfb94d219b1749e3b31f8331d"
-data_git_describe = "v0.0-8739-g36cbccbef"
+data_git_hash = "a6e6c8f1f097e56bf682b6f9966596eba76d0718"
+data_git_describe = "v0.0-8741-ga6e6c8f1f"
 data_git_msg = """\
-commit 36cbccbefa66530bfb94d219b1749e3b31f8331d
-Author: Timothy Chen <timothytim@google.com>
-Date:   Tue Nov 23 13:35:12 2021 -0800
+commit a6e6c8f1f097e56bf682b6f9966596eba76d0718
+Author: Jade Philipoom <jadep@google.com>
+Date:   Mon Nov 15 16:39:23 2021 +0000
 
-    [top] Fix pinmux reset assignment
+    [sw/otbn] Create a static check for OTBN loops.
     
-    Pinmux was assigned the wrong root reset and as a result was
-    still held in reset with the strap signal was received.
+    Checks for common mistakes that can cause LOOP errors or loop stack
+    issues.
     
-    This means pinmux does not actually perform the strapping function
-    since all its logic is held in reset.
-    
-    Fixes #9365
-    
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Jade Philipoom <jadep@google.com>
 
 """
 
