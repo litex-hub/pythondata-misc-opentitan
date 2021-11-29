2f0534d09ef1b6a8d6ef5cd8ae8206877befee61
LiteX Robot <robot@timvideos.us>
2021-11-29 15:02:21 +0000

    Updating pythondata-misc-opentitan to 0.0.post8876
    Updated data to v0.0-8764-gcc4e9fa2b based on cc4e9fa2b1ed3384df7152b2698ba029452b734b from https://github.com/lowRISC/opentitan.
    > commit cc4e9fa2b1ed3384df7152b2698ba029452b734b
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Fri Oct 22 16:41:51 2021 +0100
    >
    >     [otbn,python] Wrap code to 79 columns
    >
    >     Our Python style guide claims that we wrap code at 79 columns but our
    >     flake8 configuration is set to allow 100 (presumably because it's too
    >     much work to squeeze existing code into shape!).
    >
    >     I wrote the OTBN Python code intending it to stay at 79. Put in a
    >     local .flake8 file to set the expected width correctly and then (lots
    >     of lines...) re-format comments and break strings to get everything
    >     sorted.
    >
    >     This stricter formatting won't be checked by CI (which runs at
    >     top-level, so won't see the new .flake8 config file) but it *will* be
    >     checked by people's text editors if they've got the usual Python
    >     setup.
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,39 +4,46 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8875"
-version_tuple = (0, 0, 8875)
+version_str = "0.0.post8876"
+version_tuple = (0, 0, 8876)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8875")
+    pversion = V("0.0.post8876")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8763"
-data_version_tuple = (0, 0, 8763)
+data_version_str = "0.0.post8764"
+data_version_tuple = (0, 0, 8764)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8763")
+    pdata_version = V("0.0.post8764")
 except ImportError:
     pass
-data_git_hash = "ffb02b25462524ee90a9587844c4610e7e4ec32a"
-data_git_describe = "v0.0-8763-gffb02b254"
+data_git_hash = "cc4e9fa2b1ed3384df7152b2698ba029452b734b"
+data_git_describe = "v0.0-8764-gcc4e9fa2b"
 data_git_msg = """\
-commit ffb02b25462524ee90a9587844c4610e7e4ec32a
-Author: Pirmin Vogel <vogelpi@lowrisc.org>
-Date:   Thu Nov 25 14:00:30 2021 +0100
+commit cc4e9fa2b1ed3384df7152b2698ba029452b734b
+Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
+Date:   Fri Oct 22 16:41:51 2021 +0100
 
-    [aes, pre_sca] Enable formal verification of more than a single S-Box
+    [otbn,python] Wrap code to 79 columns
     
-    This commit enables the formal verification of either the full SubBytes,
-    i.e., the 16 S-Boxes in the data path, and even a reduced AES round
-    containing also ShiftRows and MixColumns. This is useful as formal
-    verification of the entire AES cipher core is currently out of scope
-    from a tool run time point of view whereas SCA leakage outside the
-    reduced AES round is unlikely.
+    Our Python style guide claims that we wrap code at 79 columns but our
+    flake8 configuration is set to allow 100 (presumably because it's too
+    much work to squeeze existing code into shape!).
     
-    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
+    I wrote the OTBN Python code intending it to stay at 79. Put in a
+    local .flake8 file to set the expected width correctly and then (lots
+    of lines...) re-format comments and break strings to get everything
+    sorted.
+    
+    This stricter formatting won't be checked by CI (which runs at
+    top-level, so won't see the new .flake8 config file) but it *will* be
+    checked by people's text editors if they've got the usual Python
+    setup.
+    
+    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
 """
 
