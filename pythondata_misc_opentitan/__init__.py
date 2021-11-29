81818cf0e56d16e4bbd18d6f904ba52bd6c45f20
LiteX Robot <robot@timvideos.us>
2021-11-29 20:02:25 +0000

    Updating pythondata-misc-opentitan to 0.0.post8878
    Updated data to v0.0-8766-g3bd6cebda based on 3bd6cebda2598e080aae62a3a81e36a60d7ee932 from https://github.com/lowRISC/opentitan.
    > commit 3bd6cebda2598e080aae62a3a81e36a60d7ee932
    > Author: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
    > Date:   Fri Oct 22 09:43:26 2021 -0700
    >
    >     [ spi_host, doc ] Update documentation to include deltas from recent PRs
    >
    >     - Removes all discussion of the core clock domain
    >     - Eliminates all discussion of CDC's
    >       - One exception remains the description on "Back-to-Back Commands".
    >         This section will be updated in a forthcoming PR.
    >     - New class of error event associated with various "impossible" TX FIFO write
    >       events (i.e., zero-byte writes, or other writes not associated with any known
    >       processor data type.)
    >     - General cleaned up discussion of error events.
    >     - Documents the split of the DATA memory window into separate TXDATA/RXDATA windows
    >     - Implementation section: documents the split of the FSM `cmd_end` signal
    >       into `last_read` and `last_write` signals
    >
    >     Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,37 +4,44 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8877"
-version_tuple = (0, 0, 8877)
+version_str = "0.0.post8878"
+version_tuple = (0, 0, 8878)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8877")
+    pversion = V("0.0.post8878")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8765"
-data_version_tuple = (0, 0, 8765)
+data_version_str = "0.0.post8766"
+data_version_tuple = (0, 0, 8766)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8765")
+    pdata_version = V("0.0.post8766")
 except ImportError:
     pass
-data_git_hash = "028e3fbb9d3120943810c35e136905cc31077c6a"
-data_git_describe = "v0.0-8765-g028e3fbb9"
+data_git_hash = "3bd6cebda2598e080aae62a3a81e36a60d7ee932"
+data_git_describe = "v0.0-8766-g3bd6cebda"
 data_git_msg = """\
-commit 028e3fbb9d3120943810c35e136905cc31077c6a
-Author: Eunchan Kim <eunchan@opentitan.org>
-Date:   Tue Nov 23 18:36:24 2021 +0000
+commit 3bd6cebda2598e080aae62a3a81e36a60d7ee932
+Author: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
+Date:   Fri Oct 22 09:43:26 2021 -0700
 
-    [spi_device] Add prefix to the interrupts
+    [ spi_host, doc ] Update documentation to include deltas from recent PRs
     
-    This commit adds mode or distinguishable prefixes to the existing
-    interrupts. `generic_` prefix is added to pre-existed interrupts.
-    `upload_` and `readbuf_` prefixes are added to the flash/passthrough
-    related interrupts.
+    - Removes all discussion of the core clock domain
+    - Eliminates all discussion of CDC's
+      - One exception remains the description on "Back-to-Back Commands".
+        This section will be updated in a forthcoming PR.
+    - New class of error event associated with various "impossible" TX FIFO write
+      events (i.e., zero-byte writes, or other writes not associated with any known
+      processor data type.)
+    - General cleaned up discussion of error events.
+    - Documents the split of the DATA memory window into separate TXDATA/RXDATA windows
+    - Implementation section: documents the split of the FSM `cmd_end` signal
+      into `last_read` and `last_write` signals
     
-    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>
+    Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
 
 """
 
