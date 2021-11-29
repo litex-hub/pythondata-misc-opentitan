51670b2b93f15ce1571a27906cf9c247d7ec0cf3
LiteX Robot <robot@timvideos.us>
2021-11-29 14:02:20 +0000

    Updating pythondata-misc-opentitan to 0.0.post8875
    Updated data to v0.0-8763-gffb02b254 based on ffb02b25462524ee90a9587844c4610e7e4ec32a from https://github.com/lowRISC/opentitan.
    > commit ffb02b25462524ee90a9587844c4610e7e4ec32a
    > Author: Pirmin Vogel <vogelpi@lowrisc.org>
    > Date:   Thu Nov 25 14:00:30 2021 +0100
    >
    >     [aes, pre_sca] Enable formal verification of more than a single S-Box
    >
    >     This commit enables the formal verification of either the full SubBytes,
    >     i.e., the 16 S-Boxes in the data path, and even a reduced AES round
    >     containing also ShiftRows and MixColumns. This is useful as formal
    >     verification of the entire AES cipher core is currently out of scope
    >     from a tool run time point of view whereas SCA leakage outside the
    >     reduced AES round is unlikely.
    >
    >     Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,37 +4,39 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8870"
-version_tuple = (0, 0, 8870)
+version_str = "0.0.post8875"
+version_tuple = (0, 0, 8875)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8870")
+    pversion = V("0.0.post8875")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8758"
-data_version_tuple = (0, 0, 8758)
+data_version_str = "0.0.post8763"
+data_version_tuple = (0, 0, 8763)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8758")
+    pdata_version = V("0.0.post8763")
 except ImportError:
     pass
-data_git_hash = "3a672eb36aee5942d0912a15d15055b1d21c33d6"
-data_git_describe = "v0.0-8758-g3a672eb36"
+data_git_hash = "ffb02b25462524ee90a9587844c4610e7e4ec32a"
+data_git_describe = "v0.0-8763-gffb02b254"
 data_git_msg = """\
-commit 3a672eb36aee5942d0912a15d15055b1d21c33d6
-Author: Canberk Topal <ctopal@lowrisc.org>
-Date:   Fri Nov 26 14:17:14 2021 +0000
+commit ffb02b25462524ee90a9587844c4610e7e4ec32a
+Author: Pirmin Vogel <vogelpi@lowrisc.org>
+Date:   Thu Nov 25 14:00:30 2021 +0100
 
-    [otbn,dv] Bug-fix: First insn uses faulty URND
+    [aes, pre_sca] Enable formal verification of more than a single S-Box
     
-    The bug was happening when the first executed
-    instruction tried to use URND. Since first step
-    did not resulted with a change in _value it was
-    generating a None assingment as a value.
+    This commit enables the formal verification of either the full SubBytes,
+    i.e., the 16 S-Boxes in the data path, and even a reduced AES round
+    containing also ShiftRows and MixColumns. This is useful as formal
+    verification of the entire AES cipher core is currently out of scope
+    from a tool run time point of view whereas SCA leakage outside the
+    reduced AES round is unlikely.
     
-    Signed-off-by: Canberk Topal <ctopal@lowrisc.org>
+    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
 
 """
 
