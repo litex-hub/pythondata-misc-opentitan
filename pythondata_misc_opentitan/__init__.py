5ac03c349e1011eab328c098fd9d8feb96b4822a
LiteX Robot <robot@timvideos.us>
2021-11-30 22:02:15 +0000

    Updating pythondata-misc-opentitan to 0.0.post8911
    Updated data to v0.0-8799-ga847073fb based on a847073fb8c333cbf1c240d25794517f1562c0af from https://github.com/lowRISC/opentitan.
    > commit a847073fb8c333cbf1c240d25794517f1562c0af
    > Author: Timothy Trippel <ttrippel@google.com>
    > Date:   Mon Nov 22 19:42:42 2021 +0000
    >
    >     [sw/ottf] Decouple OTTF ISRs into separate lib.
    >
    >     Previously, all default ISRs were contained in the single OTTF build
    >     target. This made using the default OTTF ISRs in a standalone bare metal
    >     test application that does not make use of the OTTF impossible.
    >
    >     This commit isolates the OTTF default ISRs into a separate library, so
    >     they can be used similar to the existing `sw/device/lib/handler.<c,h>`
    >     ISRs, since those will be deprecated in the near future when all
    >     chip-level tests migrate to the OTTF.
    >
    >     Signed-off-by: Timothy Trippel <ttrippel@google.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,41 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8909"
-version_tuple = (0, 0, 8909)
+version_str = "0.0.post8911"
+version_tuple = (0, 0, 8911)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8909")
+    pversion = V("0.0.post8911")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8797"
-data_version_tuple = (0, 0, 8797)
+data_version_str = "0.0.post8799"
+data_version_tuple = (0, 0, 8799)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8797")
+    pdata_version = V("0.0.post8799")
 except ImportError:
     pass
-data_git_hash = "ba63d1e6ad6702cfb661f8fd8233d9bfb5f692ea"
-data_git_describe = "v0.0-8797-gba63d1e6a"
+data_git_hash = "a847073fb8c333cbf1c240d25794517f1562c0af"
+data_git_describe = "v0.0-8799-ga847073fb"
 data_git_msg = """\
-commit ba63d1e6ad6702cfb661f8fd8233d9bfb5f692ea
-Author: kosta-kojdic <kosta.kojdic@ensilica.com>
-Date:   Tue Nov 30 13:08:00 2021 +0000
+commit a847073fb8c333cbf1c240d25794517f1562c0af
+Author: Timothy Trippel <ttrippel@google.com>
+Date:   Mon Nov 22 19:42:42 2021 +0000
 
-    CSB Selection Implemented
+    [sw/ottf] Decouple OTTF ISRs into separate lib.
     
-    Signed-off-by: kosta-kojdic <kosta.kojdic@ensilica.com>
+    Previously, all default ISRs were contained in the single OTTF build
+    target. This made using the default OTTF ISRs in a standalone bare metal
+    test application that does not make use of the OTTF impossible.
+    
+    This commit isolates the OTTF default ISRs into a separate library, so
+    they can be used similar to the existing `sw/device/lib/handler.<c,h>`
+    ISRs, since those will be deprecated in the near future when all
+    chip-level tests migrate to the OTTF.
+    
+    Signed-off-by: Timothy Trippel <ttrippel@google.com>
 
 """
 
