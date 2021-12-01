39e11fa57c18a809a2448a58749d402130eb597a
LiteX Robot <robot@timvideos.us>
2021-12-01 23:02:20 +0000

    Updating pythondata-misc-opentitan to 0.0.post8940
    Updated data to v0.0-8828-g0fd56dd56 based on 0fd56dd5658005d576a0440a268de6028818244e from https://github.com/lowRISC/opentitan.
    > commit 0fd56dd5658005d576a0440a268de6028818244e
    > Author: Michael Schaffner <msf@google.com>
    > Date:   Tue Nov 30 12:54:12 2021 -0800
    >
    >     [lc_ctrl] Correct invalid state error indication
    >
    >     Fixes #9431
    >
    >     Signed-off-by: Michael Schaffner <msf@google.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8939"
-version_tuple = (0, 0, 8939)
+version_str = "0.0.post8940"
+version_tuple = (0, 0, 8940)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8939")
+    pversion = V("0.0.post8940")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8827"
-data_version_tuple = (0, 0, 8827)
+data_version_str = "0.0.post8828"
+data_version_tuple = (0, 0, 8828)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8827")
+    pdata_version = V("0.0.post8828")
 except ImportError:
     pass
-data_git_hash = "62ecce1a869eba33d65b112bab37258328adda55"
-data_git_describe = "v0.0-8827-g62ecce1a8"
+data_git_hash = "0fd56dd5658005d576a0440a268de6028818244e"
+data_git_describe = "v0.0-8828-g0fd56dd56"
 data_git_msg = """\
-commit 62ecce1a869eba33d65b112bab37258328adda55
-Author: Eunchan Kim <eunchan@opentitan.org>
-Date:   Tue Nov 23 16:54:36 2021 +0000
+commit 0fd56dd5658005d576a0440a268de6028818244e
+Author: Michael Schaffner <msf@google.com>
+Date:   Tue Nov 30 12:54:12 2021 -0800
 
-    [spi_device] Connect Readbuf Flip interrupt
+    [lc_ctrl] Correct invalid state error indication
     
-    This commit connects the readbuf flip event into the interrupt line.
-    The flip event may be high for multiple SCK cycles. The logic converts
-    the signal into single cycle pulse to use prim_pulse_sync.
+    Fixes #9431
     
-    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>
+    Signed-off-by: Michael Schaffner <msf@google.com>
 
 """
 
