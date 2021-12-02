b324ca612e35140535ca1b2de98ab8a4a32e5eb1
LiteX Robot <robot@timvideos.us>
2021-12-02 05:03:05 +0000

    Updating pythondata-misc-opentitan to 0.0.post8949
    Updated data to v0.0-8837-g9aeaac797 based on 9aeaac797a34043b2a04424f3128d3dc02e75c4c from https://github.com/lowRISC/opentitan.
    > commit 9aeaac797a34043b2a04424f3128d3dc02e75c4c
    > Author: Michael Schaffner <msf@opentitan.org>
    > Date:   Wed Dec 1 16:58:58 2021 -0800
    >
    >     [alert_handler] Minor lint fix
    >
    >     Signed-off-by: Michael Schaffner <msf@opentitan.org>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8947"
-version_tuple = (0, 0, 8947)
+version_str = "0.0.post8949"
+version_tuple = (0, 0, 8949)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8947")
+    pversion = V("0.0.post8949")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8835"
-data_version_tuple = (0, 0, 8835)
+data_version_str = "0.0.post8837"
+data_version_tuple = (0, 0, 8837)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8835")
+    pdata_version = V("0.0.post8837")
 except ImportError:
     pass
-data_git_hash = "1d43766387b8fd976c0d6d665298b885ccb1b68c"
-data_git_describe = "v0.0-8835-g1d4376638"
+data_git_hash = "9aeaac797a34043b2a04424f3128d3dc02e75c4c"
+data_git_describe = "v0.0-8837-g9aeaac797"
 data_git_msg = """\
-commit 1d43766387b8fd976c0d6d665298b885ccb1b68c
-Author: Nikola Miladinovic <nikola.miladinovic@ensilica.com>
-Date:   Wed Nov 3 12:38:35 2021 +0000
+commit 9aeaac797a34043b2a04424f3128d3dc02e75c4c
+Author: Michael Schaffner <msf@opentitan.org>
+Date:   Wed Dec 1 16:58:58 2021 -0800
 
-    [flash_ctrl] Modification of testplan according to comments in pull request
+    [alert_handler] Minor lint fix
     
-    Signed-off-by: Nikola Miladinovic <nikola.miladinovic@ensilica.com>
+    Signed-off-by: Michael Schaffner <msf@opentitan.org>
 
 """
 
