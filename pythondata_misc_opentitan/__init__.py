3d40871427d19350e29de2cb1e67fa60b2a75c79
LiteX Robot <robot@timvideos.us>
2021-12-02 11:02:39 +0000

    Updating pythondata-misc-opentitan to 0.0.post8952
    Updated data to v0.0-8840-gc6213a373 based on c6213a373d5da9d798903560e94f43f937b4e36f from https://github.com/lowRISC/opentitan.
    > commit c6213a373d5da9d798903560e94f43f937b4e36f
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Wed Dec 1 15:01:30 2021 +0000
    >
    >     [rom_ctrl] Check TL command integrity in ROM adapter
    >
    >     This fixes the rom_ctrl_tl_intg_err test (where we weren't previously
    >     spotting malformed TL commands sent to the ROM).
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,53 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8950"
-version_tuple = (0, 0, 8950)
+version_str = "0.0.post8952"
+version_tuple = (0, 0, 8952)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8950")
+    pversion = V("0.0.post8952")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8838"
-data_version_tuple = (0, 0, 8838)
+data_version_str = "0.0.post8840"
+data_version_tuple = (0, 0, 8840)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8838")
+    pdata_version = V("0.0.post8840")
 except ImportError:
     pass
-data_git_hash = "15daa83ee2abb7abecc0140bcbe043a013111dfd"
-data_git_describe = "v0.0-8838-g15daa83ee"
+data_git_hash = "c6213a373d5da9d798903560e94f43f937b4e36f"
+data_git_describe = "v0.0-8840-gc6213a373"
 data_git_msg = """\
-commit 15daa83ee2abb7abecc0140bcbe043a013111dfd
-Author: Timothy Trippel <ttrippel@google.com>
-Date:   Tue Nov 30 00:38:11 2021 +0000
+commit c6213a373d5da9d798903560e94f43f937b4e36f
+Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
+Date:   Wed Dec 1 15:01:30 2021 +0000
 
-    [sw/ottf] Fix bugs saving/restoring stack pointer in ISRs.
+    [rom_ctrl] Check TL command integrity in ROM adapter
     
-    The OTTF was optimized in #9158 to allow running bare-metal tests (in
-    place of running each test as a FreeRTOS "task") if the test did NOT
-    require spawning (additional) concurrenct tasks.
+    This fixes the rom_ctrl_tl_intg_err test (where we weren't previously
+    spotting malformed TL commands sent to the ROM).
     
-    This introduced a bug in the ISR assembly that saves/restores the current
-    execution context to the stack upon entry/exit from the ISR. For tests run
-    as a FreeRTOS task, i.e., tests that set the `enable_concurrency` flag in
-    the test configuration struct (see `test_config_t` in
-    `sw/device/lib/testing/test_framework/ottf.h`), the current stack pointer must
-    be saved/restored from the current Task Control Block (TCB) because each
-    FreeRTOS task maintains a separate stack and a context switch could
-    happen in an exception handler (task yield) or ISR (preemption).
-    
-    However, for bare-metal tests, the current TCB pointer (which is initialized
-    and maintained by FreeRTOS) is NULL. Therefore attempting to dereference
-    said pointer results in a store access fault.
-    
-    This commit fixes this bug by checking the test mode (bare-metal vs.
-    concurrency) in the ISR entry/exit assembly to store/load the proper
-    execution context state.
-    
-    Signed-off-by: Timothy Trippel <ttrippel@google.com>
+    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
 """
 
