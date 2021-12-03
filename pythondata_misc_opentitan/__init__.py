538927c18609366537b4c2157b39a58b60b4b650
LiteX Robot <robot@timvideos.us>
2021-12-03 16:03:12 +0000

    Updating pythondata-misc-opentitan to 0.0.post8978
    Updated data to v0.0-8866-g76e0cfa48 based on 76e0cfa48f9a7e63093a949e19c20ed4baca805f from https://github.com/lowRISC/opentitan.
    > commit 76e0cfa48f9a7e63093a949e19c20ed4baca805f
    > Author: Michael Schaffner <msf@google.com>
    > Date:   Thu Dec 2 15:28:25 2021 -0800
    >
    >     [syn] Fix SDC constraint
    >
    >     Signed-off-by: Michael Schaffner <msf@google.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8976"
-version_tuple = (0, 0, 8976)
+version_str = "0.0.post8978"
+version_tuple = (0, 0, 8978)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8976")
+    pversion = V("0.0.post8978")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8864"
-data_version_tuple = (0, 0, 8864)
+data_version_str = "0.0.post8866"
+data_version_tuple = (0, 0, 8866)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8864")
+    pdata_version = V("0.0.post8866")
 except ImportError:
     pass
-data_git_hash = "4e9df5489bae93f95969dd6f6859ddad8e28d1e3"
-data_git_describe = "v0.0-8864-g4e9df5489"
+data_git_hash = "76e0cfa48f9a7e63093a949e19c20ed4baca805f"
+data_git_describe = "v0.0-8866-g76e0cfa48"
 data_git_msg = """\
-commit 4e9df5489bae93f95969dd6f6859ddad8e28d1e3
-Author: Michael Schaffner <msf@opentitan.org>
-Date:   Thu Dec 2 16:47:20 2021 -0800
+commit 76e0cfa48f9a7e63093a949e19c20ed4baca805f
+Author: Michael Schaffner <msf@google.com>
+Date:   Thu Dec 2 15:28:25 2021 -0800
 
-    [reggen] Add tentative list of assets and cm types
+    [syn] Fix SDC constraint
     
-    Signed-off-by: Michael Schaffner <msf@opentitan.org>
+    Signed-off-by: Michael Schaffner <msf@google.com>
 
 """
 
