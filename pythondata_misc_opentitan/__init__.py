6745e6eddce082fcc40775607208dbcdef4319fe
LiteX Robot <robot@timvideos.us>
2021-12-03 21:02:07 +0000

    Updating pythondata-misc-opentitan to 0.0.post8990
    Updated data to v0.0-8878-gd9d2faa7b based on d9d2faa7b33f8bb45931778792d930b13f421f9d from https://github.com/lowRISC/opentitan.
    > commit d9d2faa7b33f8bb45931778792d930b13f421f9d
    > Author: Michael Schaffner <msf@google.com>
    > Date:   Wed Nov 3 13:18:04 2021 -0700
    >
    >     [syn/cdc] Tune SDC file to fix all SDC warnings/errors in CDC
    >
    >     This introduces some more flexibility to the SDC file so that we can
    >     reuse it for open-source-only CDC runs where the hookup paths have
    >     different hierarchical names.
    >
    >     Also, there are a few constraints that CDC does not understand, and we
    >     introduce a variable to exclude them conditionally for CDC flows.
    >
    >     Signed-off-by: Michael Schaffner <msf@google.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,39 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post8987"
-version_tuple = (0, 0, 8987)
+version_str = "0.0.post8990"
+version_tuple = (0, 0, 8990)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post8987")
+    pversion = V("0.0.post8990")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8875"
-data_version_tuple = (0, 0, 8875)
+data_version_str = "0.0.post8878"
+data_version_tuple = (0, 0, 8878)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8875")
+    pdata_version = V("0.0.post8878")
 except ImportError:
     pass
-data_git_hash = "3fdc7395d36a3c3b07614b5ea6e1a54794b4952e"
-data_git_describe = "v0.0-8875-g3fdc7395d"
+data_git_hash = "d9d2faa7b33f8bb45931778792d930b13f421f9d"
+data_git_describe = "v0.0-8878-gd9d2faa7b"
 data_git_msg = """\
-commit 3fdc7395d36a3c3b07614b5ea6e1a54794b4952e
-Author: Mark Branstad <mark.branstad@wdc.com>
-Date:   Fri Dec 3 06:46:24 2021 -0800
+commit d9d2faa7b33f8bb45931778792d930b13f421f9d
+Author: Michael Schaffner <msf@google.com>
+Date:   Wed Nov 3 13:18:04 2021 -0700
 
-    [entropy_src/doc] uninstantiate command update
+    [syn/cdc] Tune SDC file to fix all SDC warnings/errors in CDC
     
-    The documentation has been updated to restrict the clen of an uninstantiate command to be zero.
+    This introduces some more flexibility to the SDC file so that we can
+    reuse it for open-source-only CDC runs where the hookup paths have
+    different hierarchical names.
     
-    Signed-off-by: Mark Branstad <mark.branstad@wdc.com>
+    Also, there are a few constraints that CDC does not understand, and we
+    introduce a variable to exclude them conditionally for CDC flows.
+    
+    Signed-off-by: Michael Schaffner <msf@google.com>
 
 """
 
