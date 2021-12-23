4066b854f1de6e6981b1b94021b590e772a52673
LiteX Robot <robot@timvideos.us>
2021-12-23 22:04:09 +0000

    Updating pythondata-misc-opentitan to 0.0.post9283
    Updated data to v0.0-9170-ga753200da based on a753200da1c33268c54c4dc3d3d2bec23d3349bc from https://github.com/lowRISC/opentitan.
    > commit a753200da1c33268c54c4dc3d3d2bec23d3349bc
    > Author: Steve Nelson <steve.nelson@wdc.com>
    > Date:   Wed Dec 22 13:50:35 2021 -0800
    >
    >     [csrng/dv] Removed double cmd_rdy spin_waits. rtl fix in PR#9802
    >
    >     Signed-off-by: Steve Nelson <steve.nelson@wdc.com>
    >
    
    Updated using 0.0.post113 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,49 +4,41 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9007"
-version_tuple = (0, 0, 9007)
+version_str = "0.0.post9283"
+version_tuple = (0, 0, 9283)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9007")
+    pversion = V("0.0.post9283")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post8895"
-data_version_tuple = (0, 0, 8895)
+data_version_str = "0.0.post9170"
+data_version_tuple = (0, 0, 9170)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post8895")
+    pdata_version = V("0.0.post9170")
 except ImportError:
     pass
-data_git_hash = "09912c369722c240313288cbb8fff07be237652b"
-data_git_describe = "v0.0-8895-g09912c369"
+data_git_hash = "a753200da1c33268c54c4dc3d3d2bec23d3349bc"
+data_git_describe = "v0.0-9170-ga753200da"
 data_git_msg = """\
-commit 09912c369722c240313288cbb8fff07be237652b
-Author: Pirmin Vogel <vogelpi@lowrisc.org>
-Date:   Tue Nov 16 22:14:19 2021 +0100
+commit a753200da1c33268c54c4dc3d3d2bec23d3349bc
+Author: Steve Nelson <steve.nelson@wdc.com>
+Date:   Wed Dec 22 13:50:35 2021 -0800
 
-    [es/csrng/edn] Don't clear packer FIFOs upon read
+    [csrng/dv] Removed double cmd_rdy spin_waits. rtl fix in PR#9802
     
-    Previously, all packer FIFOs in the entropy complex where by default
-    cleared to zero upon read. However, as this may simplify attacks trying
-    to manipulate the random number distribution it should better be
-    avoided. Leaving around the previous random data on the bus/wires after
-    the read is preferred over outputting a deterministic value in this
-    case.
-    
-    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
-    Signed-off-by: Michael Schaffner <msf@opentitan.org>
+    Signed-off-by: Steve Nelson <steve.nelson@wdc.com>
 
 """
 
 # Tool version info
-tool_version_str = "0.0.post112"
-tool_version_tuple = (0, 0, 112)
+tool_version_str = "0.0.post113"
+tool_version_tuple = (0, 0, 113)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post112")
+    ptool_version = V("0.0.post113")
 except ImportError:
     pass
 
