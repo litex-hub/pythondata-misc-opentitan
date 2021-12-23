8acdba2dc782a04a6e5b2bb84c32bece4bc4279c
LiteX Robot <robot@timvideos.us>
2021-12-23 23:23:53 +0000

    Updating pythondata-misc-opentitan to 0.0.post9284
    Updated data to v0.0-9171-g5c710272c based on 5c710272c119a859e495b517e8833aea95eadd5f from https://github.com/lowRISC/opentitan.
    > commit 5c710272c119a859e495b517e8833aea95eadd5f
    > Author: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
    > Date:   Fri Dec 17 16:52:42 2021 -0800
    >
    >     [ entropy_src, dv] Scoreboarding for Health Checks
    >
    >     (Note: Some checks disabled in scoreboard due to bugs in DUT)
    >
    >     - Health check scoreboarding is now functional
    >       - Monitors input RNG, and applies health checks independently
    >       - Creates predictions for watermarks, failures, and alerts
    >       - Applies thresholds based on live register settings
    >         - Handles both FIPS and BYPASS settings (still tesing BYPASS only, in smoke test)
    >       - Base vseq now checks all watermark values at end of sim.
    >       - Thresholds currently set to maximum tolerance, will test thresholds in a future PR
    >     - Some differences in IP discovered
    >       - Watermark registers are are not matching for the following tests: repcnt, repcnts
    >
    >     Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
    >
    
    Updated using 0.0.post113 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,44 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9283"
-version_tuple = (0, 0, 9283)
+version_str = "0.0.post9284"
+version_tuple = (0, 0, 9284)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9283")
+    pversion = V("0.0.post9284")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9170"
-data_version_tuple = (0, 0, 9170)
+data_version_str = "0.0.post9171"
+data_version_tuple = (0, 0, 9171)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9170")
+    pdata_version = V("0.0.post9171")
 except ImportError:
     pass
-data_git_hash = "a753200da1c33268c54c4dc3d3d2bec23d3349bc"
-data_git_describe = "v0.0-9170-ga753200da"
+data_git_hash = "5c710272c119a859e495b517e8833aea95eadd5f"
+data_git_describe = "v0.0-9171-g5c710272c"
 data_git_msg = """\
-commit a753200da1c33268c54c4dc3d3d2bec23d3349bc
-Author: Steve Nelson <steve.nelson@wdc.com>
-Date:   Wed Dec 22 13:50:35 2021 -0800
+commit 5c710272c119a859e495b517e8833aea95eadd5f
+Author: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
+Date:   Fri Dec 17 16:52:42 2021 -0800
 
-    [csrng/dv] Removed double cmd_rdy spin_waits. rtl fix in PR#9802
+    [ entropy_src, dv] Scoreboarding for Health Checks
     
-    Signed-off-by: Steve Nelson <steve.nelson@wdc.com>
+    (Note: Some checks disabled in scoreboard due to bugs in DUT)
+    
+    - Health check scoreboarding is now functional
+      - Monitors input RNG, and applies health checks independently
+      - Creates predictions for watermarks, failures, and alerts
+      - Applies thresholds based on live register settings
+        - Handles both FIPS and BYPASS settings (still tesing BYPASS only, in smoke test)
+      - Base vseq now checks all watermark values at end of sim.
+      - Thresholds currently set to maximum tolerance, will test thresholds in a future PR
+    - Some differences in IP discovered
+      - Watermark registers are are not matching for the following tests: repcnt, repcnts
+    
+    Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
 
 """
 
