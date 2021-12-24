60f120c82ddef065454a83071fe7cec5d990b1de
LiteX Robot <robot@timvideos.us>
2021-12-24 01:14:48 +0000

    Updating pythondata-misc-opentitan to 0.0.post9290
    Updated data to v0.0-9173-g854388d46 based on 854388d4644276f097acdacb1f4ed356b2d04ce2 from https://github.com/lowRISC/opentitan.
    > commit 854388d4644276f097acdacb1f4ed356b2d04ce2
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Thu Dec 23 13:58:25 2021 -0800
    >
    >     [fpv/pinmux] Update pinmux existing assertions
    >
    >     This PR updates the currect assertions to consider two more cases: sleep
    >     and jtag.
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post117 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9289"
-version_tuple = (0, 0, 9289)
+version_str = "0.0.post9290"
+version_tuple = (0, 0, 9290)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9289")
+    pversion = V("0.0.post9290")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9172"
-data_version_tuple = (0, 0, 9172)
+data_version_str = "0.0.post9173"
+data_version_tuple = (0, 0, 9173)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9172")
+    pdata_version = V("0.0.post9173")
 except ImportError:
     pass
-data_git_hash = "94308f5e5e4bc44b419463ec65ed91a577820790"
-data_git_describe = "v0.0-9172-g94308f5e5"
+data_git_hash = "854388d4644276f097acdacb1f4ed356b2d04ce2"
+data_git_describe = "v0.0-9173-g854388d46"
 data_git_msg = """\
-commit 94308f5e5e4bc44b419463ec65ed91a577820790
-Author: Guillermo Maturana <maturana@google.com>
-Date:   Wed Dec 22 17:14:43 2021 -0800
+commit 854388d4644276f097acdacb1f4ed356b2d04ce2
+Author: Cindy Chen <chencindy@opentitan.org>
+Date:   Thu Dec 23 13:58:25 2021 -0800
 
-    [dv/pwrmgr] Add more checking into scoreboard
+    [fpv/pinmux] Update pinmux existing assertions
     
-    Predict the interrupt.
-    Add interrupt coverage.
+    This PR updates the currect assertions to consider two more cases: sleep
+    and jtag.
     
-    Signed-off-by: Guillermo Maturana <maturana@google.com>
+    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
 """
 
