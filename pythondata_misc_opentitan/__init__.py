54d56c4aee02c2b79d3994b2c96cb2e090ff09c4
LiteX Robot <robot@timvideos.us>
2021-12-24 10:26:33 +0000

    Updating pythondata-misc-opentitan to 0.0.post9293
    Updated data to v0.0-9176-g478730670 based on 478730670dbe2a0342fbee7062dde7e7e7cc447d from https://github.com/lowRISC/opentitan.
    > commit 478730670dbe2a0342fbee7062dde7e7e7cc447d
    > Author: Michael Schaffner <msf@opentitan.org>
    > Date:   Thu Dec 23 08:31:23 2021 -0800
    >
    >     [top] Switch USB pads to new dual pad type
    >
    >     Signed-off-by: Michael Schaffner <msf@opentitan.org>
    >
    
    Updated using 0.0.post117 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9291"
-version_tuple = (0, 0, 9291)
+version_str = "0.0.post9293"
+version_tuple = (0, 0, 9293)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9291")
+    pversion = V("0.0.post9293")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9174"
-data_version_tuple = (0, 0, 9174)
+data_version_str = "0.0.post9176"
+data_version_tuple = (0, 0, 9176)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9174")
+    pdata_version = V("0.0.post9176")
 except ImportError:
     pass
-data_git_hash = "da5559c67ae4b0bf1671b33090f82d8bf357c78b"
-data_git_describe = "v0.0-9174-gda5559c67"
+data_git_hash = "478730670dbe2a0342fbee7062dde7e7e7cc447d"
+data_git_describe = "v0.0-9176-g478730670"
 data_git_msg = """\
-commit da5559c67ae4b0bf1671b33090f82d8bf357c78b
-Author: Weicai Yang <weicai@google.com>
-Date:   Wed Dec 22 17:50:34 2021 -0800
+commit 478730670dbe2a0342fbee7062dde7e7e7cc447d
+Author: Michael Schaffner <msf@opentitan.org>
+Date:   Thu Dec 23 08:31:23 2021 -0800
 
-    [sram/dv] clean up scb to remove old backdoor checking
+    [top] Switch USB pads to new dual pad type
     
-    The new backdoor checking was added in #9573, which is pretty stable.
-    Now removed the old backdoor checks. No other function change in this
-    update.
-    
-    Signed-off-by: Weicai Yang <weicai@google.com>
+    Signed-off-by: Michael Schaffner <msf@opentitan.org>
 
 """
 
