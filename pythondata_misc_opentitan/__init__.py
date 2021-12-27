c34cda18c92213c6cf4a7a1e6246525cdc532c41
LiteX Robot <robot@timvideos.us>
2021-12-27 14:21:44 +0000

    Updating pythondata-misc-opentitan to 0.0.post9297
    Updated data to v0.0-9180-g0d61aa807 based on 0d61aa80799c9af33e2f6755dd6ddf28bca2f95a from https://github.com/lowRISC/opentitan.
    > commit 0d61aa80799c9af33e2f6755dd6ddf28bca2f95a
    > Author: Steve Nelson <steve.nelson@wdc.com>
    > Date:   Sat Dec 18 14:17:46 2021 -0800
    >
    >     [csrng/dv] Modify seed generation/handling
    >
    >     Signed-off-by: Steve Nelson <steve.nelson@wdc.com>
    >
    
    Updated using 0.0.post117 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,37 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9294"
-version_tuple = (0, 0, 9294)
+version_str = "0.0.post9297"
+version_tuple = (0, 0, 9297)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9294")
+    pversion = V("0.0.post9297")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9177"
-data_version_tuple = (0, 0, 9177)
+data_version_str = "0.0.post9180"
+data_version_tuple = (0, 0, 9180)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9177")
+    pdata_version = V("0.0.post9180")
 except ImportError:
     pass
-data_git_hash = "fe17d1cae4c7226d39284fa081de25e33b7e6b0d"
-data_git_describe = "v0.0-9177-gfe17d1cae"
+data_git_hash = "0d61aa80799c9af33e2f6755dd6ddf28bca2f95a"
+data_git_describe = "v0.0-9180-g0d61aa807"
 data_git_msg = """\
-commit fe17d1cae4c7226d39284fa081de25e33b7e6b0d
-Author: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
-Date:   Thu Dec 23 17:06:10 2021 -0800
+commit 0d61aa80799c9af33e2f6755dd6ddf28bca2f95a
+Author: Steve Nelson <steve.nelson@wdc.com>
+Date:   Sat Dec 18 14:17:46 2021 -0800
 
-    [ entropy_src ] Minor bug-fix in scoreboarding for repcnt & repcnts
+    [csrng/dv] Modify seed generation/handling
     
-    Whenever a new bit value or symbol is encountered, the repcnt and repcnts
-    scores should be expected to reset to 1 (1 repetition of most recent value).
-    
-    Previously the scoreboard expected a reset to 0 when finding a new bit/symbol.
-    
-    Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
+    Signed-off-by: Steve Nelson <steve.nelson@wdc.com>
 
 """
 
