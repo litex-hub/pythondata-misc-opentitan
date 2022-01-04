6119d0c068253dc3d2ccc1aca211f3695d82c77d
LiteX Robot <robot@timvideos.us>
2022-01-04 15:25:43 +0000

    Updating pythondata-misc-opentitan to 0.0.post9312
    Updated data to v0.0-9195-ga510f8e1f based on a510f8e1f1ae7efd0f6924d376cea58295f2ce3b from https://github.com/lowRISC/opentitan.
    > commit a510f8e1f1ae7efd0f6924d376cea58295f2ce3b
    > Author: Jade Philipoom <jadep@google.com>
    > Date:   Tue Dec 7 09:45:55 2021 +0000
    >
    >     [sw] Fix endianness bit in HMAC DIF.
    >
    >     According to the HMAC block specification, the digest is big-endian when
    >     the `digest_swap` bit is 1, and little-endian if it's 0; the DIF had
    >     these switched.
    >
    >     Signed-off-by: Jade Philipoom <jadep@google.com>
    >
    
    Updated using 0.0.post117 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,33 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9311"
-version_tuple = (0, 0, 9311)
+version_str = "0.0.post9312"
+version_tuple = (0, 0, 9312)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9311")
+    pversion = V("0.0.post9312")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9194"
-data_version_tuple = (0, 0, 9194)
+data_version_str = "0.0.post9195"
+data_version_tuple = (0, 0, 9195)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9194")
+    pdata_version = V("0.0.post9195")
 except ImportError:
     pass
-data_git_hash = "27685eff1a0dab971c32b94c1f04a53e54442097"
-data_git_describe = "v0.0-9194-g27685eff1"
+data_git_hash = "a510f8e1f1ae7efd0f6924d376cea58295f2ce3b"
+data_git_describe = "v0.0-9195-ga510f8e1f"
 data_git_msg = """\
-commit 27685eff1a0dab971c32b94c1f04a53e54442097
+commit a510f8e1f1ae7efd0f6924d376cea58295f2ce3b
 Author: Jade Philipoom <jadep@google.com>
-Date:   Fri Dec 10 18:58:04 2021 +0000
+Date:   Tue Dec 7 09:45:55 2021 +0000
 
-    [otbn,util] Combine information- and control- flow to analyze OTBN.
+    [sw] Fix endianness bit in HMAC DIF.
     
-    Adds analysis tools that construct information-flow graphs for
-    subroutines or whole programs.
+    According to the HMAC block specification, the digest is big-endian when
+    the `digest_swap` bit is 1, and little-endian if it's 0; the DIF had
+    these switched.
     
     Signed-off-by: Jade Philipoom <jadep@google.com>
 
