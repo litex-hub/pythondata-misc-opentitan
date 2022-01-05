abc6a873331b7bb30f94616e90ef0e5e27821685
LiteX Robot <robot@timvideos.us>
2022-01-05 11:20:05 +0000

    Updating pythondata-misc-opentitan to 0.0.post9335
    Updated data to v0.0-9218-gf6e561811 based on f6e5618114efb763ed63870bc5946c12631d2ec7 from https://github.com/lowRISC/opentitan.
    > commit f6e5618114efb763ed63870bc5946c12631d2ec7
    > Author: Rasmus Madsen <rasmus.madsen@wdc.com>
    > Date:   Tue Dec 21 01:40:14 2021 -0800
    >
    >     [spi_host/dv] created env for spi_host
    >
    >     Signed-off-by: Rasmus Madsen <rasmus.madsen@wdc.com>
    >
    
    Updated using 0.0.post117 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,39 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9333"
-version_tuple = (0, 0, 9333)
+version_str = "0.0.post9335"
+version_tuple = (0, 0, 9335)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9333")
+    pversion = V("0.0.post9335")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9216"
-data_version_tuple = (0, 0, 9216)
+data_version_str = "0.0.post9218"
+data_version_tuple = (0, 0, 9218)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9216")
+    pdata_version = V("0.0.post9218")
 except ImportError:
     pass
-data_git_hash = "32517dee2de8b39438e289f85f4b4353683a9812"
-data_git_describe = "v0.0-9216-g32517dee2"
+data_git_hash = "f6e5618114efb763ed63870bc5946c12631d2ec7"
+data_git_describe = "v0.0-9218-gf6e561811"
 data_git_msg = """\
-commit 32517dee2de8b39438e289f85f4b4353683a9812
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Tue Jan 4 17:48:24 2022 +0000
+commit f6e5618114efb763ed63870bc5946c12631d2ec7
+Author: Rasmus Madsen <rasmus.madsen@wdc.com>
+Date:   Tue Dec 21 01:40:14 2021 -0800
 
-    [otbn,dv] Fix loop warping for 1st iteration of single insn loop
+    [spi_host/dv] created env for spi_host
     
-    The previous code looked at current_loop_d.iterations but the problem
-    is that if the loop has only one instruction then the count has
-    already been decremented the first time we check.
-    
-    Fix things by looking at current_loop_q.iterations and then fixing
-    things up appropriately for updating the "_d version" afterwards.
-    
-    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    Signed-off-by: Rasmus Madsen <rasmus.madsen@wdc.com>
 
 """
 
