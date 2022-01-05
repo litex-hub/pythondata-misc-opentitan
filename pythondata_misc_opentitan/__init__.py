958dbeb86f668066d7f7da7d6605bcb1220cc091
LiteX Robot <robot@timvideos.us>
2022-01-05 22:23:48 +0000

    Updating pythondata-misc-opentitan to 0.0.post9345
    Updated data to v0.0-9228-g4de8e8272 based on 4de8e8272eae0ebcebf037aa380992dbce6d984c from https://github.com/lowRISC/opentitan.
    > commit 4de8e8272eae0ebcebf037aa380992dbce6d984c
    > Author: Weicai Yang <weicai@google.com>
    > Date:   Tue Jan 4 20:49:56 2022 -0800
    >
    >     [sram/dv] Fix stress_all_with_rand_reset
    >
    >     Remove escalation test as it invokes another reset
    >
    >     Signed-off-by: Weicai Yang <weicai@google.com>
    >
    
    Updated using 0.0.post117 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,59 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9344"
-version_tuple = (0, 0, 9344)
+version_str = "0.0.post9345"
+version_tuple = (0, 0, 9345)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9344")
+    pversion = V("0.0.post9345")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9227"
-data_version_tuple = (0, 0, 9227)
+data_version_str = "0.0.post9228"
+data_version_tuple = (0, 0, 9228)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9227")
+    pdata_version = V("0.0.post9228")
 except ImportError:
     pass
-data_git_hash = "5989d2abf4c3890f927579c9ba4b845cb6d553d0"
-data_git_describe = "v0.0-9227-g5989d2abf"
+data_git_hash = "4de8e8272eae0ebcebf037aa380992dbce6d984c"
+data_git_describe = "v0.0-9228-g4de8e8272"
 data_git_msg = """\
-commit 5989d2abf4c3890f927579c9ba4b845cb6d553d0
-Author: Nigel Scales <nigel.scales@gmail.com>
-Date:   Wed Dec 22 17:18:55 2021 +0000
+commit 4de8e8272eae0ebcebf037aa380992dbce6d984c
+Author: Weicai Yang <weicai@google.com>
+Date:   Tue Jan 4 20:49:56 2022 -0800
 
-    [lc_ctrl/dv] Implemented jtag_access jtag_priority and regwen_during_op testpoints
+    [sram/dv] Fix stress_all_with_rand_reset
     
-    - Added test sequences:
-      - lc_ctrl_jtag_access_vseq to test the the mutex controlled registers via
-      both the locked and unlocked interface to verify the interfaces and mutex
-      control.
-      - lc_ctrl_jtag_priority_vseq to test that when claiming the mutex that
-      JTAG has priority over Tile Link
-      - lc_ctrl_regwen_during_op_vseq to test mutex controlled registers are
-      locked during state transition
-    - Added tests:
-      - lc_ctrl_jtag_access
-      - lc_ctrl_jtag_smoke
-      - lc_ctrl_jtag_state_post_trans
-      - lc_ctrl_jtag_errors
-      - lc_ctrl_jtag_prog_failure
-      - lc_ctrl_jtag_csr_hw_reset
-      - lc_ctrl_jtag_csr_rw
-      - lc_ctrl_jtag_csr_bit_bash
-      - lc_ctrl_jtag_csr_aliasing
-      - lc_ctrl_jtag_same_csr_outstanding
-      - lc_ctrl_jtag_csr_mem_rw_with_rand_reset
-      - lc_ctrl_jtag_alert_test
-      - lc_ctrl_regwen_during_op
-      - lc_ctrl_jtag_regwen_during_op
-    - Added regression jtag
-    - Fixed up sequences for corrected status ready bit semantics
-    from PR #9860
+    Remove escalation test as it invokes another reset
     
-    Signed-off-by: Nigel Scales <nigel.scales@gmail.com>
+    Signed-off-by: Weicai Yang <weicai@google.com>
 
 """
 
