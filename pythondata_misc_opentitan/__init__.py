0a5e93b3935424f97227590d98884c6d53e174d7
LiteX Robot <robot@timvideos.us>
2022-01-05 14:18:40 +0000

    Updating pythondata-misc-opentitan to 0.0.post9339
    Updated data to v0.0-9222-gc1cf4a8a3 based on c1cf4a8a3d62af1070ecd8777129816bfa604d4a from https://github.com/lowRISC/opentitan.
    > commit c1cf4a8a3d62af1070ecd8777129816bfa604d4a
    > Author: Dave Williams <dave.williams@ensilica.com>
    > Date:   Thu Dec 16 17:47:48 2021 +0000
    >
    >     [sw,tests] ROM Integrity check in PROD and non-PROD LC States
    >
    >     Checks that the ROM will successfully boot when there is a mismatch of the computed and expected digests in non-production LC state.
    >     Checks that it will NOT boot when the same mismatch occurs when in production LC state.
    >
    >     Signed-off-by: Dave Williams <dave.williams@ensilica.com>
    >
    
    Updated using 0.0.post117 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9335"
-version_tuple = (0, 0, 9335)
+version_str = "0.0.post9339"
+version_tuple = (0, 0, 9339)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9335")
+    pversion = V("0.0.post9339")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9218"
-data_version_tuple = (0, 0, 9218)
+data_version_str = "0.0.post9222"
+data_version_tuple = (0, 0, 9222)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9218")
+    pdata_version = V("0.0.post9222")
 except ImportError:
     pass
-data_git_hash = "f6e5618114efb763ed63870bc5946c12631d2ec7"
-data_git_describe = "v0.0-9218-gf6e561811"
+data_git_hash = "c1cf4a8a3d62af1070ecd8777129816bfa604d4a"
+data_git_describe = "v0.0-9222-gc1cf4a8a3"
 data_git_msg = """\
-commit f6e5618114efb763ed63870bc5946c12631d2ec7
-Author: Rasmus Madsen <rasmus.madsen@wdc.com>
-Date:   Tue Dec 21 01:40:14 2021 -0800
+commit c1cf4a8a3d62af1070ecd8777129816bfa604d4a
+Author: Dave Williams <dave.williams@ensilica.com>
+Date:   Thu Dec 16 17:47:48 2021 +0000
 
-    [spi_host/dv] created env for spi_host
+    [sw,tests] ROM Integrity check in PROD and non-PROD LC States
     
-    Signed-off-by: Rasmus Madsen <rasmus.madsen@wdc.com>
+    Checks that the ROM will successfully boot when there is a mismatch of the computed and expected digests in non-production LC state.
+    Checks that it will NOT boot when the same mismatch occurs when in production LC state.
+    
+    Signed-off-by: Dave Williams <dave.williams@ensilica.com>
 
 """
 
