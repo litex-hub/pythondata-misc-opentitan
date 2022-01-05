4f957f9471e4dd144e2d9561937f614d005a0a5f
LiteX Robot <robot@timvideos.us>
2022-01-05 23:24:25 +0000

    Updating pythondata-misc-opentitan to 0.0.post9348
    Updated data to v0.0-9231-g8288b0163 based on 8288b0163a4d7b7bcf904da84c6d18976ad1ffa0 from https://github.com/lowRISC/opentitan.
    > commit 8288b0163a4d7b7bcf904da84c6d18976ad1ffa0
    > Author: Jes B. Klinke <jbk@chromium.org>
    > Date:   Mon Nov 8 13:46:08 2021 -0800
    >
    >     [opentitantool] Support configuration of GPIO pin modes
    >
    >     Replace GPIO direction enum (in/out) with a mode enum
    >     (input/push pull/open drain).
    >
    >     Also, introduce functionality in TransportWrapper to iterate over pin
    >     declarations and apply mode and default output level on a set of pins.
    >     This is intended to be used both on a broad set of pins, as well as
    >     multiple named combinations on a smaller set of pins strappings.
    >
    >     Signed-off-by: Jes B. Klinke <jbk@chromium.org>
    >     Change-Id: Ia6b9f392be2c34451bcdfadf82b8864797d5553b
    >
    
    Updated using 0.0.post117 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,41 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9345"
-version_tuple = (0, 0, 9345)
+version_str = "0.0.post9348"
+version_tuple = (0, 0, 9348)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9345")
+    pversion = V("0.0.post9348")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9228"
-data_version_tuple = (0, 0, 9228)
+data_version_str = "0.0.post9231"
+data_version_tuple = (0, 0, 9231)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9228")
+    pdata_version = V("0.0.post9231")
 except ImportError:
     pass
-data_git_hash = "4de8e8272eae0ebcebf037aa380992dbce6d984c"
-data_git_describe = "v0.0-9228-g4de8e8272"
+data_git_hash = "8288b0163a4d7b7bcf904da84c6d18976ad1ffa0"
+data_git_describe = "v0.0-9231-g8288b0163"
 data_git_msg = """\
-commit 4de8e8272eae0ebcebf037aa380992dbce6d984c
-Author: Weicai Yang <weicai@google.com>
-Date:   Tue Jan 4 20:49:56 2022 -0800
+commit 8288b0163a4d7b7bcf904da84c6d18976ad1ffa0
+Author: Jes B. Klinke <jbk@chromium.org>
+Date:   Mon Nov 8 13:46:08 2021 -0800
 
-    [sram/dv] Fix stress_all_with_rand_reset
+    [opentitantool] Support configuration of GPIO pin modes
     
-    Remove escalation test as it invokes another reset
+    Replace GPIO direction enum (in/out) with a mode enum
+    (input/push pull/open drain).
     
-    Signed-off-by: Weicai Yang <weicai@google.com>
+    Also, introduce functionality in TransportWrapper to iterate over pin
+    declarations and apply mode and default output level on a set of pins.
+    This is intended to be used both on a broad set of pins, as well as
+    multiple named combinations on a smaller set of pins strappings.
+    
+    Signed-off-by: Jes B. Klinke <jbk@chromium.org>
+    Change-Id: Ia6b9f392be2c34451bcdfadf82b8864797d5553b
 
 """
 
