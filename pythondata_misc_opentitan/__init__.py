f67676a0f40b4a788ee3e471b7c12bab9361507b
LiteX Robot <robot@timvideos.us>
2022-01-05 15:25:48 +0000

    Updating pythondata-misc-opentitan to 0.0.post9340
    Updated data to v0.0-9223-g92c57334f based on 92c57334f99f03b11830f3a8a58b2615f003fe33 from https://github.com/lowRISC/opentitan.
    > commit 92c57334f99f03b11830f3a8a58b2615f003fe33
    > Author: Rasmus Madsen <rasmus.madsen@wdc.com>
    > Date:   Thu Dec 23 08:56:34 2021 -0800
    >
    >     [pwm/dv] adding pwm env
    >
    >     Signed-off-by: Rasmus Madsen <rasmus.madsen@wdc.com>
    >
    
    Updated using 0.0.post117 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9339"
-version_tuple = (0, 0, 9339)
+version_str = "0.0.post9340"
+version_tuple = (0, 0, 9340)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9339")
+    pversion = V("0.0.post9340")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9222"
-data_version_tuple = (0, 0, 9222)
+data_version_str = "0.0.post9223"
+data_version_tuple = (0, 0, 9223)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9222")
+    pdata_version = V("0.0.post9223")
 except ImportError:
     pass
-data_git_hash = "c1cf4a8a3d62af1070ecd8777129816bfa604d4a"
-data_git_describe = "v0.0-9222-gc1cf4a8a3"
+data_git_hash = "92c57334f99f03b11830f3a8a58b2615f003fe33"
+data_git_describe = "v0.0-9223-g92c57334f"
 data_git_msg = """\
-commit c1cf4a8a3d62af1070ecd8777129816bfa604d4a
-Author: Dave Williams <dave.williams@ensilica.com>
-Date:   Thu Dec 16 17:47:48 2021 +0000
+commit 92c57334f99f03b11830f3a8a58b2615f003fe33
+Author: Rasmus Madsen <rasmus.madsen@wdc.com>
+Date:   Thu Dec 23 08:56:34 2021 -0800
 
-    [sw,tests] ROM Integrity check in PROD and non-PROD LC States
+    [pwm/dv] adding pwm env
     
-    Checks that the ROM will successfully boot when there is a mismatch of the computed and expected digests in non-production LC state.
-    Checks that it will NOT boot when the same mismatch occurs when in production LC state.
-    
-    Signed-off-by: Dave Williams <dave.williams@ensilica.com>
+    Signed-off-by: Rasmus Madsen <rasmus.madsen@wdc.com>
 
 """
 
