737b46f5c16c340ad9698f7097e0e71da8f3d895
LiteX Robot <robot@timvideos.us>
2022-01-06 22:24:37 +0000

    Updating pythondata-misc-opentitan to 0.0.post9367
    Updated data to v0.0-9250-g029d20f5d based on 029d20f5daf06c862b1b8b426a923b28efffd90b from https://github.com/lowRISC/opentitan.
    > commit 029d20f5daf06c862b1b8b426a923b28efffd90b
    > Author: Guillermo Maturana <maturana@google.com>
    > Date:   Wed Jan 5 20:56:01 2022 -0800
    >
    >     [dv/pwrmgr] Add more pwrmgr SVAs
    >
    >     Add SVA checking the incoming reset requests cause rstreqs outputs to
    >     rstmgr to respond correctly.
    >     Bind clkmgr_pwrmgr_sva_if into pwrmgr to check clk_en/status protocol.
    >     Fix some SVAs to be less rigid without loosing checks.
    >     Split clkmgr_pwrmgr_sva_if.sv into separate target for sharing.
    >     Improve escalation stimulus to get cleared when pwrmgr requests lc reset.
    >     Clear resets at the end of each wakeup_reset round in case they were disabled.
    >     Disable estclk_ctrl selection at the end of each extclk sequence round.
    >     Minor formatting fixes for verible.
    >
    >     Signed-off-by: Guillermo Maturana <maturana@google.com>
    >
    
    Updated using 0.0.post117 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,42 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9366"
-version_tuple = (0, 0, 9366)
+version_str = "0.0.post9367"
+version_tuple = (0, 0, 9367)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9366")
+    pversion = V("0.0.post9367")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9249"
-data_version_tuple = (0, 0, 9249)
+data_version_str = "0.0.post9250"
+data_version_tuple = (0, 0, 9250)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9249")
+    pdata_version = V("0.0.post9250")
 except ImportError:
     pass
-data_git_hash = "30fced74b749946898a6c655e9f81895e8d8400d"
-data_git_describe = "v0.0-9249-g30fced74b"
+data_git_hash = "029d20f5daf06c862b1b8b426a923b28efffd90b"
+data_git_describe = "v0.0-9250-g029d20f5d"
 data_git_msg = """\
-commit 30fced74b749946898a6c655e9f81895e8d8400d
-Author: Weicai Yang <weicai@google.com>
-Date:   Wed Jan 5 22:19:04 2022 -0800
+commit 029d20f5daf06c862b1b8b426a923b28efffd90b
+Author: Guillermo Maturana <maturana@google.com>
+Date:   Wed Jan 5 20:56:01 2022 -0800
 
-    [sram/dv] Update testplan and fix unmapped test
+    [dv/pwrmgr] Add more pwrmgr SVAs
     
-    Remove partiy test as it's replaced by passthru_mem_tl_intg_err
-    Add stress_all
+    Add SVA checking the incoming reset requests cause rstreqs outputs to
+    rstmgr to respond correctly.
+    Bind clkmgr_pwrmgr_sva_if into pwrmgr to check clk_en/status protocol.
+    Fix some SVAs to be less rigid without loosing checks.
+    Split clkmgr_pwrmgr_sva_if.sv into separate target for sharing.
+    Improve escalation stimulus to get cleared when pwrmgr requests lc reset.
+    Clear resets at the end of each wakeup_reset round in case they were disabled.
+    Disable estclk_ctrl selection at the end of each extclk sequence round.
+    Minor formatting fixes for verible.
     
-    Signed-off-by: Weicai Yang <weicai@google.com>
+    Signed-off-by: Guillermo Maturana <maturana@google.com>
 
 """
 
