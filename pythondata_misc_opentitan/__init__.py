741074e29c5ca6202798b673161741e21f5c67f4
LiteX Robot <robot@timvideos.us>
2022-01-06 20:24:04 +0000

    Updating pythondata-misc-opentitan to 0.0.post9362
    Updated data to v0.0-9245-gd43b1b22c based on d43b1b22cad9d9f9326773101bb637b8dfe140ef from https://github.com/lowRISC/opentitan.
    > commit d43b1b22cad9d9f9326773101bb637b8dfe140ef
    > Author: Pirmin Vogel <vogelpi@lowrisc.org>
    > Date:   Thu Jan 6 16:05:18 2022 +0100
    >
    >     [top_englishbreakfast] Update utility script for removing autogen files
    >
    >     Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
    >
    
    Updated using 0.0.post117 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9360"
-version_tuple = (0, 0, 9360)
+version_str = "0.0.post9362"
+version_tuple = (0, 0, 9362)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9360")
+    pversion = V("0.0.post9362")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9243"
-data_version_tuple = (0, 0, 9243)
+data_version_str = "0.0.post9245"
+data_version_tuple = (0, 0, 9245)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9243")
+    pdata_version = V("0.0.post9245")
 except ImportError:
     pass
-data_git_hash = "b5eefa44470c62121d4556b349ef2709b7e69bce"
-data_git_describe = "v0.0-9243-gb5eefa444"
+data_git_hash = "d43b1b22cad9d9f9326773101bb637b8dfe140ef"
+data_git_describe = "v0.0-9245-gd43b1b22c"
 data_git_msg = """\
-commit b5eefa44470c62121d4556b349ef2709b7e69bce
-Author: Rasmus Madsen <rasmus.madsen@wdc.com>
-Date:   Tue Jan 4 06:09:24 2022 -0800
+commit d43b1b22cad9d9f9326773101bb637b8dfe140ef
+Author: Pirmin Vogel <vogelpi@lowrisc.org>
+Date:   Thu Jan 6 16:05:18 2022 +0100
 
-    [spi_host/dv] modified transactional behavior for more randomized behavior
+    [top_englishbreakfast] Update utility script for removing autogen files
     
-    Signed-off-by: Rasmus Madsen <rasmus.madsen@wdc.com>
+    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
 
 """
 
