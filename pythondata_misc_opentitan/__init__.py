f8a0f5a6183bb252d926074fb7e562ece123a440
LiteX Robot <robot@timvideos.us>
2022-01-06 07:22:59 +0000

    Updating pythondata-misc-opentitan to 0.0.post9349
    Updated data to v0.0-9232-g3dab726bc based on 3dab726bc1ece248cba95611400ca55ceaa5a848 from https://github.com/lowRISC/opentitan.
    > commit 3dab726bc1ece248cba95611400ca55ceaa5a848
    > Author: Palmer Dabbelt <palmer@rivosinc.com>
    > Date:   Wed Jan 5 16:36:02 2022 -0800
    >
    >     [doc] Fix a grammatical error in ug/design
    >
    >     Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>
    >
    
    Updated using 0.0.post117 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,41 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9348"
-version_tuple = (0, 0, 9348)
+version_str = "0.0.post9349"
+version_tuple = (0, 0, 9349)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9348")
+    pversion = V("0.0.post9349")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9231"
-data_version_tuple = (0, 0, 9231)
+data_version_str = "0.0.post9232"
+data_version_tuple = (0, 0, 9232)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9231")
+    pdata_version = V("0.0.post9232")
 except ImportError:
     pass
-data_git_hash = "8288b0163a4d7b7bcf904da84c6d18976ad1ffa0"
-data_git_describe = "v0.0-9231-g8288b0163"
+data_git_hash = "3dab726bc1ece248cba95611400ca55ceaa5a848"
+data_git_describe = "v0.0-9232-g3dab726bc"
 data_git_msg = """\
-commit 8288b0163a4d7b7bcf904da84c6d18976ad1ffa0
-Author: Jes B. Klinke <jbk@chromium.org>
-Date:   Mon Nov 8 13:46:08 2021 -0800
+commit 3dab726bc1ece248cba95611400ca55ceaa5a848
+Author: Palmer Dabbelt <palmer@rivosinc.com>
+Date:   Wed Jan 5 16:36:02 2022 -0800
 
-    [opentitantool] Support configuration of GPIO pin modes
+    [doc] Fix a grammatical error in ug/design
     
-    Replace GPIO direction enum (in/out) with a mode enum
-    (input/push pull/open drain).
-    
-    Also, introduce functionality in TransportWrapper to iterate over pin
-    declarations and apply mode and default output level on a set of pins.
-    This is intended to be used both on a broad set of pins, as well as
-    multiple named combinations on a smaller set of pins strappings.
-    
-    Signed-off-by: Jes B. Klinke <jbk@chromium.org>
-    Change-Id: Ia6b9f392be2c34451bcdfadf82b8864797d5553b
+    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>
 
 """
 
