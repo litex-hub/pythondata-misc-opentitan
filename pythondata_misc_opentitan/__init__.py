54fa31454148d6b9c14bdd6ffec2525d01b28871
LiteX Robot <robot@timvideos.us>
2022-01-07 22:28:38 +0000

    Updating pythondata-misc-opentitan to 0.0.post9388
    Updated data to v0.0-9271-ga8f89ad1e based on a8f89ad1ed328fde67c0e67d8bb0ca4748033974 from https://github.com/lowRISC/opentitan.
    > commit a8f89ad1ed328fde67c0e67d8bb0ca4748033974
    > Author: Weicai Yang <weicai@google.com>
    > Date:   Thu Jan 6 22:47:04 2022 -0800
    >
    >     [sram/dv] add sram_ctrl_regwen_vseq
    >
    >     `ctrl` CSR is excluded in csr tests and it's gated by ctrl_regwen.
    >     Add this test to verify it
    >
    >     Signed-off-by: Weicai Yang <weicai@google.com>
    >
    
    Updated using 0.0.post117 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9387"
-version_tuple = (0, 0, 9387)
+version_str = "0.0.post9388"
+version_tuple = (0, 0, 9388)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9387")
+    pversion = V("0.0.post9388")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9270"
-data_version_tuple = (0, 0, 9270)
+data_version_str = "0.0.post9271"
+data_version_tuple = (0, 0, 9271)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9270")
+    pdata_version = V("0.0.post9271")
 except ImportError:
     pass
-data_git_hash = "0eb98eb2d1b72c72e9577b1d7c5a38b72cbe4933"
-data_git_describe = "v0.0-9270-g0eb98eb2d"
+data_git_hash = "a8f89ad1ed328fde67c0e67d8bb0ca4748033974"
+data_git_describe = "v0.0-9271-ga8f89ad1e"
 data_git_msg = """\
-commit 0eb98eb2d1b72c72e9577b1d7c5a38b72cbe4933
-Author: Cindy Chen <chencindy@opentitan.org>
-Date:   Thu Jan 6 14:15:15 2022 -0800
+commit a8f89ad1ed328fde67c0e67d8bb0ca4748033974
+Author: Weicai Yang <weicai@google.com>
+Date:   Thu Jan 6 22:47:04 2022 -0800
 
-    [fpv/pinmux] Add an entry for chip_earlgrey settings
+    [sram/dv] add sram_ctrl_regwen_vseq
     
-    This PR adds a hjson entry for pinmux with chip_earlgrey settings.
+    `ctrl` CSR is excluded in csr tests and it's gated by ctrl_regwen.
+    Add this test to verify it
     
-    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
+    Signed-off-by: Weicai Yang <weicai@google.com>
 
 """
 
