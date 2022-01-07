ae7fe04d8af3e2dd906f032a03092ee08e3a0248
LiteX Robot <robot@timvideos.us>
2022-01-07 20:25:42 +0000

    Updating pythondata-misc-opentitan to 0.0.post9385
    Updated data to v0.0-9268-ge05b0e5e0 based on e05b0e5e0c99970cb7fb79124b44dbf5fa15455b from https://github.com/lowRISC/opentitan.
    > commit e05b0e5e0c99970cb7fb79124b44dbf5fa15455b
    > Author: Mark Branstad <mark.branstad@wdc.com>
    > Date:   Fri Jan 7 05:57:03 2022 -0800
    >
    >     [csrng/doc] fix lc_hw_debug_en comment
    >
    >     The statement in the hjson file is not true anymore, removed.
    >
    >     Signed-off-by: Mark Branstad <mark.branstad@wdc.com>
    >
    
    Updated using 0.0.post117 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9383"
-version_tuple = (0, 0, 9383)
+version_str = "0.0.post9385"
+version_tuple = (0, 0, 9385)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9383")
+    pversion = V("0.0.post9385")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9266"
-data_version_tuple = (0, 0, 9266)
+data_version_str = "0.0.post9268"
+data_version_tuple = (0, 0, 9268)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9266")
+    pdata_version = V("0.0.post9268")
 except ImportError:
     pass
-data_git_hash = "a41c0a57568f1dc8263a4ecc3913f190750959f5"
-data_git_describe = "v0.0-9266-ga41c0a575"
+data_git_hash = "e05b0e5e0c99970cb7fb79124b44dbf5fa15455b"
+data_git_describe = "v0.0-9268-ge05b0e5e0"
 data_git_msg = """\
-commit a41c0a57568f1dc8263a4ecc3913f190750959f5
-Author: Michael Schaffner <msf@opentitan.org>
-Date:   Wed Jan 5 04:37:04 2022 -0800
+commit e05b0e5e0c99970cb7fb79124b44dbf5fa15455b
+Author: Mark Branstad <mark.branstad@wdc.com>
+Date:   Fri Jan 7 05:57:03 2022 -0800
 
-    [prim_filter_cnt] Make threshold runtime programmable
+    [csrng/doc] fix lc_hw_debug_en comment
     
-    Signed-off-by: Michael Schaffner <msf@opentitan.org>
+    The statement in the hjson file is not true anymore, removed.
+    
+    Signed-off-by: Mark Branstad <mark.branstad@wdc.com>
 
 """
 
