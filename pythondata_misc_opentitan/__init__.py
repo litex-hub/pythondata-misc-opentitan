b99c21a10485237e2fcf5c2e9870f30422d09e84
LiteX Robot <robot@timvideos.us>
2022-01-10 18:01:59 +0000

    Updating pythondata-misc-opentitan to 0.0.post9399
    Updated data to v0.0-9281-g405e3219f based on 405e3219f6b4081a18255dca7a67678b9051c4f7 from https://github.com/lowRISC/opentitan.
    > commit 405e3219f6b4081a18255dca7a67678b9051c4f7
    > Author: Jade Philipoom <jadep@google.com>
    > Date:   Fri Jan 7 18:07:21 2022 +0000
    >
    >     [sw,crypto] Adjust parameters for R^2 algorithm on Ibex.
    >
    >     Because the operands in this computation are always powers of 2, it is
    >     equivalent to square the operand or to perform a certain number of
    >     (modular) doublings. For small numbers the doublings are faster and for
    >     larger ones the squaring is faster; the cutoff where this happens for
    >     our implementation is at 2^96 (empirically determined).
    >
    >     Signed-off-by: Jade Philipoom <jadep@google.com>
    >
    
    Updated using 0.0.post118 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,43 +4,47 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9390"
-version_tuple = (0, 0, 9390)
+version_str = "0.0.post9399"
+version_tuple = (0, 0, 9399)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9390")
+    pversion = V("0.0.post9399")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9273"
-data_version_tuple = (0, 0, 9273)
+data_version_str = "0.0.post9281"
+data_version_tuple = (0, 0, 9281)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9273")
+    pdata_version = V("0.0.post9281")
 except ImportError:
     pass
-data_git_hash = "fbb37b1a04097143cfa7b0c26477e5b9b4884327"
-data_git_describe = "v0.0-9273-gfbb37b1a0"
+data_git_hash = "405e3219f6b4081a18255dca7a67678b9051c4f7"
+data_git_describe = "v0.0-9281-g405e3219f"
 data_git_msg = """\
-commit fbb37b1a04097143cfa7b0c26477e5b9b4884327
-Author: Weicai Yang <weicai@google.com>
-Date:   Thu Jan 6 14:04:30 2022 -0800
+commit 405e3219f6b4081a18255dca7a67678b9051c4f7
+Author: Jade Philipoom <jadep@google.com>
+Date:   Fri Jan 7 18:07:21 2022 +0000
 
-    [sram/dv] Update DV doc
+    [sw,crypto] Adjust parameters for R^2 algorithm on Ibex.
     
-    Update the memory scb checkings
+    Because the operands in this computation are always powers of 2, it is
+    equivalent to square the operand or to perform a certain number of
+    (modular) doublings. For small numbers the doublings are faster and for
+    larger ones the squaring is faster; the cutoff where this happens for
+    our implementation is at 2^96 (empirically determined).
     
-    Signed-off-by: Weicai Yang <weicai@google.com>
+    Signed-off-by: Jade Philipoom <jadep@google.com>
 
 """
 
 # Tool version info
-tool_version_str = "0.0.post117"
-tool_version_tuple = (0, 0, 117)
+tool_version_str = "0.0.post118"
+tool_version_tuple = (0, 0, 118)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post117")
+    ptool_version = V("0.0.post118")
 except ImportError:
     pass
 
