402d9e8efabc5de8186b313009aa17c692245f33
LiteX Robot <robot@timvideos.us>
2022-01-11 08:33:53 +0000

    Updating pythondata-misc-opentitan to 0.0.post9423
    Updated data to v0.0-9305-gb293e9467 based on b293e9467b1cb828ed5794ec93d7007d73ce6565 from https://github.com/lowRISC/opentitan.
    > commit b293e9467b1cb828ed5794ec93d7007d73ce6565
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Fri Jan 7 14:29:41 2022 +0000
    >
    >     [otbn,dv] Only run the exp_end_addr check for some runs
    >
    >     The expected end address check wasn't quite working now that we had a
    >     key sideload interface. The problem is that if you read from one of
    >     the sideload key WSRs when there's no key, the operation immediately
    >     stops with an error. That's fine, but we won't stop at the address we
    >     expected.
    >
    >     To allow this check to run at least some of the time (giving us more
    >     confidence in the rig), we define a new absent_key_allowed flag. If
    >     it's true, we use the normal key sideload sequence and disable the
    >     check. If it's false, we grab control of the sideload sequencer and
    >     run a specialised version where the key might change, but it always
    >     has a value. In this case, we can assert the end address again.
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post118 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,38 +4,45 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9416"
-version_tuple = (0, 0, 9416)
+version_str = "0.0.post9423"
+version_tuple = (0, 0, 9423)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9416")
+    pversion = V("0.0.post9423")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9298"
-data_version_tuple = (0, 0, 9298)
+data_version_str = "0.0.post9305"
+data_version_tuple = (0, 0, 9305)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9298")
+    pdata_version = V("0.0.post9305")
 except ImportError:
     pass
-data_git_hash = "827b23227e7ef0beaafe73920cba51a56e8e0afa"
-data_git_describe = "v0.0-9298-g827b23227"
+data_git_hash = "b293e9467b1cb828ed5794ec93d7007d73ce6565"
+data_git_describe = "v0.0-9305-gb293e9467"
 data_git_msg = """\
-commit 827b23227e7ef0beaafe73920cba51a56e8e0afa
-Author: Michael Schaffner <msf@opentitan.org>
-Date:   Mon Jan 10 11:23:50 2022 -0800
+commit b293e9467b1cb828ed5794ec93d7007d73ce6565
+Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
+Date:   Fri Jan 7 14:29:41 2022 +0000
 
-    [pinmux/doc] Update docs
+    [otbn,dv] Only run the exp_end_addr check for some runs
     
-    Fix #9953
+    The expected end address check wasn't quite working now that we had a
+    key sideload interface. The problem is that if you read from one of
+    the sideload key WSRs when there's no key, the operation immediately
+    stops with an error. That's fine, but we won't stop at the address we
+    expected.
     
-    Indicate that all pad attribute CSRs have WARL access, and make a note
-    that the pad attributes will be temporarily forced to 0 on the JTAG IOs
-    when they are activated.
+    To allow this check to run at least some of the time (giving us more
+    confidence in the rig), we define a new absent_key_allowed flag. If
+    it's true, we use the normal key sideload sequence and disable the
+    check. If it's false, we grab control of the sideload sequencer and
+    run a specialised version where the key might change, but it always
+    has a value. In this case, we can assert the end address again.
     
-    Signed-off-by: Michael Schaffner <msf@opentitan.org>
+    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
 """
 
