5050ae1916ed8f11b8ee9b37ed6f3faefe43672d
LiteX Robot <robot@timvideos.us>
2022-01-12 15:31:34 +0000

    Updating pythondata-misc-opentitan to 0.0.post9459
    Updated data to v0.0-9341-gee1634b6b based on ee1634b6b26f59b40e35e5c6e310c783b267db5e from https://github.com/lowRISC/opentitan.
    > commit ee1634b6b26f59b40e35e5c6e310c783b267db5e
    > Author: Rasmus Madsen <rasmus.madsen@wdc.com>
    > Date:   Tue Jan 11 12:46:02 2022 -0800
    >
    >     [spi_host/dv] added spi_host to nightly regression
    >
    >     Signed-off-by: Rasmus Madsen <rasmus.madsen@wdc.com>
    >
    
    Updated using 0.0.post118 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9455"
-version_tuple = (0, 0, 9455)
+version_str = "0.0.post9459"
+version_tuple = (0, 0, 9459)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9455")
+    pversion = V("0.0.post9459")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9337"
-data_version_tuple = (0, 0, 9337)
+data_version_str = "0.0.post9341"
+data_version_tuple = (0, 0, 9341)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9337")
+    pdata_version = V("0.0.post9341")
 except ImportError:
     pass
-data_git_hash = "c3bd8451e23db3cc941ec9636735ab592d1c70bb"
-data_git_describe = "v0.0-9337-gc3bd8451e"
+data_git_hash = "ee1634b6b26f59b40e35e5c6e310c783b267db5e"
+data_git_describe = "v0.0-9341-gee1634b6b"
 data_git_msg = """\
-commit c3bd8451e23db3cc941ec9636735ab592d1c70bb
-Author: Pirmin Vogel <vogelpi@lowrisc.org>
-Date:   Tue Jan 11 09:55:09 2022 +0100
+commit ee1634b6b26f59b40e35e5c6e310c783b267db5e
+Author: Rasmus Madsen <rasmus.madsen@wdc.com>
+Date:   Tue Jan 11 12:46:02 2022 -0800
 
-    [primgen] Update AscentLint waiver in generated abstract prim wrappers
+    [spi_host/dv] added spi_host to nightly regression
     
-    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
+    Signed-off-by: Rasmus Madsen <rasmus.madsen@wdc.com>
 
 """
 
