ea3ac89e6318474b6ac0df56e2c50a5107da9fdc
LiteX Robot <robot@timvideos.us>
2022-01-13 17:09:04 +0000

    Updating pythondata-misc-opentitan to 0.0.post9511
    Updated data to v0.0-9391-g5ef1bff3d based on 5ef1bff3de774e7c58e0a61bb64d383e30440c4c from https://github.com/lowRISC/opentitan.
    > commit 5ef1bff3de774e7c58e0a61bb64d383e30440c4c
    > Author: Steve Nelson <steve.nelson@wdc.com>
    > Date:   Wed Jan 12 04:28:58 2022 -0800
    >
    >     [edn/dv] Add scoreboard checking
    >
    >     Signed-off-by: Steve Nelson <steve.nelson@wdc.com>
    >
    
    Updated using 0.0.post120 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9509"
-version_tuple = (0, 0, 9509)
+version_str = "0.0.post9511"
+version_tuple = (0, 0, 9511)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9509")
+    pversion = V("0.0.post9511")
 except ImportError:
     pass
 
@@ -34,11 +34,11 @@ Date:   Wed Jan 12 04:28:58 2022 -0800
 """
 
 # Tool version info
-tool_version_str = "0.0.post118"
-tool_version_tuple = (0, 0, 118)
+tool_version_str = "0.0.post120"
+tool_version_tuple = (0, 0, 120)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post118")
+    ptool_version = V("0.0.post120")
 except ImportError:
     pass
 
