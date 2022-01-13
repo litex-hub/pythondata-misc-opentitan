da940e4e4e431079215c57f7b92aef1a07383697
LiteX Robot <robot@timvideos.us>
2022-01-13 21:25:51 +0000

    Updating pythondata-misc-opentitan to 0.0.post9522
    Updated data to v0.0-9400-g28deecfe0 based on 28deecfe0b680203aefceb143b7ade77a2570d44 from https://github.com/lowRISC/opentitan.
    > commit 28deecfe0b680203aefceb143b7ade77a2570d44
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Wed Jan 12 16:36:54 2022 -0800
    >
    >     [doc] update tlul and flash documentation
    >
    >     - update to explain instruction type attribute in tlul user bits.
    >     - update to explain how flash execution contro makes use of the
    >       instruction type bits.
    >
    >     - Companion PR to #10035 and #10022
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post122 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,38 +4,38 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9521"
-version_tuple = (0, 0, 9521)
+version_str = "0.0.post9522"
+version_tuple = (0, 0, 9522)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9521")
+    pversion = V("0.0.post9522")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9399"
-data_version_tuple = (0, 0, 9399)
+data_version_str = "0.0.post9400"
+data_version_tuple = (0, 0, 9400)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9399")
+    pdata_version = V("0.0.post9400")
 except ImportError:
     pass
-data_git_hash = "4a6f457f4306dec5149a8fa5c67f8d1faf5e5139"
-data_git_describe = "v0.0-9399-g4a6f457f4"
+data_git_hash = "28deecfe0b680203aefceb143b7ade77a2570d44"
+data_git_describe = "v0.0-9400-g28deecfe0"
 data_git_msg = """\
-commit 4a6f457f4306dec5149a8fa5c67f8d1faf5e5139
-Author: Guillermo Maturana <maturana@google.com>
-Date:   Wed Jan 12 21:56:29 2022 -0800
+commit 28deecfe0b680203aefceb143b7ade77a2570d44
+Author: Timothy Chen <timothytim@google.com>
+Date:   Wed Jan 12 16:36:54 2022 -0800
 
-    [dv/clkmgr] Add measurement saturation and fix timeout
+    [doc] update tlul and flash documentation
     
-    Add code to set the measurement cnt to a high value to test counter
-    saturation.
-    Fix measurement timeout test to stop the clock after the measurements
-    start to make sure the CSRs are updated, and to expect all io derived
-    clocks to timeout when the primary io clock is stopped.
+    - update to explain instruction type attribute in tlul user bits.
+    - update to explain how flash execution contro makes use of the
+      instruction type bits.
     
-    Signed-off-by: Guillermo Maturana <maturana@google.com>
+    - Companion PR to #10035 and #10022
+    
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
