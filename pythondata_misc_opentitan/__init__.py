8ba55d3f62ce508f9e1c8eebc87922111ab94bc7
LiteX Robot <robot@timvideos.us>
2022-01-13 18:34:45 +0000

    Updating pythondata-misc-opentitan to 0.0.post9516
    Updated data to v0.0-9395-gaae9bf89d based on aae9bf89d25cbd877fa54f1e0fe448fc408f45d9 from https://github.com/lowRISC/opentitan.
    > commit aae9bf89d25cbd877fa54f1e0fe448fc408f45d9
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Wed Jan 12 12:26:28 2022 -0800
    >
    >     [ci] Add pwrmgr_smoketest to private CI
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post121 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,41 +4,41 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9514"
-version_tuple = (0, 0, 9514)
+version_str = "0.0.post9516"
+version_tuple = (0, 0, 9516)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9514")
+    pversion = V("0.0.post9516")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9394"
-data_version_tuple = (0, 0, 9394)
+data_version_str = "0.0.post9395"
+data_version_tuple = (0, 0, 9395)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9394")
+    pdata_version = V("0.0.post9395")
 except ImportError:
     pass
-data_git_hash = "e4c5d706cc9d6516a58dd271485f7eee1b91cf7e"
-data_git_describe = "v0.0-9394-ge4c5d706c"
+data_git_hash = "aae9bf89d25cbd877fa54f1e0fe448fc408f45d9"
+data_git_describe = "v0.0-9395-gaae9bf89d"
 data_git_msg = """\
-commit e4c5d706cc9d6516a58dd271485f7eee1b91cf7e
-Author: Alphan Ulusoy <alphan@google.com>
-Date:   Tue Jan 11 16:25:32 2022 -0500
+commit aae9bf89d25cbd877fa54f1e0fe448fc408f45d9
+Author: Timothy Chen <timothytim@google.com>
+Date:   Wed Jan 12 12:26:28 2022 -0800
 
-    [bazel] Update BUILD files
+    [ci] Add pwrmgr_smoketest to private CI
     
-    Signed-off-by: Alphan Ulusoy <alphan@google.com>
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
 # Tool version info
-tool_version_str = "0.0.post120"
-tool_version_tuple = (0, 0, 120)
+tool_version_str = "0.0.post121"
+tool_version_tuple = (0, 0, 121)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post120")
+    ptool_version = V("0.0.post121")
 except ImportError:
     pass
 
