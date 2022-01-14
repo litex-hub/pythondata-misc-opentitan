8734b02a6251cab20eff2d51225219fd211550e5
LiteX Robot <robot@timvideos.us>
2022-01-14 18:34:13 +0000

    Updating pythondata-misc-opentitan to 0.0.post9541
    Updated data to v0.0-9419-g756ea0bdf based on 756ea0bdfd5c19b6b25cc7e83eaef32452991ba1 from https://github.com/lowRISC/opentitan.
    > commit 756ea0bdfd5c19b6b25cc7e83eaef32452991ba1
    > Author: Weicai Yang <weicai@google.com>
    > Date:   Mon Jan 10 14:31:47 2022 -0800
    >
    >     [dv] Update checklist
    >
    >     As discussed in SRAM Ctrl V2 review, updated these checklist
    >     1. Updated the description for “pre_verified_sub_modules_v2”
    >     2. Updated V2S checklist to add an item for FPV
    >
    >     Signed-off-by: Weicai Yang <weicai@google.com>
    >
    
    Updated using 0.0.post122 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9539"
-version_tuple = (0, 0, 9539)
+version_str = "0.0.post9541"
+version_tuple = (0, 0, 9541)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9539")
+    pversion = V("0.0.post9541")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9417"
-data_version_tuple = (0, 0, 9417)
+data_version_str = "0.0.post9419"
+data_version_tuple = (0, 0, 9419)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9417")
+    pdata_version = V("0.0.post9419")
 except ImportError:
     pass
-data_git_hash = "ee5604553d080abc708f526bcc69f5fe886987e2"
-data_git_describe = "v0.0-9417-gee5604553"
+data_git_hash = "756ea0bdfd5c19b6b25cc7e83eaef32452991ba1"
+data_git_describe = "v0.0-9419-g756ea0bdf"
 data_git_msg = """\
-commit ee5604553d080abc708f526bcc69f5fe886987e2
-Author: Eunchan Kim <eunchan@opentitan.org>
-Date:   Tue Jan 4 22:58:13 2022 +0000
+commit 756ea0bdfd5c19b6b25cc7e83eaef32452991ba1
+Author: Weicai Yang <weicai@google.com>
+Date:   Mon Jan 10 14:31:47 2022 -0800
 
-    [spi_device] Add valid check in CmdParse
+    [dv] Update checklist
     
-    Add cmd_info_slot valid field to the opcode_* conditions. With the
-    change, the cmdparse won't raise OnlyOneDatapath_A assertion for
-    unprogrammed cmd_info slot.
+    As discussed in SRAM Ctrl V2 review, updated these checklist
+    1. Updated the description for “pre_verified_sub_modules_v2”
+    2. Updated V2S checklist to add an item for FPV
     
-    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>
+    Signed-off-by: Weicai Yang <weicai@google.com>
 
 """
 
