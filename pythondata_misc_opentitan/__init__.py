2b8388fa420221b7d6d7dd9964915da205999b71
LiteX Robot <robot@timvideos.us>
2022-01-16 21:26:19 +0000

    Updating pythondata-misc-opentitan to 0.0.post9550
    Updated data to v0.0-9428-g7ac4c36d7 based on 7ac4c36d70e956f4c2141028988c3d3f8e12b36b from https://github.com/lowRISC/opentitan.
    > commit 7ac4c36d70e956f4c2141028988c3d3f8e12b36b
    > Author: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
    > Date:   Thu Jan 13 08:56:15 2022 -0800
    >
    >     [ spi_host, prj ] Update SPI_HOST to D2
    >
    >     Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
    >
    
    Updated using 0.0.post122 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9548"
-version_tuple = (0, 0, 9548)
+version_str = "0.0.post9550"
+version_tuple = (0, 0, 9550)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9548")
+    pversion = V("0.0.post9550")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9426"
-data_version_tuple = (0, 0, 9426)
+data_version_str = "0.0.post9428"
+data_version_tuple = (0, 0, 9428)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9426")
+    pdata_version = V("0.0.post9428")
 except ImportError:
     pass
-data_git_hash = "1e2e6b3469d36dd13cdd2d0a518c98c1c5a4de79"
-data_git_describe = "v0.0-9426-g1e2e6b346"
+data_git_hash = "7ac4c36d70e956f4c2141028988c3d3f8e12b36b"
+data_git_describe = "v0.0-9428-g7ac4c36d7"
 data_git_msg = """\
-commit 1e2e6b3469d36dd13cdd2d0a518c98c1c5a4de79
-Author: Guillermo Maturana <maturana@google.com>
-Date:   Thu Jan 13 12:37:14 2022 -0800
+commit 7ac4c36d70e956f4c2141028988c3d3f8e12b36b
+Author: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
+Date:   Thu Jan 13 08:56:15 2022 -0800
 
-    [dv/clkmgr] Add stress test
+    [ spi_host, prj ] Update SPI_HOST to D2
     
-    Fix for forks missing isolation forks.
-    Fix other minor issues caught by this test.
-    Move CSR value tracking to clkmgr interface.
-    
-    Signed-off-by: Guillermo Maturana <maturana@google.com>
+    Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
 
 """
 
