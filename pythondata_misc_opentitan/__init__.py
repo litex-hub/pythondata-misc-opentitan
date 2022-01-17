44864399037f42aef72d69f9b85212e133accd06
LiteX Robot <robot@timvideos.us>
2022-01-17 16:44:28 +0000

    Updating pythondata-misc-opentitan to 0.0.post9564
    Updated data to v0.0-9442-g8c1c899c5 based on 8c1c899c57a7479ab637f2b2c00fb4ccc03a2928 from https://github.com/lowRISC/opentitan.
    > commit 8c1c899c57a7479ab637f2b2c00fb4ccc03a2928
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Thu Dec 16 16:55:55 2021 +0000
    >
    >     [otbn,dv,doc] Add note in fcov.md about LC escalation tracking
    >
    >     This is mainly for completeness so that someone reading through the
    >     document who thinks "Hey, what about life-cycle escalation signals?"
    >     can find the answer easily.
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post122 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9562"
-version_tuple = (0, 0, 9562)
+version_str = "0.0.post9564"
+version_tuple = (0, 0, 9564)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9562")
+    pversion = V("0.0.post9564")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9440"
-data_version_tuple = (0, 0, 9440)
+data_version_str = "0.0.post9442"
+data_version_tuple = (0, 0, 9442)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9440")
+    pdata_version = V("0.0.post9442")
 except ImportError:
     pass
-data_git_hash = "90ff24f3a186d6015d7f80e2b50eea3925c59125"
-data_git_describe = "v0.0-9440-g90ff24f3a"
+data_git_hash = "8c1c899c57a7479ab637f2b2c00fb4ccc03a2928"
+data_git_describe = "v0.0-9442-g8c1c899c5"
 data_git_msg = """\
-commit 90ff24f3a186d6015d7f80e2b50eea3925c59125
+commit 8c1c899c57a7479ab637f2b2c00fb4ccc03a2928
 Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Thu Dec 16 17:04:34 2021 +0000
+Date:   Thu Dec 16 16:55:55 2021 +0000
 
-    [otbn,dv] Document how we cover integrity protection
+    [otbn,dv,doc] Add note in fcov.md about LC escalation tracking
     
-    Also mark the existing mem_integrity testpoint and the new
-    internal_integrity testpoint as V2S since they're testing fault
-    detection.
+    This is mainly for completeness so that someone reading through the
+    document who thinks "Hey, what about life-cycle escalation signals?"
+    can find the answer easily.
     
     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
