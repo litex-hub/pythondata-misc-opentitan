2d5248e132d880482a50e2fa5c7ac7dd62d10367
LiteX Robot <robot@timvideos.us>
2022-01-17 23:29:59 +0000

    Updating pythondata-misc-opentitan to 0.0.post9567
    Updated data to v0.0-9445-g53b9e7d8e based on 53b9e7d8e3c616764c175a489d566bddb1e6a6d9 from https://github.com/lowRISC/opentitan.
    > commit 53b9e7d8e3c616764c175a489d566bddb1e6a6d9
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Tue Dec 14 18:31:26 2021 +0000
    >
    >     [otbn,dv] Add functional coverage for fatal SW errs
    >
    >     This is a little fiddly because observing it happening requires two TL
    >     reads. If we're struggling to hit these bins, we could improve the
    >     tracking in the coverage code so that we can see it in more ways (e.g.
    >     read FATAL_ALERT_CAUSE before ERR_BITS).
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post122 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9564"
-version_tuple = (0, 0, 9564)
+version_str = "0.0.post9567"
+version_tuple = (0, 0, 9567)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9564")
+    pversion = V("0.0.post9567")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9442"
-data_version_tuple = (0, 0, 9442)
+data_version_str = "0.0.post9445"
+data_version_tuple = (0, 0, 9445)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9442")
+    pdata_version = V("0.0.post9445")
 except ImportError:
     pass
-data_git_hash = "8c1c899c57a7479ab637f2b2c00fb4ccc03a2928"
-data_git_describe = "v0.0-9442-g8c1c899c5"
+data_git_hash = "53b9e7d8e3c616764c175a489d566bddb1e6a6d9"
+data_git_describe = "v0.0-9445-g53b9e7d8e"
 data_git_msg = """\
-commit 8c1c899c57a7479ab637f2b2c00fb4ccc03a2928
+commit 53b9e7d8e3c616764c175a489d566bddb1e6a6d9
 Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Thu Dec 16 16:55:55 2021 +0000
+Date:   Tue Dec 14 18:31:26 2021 +0000
 
-    [otbn,dv,doc] Add note in fcov.md about LC escalation tracking
+    [otbn,dv] Add functional coverage for fatal SW errs
     
-    This is mainly for completeness so that someone reading through the
-    document who thinks "Hey, what about life-cycle escalation signals?"
-    can find the answer easily.
+    This is a little fiddly because observing it happening requires two TL
+    reads. If we're struggling to hit these bins, we could improve the
+    tracking in the coverage code so that we can see it in more ways (e.g.
+    read FATAL_ALERT_CAUSE before ERR_BITS).
     
     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
