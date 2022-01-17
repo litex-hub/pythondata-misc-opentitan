e2ffcccfd079a3f7fc7c5d448299a7249096c61c
LiteX Robot <robot@timvideos.us>
2022-01-17 10:32:36 +0000

    Updating pythondata-misc-opentitan to 0.0.post9555
    Updated data to v0.0-9433-g50dd56998 based on 50dd5699865d116ee0a313cbda096adf44576bca from https://github.com/lowRISC/opentitan.
    > commit 50dd5699865d116ee0a313cbda096adf44576bca
    > Author: Prajwala Puttappa <prajwalaputtappa@lowrisc.org>
    > Date:   Fri Jan 14 12:02:31 2022 +0000
    >
    >     [rom_ctrl, dv] V1 signoff
    >
    >     All actions items from V1 review have been finished and PRs have been
    >     merged.
    >
    >     Signed-off-by: Prajwala Puttappa <prajwalaputtappa@lowrisc.org>
    >
    
    Updated using 0.0.post122 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9553"
-version_tuple = (0, 0, 9553)
+version_str = "0.0.post9555"
+version_tuple = (0, 0, 9555)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9553")
+    pversion = V("0.0.post9555")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9431"
-data_version_tuple = (0, 0, 9431)
+data_version_str = "0.0.post9433"
+data_version_tuple = (0, 0, 9433)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9431")
+    pdata_version = V("0.0.post9433")
 except ImportError:
     pass
-data_git_hash = "733e3ecf5ccc3fd89755c9e3ec6c3532bc53c12d"
-data_git_describe = "v0.0-9431-g733e3ecf5"
+data_git_hash = "50dd5699865d116ee0a313cbda096adf44576bca"
+data_git_describe = "v0.0-9433-g50dd56998"
 data_git_msg = """\
-commit 733e3ecf5ccc3fd89755c9e3ec6c3532bc53c12d
-Author: Muqing Liu <muqing.liu@wdc.com>
-Date:   Wed Jan 5 16:40:55 2022 -0800
+commit 50dd5699865d116ee0a313cbda096adf44576bca
+Author: Prajwala Puttappa <prajwalaputtappa@lowrisc.org>
+Date:   Fri Jan 14 12:02:31 2022 +0000
 
-    [edn, dv] EDN testplan update
+    [rom_ctrl, dv] V1 signoff
     
-      - Update EDN testplan to include alert and error tests
-      - Move the ERR_CODE verification from the interrupt test to error test
-      - Add a covergroup for interrupt, alert and error test
+    All actions items from V1 review have been finished and PRs have been
+    merged.
     
-    Signed-off-by: Muqing Liu <muqing.liu@wdc.com>
+    Signed-off-by: Prajwala Puttappa <prajwalaputtappa@lowrisc.org>
 
 """
 
