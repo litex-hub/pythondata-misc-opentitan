f370d8b4bcadc1b93995b693a07d8cb3f93acfd5
LiteX Robot <robot@timvideos.us>
2022-01-17 01:24:44 +0000

    Updating pythondata-misc-opentitan to 0.0.post9551
    Updated data to v0.0-9429-g84ad1434d based on 84ad1434dbbab673f340a189dfdcae0c93aeff8b from https://github.com/lowRISC/opentitan.
    > commit 84ad1434dbbab673f340a189dfdcae0c93aeff8b
    > Author: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
    > Date:   Thu Jan 13 11:42:49 2022 -0800
    >
    >     [ spi_host, doc ] fix minor typo found in D2 review.
    >
    >     Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
    >
    
    Updated using 0.0.post122 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,30 +4,30 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9550"
-version_tuple = (0, 0, 9550)
+version_str = "0.0.post9551"
+version_tuple = (0, 0, 9551)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9550")
+    pversion = V("0.0.post9551")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9428"
-data_version_tuple = (0, 0, 9428)
+data_version_str = "0.0.post9429"
+data_version_tuple = (0, 0, 9429)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9428")
+    pdata_version = V("0.0.post9429")
 except ImportError:
     pass
-data_git_hash = "7ac4c36d70e956f4c2141028988c3d3f8e12b36b"
-data_git_describe = "v0.0-9428-g7ac4c36d7"
+data_git_hash = "84ad1434dbbab673f340a189dfdcae0c93aeff8b"
+data_git_describe = "v0.0-9429-g84ad1434d"
 data_git_msg = """\
-commit 7ac4c36d70e956f4c2141028988c3d3f8e12b36b
+commit 84ad1434dbbab673f340a189dfdcae0c93aeff8b
 Author: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
-Date:   Thu Jan 13 08:56:15 2022 -0800
+Date:   Thu Jan 13 11:42:49 2022 -0800
 
-    [ spi_host, prj ] Update SPI_HOST to D2
+    [ spi_host, doc ] fix minor typo found in D2 review.
     
     Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
 
