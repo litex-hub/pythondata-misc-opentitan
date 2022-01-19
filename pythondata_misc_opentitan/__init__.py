44a29694acaea3a3a407d83e2897b37d05778f3d
LiteX Robot <robot@timvideos.us>
2022-01-19 02:38:45 +0000

    Updating pythondata-misc-opentitan to 0.0.post9615
    Updated data to v0.0-9493-gaa35abbf0 based on aa35abbf0683f2dcbf1e75451d4f5380164908f2 from https://github.com/lowRISC/opentitan.
    > commit aa35abbf0683f2dcbf1e75451d4f5380164908f2
    > Author: Nigel Scales <nigel.scales@gmail.com>
    > Date:   Thu Jan 6 13:22:09 2022 +0000
    >
    >     [lc_ctrl/dv] Added test lc_ctrl_stress_all
    >
    >     - Added test sequences:
    >       - lc_ctrl_stress_all_vseq.sv
    >      Randomly cycles through the other sequences and randomly choosing
    >      TL or JTAG for CSR access for that sequence
    >       - lc_ctrl_security_escalation_vseq.sv
    >      Randomly triggers an escalation while the process to transition is
    >      occuring so as to exercise all the <state> -> escalate arcs of the FSM
    >     - Fixed lc_ctrl_smoke_vseq which needed to set cfg.test_phase
    >     - Fixed lc_ctrl_errors_vseq so it can be rerun by lc_ctrl_stress_all
    >     - Fixed scoreboard for new escalate scenarios
    >     to avoid scoreboard errors when run after another sequence.
    >     - Added test_sequence_typename string to lc_ctrl_if to aid debugging
    >     this is set to to the test sequence typename at the start of each sequence.
    >
    >     Signed-off-by: Nigel Scales <nigel.scales@gmail.com>
    >
    
    Updated using 0.0.post122 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,46 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9608"
-version_tuple = (0, 0, 9608)
+version_str = "0.0.post9615"
+version_tuple = (0, 0, 9615)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9608")
+    pversion = V("0.0.post9615")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9486"
-data_version_tuple = (0, 0, 9486)
+data_version_str = "0.0.post9493"
+data_version_tuple = (0, 0, 9493)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9486")
+    pdata_version = V("0.0.post9493")
 except ImportError:
     pass
-data_git_hash = "bf6c7513e86976b6a6f36792300a173c6819daaa"
-data_git_describe = "v0.0-9486-gbf6c7513e"
+data_git_hash = "aa35abbf0683f2dcbf1e75451d4f5380164908f2"
+data_git_describe = "v0.0-9493-gaa35abbf0"
 data_git_msg = """\
-commit bf6c7513e86976b6a6f36792300a173c6819daaa
-Author: Timothy Chen <timothytim@google.com>
-Date:   Thu Jan 13 10:41:47 2022 -0800
+commit aa35abbf0683f2dcbf1e75451d4f5380164908f2
+Author: Nigel Scales <nigel.scales@gmail.com>
+Date:   Thu Jan 6 13:22:09 2022 +0000
 
-    [flash_ctrl] prep work for flash exec value update
+    [lc_ctrl/dv] Added test lc_ctrl_stress_all
     
-    - Address #10022 with a 32-bit constant value
+    - Added test sequences:
+      - lc_ctrl_stress_all_vseq.sv
+     Randomly cycles through the other sequences and randomly choosing
+     TL or JTAG for CSR access for that sequence
+      - lc_ctrl_security_escalation_vseq.sv
+     Randomly triggers an escalation while the process to transition is
+     occuring so as to exercise all the <state> -> escalate arcs of the FSM
+    - Fixed lc_ctrl_smoke_vseq which needed to set cfg.test_phase
+    - Fixed lc_ctrl_errors_vseq so it can be rerun by lc_ctrl_stress_all
+    - Fixed scoreboard for new escalate scenarios
+    to avoid scoreboard errors when run after another sequence.
+    - Added test_sequence_typename string to lc_ctrl_if to aid debugging
+    this is set to to the test sequence typename at the start of each sequence.
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Nigel Scales <nigel.scales@gmail.com>
 
 """
 
