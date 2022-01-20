7b11c83bb9ffc7dbc4ea2ffcd8bb1bae9294257b
LiteX Robot <robot@timvideos.us>
2022-01-20 12:40:59 +0000

    Updating pythondata-misc-opentitan to 0.0.post9650
    Updated data to v0.0-9528-g2e5e1c322 based on 2e5e1c3222109f34264b6aea7aa1bb2e19baddf9 from https://github.com/lowRISC/opentitan.
    > commit 2e5e1c3222109f34264b6aea7aa1bb2e19baddf9
    > Author: Pirmin Vogel <vogelpi@lowrisc.org>
    > Date:   Mon Jan 10 12:28:18 2022 +0100
    >
    >     [aes] Move to D2
    >
    >     All D2 items for AES have been done. Implementation and verification
    >     of countermeasures have been moved to D2S and V2S stages, respectively.
    >     Thus, it's time to move AES into D2.
    >
    >     Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
    >
    
    Updated using 0.0.post122 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9647"
-version_tuple = (0, 0, 9647)
+version_str = "0.0.post9650"
+version_tuple = (0, 0, 9650)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9647")
+    pversion = V("0.0.post9650")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9525"
-data_version_tuple = (0, 0, 9525)
+data_version_str = "0.0.post9528"
+data_version_tuple = (0, 0, 9528)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9525")
+    pdata_version = V("0.0.post9528")
 except ImportError:
     pass
-data_git_hash = "40203d2c65a2b1e1986ed31292bea8e17e9fb4e1"
-data_git_describe = "v0.0-9525-g40203d2c6"
+data_git_hash = "2e5e1c3222109f34264b6aea7aa1bb2e19baddf9"
+data_git_describe = "v0.0-9528-g2e5e1c322"
 data_git_msg = """\
-commit 40203d2c65a2b1e1986ed31292bea8e17e9fb4e1
+commit 2e5e1c3222109f34264b6aea7aa1bb2e19baddf9
 Author: Pirmin Vogel <vogelpi@lowrisc.org>
-Date:   Fri Jan 14 15:29:19 2022 +0100
+Date:   Mon Jan 10 12:28:18 2022 +0100
 
-    [aes] Fix CSR test exclusions for key, IV and data input registers
+    [aes] Move to D2
     
-    Just like the data output registers, these are cleared with pseudo-
-    random data after reset meaning they should be excluded from the HW
-    reset tests.
-    
-    This resolves lowRISC/OpenTitan#10081.
+    All D2 items for AES have been done. Implementation and verification
+    of countermeasures have been moved to D2S and V2S stages, respectively.
+    Thus, it's time to move AES into D2.
     
     Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
 
