4ffe0fa7b744c4799c5322ec0967dc0aa6e9f150
LiteX Robot <robot@timvideos.us>
2022-01-20 19:24:29 +0000

    Updating pythondata-misc-opentitan to 0.0.post9660
    Updated data to v0.0-9538-g722d1fa9d based on 722d1fa9d62abf1c1a2cc8d06eba7eae0c540c47 from https://github.com/lowRISC/opentitan.
    > commit 722d1fa9d62abf1c1a2cc8d06eba7eae0c540c47
    > Author: Timothy Trippel <ttrippel@google.com>
    > Date:   Tue Jan 18 14:58:14 2022 -0800
    >
    >     [dif/alert_handler] Fix bug in ping timer lock DIFs.
    >
    >     The DIFs that checked whether the ping timer configuration was locked,
    >     and locked it, read-from/wrote-to the wrong CSR (the ping timer enable
    >     CSR). This commit fixes these two DIFs to write to the correct ping
    >     timer REGWEN CSR.
    >
    >     This partially addresses a task in #9899.
    >
    >     Signed-off-by: Timothy Trippel <ttrippel@google.com>
    >
    
    Updated using 0.0.post122 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,39 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9659"
-version_tuple = (0, 0, 9659)
+version_str = "0.0.post9660"
+version_tuple = (0, 0, 9660)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9659")
+    pversion = V("0.0.post9660")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9537"
-data_version_tuple = (0, 0, 9537)
+data_version_str = "0.0.post9538"
+data_version_tuple = (0, 0, 9538)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9537")
+    pdata_version = V("0.0.post9538")
 except ImportError:
     pass
-data_git_hash = "954592a075a4a0fad158a8363715c4b6d12219db"
-data_git_describe = "v0.0-9537-g954592a07"
+data_git_hash = "722d1fa9d62abf1c1a2cc8d06eba7eae0c540c47"
+data_git_describe = "v0.0-9538-g722d1fa9d"
 data_git_msg = """\
-commit 954592a075a4a0fad158a8363715c4b6d12219db
-Author: Nigel Scales <nigel.scales@gmail.com>
-Date:   Wed Jan 19 13:10:24 2022 +0000
+commit 722d1fa9d62abf1c1a2cc8d06eba7eae0c540c47
+Author: Timothy Trippel <ttrippel@google.com>
+Date:   Tue Jan 18 14:58:14 2022 -0800
 
-    [adc_ctrl] Excluded adc_en_ctl.adc_enable field from CSR write tests
+    [dif/alert_handler] Fix bug in ping timer lock DIFs.
     
-    - Updated adc_ctrl.hjson to disable writing to adc_en_ctl.adc_enable
-    during CSR tests this is to prevent unpredictable side effects.
+    The DIFs that checked whether the ping timer configuration was locked,
+    and locked it, read-from/wrote-to the wrong CSR (the ping timer enable
+    CSR). This commit fixes these two DIFs to write to the correct ping
+    timer REGWEN CSR.
     
-    Signed-off-by: Nigel Scales <nigel.scales@gmail.com>
+    This partially addresses a task in #9899.
+    
+    Signed-off-by: Timothy Trippel <ttrippel@google.com>
 
 """
 
