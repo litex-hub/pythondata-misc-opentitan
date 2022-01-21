29ab7d855c78fe690276c28434a8822eb7989db9
LiteX Robot <robot@timvideos.us>
2022-01-21 14:26:15 +0000

    Updating pythondata-misc-opentitan to 0.0.post9687
    Updated data to v0.0-9565-gdce6eb7ea based on dce6eb7ea9e842b76e90e2360776bc17a7214f22 from https://github.com/lowRISC/opentitan.
    > commit dce6eb7ea9e842b76e90e2360776bc17a7214f22
    > Author: Daniel Beitel <dbeitel@rivosinc.com>
    > Date:   Wed Jan 19 13:18:05 2022 -0800
    >
    >     [sw,mask_rom] Rename manifest.S so it doesn't collide with manifest.c rule
    >
    >     Signed-off-by: Daniel Beitel <dbeitel@rivosinc.com>
    >
    
    Updated using 0.0.post122 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9683"
-version_tuple = (0, 0, 9683)
+version_str = "0.0.post9687"
+version_tuple = (0, 0, 9687)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9683")
+    pversion = V("0.0.post9687")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9561"
-data_version_tuple = (0, 0, 9561)
+data_version_str = "0.0.post9565"
+data_version_tuple = (0, 0, 9565)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9561")
+    pdata_version = V("0.0.post9565")
 except ImportError:
     pass
-data_git_hash = "a06044b38635655c59396463d743b7d5bf9dd287"
-data_git_describe = "v0.0-9561-ga06044b38"
+data_git_hash = "dce6eb7ea9e842b76e90e2360776bc17a7214f22"
+data_git_describe = "v0.0-9565-gdce6eb7ea"
 data_git_msg = """\
-commit a06044b38635655c59396463d743b7d5bf9dd287
-Author: Prajwala Puttappa <prajwalaputtappa@lowrisc.org>
-Date:   Tue Jan 18 13:35:36 2022 +0000
+commit dce6eb7ea9e842b76e90e2360776bc17a7214f22
+Author: Daniel Beitel <dbeitel@rivosinc.com>
+Date:   Wed Jan 19 13:18:05 2022 -0800
 
-    [rom_ctrl, dv] Fixes regression failures in rom_ctrl_passthru_mem_tl_intg_err
+    [sw,mask_rom] Rename manifest.S so it doesn't collide with manifest.c rule
     
-    Fixes regression failures in rom_ctrl_passthru_mem_tl_intg_err testcase.
-    
-    Signed-off-by: Prajwala Puttappa <prajwalaputtappa@lowrisc.org>
+    Signed-off-by: Daniel Beitel <dbeitel@rivosinc.com>
 
 """
 
