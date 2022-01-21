a875997c5bc2939d2a852a8dd8c84e288c2b18e0
LiteX Robot <robot@timvideos.us>
2022-01-21 18:32:04 +0000

    Updating pythondata-misc-opentitan to 0.0.post9691
    Updated data to v0.0-9569-ge408ebf4a based on e408ebf4a40a36a59a79eb67878bcfa7193d7c25 from https://github.com/lowRISC/opentitan.
    > commit e408ebf4a40a36a59a79eb67878bcfa7193d7c25
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Tue Jan 18 12:51:51 2022 -0800
    >
    >     [adc_ctrl] combine current status into new status
    >
    >     - or the existing status into the next update to ensure filter
    >       match status is not lost
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post122 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,45 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9690"
-version_tuple = (0, 0, 9690)
+version_str = "0.0.post9691"
+version_tuple = (0, 0, 9691)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9690")
+    pversion = V("0.0.post9691")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9568"
-data_version_tuple = (0, 0, 9568)
+data_version_str = "0.0.post9569"
+data_version_tuple = (0, 0, 9569)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9568")
+    pdata_version = V("0.0.post9569")
 except ImportError:
     pass
-data_git_hash = "ca1abd11608eb7d5f6e41e489bc9519f8bdcf4b7"
-data_git_describe = "v0.0-9568-gca1abd116"
+data_git_hash = "e408ebf4a40a36a59a79eb67878bcfa7193d7c25"
+data_git_describe = "v0.0-9569-ge408ebf4a"
 data_git_msg = """\
-commit ca1abd11608eb7d5f6e41e489bc9519f8bdcf4b7
-Author: Michael Munday <mike.munday@lowrisc.org>
-Date:   Thu Jan 20 12:47:33 2022 +0000
+commit e408ebf4a40a36a59a79eb67878bcfa7193d7c25
+Author: Timothy Chen <timothytim@google.com>
+Date:   Tue Jan 18 12:51:51 2022 -0800
 
-    [rom] Setup watchdog timer in assembly before entropy setup
+    [adc_ctrl] combine current status into new status
     
-    This new code enables the watchdog timer right at the start of mask
-    ROM execution (before entropy setup and SRAM scrambling). This means
-    that unless the watchdog timer is 'petted' (i.e. the
-    aon_timer.WDOG_COUNT register is reset to 0 periodically) the device
-    will eventually reset itself.
+    - or the existing status into the next update to ensure filter
+      match status is not lost
     
-    The timeout value is currently the maximum possible (~6 hours assuming
-    a clock of 200KHz). This value should be reduced to something lower,
-    issue #10215 tracks this.
-    
-    This change has been manually tested by setting the timer value to 0
-    and checking that the device is quickly reset.
-    
-    Signed-off-by: Michael Munday <mike.munday@lowrisc.org>
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
