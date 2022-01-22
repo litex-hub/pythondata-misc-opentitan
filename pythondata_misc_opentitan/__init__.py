21930c4be7a458d3e9c150e19f8db7cd0bef47d4
LiteX Robot <robot@timvideos.us>
2022-01-22 02:41:01 +0000

    Updating pythondata-misc-opentitan to 0.0.post9711
    Updated data to v0.0-9589-gdc40cb577 based on dc40cb5774741bca306f536b1f5abe1f59cb1319 from https://github.com/lowRISC/opentitan.
    > commit dc40cb5774741bca306f536b1f5abe1f59cb1319
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Fri Jan 21 15:28:23 2022 -0800
    >
    >     [dv/otp_ctrl] Add checkings for vendor_test partition
    >
    >     This PR adds two items for vendor_test partition:
    >     1). Read lock vendor test partition via csr.
    >     2). Read out vendor test partition digest value via csrs.
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post122 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,33 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9710"
-version_tuple = (0, 0, 9710)
+version_str = "0.0.post9711"
+version_tuple = (0, 0, 9711)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9710")
+    pversion = V("0.0.post9711")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9588"
-data_version_tuple = (0, 0, 9588)
+data_version_str = "0.0.post9589"
+data_version_tuple = (0, 0, 9589)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9588")
+    pdata_version = V("0.0.post9589")
 except ImportError:
     pass
-data_git_hash = "8392480045c152992aeb1121df225090fd310252"
-data_git_describe = "v0.0-9588-g839248004"
+data_git_hash = "dc40cb5774741bca306f536b1f5abe1f59cb1319"
+data_git_describe = "v0.0-9589-gdc40cb577"
 data_git_msg = """\
-commit 8392480045c152992aeb1121df225090fd310252
+commit dc40cb5774741bca306f536b1f5abe1f59cb1319
 Author: Cindy Chen <chencindy@opentitan.org>
-Date:   Fri Jan 21 14:22:36 2022 -0800
+Date:   Fri Jan 21 15:28:23 2022 -0800
 
-    [dv/otp_ctrl] Fix covergroups
+    [dv/otp_ctrl] Add checkings for vendor_test partition
     
-    This PR fixes prim_tl_agent covergroups and small documentation related
-    to vendortest partition.
+    This PR adds two items for vendor_test partition:
+    1). Read lock vendor test partition via csr.
+    2). Read out vendor test partition digest value via csrs.
     
     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
