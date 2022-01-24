7af1113590148b91a39f78863e7d2e843f738365
LiteX Robot <robot@timvideos.us>
2022-01-24 15:27:52 +0000

    Updating pythondata-misc-opentitan to 0.0.post9718
    Updated data to v0.0-9596-g0262e24d6 based on 0262e24d61f6019199f47c50d34ae8e7aa0acf20 from https://github.com/lowRISC/opentitan.
    > commit 0262e24d61f6019199f47c50d34ae8e7aa0acf20
    > Author: Drew Macrae <drewmacrae@google.com>
    > Date:   Sun Jan 23 19:35:14 2022 -0800
    >
    >     [bazel] fixup dependency in mask_rom_epmp_test
    >
    >     didn't commit and push the version of the fix I tested and worked on
    >     for #10280. This commit should actually work to build the
    >     mask_rom_epmp_test.
    >
    >     Signed-off-by: Drew Macrae <drewmacrae@google.com>
    >
    
    Updated using 0.0.post122 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9717"
-version_tuple = (0, 0, 9717)
+version_str = "0.0.post9718"
+version_tuple = (0, 0, 9718)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9717")
+    pversion = V("0.0.post9718")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9595"
-data_version_tuple = (0, 0, 9595)
+data_version_str = "0.0.post9596"
+data_version_tuple = (0, 0, 9596)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9595")
+    pdata_version = V("0.0.post9596")
 except ImportError:
     pass
-data_git_hash = "f1661052e9676eadec99601bfa46485b2c0a85e1"
-data_git_describe = "v0.0-9595-gf1661052e"
+data_git_hash = "0262e24d61f6019199f47c50d34ae8e7aa0acf20"
+data_git_describe = "v0.0-9596-g0262e24d6"
 data_git_msg = """\
-commit f1661052e9676eadec99601bfa46485b2c0a85e1
-Author: Timothy Chen <timothytim@google.com>
-Date:   Tue Jan 18 17:04:56 2022 -0800
+commit 0262e24d61f6019199f47c50d34ae8e7aa0acf20
+Author: Drew Macrae <drewmacrae@google.com>
+Date:   Sun Jan 23 19:35:14 2022 -0800
 
-    [flash_ctrl] D2 preparation steps
+    [bazel] fixup dependency in mask_rom_epmp_test
     
-    - fully covnert to new sparse fsm
-    - add additional fsms as needed
+    didn't commit and push the version of the fix I tested and worked on
+    for #10280. This commit should actually work to build the
+    mask_rom_epmp_test.
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Drew Macrae <drewmacrae@google.com>
 
 """
 
