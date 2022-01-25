abc69e708cd5a4b508129fe47bb88ea870775a8d
LiteX Robot <robot@timvideos.us>
2022-01-25 17:29:54 +0000

    Updating pythondata-misc-opentitan to 0.0.post9774
    Updated data to v0.0-9652-gcf5bcb704 based on cf5bcb704e25ff981c8471078a28ec9be19542a2 from https://github.com/lowRISC/opentitan.
    > commit cf5bcb704e25ff981c8471078a28ec9be19542a2
    > Author: Mark Branstad <mark.branstad@wdc.com>
    > Date:   Fri Jan 21 14:55:38 2022 -0800
    >
    >     [entropy_src/rtl] Clear stats only on enable
    >
    >     All statistics clear when the main enable is turned off.
    >     This is not good for post-processing of health test effectiveness.
    >     Fix is clear all when enabling.
    >     Fixes #10227.
    >
    >     Signed-off-by: Mark Branstad <mark.branstad@wdc.com>
    >
    
    Updated using 0.0.post122 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,37 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9772"
-version_tuple = (0, 0, 9772)
+version_str = "0.0.post9774"
+version_tuple = (0, 0, 9774)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9772")
+    pversion = V("0.0.post9774")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9650"
-data_version_tuple = (0, 0, 9650)
+data_version_str = "0.0.post9652"
+data_version_tuple = (0, 0, 9652)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9650")
+    pdata_version = V("0.0.post9652")
 except ImportError:
     pass
-data_git_hash = "4db9450e5d9b92a3c4c6020fba4f9fb639d79423"
-data_git_describe = "v0.0-9650-g4db9450e5"
+data_git_hash = "cf5bcb704e25ff981c8471078a28ec9be19542a2"
+data_git_describe = "v0.0-9652-gcf5bcb704"
 data_git_msg = """\
-commit 4db9450e5d9b92a3c4c6020fba4f9fb639d79423
-Author: Igor Kouznetsov <igor.kouznetsov@wdc.com>
-Date:   Tue Jan 18 14:12:12 2022 -0800
+commit cf5bcb704e25ff981c8471078a28ec9be19542a2
+Author: Mark Branstad <mark.branstad@wdc.com>
+Date:   Fri Jan 21 14:55:38 2022 -0800
 
-    [i2c, rtl] Removed SDA toggling when sending multiple bits of "1"s.
+    [entropy_src/rtl] Clear stats only on enable
     
-    Signed-off-by: Igor Kouznetsov <igor.kouznetsov@wdc.com>
+    All statistics clear when the main enable is turned off.
+    This is not good for post-processing of health test effectiveness.
+    Fix is clear all when enabling.
+    Fixes #10227.
+    
+    Signed-off-by: Mark Branstad <mark.branstad@wdc.com>
 
 """
 
