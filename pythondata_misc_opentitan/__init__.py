be50649c15c04dd38830218bd2d732f26176a27b
LiteX Robot <robot@timvideos.us>
2022-01-25 21:24:41 +0000

    Updating pythondata-misc-opentitan to 0.0.post9791
    Updated data to v0.0-9669-gcc0f9024c based on cc0f9024cb2aaad7a46b7ff3f6b4e7a24b5e79b6 from https://github.com/lowRISC/opentitan.
    > commit cc0f9024cb2aaad7a46b7ff3f6b4e7a24b5e79b6
    > Author: Michael Schaffner <msf@opentitan.org>
    > Date:   Mon Jan 24 20:59:43 2022 -0800
    >
    >     [otp_ctrl] Amend documentation for VENDOR_TEST partition
    >
    >     Signed-off-by: Michael Schaffner <msf@opentitan.org>
    >
    
    Updated using 0.0.post122 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,38 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9787"
-version_tuple = (0, 0, 9787)
+version_str = "0.0.post9791"
+version_tuple = (0, 0, 9791)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9787")
+    pversion = V("0.0.post9791")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9665"
-data_version_tuple = (0, 0, 9665)
+data_version_str = "0.0.post9669"
+data_version_tuple = (0, 0, 9669)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9665")
+    pdata_version = V("0.0.post9669")
 except ImportError:
     pass
-data_git_hash = "e99015e644d03768a616d463f4f8c991f07f3bb3"
-data_git_describe = "v0.0-9665-ge99015e64"
+data_git_hash = "cc0f9024cb2aaad7a46b7ff3f6b4e7a24b5e79b6"
+data_git_describe = "v0.0-9669-gcc0f9024c"
 data_git_msg = """\
-commit e99015e644d03768a616d463f4f8c991f07f3bb3
-Author: Miguel Osorio <miguelosorio@google.com>
-Date:   Mon Jan 24 18:12:10 2022 -0800
+commit cc0f9024cb2aaad7a46b7ff3f6b4e7a24b5e79b6
+Author: Michael Schaffner <msf@opentitan.org>
+Date:   Mon Jan 24 20:59:43 2022 -0800
 
-    [sw/rom] Update bazel rules
+    [otp_ctrl] Amend documentation for VENDOR_TEST partition
     
-    1. Remove abs and sec_mmio dependencies from their respective mock
-       functions.
-    2. Add OPENTITAN_CPU target to libraries that depend on target specific
-       definitions.
-    3. Fix build issues with sigverify OTBN rules.
-    
-    Signed-off-by: Miguel Osorio <miguelosorio@google.com>
+    Signed-off-by: Michael Schaffner <msf@opentitan.org>
 
 """
 
