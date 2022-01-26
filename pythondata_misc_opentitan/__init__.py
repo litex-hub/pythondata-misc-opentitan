185832db42ab71e8472c92b86c70f41d50c2ca68
LiteX Robot <robot@timvideos.us>
2022-01-26 19:23:43 +0000

    Updating pythondata-misc-opentitan to 0.0.post9815
    Updated data to v0.0-9693-g51fde5a98 based on 51fde5a9830ab1501b01d953c7651c63727a546d from https://github.com/lowRISC/opentitan.
    > commit 51fde5a9830ab1501b01d953c7651c63727a546d
    > Author: Mark Branstad <mark.branstad@wdc.com>
    > Date:   Fri Jan 7 14:46:56 2022 -0800
    >
    >     [entropy_src/rtl] Modify enable registers
    >
    >     This PR makes two broad changes to the entropy_src enable registers:
    >
    >     - The conf.enable field has been split into two registers
    >       - module_enable.module_enable: for overall activation of the IP in
    >         any context (BOOT_ROM or otherwise)
    >       - conf.fips_enable: For activation of strict RNG health testing, using
    >         stricter CC or FIPS grade health-test thresholds (not recommended
    >         for BOOT_ROM driven operation).
    >     - For better sequencing control, and error handling capabilities, the
    >       module_enable has been split out into its own register, with a
    >       separate write enable control (REGWEN_ME vs. REGWEN).
    >
    >     Note on Software guidelines following this commit:
    >
    >     The appropriate use of these registers depends on whether they are
    >     set in the BOOT_ROM or in later (mutable) C-code:
    >
    >     - The enable fields should only differ when set by the BOOT ROM, which
    >       uses a preliminary set of health test thresholds.  Here, the
    >       MODULE_ENABLE should be set _without_ setting the FIPS_ENABLE field.
    >       This indicates that the stronger "FIPS" thresholds have not been set,
    >       and the IP is not configured for reliable entropy generation.
    >
    >     - For all later, DIF-driven software stages we assume that the
    >       "Module" enable and the "FIPS" enable are always set together,
    >       after the final stricter health test thresholds have been set.
    >
    >     Fixes #9637.
    >
    >     Signed-off-by: Mark Branstad <mark.branstad@wdc.com>
    >     Co-authored-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
    >
    
    Updated using 0.0.post122 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,39 +4,62 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9813"
-version_tuple = (0, 0, 9813)
+version_str = "0.0.post9815"
+version_tuple = (0, 0, 9815)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9813")
+    pversion = V("0.0.post9815")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9691"
-data_version_tuple = (0, 0, 9691)
+data_version_str = "0.0.post9693"
+data_version_tuple = (0, 0, 9693)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9691")
+    pdata_version = V("0.0.post9693")
 except ImportError:
     pass
-data_git_hash = "d6e91619f67258464ec8f3a3ba68dfa19f7319fd"
-data_git_describe = "v0.0-9691-gd6e91619f"
+data_git_hash = "51fde5a9830ab1501b01d953c7651c63727a546d"
+data_git_describe = "v0.0-9693-g51fde5a98"
 data_git_msg = """\
-commit d6e91619f67258464ec8f3a3ba68dfa19f7319fd
-Author: Miguel Osorio <miguelosorio@google.com>
-Date:   Thu Jan 20 19:53:12 2022 -0800
+commit 51fde5a9830ab1501b01d953c7651c63727a546d
+Author: Mark Branstad <mark.branstad@wdc.com>
+Date:   Fri Jan 7 14:46:56 2022 -0800
 
-    [sw/rom] Updates to sec_mmio hardening
+    [entropy_src/rtl] Modify enable registers
     
-    * Introduce launder32 and HARDENED_CHECK to sec_mmio.
-    * Remove shutdown callback function.
-    * Remove expensive modulus operation.
-    * Remove direct use of volatile.
-    * Add checks to sec_mmio_init function to ensure counters are properly
-      initialized.
+    This PR makes two broad changes to the entropy_src enable registers:
     
-    Signed-off-by: Miguel Osorio <miguelosorio@google.com>
+    - The conf.enable field has been split into two registers
+      - module_enable.module_enable: for overall activation of the IP in
+        any context (BOOT_ROM or otherwise)
+      - conf.fips_enable: For activation of strict RNG health testing, using
+        stricter CC or FIPS grade health-test thresholds (not recommended
+        for BOOT_ROM driven operation).
+    - For better sequencing control, and error handling capabilities, the
+      module_enable has been split out into its own register, with a
+      separate write enable control (REGWEN_ME vs. REGWEN).
+    
+    Note on Software guidelines following this commit:
+    
+    The appropriate use of these registers depends on whether they are
+    set in the BOOT_ROM or in later (mutable) C-code:
+    
+    - The enable fields should only differ when set by the BOOT ROM, which
+      uses a preliminary set of health test thresholds.  Here, the
+      MODULE_ENABLE should be set _without_ setting the FIPS_ENABLE field.
+      This indicates that the stronger "FIPS" thresholds have not been set,
+      and the IP is not configured for reliable entropy generation.
+    
+    - For all later, DIF-driven software stages we assume that the
+      "Module" enable and the "FIPS" enable are always set together,
+      after the final stricter health test thresholds have been set.
+    
+    Fixes #9637.
+    
+    Signed-off-by: Mark Branstad <mark.branstad@wdc.com>
+    Co-authored-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
 
 """
 
