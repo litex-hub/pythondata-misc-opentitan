d386317f46a2efc58363d6e6faa861dde5f9507d
LiteX Robot <robot@timvideos.us>
2022-01-26 05:25:21 +0000

    Updating pythondata-misc-opentitan to 0.0.post9804
    Updated data to v0.0-9682-g68e865159 based on 68e86515989c047e28915f6d5d534c578c9bdb3d from https://github.com/lowRISC/opentitan.
    > commit 68e86515989c047e28915f6d5d534c578c9bdb3d
    > Author: Cindy Chen <chencindy@google.com>
    > Date:   Tue Jan 25 10:17:54 2022 -0800
    >
    >     [dv/otp_ctrl] Fix prim_tl_agent close source monitor error
    >
    >     Due to the current reggen tool, we have an empty prim_tl_o/i interface
    >     without any CSRs for test_access. To ensure the connection for
    >     read/write works, open source OTP tb creates a temp tl_agent. However,
    >     because it hooks up same interface with close source, the monitor will
    >     report some runtime error.
    >     To avoid this, we introduced a cfg flag that can disable creating this temp
    >     tl_agent.
    >
    >     Signed-off-by: Cindy Chen <chencindy@google.com>
    >
    
    Updated using 0.0.post122 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,40 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9802"
-version_tuple = (0, 0, 9802)
+version_str = "0.0.post9804"
+version_tuple = (0, 0, 9804)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9802")
+    pversion = V("0.0.post9804")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9680"
-data_version_tuple = (0, 0, 9680)
+data_version_str = "0.0.post9682"
+data_version_tuple = (0, 0, 9682)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9680")
+    pdata_version = V("0.0.post9682")
 except ImportError:
     pass
-data_git_hash = "5556d17ed47aeed5ea88a4480d9b4d8fb2aeb751"
-data_git_describe = "v0.0-9680-g5556d17ed"
+data_git_hash = "68e86515989c047e28915f6d5d534c578c9bdb3d"
+data_git_describe = "v0.0-9682-g68e865159"
 data_git_msg = """\
-commit 5556d17ed47aeed5ea88a4480d9b4d8fb2aeb751
-Author: Eunchan Kim <eunchan@opentitan.org>
-Date:   Tue Jan 25 15:47:43 2022 -0800
+commit 68e86515989c047e28915f6d5d534c578c9bdb3d
+Author: Cindy Chen <chencindy@google.com>
+Date:   Tue Jan 25 10:17:54 2022 -0800
 
-    [spi_device] Update prj.hjson to D2
+    [dv/otp_ctrl] Fix prim_tl_agent close source monitor error
     
-    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>
+    Due to the current reggen tool, we have an empty prim_tl_o/i interface
+    without any CSRs for test_access. To ensure the connection for
+    read/write works, open source OTP tb creates a temp tl_agent. However,
+    because it hooks up same interface with close source, the monitor will
+    report some runtime error.
+    To avoid this, we introduced a cfg flag that can disable creating this temp
+    tl_agent.
+    
+    Signed-off-by: Cindy Chen <chencindy@google.com>
 
 """
 
