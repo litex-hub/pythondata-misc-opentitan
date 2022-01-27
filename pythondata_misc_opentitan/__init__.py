fd54e22e573a6baa26e07e4bdde01bd6db7240f3
LiteX Robot <robot@timvideos.us>
2022-01-27 23:27:24 +0000

    Updating pythondata-misc-opentitan to 0.0.post9848
    Updated data to v0.0-9724-g8a35814ef based on 8a35814ef0db693f67549738f46087d57dcb5251 from https://github.com/lowRISC/opentitan.
    > commit 8a35814ef0db693f67549738f46087d57dcb5251
    > Author: Drew Macrae <drewmacrae@google.com>
    > Date:   Wed Jan 26 07:20:04 2022 -0800
    >
    >     [bazel] start running in CI and warning
    >
    >     Signed-off-by: Drew Macrae <drewmacrae@google.com>
    >
    
    Updated using 0.0.post124 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,39 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9847"
-version_tuple = (0, 0, 9847)
+version_str = "0.0.post9848"
+version_tuple = (0, 0, 9848)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9847")
+    pversion = V("0.0.post9848")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9723"
-data_version_tuple = (0, 0, 9723)
+data_version_str = "0.0.post9724"
+data_version_tuple = (0, 0, 9724)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9723")
+    pdata_version = V("0.0.post9724")
 except ImportError:
     pass
-data_git_hash = "79d1e7a4d3e48a817ffa7a5eaa28b07e16969bf0"
-data_git_describe = "v0.0-9723-g79d1e7a4d"
+data_git_hash = "8a35814ef0db693f67549738f46087d57dcb5251"
+data_git_describe = "v0.0-9724-g8a35814ef"
 data_git_msg = """\
-commit 79d1e7a4d3e48a817ffa7a5eaa28b07e16969bf0
-Author: Timothy Trippel <ttrippel@google.com>
-Date:   Mon Jan 24 23:48:35 2022 -0800
+commit 8a35814ef0db693f67549738f46087d57dcb5251
+Author: Drew Macrae <drewmacrae@google.com>
+Date:   Wed Jan 26 07:20:04 2022 -0800
 
-    [dif/alert_handler] Add static asserts for multireg layouts.
+    [bazel] start running in CI and warning
     
-    Several CSRs for both alerts and local alerts are organized as
-    multiregs, enabling easy DIF development. However, there is currently no
-    mechanism to indicate of the multireg layout of said CSRs ever changes.
-    This commit adds static asserts to check these multireg layouts.
-    
-    This partially addresses a task in #9899.
-    
-    Signed-off-by: Timothy Trippel <ttrippel@google.com>
+    Signed-off-by: Drew Macrae <drewmacrae@google.com>
 
 """
 
