504ee53f28f826e5aa630e096e0bf3fba744bbcd
LiteX Robot <robot@timvideos.us>
2022-01-27 03:27:47 +0000

    Updating pythondata-misc-opentitan to 0.0.post9828
    Updated data to v0.0-9706-ga993c4b23 based on a993c4b23d936609e91b1fbea6d8a3d4001d9bea from https://github.com/lowRISC/opentitan.
    > commit a993c4b23d936609e91b1fbea6d8a3d4001d9bea
    > Author: Pirmin Vogel <vogelpi@lowrisc.org>
    > Date:   Tue Jan 25 19:10:19 2022 +0100
    >
    >     [aes] Fix AscentLint errors
    >
    >     Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
    >
    
    Updated using 0.0.post122 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,39 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9827"
-version_tuple = (0, 0, 9827)
+version_str = "0.0.post9828"
+version_tuple = (0, 0, 9828)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9827")
+    pversion = V("0.0.post9828")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9705"
-data_version_tuple = (0, 0, 9705)
+data_version_str = "0.0.post9706"
+data_version_tuple = (0, 0, 9706)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9705")
+    pdata_version = V("0.0.post9706")
 except ImportError:
     pass
-data_git_hash = "a07ba27d992d79b2e77480523232e5e1eadab35f"
-data_git_describe = "v0.0-9705-ga07ba27d9"
+data_git_hash = "a993c4b23d936609e91b1fbea6d8a3d4001d9bea"
+data_git_describe = "v0.0-9706-ga993c4b23"
 data_git_msg = """\
-commit a07ba27d992d79b2e77480523232e5e1eadab35f
-Author: Guillermo Maturana <maturana@google.com>
-Date:   Tue Jan 25 08:15:04 2022 -0800
+commit a993c4b23d936609e91b1fbea6d8a3d4001d9bea
+Author: Pirmin Vogel <vogelpi@lowrisc.org>
+Date:   Tue Jan 25 19:10:19 2022 +0100
 
-    [sw,tests,pwrmgr] Improve synchronization
+    [aes] Fix AscentLint errors
     
-    Allow each CSR updates that need to transfer to the slow domain to make
-    synchronization optional.
-    This enables entry into low power to use a single synchronization when
-    it is done with the last update.
-    Update pwrmgr unittests for this change.
-    Change some tests to use pwrmgr_testutils_enable_low_power.
-    
-    Signed-off-by: Guillermo Maturana <maturana@google.com>
+    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
 
 """
 
