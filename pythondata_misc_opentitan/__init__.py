2c7b6afa0000df41c2640d40dde8a123c1916a4e
LiteX Robot <robot@timvideos.us>
2022-01-27 19:22:14 +0000

    Updating pythondata-misc-opentitan to 0.0.post9846
    Updated data to v0.0-9722-ga1ba95ee3 based on a1ba95ee32a5e77992da21b0d8230af8e5f7112f from https://github.com/lowRISC/opentitan.
    > commit a1ba95ee32a5e77992da21b0d8230af8e5f7112f
    > Author: Alexander Williams <awill@google.com>
    > Date:   Thu Jan 20 19:24:36 2022 -0800
    >
    >     [usbdev] Move sense to an MIO
    >
    >     The ASIC was missing a connection for the VBUS detection, so the MIO
    >     adds that. In addition, the FPGAs connect the two VBUS detection sources
    >     to IOR0 and IOR1--Software will have to select the correct I/O based on
    >     the `use_uphy` value.
    >
    >     Moving to an MIO brings straightforward options for a simple constant
    >     via periph_insel if OT is completely bus-powered, in addition to
    >     making it available to both power domains for usbdev.
    >
    >     Signed-off-by: Alexander Williams <awill@google.com>
    >
    
    Updated using 0.0.post124 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,41 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9844"
-version_tuple = (0, 0, 9844)
+version_str = "0.0.post9846"
+version_tuple = (0, 0, 9846)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9844")
+    pversion = V("0.0.post9846")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9720"
-data_version_tuple = (0, 0, 9720)
+data_version_str = "0.0.post9722"
+data_version_tuple = (0, 0, 9722)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9720")
+    pdata_version = V("0.0.post9722")
 except ImportError:
     pass
-data_git_hash = "b282937c479842af7f87a4a407d3dac7a0ba7faa"
-data_git_describe = "v0.0-9720-gb282937c4"
+data_git_hash = "a1ba95ee32a5e77992da21b0d8230af8e5f7112f"
+data_git_describe = "v0.0-9722-ga1ba95ee3"
 data_git_msg = """\
-commit b282937c479842af7f87a4a407d3dac7a0ba7faa
-Author: Jade Philipoom <jadep@google.com>
-Date:   Thu Jan 27 08:58:42 2022 +0000
+commit a1ba95ee32a5e77992da21b0d8230af8e5f7112f
+Author: Alexander Williams <awill@google.com>
+Date:   Thu Jan 20 19:24:36 2022 -0800
 
-    [sw/silicon_creator] Remove dedicated OTBN error type.
+    [usbdev] Move sense to an MIO
     
-    Replace otbn_error_t with a few new cases in rom_error_t.
+    The ASIC was missing a connection for the VBUS detection, so the MIO
+    adds that. In addition, the FPGAs connect the two VBUS detection sources
+    to IOR0 and IOR1--Software will have to select the correct I/O based on
+    the `use_uphy` value.
     
-    Signed-off-by: Jade Philipoom <jadep@google.com>
+    Moving to an MIO brings straightforward options for a simple constant
+    via periph_insel if OT is completely bus-powered, in addition to
+    making it available to both power domains for usbdev.
+    
+    Signed-off-by: Alexander Williams <awill@google.com>
 
 """
 
