c39ab5eaf4b2f7da8c482bf44ff94f1f72a2f1ce
LiteX Robot <robot@timvideos.us>
2022-01-29 03:26:37 +0000

    Updating pythondata-misc-opentitan to 0.0.post9902
    Updated data to v0.0-9778-g3b12971e3 based on 3b12971e3786ca29ae12a6f5e40bcc98232d49c6 from https://github.com/lowRISC/opentitan.
    > commit 3b12971e3786ca29ae12a6f5e40bcc98232d49c6
    > Author: Michael Schaffner <msf@opentitan.org>
    > Date:   Thu Jan 27 20:01:09 2022 -0800
    >
    >     [otp_ctrl/doc] Update DAI FSM diagram
    >
    >     Signed-off-by: Michael Schaffner <msf@opentitan.org>
    >
    
    Updated using 0.0.post124 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9888"
-version_tuple = (0, 0, 9888)
+version_str = "0.0.post9902"
+version_tuple = (0, 0, 9902)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9888")
+    pversion = V("0.0.post9902")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9764"
-data_version_tuple = (0, 0, 9764)
+data_version_str = "0.0.post9778"
+data_version_tuple = (0, 0, 9778)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9764")
+    pdata_version = V("0.0.post9778")
 except ImportError:
     pass
-data_git_hash = "b8a1093b1de25ff7f84c6b9dc58bbc384815aeaa"
-data_git_describe = "v0.0-9764-gb8a1093b1"
+data_git_hash = "3b12971e3786ca29ae12a6f5e40bcc98232d49c6"
+data_git_describe = "v0.0-9778-g3b12971e3"
 data_git_msg = """\
-commit b8a1093b1de25ff7f84c6b9dc58bbc384815aeaa
-Author: Timothy Chen <timothytim@google.com>
-Date:   Thu Jan 27 20:47:28 2022 -0800
+commit 3b12971e3786ca29ae12a6f5e40bcc98232d49c6
+Author: Michael Schaffner <msf@opentitan.org>
+Date:   Thu Jan 27 20:01:09 2022 -0800
 
-    [keymgr] Convert configuration to shadow register
+    [otp_ctrl/doc] Update DAI FSM diagram
     
-    - split start from configuration since shadow registers do now support hrw
-    - this is part of keymgr d2s likely review items
-    
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Michael Schaffner <msf@opentitan.org>
 
 """
 
