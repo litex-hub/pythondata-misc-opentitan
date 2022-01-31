413cc556bbb080880e0fb7f181cadebf32a2611d
LiteX Robot <robot@timvideos.us>
2022-01-31 15:28:30 +0000

    Updating pythondata-misc-opentitan to 0.0.post9921
    Updated data to v0.0-9797-g8cb33a7b3 based on 8cb33a7b336adee07424238215157e0539833df4 from https://github.com/lowRISC/opentitan.
    > commit 8cb33a7b336adee07424238215157e0539833df4
    > Author: David Pudner <davidpudner@protonmail.com>
    > Date:   Wed Jan 26 10:49:28 2022 +0000
    >
    >     [hw/dv] Changed set_response_queue to be umbounded due to queue overflow issues in Questa
    >
    >     As discussed in issue 9514 Questa was running into queue overflow issues when running uart_stress_all_with_rand_reset tests.
    >     This is due to the driver put happening earlier than the sequence item get. It was aggreed that the check could be changed to be umbounded. The test now passes.
    >
    >     Signed-off-by: David Pudner <davidpudner@protonmail.com>
    >
    
    Updated using 0.0.post124 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9920"
-version_tuple = (0, 0, 9920)
+version_str = "0.0.post9921"
+version_tuple = (0, 0, 9921)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9920")
+    pversion = V("0.0.post9921")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9796"
-data_version_tuple = (0, 0, 9796)
+data_version_str = "0.0.post9797"
+data_version_tuple = (0, 0, 9797)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9796")
+    pdata_version = V("0.0.post9797")
 except ImportError:
     pass
-data_git_hash = "442d3deebe4f5ed32960f9260e0b5029bcb06037"
-data_git_describe = "v0.0-9796-g442d3deeb"
+data_git_hash = "8cb33a7b336adee07424238215157e0539833df4"
+data_git_describe = "v0.0-9797-g8cb33a7b3"
 data_git_msg = """\
-commit 442d3deebe4f5ed32960f9260e0b5029bcb06037
-Author: Greg Chadwick <gac@lowrisc.org>
-Date:   Fri Jan 28 17:18:08 2022 +0000
+commit 8cb33a7b336adee07424238215157e0539833df4
+Author: David Pudner <davidpudner@protonmail.com>
+Date:   Wed Jan 26 10:49:28 2022 +0000
 
-    [ci] Add script to dynamically choose VCS_PARALLEL_BUILDS
+    [hw/dv] Changed set_response_queue to be umbounded due to queue overflow issues in Questa
     
-    Signed-off-by: Greg Chadwick <gac@lowrisc.org>
+    As discussed in issue 9514 Questa was running into queue overflow issues when running uart_stress_all_with_rand_reset tests.
+    This is due to the driver put happening earlier than the sequence item get. It was aggreed that the check could be changed to be umbounded. The test now passes.
+    
+    Signed-off-by: David Pudner <davidpudner@protonmail.com>
 
 """
 
