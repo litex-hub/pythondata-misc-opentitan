065ec058a2db222120e7367f3ffa94b70b9cc077
LiteX Robot <robot@timvideos.us>
2022-02-01 20:27:13 +0000

    Updating pythondata-misc-opentitan to 0.0.post9975
    Updated data to v0.0-9851-g040994401 based on 0409944012596a05d679a976fa2510a5c73ad781 from https://github.com/lowRISC/opentitan.
    > commit 0409944012596a05d679a976fa2510a5c73ad781
    > Author: Michael Schaffner <msf@google.com>
    > Date:   Tue Feb 1 03:35:44 2022 +0000
    >
    >     [fpga] Make divided clock constraint more precise
    >
    >     Signed-off-by: Michael Schaffner <msf@google.com>
    >
    
    Updated using 0.0.post124 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9972"
-version_tuple = (0, 0, 9972)
+version_str = "0.0.post9975"
+version_tuple = (0, 0, 9975)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9972")
+    pversion = V("0.0.post9975")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9848"
-data_version_tuple = (0, 0, 9848)
+data_version_str = "0.0.post9851"
+data_version_tuple = (0, 0, 9851)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9848")
+    pdata_version = V("0.0.post9851")
 except ImportError:
     pass
-data_git_hash = "bd1bae0e7174e2df7306cc10dc370bb7782268ae"
-data_git_describe = "v0.0-9848-gbd1bae0e7"
+data_git_hash = "0409944012596a05d679a976fa2510a5c73ad781"
+data_git_describe = "v0.0-9851-g040994401"
 data_git_msg = """\
-commit bd1bae0e7174e2df7306cc10dc370bb7782268ae
-Author: Greg Chadwick <gac@lowrisc.org>
-Date:   Mon Jan 31 13:32:28 2022 +0000
+commit 0409944012596a05d679a976fa2510a5c73ad781
+Author: Michael Schaffner <msf@google.com>
+Date:   Tue Feb 1 03:35:44 2022 +0000
 
-    [rv_dm] Move to D2
+    [fpga] Make divided clock constraint more precise
     
-    Signed-off-by: Greg Chadwick <gac@lowrisc.org>
+    Signed-off-by: Michael Schaffner <msf@google.com>
 
 """
 
