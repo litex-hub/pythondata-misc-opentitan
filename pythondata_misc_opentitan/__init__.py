a9abbe698f5001c604b220385a714c6ed327da8a
LiteX Robot <robot@timvideos.us>
2022-02-01 01:23:08 +0000

    Updating pythondata-misc-opentitan to 0.0.post9928
    Updated data to v0.0-9804-g20c314947 based on 20c314947936d044071f2820570c22d9f3ba913f from https://github.com/lowRISC/opentitan.
    > commit 20c314947936d044071f2820570c22d9f3ba913f
    > Author: Srikrishna Iyer <sriyer@google.com>
    > Date:   Mon Jan 31 12:17:57 2022 -0800
    >
    >     [dv, mem_bkdr_util] Fix ECC-computed backdoor WRs
    >
    >     - Minor enhancement to 'global' mem modification methods
    >       (set|clear|randomize|invalidate_mem()) - manipulate the entire array
    >       rather than go byte-by-byte, which is computationally much slower
    >     - It also fixes the flash case which has the unaddressable metadata bits
    >       (first level ECC) at [67:64].
    >     - Refactor the ECC computation into its own function for reuse and
    >       extension.
    >     - Extend the class for flash customization - for flash, override the
    >       `get_ecc_computed_data()` to perform 2 level ECC. Likewise, override
    >       the randomize_mem() to similar effect.
    >
    >     Signed-off-by: Srikrishna Iyer <sriyer@google.com>
    >
    
    Updated using 0.0.post124 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,43 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9925"
-version_tuple = (0, 0, 9925)
+version_str = "0.0.post9928"
+version_tuple = (0, 0, 9928)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9925")
+    pversion = V("0.0.post9928")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9801"
-data_version_tuple = (0, 0, 9801)
+data_version_str = "0.0.post9804"
+data_version_tuple = (0, 0, 9804)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9801")
+    pdata_version = V("0.0.post9804")
 except ImportError:
     pass
-data_git_hash = "a45456ab3434369c06d62bc249970e5a8669bd70"
-data_git_describe = "v0.0-9801-ga45456ab3"
+data_git_hash = "20c314947936d044071f2820570c22d9f3ba913f"
+data_git_describe = "v0.0-9804-g20c314947"
 data_git_msg = """\
-commit a45456ab3434369c06d62bc249970e5a8669bd70
-Author: Jade Philipoom <jadep@google.com>
-Date:   Mon Jan 17 10:25:16 2022 +0000
+commit 20c314947936d044071f2820570c22d9f3ba913f
+Author: Srikrishna Iyer <sriyer@google.com>
+Date:   Mon Jan 31 12:17:57 2022 -0800
 
-    [sw,crypto] Reject large signatures in RSA verification.
+    [dv, mem_bkdr_util] Fix ECC-computed backdoor WRs
     
-    Modify sigverify and the crypto library RSA implementation to check that
-    signatures are within bounds and reject them otherwise.
+    - Minor enhancement to 'global' mem modification methods
+      (set|clear|randomize|invalidate_mem()) - manipulate the entire array
+      rather than go byte-by-byte, which is computationally much slower
+    - It also fixes the flash case which has the unaddressable metadata bits
+      (first level ECC) at [67:64].
+    - Refactor the ECC computation into its own function for reuse and
+      extension.
+    - Extend the class for flash customization - for flash, override the
+      `get_ecc_computed_data()` to perform 2 level ECC. Likewise, override
+      the randomize_mem() to similar effect.
     
-    Signed-off-by: Jade Philipoom <jadep@google.com>
+    Signed-off-by: Srikrishna Iyer <sriyer@google.com>
 
 """
 
