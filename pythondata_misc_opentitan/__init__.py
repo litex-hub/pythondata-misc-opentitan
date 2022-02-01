725c533a14e5d1896d33c372d396aec7532b5e63
LiteX Robot <robot@timvideos.us>
2022-02-01 17:30:27 +0000

    Updating pythondata-misc-opentitan to 0.0.post9967
    Updated data to v0.0-9843-gc02272adb based on c02272adbfb16b67e53b93082b4fb8dec5e29858 from https://github.com/lowRISC/opentitan.
    > commit c02272adbfb16b67e53b93082b4fb8dec5e29858
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Mon Jan 31 14:35:00 2022 -0800
    >
    >     [dv/kmac] fifo empty interrupt behavior
    >
    >     This PR aligns a fifo empty interrupt related behavior:
    >     If the read and write pointers are equal, and next clock cycle a write
    >     is issued and accepted, the fifo empty interrupt won't fire.
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post124 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,43 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9964"
-version_tuple = (0, 0, 9964)
+version_str = "0.0.post9967"
+version_tuple = (0, 0, 9967)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9964")
+    pversion = V("0.0.post9967")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9840"
-data_version_tuple = (0, 0, 9840)
+data_version_str = "0.0.post9843"
+data_version_tuple = (0, 0, 9843)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9840")
+    pdata_version = V("0.0.post9843")
 except ImportError:
     pass
-data_git_hash = "315e2f1b3fd1ff35061021e40c8eddc12c443c9b"
-data_git_describe = "v0.0-9840-g315e2f1b3"
+data_git_hash = "c02272adbfb16b67e53b93082b4fb8dec5e29858"
+data_git_describe = "v0.0-9843-gc02272adb"
 data_git_msg = """\
-commit 315e2f1b3fd1ff35061021e40c8eddc12c443c9b
-Author: Michael Schaffner <msf@opentitan.org>
-Date:   Mon Jan 31 14:45:27 2022 -0800
+commit c02272adbfb16b67e53b93082b4fb8dec5e29858
+Author: Cindy Chen <chencindy@opentitan.org>
+Date:   Mon Jan 31 14:35:00 2022 -0800
 
-    [lc_ctrl] Beef up internal usage of redundant CSR encoding
+    [dv/kmac] fifo empty interrupt behavior
     
-    The replicated enum encoding of the LC_STATE and TRANSITION_TARGET CSRs
-    has been added in order to ease hardening on the firmware side.
+    This PR aligns a fifo empty interrupt related behavior:
+    If the read and write pointers are equal, and next clock cycle a write
+    is issued and accepted, the fifo empty interrupt won't fire.
     
-    This patch lists this as an explicit countermeasure, and beefs up the
-    internal usage of this encoding. I.e., the transition logic only checked
-    the TRANSITION_TARGET encoding coming from the CSRs before. With this
-    change, the logic now also makes use of the redundancy present in the
-    decoded state vector. This is useful in particular when indexing the
-    transition matrix LUT (to get the valid bits and token indices), since
-    this indexing operation can now be duplicated.
-    
-    Signed-off-by: Michael Schaffner <msf@opentitan.org>
+    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
 """
 
