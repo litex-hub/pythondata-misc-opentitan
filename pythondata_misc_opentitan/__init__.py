454b89265755e63495cdfa18255b80541c4ba520
LiteX Robot <robot@timvideos.us>
2022-02-01 02:42:24 +0000

    Updating pythondata-misc-opentitan to 0.0.post9931
    Updated data to v0.0-9807-g9ab907b98 based on 9ab907b98e275f5bb86f06add43e280b4eb234c6 from https://github.com/lowRISC/opentitan.
    > commit 9ab907b98e275f5bb86f06add43e280b4eb234c6
    > Author: Nikola Miladinovic <nikola.miladinovic@ensilica.com>
    > Date:   Thu Jan 13 14:30:20 2022 +0000
    >
    >     [flash_ctrl] ADD TEST FOR HOST DIRECT READ DATA
    >
    >     Add flash_ctrl_host_rd_dir_test. Add randomization
    >     for scramble,ecc and he. Add checking host direct read
    >     data transactions in scoreboard when scramble is disabled.
    >
    >     Signed-off-by: Nikola Miladinovic <nikola.miladinovic@ensilica.com>
    >
    
    Updated using 0.0.post124 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,43 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post9928"
-version_tuple = (0, 0, 9928)
+version_str = "0.0.post9931"
+version_tuple = (0, 0, 9931)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post9928")
+    pversion = V("0.0.post9931")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9804"
-data_version_tuple = (0, 0, 9804)
+data_version_str = "0.0.post9807"
+data_version_tuple = (0, 0, 9807)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9804")
+    pdata_version = V("0.0.post9807")
 except ImportError:
     pass
-data_git_hash = "20c314947936d044071f2820570c22d9f3ba913f"
-data_git_describe = "v0.0-9804-g20c314947"
+data_git_hash = "9ab907b98e275f5bb86f06add43e280b4eb234c6"
+data_git_describe = "v0.0-9807-g9ab907b98"
 data_git_msg = """\
-commit 20c314947936d044071f2820570c22d9f3ba913f
-Author: Srikrishna Iyer <sriyer@google.com>
-Date:   Mon Jan 31 12:17:57 2022 -0800
+commit 9ab907b98e275f5bb86f06add43e280b4eb234c6
+Author: Nikola Miladinovic <nikola.miladinovic@ensilica.com>
+Date:   Thu Jan 13 14:30:20 2022 +0000
 
-    [dv, mem_bkdr_util] Fix ECC-computed backdoor WRs
+    [flash_ctrl] ADD TEST FOR HOST DIRECT READ DATA
     
-    - Minor enhancement to 'global' mem modification methods
-      (set|clear|randomize|invalidate_mem()) - manipulate the entire array
-      rather than go byte-by-byte, which is computationally much slower
-    - It also fixes the flash case which has the unaddressable metadata bits
-      (first level ECC) at [67:64].
-    - Refactor the ECC computation into its own function for reuse and
-      extension.
-    - Extend the class for flash customization - for flash, override the
-      `get_ecc_computed_data()` to perform 2 level ECC. Likewise, override
-      the randomize_mem() to similar effect.
+    Add flash_ctrl_host_rd_dir_test. Add randomization
+    for scramble,ecc and he. Add checking host direct read
+    data transactions in scoreboard when scramble is disabled.
     
-    Signed-off-by: Srikrishna Iyer <sriyer@google.com>
+    Signed-off-by: Nikola Miladinovic <nikola.miladinovic@ensilica.com>
 
 """
 
