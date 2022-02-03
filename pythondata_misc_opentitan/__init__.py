ac6c35c1db8627fbceecab470af830f7b64e6004
LiteX Robot <robot@timvideos.us>
2022-02-03 23:24:03 +0000

    Updating pythondata-misc-opentitan to 0.0.post10061
    Updated data to v0.0-9937-g00a2a30c6 based on 00a2a30c6cb4d292b1364d86433e050427cf6f22 from https://github.com/lowRISC/opentitan.
    > commit 00a2a30c6cb4d292b1364d86433e050427cf6f22
    > Author: Eunchan Kim <eunchan@opentitan.org>
    > Date:   Tue Feb 1 09:49:56 2022 -0800
    >
    >     [kmac] Fix Fatal Alert connection
    >
    >     @m-temp reported incorrect connection for the fatal alert in #10516.
    >     The alert_sender modules are instantiated in opposit way.
    >
    >     Signed-off-by: Eunchan Kim <eunchan@opentitan.org>
    >
    
    Updated using 0.0.post124 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,38 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10054"
-version_tuple = (0, 0, 10054)
+version_str = "0.0.post10061"
+version_tuple = (0, 0, 10061)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10054")
+    pversion = V("0.0.post10061")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9930"
-data_version_tuple = (0, 0, 9930)
+data_version_str = "0.0.post9937"
+data_version_tuple = (0, 0, 9937)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9930")
+    pdata_version = V("0.0.post9937")
 except ImportError:
     pass
-data_git_hash = "cc8ad6a1db0afdbbe3cd147bc63fda7e9e26ef6e"
-data_git_describe = "v0.0-9930-gcc8ad6a1d"
+data_git_hash = "00a2a30c6cb4d292b1364d86433e050427cf6f22"
+data_git_describe = "v0.0-9937-g00a2a30c6"
 data_git_msg = """\
-commit cc8ad6a1db0afdbbe3cd147bc63fda7e9e26ef6e
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Thu Feb 3 17:07:49 2022 +0000
+commit 00a2a30c6cb4d292b1364d86433e050427cf6f22
+Author: Eunchan Kim <eunchan@opentitan.org>
+Date:   Tue Feb 1 09:49:56 2022 -0800
 
-    [otbn,dv] Wait to come out of reset in the stress_all vseq
+    [kmac] Fix Fatal Alert connection
     
-    This sequence is used by otbn_stress_all_with_rand_reset. The
-    sequences that otbn_stress_all_vseq calls all exit immediately when
-    the device goes into reset. We need to make sure we don't immediately
-    start another sequence (when we're still in reset) because that would,
-    in turn exit immediately.
+    @m-temp reported incorrect connection for the fatal alert in #10516.
+    The alert_sender modules are instantiated in opposit way.
     
-    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>
 
 """
 
