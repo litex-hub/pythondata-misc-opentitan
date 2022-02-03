6ea45bdffe2453785972b575e509df982eee4536
LiteX Robot <robot@timvideos.us>
2022-02-03 18:30:38 +0000

    Updating pythondata-misc-opentitan to 0.0.post10045
    Updated data to v0.0-9921-gfc31f2be8 based on fc31f2be84afcaee9c1450f770525e966e6d4e11 from https://github.com/lowRISC/opentitan.
    > commit fc31f2be84afcaee9c1450f770525e966e6d4e11
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Wed Feb 2 17:24:41 2022 -0800
    >
    >     [dv] Fixes to enable foundry database pwrmgr_smoketest
    >
    >     - The old sram randomization scheme did not work because it treated each
    >       tile as an independent memory.  This meant if the address scramble caused
    >       a word to be relocated to another tile, the write would not work correctly.
    >
    >     - This PR addes some enhancements to calculate the target tile and ensures the
    >       scrmabled data is written to the right place.
    >
    >     - A few helper functions are added to mem_bkdr_util__sram.sv, these should be
    >       used locally in the other functions as well to reduce code duplication.
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post124 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,30 +4,40 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10044"
-version_tuple = (0, 0, 10044)
+version_str = "0.0.post10045"
+version_tuple = (0, 0, 10045)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10044")
+    pversion = V("0.0.post10045")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9920"
-data_version_tuple = (0, 0, 9920)
+data_version_str = "0.0.post9921"
+data_version_tuple = (0, 0, 9921)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9920")
+    pdata_version = V("0.0.post9921")
 except ImportError:
     pass
-data_git_hash = "a245d27de13d69c31eb7d1eadfa62e243f2f1c34"
-data_git_describe = "v0.0-9920-ga245d27de"
+data_git_hash = "fc31f2be84afcaee9c1450f770525e966e6d4e11"
+data_git_describe = "v0.0-9921-gfc31f2be8"
 data_git_msg = """\
-commit a245d27de13d69c31eb7d1eadfa62e243f2f1c34
+commit fc31f2be84afcaee9c1450f770525e966e6d4e11
 Author: Timothy Chen <timothytim@google.com>
-Date:   Wed Feb 2 16:52:04 2022 -0800
+Date:   Wed Feb 2 17:24:41 2022 -0800
 
-    [keymgr] Remove unnecessary line in doc
+    [dv] Fixes to enable foundry database pwrmgr_smoketest
+    
+    - The old sram randomization scheme did not work because it treated each
+      tile as an independent memory.  This meant if the address scramble caused
+      a word to be relocated to another tile, the write would not work correctly.
+    
+    - This PR addes some enhancements to calculate the target tile and ensures the
+      scrmabled data is written to the right place.
+    
+    - A few helper functions are added to mem_bkdr_util__sram.sv, these should be
+      used locally in the other functions as well to reduce code duplication.
     
     Signed-off-by: Timothy Chen <timothytim@google.com>
 
