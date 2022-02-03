a133171f493ac606ddd535013aa8e9f5a2102abf
LiteX Robot <robot@timvideos.us>
2022-02-03 17:28:34 +0000

    Updating pythondata-misc-opentitan to 0.0.post10044
    Updated data to v0.0-9920-ga245d27de based on a245d27de13d69c31eb7d1eadfa62e243f2f1c34 from https://github.com/lowRISC/opentitan.
    > commit a245d27de13d69c31eb7d1eadfa62e243f2f1c34
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Wed Feb 2 16:52:04 2022 -0800
    >
    >     [keymgr] Remove unnecessary line in doc
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post124 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10039"
-version_tuple = (0, 0, 10039)
+version_str = "0.0.post10044"
+version_tuple = (0, 0, 10044)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10039")
+    pversion = V("0.0.post10044")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9915"
-data_version_tuple = (0, 0, 9915)
+data_version_str = "0.0.post9920"
+data_version_tuple = (0, 0, 9920)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9915")
+    pdata_version = V("0.0.post9920")
 except ImportError:
     pass
-data_git_hash = "262651b7db24d593b5ac697b6701da0ed3123628"
-data_git_describe = "v0.0-9915-g262651b7d"
+data_git_hash = "a245d27de13d69c31eb7d1eadfa62e243f2f1c34"
+data_git_describe = "v0.0-9920-ga245d27de"
 data_git_msg = """\
-commit 262651b7db24d593b5ac697b6701da0ed3123628
-Author: Eunchan Kim <eunchan@opentitan.org>
-Date:   Wed Feb 2 18:31:29 2022 -0800
+commit a245d27de13d69c31eb7d1eadfa62e243f2f1c34
+Author: Timothy Chen <timothytim@google.com>
+Date:   Wed Feb 2 16:52:04 2022 -0800
 
-    [gpio] Waive Reconvergence CDC warning
+    [keymgr] Remove unnecessary line in doc
     
-    As GPIO inputs are not assumed as a bus signal, it is OK to just 2FF
-    PAD_IN values.
-    
-    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
