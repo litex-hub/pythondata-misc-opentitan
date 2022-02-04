a06559438a7e2561254f7386399e0fb758806b0c
LiteX Robot <robot@timvideos.us>
2022-02-04 20:24:35 +0000

    Updating pythondata-misc-opentitan to 0.0.post10083
    Updated data to v0.0-9959-gd255d45f2 based on d255d45f29a6af156a2df2a10e5e53aad777fde4 from https://github.com/lowRISC/opentitan.
    > commit d255d45f29a6af156a2df2a10e5e53aad777fde4
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Fri Feb 4 00:12:57 2022 -0800
    >
    >     [dv/otp_ctrl] Update UNR with blackbox
    >
    >     This PR updates the UNR module with security modules being blackboxed.
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post124 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,42 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10082"
-version_tuple = (0, 0, 10082)
+version_str = "0.0.post10083"
+version_tuple = (0, 0, 10083)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10082")
+    pversion = V("0.0.post10083")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9958"
-data_version_tuple = (0, 0, 9958)
+data_version_str = "0.0.post9959"
+data_version_tuple = (0, 0, 9959)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9958")
+    pdata_version = V("0.0.post9959")
 except ImportError:
     pass
-data_git_hash = "ca6dabfa85d0c8702f9d99f057a3778fb96f2637"
-data_git_describe = "v0.0-9958-gca6dabfa8"
+data_git_hash = "d255d45f29a6af156a2df2a10e5e53aad777fde4"
+data_git_describe = "v0.0-9959-gd255d45f2"
 data_git_msg = """\
-commit ca6dabfa85d0c8702f9d99f057a3778fb96f2637
-Author: Mark Branstad <mark.branstad@wdc.com>
-Date:   Mon Jan 17 14:57:46 2022 -0800
+commit d255d45f29a6af156a2df2a10e5e53aad777fde4
+Author: Cindy Chen <chencindy@opentitan.org>
+Date:   Fri Feb 4 00:12:57 2022 -0800
 
-    [csrng/rtl] d2s review updates
+    [dv/otp_ctrl] Update UNR with blackbox
     
-    RTL and documentation updates based on D2S review feedback.
+    This PR updates the UNR module with security modules being blackboxed.
     
-    Signed-off-by: Mark Branstad <mark.branstad@wdc.com>
-    
-    [csrng/rtl] D2S review items update
-    
-    Based on feedback from the D2S review, several items were updated
-    in the PR for the csrng entropy block.
-    See #10095 for details.
-    
-    Signed-off-by: Mark Branstad <mark.branstad@wdc.com>
+    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
 """
 
