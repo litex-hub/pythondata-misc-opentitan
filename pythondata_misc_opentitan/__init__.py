6aac1566e22401d7b3dd33599d575ba01b8d137d
LiteX Robot <robot@timvideos.us>
2022-02-04 06:26:33 +0000

    Updating pythondata-misc-opentitan to 0.0.post10071
    Updated data to v0.0-9947-g180c2536c based on 180c2536cd79881638b601e6a9857913dcd87888 from https://github.com/lowRISC/opentitan.
    > commit 180c2536cd79881638b601e6a9857913dcd87888
    > Author: Weicai Yang <weicai@google.com>
    > Date:   Thu Feb 3 15:40:18 2022 -0800
    >
    >     [dv] Address 2 comments in sec_cm update PR
    >
    >     For PR #10539
    >     1. removed unused CntStyle
    >     2. moved sec_cm_alert_name check to the top of the seq
    >
    >     Signed-off-by: Weicai Yang <weicai@google.com>
    >
    
    Updated using 0.0.post124 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10064"
-version_tuple = (0, 0, 10064)
+version_str = "0.0.post10071"
+version_tuple = (0, 0, 10071)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10064")
+    pversion = V("0.0.post10071")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9940"
-data_version_tuple = (0, 0, 9940)
+data_version_str = "0.0.post9947"
+data_version_tuple = (0, 0, 9947)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9940")
+    pdata_version = V("0.0.post9947")
 except ImportError:
     pass
-data_git_hash = "edbd0a0a8dc42d82ad6d36928aad0c7e51eac70d"
-data_git_describe = "v0.0-9940-gedbd0a0a8"
+data_git_hash = "180c2536cd79881638b601e6a9857913dcd87888"
+data_git_describe = "v0.0-9947-g180c2536c"
 data_git_msg = """\
-commit edbd0a0a8dc42d82ad6d36928aad0c7e51eac70d
-Author: Cindy Chen <chencindy@opentitan.org>
-Date:   Thu Feb 3 15:49:08 2022 -0800
+commit 180c2536cd79881638b601e6a9857913dcd87888
+Author: Weicai Yang <weicai@google.com>
+Date:   Thu Feb 3 15:40:18 2022 -0800
 
-    [foundry/CI] Fix close source OTP smoke test timeout
+    [dv] Address 2 comments in sec_cm update PR
     
-    Close source write takes long simulation time so to avoid timeout, we
-    issue less read/write if it is CI check.
+    For PR #10539
+    1. removed unused CntStyle
+    2. moved sec_cm_alert_name check to the top of the seq
     
-    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
+    Signed-off-by: Weicai Yang <weicai@google.com>
 
 """
 
