94dc0e6e6e51bb651443d3c0c6d104da216d3e51
LiteX Robot <robot@timvideos.us>
2022-02-04 16:33:23 +0000

    Updating pythondata-misc-opentitan to 0.0.post10079
    Updated data to v0.0-9955-g6d12a3861 based on 6d12a3861c6db1caea19277178200077569d3e69 from https://github.com/lowRISC/opentitan.
    > commit 6d12a3861c6db1caea19277178200077569d3e69
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Thu Feb 3 17:18:52 2022 -0800
    >
    >     [entropy_src] related software updates
    >
    >     - update disable register write
    >     - update expected values
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post124 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,40 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10076"
-version_tuple = (0, 0, 10076)
+version_str = "0.0.post10079"
+version_tuple = (0, 0, 10079)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10076")
+    pversion = V("0.0.post10079")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9952"
-data_version_tuple = (0, 0, 9952)
+data_version_str = "0.0.post9955"
+data_version_tuple = (0, 0, 9955)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9952")
+    pdata_version = V("0.0.post9955")
 except ImportError:
     pass
-data_git_hash = "e1e90cdc36b1b4003926212f04ea54eb62ad3044"
-data_git_describe = "v0.0-9952-ge1e90cdc3"
+data_git_hash = "6d12a3861c6db1caea19277178200077569d3e69"
+data_git_describe = "v0.0-9955-g6d12a3861"
 data_git_msg = """\
-commit e1e90cdc36b1b4003926212f04ea54eb62ad3044
-Author: Pirmin Vogel <vogelpi@lowrisc.org>
-Date:   Fri Feb 4 10:01:32 2022 +0100
+commit 6d12a3861c6db1caea19277178200077569d3e69
+Author: Timothy Chen <timothytim@google.com>
+Date:   Thu Feb 3 17:18:52 2022 -0800
 
-    [aes] Update sideload key on second write to the shadowed ctrl reg only
+    [entropy_src] related software updates
     
-    Updating the sideload key on every write to the shadow register can
-    trigger PRNG reseed operations in the middle of a control register
-    update complicating software and DV. For this reason, this commit
-    forwards the phase of the shadow register to the main controller to
-    only trigger the sideload key update on the second write.
+    - update disable register write
+    - update expected values
     
-    This is related to lowRISC/OpenTitan#10518.
-    
-    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
