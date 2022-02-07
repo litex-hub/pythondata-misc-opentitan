785db5b39e178568726fd083c8b12821a6272cda
LiteX Robot <robot@timvideos.us>
2022-02-07 19:21:25 +0000

    Updating pythondata-misc-opentitan to 0.0.post10103
    Updated data to v0.0-9979-g2babd940e based on 2babd940e9b65794cabd1351c112e7b016475d57 from https://github.com/lowRISC/opentitan.
    > commit 2babd940e9b65794cabd1351c112e7b016475d57
    > Author: Michael Schaffner <msf@opentitan.org>
    > Date:   Fri Feb 4 20:19:18 2022 -0800
    >
    >     [sysrst_ctrl] Add some more details regarding combo actions
    >
    >     Signed-off-by: Michael Schaffner <msf@opentitan.org>
    >
    
    Updated using 0.0.post124 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10099"
-version_tuple = (0, 0, 10099)
+version_str = "0.0.post10103"
+version_tuple = (0, 0, 10103)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10099")
+    pversion = V("0.0.post10103")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9975"
-data_version_tuple = (0, 0, 9975)
+data_version_str = "0.0.post9979"
+data_version_tuple = (0, 0, 9979)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9975")
+    pdata_version = V("0.0.post9979")
 except ImportError:
     pass
-data_git_hash = "78d0839fd7cdec1b23834bbdb26244665c55cc33"
-data_git_describe = "v0.0-9975-g78d0839fd"
+data_git_hash = "2babd940e9b65794cabd1351c112e7b016475d57"
+data_git_describe = "v0.0-9979-g2babd940e"
 data_git_msg = """\
-commit 78d0839fd7cdec1b23834bbdb26244665c55cc33
-Author: Cindy Chen <chencindy@opentitan.org>
-Date:   Sun Feb 6 10:26:43 2022 -0800
+commit 2babd940e9b65794cabd1351c112e7b016475d57
+Author: Michael Schaffner <msf@opentitan.org>
+Date:   Fri Feb 4 20:19:18 2022 -0800
 
-    [fpv/flash_ctrl] Fix compile error
+    [sysrst_ctrl] Add some more details regarding combo actions
     
-    This PR fixes the compile error due to index out of bound.
-    
-    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
+    Signed-off-by: Michael Schaffner <msf@opentitan.org>
 
 """
 
