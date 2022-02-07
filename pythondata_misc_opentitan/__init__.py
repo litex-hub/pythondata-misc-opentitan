333962a6fc8d9e735cc81885bcf112d1b08ffd76
LiteX Robot <robot@timvideos.us>
2022-02-07 18:31:09 +0000

    Updating pythondata-misc-opentitan to 0.0.post10099
    Updated data to v0.0-9975-g78d0839fd based on 78d0839fd7cdec1b23834bbdb26244665c55cc33 from https://github.com/lowRISC/opentitan.
    > commit 78d0839fd7cdec1b23834bbdb26244665c55cc33
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Sun Feb 6 10:26:43 2022 -0800
    >
    >     [fpv/flash_ctrl] Fix compile error
    >
    >     This PR fixes the compile error due to index out of bound.
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post124 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,56 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10098"
-version_tuple = (0, 0, 10098)
+version_str = "0.0.post10099"
+version_tuple = (0, 0, 10099)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10098")
+    pversion = V("0.0.post10099")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9974"
-data_version_tuple = (0, 0, 9974)
+data_version_str = "0.0.post9975"
+data_version_tuple = (0, 0, 9975)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9974")
+    pdata_version = V("0.0.post9975")
 except ImportError:
     pass
-data_git_hash = "feaca732a51129d7c598749bb35813cbe61f88fc"
-data_git_describe = "v0.0-9974-gfeaca732a"
+data_git_hash = "78d0839fd7cdec1b23834bbdb26244665c55cc33"
+data_git_describe = "v0.0-9975-g78d0839fd"
 data_git_msg = """\
-commit feaca732a51129d7c598749bb35813cbe61f88fc
-Author: Drew Macrae <drewmacrae@google.com>
-Date:   Thu Feb 3 16:04:17 2022 +0000
+commit 78d0839fd7cdec1b23834bbdb26244665c55cc33
+Author: Cindy Chen <chencindy@opentitan.org>
+Date:   Sun Feb 6 10:26:43 2022 -0800
 
-    [bazel] Solving little warnings in the SW
+    [fpv/flash_ctrl] Fix compile error
     
-    //sw/device/silicon_creator/lib:verilator_boot_data_functest is flaky
-    with a long timeout so I've adjusted it to eternal to make it repeatable
-    and to prevent running it when we're under time pressure
+    This PR fixes the compile error due to index out of bound.
     
-    The entropy_smoketest and the verilator_watchdog_functest are flaky with a
-    default timeout so I've increased them to long (900s)
-    
-    I ran `bazel run //:buildifier_fix so it will be cleaner when run on
-    other systems and commits.
-    
-    Also addressing the following warnings that appear during a SW build:
-    
-    sw/device/lib/dif/dif_kmac_unittest.cc:115:23: warning: comparison of
-    integer expressions of different signedness: 'int' and 'const size_t'
-    {aka 'const long unsigned int'} [-Wsign-compare]
-      115 |     for (int i = 0; i < size; ++i) {
-          |                     ~~^~~~~~
-    
-    sw/device/lib/dif/dif_kmac_unittest.cc:161:21: warning:
-    comparison of integer expressions of different signedness: 'int'
-    and 'long unsigned int' [-Wsign-compare]
-      161 |   for (int i = 0; i < sizeof(uint32_t); i++) {
-          |                   ~~^~~~~~~~~~~~~~~~~~
-    
-    Signed-off-by: Drew Macrae <drewmacrae@google.com>
+    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
 """
 
