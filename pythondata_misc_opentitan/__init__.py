632626c8ea9f83cda13b103db17412c67f66cf0d
LiteX Robot <robot@timvideos.us>
2022-02-08 03:26:51 +0000

    Updating pythondata-misc-opentitan to 0.0.post10122
    Updated data to v0.0-9998-g58f5ad256 based on 58f5ad256abf6c6d69622e192028748beff687ad from https://github.com/lowRISC/opentitan.
    > commit 58f5ad256abf6c6d69622e192028748beff687ad
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Mon Feb 7 15:30:11 2022 -0800
    >
    >     [kmac/rtl,DV] Alignment on swapping alerts order
    >
    >     This PR follows PR #10517 which swaps the order of fatal and recoverable
    >     alerts. With that PR, now alert[0] is recoverable alert and alert[1] is
    >     fatal alert.
    >     With this change, we would like to update the alert_test register as
    >     well, so now:
    >     alert_test field[0] corresponds to recov_operation_err
    >     alert_test field[1] corresponds to fatal_fault_err
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post124 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,41 +4,40 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10121"
-version_tuple = (0, 0, 10121)
+version_str = "0.0.post10122"
+version_tuple = (0, 0, 10122)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10121")
+    pversion = V("0.0.post10122")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9997"
-data_version_tuple = (0, 0, 9997)
+data_version_str = "0.0.post9998"
+data_version_tuple = (0, 0, 9998)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9997")
+    pdata_version = V("0.0.post9998")
 except ImportError:
     pass
-data_git_hash = "72f98d6f87cb58bbcaf96f28d2b99b6c4333a6ee"
-data_git_describe = "v0.0-9997-g72f98d6f8"
+data_git_hash = "58f5ad256abf6c6d69622e192028748beff687ad"
+data_git_describe = "v0.0-9998-g58f5ad256"
 data_git_msg = """\
-commit 72f98d6f87cb58bbcaf96f28d2b99b6c4333a6ee
-Author: Pirmin Vogel <vogelpi@lowrisc.org>
-Date:   Fri Feb 4 11:59:22 2022 +0100
+commit 58f5ad256abf6c6d69622e192028748beff687ad
+Author: Cindy Chen <chencindy@opentitan.org>
+Date:   Mon Feb 7 15:30:11 2022 -0800
 
-    [aes/doc] Update documentation based on D2S review
+    [kmac/rtl,DV] Alignment on swapping alerts order
     
-    In particular, this commit documents:
-    - the use of multi-rail control logic,
-    - the need for a system-supplied reset in case of FI detection,
-    - worst-case entropy consumption rates
-    - the re-use of partial, intermediate results for remasking among
-      DOM S-Boxes.
+    This PR follows PR #10517 which swaps the order of fatal and recoverable
+    alerts. With that PR, now alert[0] is recoverable alert and alert[1] is
+    fatal alert.
+    With this change, we would like to update the alert_test register as
+    well, so now:
+    alert_test field[0] corresponds to recov_operation_err
+    alert_test field[1] corresponds to fatal_fault_err
     
-    This is related to lowRISC/OpenTitan#10422.
-    
-    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
+    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
 """
 
