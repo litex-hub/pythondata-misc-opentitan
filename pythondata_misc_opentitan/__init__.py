b685e37b25752fe62447907c2dfeac45fd051e16
LiteX Robot <robot@timvideos.us>
2022-02-08 06:30:57 +0000

    Updating pythondata-misc-opentitan to 0.0.post10128
    Updated data to v0.0-10004-gcc159e919 based on cc159e919757a96b2ece03b1099139978e248380 from https://github.com/lowRISC/opentitan.
    > commit cc159e919757a96b2ece03b1099139978e248380
    > Author: Miguel Osorio <miguelosorio@google.com>
    > Date:   Wed Feb 2 20:57:01 2022 -0800
    >
    >     [sw/rom] Remove sec_mmio_write_increment function.
    >
    >     Remove sec_mmio_write_increment function as it is only replaced by a
    >     macro.
    >
    >     Signed-off-by: Miguel Osorio <miguelosorio@google.com>
    >
    
    Updated using 0.0.post124 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,40 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10122"
-version_tuple = (0, 0, 10122)
+version_str = "0.0.post10128"
+version_tuple = (0, 0, 10128)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10122")
+    pversion = V("0.0.post10128")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9998"
-data_version_tuple = (0, 0, 9998)
+data_version_str = "0.0.post10004"
+data_version_tuple = (0, 0, 10004)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9998")
+    pdata_version = V("0.0.post10004")
 except ImportError:
     pass
-data_git_hash = "58f5ad256abf6c6d69622e192028748beff687ad"
-data_git_describe = "v0.0-9998-g58f5ad256"
+data_git_hash = "cc159e919757a96b2ece03b1099139978e248380"
+data_git_describe = "v0.0-10004-gcc159e919"
 data_git_msg = """\
-commit 58f5ad256abf6c6d69622e192028748beff687ad
-Author: Cindy Chen <chencindy@opentitan.org>
-Date:   Mon Feb 7 15:30:11 2022 -0800
+commit cc159e919757a96b2ece03b1099139978e248380
+Author: Miguel Osorio <miguelosorio@google.com>
+Date:   Wed Feb 2 20:57:01 2022 -0800
 
-    [kmac/rtl,DV] Alignment on swapping alerts order
+    [sw/rom] Remove sec_mmio_write_increment function.
     
-    This PR follows PR #10517 which swaps the order of fatal and recoverable
-    alerts. With that PR, now alert[0] is recoverable alert and alert[1] is
-    fatal alert.
-    With this change, we would like to update the alert_test register as
-    well, so now:
-    alert_test field[0] corresponds to recov_operation_err
-    alert_test field[1] corresponds to fatal_fault_err
+    Remove sec_mmio_write_increment function as it is only replaced by a
+    macro.
     
-    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
+    Signed-off-by: Miguel Osorio <miguelosorio@google.com>
 
 """
 
