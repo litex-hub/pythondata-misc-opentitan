646e3be4bb4e68fc1fc6726c804b2623fae341b1
LiteX Robot <robot@timvideos.us>
2022-02-08 19:22:29 +0000

    Updating pythondata-misc-opentitan to 0.0.post10150
    Updated data to v0.0-10026-g5a990dd81 based on 5a990dd81fb8b33c21232c9621da7274ff5b643b from https://github.com/lowRISC/opentitan.
    > commit 5a990dd81fb8b33c21232c9621da7274ff5b643b
    > Author: Rasmus Madsen <rasmus.madsen@wdc.com>
    > Date:   Mon Jan 31 06:47:51 2022 -0800
    >
    >     [aes/dv] updated dv to handle new reseeding feature
    >
    >     Signed-off-by: Rasmus Madsen <rasmus.madsen@wdc.com>
    >
    
    Updated using 0.0.post124 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,41 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10146"
-version_tuple = (0, 0, 10146)
+version_str = "0.0.post10150"
+version_tuple = (0, 0, 10150)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10146")
+    pversion = V("0.0.post10150")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10022"
-data_version_tuple = (0, 0, 10022)
+data_version_str = "0.0.post10026"
+data_version_tuple = (0, 0, 10026)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10022")
+    pdata_version = V("0.0.post10026")
 except ImportError:
     pass
-data_git_hash = "5dfe96ba8da3c19400ceeebed338e5ab665521f0"
-data_git_describe = "v0.0-10022-g5dfe96ba8"
+data_git_hash = "5a990dd81fb8b33c21232c9621da7274ff5b643b"
+data_git_describe = "v0.0-10026-g5a990dd81"
 data_git_msg = """\
-commit 5dfe96ba8da3c19400ceeebed338e5ab665521f0
-Author: Eunchan Kim <eunchan@opentitan.org>
-Date:   Thu Feb 3 17:12:39 2022 -0800
+commit 5a990dd81fb8b33c21232c9621da7274ff5b643b
+Author: Rasmus Madsen <rasmus.madsen@wdc.com>
+Date:   Mon Jan 31 06:47:51 2022 -0800
 
-    [cdc] Working version of CDC waivers
+    [aes/dv] updated dv to handle new reseeding feature
     
-    * Waiver should not have spaces between the status braces.
-      changed to `-status {Waived}`
-    
-    * `[info exists ...]` should use variable name not the value of variable
-      changed to `[info exists modules]` in `cdc_waivers.tcl`
-    
-    * waiver directory is used to source individual waiver files.
-      defined `CDC_WAIVERS_DIR
-    
-    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>
+    Signed-off-by: Rasmus Madsen <rasmus.madsen@wdc.com>
 
 """
 
