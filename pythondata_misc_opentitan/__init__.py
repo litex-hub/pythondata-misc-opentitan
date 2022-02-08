1fed0e4eef0d9d431c0a5012cdbb5f325e3236fc
LiteX Robot <robot@timvideos.us>
2022-02-08 01:23:06 +0000

    Updating pythondata-misc-opentitan to 0.0.post10121
    Updated data to v0.0-9997-g72f98d6f8 based on 72f98d6f87cb58bbcaf96f28d2b99b6c4333a6ee from https://github.com/lowRISC/opentitan.
    > commit 72f98d6f87cb58bbcaf96f28d2b99b6c4333a6ee
    > Author: Pirmin Vogel <vogelpi@lowrisc.org>
    > Date:   Fri Feb 4 11:59:22 2022 +0100
    >
    >     [aes/doc] Update documentation based on D2S review
    >
    >     In particular, this commit documents:
    >     - the use of multi-rail control logic,
    >     - the need for a system-supplied reset in case of FI detection,
    >     - worst-case entropy consumption rates
    >     - the re-use of partial, intermediate results for remasking among
    >       DOM S-Boxes.
    >
    >     This is related to lowRISC/OpenTitan#10422.
    >
    >     Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
    >
    
    Updated using 0.0.post124 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,33 +4,41 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10115"
-version_tuple = (0, 0, 10115)
+version_str = "0.0.post10121"
+version_tuple = (0, 0, 10121)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10115")
+    pversion = V("0.0.post10121")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post9991"
-data_version_tuple = (0, 0, 9991)
+data_version_str = "0.0.post9997"
+data_version_tuple = (0, 0, 9997)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post9991")
+    pdata_version = V("0.0.post9997")
 except ImportError:
     pass
-data_git_hash = "99263a63f3eec22e1d34ebeef039a74c1ea7ec2b"
-data_git_describe = "v0.0-9991-g99263a63f"
+data_git_hash = "72f98d6f87cb58bbcaf96f28d2b99b6c4333a6ee"
+data_git_describe = "v0.0-9997-g72f98d6f8"
 data_git_msg = """\
-commit 99263a63f3eec22e1d34ebeef039a74c1ea7ec2b
-Author: Weicai Yang <weicai@google.com>
-Date:   Mon Feb 7 13:39:25 2022 -0800
+commit 72f98d6f87cb58bbcaf96f28d2b99b6c4333a6ee
+Author: Pirmin Vogel <vogelpi@lowrisc.org>
+Date:   Fri Feb 4 11:59:22 2022 +0100
 
-    [dv] Enable xcelium to include X for toggle coverage
+    [aes/doc] Update documentation based on D2S review
     
-    Address #10332
-    Signed-off-by: Weicai Yang <weicai@google.com>
+    In particular, this commit documents:
+    - the use of multi-rail control logic,
+    - the need for a system-supplied reset in case of FI detection,
+    - worst-case entropy consumption rates
+    - the re-use of partial, intermediate results for remasking among
+      DOM S-Boxes.
+    
+    This is related to lowRISC/OpenTitan#10422.
+    
+    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
 
 """
 
