2fc3aa40787a61618fb34f453c6cca73e04c6da6
LiteX Robot <robot@timvideos.us>
2022-02-08 20:29:17 +0000

    Updating pythondata-misc-opentitan to 0.0.post10151
    Updated data to v0.0-10027-gcdc4bd4c8 based on cdc4bd4c821fce7a32367ee7450358d0f9a7f0d2 from https://github.com/lowRISC/opentitan.
    > commit cdc4bd4c821fce7a32367ee7450358d0f9a7f0d2
    > Author: Timothy Trippel <ttrippel@google.com>
    > Date:   Sun Feb 6 22:33:21 2022 -0800
    >
    >     [sw/test_rom] Add AST init to test ROM
    >
    >     This commit adds the blind-data-copy initialization code to the test ROM
    >     to enable testing the AST in chip-level tests.
    >
    >     Signed-off-by: Timothy Trippel <ttrippel@google.com>
    >
    
    Updated using 0.0.post124 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10150"
-version_tuple = (0, 0, 10150)
+version_str = "0.0.post10151"
+version_tuple = (0, 0, 10151)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10150")
+    pversion = V("0.0.post10151")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10026"
-data_version_tuple = (0, 0, 10026)
+data_version_str = "0.0.post10027"
+data_version_tuple = (0, 0, 10027)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10026")
+    pdata_version = V("0.0.post10027")
 except ImportError:
     pass
-data_git_hash = "5a990dd81fb8b33c21232c9621da7274ff5b643b"
-data_git_describe = "v0.0-10026-g5a990dd81"
+data_git_hash = "cdc4bd4c821fce7a32367ee7450358d0f9a7f0d2"
+data_git_describe = "v0.0-10027-gcdc4bd4c8"
 data_git_msg = """\
-commit 5a990dd81fb8b33c21232c9621da7274ff5b643b
-Author: Rasmus Madsen <rasmus.madsen@wdc.com>
-Date:   Mon Jan 31 06:47:51 2022 -0800
+commit cdc4bd4c821fce7a32367ee7450358d0f9a7f0d2
+Author: Timothy Trippel <ttrippel@google.com>
+Date:   Sun Feb 6 22:33:21 2022 -0800
 
-    [aes/dv] updated dv to handle new reseeding feature
+    [sw/test_rom] Add AST init to test ROM
     
-    Signed-off-by: Rasmus Madsen <rasmus.madsen@wdc.com>
+    This commit adds the blind-data-copy initialization code to the test ROM
+    to enable testing the AST in chip-level tests.
+    
+    Signed-off-by: Timothy Trippel <ttrippel@google.com>
 
 """
 
