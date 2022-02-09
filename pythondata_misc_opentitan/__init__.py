59bc230280d9cfd4945e017b504060ccc8f14ece
LiteX Robot <robot@timvideos.us>
2022-02-09 07:26:43 +0000

    Updating pythondata-misc-opentitan to 0.0.post10175
    Updated data to v0.0-10051-g563312c8c based on 563312c8cabd3f135c58df639634e741e241d638 from https://github.com/lowRISC/opentitan.
    > commit 563312c8cabd3f135c58df639634e741e241d638
    > Author: Timothy Trippel <ttrippel@google.com>
    > Date:   Wed Feb 2 15:10:28 2022 -0800
    >
    >     [sw/ottf] Rename test_rom_ext to ottf_start
    >
    >     Previously the boot sequence for chip level tests looked like:
    >
    >     chip reset --> test_rom --> test_rom_ext --> ottf --> test_main()
    >
    >     In the above sequence the `test_rom_ext` was really just an ASM file
    >     that performed initializations prior to jumping to the OTTF `main()`.
    >     However, as we refactor the OTTF to enable running chip-level tests at
    >     various boot stages (e.g., ROM_EXT and BL0, see #10498), it makes more
    >     sense to model the combined OTTF and test bundle as its own boot stage
    >     that can be run in inplace of the ROM_EXT or BL0.
    >
    >     Therefore, this commit renames/consolidates the test_rom_ext into a
    >     component of the OTTF, called the `ottf_start`. The `ottf_start` is kept
    >     as a separate static library from the `ottf` to enable running tests
    >     that do not use the OTTF, i.e. the crt_test (which tests the
    >     functionality of the `ottf_start` library), and the "Hello World"
    >     example programs. However, when test images are built, both the
    >     `ottf_start` and `ottf` are linked with the test library itself, to
    >     create one cohesive "test boot stage".
    >
    >     The new boot sequence for a test will look like:
    >
    >     chip reset --> test_rom --> ottf --> test_main(),
    >
    >     where OTTF = (ottf_start --> OTTF).
    >
    >     This partially addresses a task in #10498.
    >
    >     Signed-off-by: Timothy Trippel <ttrippel@google.com>
    >
    
    Updated using 0.0.post124 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,37 +4,60 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10173"
-version_tuple = (0, 0, 10173)
+version_str = "0.0.post10175"
+version_tuple = (0, 0, 10175)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10173")
+    pversion = V("0.0.post10175")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10049"
-data_version_tuple = (0, 0, 10049)
+data_version_str = "0.0.post10051"
+data_version_tuple = (0, 0, 10051)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10049")
+    pdata_version = V("0.0.post10051")
 except ImportError:
     pass
-data_git_hash = "2bc4edbd859334dd3edd253ab9c12deb05d003df"
-data_git_describe = "v0.0-10049-g2bc4edbd8"
+data_git_hash = "563312c8cabd3f135c58df639634e741e241d638"
+data_git_describe = "v0.0-10051-g563312c8c"
 data_git_msg = """\
-commit 2bc4edbd859334dd3edd253ab9c12deb05d003df
-Author: Guillermo Maturana <maturana@google.com>
-Date:   Tue Feb 8 00:12:57 2022 -0800
+commit 563312c8cabd3f135c58df639634e741e241d638
+Author: Timothy Trippel <ttrippel@google.com>
+Date:   Wed Feb 2 15:10:28 2022 -0800
 
-    [dv/rstmgr] Add test for sw_rst and hw reset race
+    [sw/ottf] Rename test_rom_ext to ottf_start
     
-    Create a test that sends sw_rst and hardware resets in close temporal
-    proximity.
-    Add UNR generated exclusion file.
-    Exclude rstmgr_cnsty_chk from coverage since it is tested separately.
+    Previously the boot sequence for chip level tests looked like:
     
-    Signed-off-by: Guillermo Maturana <maturana@google.com>
+    chip reset --> test_rom --> test_rom_ext --> ottf --> test_main()
+    
+    In the above sequence the `test_rom_ext` was really just an ASM file
+    that performed initializations prior to jumping to the OTTF `main()`.
+    However, as we refactor the OTTF to enable running chip-level tests at
+    various boot stages (e.g., ROM_EXT and BL0, see #10498), it makes more
+    sense to model the combined OTTF and test bundle as its own boot stage
+    that can be run in inplace of the ROM_EXT or BL0.
+    
+    Therefore, this commit renames/consolidates the test_rom_ext into a
+    component of the OTTF, called the `ottf_start`. The `ottf_start` is kept
+    as a separate static library from the `ottf` to enable running tests
+    that do not use the OTTF, i.e. the crt_test (which tests the
+    functionality of the `ottf_start` library), and the "Hello World"
+    example programs. However, when test images are built, both the
+    `ottf_start` and `ottf` are linked with the test library itself, to
+    create one cohesive "test boot stage".
+    
+    The new boot sequence for a test will look like:
+    
+    chip reset --> test_rom --> ottf --> test_main(),
+    
+    where OTTF = (ottf_start --> OTTF).
+    
+    This partially addresses a task in #10498.
+    
+    Signed-off-by: Timothy Trippel <ttrippel@google.com>
 
 """
 
