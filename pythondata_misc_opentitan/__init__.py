ee521b9259dc6d09b0c32cb8ea016c54afd37b32
LiteX Robot <robot@timvideos.us>
2022-02-10 12:39:17 +0000

    Updating pythondata-misc-opentitan to 0.0.post10220
    Updated data to v0.0-10096-g6b6ed4590 based on 6b6ed4590aa6c665e2512d42360eec1be39cb415 from https://github.com/lowRISC/opentitan.
    > commit 6b6ed4590aa6c665e2512d42360eec1be39cb415
    > Author: Pirmin Vogel <vogelpi@lowrisc.org>
    > Date:   Wed Feb 9 12:33:58 2022 +0100
    >
    >     [aes] Re-enable synthesis optimizations for FWD/INV-only core instances
    >
    >     This commit moves the prim_buf on the cipher core operation signal to
    >     the outside of the cipher core. Having this prim_buf inside the cipher
    >     core prevents synthesis from performing area optimizations if only
    >     forward or inverse operation is needed for a particular cipher core
    >     instance (see e.g. CSRNG).
    >
    >     This is related to lowRISC/OpenTitan#10483.
    >
    >     Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
    >
    
    Updated using 0.0.post124 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,40 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10218"
-version_tuple = (0, 0, 10218)
+version_str = "0.0.post10220"
+version_tuple = (0, 0, 10220)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10218")
+    pversion = V("0.0.post10220")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10094"
-data_version_tuple = (0, 0, 10094)
+data_version_str = "0.0.post10096"
+data_version_tuple = (0, 0, 10096)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10094")
+    pdata_version = V("0.0.post10096")
 except ImportError:
     pass
-data_git_hash = "8c4d8136d974855ad1c1608a5363605921f5d376"
-data_git_describe = "v0.0-10094-g8c4d8136d"
+data_git_hash = "6b6ed4590aa6c665e2512d42360eec1be39cb415"
+data_git_describe = "v0.0-10096-g6b6ed4590"
 data_git_msg = """\
-commit 8c4d8136d974855ad1c1608a5363605921f5d376
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Wed Feb 9 13:03:22 2022 +0000
+commit 6b6ed4590aa6c665e2512d42360eec1be39cb415
+Author: Pirmin Vogel <vogelpi@lowrisc.org>
+Date:   Wed Feb 9 12:33:58 2022 +0100
 
-    [rom_ctrl] Connect up error signal from KMAC
+    [aes] Re-enable synthesis optimizations for FWD/INV-only core instances
     
-    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    This commit moves the prim_buf on the cipher core operation signal to
+    the outside of the cipher core. Having this prim_buf inside the cipher
+    core prevents synthesis from performing area optimizations if only
+    forward or inverse operation is needed for a particular cipher core
+    instance (see e.g. CSRNG).
+    
+    This is related to lowRISC/OpenTitan#10483.
+    
+    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
 
 """
 
