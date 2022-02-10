84a348db3b229469b4c6f2535b9aabce15776233
LiteX Robot <robot@timvideos.us>
2022-02-10 14:25:01 +0000

    Updating pythondata-misc-opentitan to 0.0.post10224
    Updated data to v0.0-10100-g018dac548 based on 018dac54829f0309e6cd215f63e670c933e3fa6e from https://github.com/lowRISC/opentitan.
    > commit 018dac54829f0309e6cd215f63e670c933e3fa6e
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Thu Feb 3 11:31:46 2022 +0000
    >
    >     [otbn,dv] Properly represent DMEM store latency in ISS
    >
    >     This wouldn't matter except that we want to support a test where we
    >     invalidate the whole of DMEM (to trigger integrity errors) at some
    >     arbitrary point in the run.
    >
    >     If we're unlucky and happen to do this just after executing a store
    >     then the RTL will apply the store on the following cycle (leaving
    >     valid integrity bits) but the ISS, which had applied the store
    >     already, would mark the location as having bad integrity.
    >
    >     The correct fix is easy enough: keep an extra "pending" queue that
    >     only gets applied on the following cycle.
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post124 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,40 +4,44 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10220"
-version_tuple = (0, 0, 10220)
+version_str = "0.0.post10224"
+version_tuple = (0, 0, 10224)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10220")
+    pversion = V("0.0.post10224")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10096"
-data_version_tuple = (0, 0, 10096)
+data_version_str = "0.0.post10100"
+data_version_tuple = (0, 0, 10100)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10096")
+    pdata_version = V("0.0.post10100")
 except ImportError:
     pass
-data_git_hash = "6b6ed4590aa6c665e2512d42360eec1be39cb415"
-data_git_describe = "v0.0-10096-g6b6ed4590"
+data_git_hash = "018dac54829f0309e6cd215f63e670c933e3fa6e"
+data_git_describe = "v0.0-10100-g018dac548"
 data_git_msg = """\
-commit 6b6ed4590aa6c665e2512d42360eec1be39cb415
-Author: Pirmin Vogel <vogelpi@lowrisc.org>
-Date:   Wed Feb 9 12:33:58 2022 +0100
+commit 018dac54829f0309e6cd215f63e670c933e3fa6e
+Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
+Date:   Thu Feb 3 11:31:46 2022 +0000
 
-    [aes] Re-enable synthesis optimizations for FWD/INV-only core instances
+    [otbn,dv] Properly represent DMEM store latency in ISS
     
-    This commit moves the prim_buf on the cipher core operation signal to
-    the outside of the cipher core. Having this prim_buf inside the cipher
-    core prevents synthesis from performing area optimizations if only
-    forward or inverse operation is needed for a particular cipher core
-    instance (see e.g. CSRNG).
+    This wouldn't matter except that we want to support a test where we
+    invalidate the whole of DMEM (to trigger integrity errors) at some
+    arbitrary point in the run.
     
-    This is related to lowRISC/OpenTitan#10483.
+    If we're unlucky and happen to do this just after executing a store
+    then the RTL will apply the store on the following cycle (leaving
+    valid integrity bits) but the ISS, which had applied the store
+    already, would mark the location as having bad integrity.
     
-    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
+    The correct fix is easy enough: keep an extra "pending" queue that
+    only gets applied on the following cycle.
+    
+    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
 """
 
