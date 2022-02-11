4a673a7f38d78b48386756fdc8aafe80d8950538
LiteX Robot <robot@timvideos.us>
2022-02-11 03:26:52 +0000

    Updating pythondata-misc-opentitan to 0.0.post10244
    Updated data to v0.0-10120-g5fbb8f726 based on 5fbb8f72677bf48ef034222a514913c09ce61c6e from https://github.com/lowRISC/opentitan.
    > commit 5fbb8f72677bf48ef034222a514913c09ce61c6e
    > Author: Nigel Scales <nigel.scales@gmail.com>
    > Date:   Thu Feb 10 09:58:16 2022 +0000
    >
    >     [lc_ctrl/dv] Addressed comments in #10246
    >
    >     - Deleted lc_ctrl_cov_if.sv:
    >     https://github.com/lowRISC/opentitan/pull/10246#discussion_r802079172
    >     - Updated type of otp_vendor_test_ctrl and otp_vendor_test_status in lc_ctrl_env_cfg.sv:
    >     https://github.com/lowRISC/opentitan/pull/10246#discussion_r802082681
    >     - Updated error message to UVM_MEDIUM:
    >     https://github.com/lowRISC/opentitan/pull/10246#discussion_r802084957
    >     - Added back port toggle coverage for RV_TAP:
    >     https://github.com/lowRISC/opentitan/pull/10246#discussion_r803431294
    >
    >     Signed-off-by: Nigel Scales <nigel.scales@gmail.com>
    >
    
    Updated using 0.0.post124 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,41 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10243"
-version_tuple = (0, 0, 10243)
+version_str = "0.0.post10244"
+version_tuple = (0, 0, 10244)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10243")
+    pversion = V("0.0.post10244")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10119"
-data_version_tuple = (0, 0, 10119)
+data_version_str = "0.0.post10120"
+data_version_tuple = (0, 0, 10120)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10119")
+    pdata_version = V("0.0.post10120")
 except ImportError:
     pass
-data_git_hash = "4360bf23629d1d5771f0340b6d4772ebdd1f6f5a"
-data_git_describe = "v0.0-10119-g4360bf236"
+data_git_hash = "5fbb8f72677bf48ef034222a514913c09ce61c6e"
+data_git_describe = "v0.0-10120-g5fbb8f726"
 data_git_msg = """\
-commit 4360bf23629d1d5771f0340b6d4772ebdd1f6f5a
-Author: Timothy Chen <timothytim@google.com>
-Date:   Wed Feb 9 16:12:43 2022 -0800
+commit 5fbb8f72677bf48ef034222a514913c09ce61c6e
+Author: Nigel Scales <nigel.scales@gmail.com>
+Date:   Thu Feb 10 09:58:16 2022 +0000
 
-    [clkmgr] Add secure buffers for clkmgr byp_req
+    [lc_ctrl/dv] Addressed comments in #10246
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    - Deleted lc_ctrl_cov_if.sv:
+    https://github.com/lowRISC/opentitan/pull/10246#discussion_r802079172
+    - Updated type of otp_vendor_test_ctrl and otp_vendor_test_status in lc_ctrl_env_cfg.sv:
+    https://github.com/lowRISC/opentitan/pull/10246#discussion_r802082681
+    - Updated error message to UVM_MEDIUM:
+    https://github.com/lowRISC/opentitan/pull/10246#discussion_r802084957
+    - Added back port toggle coverage for RV_TAP:
+    https://github.com/lowRISC/opentitan/pull/10246#discussion_r803431294
+    
+    Signed-off-by: Nigel Scales <nigel.scales@gmail.com>
 
 """
 
