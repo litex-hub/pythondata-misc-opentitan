0f1a0e9195b3ac3b68e32edb84573f3b31675c58
LiteX Robot <robot@timvideos.us>
2022-02-11 11:24:24 +0000

    Updating pythondata-misc-opentitan to 0.0.post10246
    Updated data to v0.0-10122-g1a2c4ec93 based on 1a2c4ec9327c24d2ee9bcba4c2a08983ed7a79c7 from https://github.com/lowRISC/opentitan.
    > commit 1a2c4ec9327c24d2ee9bcba4c2a08983ed7a79c7
    > Author: Rasmus Madsen <rasmus.madsen@wdc.com>
    > Date:   Thu Feb 10 04:44:08 2022 -0800
    >
    >     [aes/dv] fixed compile errors introduced with new rtl
    >
    >     Signed-off-by: Rasmus Madsen <rasmus.madsen@wdc.com>
    >
    
    Updated using 0.0.post124 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,40 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10245"
-version_tuple = (0, 0, 10245)
+version_str = "0.0.post10246"
+version_tuple = (0, 0, 10246)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10245")
+    pversion = V("0.0.post10246")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10121"
-data_version_tuple = (0, 0, 10121)
+data_version_str = "0.0.post10122"
+data_version_tuple = (0, 0, 10122)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10121")
+    pdata_version = V("0.0.post10122")
 except ImportError:
     pass
-data_git_hash = "ca221a00278b20c82d3d0ffa092a52782c1c0b0d"
-data_git_describe = "v0.0-10121-gca221a002"
+data_git_hash = "1a2c4ec9327c24d2ee9bcba4c2a08983ed7a79c7"
+data_git_describe = "v0.0-10122-g1a2c4ec93"
 data_git_msg = """\
-commit ca221a00278b20c82d3d0ffa092a52782c1c0b0d
-Author: Cindy Chen <chencindy@opentitan.org>
-Date:   Thu Feb 10 10:21:48 2022 -0800
+commit 1a2c4ec9327c24d2ee9bcba4c2a08983ed7a79c7
+Author: Rasmus Madsen <rasmus.madsen@wdc.com>
+Date:   Thu Feb 10 04:44:08 2022 -0800
 
-    [dv/kmac] Fix kmac TL integrity error failure
+    [aes/dv] fixed compile errors introduced with new rtl
     
-    When kmac TL integrity error triggers, kmac will lock its internal
-    states and also lock `cfg_regwen` register. Because the `cfg_regwen` is
-    a `ro` register, RAL model won't lock its lockable registers
-    automatically.
-    So I separate out a task in common sequence, and override it in
-    kmac_common_seq to lock the lockable regs
-    immediately after tl integrity error is triggered.
-    
-    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
+    Signed-off-by: Rasmus Madsen <rasmus.madsen@wdc.com>
 
 """
 
