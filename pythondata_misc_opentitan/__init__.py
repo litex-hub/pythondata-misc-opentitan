89c355dbd881dd207579963c15e4ef73128a6b6c
LiteX Robot <robot@timvideos.us>
2022-02-13 16:37:35 +0000

    Updating pythondata-misc-opentitan to 0.0.post10277
    Updated data to v0.0-10151-g65ab4c15c based on 65ab4c15c77e98f228642e05cb6c6b509cb9bb5d from https://github.com/lowRISC/opentitan.
    > commit 65ab4c15c77e98f228642e05cb6c6b509cb9bb5d
    > Author: Michael Munday <mike.munday@lowrisc.org>
    > Date:   Thu Jan 20 14:48:22 2022 +0000
    >
    >     [mask_rom] Configure watchdog using OTP value
    >
    >     Configure the watchdog timer using an OTP value when in the DEV,
    >     PROD and PROD_END lifecycle states. Otherwise disable it.
    >
    >     Signed-off-by: Michael Munday <mike.munday@lowrisc.org>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10276"
-version_tuple = (0, 0, 10276)
+version_str = "0.0.post10277"
+version_tuple = (0, 0, 10277)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10276")
+    pversion = V("0.0.post10277")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10150"
-data_version_tuple = (0, 0, 10150)
+data_version_str = "0.0.post10151"
+data_version_tuple = (0, 0, 10151)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10150")
+    pdata_version = V("0.0.post10151")
 except ImportError:
     pass
-data_git_hash = "7f5112a95ae059cb6af5fd6a84772e2c463af509"
-data_git_describe = "v0.0-10150-g7f5112a95"
+data_git_hash = "65ab4c15c77e98f228642e05cb6c6b509cb9bb5d"
+data_git_describe = "v0.0-10151-g65ab4c15c"
 data_git_msg = """\
-commit 7f5112a95ae059cb6af5fd6a84772e2c463af509
+commit 65ab4c15c77e98f228642e05cb6c6b509cb9bb5d
 Author: Michael Munday <mike.munday@lowrisc.org>
-Date:   Sat Feb 5 00:18:48 2022 +0000
+Date:   Thu Jan 20 14:48:22 2022 +0000
 
-    [silicon_creator] Harden epmp_state_check return value
+    [mask_rom] Configure watchdog using OTP value
     
-    Use a counter rather than a bool to track whether a mismatch has
-    been detected.
+    Configure the watchdog timer using an OTP value when in the DEV,
+    PROD and PROD_END lifecycle states. Otherwise disable it.
     
     Signed-off-by: Michael Munday <mike.munday@lowrisc.org>
 
