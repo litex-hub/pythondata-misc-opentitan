27ea98340adc7217d23dbc63b5a2edf38d3cea8c
LiteX Robot <robot@timvideos.us>
2022-02-15 04:35:03 +0000

    Updating pythondata-misc-opentitan to 0.0.post10304
    Updated data to v0.0-10178-g2ff8a74d5 based on 2ff8a74d5ac6a5d028a1da0e093739950514207f from https://github.com/lowRISC/opentitan.
    > commit 2ff8a74d5ac6a5d028a1da0e093739950514207f
    > Author: Nigel Scales <nigel.scales@gmail.com>
    > Date:   Thu Feb 10 12:24:45 2022 +0000
    >
    >     [lc_ctrl] Moved to V2
    >
    >     - Updated project to V2
    >     - Updated note regarding RV_TAP
    >     - Added caveat regarding condition coverage to checklist
    >
    >     Signed-off-by: Nigel Scales <nigel.scales@gmail.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10303"
-version_tuple = (0, 0, 10303)
+version_str = "0.0.post10304"
+version_tuple = (0, 0, 10304)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10303")
+    pversion = V("0.0.post10304")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10177"
-data_version_tuple = (0, 0, 10177)
+data_version_str = "0.0.post10178"
+data_version_tuple = (0, 0, 10178)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10177")
+    pdata_version = V("0.0.post10178")
 except ImportError:
     pass
-data_git_hash = "166d7afd7b8744c72e6d6da7a4456c8574ddfd84"
-data_git_describe = "v0.0-10177-g166d7afd7"
+data_git_hash = "2ff8a74d5ac6a5d028a1da0e093739950514207f"
+data_git_describe = "v0.0-10178-g2ff8a74d5"
 data_git_msg = """\
-commit 166d7afd7b8744c72e6d6da7a4456c8574ddfd84
-Author: Cindy Chen <chencindy@opentitan.org>
-Date:   Fri Feb 11 14:48:44 2022 -0800
+commit 2ff8a74d5ac6a5d028a1da0e093739950514207f
+Author: Nigel Scales <nigel.scales@gmail.com>
+Date:   Thu Feb 10 12:24:45 2022 +0000
 
-    [dv/kmac] Add lc_escalation seq
+    [lc_ctrl] Moved to V2
     
-    This PR adds lc_escalation seq in DV:
-    It randomly issue lc_escalate_en in the middle of a Kmac operation,
-    Then check if Kmac is in terminal state.
+    - Updated project to V2
+    - Updated note regarding RV_TAP
+    - Added caveat regarding condition coverage to checklist
     
-    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
+    Signed-off-by: Nigel Scales <nigel.scales@gmail.com>
 
 """
 
