3d8fb70946655fc26130bb99f2544292a326b48b
LiteX Robot <robot@timvideos.us>
2022-02-16 06:34:12 +0000

    Updating pythondata-misc-opentitan to 0.0.post10340
    Updated data to v0.0-10214-g87d485a80 based on 87d485a80468a6e64a3267a50cbbf2a4a3d8c259 from https://github.com/lowRISC/opentitan.
    > commit 87d485a80468a6e64a3267a50cbbf2a4a3d8c259
    > Author: Timothy Trippel <ttrippel@google.com>
    > Date:   Fri Feb 11 15:18:16 2022 -0800
    >
    >     [dif/csrng] Update DIF milestones and move to S1
    >
    >     This commit updates the DIF milestones for the CSRNG DIF library to
    >     reflect the current state of the SW.
    >
    >     This addresses a task in #10504.
    >
    >     Signed-off-by: Timothy Trippel <ttrippel@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,33 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10332"
-version_tuple = (0, 0, 10332)
+version_str = "0.0.post10340"
+version_tuple = (0, 0, 10340)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10332")
+    pversion = V("0.0.post10340")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10206"
-data_version_tuple = (0, 0, 10206)
+data_version_str = "0.0.post10214"
+data_version_tuple = (0, 0, 10214)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10206")
+    pdata_version = V("0.0.post10214")
 except ImportError:
     pass
-data_git_hash = "48a9fb73f951577cd69421cb1c6ff4c6f53f532b"
-data_git_describe = "v0.0-10206-g48a9fb73f"
+data_git_hash = "87d485a80468a6e64a3267a50cbbf2a4a3d8c259"
+data_git_describe = "v0.0-10214-g87d485a80"
 data_git_msg = """\
-commit 48a9fb73f951577cd69421cb1c6ff4c6f53f532b
+commit 87d485a80468a6e64a3267a50cbbf2a4a3d8c259
 Author: Timothy Trippel <ttrippel@google.com>
-Date:   Thu Feb 10 22:36:40 2022 -0800
+Date:   Fri Feb 11 15:18:16 2022 -0800
 
-    [dif] Update DIF milestones and descriptions
+    [dif/csrng] Update DIF milestones and move to S1
     
-    The DIF milestones were out of date. This commit addresses (and a task
-    in #10504).
+    This commit updates the DIF milestones for the CSRNG DIF library to
+    reflect the current state of the SW.
+    
+    This addresses a task in #10504.
     
     Signed-off-by: Timothy Trippel <ttrippel@google.com>
 
