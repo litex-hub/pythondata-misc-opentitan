d082de2a74273468890503b94f491aa6171175ee
LiteX Robot <robot@timvideos.us>
2022-02-16 01:29:59 +0000

    Updating pythondata-misc-opentitan to 0.0.post10330
    Updated data to v0.0-10204-ga48e5e162 based on a48e5e162aa7a04fc47e4fab6866f0126567bf77 from https://github.com/lowRISC/opentitan.
    > commit a48e5e162aa7a04fc47e4fab6866f0126567bf77
    > Author: Alexander Williams <awill@google.com>
    > Date:   Mon Feb 14 16:20:47 2022 -0800
    >
    >     [usbdev] Clean up headers and validate num EPs
    >
    >     The number of IN endpoints and number of OUT endpoints must be equal for
    >     the current code.
    >
    >     Signed-off-by: Alexander Williams <awill@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,40 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10323"
-version_tuple = (0, 0, 10323)
+version_str = "0.0.post10330"
+version_tuple = (0, 0, 10330)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10323")
+    pversion = V("0.0.post10330")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10197"
-data_version_tuple = (0, 0, 10197)
+data_version_str = "0.0.post10204"
+data_version_tuple = (0, 0, 10204)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10197")
+    pdata_version = V("0.0.post10204")
 except ImportError:
     pass
-data_git_hash = "a4e8721132e8e259d0bcf9359fc90244a9d3637e"
-data_git_describe = "v0.0-10197-ga4e872113"
+data_git_hash = "a48e5e162aa7a04fc47e4fab6866f0126567bf77"
+data_git_describe = "v0.0-10204-ga48e5e162"
 data_git_msg = """\
-commit a4e8721132e8e259d0bcf9359fc90244a9d3637e
+commit a48e5e162aa7a04fc47e4fab6866f0126567bf77
 Author: Alexander Williams <awill@google.com>
-Date:   Tue Feb 15 09:06:24 2022 -0800
+Date:   Mon Feb 14 16:20:47 2022 -0800
 
-    [flash_ctrl/dif] Fix word_count handling for erase
+    [usbdev] Clean up headers and validate num EPs
     
-    The word_count / words_remaining handling was forbidding erase
-    operations from completing, as the transaction functions required a
-    nonzero word_count and activity on the FIFOs to reduce it to 0 before
-    completion.
+    The number of IN endpoints and number of OUT endpoints must be equal for
+    the current code.
     
-    Exempt erase operations from needing any specific word_count, and set
-    words_remaining to 0.
-    
-    Co-authored-by: Dave Williams <dave.williams@ensilica.com>
     Signed-off-by: Alexander Williams <awill@google.com>
 
 """
