270fc99358edb6307925b7d7dbf642381dd59429
LiteX Robot <robot@timvideos.us>
2022-02-16 21:24:55 +0000

    Updating pythondata-misc-opentitan to 0.0.post10363
    Updated data to v0.0-10237-g85dbfb665 based on 85dbfb665b5f813d01ed979d997039cd3dd50de3 from https://github.com/lowRISC/opentitan.
    > commit 85dbfb665b5f813d01ed979d997039cd3dd50de3
    > Author: Weicai Yang <weicai@google.com>
    > Date:   Mon Feb 14 21:59:33 2022 -0800
    >
    >     [prim] Move sec_cm assertion to an include file in prim_assert
    >
    >     Addressed problem raised in #10248 - Yosys only allows macros to be in
    >     an included file
    >
    >     Signed-off-by: Weicai Yang <weicai@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10354"
-version_tuple = (0, 0, 10354)
+version_str = "0.0.post10363"
+version_tuple = (0, 0, 10363)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10354")
+    pversion = V("0.0.post10363")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10228"
-data_version_tuple = (0, 0, 10228)
+data_version_str = "0.0.post10237"
+data_version_tuple = (0, 0, 10237)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10228")
+    pdata_version = V("0.0.post10237")
 except ImportError:
     pass
-data_git_hash = "bbb36c003493e88fe4c1160594bd5ad686177e8c"
-data_git_describe = "v0.0-10228-gbbb36c003"
+data_git_hash = "85dbfb665b5f813d01ed979d997039cd3dd50de3"
+data_git_describe = "v0.0-10237-g85dbfb665"
 data_git_msg = """\
-commit bbb36c003493e88fe4c1160594bd5ad686177e8c
-Author: Steve Nelson <steve.nelson@wdc.com>
-Date:   Fri Feb 11 09:17:50 2022 -0800
+commit 85dbfb665b5f813d01ed979d997039cd3dd50de3
+Author: Weicai Yang <weicai@google.com>
+Date:   Mon Feb 14 21:59:33 2022 -0800
 
-    [csrng/dv] Verify lc_hw_debug_en and cs_aes_halt
+    [prim] Move sec_cm assertion to an include file in prim_assert
     
-    Signed-off-by: Steve Nelson <steve.nelson@wdc.com>
+    Addressed problem raised in #10248 - Yosys only allows macros to be in
+    an included file
+    
+    Signed-off-by: Weicai Yang <weicai@google.com>
 
 """
 
