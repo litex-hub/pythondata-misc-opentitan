fa4e37026cfe7ef044912b30475fff6d91f0e871
LiteX Robot <robot@timvideos.us>
2022-02-16 02:42:55 +0000

    Updating pythondata-misc-opentitan to 0.0.post10331
    Updated data to v0.0-10205-gb8e70c002 based on b8e70c002ff2f34de0cb321db9c459f2e1908d45 from https://github.com/lowRISC/opentitan.
    > commit b8e70c002ff2f34de0cb321db9c459f2e1908d45
    > Author: Weicai Yang <weicai@google.com>
    > Date:   Tue Feb 15 16:08:43 2022 -0800
    >
    >     [dv] Add macro DV_LC_TX_DIST and update get_rand_lc_tx_val
    >
    >     lc_tx_pkg::On/Off is different than Mubi4True/False
    >     Add a macro so that users can use `DV_LC_TX_DIST` in constraint
    >     And update `get_rand_lc_tx_val` to use this constraint macro
    >
    >     Signed-off-by: Weicai Yang <weicai@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10330"
-version_tuple = (0, 0, 10330)
+version_str = "0.0.post10331"
+version_tuple = (0, 0, 10331)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10330")
+    pversion = V("0.0.post10331")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10204"
-data_version_tuple = (0, 0, 10204)
+data_version_str = "0.0.post10205"
+data_version_tuple = (0, 0, 10205)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10204")
+    pdata_version = V("0.0.post10205")
 except ImportError:
     pass
-data_git_hash = "a48e5e162aa7a04fc47e4fab6866f0126567bf77"
-data_git_describe = "v0.0-10204-ga48e5e162"
+data_git_hash = "b8e70c002ff2f34de0cb321db9c459f2e1908d45"
+data_git_describe = "v0.0-10205-gb8e70c002"
 data_git_msg = """\
-commit a48e5e162aa7a04fc47e4fab6866f0126567bf77
-Author: Alexander Williams <awill@google.com>
-Date:   Mon Feb 14 16:20:47 2022 -0800
+commit b8e70c002ff2f34de0cb321db9c459f2e1908d45
+Author: Weicai Yang <weicai@google.com>
+Date:   Tue Feb 15 16:08:43 2022 -0800
 
-    [usbdev] Clean up headers and validate num EPs
+    [dv] Add macro DV_LC_TX_DIST and update get_rand_lc_tx_val
     
-    The number of IN endpoints and number of OUT endpoints must be equal for
-    the current code.
+    lc_tx_pkg::On/Off is different than Mubi4True/False
+    Add a macro so that users can use `DV_LC_TX_DIST` in constraint
+    And update `get_rand_lc_tx_val` to use this constraint macro
     
-    Signed-off-by: Alexander Williams <awill@google.com>
+    Signed-off-by: Weicai Yang <weicai@google.com>
 
 """
 
