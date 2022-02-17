83fdbd2f86508bdf515d70676ae9a1300c2f8dc1
LiteX Robot <robot@timvideos.us>
2022-02-17 23:26:33 +0000

    Updating pythondata-misc-opentitan to 0.0.post10411
    Updated data to v0.0-10285-g47b50cbcb based on 47b50cbcbb9ae8a331310e4ea4befca0c2c130a4 from https://github.com/lowRISC/opentitan.
    > commit 47b50cbcbb9ae8a331310e4ea4befca0c2c130a4
    > Author: Eunchan Kim <eunchan@opentitan.org>
    > Date:   Thu Feb 17 12:20:08 2022 -0800
    >
    >     [kmac] Regenerate TOP Earlgrey
    >
    >     Signed-off-by: Eunchan Kim <eunchan@opentitan.org>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10407"
-version_tuple = (0, 0, 10407)
+version_str = "0.0.post10411"
+version_tuple = (0, 0, 10411)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10407")
+    pversion = V("0.0.post10411")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10281"
-data_version_tuple = (0, 0, 10281)
+data_version_str = "0.0.post10285"
+data_version_tuple = (0, 0, 10285)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10281")
+    pdata_version = V("0.0.post10285")
 except ImportError:
     pass
-data_git_hash = "3fc70360b5e814b3ce183bfff653cfe0e03b3675"
-data_git_describe = "v0.0-10281-g3fc70360b"
+data_git_hash = "47b50cbcbb9ae8a331310e4ea4befca0c2c130a4"
+data_git_describe = "v0.0-10285-g47b50cbcb"
 data_git_msg = """\
-commit 3fc70360b5e814b3ce183bfff653cfe0e03b3675
-Author: Greg Chadwick <gac@lowrisc.org>
-Date:   Wed Feb 16 12:41:59 2022 +0000
+commit 47b50cbcbb9ae8a331310e4ea4befca0c2c130a4
+Author: Eunchan Kim <eunchan@opentitan.org>
+Date:   Thu Feb 17 12:20:08 2022 -0800
 
-    [dv] Add rv_core_ibex chip level tests to testplan
+    [kmac] Regenerate TOP Earlgrey
     
-    Signed-off-by: Greg Chadwick <gac@lowrisc.org>
+    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>
 
 """
 
