6648e25e1b7f8c07ed7e558f62481d49c75a527a
LiteX Robot <robot@timvideos.us>
2022-02-18 23:27:01 +0000

    Updating pythondata-misc-opentitan to 0.0.post10445
    Updated data to v0.0-10319-gcf1c301d9 based on cf1c301d96ac9242f974972c65026c3a71cb86a7 from https://github.com/lowRISC/opentitan.
    > commit cf1c301d96ac9242f974972c65026c3a71cb86a7
    > Author: Nikola Miladinovic <nikola.miladinovic@ensilica.com>
    > Date:   Thu Feb 10 15:10:05 2022 +0000
    >
    >     [flash_ctrl] ADD ERASE SUSPEND TEST
    >
    >     Test erase suspend while random erase is in progress and also when there is
    >     no erase at all. Added support for backdoor reading of erased bank and page.
    >
    >     Signed-off-by: Nikola Miladinovic <nikola.miladinovic@ensilica.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10443"
-version_tuple = (0, 0, 10443)
+version_str = "0.0.post10445"
+version_tuple = (0, 0, 10445)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10443")
+    pversion = V("0.0.post10445")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10317"
-data_version_tuple = (0, 0, 10317)
+data_version_str = "0.0.post10319"
+data_version_tuple = (0, 0, 10319)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10317")
+    pdata_version = V("0.0.post10319")
 except ImportError:
     pass
-data_git_hash = "5ee89951047ce0f3efe6a3b663451c20305e6faf"
-data_git_describe = "v0.0-10317-g5ee899510"
+data_git_hash = "cf1c301d96ac9242f974972c65026c3a71cb86a7"
+data_git_describe = "v0.0-10319-gcf1c301d9"
 data_git_msg = """\
-commit 5ee89951047ce0f3efe6a3b663451c20305e6faf
-Author: Eunchan Kim <eunchan@opentitan.org>
-Date:   Fri Feb 18 14:10:44 2022 -0800
+commit cf1c301d96ac9242f974972c65026c3a71cb86a7
+Author: Nikola Miladinovic <nikola.miladinovic@ensilica.com>
+Date:   Thu Feb 10 15:10:05 2022 +0000
 
-    [lint] Increase the unroll count
+    [flash_ctrl] ADD ERASE SUSPEND TEST
     
-    This commit increases Ascentlint max unroll count from 1024 (default) to
-    3200 (max Kmac state x2)
+    Test erase suspend while random erase is in progress and also when there is
+    no erase at all. Added support for backdoor reading of erased bank and page.
     
-    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>
+    Signed-off-by: Nikola Miladinovic <nikola.miladinovic@ensilica.com>
 
 """
 
