0f84ed6c4a680e5c3b195b4256bf20c4795914f9
LiteX Robot <robot@timvideos.us>
2022-02-22 20:29:18 +0000

    Updating pythondata-misc-opentitan to 0.0.post10473
    Updated data to v0.0-10347-g7d1bf1e3c based on 7d1bf1e3cb81ac75ea0a966d0178064b1b6d2c12 from https://github.com/lowRISC/opentitan.
    > commit 7d1bf1e3cb81ac75ea0a966d0178064b1b6d2c12
    > Author: Weicai Yang <weicai@google.com>
    > Date:   Thu Feb 17 15:13:47 2022 -0800
    >
    >     [sramd/dv] Enable sec_cm test for sram
    >
    >     Add additional checks
    >      - Check alert and `status.init_error` is set.
    >      -  After injecting faults, reading any address should return 0. #10909
    >
    >     Signed-off-by: Weicai Yang <weicai@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10469"
-version_tuple = (0, 0, 10469)
+version_str = "0.0.post10473"
+version_tuple = (0, 0, 10473)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10469")
+    pversion = V("0.0.post10473")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10343"
-data_version_tuple = (0, 0, 10343)
+data_version_str = "0.0.post10347"
+data_version_tuple = (0, 0, 10347)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10343")
+    pdata_version = V("0.0.post10347")
 except ImportError:
     pass
-data_git_hash = "ae81bdd2c53d743d20accaf6b12040e07595370e"
-data_git_describe = "v0.0-10343-gae81bdd2c"
+data_git_hash = "7d1bf1e3cb81ac75ea0a966d0178064b1b6d2c12"
+data_git_describe = "v0.0-10347-g7d1bf1e3c"
 data_git_msg = """\
-commit ae81bdd2c53d743d20accaf6b12040e07595370e
-Author: Timothy Chen <timothytim@google.com>
-Date:   Fri Feb 18 21:06:14 2022 -0800
+commit 7d1bf1e3cb81ac75ea0a966d0178064b1b6d2c12
+Author: Weicai Yang <weicai@google.com>
+Date:   Thu Feb 17 15:13:47 2022 -0800
 
-    [rstmgr, dv] corresponding dv updates for crash_dump change
+    [sramd/dv] Enable sec_cm test for sram
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Add additional checks
+     - Check alert and `status.init_error` is set.
+     -  After injecting faults, reading any address should return 0. #10909
+    
+    Signed-off-by: Weicai Yang <weicai@google.com>
 
 """
 
