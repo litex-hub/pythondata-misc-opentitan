6ddc24deedc84df6555e461903cf08101eba245b
LiteX Robot <robot@timvideos.us>
2022-02-22 17:27:17 +0000

    Updating pythondata-misc-opentitan to 0.0.post10465
    Updated data to v0.0-10339-g8bb8cc98c based on 8bb8cc98c47a3b63e4a528005f3fdbd3b728fdb2 from https://github.com/lowRISC/opentitan.
    > commit 8bb8cc98c47a3b63e4a528005f3fdbd3b728fdb2
    > Author: Prajwala Puttappa <prajwalaputtappa@lowrisc.org>
    > Date:   Tue Feb 22 11:12:23 2022 +0000
    >
    >     [rom_ctrl, dv] Fix for rom_ctrl_kmac_err_chk regression failure
    >
    >     Fatal alert is not triggered unless error response is signalled from
    >     Kmac. Kmac signals an error only after kmac_data_o.last is asserted.
    >     Last is asserted after all the address spaces in the rom is read and
    >     sent to KMAC. Ths depends on the delay between ready-valid handshake.
    >     Therefore, delay to expect the alert has been increased to ensure that
    >     we wait till last has been asserted.
    >
    >     Signed-off-by: Prajwala Puttappa <prajwalaputtappa@lowrisc.org>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,39 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10464"
-version_tuple = (0, 0, 10464)
+version_str = "0.0.post10465"
+version_tuple = (0, 0, 10465)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10464")
+    pversion = V("0.0.post10465")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10338"
-data_version_tuple = (0, 0, 10338)
+data_version_str = "0.0.post10339"
+data_version_tuple = (0, 0, 10339)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10338")
+    pdata_version = V("0.0.post10339")
 except ImportError:
     pass
-data_git_hash = "75e2a78669091adf5dcab7bb7e4e1bbfb3484886"
-data_git_describe = "v0.0-10338-g75e2a7866"
+data_git_hash = "8bb8cc98c47a3b63e4a528005f3fdbd3b728fdb2"
+data_git_describe = "v0.0-10339-g8bb8cc98c"
 data_git_msg = """\
-commit 75e2a78669091adf5dcab7bb7e4e1bbfb3484886
-Author: Nikola Miladinovic <nikola.miladinovic@ensilica.com>
-Date:   Mon Feb 21 11:35:44 2022 +0000
+commit 8bb8cc98c47a3b63e4a528005f3fdbd3b728fdb2
+Author: Prajwala Puttappa <prajwalaputtappa@lowrisc.org>
+Date:   Tue Feb 22 11:12:23 2022 +0000
 
-    [flash_ctrl] Small fix of rd buff eviction test
+    [rom_ctrl, dv] Fix for rom_ctrl_kmac_err_chk regression failure
     
-    Due to update of flash read backdoor function, this test was failing
-    in regression. Simple fix is implemented to directly modify flash operations
+    Fatal alert is not triggered unless error response is signalled from
+    Kmac. Kmac signals an error only after kmac_data_o.last is asserted.
+    Last is asserted after all the address spaces in the rom is read and
+    sent to KMAC. Ths depends on the delay between ready-valid handshake.
+    Therefore, delay to expect the alert has been increased to ensure that
+    we wait till last has been asserted.
     
-    Signed-off-by: Nikola Miladinovic <nikola.miladinovic@ensilica.com>
+    Signed-off-by: Prajwala Puttappa <prajwalaputtappa@lowrisc.org>
 
 """
 
