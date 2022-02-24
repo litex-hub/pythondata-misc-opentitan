f05348100145f08cdcc6f7fae0b0961a7232da5a
LiteX Robot <robot@timvideos.us>
2022-02-24 02:46:11 +0000

    Updating pythondata-misc-opentitan to 0.0.post10506
    Updated data to v0.0-10380-g19d4f9a4f based on 19d4f9a4f817e4c379c8b48d678a2eaf6ad335a6 from https://github.com/lowRISC/opentitan.
    > commit 19d4f9a4f817e4c379c8b48d678a2eaf6ad335a6
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Thu Feb 17 18:06:07 2022 -0800
    >
    >     [usbdev] Remove aon_usb_events_active from async reset
    >
    >     - fixes #10879
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,42 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10505"
-version_tuple = (0, 0, 10505)
+version_str = "0.0.post10506"
+version_tuple = (0, 0, 10506)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10505")
+    pversion = V("0.0.post10506")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10379"
-data_version_tuple = (0, 0, 10379)
+data_version_str = "0.0.post10380"
+data_version_tuple = (0, 0, 10380)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10379")
+    pdata_version = V("0.0.post10380")
 except ImportError:
     pass
-data_git_hash = "df0596a177a1c5c6f249db7858a054ff343cf7f2"
-data_git_describe = "v0.0-10379-gdf0596a17"
+data_git_hash = "19d4f9a4f817e4c379c8b48d678a2eaf6ad335a6"
+data_git_describe = "v0.0-10380-g19d4f9a4f"
 data_git_msg = """\
-commit df0596a177a1c5c6f249db7858a054ff343cf7f2
-Author: Michael Schaffner <msf@opentitan.org>
-Date:   Thu Feb 17 16:45:16 2022 -0800
+commit 19d4f9a4f817e4c379c8b48d678a2eaf6ad335a6
+Author: Timothy Chen <timothytim@google.com>
+Date:   Thu Feb 17 18:06:07 2022 -0800
 
-    [sram_ctrl] Align behavior of global and local escalation
+    [usbdev] Remove aon_usb_events_active from async reset
     
-    Before, global escalation caused the sram_ctrl to block transactions on
-    top of other defenses (like scrapping the key, blanking return data).
-    Local escalation on the other hand did not cause the sram_ctrl to block
-    transactions.
+    - fixes #10879
     
-    This patch aligns the behavior of both escalations (block transactions),
-    since both are fatal conditions.
-    
-    See #10909 for context.
-    
-    Signed-off-by: Michael Schaffner <msf@opentitan.org>
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
