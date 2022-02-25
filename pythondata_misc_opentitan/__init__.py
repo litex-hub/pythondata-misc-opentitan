792a78ea1e2fd5e82bb26825c214eb0ff8851de3
LiteX Robot <robot@timvideos.us>
2022-02-25 21:25:31 +0000

    Updating pythondata-misc-opentitan to 0.0.post10557
    Updated data to v0.0-10431-g354bf0588 based on 354bf05889db9c10d7a34f835cea2428dcdb8b64 from https://github.com/lowRISC/opentitan.
    > commit 354bf05889db9c10d7a34f835cea2428dcdb8b64
    > Author: TIM EWINS <tim.ewins@ensilica.com>
    > Date:   Tue Feb 15 15:21:36 2022 +0000
    >
    >     [flash_ctrl] ADD TEST FOR THE RMA FEATURE
    >
    >     Test the Flash RMA Functionality
    >
    >     Fash Partitions are Erased, Programmed and then Wiped by the RMA.  After the RMA has completed,
    >     The test checks that SW Access to the FLASH is denied, and after a subsequent Reset, the Flash
    >     is confirmed to be set to data other than that previously programmed.
    >     Each run of the test performs 4 RMA cycles, each seperated by a Reset.
    >
    >     Signed-off-by: TIM EWINS <tim.ewins@ensilica.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,39 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10556"
-version_tuple = (0, 0, 10556)
+version_str = "0.0.post10557"
+version_tuple = (0, 0, 10557)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10556")
+    pversion = V("0.0.post10557")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10430"
-data_version_tuple = (0, 0, 10430)
+data_version_str = "0.0.post10431"
+data_version_tuple = (0, 0, 10431)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10430")
+    pdata_version = V("0.0.post10431")
 except ImportError:
     pass
-data_git_hash = "7e5ab7acc603347b7926e3d5f1bdba2764bd68d9"
-data_git_describe = "v0.0-10430-g7e5ab7acc"
+data_git_hash = "354bf05889db9c10d7a34f835cea2428dcdb8b64"
+data_git_describe = "v0.0-10431-g354bf0588"
 data_git_msg = """\
-commit 7e5ab7acc603347b7926e3d5f1bdba2764bd68d9
-Author: Timothy Chen <timothytim@google.com>
-Date:   Wed Feb 23 17:24:51 2022 -0800
+commit 354bf05889db9c10d7a34f835cea2428dcdb8b64
+Author: TIM EWINS <tim.ewins@ensilica.com>
+Date:   Tue Feb 15 15:21:36 2022 +0000
 
-    [rstmgr] documentation updates based on d2s feedback
+    [flash_ctrl] ADD TEST FOR THE RMA FEATURE
     
-    - there is a separate readability feedback that will be done in a different PR.
+    Test the Flash RMA Functionality
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Fash Partitions are Erased, Programmed and then Wiped by the RMA.  After the RMA has completed,
+    The test checks that SW Access to the FLASH is denied, and after a subsequent Reset, the Flash
+    is confirmed to be set to data other than that previously programmed.
+    Each run of the test performs 4 RMA cycles, each seperated by a Reset.
+    
+    Signed-off-by: TIM EWINS <tim.ewins@ensilica.com>
 
 """
 
