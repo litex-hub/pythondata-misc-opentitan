e6e04d49a138cb1d2f6bef5a71bf51d848f0e4e5
LiteX Robot <robot@timvideos.us>
2022-02-25 18:32:14 +0000

    Updating pythondata-misc-opentitan to 0.0.post10554
    Updated data to v0.0-10428-g6497101e7 based on 6497101e7a591c70c535822e09f8e324cea97455 from https://github.com/lowRISC/opentitan.
    > commit 6497101e7a591c70c535822e09f8e324cea97455
    > Author: Guillermo Maturana <maturana@google.com>
    > Date:   Fri Feb 11 09:47:00 2022 -0800
    >
    >     [doc/flash_crtl] Fix some of the wording
    >
    >     Signed-off-by: Guillermo Maturana <maturana@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,41 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10552"
-version_tuple = (0, 0, 10552)
+version_str = "0.0.post10554"
+version_tuple = (0, 0, 10554)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10552")
+    pversion = V("0.0.post10554")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10426"
-data_version_tuple = (0, 0, 10426)
+data_version_str = "0.0.post10428"
+data_version_tuple = (0, 0, 10428)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10426")
+    pdata_version = V("0.0.post10428")
 except ImportError:
     pass
-data_git_hash = "419d7b14251c2aa8f81b71ff8efde9b31223f52f"
-data_git_describe = "v0.0-10426-g419d7b142"
+data_git_hash = "6497101e7a591c70c535822e09f8e324cea97455"
+data_git_describe = "v0.0-10428-g6497101e7"
 data_git_msg = """\
-commit 419d7b14251c2aa8f81b71ff8efde9b31223f52f
-Author: Cindy Chen <chencindy@opentitan.org>
-Date:   Wed Feb 23 17:14:04 2022 -0800
+commit 6497101e7a591c70c535822e09f8e324cea97455
+Author: Guillermo Maturana <maturana@google.com>
+Date:   Fri Feb 11 09:47:00 2022 -0800
 
-    [fpv/pinmux] Pinmux FPV tb fix
+    [doc/flash_crtl] Fix some of the wording
     
-    This PR fixes pinmux FPV:
-    
-    1). Blackbox usb module because we only check connectivity here.
-    2). Disable assertions for `SyncCheckTransients*` because in formal we
-      do not constraint inputs.
-    3). Updated lc_ctrl_pkg::On/Off to not use `mubi4_test_true_strict`
-      function.
-    4). Update a few design input related assertions to use assumptions.
-    
-    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
+    Signed-off-by: Guillermo Maturana <maturana@google.com>
 
 """
 
