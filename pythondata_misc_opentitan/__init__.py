de7609fd12cc5ecaaa3c84f4ce43e74bd5a62b36
LiteX Robot <robot@timvideos.us>
2022-02-25 23:28:19 +0000

    Updating pythondata-misc-opentitan to 0.0.post10562
    Updated data to v0.0-10436-g8073b2262 based on 8073b2262a3970fca87dfdee1e624235f618f2bd from https://github.com/lowRISC/opentitan.
    > commit 8073b2262a3970fca87dfdee1e624235f618f2bd
    > Author: Kosta Kojdic <kosta.kojdic@ensilica.com>
    > Date:   Thu Feb 24 16:16:33 2022 +0000
    >
    >     Address and payload passthrough swap scenarios
    >
    >     Signed-off-by: Kosta Kojdic <kosta.kojdic@ensilica.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,39 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10557"
-version_tuple = (0, 0, 10557)
+version_str = "0.0.post10562"
+version_tuple = (0, 0, 10562)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10557")
+    pversion = V("0.0.post10562")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10431"
-data_version_tuple = (0, 0, 10431)
+data_version_str = "0.0.post10436"
+data_version_tuple = (0, 0, 10436)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10431")
+    pdata_version = V("0.0.post10436")
 except ImportError:
     pass
-data_git_hash = "354bf05889db9c10d7a34f835cea2428dcdb8b64"
-data_git_describe = "v0.0-10431-g354bf0588"
+data_git_hash = "8073b2262a3970fca87dfdee1e624235f618f2bd"
+data_git_describe = "v0.0-10436-g8073b2262"
 data_git_msg = """\
-commit 354bf05889db9c10d7a34f835cea2428dcdb8b64
-Author: TIM EWINS <tim.ewins@ensilica.com>
-Date:   Tue Feb 15 15:21:36 2022 +0000
+commit 8073b2262a3970fca87dfdee1e624235f618f2bd
+Author: Kosta Kojdic <kosta.kojdic@ensilica.com>
+Date:   Thu Feb 24 16:16:33 2022 +0000
 
-    [flash_ctrl] ADD TEST FOR THE RMA FEATURE
+    Address and payload passthrough swap scenarios
     
-    Test the Flash RMA Functionality
-    
-    Fash Partitions are Erased, Programmed and then Wiped by the RMA.  After the RMA has completed,
-    The test checks that SW Access to the FLASH is denied, and after a subsequent Reset, the Flash
-    is confirmed to be set to data other than that previously programmed.
-    Each run of the test performs 4 RMA cycles, each seperated by a Reset.
-    
-    Signed-off-by: TIM EWINS <tim.ewins@ensilica.com>
+    Signed-off-by: Kosta Kojdic <kosta.kojdic@ensilica.com>
 
 """
 
