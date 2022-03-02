4b33fe318a3895377a1ac5afdca4622c7efb5579
LiteX Robot <robot@timvideos.us>
2022-03-02 15:29:43 +0000

    Updating pythondata-misc-opentitan to 0.0.post10651
    Updated data to v0.0-10525-g285c5f2df based on 285c5f2df0e0d6c635483d11285df55eae30f9f2 from https://github.com/lowRISC/opentitan.
    > commit 285c5f2df0e0d6c635483d11285df55eae30f9f2
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Wed Mar 2 11:19:19 2022 +0000
    >
    >     [otbn,dv] Use always (not always_ff) for a block in otbn_core_model
    >
    >     Xcelium correctly complains about using an always_ff here: we also
    >     initialise model_state in an (implicit) inital block, which isn't
    >     allowed by always_ff (a construct that's really designed for
    >     synthesizable code).
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,37 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10644"
-version_tuple = (0, 0, 10644)
+version_str = "0.0.post10651"
+version_tuple = (0, 0, 10651)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10644")
+    pversion = V("0.0.post10651")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10518"
-data_version_tuple = (0, 0, 10518)
+data_version_str = "0.0.post10525"
+data_version_tuple = (0, 0, 10525)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10518")
+    pdata_version = V("0.0.post10525")
 except ImportError:
     pass
-data_git_hash = "b04083ba8b2b5c671f962654b02457ca2dbe936a"
-data_git_describe = "v0.0-10518-gb04083ba8"
+data_git_hash = "285c5f2df0e0d6c635483d11285df55eae30f9f2"
+data_git_describe = "v0.0-10525-g285c5f2df"
 data_git_msg = """\
-commit b04083ba8b2b5c671f962654b02457ca2dbe936a
-Author: Greg Chadwick <gac@lowrisc.org>
-Date:   Mon Feb 21 17:08:54 2022 +0000
+commit 285c5f2df0e0d6c635483d11285df55eae30f9f2
+Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
+Date:   Wed Mar 2 11:19:19 2022 +0000
 
-    [rv_core_ibex, rtl] Add local escalation path to disable fetch
+    [otbn,dv] Use always (not always_ff) for a block in otbn_core_model
     
-    When a major core error is seen the local escalation path immediately
-    shuts down fetch enable in addition to raising a fatal alert.
+    Xcelium correctly complains about using an always_ff here: we also
+    initialise model_state in an (implicit) inital block, which isn't
+    allowed by always_ff (a construct that's really designed for
+    synthesizable code).
     
-    Signed-off-by: Greg Chadwick <gac@lowrisc.org>
+    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
 """
 
