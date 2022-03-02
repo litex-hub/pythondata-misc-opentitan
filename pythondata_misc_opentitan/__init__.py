7f4482e252efbdb2f41cd99dcd9c4658101b526c
LiteX Robot <robot@timvideos.us>
2022-03-02 18:32:16 +0000

    Updating pythondata-misc-opentitan to 0.0.post10656
    Updated data to v0.0-10530-g2ed086ff5 based on 2ed086ff5f6667491b9c019de2e6122ad6fa2572 from https://github.com/lowRISC/opentitan.
    > commit 2ed086ff5f6667491b9c019de2e6122ad6fa2572
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Tue Mar 1 16:05:16 2022 -0800
    >
    >     [tlul] adjust tlul_sram_byte enable condition
    >
    >     Previously, the tlul_sram_byte module was enabled whenever an adapter sram
    >     allowed both byte access and checked for integrity.
    >
    >     This is not the correct enable condition. The tlul_sram_byte is meant to
    >     generate correct integrity when the downstream memory actually stores
    >     full word integrity and requires a read modified write for byte accesses.
    >
    >     If a module simply checks for integrity, it does not say anything about
    >     whether the downstream module stores integrity and whether byte writes require
    >     a read modified write.
    >
    >     Instead, change the condition to byte access AND data integrity passthrough are
    >     enabled. Data integrity passthrough implies the downstream memory will make use of
    >     extra integrity in some way and thus requires correct read modified write handling
    >     on byte accesses.
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,48 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10653"
-version_tuple = (0, 0, 10653)
+version_str = "0.0.post10656"
+version_tuple = (0, 0, 10656)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10653")
+    pversion = V("0.0.post10656")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10527"
-data_version_tuple = (0, 0, 10527)
+data_version_str = "0.0.post10530"
+data_version_tuple = (0, 0, 10530)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10527")
+    pdata_version = V("0.0.post10530")
 except ImportError:
     pass
-data_git_hash = "9f60770816a837425c5cc3faf71d748ae7e7f3b9"
-data_git_describe = "v0.0-10527-g9f6077081"
+data_git_hash = "2ed086ff5f6667491b9c019de2e6122ad6fa2572"
+data_git_describe = "v0.0-10530-g2ed086ff5"
 data_git_msg = """\
-commit 9f60770816a837425c5cc3faf71d748ae7e7f3b9
-Author: Steve Nelson <steve.nelson@wdc.com>
-Date:   Tue Mar 1 10:58:05 2022 -0800
+commit 2ed086ff5f6667491b9c019de2e6122ad6fa2572
+Author: Timothy Chen <timothytim@google.com>
+Date:   Tue Mar 1 16:05:16 2022 -0800
 
-    [edn/dv] Allow/generate additional command data
+    [tlul] adjust tlul_sram_byte enable condition
     
-    Signed-off-by: Steve Nelson <steve.nelson@wdc.com>
+    Previously, the tlul_sram_byte module was enabled whenever an adapter sram
+    allowed both byte access and checked for integrity.
+    
+    This is not the correct enable condition. The tlul_sram_byte is meant to
+    generate correct integrity when the downstream memory actually stores
+    full word integrity and requires a read modified write for byte accesses.
+    
+    If a module simply checks for integrity, it does not say anything about
+    whether the downstream module stores integrity and whether byte writes require
+    a read modified write.
+    
+    Instead, change the condition to byte access AND data integrity passthrough are
+    enabled. Data integrity passthrough implies the downstream memory will make use of
+    extra integrity in some way and thus requires correct read modified write handling
+    on byte accesses.
+    
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
