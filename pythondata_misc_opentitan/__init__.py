6340ff4d6f216d94a92d7d4077e568d60ede296b
LiteX Robot <robot@timvideos.us>
2022-03-02 16:37:37 +0000

    Updating pythondata-misc-opentitan to 0.0.post10653
    Updated data to v0.0-10527-g9f6077081 based on 9f60770816a837425c5cc3faf71d748ae7e7f3b9 from https://github.com/lowRISC/opentitan.
    > commit 9f60770816a837425c5cc3faf71d748ae7e7f3b9
    > Author: Steve Nelson <steve.nelson@wdc.com>
    > Date:   Tue Mar 1 10:58:05 2022 -0800
    >
    >     [edn/dv] Allow/generate additional command data
    >
    >     Signed-off-by: Steve Nelson <steve.nelson@wdc.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,37 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10651"
-version_tuple = (0, 0, 10651)
+version_str = "0.0.post10653"
+version_tuple = (0, 0, 10653)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10651")
+    pversion = V("0.0.post10653")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10525"
-data_version_tuple = (0, 0, 10525)
+data_version_str = "0.0.post10527"
+data_version_tuple = (0, 0, 10527)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10525")
+    pdata_version = V("0.0.post10527")
 except ImportError:
     pass
-data_git_hash = "285c5f2df0e0d6c635483d11285df55eae30f9f2"
-data_git_describe = "v0.0-10525-g285c5f2df"
+data_git_hash = "9f60770816a837425c5cc3faf71d748ae7e7f3b9"
+data_git_describe = "v0.0-10527-g9f6077081"
 data_git_msg = """\
-commit 285c5f2df0e0d6c635483d11285df55eae30f9f2
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Wed Mar 2 11:19:19 2022 +0000
+commit 9f60770816a837425c5cc3faf71d748ae7e7f3b9
+Author: Steve Nelson <steve.nelson@wdc.com>
+Date:   Tue Mar 1 10:58:05 2022 -0800
 
-    [otbn,dv] Use always (not always_ff) for a block in otbn_core_model
+    [edn/dv] Allow/generate additional command data
     
-    Xcelium correctly complains about using an always_ff here: we also
-    initialise model_state in an (implicit) inital block, which isn't
-    allowed by always_ff (a construct that's really designed for
-    synthesizable code).
-    
-    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    Signed-off-by: Steve Nelson <steve.nelson@wdc.com>
 
 """
 
