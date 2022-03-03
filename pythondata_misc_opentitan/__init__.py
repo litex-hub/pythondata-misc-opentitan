495607297a3e18f280b168866594f6177e636dfd
LiteX Robot <robot@timvideos.us>
2022-03-03 13:31:26 +0000

    Updating pythondata-misc-opentitan to 0.0.post10676
    Updated data to v0.0-10550-g196b79a17 based on 196b79a1748d641d0ed237f763f4d47fdaf5e0e7 from https://github.com/lowRISC/opentitan.
    > commit 196b79a1748d641d0ed237f763f4d47fdaf5e0e7
    > Author: Alex Bradbury <asb@lowrisc.org>
    > Date:   Thu Mar 3 07:49:37 2022 +0000
    >
    >     [misc] Remove myself from COMMITTERS/CODEOWNERS
    >
    >     Signed-off-by: Alex Bradbury <asb@lowrisc.org>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,42 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10675"
-version_tuple = (0, 0, 10675)
+version_str = "0.0.post10676"
+version_tuple = (0, 0, 10676)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10675")
+    pversion = V("0.0.post10676")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10549"
-data_version_tuple = (0, 0, 10549)
+data_version_str = "0.0.post10550"
+data_version_tuple = (0, 0, 10550)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10549")
+    pdata_version = V("0.0.post10550")
 except ImportError:
     pass
-data_git_hash = "0d0fa1a09f8f43b697a8f0600325acce8d1a2751"
-data_git_describe = "v0.0-10549-g0d0fa1a09"
+data_git_hash = "196b79a1748d641d0ed237f763f4d47fdaf5e0e7"
+data_git_describe = "v0.0-10550-g196b79a17"
 data_git_msg = """\
-commit 0d0fa1a09f8f43b697a8f0600325acce8d1a2751
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Tue Mar 1 16:30:40 2022 +0000
+commit 196b79a1748d641d0ed237f763f4d47fdaf5e0e7
+Author: Alex Bradbury <asb@lowrisc.org>
+Date:   Thu Mar 3 07:49:37 2022 +0000
 
-    [otbn,dv] Remove support for the model in the OTBN module
+    [misc] Remove myself from COMMITTERS/CODEOWNERS
     
-    This was originally put in so that we could do system-level testing of
-    OTBN before the implementation was complete. Since then, it has been
-    used once or twice for performance tests where we needed to model the
-    Ibex side as well. But it's really rather difficult to maintain, and
-    leaves a bit of "DV-ish" code in the design folder.
-    
-    If we need to do something like this in future, it shouldn't be too
-    hard to knock together another frontend to the simulator (like
-    stepped.sv) and to build a testbench in Python.
-    
-    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    Signed-off-by: Alex Bradbury <asb@lowrisc.org>
 
 """
 
