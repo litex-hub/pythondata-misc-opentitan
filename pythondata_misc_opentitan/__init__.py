de5b62dc59e74ae8a65bd54f14d14788f7ef184f
LiteX Robot <robot@timvideos.us>
2022-03-03 11:23:39 +0000

    Updating pythondata-misc-opentitan to 0.0.post10675
    Updated data to v0.0-10549-g0d0fa1a09 based on 0d0fa1a09f8f43b697a8f0600325acce8d1a2751 from https://github.com/lowRISC/opentitan.
    > commit 0d0fa1a09f8f43b697a8f0600325acce8d1a2751
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Tue Mar 1 16:30:40 2022 +0000
    >
    >     [otbn,dv] Remove support for the model in the OTBN module
    >
    >     This was originally put in so that we could do system-level testing of
    >     OTBN before the implementation was complete. Since then, it has been
    >     used once or twice for performance tests where we needed to model the
    >     Ibex side as well. But it's really rather difficult to maintain, and
    >     leaves a bit of "DV-ish" code in the design folder.
    >
    >     If we need to do something like this in future, it shouldn't be too
    >     hard to knock together another frontend to the simulator (like
    >     stepped.sv) and to build a testbench in Python.
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,42 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10674"
-version_tuple = (0, 0, 10674)
+version_str = "0.0.post10675"
+version_tuple = (0, 0, 10675)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10674")
+    pversion = V("0.0.post10675")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10548"
-data_version_tuple = (0, 0, 10548)
+data_version_str = "0.0.post10549"
+data_version_tuple = (0, 0, 10549)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10548")
+    pdata_version = V("0.0.post10549")
 except ImportError:
     pass
-data_git_hash = "7f5aca5cc0b0a08d69e242661a8afd320a184e94"
-data_git_describe = "v0.0-10548-g7f5aca5cc"
+data_git_hash = "0d0fa1a09f8f43b697a8f0600325acce8d1a2751"
+data_git_describe = "v0.0-10549-g0d0fa1a09"
 data_git_msg = """\
-commit 7f5aca5cc0b0a08d69e242661a8afd320a184e94
-Author: Srikrishna Iyer <sriyer@google.com>
-Date:   Wed Mar 2 17:23:14 2022 -0800
+commit 0d0fa1a09f8f43b697a8f0600325acce8d1a2751
+Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
+Date:   Tue Mar 1 16:30:40 2022 +0000
 
-    [rv_dm dv] Add rv_dm SBA access test
+    [otbn,dv] Remove support for the model in the OTBN module
     
-    This commit updates the RV DM env and adds the SBA access test.
-    There is no checking at the moment- the checks will be implemented
-    in the scoreboard, which will be updated in the next PR.
+    This was originally put in so that we could do system-level testing of
+    OTBN before the implementation was complete. Since then, it has been
+    used once or twice for performance tests where we needed to model the
+    Ibex side as well. But it's really rather difficult to maintain, and
+    leaves a bit of "DV-ish" code in the design folder.
     
-    Signed-off-by: Srikrishna Iyer <sriyer@google.com>
+    If we need to do something like this in future, it shouldn't be too
+    hard to knock together another frontend to the simulator (like
+    stepped.sv) and to build a testbench in Python.
+    
+    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
 """
 
