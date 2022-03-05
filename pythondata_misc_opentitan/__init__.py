2679d9cb66acb5de6ec143cce18da8514490edf7
LiteX Robot <robot@timvideos.us>
2022-03-05 02:47:25 +0000

    Updating pythondata-misc-opentitan to 0.0.post10726
    Updated data to v0.0-10600-g4e0e44a3c based on 4e0e44a3c86943209c7002d2b54aac0821c89a3d from https://github.com/lowRISC/opentitan.
    > commit 4e0e44a3c86943209c7002d2b54aac0821c89a3d
    > Author: Michael Schaffner <msf@opentitan.org>
    > Date:   Wed Mar 2 15:13:58 2022 -0800
    >
    >     [pinmux] Update docs
    >
    >     Fix #11194
    >
    >     Signed-off-by: Michael Schaffner <msf@opentitan.org>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10725"
-version_tuple = (0, 0, 10725)
+version_str = "0.0.post10726"
+version_tuple = (0, 0, 10726)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10725")
+    pversion = V("0.0.post10726")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10599"
-data_version_tuple = (0, 0, 10599)
+data_version_str = "0.0.post10600"
+data_version_tuple = (0, 0, 10600)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10599")
+    pdata_version = V("0.0.post10600")
 except ImportError:
     pass
-data_git_hash = "988d400251c547fd0b41bf7b8feb8543174d0ee5"
-data_git_describe = "v0.0-10599-g988d40025"
+data_git_hash = "4e0e44a3c86943209c7002d2b54aac0821c89a3d"
+data_git_describe = "v0.0-10600-g4e0e44a3c"
 data_git_msg = """\
-commit 988d400251c547fd0b41bf7b8feb8543174d0ee5
-Author: Timothy Chen <timothytim@google.com>
-Date:   Thu Mar 3 16:21:53 2022 -0800
+commit 4e0e44a3c86943209c7002d2b54aac0821c89a3d
+Author: Michael Schaffner <msf@opentitan.org>
+Date:   Wed Mar 2 15:13:58 2022 -0800
 
-    [flash_ctrl] Fix init_busy glitches
+    [pinmux] Update docs
     
-    - identified by weicai in #11235
+    Fix #11194
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Michael Schaffner <msf@opentitan.org>
 
 """
 
