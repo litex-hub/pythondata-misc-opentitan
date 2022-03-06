46ad0d71650fd82b0814b0bcfdef57e2e995707a
LiteX Robot <robot@timvideos.us>
2022-03-06 01:30:14 +0000

    Updating pythondata-misc-opentitan to 0.0.post10731
    Updated data to v0.0-10605-g59181de51 based on 59181de51c70b3cd6eff7037c302dca775bd7d82 from https://github.com/lowRISC/opentitan.
    > commit 59181de51c70b3cd6eff7037c302dca775bd7d82
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Fri Mar 4 15:15:04 2022 -0800
    >
    >     [adc_ctrl] Adjust fsm power-up timing and counter clearing
    >
    >     - fixes #11256
    >     - Adjust fsm's such that initial power-up to sample and low power exit
    >       to sample have the same timing.  The IDLE state is removed as a result.
    >     - Update documentation to reflect change
    >     - Clear counters on adc disable otherwise the old count value carries over
    >       and affects the next round of detection.
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,39 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10730"
-version_tuple = (0, 0, 10730)
+version_str = "0.0.post10731"
+version_tuple = (0, 0, 10731)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10730")
+    pversion = V("0.0.post10731")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10604"
-data_version_tuple = (0, 0, 10604)
+data_version_str = "0.0.post10605"
+data_version_tuple = (0, 0, 10605)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10604")
+    pdata_version = V("0.0.post10605")
 except ImportError:
     pass
-data_git_hash = "3020470fc56252bd8db4a898a628d1ff761beeab"
-data_git_describe = "v0.0-10604-g3020470fc"
+data_git_hash = "59181de51c70b3cd6eff7037c302dca775bd7d82"
+data_git_describe = "v0.0-10605-g59181de51"
 data_git_msg = """\
-commit 3020470fc56252bd8db4a898a628d1ff761beeab
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Fri Mar 4 11:10:23 2022 +0000
+commit 59181de51c70b3cd6eff7037c302dca775bd7d82
+Author: Timothy Chen <timothytim@google.com>
+Date:   Fri Mar 4 15:15:04 2022 -0800
 
-    [ipgen] Mark the --config-file argument as required
+    [adc_ctrl] Adjust fsm power-up timing and counter clearing
     
-    The script unconditionally reads the configuration file (not
-    unreasonable: this tells it what to do!), so that argument must be
-    given.
+    - fixes #11256
+    - Adjust fsm's such that initial power-up to sample and low power exit
+      to sample have the same timing.  The IDLE state is removed as a result.
+    - Update documentation to reflect change
+    - Clear counters on adc disable otherwise the old count value carries over
+      and affects the next round of detection.
     
-    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
