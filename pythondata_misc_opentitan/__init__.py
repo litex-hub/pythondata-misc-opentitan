4529451aadfd9882043db3e6803348a482b42901
LiteX Robot <robot@timvideos.us>
2022-03-06 07:25:19 +0000

    Updating pythondata-misc-opentitan to 0.0.post10733
    Updated data to v0.0-10607-ga44bb9681 based on a44bb9681ea186fb8ca2171fdceee86efa35d272 from https://github.com/lowRISC/opentitan.
    > commit a44bb9681ea186fb8ca2171fdceee86efa35d272
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Fri Mar 4 16:45:07 2022 -0800
    >
    >     [fpv/sec cm] Update the list of sec IPs
    >
    >     This PR updates the IPs that has security countermeasures.
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,39 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10731"
-version_tuple = (0, 0, 10731)
+version_str = "0.0.post10733"
+version_tuple = (0, 0, 10733)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10731")
+    pversion = V("0.0.post10733")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10605"
-data_version_tuple = (0, 0, 10605)
+data_version_str = "0.0.post10607"
+data_version_tuple = (0, 0, 10607)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10605")
+    pdata_version = V("0.0.post10607")
 except ImportError:
     pass
-data_git_hash = "59181de51c70b3cd6eff7037c302dca775bd7d82"
-data_git_describe = "v0.0-10605-g59181de51"
+data_git_hash = "a44bb9681ea186fb8ca2171fdceee86efa35d272"
+data_git_describe = "v0.0-10607-ga44bb9681"
 data_git_msg = """\
-commit 59181de51c70b3cd6eff7037c302dca775bd7d82
-Author: Timothy Chen <timothytim@google.com>
-Date:   Fri Mar 4 15:15:04 2022 -0800
+commit a44bb9681ea186fb8ca2171fdceee86efa35d272
+Author: Cindy Chen <chencindy@opentitan.org>
+Date:   Fri Mar 4 16:45:07 2022 -0800
 
-    [adc_ctrl] Adjust fsm power-up timing and counter clearing
+    [fpv/sec cm] Update the list of sec IPs
     
-    - fixes #11256
-    - Adjust fsm's such that initial power-up to sample and low power exit
-      to sample have the same timing.  The IDLE state is removed as a result.
-    - Update documentation to reflect change
-    - Clear counters on adc disable otherwise the old count value carries over
-      and affects the next round of detection.
+    This PR updates the IPs that has security countermeasures.
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
 """
 
