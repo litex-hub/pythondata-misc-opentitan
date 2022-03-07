de0840e735b7fe6fdba6dc5cab747c6e970ea3d0
LiteX Robot <robot@timvideos.us>
2022-03-07 16:38:45 +0000

    Updating pythondata-misc-opentitan to 0.0.post10743
    Updated data to v0.0-10617-g03ee2dcb4 based on 03ee2dcb4c0c1296421ecdb9fbeddf071a67141f from https://github.com/lowRISC/opentitan.
    > commit 03ee2dcb4c0c1296421ecdb9fbeddf071a67141f
    > Author: Nigel Scales <nigel.scales@gmail.com>
    > Date:   Mon Mar 7 13:28:07 2022 +0000
    >
    >     [adc_ctrl/dv] Added counter tests
    >
    >     - Added tests
    >       - adc_ctrl_poweron_counter
    >       - adc_ctrl_lowpower_counter
    >     - Added test sequences
    >       - adc_ctrl_counter_vseq
    >         Randomize wakeup_time powerup_time and low/high/one shot power mode, wait for data then repeat.
    >       - adc_ctrl_poweron_counter_vseq
    >         Extends adc_ctrl_counter_vseq for normal and one shot mode.
    >       - adc_ctrl_lowpower_counter_vseq
    >         Extends adc_ctrl_counter_vseq for low power mode.
    >     - Added assertions
    >       - WakeupTime_A - checks wakeup timing in low power mode
    >       - EnterLowPower_A - Checks ADC controller enters low power mode
    >     - Updated model
    >       - Updated to latest spec / RTL
    >       - Modelled adc_fsm_reset register
    >       - Modelled interrupt registers for One Shot mode.
    >     - Updated adc_ctrl_filters_polled_vseq to perform FSM reset at the end of each iteration as
    >     to make sure model and RTL are synchronised before each new configuration
    >     - Updated PwrupTime_A to new spec (programmed value + 1) cycles
    >     - Enabled PwrupTime_A for all tests and removed plusarg control
    >
    >     Signed-off-by: Nigel Scales <nigel.scales@gmail.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,37 +4,54 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10742"
-version_tuple = (0, 0, 10742)
+version_str = "0.0.post10743"
+version_tuple = (0, 0, 10743)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10742")
+    pversion = V("0.0.post10743")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10616"
-data_version_tuple = (0, 0, 10616)
+data_version_str = "0.0.post10617"
+data_version_tuple = (0, 0, 10617)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10616")
+    pdata_version = V("0.0.post10617")
 except ImportError:
     pass
-data_git_hash = "2b37bc7b9c0a3574ac71bdb3b0afe27425a629e9"
-data_git_describe = "v0.0-10616-g2b37bc7b9"
+data_git_hash = "03ee2dcb4c0c1296421ecdb9fbeddf071a67141f"
+data_git_describe = "v0.0-10617-g03ee2dcb4"
 data_git_msg = """\
-commit 2b37bc7b9c0a3574ac71bdb3b0afe27425a629e9
-Author: Timothy Trippel <ttrippel@google.com>
-Date:   Fri Mar 4 17:27:08 2022 -0800
+commit 03ee2dcb4c0c1296421ecdb9fbeddf071a67141f
+Author: Nigel Scales <nigel.scales@gmail.com>
+Date:   Mon Mar 7 13:28:07 2022 +0000
 
-    [dif/pwm] Make X macro names consistent across DIF libs
+    [adc_ctrl/dv] Added counter tests
     
-    This commit aims to make then names of X macros (used for code gen)
-    consistent across DIF libraries. X macros simplify maintenance of DIF
-    libraries that rely on a specific number of resources (e.g., pins,
-    channels, alerts, etc.) to be available in the HW.
+    - Added tests
+      - adc_ctrl_poweron_counter
+      - adc_ctrl_lowpower_counter
+    - Added test sequences
+      - adc_ctrl_counter_vseq
+        Randomize wakeup_time powerup_time and low/high/one shot power mode, wait for data then repeat.
+      - adc_ctrl_poweron_counter_vseq
+        Extends adc_ctrl_counter_vseq for normal and one shot mode.
+      - adc_ctrl_lowpower_counter_vseq
+        Extends adc_ctrl_counter_vseq for low power mode.
+    - Added assertions
+      - WakeupTime_A - checks wakeup timing in low power mode
+      - EnterLowPower_A - Checks ADC controller enters low power mode
+    - Updated model
+      - Updated to latest spec / RTL
+      - Modelled adc_fsm_reset register
+      - Modelled interrupt registers for One Shot mode.
+    - Updated adc_ctrl_filters_polled_vseq to perform FSM reset at the end of each iteration as
+    to make sure model and RTL are synchronised before each new configuration
+    - Updated PwrupTime_A to new spec (programmed value + 1) cycles
+    - Enabled PwrupTime_A for all tests and removed plusarg control
     
-    Signed-off-by: Timothy Trippel <ttrippel@google.com>
+    Signed-off-by: Nigel Scales <nigel.scales@gmail.com>
 
 """
 
