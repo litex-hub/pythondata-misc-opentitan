14cf67e4dde55258da81dc5e4aa8d715a77a6e8c
LiteX Robot <robot@timvideos.us>
2022-03-07 17:28:57 +0000

    Updating pythondata-misc-opentitan to 0.0.post10747
    Updated data to v0.0-10621-g54b949255 based on 54b949255a8c463ea365126d90c39cd26c462638 from https://github.com/lowRISC/opentitan.
    > commit 54b949255a8c463ea365126d90c39cd26c462638
    > Author: Michael Tempelmeier <michael.tempelmeier@gi-de.com>
    > Date:   Mon Feb 28 16:36:53 2022 +0100
    >
    >     [kmac] changed hash-threshold reg to shadow reg
    >
    >     Signed-off-by: Michael Tempelmeier <michael.tempelmeier@gi-de.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,54 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10743"
-version_tuple = (0, 0, 10743)
+version_str = "0.0.post10747"
+version_tuple = (0, 0, 10747)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10743")
+    pversion = V("0.0.post10747")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10617"
-data_version_tuple = (0, 0, 10617)
+data_version_str = "0.0.post10621"
+data_version_tuple = (0, 0, 10621)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10617")
+    pdata_version = V("0.0.post10621")
 except ImportError:
     pass
-data_git_hash = "03ee2dcb4c0c1296421ecdb9fbeddf071a67141f"
-data_git_describe = "v0.0-10617-g03ee2dcb4"
+data_git_hash = "54b949255a8c463ea365126d90c39cd26c462638"
+data_git_describe = "v0.0-10621-g54b949255"
 data_git_msg = """\
-commit 03ee2dcb4c0c1296421ecdb9fbeddf071a67141f
-Author: Nigel Scales <nigel.scales@gmail.com>
-Date:   Mon Mar 7 13:28:07 2022 +0000
+commit 54b949255a8c463ea365126d90c39cd26c462638
+Author: Michael Tempelmeier <michael.tempelmeier@gi-de.com>
+Date:   Mon Feb 28 16:36:53 2022 +0100
 
-    [adc_ctrl/dv] Added counter tests
+    [kmac] changed hash-threshold reg to shadow reg
     
-    - Added tests
-      - adc_ctrl_poweron_counter
-      - adc_ctrl_lowpower_counter
-    - Added test sequences
-      - adc_ctrl_counter_vseq
-        Randomize wakeup_time powerup_time and low/high/one shot power mode, wait for data then repeat.
-      - adc_ctrl_poweron_counter_vseq
-        Extends adc_ctrl_counter_vseq for normal and one shot mode.
-      - adc_ctrl_lowpower_counter_vseq
-        Extends adc_ctrl_counter_vseq for low power mode.
-    - Added assertions
-      - WakeupTime_A - checks wakeup timing in low power mode
-      - EnterLowPower_A - Checks ADC controller enters low power mode
-    - Updated model
-      - Updated to latest spec / RTL
-      - Modelled adc_fsm_reset register
-      - Modelled interrupt registers for One Shot mode.
-    - Updated adc_ctrl_filters_polled_vseq to perform FSM reset at the end of each iteration as
-    to make sure model and RTL are synchronised before each new configuration
-    - Updated PwrupTime_A to new spec (programmed value + 1) cycles
-    - Enabled PwrupTime_A for all tests and removed plusarg control
-    
-    Signed-off-by: Nigel Scales <nigel.scales@gmail.com>
+    Signed-off-by: Michael Tempelmeier <michael.tempelmeier@gi-de.com>
 
 """
 
