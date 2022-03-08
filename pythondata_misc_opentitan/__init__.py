19066d8dce43fd9f15d0f6a4609218ce5d1812ad
LiteX Robot <robot@timvideos.us>
2022-03-08 15:29:48 +0000

    Updating pythondata-misc-opentitan to 0.0.post10771
    Updated data to v0.0-10645-g7c4f8b3fd based on 7c4f8b3fde4bb625ac3330ff52d3f66507190fe5 from https://github.com/lowRISC/opentitan.
    > commit 7c4f8b3fde4bb625ac3330ff52d3f66507190fe5
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Mon Mar 7 12:41:52 2022 +0000
    >
    >     [dv,lc_ctrl] Make two static functions automatic
    >
    >     These functions are marked static but I don't think there's any
    >     particular reason to do so. They each have local
    >     variables (err_inj_idx) and Xcelium issues a warning because these
    >     aren't also explicitly marked as static. A good warning, because it's
    >     a "foot gun" just waiting to catch you out!
    >
    >     Let's just make the functions automatic and not worry about it.
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,38 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10767"
-version_tuple = (0, 0, 10767)
+version_str = "0.0.post10771"
+version_tuple = (0, 0, 10771)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10767")
+    pversion = V("0.0.post10771")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10641"
-data_version_tuple = (0, 0, 10641)
+data_version_str = "0.0.post10645"
+data_version_tuple = (0, 0, 10645)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10641")
+    pdata_version = V("0.0.post10645")
 except ImportError:
     pass
-data_git_hash = "a46a190fdb95844884ab0c534525178692d3cdde"
-data_git_describe = "v0.0-10641-ga46a190fd"
+data_git_hash = "7c4f8b3fde4bb625ac3330ff52d3f66507190fe5"
+data_git_describe = "v0.0-10645-g7c4f8b3fd"
 data_git_msg = """\
-commit a46a190fdb95844884ab0c534525178692d3cdde
+commit 7c4f8b3fde4bb625ac3330ff52d3f66507190fe5
 Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Thu Mar 3 15:49:08 2022 +0000
+Date:   Mon Mar 7 12:41:52 2022 +0000
 
-    [otbn,dv] Remove unused variables from OTBN C++ support code
+    [dv,lc_ctrl] Make two static functions automatic
     
-    Some of these have been hanging around for quite a while :-/
+    These functions are marked static but I don't think there's any
+    particular reason to do so. They each have local
+    variables (err_inj_idx) and Xcelium issues a warning because these
+    aren't also explicitly marked as static. A good warning, because it's
+    a "foot gun" just waiting to catch you out!
+    
+    Let's just make the functions automatic and not worry about it.
     
     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
