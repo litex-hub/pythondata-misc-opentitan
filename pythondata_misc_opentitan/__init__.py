b6793115ada3a4742ca6035328bdff85fe604228
LiteX Robot <robot@timvideos.us>
2022-03-08 17:28:57 +0000

    Updating pythondata-misc-opentitan to 0.0.post10782
    Updated data to v0.0-10656-g1938c4026 based on 1938c40267b74458d3f0a7b80892ac9921424bd9 from https://github.com/lowRISC/opentitan.
    > commit 1938c40267b74458d3f0a7b80892ac9921424bd9
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Tue Mar 1 16:33:44 2022 -0800
    >
    >     [clkmgr, top] clkmgr and top updates to better support ast external clock
    >
    >     - ast provides a direct indication whether clock dividers should be stepped
    >       down during an external switch.
    >
    >     - clkmgr can make use of this mechanism directly without having to piece
    >       separate logic together.
    >
    >     - This PR may be incomplete at the moment because the relevant signals are
    >       in an async domain, this may be changed by an AST update.
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,41 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10773"
-version_tuple = (0, 0, 10773)
+version_str = "0.0.post10782"
+version_tuple = (0, 0, 10782)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10773")
+    pversion = V("0.0.post10782")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10647"
-data_version_tuple = (0, 0, 10647)
+data_version_str = "0.0.post10656"
+data_version_tuple = (0, 0, 10656)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10647")
+    pdata_version = V("0.0.post10656")
 except ImportError:
     pass
-data_git_hash = "ff1944cca2954721cebfa8e9f90087f955a308a9"
-data_git_describe = "v0.0-10647-gff1944cca"
+data_git_hash = "1938c40267b74458d3f0a7b80892ac9921424bd9"
+data_git_describe = "v0.0-10656-g1938c4026"
 data_git_msg = """\
-commit ff1944cca2954721cebfa8e9f90087f955a308a9
-Author: Jade Philipoom <jadep@google.com>
-Date:   Tue Mar 8 12:43:38 2022 +0000
+commit 1938c40267b74458d3f0a7b80892ac9921424bd9
+Author: Timothy Chen <timothytim@google.com>
+Date:   Tue Mar 1 16:33:44 2022 -0800
 
-    [sw,crypto] Bazel target for non-mask-ROM RSA-3072 entrypoint.
+    [clkmgr, top] clkmgr and top updates to better support ast external clock
     
-    Now that OTBN binaries can have shared dependencies, use that new
-    ability to create an additional target for the RSA-3072 executable that
-    is not specialized to mask ROM.
+    - ast provides a direct indication whether clock dividers should be stepped
+      down during an external switch.
     
-    Signed-off-by: Jade Philipoom <jadep@google.com>
+    - clkmgr can make use of this mechanism directly without having to piece
+      separate logic together.
+    
+    - This PR may be incomplete at the moment because the relevant signals are
+      in an async domain, this may be changed by an AST update.
+    
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
