1617f846fcc525bda57d1835f1dfc50d62908f01
LiteX Robot <robot@timvideos.us>
2022-03-09 20:30:13 +0000

    Updating pythondata-misc-opentitan to 0.0.post10802
    Updated data to v0.0-10676-g87c41e52d based on 87c41e52d089527095e7df84931df5f2a550f3ce from https://github.com/lowRISC/opentitan.
    > commit 87c41e52d089527095e7df84931df5f2a550f3ce
    > Author: Jaedon Kim <jdonjdon@google.com>
    > Date:   Fri Mar 4 20:31:31 2022 +0000
    >
    >     [dv,top,pwm] top sleep pwm pulses test
    >
    >     Signed-off-by: Jaedon Kim <jdonjdon@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,48 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10801"
-version_tuple = (0, 0, 10801)
+version_str = "0.0.post10802"
+version_tuple = (0, 0, 10802)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10801")
+    pversion = V("0.0.post10802")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10675"
-data_version_tuple = (0, 0, 10675)
+data_version_str = "0.0.post10676"
+data_version_tuple = (0, 0, 10676)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10675")
+    pdata_version = V("0.0.post10676")
 except ImportError:
     pass
-data_git_hash = "3dcc25e60d768d679103bbf381e0204d988f2e54"
-data_git_describe = "v0.0-10675-g3dcc25e60"
+data_git_hash = "87c41e52d089527095e7df84931df5f2a550f3ce"
+data_git_describe = "v0.0-10676-g87c41e52d"
 data_git_msg = """\
-commit 3dcc25e60d768d679103bbf381e0204d988f2e54
-Author: Greg Chadwick <gac@lowrisc.org>
-Date:   Wed Mar 9 09:28:27 2022 +0000
+commit 87c41e52d089527095e7df84931df5f2a550f3ce
+Author: Jaedon Kim <jdonjdon@google.com>
+Date:   Fri Mar 4 20:31:31 2022 +0000
 
-    Update lowrisc_ibex to lowRISC/ibex@3438b779
+    [dv,top,pwm] top sleep pwm pulses test
     
-    Update code from upstream repository
-    https://github.com/lowRISC/ibex.git to revision
-    3438b77921941ccbf50cba46b3009a6a37203d4b
-    
-    * [rtl] Add minor alert for icache ECC error (Greg Chadwick)
-    * [icache, rtl] Fix ECC error indication (Greg Chadwick)
-    * [rtl] Add SEC_CM markers for security features (Greg Chadwick)
-    * [ibex, dv] Makes delays between req, gnt and rvalid configurable
-      (Prajwala Puttappa)
-    * [ibex, dv] Added new base, interrupt, debug and mem error sequences
-      (Prajwala Puttappa)
-    * [icache] Define some fake DPI functions to simplify linking (Rupert
-      Swarbrick)
-    * [ibex, dv] Added agent configuration for
-      ibex_mem_intf_response_agent (Prajwala Puttappa)
-    
-    Signed-off-by: Greg Chadwick <gac@lowrisc.org>
+    Signed-off-by: Jaedon Kim <jdonjdon@google.com>
 
 """
 
