206ea69cf3a07ff0151ec2f7cf2b0587711a80f2
LiteX Robot <robot@timvideos.us>
2022-03-09 06:32:26 +0000

    Updating pythondata-misc-opentitan to 0.0.post10791
    Updated data to v0.0-10665-ge5b4eda44 based on e5b4eda44e53a5950716a3993d397d5374f1af95 from https://github.com/lowRISC/opentitan.
    > commit e5b4eda44e53a5950716a3993d397d5374f1af95
    > Author: Weicai Yang <weicai@google.com>
    > Date:   Tue Mar 8 15:51:00 2022 -0800
    >
    >     [dv] Update checklist for all blocks
    >
    >     A follow-up update for #11053
    >     Signed-off-by: Weicai Yang <weicai@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,33 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10789"
-version_tuple = (0, 0, 10789)
+version_str = "0.0.post10791"
+version_tuple = (0, 0, 10791)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10789")
+    pversion = V("0.0.post10791")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10663"
-data_version_tuple = (0, 0, 10663)
+data_version_str = "0.0.post10665"
+data_version_tuple = (0, 0, 10665)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10663")
+    pdata_version = V("0.0.post10665")
 except ImportError:
     pass
-data_git_hash = "6292b1ac4388d9c0278b2b4ef569cd36138a5455"
-data_git_describe = "v0.0-10663-g6292b1ac4"
+data_git_hash = "e5b4eda44e53a5950716a3993d397d5374f1af95"
+data_git_describe = "v0.0-10665-ge5b4eda44"
 data_git_msg = """\
-commit 6292b1ac4388d9c0278b2b4ef569cd36138a5455
-Author: Cindy Chen <chencindy@opentitan.org>
-Date:   Tue Mar 8 16:21:20 2022 -0800
+commit e5b4eda44e53a5950716a3993d397d5374f1af95
+Author: Weicai Yang <weicai@google.com>
+Date:   Tue Mar 8 15:51:00 2022 -0800
 
-    [dv/entropy_src] Temp remove stress_all_with_rand_reset test
+    [dv] Update checklist for all blocks
     
-    Because some of the `stress_all_with_rand_reset` tests seem to hang
-    during nightly regression, we decided to temp remove that test.
-    
-    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
+    A follow-up update for #11053
+    Signed-off-by: Weicai Yang <weicai@google.com>
 
 """
 
