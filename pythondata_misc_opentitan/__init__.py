745a69118fa50399c21e6acb59dd2a42326ba6e1
LiteX Robot <robot@timvideos.us>
2022-03-09 04:34:21 +0000

    Updating pythondata-misc-opentitan to 0.0.post10789
    Updated data to v0.0-10663-g6292b1ac4 based on 6292b1ac4388d9c0278b2b4ef569cd36138a5455 from https://github.com/lowRISC/opentitan.
    > commit 6292b1ac4388d9c0278b2b4ef569cd36138a5455
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Tue Mar 8 16:21:20 2022 -0800
    >
    >     [dv/entropy_src] Temp remove stress_all_with_rand_reset test
    >
    >     Because some of the `stress_all_with_rand_reset` tests seem to hang
    >     during nightly regression, we decided to temp remove that test.
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,33 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10788"
-version_tuple = (0, 0, 10788)
+version_str = "0.0.post10789"
+version_tuple = (0, 0, 10789)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10788")
+    pversion = V("0.0.post10789")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10662"
-data_version_tuple = (0, 0, 10662)
+data_version_str = "0.0.post10663"
+data_version_tuple = (0, 0, 10663)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10662")
+    pdata_version = V("0.0.post10663")
 except ImportError:
     pass
-data_git_hash = "8ccce1c64a9cd0ce6039e33050ac1e8fad83bbb6"
-data_git_describe = "v0.0-10662-g8ccce1c64"
+data_git_hash = "6292b1ac4388d9c0278b2b4ef569cd36138a5455"
+data_git_describe = "v0.0-10663-g6292b1ac4"
 data_git_msg = """\
-commit 8ccce1c64a9cd0ce6039e33050ac1e8fad83bbb6
+commit 6292b1ac4388d9c0278b2b4ef569cd36138a5455
 Author: Cindy Chen <chencindy@opentitan.org>
-Date:   Tue Mar 8 11:33:17 2022 -0800
+Date:   Tue Mar 8 16:21:20 2022 -0800
 
-    [dv/shadow_reg] Minor update on shadow_reg reset
+    [dv/entropy_src] Temp remove stress_all_with_rand_reset test
     
-    When we glitch reset in shadowed_regs, we predict error status before
-    reset is issued, that might cause scb to clear the predicted value.
-    So in this PR, we moved the error prediction after reset is finished.
+    Because some of the `stress_all_with_rand_reset` tests seem to hang
+    during nightly regression, we decided to temp remove that test.
     
     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
