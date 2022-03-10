eb9f60727c6334bc0ef50ce15dfb8330108d6da6
LiteX Robot <robot@timvideos.us>
2022-03-10 23:29:05 +0000

    Updating pythondata-misc-opentitan to 0.0.post10833
    Updated data to v0.0-10707-g57cc44f6f based on 57cc44f6f5d166a2b4d9079811d6c1aca4675252 from https://github.com/lowRISC/opentitan.
    > commit 57cc44f6f5d166a2b4d9079811d6c1aca4675252
    > Author: Alexander Williams <awill@google.com>
    > Date:   Wed Feb 23 09:04:33 2022 -0800
    >
    >     [usbdev] Move pinflip and dp_o/dn_o to usb_fs_nb_pe
    >
    >     Handle the interface changes and alternate TX interface in logic, before
    >     the final register. Prior to this change, the pinflip was sometimes
    >     handled in clock muxes, on the I/O side of the first/final registers,
    >     and the dp_o/dn_o interface was only done in behavioral logic. Move this
    >     all inside the USB clock domain, and perform only the pin overrides with
    >     clock muxes.
    >
    >     Add override for rx_enable and remove the redundant one for
    >     tx_use_d_se0.
    >
    >     Signed-off-by: Alexander Williams <awill@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,42 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10831"
-version_tuple = (0, 0, 10831)
+version_str = "0.0.post10833"
+version_tuple = (0, 0, 10833)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10831")
+    pversion = V("0.0.post10833")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10705"
-data_version_tuple = (0, 0, 10705)
+data_version_str = "0.0.post10707"
+data_version_tuple = (0, 0, 10707)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10705")
+    pdata_version = V("0.0.post10707")
 except ImportError:
     pass
-data_git_hash = "de5926469c2d14a6427896a6e7b9040eeb4e65ce"
-data_git_describe = "v0.0-10705-gde5926469"
+data_git_hash = "57cc44f6f5d166a2b4d9079811d6c1aca4675252"
+data_git_describe = "v0.0-10707-g57cc44f6f"
 data_git_msg = """\
-commit de5926469c2d14a6427896a6e7b9040eeb4e65ce
-Author: Guillermo Maturana <maturana@google.com>
-Date:   Mon Mar 7 21:45:26 2022 -0800
+commit 57cc44f6f5d166a2b4d9079811d6c1aca4675252
+Author: Alexander Williams <awill@google.com>
+Date:   Wed Feb 23 09:04:33 2022 -0800
 
-    [sw/runtime] Add return code in CHECK_DIF_OK
+    [usbdev] Move pinflip and dp_o/dn_o to usb_fs_nb_pe
     
-    Signed-off-by: Guillermo Maturana <maturana@google.com>
+    Handle the interface changes and alternate TX interface in logic, before
+    the final register. Prior to this change, the pinflip was sometimes
+    handled in clock muxes, on the I/O side of the first/final registers,
+    and the dp_o/dn_o interface was only done in behavioral logic. Move this
+    all inside the USB clock domain, and perform only the pin overrides with
+    clock muxes.
+    
+    Add override for rx_enable and remove the redundant one for
+    tx_use_d_se0.
+    
+    Signed-off-by: Alexander Williams <awill@google.com>
 
 """
 
