9562df50137be10f2daf77482bb80bb4f1cf39ac
LiteX Robot <robot@timvideos.us>
2022-03-11 15:30:41 +0000

    Updating pythondata-misc-opentitan to 0.0.post10849
    Updated data to v0.0-10723-g48f14bc59 based on 48f14bc59126055488290fef9ca34e77a29685fd from https://github.com/lowRISC/opentitan.
    > commit 48f14bc59126055488290fef9ca34e77a29685fd
    > Author: Greg Chadwick <gac@lowrisc.org>
    > Date:   Fri Mar 11 10:42:51 2022 +0000
    >
    >     [otbn, rtl] Fix `idle_o` timing
    >
    >     `idle_o` will gate the clock if clock hints are setup to do so. Any done
    >     interrupt raised must occur at or before the cycle `idle_o` changes to
    >     ensure the clock gating doesn't erase the interrupt.
    >
    >     This adds a delay to the externally visible `idle_o` to ensure it
    >     synchronises with changes to the status register and gives correct
    >     interrupt timing.
    >
    >     Signed-off-by: Greg Chadwick <gac@lowrisc.org>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,40 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10847"
-version_tuple = (0, 0, 10847)
+version_str = "0.0.post10849"
+version_tuple = (0, 0, 10849)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10847")
+    pversion = V("0.0.post10849")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10721"
-data_version_tuple = (0, 0, 10721)
+data_version_str = "0.0.post10723"
+data_version_tuple = (0, 0, 10723)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10721")
+    pdata_version = V("0.0.post10723")
 except ImportError:
     pass
-data_git_hash = "951a93e8f41193439d0121cf89afc39da56a4e7e"
-data_git_describe = "v0.0-10721-g951a93e8f"
+data_git_hash = "48f14bc59126055488290fef9ca34e77a29685fd"
+data_git_describe = "v0.0-10723-g48f14bc59"
 data_git_msg = """\
-commit 951a93e8f41193439d0121cf89afc39da56a4e7e
-Author: Douglas Reis <doreis@lowrisc.org>
-Date:   Tue Mar 1 18:28:32 2022 +0000
+commit 48f14bc59126055488290fef9ca34e77a29685fd
+Author: Greg Chadwick <gac@lowrisc.org>
+Date:   Fri Mar 11 10:42:51 2022 +0000
 
-    [sw, aes] Add unit test for AES status register
+    [otbn, rtl] Fix `idle_o` timing
     
-    Signed-off-by: Douglas Reis <doreis@lowrisc.org>
+    `idle_o` will gate the clock if clock hints are setup to do so. Any done
+    interrupt raised must occur at or before the cycle `idle_o` changes to
+    ensure the clock gating doesn't erase the interrupt.
+    
+    This adds a delay to the externally visible `idle_o` to ensure it
+    synchronises with changes to the status register and gives correct
+    interrupt timing.
+    
+    Signed-off-by: Greg Chadwick <gac@lowrisc.org>
 
 """
 
