2029d4f87c394c8c4406141ed532757d00b30707
LiteX Robot <robot@timvideos.us>
2022-03-12 01:26:15 +0000

    Updating pythondata-misc-opentitan to 0.0.post10850
    Updated data to v0.0-10724-gb40ec27bf based on b40ec27bf69345d00908b352e08ef1c9d0a58e36 from https://github.com/lowRISC/opentitan.
    > commit b40ec27bf69345d00908b352e08ef1c9d0a58e36
    > Author: Nigel Scales <nigel.scales@gmail.com>
    > Date:   Fri Mar 11 13:08:47 2022 +0000
    >
    >     [adc_ctrl/dv] Added reset tests
    >
    >     - Added tests
    >       - adc_ctrl_fsm_reset
    >     - Added sequences
    >       - adc_ctrl_fsm_reset_vseq - randomly assert software/hardware reset for
    >       all ADC_CTRL modes preloading counters to minimise simulation time.
    >     - Added assertions in adc_ctrl_fsm_sva
    >       - FsmStateSwReset_A check FSM software reset
    >       - PwrupTimerCntSwReset_A check power up counter software reset
    >       - WakeupTimerCntSwReset_A check wake up counter software reset
    >       - NpSampleCntSwReset_A check normal power sample counter software reset
    >       - LpSampleCntSwReset_A check low power sample counter software reset
    >       - FsmStateHwReset_A check FSM hardware reset
    >       - PwrupTimerCntHwReset_A check power up counter hardware reset
    >       - WakeupTimerCntHwReset_A check wake up counter hardware reset
    >       - NpSampleCntHwReset_A check normal power sample counter hardware reset
    >       - LpSampleCntHwReset_A check low power sample counter hardware reset
    >     - Fixed wake up assertion auxiliary code.
    >     - Renamed enumeration constants to fit coding guidelines
    >     - Fixed EnterLowPower_A to be disabled when not in low power test mode.
    >     - Added hook task post_adc_ctrl_enable to adc_ctrl_filters_polled_vseq.sv
    >     - Killing background tasks in adc_ctrl_filters_polled_vseq::post_start
    >
    >     Signed-off-by: Nigel Scales <nigel.scales@gmail.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,40 +4,54 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10849"
-version_tuple = (0, 0, 10849)
+version_str = "0.0.post10850"
+version_tuple = (0, 0, 10850)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10849")
+    pversion = V("0.0.post10850")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10723"
-data_version_tuple = (0, 0, 10723)
+data_version_str = "0.0.post10724"
+data_version_tuple = (0, 0, 10724)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10723")
+    pdata_version = V("0.0.post10724")
 except ImportError:
     pass
-data_git_hash = "48f14bc59126055488290fef9ca34e77a29685fd"
-data_git_describe = "v0.0-10723-g48f14bc59"
+data_git_hash = "b40ec27bf69345d00908b352e08ef1c9d0a58e36"
+data_git_describe = "v0.0-10724-gb40ec27bf"
 data_git_msg = """\
-commit 48f14bc59126055488290fef9ca34e77a29685fd
-Author: Greg Chadwick <gac@lowrisc.org>
-Date:   Fri Mar 11 10:42:51 2022 +0000
+commit b40ec27bf69345d00908b352e08ef1c9d0a58e36
+Author: Nigel Scales <nigel.scales@gmail.com>
+Date:   Fri Mar 11 13:08:47 2022 +0000
 
-    [otbn, rtl] Fix `idle_o` timing
+    [adc_ctrl/dv] Added reset tests
     
-    `idle_o` will gate the clock if clock hints are setup to do so. Any done
-    interrupt raised must occur at or before the cycle `idle_o` changes to
-    ensure the clock gating doesn't erase the interrupt.
+    - Added tests
+      - adc_ctrl_fsm_reset
+    - Added sequences
+      - adc_ctrl_fsm_reset_vseq - randomly assert software/hardware reset for
+      all ADC_CTRL modes preloading counters to minimise simulation time.
+    - Added assertions in adc_ctrl_fsm_sva
+      - FsmStateSwReset_A check FSM software reset
+      - PwrupTimerCntSwReset_A check power up counter software reset
+      - WakeupTimerCntSwReset_A check wake up counter software reset
+      - NpSampleCntSwReset_A check normal power sample counter software reset
+      - LpSampleCntSwReset_A check low power sample counter software reset
+      - FsmStateHwReset_A check FSM hardware reset
+      - PwrupTimerCntHwReset_A check power up counter hardware reset
+      - WakeupTimerCntHwReset_A check wake up counter hardware reset
+      - NpSampleCntHwReset_A check normal power sample counter hardware reset
+      - LpSampleCntHwReset_A check low power sample counter hardware reset
+    - Fixed wake up assertion auxiliary code.
+    - Renamed enumeration constants to fit coding guidelines
+    - Fixed EnterLowPower_A to be disabled when not in low power test mode.
+    - Added hook task post_adc_ctrl_enable to adc_ctrl_filters_polled_vseq.sv
+    - Killing background tasks in adc_ctrl_filters_polled_vseq::post_start
     
-    This adds a delay to the externally visible `idle_o` to ensure it
-    synchronises with changes to the status register and gives correct
-    interrupt timing.
-    
-    Signed-off-by: Greg Chadwick <gac@lowrisc.org>
+    Signed-off-by: Nigel Scales <nigel.scales@gmail.com>
 
 """
 
