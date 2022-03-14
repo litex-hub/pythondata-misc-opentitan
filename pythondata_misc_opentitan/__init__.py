2cc8a6e07122f8811679297084421cd571ebdd20
LiteX Robot <robot@timvideos.us>
2022-03-14 16:38:48 +0000

    Updating pythondata-misc-opentitan to 0.0.post10856
    Updated data to v0.0-10730-g266a448c7 based on 266a448c729b0207cbbe17b383257e68d8c3b481 from https://github.com/lowRISC/opentitan.
    > commit 266a448c729b0207cbbe17b383257e68d8c3b481
    > Author: Guillermo Maturana <maturana@google.com>
    > Date:   Mon Mar 7 19:07:23 2022 -0800
    >
    >     [dif/clkmgr] Add support for count measurements
    >
    >     Signed-off-by: Guillermo Maturana <maturana@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10853"
-version_tuple = (0, 0, 10853)
+version_str = "0.0.post10856"
+version_tuple = (0, 0, 10856)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10853")
+    pversion = V("0.0.post10856")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10727"
-data_version_tuple = (0, 0, 10727)
+data_version_str = "0.0.post10730"
+data_version_tuple = (0, 0, 10730)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10727")
+    pdata_version = V("0.0.post10730")
 except ImportError:
     pass
-data_git_hash = "b0fd9bf680319c101c16c9f0bc50909b1d9acfc4"
-data_git_describe = "v0.0-10727-gb0fd9bf68"
+data_git_hash = "266a448c729b0207cbbe17b383257e68d8c3b481"
+data_git_describe = "v0.0-10730-g266a448c7"
 data_git_msg = """\
-commit b0fd9bf680319c101c16c9f0bc50909b1d9acfc4
-Author: Cindy Chen <chencindy@opentitan.org>
-Date:   Thu Mar 10 12:31:01 2022 -0800
+commit 266a448c729b0207cbbe17b383257e68d8c3b481
+Author: Guillermo Maturana <maturana@google.com>
+Date:   Mon Mar 7 19:07:23 2022 -0800
 
-    [rstmgr/rtl] Swap alert_test alerts orders
+    [dif/clkmgr] Add support for count measurements
     
-    I believe the order of alert_test is swapped.
-    
-    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
+    Signed-off-by: Guillermo Maturana <maturana@google.com>
 
 """
 
