e682a442a007429e03cb9195491f4d2654e269bc
LiteX Robot <robot@timvideos.us>
2022-03-16 21:25:58 +0000

    Updating pythondata-misc-opentitan to 0.0.post10931
    Updated data to v0.0-10805-g0f3371021 based on 0f337102132a1bb0209f93414d1a3cd0b2734c85 from https://github.com/lowRISC/opentitan.
    > commit 0f337102132a1bb0209f93414d1a3cd0b2734c85
    > Author: Miguel Young de la Sota <mcyoung@google.com>
    > Date:   Wed Mar 16 14:55:09 2022 -0400
    >
    >     Add #include clarifications in freestading/README.md
    >
    >     Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,38 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10928"
-version_tuple = (0, 0, 10928)
+version_str = "0.0.post10931"
+version_tuple = (0, 0, 10931)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10928")
+    pversion = V("0.0.post10931")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10802"
-data_version_tuple = (0, 0, 10802)
+data_version_str = "0.0.post10805"
+data_version_tuple = (0, 0, 10805)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10802")
+    pdata_version = V("0.0.post10805")
 except ImportError:
     pass
-data_git_hash = "2a2eb5ff7d8db22445086f6cf60cda29b59308b5"
-data_git_describe = "v0.0-10802-g2a2eb5ff7"
+data_git_hash = "0f337102132a1bb0209f93414d1a3cd0b2734c85"
+data_git_describe = "v0.0-10805-g0f3371021"
 data_git_msg = """\
-commit 2a2eb5ff7d8db22445086f6cf60cda29b59308b5
-Author: TIM EWINS <tim.ewins@ensilica.com>
-Date:   Fri Mar 4 11:10:10 2022 +0000
+commit 0f337102132a1bb0209f93414d1a3cd0b2734c85
+Author: Miguel Young de la Sota <mcyoung@google.com>
+Date:   Wed Mar 16 14:55:09 2022 -0400
 
-    [flash_ctrl] ADD TEST FOR THE HOST CTRL ARBITER
+    Add #include clarifications in freestading/README.md
     
-    Perform operations via the Flash Software Interface, and at the same time invoke a
-    Hardware RMA operation. This verifies the arbitration within the Flash Protocol
-    Controller. The arbiter should allow any outstanding Software operations to complete
-    before the RMA starts.  When the RMA completes the RMA FSM remains in its final state
-    until Reset and Software access is blocked.
-    
-    Signed-off-by: TIM EWINS <tim.ewins@ensilica.com>
+    Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>
 
 """
 
