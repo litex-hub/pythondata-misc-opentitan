6759412794a424227a0f2b1a41f89984ce1eadf1
LiteX Robot <robot@timvideos.us>
2022-03-17 23:27:37 +0000

    Updating pythondata-misc-opentitan to 0.0.post10963
    Updated data to v0.0-10837-ge5928dfec based on e5928dfec8723c6be150342a18b80c72b1c4659f from https://github.com/lowRISC/opentitan.
    > commit e5928dfec8723c6be150342a18b80c72b1c4659f
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Thu Feb 10 15:01:51 2022 +0000
    >
    >     [otbn,dv] Clear the "imem is invalidated" flag when loading program
    >
    >     This hasn't usually mattered because we reset the dut (and also the
    >     ISS) after injecting IMEM errors to clear the locked state. However,
    >     there was a race window where we injected the errors just after OTBN
    >     had finished. In this case, we don't need to reset the DUT, but we do
    >     have to clear the flag when the IMEM contents are replaced.
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,38 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10959"
-version_tuple = (0, 0, 10959)
+version_str = "0.0.post10963"
+version_tuple = (0, 0, 10963)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10959")
+    pversion = V("0.0.post10963")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10833"
-data_version_tuple = (0, 0, 10833)
+data_version_str = "0.0.post10837"
+data_version_tuple = (0, 0, 10837)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10833")
+    pdata_version = V("0.0.post10837")
 except ImportError:
     pass
-data_git_hash = "998c4e628705541647a0ffdfcc3decc5ba8da9e4"
-data_git_describe = "v0.0-10833-g998c4e628"
+data_git_hash = "e5928dfec8723c6be150342a18b80c72b1c4659f"
+data_git_describe = "v0.0-10837-ge5928dfec"
 data_git_msg = """\
-commit 998c4e628705541647a0ffdfcc3decc5ba8da9e4
-Author: Miguel Young de la Sota <mcyoung@google.com>
-Date:   Tue Mar 15 15:35:52 2022 -0400
+commit e5928dfec8723c6be150342a18b80c72b1c4659f
+Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
+Date:   Thu Feb 10 15:01:51 2022 +0000
 
-    [lib] Remove libgcc dependency in on-device code
+    [otbn,dv] Clear the "imem is invalidated" flag when loading program
     
-    This was the last bit of non-OT-owned code compiled into on-device
-    binaries.
+    This hasn't usually mattered because we reset the dut (and also the
+    ISS) after injecting IMEM errors to clear the locked state. However,
+    there was a race window where we injected the errors just after OTBN
+    had finished. In this case, we don't need to reset the DUT, but we do
+    have to clear the flag when the IMEM contents are replaced.
     
-    Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>
+    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
 """
 
