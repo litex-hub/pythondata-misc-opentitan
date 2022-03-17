07dcd7756c71c3687c4787d87eb4433c71b35af8
LiteX Robot <robot@timvideos.us>
2022-03-17 22:28:20 +0000

    Updating pythondata-misc-opentitan to 0.0.post10959
    Updated data to v0.0-10833-g998c4e628 based on 998c4e628705541647a0ffdfcc3decc5ba8da9e4 from https://github.com/lowRISC/opentitan.
    > commit 998c4e628705541647a0ffdfcc3decc5ba8da9e4
    > Author: Miguel Young de la Sota <mcyoung@google.com>
    > Date:   Tue Mar 15 15:35:52 2022 -0400
    >
    >     [lib] Remove libgcc dependency in on-device code
    >
    >     This was the last bit of non-OT-owned code compiled into on-device
    >     binaries.
    >
    >     Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10955"
-version_tuple = (0, 0, 10955)
+version_str = "0.0.post10959"
+version_tuple = (0, 0, 10959)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10955")
+    pversion = V("0.0.post10959")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10829"
-data_version_tuple = (0, 0, 10829)
+data_version_str = "0.0.post10833"
+data_version_tuple = (0, 0, 10833)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10829")
+    pdata_version = V("0.0.post10833")
 except ImportError:
     pass
-data_git_hash = "cbe41e0de3047b1fec8cdaac21ac11910df1859b"
-data_git_describe = "v0.0-10829-gcbe41e0de"
+data_git_hash = "998c4e628705541647a0ffdfcc3decc5ba8da9e4"
+data_git_describe = "v0.0-10833-g998c4e628"
 data_git_msg = """\
-commit cbe41e0de3047b1fec8cdaac21ac11910df1859b
-Author: Guillermo Maturana <maturana@google.com>
-Date:   Mon Mar 14 15:43:33 2022 -0700
+commit 998c4e628705541647a0ffdfcc3decc5ba8da9e4
+Author: Miguel Young de la Sota <mcyoung@google.com>
+Date:   Tue Mar 15 15:35:52 2022 -0400
 
-    [dv/clkmgr] Fix external clock handling
+    [lib] Remove libgcc dependency in on-device code
     
-    The RTL handling of external clock was recently changed.
-    Update DV to match interface changes.
+    This was the last bit of non-OT-owned code compiled into on-device
+    binaries.
     
-    Signed-off-by: Guillermo Maturana <maturana@google.com>
+    Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>
 
 """
 
