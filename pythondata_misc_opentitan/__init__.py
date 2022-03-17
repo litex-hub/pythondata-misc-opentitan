3158f342d87ff785eea3d12e6e748a8fcd627240
LiteX Robot <robot@timvideos.us>
2022-03-17 16:38:00 +0000

    Updating pythondata-misc-opentitan to 0.0.post10939
    Updated data to v0.0-10813-g5857c501e based on 5857c501eb04b8cbd1b90f5b146fb84e8b9d7e0b from https://github.com/lowRISC/opentitan.
    > commit 5857c501eb04b8cbd1b90f5b146fb84e8b9d7e0b
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Wed Mar 16 15:21:26 2022 -0700
    >
    >     [dv/otp] sec_cm
    >
    >     This PR update security countermeasure tasks:
    >     1). Update exp_status_val when injecting fault on DV
    >     2). After fault injected, this PR checks if otp is blocked by accessing
    >       the OTP.
    >     3). Disable assertions due to index OOB.
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,38 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10938"
-version_tuple = (0, 0, 10938)
+version_str = "0.0.post10939"
+version_tuple = (0, 0, 10939)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10938")
+    pversion = V("0.0.post10939")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10812"
-data_version_tuple = (0, 0, 10812)
+data_version_str = "0.0.post10813"
+data_version_tuple = (0, 0, 10813)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10812")
+    pdata_version = V("0.0.post10813")
 except ImportError:
     pass
-data_git_hash = "ef7251a509edea71b16b00af64c363f72e224dd8"
-data_git_describe = "v0.0-10812-gef7251a50"
+data_git_hash = "5857c501eb04b8cbd1b90f5b146fb84e8b9d7e0b"
+data_git_describe = "v0.0-10813-g5857c501e"
 data_git_msg = """\
-commit ef7251a509edea71b16b00af64c363f72e224dd8
-Author: Miguel Young de la Sota <mcyoung@google.com>
-Date:   Wed Mar 16 14:58:37 2022 -0400
+commit 5857c501eb04b8cbd1b90f5b146fb84e8b9d7e0b
+Author: Cindy Chen <chencindy@opentitan.org>
+Date:   Wed Mar 16 15:21:26 2022 -0700
 
-    [sw] Get rid of bespoke *_WARN_UNUSED_RESULT macros
+    [dv/otp] sec_cm
     
-    Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>
+    This PR update security countermeasure tasks:
+    1). Update exp_status_val when injecting fault on DV
+    2). After fault injected, this PR checks if otp is blocked by accessing
+      the OTP.
+    3). Disable assertions due to index OOB.
+    
+    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
 """
 
