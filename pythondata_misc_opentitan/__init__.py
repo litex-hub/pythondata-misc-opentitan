f75083c0edd3c733b718753116826524c3efc542
LiteX Robot <robot@timvideos.us>
2022-03-18 19:23:00 +0000

    Updating pythondata-misc-opentitan to 0.0.post10980
    Updated data to v0.0-10854-g74cf43757 based on 74cf437574da7a5e62d6caba563e38de407cb07c from https://github.com/lowRISC/opentitan.
    > commit 74cf437574da7a5e62d6caba563e38de407cb07c
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Wed Mar 9 13:46:52 2022 -0800
    >
    >     [hmac] Minor lint fix
    >
    >     - re-code the flop since otherwise it causes a RESET_USE error
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post10979"
-version_tuple = (0, 0, 10979)
+version_str = "0.0.post10980"
+version_tuple = (0, 0, 10980)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post10979")
+    pversion = V("0.0.post10980")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10853"
-data_version_tuple = (0, 0, 10853)
+data_version_str = "0.0.post10854"
+data_version_tuple = (0, 0, 10854)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10853")
+    pdata_version = V("0.0.post10854")
 except ImportError:
     pass
-data_git_hash = "6d96cdf7819cfaf0a58638abe6eff21b651ed475"
-data_git_describe = "v0.0-10853-g6d96cdf78"
+data_git_hash = "74cf437574da7a5e62d6caba563e38de407cb07c"
+data_git_describe = "v0.0-10854-g74cf43757"
 data_git_msg = """\
-commit 6d96cdf7819cfaf0a58638abe6eff21b651ed475
-Author: Miles Dai <milesdai@google.com>
-Date:   Wed Mar 9 18:45:49 2022 -0500
+commit 74cf437574da7a5e62d6caba563e38de407cb07c
+Author: Timothy Chen <timothytim@google.com>
+Date:   Wed Mar 9 13:46:52 2022 -0800
 
-    [ci] Upload generated bitstream to public GCP bucket
+    [hmac] Minor lint fix
     
-    Signed-off-by: Miles Dai <milesdai@google.com>
+    - re-code the flop since otherwise it causes a RESET_USE error
+    
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
