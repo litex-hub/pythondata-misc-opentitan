603a6b3c007ea1e8adbe052b6620d7300c7cec0c
LiteX Robot <robot@timvideos.us>
2022-03-21 22:29:59 +0000

    Updating pythondata-misc-opentitan to 0.0.post11013
    Updated data to v0.0-10887-g87b087b65 based on 87b087b65d5b852510a83267d9ae8e68bafc5061 from https://github.com/lowRISC/opentitan.
    > commit 87b087b65d5b852510a83267d9ae8e68bafc5061
    > Author: Michael Schaffner <msf@opentitan.org>
    > Date:   Mon Mar 21 11:57:49 2022 -0700
    >
    >     [sysrst_ctrl] Minor label cleanup in sysrst_ctrl_keyintr
    >
    >     Signed-off-by: Michael Schaffner <msf@opentitan.org>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11011"
-version_tuple = (0, 0, 11011)
+version_str = "0.0.post11013"
+version_tuple = (0, 0, 11013)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11011")
+    pversion = V("0.0.post11013")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10885"
-data_version_tuple = (0, 0, 10885)
+data_version_str = "0.0.post10887"
+data_version_tuple = (0, 0, 10887)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10885")
+    pdata_version = V("0.0.post10887")
 except ImportError:
     pass
-data_git_hash = "4ac33d98b562fb25a55fb3d9793bce8e534134e5"
-data_git_describe = "v0.0-10885-g4ac33d98b"
+data_git_hash = "87b087b65d5b852510a83267d9ae8e68bafc5061"
+data_git_describe = "v0.0-10887-g87b087b65"
 data_git_msg = """\
-commit 4ac33d98b562fb25a55fb3d9793bce8e534134e5
-Author: Philipp Wagner <phw@lowrisc.org>
-Date:   Mon Mar 21 20:15:08 2022 +0100
+commit 87b087b65d5b852510a83267d9ae8e68bafc5061
+Author: Michael Schaffner <msf@opentitan.org>
+Date:   Mon Mar 21 11:57:49 2022 -0700
 
-    [doc] Fix markdown in firmware update documentation
+    [sysrst_ctrl] Minor label cleanup in sysrst_ctrl_keyintr
     
-    The sub-items are not rendered correctly if the numbering is wrong.
-    
-    Signed-off-by: Philipp Wagner <phw@lowrisc.org>
+    Signed-off-by: Michael Schaffner <msf@opentitan.org>
 
 """
 
