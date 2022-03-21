bcfa79b9afce6746c2850c857090f367943f961f
LiteX Robot <robot@timvideos.us>
2022-03-21 16:39:17 +0000

    Updating pythondata-misc-opentitan to 0.0.post11001
    Updated data to v0.0-10875-gb8cdb2e1e based on b8cdb2e1e6dff9f714963dcfd8f01d73cbc2cf2e from https://github.com/lowRISC/opentitan.
    > commit b8cdb2e1e6dff9f714963dcfd8f01d73cbc2cf2e
    > Author: Nikola Miladinovic <nikola.miladinovic@ensilica.com>
    > Date:   Fri Mar 18 12:14:04 2022 +0000
    >
    >     [flash_ctrl] Small fix for flash_erase_suspend test
    >
    >     Fix of erase suspend test for creator and owner partition
    >
    >     Signed-off-by: Nikola Miladinovic <nikola.miladinovic@ensilica.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11000"
-version_tuple = (0, 0, 11000)
+version_str = "0.0.post11001"
+version_tuple = (0, 0, 11001)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11000")
+    pversion = V("0.0.post11001")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10874"
-data_version_tuple = (0, 0, 10874)
+data_version_str = "0.0.post10875"
+data_version_tuple = (0, 0, 10875)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10874")
+    pdata_version = V("0.0.post10875")
 except ImportError:
     pass
-data_git_hash = "eb8ab0f133f5989a2f52904d78630bf9e333f4cc"
-data_git_describe = "v0.0-10874-geb8ab0f13"
+data_git_hash = "b8cdb2e1e6dff9f714963dcfd8f01d73cbc2cf2e"
+data_git_describe = "v0.0-10875-gb8cdb2e1e"
 data_git_msg = """\
-commit eb8ab0f133f5989a2f52904d78630bf9e333f4cc
-Author: Douglas Reis <doreis@lowrisc.org>
-Date:   Tue Mar 15 14:44:06 2022 +0000
+commit b8cdb2e1e6dff9f714963dcfd8f01d73cbc2cf2e
+Author: Nikola Miladinovic <nikola.miladinovic@ensilica.com>
+Date:   Fri Mar 18 12:14:04 2022 +0000
 
-    [sw, test, otbn] Add the chip level test otbn idle
+    [flash_ctrl] Small fix for flash_erase_suspend test
     
-    Signed-off-by: Douglas Reis <doreis@lowrisc.org>
+    Fix of erase suspend test for creator and owner partition
+    
+    Signed-off-by: Nikola Miladinovic <nikola.miladinovic@ensilica.com>
 
 """
 
