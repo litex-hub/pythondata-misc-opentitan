37135cebae55b6d049f401a4254f14b41c0cf2a7
LiteX Robot <robot@timvideos.us>
2022-03-22 21:25:17 +0000

    Updating pythondata-misc-opentitan to 0.0.post11026
    Updated data to v0.0-10900-g110bf9869 based on 110bf9869abd71c82a894790a62ec8956dda7b14 from https://github.com/lowRISC/opentitan.
    > commit 110bf9869abd71c82a894790a62ec8956dda7b14
    > Author: Nigel Scales <nigel.scales@gmail.com>
    > Date:   Tue Mar 22 18:11:17 2022 +0000
    >
    >     [adc_ctrl/dv] Added adc_ctrl_stress_all test
    >
    >     - Added tests
    >       - adc_ctrl_stress_all
    >     - Added test sequences
    >       - adc_ctrl_stress_all_vseq - runs a random choice of the others test sequences
    >     - Fixed adc_ctrl_intr - Disabled scoreboard and enabled autopredict
    >     - Fixed adc_ctrl_bit_bash - Added assertion control ADC_CTRL_FSM_A_CTRL
    >     - Fixed adc_ctrl_clock_gating - Disabled interrupts for this test
    >     - Corrected filter parameter coverage
    >     - Enhanced variable filter config constraints to generate maximum and
    >     minimum values for min_v and max_v
    >     - Added reads of channel value registers to interrupt/both tests
    >     - Aligned capture of interrupt channel values in model with RTL
    >     - Modelling intr_test in scoreboard
    >     - Added interrupt cover groups sampling to scoreboard
    >
    >     Signed-off-by: Nigel Scales <nigel.scales@gmail.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,47 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11025"
-version_tuple = (0, 0, 11025)
+version_str = "0.0.post11026"
+version_tuple = (0, 0, 11026)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11025")
+    pversion = V("0.0.post11026")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10899"
-data_version_tuple = (0, 0, 10899)
+data_version_str = "0.0.post10900"
+data_version_tuple = (0, 0, 10900)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10899")
+    pdata_version = V("0.0.post10900")
 except ImportError:
     pass
-data_git_hash = "f95a6861273d99c46663532772a8a3b081e6efaf"
-data_git_describe = "v0.0-10899-gf95a68612"
+data_git_hash = "110bf9869abd71c82a894790a62ec8956dda7b14"
+data_git_describe = "v0.0-10900-g110bf9869"
 data_git_msg = """\
-commit f95a6861273d99c46663532772a8a3b081e6efaf
-Author: Eunchan Kim <eunchan@opentitan.org>
-Date:   Mon Mar 14 15:10:05 2022 -0700
+commit 110bf9869abd71c82a894790a62ec8956dda7b14
+Author: Nigel Scales <nigel.scales@gmail.com>
+Date:   Tue Mar 22 18:11:17 2022 +0000
 
-    [spi_device] Revise spec to describe BUSY
+    [adc_ctrl/dv] Added adc_ctrl_stress_all test
     
-    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>
+    - Added tests
+      - adc_ctrl_stress_all
+    - Added test sequences
+      - adc_ctrl_stress_all_vseq - runs a random choice of the others test sequences
+    - Fixed adc_ctrl_intr - Disabled scoreboard and enabled autopredict
+    - Fixed adc_ctrl_bit_bash - Added assertion control ADC_CTRL_FSM_A_CTRL
+    - Fixed adc_ctrl_clock_gating - Disabled interrupts for this test
+    - Corrected filter parameter coverage
+    - Enhanced variable filter config constraints to generate maximum and
+    minimum values for min_v and max_v
+    - Added reads of channel value registers to interrupt/both tests
+    - Aligned capture of interrupt channel values in model with RTL
+    - Modelling intr_test in scoreboard
+    - Added interrupt cover groups sampling to scoreboard
+    
+    Signed-off-by: Nigel Scales <nigel.scales@gmail.com>
 
 """
 
