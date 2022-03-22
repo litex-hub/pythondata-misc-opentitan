cea7303c794b0aad7ac2d72d8cfc862587fd3489
LiteX Robot <robot@timvideos.us>
2022-03-22 17:34:12 +0000

    Updating pythondata-misc-opentitan to 0.0.post11022
    Updated data to v0.0-10896-gee05909e2 based on ee05909e2a5a39be116484e537f62e35e5ae7573 from https://github.com/lowRISC/opentitan.
    > commit ee05909e2a5a39be116484e537f62e35e5ae7573
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Fri Mar 18 13:22:10 2022 -0700
    >
    >     [flah_ctrl] Fix alert indexing
    >
    >     - standard fatal alerts are at position 1 instead of 0
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11021"
-version_tuple = (0, 0, 11021)
+version_str = "0.0.post11022"
+version_tuple = (0, 0, 11022)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11021")
+    pversion = V("0.0.post11022")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10895"
-data_version_tuple = (0, 0, 10895)
+data_version_str = "0.0.post10896"
+data_version_tuple = (0, 0, 10896)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10895")
+    pdata_version = V("0.0.post10896")
 except ImportError:
     pass
-data_git_hash = "311983c3c842c21230acbe09da10703e14cc5bb8"
-data_git_describe = "v0.0-10895-g311983c3c"
+data_git_hash = "ee05909e2a5a39be116484e537f62e35e5ae7573"
+data_git_describe = "v0.0-10896-gee05909e2"
 data_git_msg = """\
-commit 311983c3c842c21230acbe09da10703e14cc5bb8
-Author: Madhuri Patel <madhuri.patel@ensilica.com>
-Date:   Tue Mar 22 14:56:02 2022 +0000
+commit ee05909e2a5a39be116484e537f62e35e5ae7573
+Author: Timothy Chen <timothytim@google.com>
+Date:   Fri Mar 18 13:22:10 2022 -0700
 
-    [sysrst_ctrl,dv] Randomize flash_wp_l_in for flash_wp_prot test
+    [flah_ctrl] Fix alert indexing
     
-    Signed-off-by: Madhuri Patel <madhuri.patel@ensilica.com>
+    - standard fatal alerts are at position 1 instead of 0
+    
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
