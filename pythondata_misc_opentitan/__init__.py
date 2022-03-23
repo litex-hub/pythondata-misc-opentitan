fa5d0f5d46ab50aadfefd6b02f909e2952442283
LiteX Robot <robot@timvideos.us>
2022-03-23 14:27:16 +0000

    Updating pythondata-misc-opentitan to 0.0.post11035
    Updated data to v0.0-10909-g18cde68b2 based on 18cde68b2f3689f79a320f3006f84f06d33513e1 from https://github.com/lowRISC/opentitan.
    > commit 18cde68b2f3689f79a320f3006f84f06d33513e1
    > Author: Jade Philipoom <jadep@google.com>
    > Date:   Tue Mar 1 17:11:13 2022 +0000
    >
    >     [sw,crypto] Adjust cryptolib drivers to use abs_mmio.
    >
    >     Signed-off-by: Jade Philipoom <jadep@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,38 +4,30 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11031"
-version_tuple = (0, 0, 11031)
+version_str = "0.0.post11035"
+version_tuple = (0, 0, 11035)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11031")
+    pversion = V("0.0.post11035")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10905"
-data_version_tuple = (0, 0, 10905)
+data_version_str = "0.0.post10909"
+data_version_tuple = (0, 0, 10909)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10905")
+    pdata_version = V("0.0.post10909")
 except ImportError:
     pass
-data_git_hash = "cf7df522acbb58bdf604302a36c580bc02550f0a"
-data_git_describe = "v0.0-10905-gcf7df522a"
+data_git_hash = "18cde68b2f3689f79a320f3006f84f06d33513e1"
+data_git_describe = "v0.0-10909-g18cde68b2"
 data_git_msg = """\
-commit cf7df522acbb58bdf604302a36c580bc02550f0a
+commit 18cde68b2f3689f79a320f3006f84f06d33513e1
 Author: Jade Philipoom <jadep@google.com>
-Date:   Mon Mar 21 19:45:41 2022 +0000
+Date:   Tue Mar 1 17:11:13 2022 +0000
 
-    [doc] Update "getting started" diagram to show software first.
-    
-    Because everyone needs to build software, the workflow seems easier if
-    the software is built first, and then the different workflows branch off
-    afterwards.
-    
-    Also adjusts the FPGA flow to more accurately describe the steps
-    involved (bitstream must be loaded, and "loading" and "running" the
-    software are not really separate steps).
+    [sw,crypto] Adjust cryptolib drivers to use abs_mmio.
     
     Signed-off-by: Jade Philipoom <jadep@google.com>
 
