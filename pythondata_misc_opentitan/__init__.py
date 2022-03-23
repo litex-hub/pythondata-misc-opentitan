74d28bea071d88c03a7809eaf333ef7b4c03dc0c
LiteX Robot <robot@timvideos.us>
2022-03-23 19:24:15 +0000

    Updating pythondata-misc-opentitan to 0.0.post11040
    Updated data to v0.0-10914-gd7f048e47 based on d7f048e471b6da4ff0b7283be47572ce90b6558f from https://github.com/lowRISC/opentitan.
    > commit d7f048e471b6da4ff0b7283be47572ce90b6558f
    > Author: Michael Schaffner <msf@opentitan.org>
    > Date:   Wed Mar 23 10:36:03 2022 -0700
    >
    >     [otp_ctrl] Correct description to reflect current behavior
    >
    >     Signed-off-by: Michael Schaffner <msf@opentitan.org>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,30 +4,30 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11039"
-version_tuple = (0, 0, 11039)
+version_str = "0.0.post11040"
+version_tuple = (0, 0, 11040)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11039")
+    pversion = V("0.0.post11040")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10913"
-data_version_tuple = (0, 0, 10913)
+data_version_str = "0.0.post10914"
+data_version_tuple = (0, 0, 10914)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10913")
+    pdata_version = V("0.0.post10914")
 except ImportError:
     pass
-data_git_hash = "9634d8b81076f5746485bdf874d175ee824617dc"
-data_git_describe = "v0.0-10913-g9634d8b81"
+data_git_hash = "d7f048e471b6da4ff0b7283be47572ce90b6558f"
+data_git_describe = "v0.0-10914-gd7f048e47"
 data_git_msg = """\
-commit 9634d8b81076f5746485bdf874d175ee824617dc
+commit d7f048e471b6da4ff0b7283be47572ce90b6558f
 Author: Michael Schaffner <msf@opentitan.org>
-Date:   Tue Mar 22 13:55:50 2022 -0700
+Date:   Wed Mar 23 10:36:03 2022 -0700
 
-    [lc_ctrl/dv] Update DIF to account for hardware revision CSR
+    [otp_ctrl] Correct description to reflect current behavior
     
     Signed-off-by: Michael Schaffner <msf@opentitan.org>
 
