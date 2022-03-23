f486b84f2c81ee80e204ca453294958934269c20
LiteX Robot <robot@timvideos.us>
2022-03-23 06:33:53 +0000

    Updating pythondata-misc-opentitan to 0.0.post11029
    Updated data to v0.0-10903-g356c0543e based on 356c0543eec55b5b583592329ffa787612d06830 from https://github.com/lowRISC/opentitan.
    > commit 356c0543eec55b5b583592329ffa787612d06830
    > Author: Cindy Liu <hcindyl@google.com>
    > Date:   Mon Mar 21 16:57:49 2022 -0700
    >
    >     [topgen] Remove hard-code top name from topgen templates
    >
    >     Retrieve the top name from the config dictionary.
    >
    >     top level meson.build only has top_earlgrey defined. Leave top_earlgrey
    >     in SW device unit test meson.build.tpl for now since the build system
    >     goiing to be moved to bazel soon and this file will be deprecated
    >     afterwards.
    >
    >     Signed-off-by: Cindy Liu <hcindyl@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,39 +4,39 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11028"
-version_tuple = (0, 0, 11028)
+version_str = "0.0.post11029"
+version_tuple = (0, 0, 11029)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11028")
+    pversion = V("0.0.post11029")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10902"
-data_version_tuple = (0, 0, 10902)
+data_version_str = "0.0.post10903"
+data_version_tuple = (0, 0, 10903)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10902")
+    pdata_version = V("0.0.post10903")
 except ImportError:
     pass
-data_git_hash = "80bc06cb8940588d970b3ee05468978067c1e244"
-data_git_describe = "v0.0-10902-g80bc06cb8"
+data_git_hash = "356c0543eec55b5b583592329ffa787612d06830"
+data_git_describe = "v0.0-10903-g356c0543e"
 data_git_msg = """\
-commit 80bc06cb8940588d970b3ee05468978067c1e244
-Author: Cindy Chen <chencindy@opentitan.org>
-Date:   Tue Mar 22 13:15:30 2022 -0700
+commit 356c0543eec55b5b583592329ffa787612d06830
+Author: Cindy Liu <hcindyl@google.com>
+Date:   Mon Mar 21 16:57:49 2022 -0700
 
-    [dv/otp] Add cov for sw partition lock status
+    [topgen] Remove hard-code top name from topgen templates
     
-    this PR adds a covergroup for sw partition lock status:
-    1). Collect which SW partition it is.
-    2). Collect if it is write locked.
-    3). Collect if it is read locked.
-    4). Collect the current operation type.
-    Then cross the above coverpoints.
+    Retrieve the top name from the config dictionary.
     
-    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
+    top level meson.build only has top_earlgrey defined. Leave top_earlgrey
+    in SW device unit test meson.build.tpl for now since the build system
+    goiing to be moved to bazel soon and this file will be deprecated
+    afterwards.
+    
+    Signed-off-by: Cindy Liu <hcindyl@google.com>
 
 """
 
