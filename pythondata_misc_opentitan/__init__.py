5f04641f6f4837f53c56f0100eb5a2626009592e
LiteX Robot <robot@timvideos.us>
2022-03-24 06:34:03 +0000

    Updating pythondata-misc-opentitan to 0.0.post11068
    Updated data to v0.0-10942-ga66d66236 based on a66d66236aa3077e753a0c84533f64410c371d68 from https://github.com/lowRISC/opentitan.
    > commit a66d66236aa3077e753a0c84533f64410c371d68
    > Author: Srikrishna Iyer <sriyer@google.com>
    > Date:   Tue Mar 15 21:31:58 2022 -0700
    >
    >     [rv_dm dv] Testplan & DV doc updates
    >
    >     This commit adds the (almost) complete testplan for rv_dm.
    >     It incorporates the testplan review comments captured in the meeting
    >     notes:
    >     https://docs.google.com/document/d/1zIU5XQKFuu72SqUzEpaFGZEgQlIKgy6_iejXfatb1PQ/edit#heading=h.5utrntqp5k6d
    >
    >     The DV doc highlights the testbench block diagram and the flow of
    >     information into the scoreboard where most of the checks will be
    >     performed.
    >
    >     Signed-off-by: Srikrishna Iyer <sriyer@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,41 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11065"
-version_tuple = (0, 0, 11065)
+version_str = "0.0.post11068"
+version_tuple = (0, 0, 11068)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11065")
+    pversion = V("0.0.post11068")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10939"
-data_version_tuple = (0, 0, 10939)
+data_version_str = "0.0.post10942"
+data_version_tuple = (0, 0, 10942)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10939")
+    pdata_version = V("0.0.post10942")
 except ImportError:
     pass
-data_git_hash = "236a8d2c59c4b4eef903ca83abaa1a84fe52c162"
-data_git_describe = "v0.0-10939-g236a8d2c5"
+data_git_hash = "a66d66236aa3077e753a0c84533f64410c371d68"
+data_git_describe = "v0.0-10942-ga66d66236"
 data_git_msg = """\
-commit 236a8d2c59c4b4eef903ca83abaa1a84fe52c162
-Author: Timothy Trippel <ttrippel@google.com>
-Date:   Wed Mar 23 14:57:57 2022 -0700
+commit a66d66236aa3077e753a0c84533f64410c371d68
+Author: Srikrishna Iyer <sriyer@google.com>
+Date:   Tue Mar 15 21:31:58 2022 -0700
 
-    [dif/spi_host] update checklist and milestone
+    [rv_dm dv] Testplan & DV doc updates
     
-    This updates the DIF checklist and milestone to bring the DIF to S2.
+    This commit adds the (almost) complete testplan for rv_dm.
+    It incorporates the testplan review comments captured in the meeting
+    notes:
+    https://docs.google.com/document/d/1zIU5XQKFuu72SqUzEpaFGZEgQlIKgy6_iejXfatb1PQ/edit#heading=h.5utrntqp5k6d
     
-    Signed-off-by: Timothy Trippel <ttrippel@google.com>
+    The DV doc highlights the testbench block diagram and the flow of
+    information into the scoreboard where most of the checks will be
+    performed.
+    
+    Signed-off-by: Srikrishna Iyer <sriyer@google.com>
 
 """
 
