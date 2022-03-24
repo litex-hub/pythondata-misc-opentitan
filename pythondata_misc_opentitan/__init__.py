febe08cc30ad0dfe3e0ef2408542071ba271def9
LiteX Robot <robot@timvideos.us>
2022-03-24 10:30:57 +0000

    Updating pythondata-misc-opentitan to 0.0.post11078
    Updated data to v0.0-10952-g3cb095466 based on 3cb095466f4e404ad585364887878654d2ebc6db from https://github.com/lowRISC/opentitan.
    > commit 3cb095466f4e404ad585364887878654d2ebc6db
    > Author: David Pudner <davidpudner@protonmail.com>
    > Date:   Fri Feb 11 15:04:51 2022 +0000
    >
    >     [hw/dv] Removed colon from Questa build and run fail patterns.
    >
    >     It was found that not all error messages output in the Questa log output contained a colon following the Error keyword.
    >     This meant some error messages were missed.
    >
    >     Signed-off-by: David Pudner <davidpudner@protonmail.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,44 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11073"
-version_tuple = (0, 0, 11073)
+version_str = "0.0.post11078"
+version_tuple = (0, 0, 11078)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11073")
+    pversion = V("0.0.post11078")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10947"
-data_version_tuple = (0, 0, 10947)
+data_version_str = "0.0.post10952"
+data_version_tuple = (0, 0, 10952)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10947")
+    pdata_version = V("0.0.post10952")
 except ImportError:
     pass
-data_git_hash = "67783a65527f1aaaf937b9ce30866f19816e0205"
-data_git_describe = "v0.0-10947-g67783a655"
+data_git_hash = "3cb095466f4e404ad585364887878654d2ebc6db"
+data_git_describe = "v0.0-10952-g3cb095466"
 data_git_msg = """\
-commit 67783a65527f1aaaf937b9ce30866f19816e0205
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Fri Mar 18 13:11:07 2022 +0000
+commit 3cb095466f4e404ad585364887878654d2ebc6db
+Author: David Pudner <davidpudner@protonmail.com>
+Date:   Fri Feb 11 15:04:51 2022 +0000
 
-    [otbn,rtl] Fix start_stop_control done_o signal when SecWipeEn=0
+    [hw/dv] Removed colon from Questa build and run fail patterns.
     
-    This single-cycle pulse is supposed to happen at the end of the secure
-    wipe. When SecWipeEn=0, we have a single-cycle "secure wipe" that just
-    jumps straight to OtbnStartStopSecureWipeComplete. But the done_o
-    signal didn't match: fix that.
+    It was found that not all error messages output in the Questa log output contained a colon following the Error keyword.
+    This meant some error messages were missed.
     
-    There's also a corresponding DV change needed to get the status signal
-    timing right.
-    
-    Note that this commit only works with the previous two: we are now
-    delaying the "done_core" signal in otbn.sv by a cycle, even when
-    secure wipe is disabled.
-    
-    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    Signed-off-by: David Pudner <davidpudner@protonmail.com>
 
 """
 
