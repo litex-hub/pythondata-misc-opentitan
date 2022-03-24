f48ec9e9b065bcc9b82eedbf454430d6f2e4fa1d
LiteX Robot <robot@timvideos.us>
2022-03-24 12:44:06 +0000

    Updating pythondata-misc-opentitan to 0.0.post11082
    Updated data to v0.0-10956-ge99ab35e9 based on e99ab35e97c29d8f7b27694fd95f804811161833 from https://github.com/lowRISC/opentitan.
    > commit e99ab35e97c29d8f7b27694fd95f804811161833
    > Author: Alphan Ulusoy <alphan@google.com>
    > Date:   Wed Mar 23 15:38:58 2022 -0400
    >
    >     [sw,crypto] Remove remaining usages of mmio in the hmac driver
    >
    >     It seems like these were skipped in #11168, noticed after removing the
    >     mmio.h include from abs_mmio.h
    >
    >     Signed-off-by: Alphan Ulusoy <alphan@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11078"
-version_tuple = (0, 0, 11078)
+version_str = "0.0.post11082"
+version_tuple = (0, 0, 11082)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11078")
+    pversion = V("0.0.post11082")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post10952"
-data_version_tuple = (0, 0, 10952)
+data_version_str = "0.0.post10956"
+data_version_tuple = (0, 0, 10956)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post10952")
+    pdata_version = V("0.0.post10956")
 except ImportError:
     pass
-data_git_hash = "3cb095466f4e404ad585364887878654d2ebc6db"
-data_git_describe = "v0.0-10952-g3cb095466"
+data_git_hash = "e99ab35e97c29d8f7b27694fd95f804811161833"
+data_git_describe = "v0.0-10956-ge99ab35e9"
 data_git_msg = """\
-commit 3cb095466f4e404ad585364887878654d2ebc6db
-Author: David Pudner <davidpudner@protonmail.com>
-Date:   Fri Feb 11 15:04:51 2022 +0000
+commit e99ab35e97c29d8f7b27694fd95f804811161833
+Author: Alphan Ulusoy <alphan@google.com>
+Date:   Wed Mar 23 15:38:58 2022 -0400
 
-    [hw/dv] Removed colon from Questa build and run fail patterns.
+    [sw,crypto] Remove remaining usages of mmio in the hmac driver
     
-    It was found that not all error messages output in the Questa log output contained a colon following the Error keyword.
-    This meant some error messages were missed.
+    It seems like these were skipped in #11168, noticed after removing the
+    mmio.h include from abs_mmio.h
     
-    Signed-off-by: David Pudner <davidpudner@protonmail.com>
+    Signed-off-by: Alphan Ulusoy <alphan@google.com>
 
 """
 
