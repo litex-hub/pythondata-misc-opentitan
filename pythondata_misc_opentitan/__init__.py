25e16ef5091ec5d0a90e96a7ec2a09ba3f300f9a
LiteX Robot <robot@timvideos.us>
2022-03-25 15:29:24 +0000

    Updating pythondata-misc-opentitan to 0.0.post11128
    Updated data to v0.0-11002-ge9c6fcce9 based on e9c6fcce94aa3fae40ecd5388710abd1720c7fc5 from https://github.com/lowRISC/opentitan.
    > commit e9c6fcce94aa3fae40ecd5388710abd1720c7fc5
    > Author: Timothy Trippel <ttrippel@google.com>
    > Date:   Thu Mar 24 15:54:17 2022 -0700
    >
    >     [bazel] add python rules and register python toolchain
    >
    >     Currently, Python scripts are referenced across our repository as bazel
    >     files/filegroups, rather than using actualy Python bazel rules.
    >     Moreover, our current bazel configuration did not enforce hermiticity
    >     w.r.t. a python toolchain or packages. By adding Python rules and
    >     registering a toolchain, makes hermetic Python builds possible.
    >
    >     This fixes #11683.
    >
    >     Signed-off-by: Timothy Trippel <ttrippel@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,39 +4,40 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11126"
-version_tuple = (0, 0, 11126)
+version_str = "0.0.post11128"
+version_tuple = (0, 0, 11128)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11126")
+    pversion = V("0.0.post11128")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11000"
-data_version_tuple = (0, 0, 11000)
+data_version_str = "0.0.post11002"
+data_version_tuple = (0, 0, 11002)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11000")
+    pdata_version = V("0.0.post11002")
 except ImportError:
     pass
-data_git_hash = "313611f217e0e1cc52fd6faec306f866decb502b"
-data_git_describe = "v0.0-11000-g313611f21"
+data_git_hash = "e9c6fcce94aa3fae40ecd5388710abd1720c7fc5"
+data_git_describe = "v0.0-11002-ge9c6fcce9"
 data_git_msg = """\
-commit 313611f217e0e1cc52fd6faec306f866decb502b
-Author: Jade Philipoom <jadep@google.com>
-Date:   Wed Mar 23 13:59:17 2022 +0000
+commit e9c6fcce94aa3fae40ecd5388710abd1720c7fc5
+Author: Timothy Trippel <ttrippel@google.com>
+Date:   Thu Mar 24 15:54:17 2022 -0700
 
-    [doc] Clean up after feedback on Getting Started refactor.
+    [bazel] add python rules and register python toolchain
     
-    Fix some awkward wording etc, add some clarification about the
-    `--meminit` arguments to Verilator, and clarify that we do not currently
-    provide pre-built Docker containers.
+    Currently, Python scripts are referenced across our repository as bazel
+    files/filegroups, rather than using actualy Python bazel rules.
+    Moreover, our current bazel configuration did not enforce hermiticity
+    w.r.t. a python toolchain or packages. By adding Python rules and
+    registering a toolchain, makes hermetic Python builds possible.
     
-    Also incorporate sometext about Siemens Questa that was added to the
-    old install guide while the PR was pending.
+    This fixes #11683.
     
-    Signed-off-by: Jade Philipoom <jadep@google.com>
+    Signed-off-by: Timothy Trippel <ttrippel@google.com>
 
 """
 
