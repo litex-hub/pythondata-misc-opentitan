b138eb0ae3d9a6ce8b1fa7c322771e8617a4fa22
LiteX Robot <robot@timvideos.us>
2022-03-28 12:47:18 +0000

    Updating pythondata-misc-opentitan to 0.0.post11156
    Updated data to v0.0-11030-g8d507ad3f based on 8d507ad3f9dc47705b292a8e9568f73b772976ed from https://github.com/lowRISC/opentitan.
    > commit 8d507ad3f9dc47705b292a8e9568f73b772976ed
    > Author: Pirmin Vogel <vogelpi@lowrisc.org>
    > Date:   Thu Mar 24 16:32:08 2022 +0100
    >
    >     [aes] Fix clearing of data input registers
    >
    >     Previously, the write enable for the data input registers was set for
    >     two clock cycles when clearing the registers. This caused the
    >     data_in_qe_i signals used for status tracking to be high during the
    >     first clock cycle when back in IDLE. As a result, the AES unit would
    >     immediately start when running in automatic operation.
    >
    >     This is related to lowRISC/OpenTitan#11431.
    >
    >     Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,40 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11154"
-version_tuple = (0, 0, 11154)
+version_str = "0.0.post11156"
+version_tuple = (0, 0, 11156)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11154")
+    pversion = V("0.0.post11156")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11028"
-data_version_tuple = (0, 0, 11028)
+data_version_str = "0.0.post11030"
+data_version_tuple = (0, 0, 11030)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11028")
+    pdata_version = V("0.0.post11030")
 except ImportError:
     pass
-data_git_hash = "f95053d98ae63cc11627b9526ef96775ff18979a"
-data_git_describe = "v0.0-11028-gf95053d98"
+data_git_hash = "8d507ad3f9dc47705b292a8e9568f73b772976ed"
+data_git_describe = "v0.0-11030-g8d507ad3f"
 data_git_msg = """\
-commit f95053d98ae63cc11627b9526ef96775ff18979a
-Author: Muqing Liu <muqing.liu@wdc.com>
-Date:   Fri Mar 25 17:14:19 2022 -0700
+commit 8d507ad3f9dc47705b292a8e9568f73b772976ed
+Author: Pirmin Vogel <vogelpi@lowrisc.org>
+Date:   Thu Mar 24 16:32:08 2022 +0100
 
-    [edn, dv] Fix a bug for edn intr/err test
+    [aes] Fix clearing of data input registers
     
-      - Fix a path reference issue in edn intr/err test
+    Previously, the write enable for the data input registers was set for
+    two clock cycles when clearing the registers. This caused the
+    data_in_qe_i signals used for status tracking to be high during the
+    first clock cycle when back in IDLE. As a result, the AES unit would
+    immediately start when running in automatic operation.
     
-    Signed-off-by: Muqing Liu <muqing.liu@wdc.com>
+    This is related to lowRISC/OpenTitan#11431.
+    
+    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
 
 """
 
