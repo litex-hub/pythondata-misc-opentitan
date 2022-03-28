a3c4ce19ae1954b9790e82fd6eb37ab4ac39ac8f
LiteX Robot <robot@timvideos.us>
2022-03-28 18:33:26 +0000

    Updating pythondata-misc-opentitan to 0.0.post11162
    Updated data to v0.0-11036-gce0b9241b based on ce0b9241b4bf70ee407e5cd5385d246140b5ae10 from https://github.com/lowRISC/opentitan.
    > commit ce0b9241b4bf70ee407e5cd5385d246140b5ae10
    > Author: Jaedon Kim <jdonjdon@google.com>
    > Date:   Fri Mar 25 19:43:18 2022 +0000
    >
    >     [dv,clkmgr] sec cm testplan
    >
    >     Signed-off-by: Jaedon Kim <jdonjdon@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11161"
-version_tuple = (0, 0, 11161)
+version_str = "0.0.post11162"
+version_tuple = (0, 0, 11162)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11161")
+    pversion = V("0.0.post11162")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11035"
-data_version_tuple = (0, 0, 11035)
+data_version_str = "0.0.post11036"
+data_version_tuple = (0, 0, 11036)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11035")
+    pdata_version = V("0.0.post11036")
 except ImportError:
     pass
-data_git_hash = "68ce21e57a229c356b742752f007db49d44d884c"
-data_git_describe = "v0.0-11035-g68ce21e57"
+data_git_hash = "ce0b9241b4bf70ee407e5cd5385d246140b5ae10"
+data_git_describe = "v0.0-11036-gce0b9241b"
 data_git_msg = """\
-commit 68ce21e57a229c356b742752f007db49d44d884c
-Author: Cindy Chen <chencindy@opentitan.org>
-Date:   Fri Mar 25 17:21:35 2022 -0700
+commit ce0b9241b4bf70ee407e5cd5385d246140b5ae10
+Author: Jaedon Kim <jdonjdon@google.com>
+Date:   Fri Mar 25 19:43:18 2022 +0000
 
-    [conn] connectivity test naming fix
+    [dv,clkmgr] sec cm testplan
     
-    This PR fixed a duplicated naming error.
-    
-    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
+    Signed-off-by: Jaedon Kim <jdonjdon@google.com>
 
 """
 
