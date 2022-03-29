a7cf556feeb3ebcac1f235459e5a018bd8508f63
LiteX Robot <robot@timvideos.us>
2022-03-29 14:27:35 +0000

    Updating pythondata-misc-opentitan to 0.0.post11169
    Updated data to v0.0-11043-gcdcb6967f based on cdcb6967f9ff49f3e6b932734303695a7c51dab7 from https://github.com/lowRISC/opentitan.
    > commit cdcb6967f9ff49f3e6b932734303695a7c51dab7
    > Author: Jade Philipoom <jadep@google.com>
    > Date:   Tue Mar 22 13:46:33 2022 +0000
    >
    >     [sw,otbn] Remove outdated Makefile/rules.mk from code-snippets.
    >
    >     Found these files when making Bazel rules and they seem to be from two
    >     build systems ago; they say to remove once the Meson system is working.
    >     Seems like it should definitely be safe to remove them now.
    >
    >     Signed-off-by: Jade Philipoom <jadep@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11168"
-version_tuple = (0, 0, 11168)
+version_str = "0.0.post11169"
+version_tuple = (0, 0, 11169)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11168")
+    pversion = V("0.0.post11169")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11042"
-data_version_tuple = (0, 0, 11042)
+data_version_str = "0.0.post11043"
+data_version_tuple = (0, 0, 11043)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11042")
+    pdata_version = V("0.0.post11043")
 except ImportError:
     pass
-data_git_hash = "c2995063d22faaaf02b2054bc689a272a2499390"
-data_git_describe = "v0.0-11042-gc2995063d"
+data_git_hash = "cdcb6967f9ff49f3e6b932734303695a7c51dab7"
+data_git_describe = "v0.0-11043-gcdcb6967f"
 data_git_msg = """\
-commit c2995063d22faaaf02b2054bc689a272a2499390
-Author: Timothy Chen <timothytim@google.com>
-Date:   Mon Mar 28 18:29:11 2022 -0700
+commit cdcb6967f9ff49f3e6b932734303695a7c51dab7
+Author: Jade Philipoom <jadep@google.com>
+Date:   Tue Mar 22 13:46:33 2022 +0000
 
-    Revert "[aes] Fix clearing of data input registers"
+    [sw,otbn] Remove outdated Makefile/rules.mk from code-snippets.
     
-    This reverts commit 8d507ad3f9dc47705b292a8e9568f73b772976ed.
+    Found these files when making Bazel rules and they seem to be from two
+    build systems ago; they say to remove once the Meson system is working.
+    Seems like it should definitely be safe to remove them now.
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Jade Philipoom <jadep@google.com>
 
 """
 
