68298c5f6e6cf7d2d4d1a20b4869c9747017d645
LiteX Robot <robot@timvideos.us>
2022-03-30 21:26:50 +0000

    Updating pythondata-misc-opentitan to 0.0.post11223
    Updated data to v0.0-11097-gc427bf5ea based on c427bf5eafea5326e8ee0b979e6e39722016051f from https://github.com/lowRISC/opentitan.
    > commit c427bf5eafea5326e8ee0b979e6e39722016051f
    > Author: Steve Nelson <steve.nelson@wdc.com>
    > Date:   Wed Mar 30 10:35:07 2022 -0700
    >
    >     [edn/dv] Update testplan, add coverage
    >
    >     Signed-off-by: Steve Nelson <steve.nelson@wdc.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11221"
-version_tuple = (0, 0, 11221)
+version_str = "0.0.post11223"
+version_tuple = (0, 0, 11223)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11221")
+    pversion = V("0.0.post11223")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11095"
-data_version_tuple = (0, 0, 11095)
+data_version_str = "0.0.post11097"
+data_version_tuple = (0, 0, 11097)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11095")
+    pdata_version = V("0.0.post11097")
 except ImportError:
     pass
-data_git_hash = "339e498ed31a10b9acc2bc2063a2cfdeb131948f"
-data_git_describe = "v0.0-11095-g339e498ed"
+data_git_hash = "c427bf5eafea5326e8ee0b979e6e39722016051f"
+data_git_describe = "v0.0-11097-gc427bf5ea"
 data_git_msg = """\
-commit 339e498ed31a10b9acc2bc2063a2cfdeb131948f
-Author: Jason Hoddinett <jason.hoddinett.ensilica@opentitan.org>
-Date:   Tue Mar 22 09:51:26 2022 +0000
+commit c427bf5eafea5326e8ee0b979e6e39722016051f
+Author: Steve Nelson <steve.nelson@wdc.com>
+Date:   Wed Mar 30 10:35:07 2022 -0700
 
-    [pwm/dv] Added pwm_rand_output_vseq and environment changes
+    [edn/dv] Update testplan, add coverage
     
-    Fixed any outstanding typos or mistakes
-    
-    Signed-off-by: Jason Hoddinett <jason.hoddinett.ensilica@opentitan.org>
+    Signed-off-by: Steve Nelson <steve.nelson@wdc.com>
 
 """
 
