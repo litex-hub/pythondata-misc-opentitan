fe2a73fc9eb4350e3dbe3f619bda0250229a0024
LiteX Robot <robot@timvideos.us>
2022-03-31 10:31:58 +0000

    Updating pythondata-misc-opentitan to 0.0.post11247
    Updated data to v0.0-11121-g33ef10435 based on 33ef10435f5c2b5edbf93a54b39a14365c5059ff from https://github.com/lowRISC/opentitan.
    > commit 33ef10435f5c2b5edbf93a54b39a14365c5059ff
    > Author: Pirmin Vogel <vogelpi@lowrisc.org>
    > Date:   Tue Mar 29 10:57:03 2022 +0200
    >
    >     [aes] Fix clearing of data input registers without inferring combo loop
    >
    >     Previously, the write enable for the data input registers was set for
    >     two clock cycles when clearing the registers. This caused the
    >     data_in_qe_i signals used for status tracking to be high during the
    >     first clock cycle when back in IDLE. As a result, the AES unit would
    >     immediately start when running in automatic operation.
    >
    >     This is a second version of the fix that doesn't infer a combo loop by
    >     splitting the clearing operation into two distinct states: First CLEAR_I
    >     clears input registers such as Initial Key, IV and input data registers.
    >     Then CLEAR_CO waits for the cipher core, clears the trigger bits and
    >     if selected also clear the output data registers.
    >
    >     This is related to lowRISC/OpenTitan#11431 and lowRISC/OpenTitan#11758.
    >
    >     Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,46 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11245"
-version_tuple = (0, 0, 11245)
+version_str = "0.0.post11247"
+version_tuple = (0, 0, 11247)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11245")
+    pversion = V("0.0.post11247")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11119"
-data_version_tuple = (0, 0, 11119)
+data_version_str = "0.0.post11121"
+data_version_tuple = (0, 0, 11121)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11119")
+    pdata_version = V("0.0.post11121")
 except ImportError:
     pass
-data_git_hash = "0fb66204ea9e5c7b058763f88ac402d71783788d"
-data_git_describe = "v0.0-11119-g0fb66204e"
+data_git_hash = "33ef10435f5c2b5edbf93a54b39a14365c5059ff"
+data_git_describe = "v0.0-11121-g33ef10435"
 data_git_msg = """\
-commit 0fb66204ea9e5c7b058763f88ac402d71783788d
-Author: Douglas Reis <doreis@lowrisc.org>
-Date:   Tue Mar 22 16:23:13 2022 +0000
+commit 33ef10435f5c2b5edbf93a54b39a14365c5059ff
+Author: Pirmin Vogel <vogelpi@lowrisc.org>
+Date:   Tue Mar 29 10:57:03 2022 +0200
 
-    [doc, aes] Update the AES checklist status to S2
+    [aes] Fix clearing of data input registers without inferring combo loop
     
-    Signed-off-by: Douglas Reis <doreis@lowrisc.org>
+    Previously, the write enable for the data input registers was set for
+    two clock cycles when clearing the registers. This caused the
+    data_in_qe_i signals used for status tracking to be high during the
+    first clock cycle when back in IDLE. As a result, the AES unit would
+    immediately start when running in automatic operation.
+    
+    This is a second version of the fix that doesn't infer a combo loop by
+    splitting the clearing operation into two distinct states: First CLEAR_I
+    clears input registers such as Initial Key, IV and input data registers.
+    Then CLEAR_CO waits for the cipher core, clears the trigger bits and
+    if selected also clear the output data registers.
+    
+    This is related to lowRISC/OpenTitan#11431 and lowRISC/OpenTitan#11758.
+    
+    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
 
 """
 
