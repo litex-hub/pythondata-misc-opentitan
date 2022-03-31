8335d025bc71c1ffbcc78806cd9ba3e716dd0dd0
LiteX Robot <robot@timvideos.us>
2022-03-31 18:33:29 +0000

    Updating pythondata-misc-opentitan to 0.0.post11256
    Updated data to v0.0-11130-g3219d9c63 based on 3219d9c63bcd0040b49e664d789efe8021d287df from https://github.com/lowRISC/opentitan.
    > commit 3219d9c63bcd0040b49e664d789efe8021d287df
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Thu Mar 31 07:49:30 2022 -0700
    >
    >     [lc_ctrl] change the assert construction for clarity
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11255"
-version_tuple = (0, 0, 11255)
+version_str = "0.0.post11256"
+version_tuple = (0, 0, 11256)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11255")
+    pversion = V("0.0.post11256")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11129"
-data_version_tuple = (0, 0, 11129)
+data_version_str = "0.0.post11130"
+data_version_tuple = (0, 0, 11130)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11129")
+    pdata_version = V("0.0.post11130")
 except ImportError:
     pass
-data_git_hash = "47854c7edecab6f0f95c122a7cbab52368984e46"
-data_git_describe = "v0.0-11129-g47854c7ed"
+data_git_hash = "3219d9c63bcd0040b49e664d789efe8021d287df"
+data_git_describe = "v0.0-11130-g3219d9c63"
 data_git_msg = """\
-commit 47854c7edecab6f0f95c122a7cbab52368984e46
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Thu Mar 31 09:52:40 2022 +0100
+commit 3219d9c63bcd0040b49e664d789efe8021d287df
+Author: Timothy Chen <timothytim@google.com>
+Date:   Thu Mar 31 07:49:30 2022 -0700
 
-    [otbn] Declare V2
+    [lc_ctrl] change the assert construction for clarity
     
-    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
