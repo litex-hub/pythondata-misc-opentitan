be017d144897b6861161777ecf040b1480749df9
LiteX Robot <robot@timvideos.us>
2022-04-01 18:35:25 +0000

    Updating pythondata-misc-opentitan to 0.0.post11276
    Updated data to v0.0-11150-ga7dd59421 based on a7dd5942185b02b928e3b03dc1c5ea38c4d7517a from https://github.com/lowRISC/opentitan.
    > commit a7dd5942185b02b928e3b03dc1c5ea38c4d7517a
    > Author: Jaedon Kim <jdonjdon@google.com>
    > Date:   Thu Mar 31 19:47:57 2022 +0000
    >
    >     [clkmgr,dv] fix regression failure
    >
    >       - add back *_meas_ctrl_shadowed_* to exclusion list to avoid
    >         RECOV_ERR_CODE failure
    >       - update default idle value to false in csr test
    >
    >     Signed-off-by: Jaedon Kim <jdonjdon@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11274"
-version_tuple = (0, 0, 11274)
+version_str = "0.0.post11276"
+version_tuple = (0, 0, 11276)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11274")
+    pversion = V("0.0.post11276")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11148"
-data_version_tuple = (0, 0, 11148)
+data_version_str = "0.0.post11150"
+data_version_tuple = (0, 0, 11150)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11148")
+    pdata_version = V("0.0.post11150")
 except ImportError:
     pass
-data_git_hash = "c9f41377ba08715d659ae55266982965da61f67c"
-data_git_describe = "v0.0-11148-gc9f41377b"
+data_git_hash = "a7dd5942185b02b928e3b03dc1c5ea38c4d7517a"
+data_git_describe = "v0.0-11150-ga7dd59421"
 data_git_msg = """\
-commit c9f41377ba08715d659ae55266982965da61f67c
-Author: Timothy Chen <timothytim@google.com>
-Date:   Thu Mar 31 11:26:50 2022 -0700
+commit a7dd5942185b02b928e3b03dc1c5ea38c4d7517a
+Author: Jaedon Kim <jdonjdon@google.com>
+Date:   Thu Mar 31 19:47:57 2022 +0000
 
-    [flash_ctrl] make the rma termination process clearer
+    [clkmgr,dv] fix regression failure
     
-    - make it clear that after rma completes the flash controller disables itself
+      - add back *_meas_ctrl_shadowed_* to exclusion list to avoid
+        RECOV_ERR_CODE failure
+      - update default idle value to false in csr test
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Jaedon Kim <jdonjdon@google.com>
 
 """
 
