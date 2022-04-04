277fdfef4be636bfa2b9a0fbb1aa037b7b59984c
LiteX Robot <robot@timvideos.us>
2022-04-04 19:25:06 +0000

    Updating pythondata-misc-opentitan to 0.0.post11352
    Updated data to v0.0-11226-g615579ef0 based on 615579ef088d272354f6cd1df7c8f20642733dec from https://github.com/lowRISC/opentitan.
    > commit 615579ef088d272354f6cd1df7c8f20642733dec
    > Author: Miguel Young de la Sota <mcyoung@google.com>
    > Date:   Thu Mar 31 14:34:51 2022 -0400
    >
    >     [bazel] Migrate all linker scripts to use ld_library()
    >
    >     Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,30 +4,30 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11343"
-version_tuple = (0, 0, 11343)
+version_str = "0.0.post11352"
+version_tuple = (0, 0, 11352)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11343")
+    pversion = V("0.0.post11352")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11217"
-data_version_tuple = (0, 0, 11217)
+data_version_str = "0.0.post11226"
+data_version_tuple = (0, 0, 11226)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11217")
+    pdata_version = V("0.0.post11226")
 except ImportError:
     pass
-data_git_hash = "35d9712caf174da4da19e737386f4239db7ccfdb"
-data_git_describe = "v0.0-11217-g35d9712ca"
+data_git_hash = "615579ef088d272354f6cd1df7c8f20642733dec"
+data_git_describe = "v0.0-11226-g615579ef0"
 data_git_msg = """\
-commit 35d9712caf174da4da19e737386f4239db7ccfdb
+commit 615579ef088d272354f6cd1df7c8f20642733dec
 Author: Miguel Young de la Sota <mcyoung@google.com>
-Date:   Mon Apr 4 11:25:38 2022 -0400
+Date:   Thu Mar 31 14:34:51 2022 -0400
 
-    [ci] Inform the Bazel regression tests that some tests moved
+    [bazel] Migrate all linker scripts to use ld_library()
     
     Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>
 
