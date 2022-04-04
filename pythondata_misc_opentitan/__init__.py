bd6a8a413f28ca4dc3503cad803d119e91108036
LiteX Robot <robot@timvideos.us>
2022-04-04 18:34:14 +0000

    Updating pythondata-misc-opentitan to 0.0.post11343
    Updated data to v0.0-11217-g35d9712ca based on 35d9712caf174da4da19e737386f4239db7ccfdb from https://github.com/lowRISC/opentitan.
    > commit 35d9712caf174da4da19e737386f4239db7ccfdb
    > Author: Miguel Young de la Sota <mcyoung@google.com>
    > Date:   Mon Apr 4 11:25:38 2022 -0400
    >
    >     [ci] Inform the Bazel regression tests that some tests moved
    >
    >     Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11336"
-version_tuple = (0, 0, 11336)
+version_str = "0.0.post11343"
+version_tuple = (0, 0, 11343)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11336")
+    pversion = V("0.0.post11343")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11210"
-data_version_tuple = (0, 0, 11210)
+data_version_str = "0.0.post11217"
+data_version_tuple = (0, 0, 11217)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11210")
+    pdata_version = V("0.0.post11217")
 except ImportError:
     pass
-data_git_hash = "5852b71c5c0d70709699b4a18fc6de921bc34836"
-data_git_describe = "v0.0-11210-g5852b71c5"
+data_git_hash = "35d9712caf174da4da19e737386f4239db7ccfdb"
+data_git_describe = "v0.0-11217-g35d9712ca"
 data_git_msg = """\
-commit 5852b71c5c0d70709699b4a18fc6de921bc34836
-Author: Timothy Chen <timothytim@google.com>
-Date:   Fri Apr 1 16:06:15 2022 -0700
+commit 35d9712caf174da4da19e737386f4239db7ccfdb
+Author: Miguel Young de la Sota <mcyoung@google.com>
+Date:   Mon Apr 4 11:25:38 2022 -0400
 
-    [sensor_ctrl] move to D2
+    [ci] Inform the Bazel regression tests that some tests moved
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>
 
 """
 
