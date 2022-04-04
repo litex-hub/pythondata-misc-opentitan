7c3a3307dfb2b222301980167a5a63907800b55f
LiteX Robot <robot@timvideos.us>
2022-04-04 06:34:19 +0000

    Updating pythondata-misc-opentitan to 0.0.post11318
    Updated data to v0.0-11192-g98114295f based on 98114295f3ee93a4b4ebf1b7326645a5962eb67c from https://github.com/lowRISC/opentitan.
    > commit 98114295f3ee93a4b4ebf1b7326645a5962eb67c
    > Author: Madhuri Patel <madhuri.patel@ensilica.com>
    > Date:   Thu Mar 31 17:42:02 2022 +0100
    >
    >     [sysrst_ctrl,dv] Added register reads to fill coverage holes
    >
    >     Few register reads are added in the functional tests to fill
    >     in the assert coverage holes from sysrst_ctrl_csr_assert module.
    >
    >     Signed-off-by: Madhuri Patel <madhuri.patel@ensilica.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,30 +4,33 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11317"
-version_tuple = (0, 0, 11317)
+version_str = "0.0.post11318"
+version_tuple = (0, 0, 11318)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11317")
+    pversion = V("0.0.post11318")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11191"
-data_version_tuple = (0, 0, 11191)
+data_version_str = "0.0.post11192"
+data_version_tuple = (0, 0, 11192)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11191")
+    pdata_version = V("0.0.post11192")
 except ImportError:
     pass
-data_git_hash = "4d69eb2c229973a0aa43ffab60721504ce79e0bc"
-data_git_describe = "v0.0-11191-g4d69eb2c2"
+data_git_hash = "98114295f3ee93a4b4ebf1b7326645a5962eb67c"
+data_git_describe = "v0.0-11192-g98114295f"
 data_git_msg = """\
-commit 4d69eb2c229973a0aa43ffab60721504ce79e0bc
+commit 98114295f3ee93a4b4ebf1b7326645a5962eb67c
 Author: Madhuri Patel <madhuri.patel@ensilica.com>
-Date:   Thu Mar 31 10:33:22 2022 +0100
+Date:   Thu Mar 31 17:42:02 2022 +0100
 
-    [sysrst_ctrl,dv] Add sysrst_ctrl stress_all test
+    [sysrst_ctrl,dv] Added register reads to fill coverage holes
+    
+    Few register reads are added in the functional tests to fill
+    in the assert coverage holes from sysrst_ctrl_csr_assert module.
     
     Signed-off-by: Madhuri Patel <madhuri.patel@ensilica.com>
 
