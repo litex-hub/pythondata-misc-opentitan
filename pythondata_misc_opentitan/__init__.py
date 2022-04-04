ac48e4e46fdc291bb817506f6bdc18f40a3862cd
LiteX Robot <robot@timvideos.us>
2022-04-04 16:41:55 +0000

    Updating pythondata-misc-opentitan to 0.0.post11336
    Updated data to v0.0-11210-g5852b71c5 based on 5852b71c5c0d70709699b4a18fc6de921bc34836 from https://github.com/lowRISC/opentitan.
    > commit 5852b71c5c0d70709699b4a18fc6de921bc34836
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Fri Apr 1 16:06:15 2022 -0700
    >
    >     [sensor_ctrl] move to D2
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,38 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11328"
-version_tuple = (0, 0, 11328)
+version_str = "0.0.post11336"
+version_tuple = (0, 0, 11336)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11328")
+    pversion = V("0.0.post11336")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11202"
-data_version_tuple = (0, 0, 11202)
+data_version_str = "0.0.post11210"
+data_version_tuple = (0, 0, 11210)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11202")
+    pdata_version = V("0.0.post11210")
 except ImportError:
     pass
-data_git_hash = "55db958b26235dd786d231a79c060a5d5d47358d"
-data_git_describe = "v0.0-11202-g55db958b2"
+data_git_hash = "5852b71c5c0d70709699b4a18fc6de921bc34836"
+data_git_describe = "v0.0-11210-g5852b71c5"
 data_git_msg = """\
-commit 55db958b26235dd786d231a79c060a5d5d47358d
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Mon Apr 4 13:11:50 2022 +0100
+commit 5852b71c5c0d70709699b4a18fc6de921bc34836
+Author: Timothy Chen <timothytim@google.com>
+Date:   Fri Apr 1 16:06:15 2022 -0700
 
-    [dvsim,xcelium] Fix sed commands to generate plusargs
+    [sensor_ctrl] move to D2
     
-    I really messed up 0dc541c. I think that I'd checked the code with .*
-    and a non-empty string, then noticed that it also matched empty
-    strings. So I changed to .+, but forgot to check that .+ worked with
-    non-empty strings. It turns out that "+" for "one or more occurrences"
-    is only supported by sed with -E.
-    
-    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
