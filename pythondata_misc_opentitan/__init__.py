432a9bb700c0c5efb07527f5e13507aff77ae090
LiteX Robot <robot@timvideos.us>
2022-04-05 14:27:55 +0000

    Updating pythondata-misc-opentitan to 0.0.post11381
    Updated data to v0.0-11255-gf78b28395 based on f78b283951fd2d5c18d40348ce4b12b23b17f2ae from https://github.com/lowRISC/opentitan.
    > commit f78b283951fd2d5c18d40348ce4b12b23b17f2ae
    > Author: Miguel Young de la Sota <mcyoung@google.com>
    > Date:   Fri Apr 1 18:05:34 2022 -0400
    >
    >     [bazel] Merge mock targets in mask_rom
    >
    >     Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11375"
-version_tuple = (0, 0, 11375)
+version_str = "0.0.post11381"
+version_tuple = (0, 0, 11381)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11375")
+    pversion = V("0.0.post11381")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11249"
-data_version_tuple = (0, 0, 11249)
+data_version_str = "0.0.post11255"
+data_version_tuple = (0, 0, 11255)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11249")
+    pdata_version = V("0.0.post11255")
 except ImportError:
     pass
-data_git_hash = "e8be89f08c47cbd2934bad19decdd0f75a4823b2"
-data_git_describe = "v0.0-11249-ge8be89f08"
+data_git_hash = "f78b283951fd2d5c18d40348ce4b12b23b17f2ae"
+data_git_describe = "v0.0-11255-gf78b28395"
 data_git_msg = """\
-commit e8be89f08c47cbd2934bad19decdd0f75a4823b2
-Author: Andreas Kurth <adk@lowrisc.org>
-Date:   Fri Apr 1 08:38:07 2022 +0200
+commit f78b283951fd2d5c18d40348ce4b12b23b17f2ae
+Author: Miguel Young de la Sota <mcyoung@google.com>
+Date:   Fri Apr 1 18:05:34 2022 -0400
 
-    [otbn] Add FSM-related CM labels
+    [bazel] Merge mock targets in mask_rom
     
-    Signed-off-by: Andreas Kurth <adk@lowrisc.org>
+    Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>
 
 """
 
