d7df1a6cc925f379b5cd75788e8ab5fc8f0db6c7
LiteX Robot <robot@timvideos.us>
2022-04-05 08:33:53 +0000

    Updating pythondata-misc-opentitan to 0.0.post11368
    Updated data to v0.0-11242-g4d80330c6 based on 4d80330c6d13e4c8490bf5f8e4f0a9727ac5759d from https://github.com/lowRISC/opentitan.
    > commit 4d80330c6d13e4c8490bf5f8e4f0a9727ac5759d
    > Author: Pirmin Vogel <vogelpi@lowrisc.org>
    > Date:   Mon Apr 4 13:04:41 2022 +0200
    >
    >     [kmac] Ignore entropy refresh requests when running in SW entropy mode
    >
    >     When running in EDN entropy mode, software can manually request the
    >     entropy to be refreshed from EDN.
    >
    >     If instead running in SW entropy mode, SW provides the entropy via CSRs.
    >     SW is still able to request the entropy to be refreshed. Prior to this
    >     commit, the design would then go into the StRandEdn state and request
    >     fresh entropy from EDN, but the entropy received from EDN would be
    >     ignored. Instead, the LFSR would be reseeded using the value provided by
    >     SW via CSRs. If SW "forgot" to update the CSRs, the LFSR
    >     would be reseeded using the same value. This is a bit counter-intuitive
    >     and can be exploitet for SCA. It's safer to just ignore these requests
    >     when running in SW entropy mode.
    >
    >     Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,45 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11367"
-version_tuple = (0, 0, 11367)
+version_str = "0.0.post11368"
+version_tuple = (0, 0, 11368)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11367")
+    pversion = V("0.0.post11368")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11241"
-data_version_tuple = (0, 0, 11241)
+data_version_str = "0.0.post11242"
+data_version_tuple = (0, 0, 11242)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11241")
+    pdata_version = V("0.0.post11242")
 except ImportError:
     pass
-data_git_hash = "a01a0cc9621247b40b2a149154496f5cf7377e4d"
-data_git_describe = "v0.0-11241-ga01a0cc96"
+data_git_hash = "4d80330c6d13e4c8490bf5f8e4f0a9727ac5759d"
+data_git_describe = "v0.0-11242-g4d80330c6"
 data_git_msg = """\
-commit a01a0cc9621247b40b2a149154496f5cf7377e4d
-Author: Jaedon Kim <jdonjdon@google.com>
-Date:   Tue Apr 5 01:51:07 2022 +0000
+commit 4d80330c6d13e4c8490bf5f8e4f0a9727ac5759d
+Author: Pirmin Vogel <vogelpi@lowrisc.org>
+Date:   Mon Apr 4 13:04:41 2022 +0200
 
-    [dv,pwrmgr] fix regression failure
+    [kmac] Ignore entropy refresh requests when running in SW entropy mode
     
-     - pwrmgr_sec_cm_lc_ctrl_intersig_mubi : disalbe assertion
+    When running in EDN entropy mode, software can manually request the
+    entropy to be refreshed from EDN.
     
-    Signed-off-by: Jaedon Kim <jdonjdon@google.com>
+    If instead running in SW entropy mode, SW provides the entropy via CSRs.
+    SW is still able to request the entropy to be refreshed. Prior to this
+    commit, the design would then go into the StRandEdn state and request
+    fresh entropy from EDN, but the entropy received from EDN would be
+    ignored. Instead, the LFSR would be reseeded using the value provided by
+    SW via CSRs. If SW "forgot" to update the CSRs, the LFSR
+    would be reseeded using the same value. This is a bit counter-intuitive
+    and can be exploitet for SCA. It's safer to just ignore these requests
+    when running in SW entropy mode.
+    
+    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
 
 """
 
