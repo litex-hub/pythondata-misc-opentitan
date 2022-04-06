d8efadca169fd039e91b41b76bc1b3597c55957b
LiteX Robot <robot@timvideos.us>
2022-04-06 03:43:33 +0000

    Updating pythondata-misc-opentitan to 0.0.post11398
    Updated data to v0.0-11272-ge0261d0cb based on e0261d0cbeb6a0ff99500532b6784571b04297d3 from https://github.com/lowRISC/opentitan.
    > commit e0261d0cbeb6a0ff99500532b6784571b04297d3
    > Author: Michael Schaffner <msf@google.com>
    > Date:   Tue Apr 5 16:42:06 2022 -0700
    >
    >     [uart] Add assertions for constant output enables
    >
    >     Signed-off-by: Michael Schaffner <msf@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,38 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11391"
-version_tuple = (0, 0, 11391)
+version_str = "0.0.post11398"
+version_tuple = (0, 0, 11398)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11391")
+    pversion = V("0.0.post11398")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11265"
-data_version_tuple = (0, 0, 11265)
+data_version_str = "0.0.post11272"
+data_version_tuple = (0, 0, 11272)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11265")
+    pdata_version = V("0.0.post11272")
 except ImportError:
     pass
-data_git_hash = "75082fb5a6a92ec58d7adfb9abf7bfacb26f87c7"
-data_git_describe = "v0.0-11265-g75082fb5a"
+data_git_hash = "e0261d0cbeb6a0ff99500532b6784571b04297d3"
+data_git_describe = "v0.0-11272-ge0261d0cb"
 data_git_msg = """\
-commit 75082fb5a6a92ec58d7adfb9abf7bfacb26f87c7
-Author: Timothy Chen <timothytim@google.com>
-Date:   Mon Apr 4 10:52:33 2022 -0700
+commit e0261d0cbeb6a0ff99500532b6784571b04297d3
+Author: Michael Schaffner <msf@google.com>
+Date:   Tue Apr 5 16:42:06 2022 -0700
 
-    [flash_ctrl] Incorporate flash disable into flash_exec
+    [uart] Add assertions for constant output enables
     
-    - also add handling for what happens if instr_type is invalid
-      This was previously handled as part of transmission integrity, but
-      it does not hurt to make it more explicit.
-    
-    - fixes #11883
-    
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Michael Schaffner <msf@google.com>
 
 """
 
