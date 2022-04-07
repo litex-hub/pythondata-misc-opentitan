d5a5a85b074e5e96e2adb924ef553e02c2b05ecf
LiteX Robot <robot@timvideos.us>
2022-04-07 07:28:49 +0000

    Updating pythondata-misc-opentitan to 0.0.post11425
    Updated data to v0.0-11299-gc67e7fe12 based on c67e7fe129c145a65b6506dc962e5abdc11bdb7e from https://github.com/lowRISC/opentitan.
    > commit c67e7fe129c145a65b6506dc962e5abdc11bdb7e
    > Author: Greg Chadwick <gac@lowrisc.org>
    > Date:   Thu Mar 17 18:12:38 2022 +0000
    >
    >     [prim] Add prim_blanker
    >
    >     This wraps prim_and2 for use in blanking. prim_blanker provides a neater
    >     interface and a clear statement of intent for what it's being used for.
    >
    >     Signed-off-by: Greg Chadwick <gac@lowrisc.org>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,33 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11423"
-version_tuple = (0, 0, 11423)
+version_str = "0.0.post11425"
+version_tuple = (0, 0, 11425)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11423")
+    pversion = V("0.0.post11425")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11297"
-data_version_tuple = (0, 0, 11297)
+data_version_str = "0.0.post11299"
+data_version_tuple = (0, 0, 11299)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11297")
+    pdata_version = V("0.0.post11299")
 except ImportError:
     pass
-data_git_hash = "d8b2ed5accf591796a58e0a428ae9670e74d5646"
-data_git_describe = "v0.0-11297-gd8b2ed5ac"
+data_git_hash = "c67e7fe129c145a65b6506dc962e5abdc11bdb7e"
+data_git_describe = "v0.0-11299-gc67e7fe12"
 data_git_msg = """\
-commit d8b2ed5accf591796a58e0a428ae9670e74d5646
-Author: Weicai Yang <weicai@google.com>
-Date:   Wed Apr 6 14:31:12 2022 -0700
+commit c67e7fe129c145a65b6506dc962e5abdc11bdb7e
+Author: Greg Chadwick <gac@lowrisc.org>
+Date:   Thu Mar 17 18:12:38 2022 +0000
 
-    [flash, dv] Fix CI failures
+    [prim] Add prim_blanker
     
-    Due to merging of these 2 PR - #11823 and #11770
-    Signed-off-by: Weicai Yang <weicai@google.com>
+    This wraps prim_and2 for use in blanking. prim_blanker provides a neater
+    interface and a clear statement of intent for what it's being used for.
+    
+    Signed-off-by: Greg Chadwick <gac@lowrisc.org>
 
 """
 
