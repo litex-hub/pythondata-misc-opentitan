af6f7cc3b8cf6292f9453b096174bdb2a4f54205
LiteX Robot <robot@timvideos.us>
2022-04-07 19:25:48 +0000

    Updating pythondata-misc-opentitan to 0.0.post11449
    Updated data to v0.0-11323-g6fa56cff0 based on 6fa56cff07af3bcaad0a411bb4e47c09232d85f5 from https://github.com/lowRISC/opentitan.
    > commit 6fa56cff07af3bcaad0a411bb4e47c09232d85f5
    > Author: Weicai Yang <weicai@google.com>
    > Date:   Wed Apr 6 22:23:25 2022 -0700
    >
    >     [top, dv] Fix bootstrap test
    >
    >     It takes more time to process a frame of data, fixed TB sending SPI data
    >     too fast.
    >     Changed to wait for a message from SW to know the frame processed is
    >     done.
    >
    >     Signed-off-by: Weicai Yang <weicai@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,37 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11445"
-version_tuple = (0, 0, 11445)
+version_str = "0.0.post11449"
+version_tuple = (0, 0, 11449)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11445")
+    pversion = V("0.0.post11449")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11319"
-data_version_tuple = (0, 0, 11319)
+data_version_str = "0.0.post11323"
+data_version_tuple = (0, 0, 11323)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11319")
+    pdata_version = V("0.0.post11323")
 except ImportError:
     pass
-data_git_hash = "b670be80eefb83408568167c82c3f2dd255adcdd"
-data_git_describe = "v0.0-11319-gb670be80e"
+data_git_hash = "6fa56cff07af3bcaad0a411bb4e47c09232d85f5"
+data_git_describe = "v0.0-11323-g6fa56cff0"
 data_git_msg = """\
-commit b670be80eefb83408568167c82c3f2dd255adcdd
-Author: Michael Schaffner <msf@opentitan.org>
-Date:   Wed Apr 6 21:34:26 2022 -0700
+commit 6fa56cff07af3bcaad0a411bb4e47c09232d85f5
+Author: Weicai Yang <weicai@google.com>
+Date:   Wed Apr 6 22:23:25 2022 -0700
 
-    [lc_ctrl] Add more details to countermeasure testplan
+    [top, dv] Fix bootstrap test
     
-    This addresses #11731 and #11919.
+    It takes more time to process a frame of data, fixed TB sending SPI data
+    too fast.
+    Changed to wait for a message from SW to know the frame processed is
+    done.
     
-    Signed-off-by: Michael Schaffner <msf@opentitan.org>
+    Signed-off-by: Weicai Yang <weicai@google.com>
 
 """
 
