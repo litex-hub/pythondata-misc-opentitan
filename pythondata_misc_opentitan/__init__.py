bf9ba92877928c6faf0f12133226aadc876cb1df
LiteX Robot <robot@timvideos.us>
2022-04-08 18:34:44 +0000

    Updating pythondata-misc-opentitan to 0.0.post11476
    Updated data to v0.0-11350-g876de558d based on 876de558dc1e58d00c7742bb25e115ca23f96096 from https://github.com/lowRISC/opentitan.
    > commit 876de558dc1e58d00c7742bb25e115ca23f96096
    > Author: Srikrishna Iyer <sriyer@google.com>
    > Date:   Fri Apr 8 00:29:43 2022 -0700
    >
    >     [chip dv] Fix coverage collection for chip XBAR tests
    >
    >     The coverage on all functional hierarchies were inadvertantly
    >     being collected on chip level XBAR tests, which are supposed to
    >     stub all TL hosts and devices. This was causing a larger coverge
    >     numbers to be reported than intended. This commit fixes that.
    >
    >     We add ` xbar_build_mode` as a replacement for the `default` build
    >     mode. This is done so, so that the chip level testbench which
    >     reuses XBAR tests can append / override `xbar_build_mode` with
    >     additional build-time settings. We use this method to specify
    >     the right coverage collection hierarchy file for the XBAR tests.
    >     This coverage collection hierarchy file is the same one as the one
    >     use for the chip level `cover_reg_top` build mode - which restricts
    >     the coverage collection only to TL interfaces on all hosts and
    >     devices.
    >
    >     Signed-off-by: Srikrishna Iyer <sriyer@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,47 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11473"
-version_tuple = (0, 0, 11473)
+version_str = "0.0.post11476"
+version_tuple = (0, 0, 11476)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11473")
+    pversion = V("0.0.post11476")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11347"
-data_version_tuple = (0, 0, 11347)
+data_version_str = "0.0.post11350"
+data_version_tuple = (0, 0, 11350)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11347")
+    pdata_version = V("0.0.post11350")
 except ImportError:
     pass
-data_git_hash = "bb069b45a1d859edd14aacd0ae2dc6a028fba224"
-data_git_describe = "v0.0-11347-gbb069b45a"
+data_git_hash = "876de558dc1e58d00c7742bb25e115ca23f96096"
+data_git_describe = "v0.0-11350-g876de558d"
 data_git_msg = """\
-commit bb069b45a1d859edd14aacd0ae2dc6a028fba224
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Fri Apr 8 08:20:19 2022 +0100
+commit 876de558dc1e58d00c7742bb25e115ca23f96096
+Author: Srikrishna Iyer <sriyer@google.com>
+Date:   Fri Apr 8 00:29:43 2022 -0700
 
-    [keymgr,lint] Make some implicit widenings explicit
+    [chip dv] Fix coverage collection for chip XBAR tests
     
-    This silences lint warnings from Verilator
+    The coverage on all functional hierarchies were inadvertantly
+    being collected on chip level XBAR tests, which are supposed to
+    stub all TL hosts and devices. This was causing a larger coverge
+    numbers to be reported than intended. This commit fixes that.
     
-    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    We add ` xbar_build_mode` as a replacement for the `default` build
+    mode. This is done so, so that the chip level testbench which
+    reuses XBAR tests can append / override `xbar_build_mode` with
+    additional build-time settings. We use this method to specify
+    the right coverage collection hierarchy file for the XBAR tests.
+    This coverage collection hierarchy file is the same one as the one
+    use for the chip level `cover_reg_top` build mode - which restricts
+    the coverage collection only to TL interfaces on all hosts and
+    devices.
+    
+    Signed-off-by: Srikrishna Iyer <sriyer@google.com>
 
 """
 
