002a8c8d782d4bd52956b0b336095d491ca0623a
LiteX Robot <robot@timvideos.us>
2022-04-08 21:26:45 +0000

    Updating pythondata-misc-opentitan to 0.0.post11488
    Updated data to v0.0-11362-g375f4fd47 based on 375f4fd479d1392a6d2be219384f49f8159cf20f from https://github.com/lowRISC/opentitan.
    > commit 375f4fd479d1392a6d2be219384f49f8159cf20f
    > Author: Jaedon Kim <jdonjdon@google.com>
    > Date:   Wed Apr 6 14:27:43 2022 +0000
    >
    >     dv,top,rstmgr] chip_sw_rstrmgr_sw_rst
    >
    >        1. This test checks rstmgr.sw_rst_ctrl_n[] with peripheral devices.
    >        2. fix typo in chip_testplan reset manager section
    >
    >     Signed-off-by: Jaedon Kim <jdonjdon@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11481"
-version_tuple = (0, 0, 11481)
+version_str = "0.0.post11488"
+version_tuple = (0, 0, 11488)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11481")
+    pversion = V("0.0.post11488")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11355"
-data_version_tuple = (0, 0, 11355)
+data_version_str = "0.0.post11362"
+data_version_tuple = (0, 0, 11362)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11355")
+    pdata_version = V("0.0.post11362")
 except ImportError:
     pass
-data_git_hash = "a08273a6996906e0e68372a72ac838cd23346594"
-data_git_describe = "v0.0-11355-ga08273a69"
+data_git_hash = "375f4fd479d1392a6d2be219384f49f8159cf20f"
+data_git_describe = "v0.0-11362-g375f4fd47"
 data_git_msg = """\
-commit a08273a6996906e0e68372a72ac838cd23346594
-Author: Timothy Chen <timothytim@google.com>
-Date:   Thu Apr 7 10:36:58 2022 -0700
+commit 375f4fd479d1392a6d2be219384f49f8159cf20f
+Author: Jaedon Kim <jdonjdon@google.com>
+Date:   Wed Apr 6 14:27:43 2022 +0000
 
-    [rv_core_ibex, top] Make USR_ACCESS available to software
+    dv,top,rstmgr] chip_sw_rstrmgr_sw_rst
     
-    - fixes #11964
+       1. This test checks rstmgr.sw_rst_ctrl_n[] with peripheral devices.
+       2. fix typo in chip_testplan reset manager section
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Jaedon Kim <jdonjdon@google.com>
 
 """
 
