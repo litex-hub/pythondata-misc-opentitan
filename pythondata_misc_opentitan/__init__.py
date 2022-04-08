46d84f86fb65bfff7e2c631f6198d72bb6d7799e
LiteX Robot <robot@timvideos.us>
2022-04-08 22:29:00 +0000

    Updating pythondata-misc-opentitan to 0.0.post11492
    Updated data to v0.0-11366-g88a4bcc87 based on 88a4bcc87472a2e29d75faa2ee2756b6d5d19bec from https://github.com/lowRISC/opentitan.
    > commit 88a4bcc87472a2e29d75faa2ee2756b6d5d19bec
    > Author: Steve Nelson <steve.nelson@wdc.com>
    > Date:   Fri Apr 8 10:21:39 2022 -0700
    >
    >     [edn/dv] Implement my V2 Review TODO Items listed in Issue #11489
    >
    >     Signed-off-by: Steve Nelson <steve.nelson@wdc.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11488"
-version_tuple = (0, 0, 11488)
+version_str = "0.0.post11492"
+version_tuple = (0, 0, 11492)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11488")
+    pversion = V("0.0.post11492")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11362"
-data_version_tuple = (0, 0, 11362)
+data_version_str = "0.0.post11366"
+data_version_tuple = (0, 0, 11366)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11362")
+    pdata_version = V("0.0.post11366")
 except ImportError:
     pass
-data_git_hash = "375f4fd479d1392a6d2be219384f49f8159cf20f"
-data_git_describe = "v0.0-11362-g375f4fd47"
+data_git_hash = "88a4bcc87472a2e29d75faa2ee2756b6d5d19bec"
+data_git_describe = "v0.0-11366-g88a4bcc87"
 data_git_msg = """\
-commit 375f4fd479d1392a6d2be219384f49f8159cf20f
-Author: Jaedon Kim <jdonjdon@google.com>
-Date:   Wed Apr 6 14:27:43 2022 +0000
+commit 88a4bcc87472a2e29d75faa2ee2756b6d5d19bec
+Author: Steve Nelson <steve.nelson@wdc.com>
+Date:   Fri Apr 8 10:21:39 2022 -0700
 
-    dv,top,rstmgr] chip_sw_rstrmgr_sw_rst
+    [edn/dv] Implement my V2 Review TODO Items listed in Issue #11489
     
-       1. This test checks rstmgr.sw_rst_ctrl_n[] with peripheral devices.
-       2. fix typo in chip_testplan reset manager section
-    
-    Signed-off-by: Jaedon Kim <jdonjdon@google.com>
+    Signed-off-by: Steve Nelson <steve.nelson@wdc.com>
 
 """
 
