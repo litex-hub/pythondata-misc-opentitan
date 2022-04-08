ebfbfba9af77275f0567fa179a9cd1086da568b2
LiteX Robot <robot@timvideos.us>
2022-04-08 20:30:22 +0000

    Updating pythondata-misc-opentitan to 0.0.post11481
    Updated data to v0.0-11355-ga08273a69 based on a08273a6996906e0e68372a72ac838cd23346594 from https://github.com/lowRISC/opentitan.
    > commit a08273a6996906e0e68372a72ac838cd23346594
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Thu Apr 7 10:36:58 2022 -0700
    >
    >     [rv_core_ibex, top] Make USR_ACCESS available to software
    >
    >     - fixes #11964
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11479"
-version_tuple = (0, 0, 11479)
+version_str = "0.0.post11481"
+version_tuple = (0, 0, 11481)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11479")
+    pversion = V("0.0.post11481")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11353"
-data_version_tuple = (0, 0, 11353)
+data_version_str = "0.0.post11355"
+data_version_tuple = (0, 0, 11355)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11353")
+    pdata_version = V("0.0.post11355")
 except ImportError:
     pass
-data_git_hash = "06ccc1ea1828d78b5a9651ab3c6d4e7480a6b541"
-data_git_describe = "v0.0-11353-g06ccc1ea1"
+data_git_hash = "a08273a6996906e0e68372a72ac838cd23346594"
+data_git_describe = "v0.0-11355-ga08273a69"
 data_git_msg = """\
-commit 06ccc1ea1828d78b5a9651ab3c6d4e7480a6b541
-Author: Michael Schaffner <msf@google.com>
-Date:   Thu Apr 7 19:11:49 2022 -0700
+commit a08273a6996906e0e68372a72ac838cd23346594
+Author: Timothy Chen <timothytim@google.com>
+Date:   Thu Apr 7 10:36:58 2022 -0700
 
-    [tlul_lc_gate] Use prim_blanker instead of muxes and buffers
+    [rv_core_ibex, top] Make USR_ACCESS available to software
     
-    Signed-off-by: Michael Schaffner <msf@google.com>
+    - fixes #11964
+    
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
