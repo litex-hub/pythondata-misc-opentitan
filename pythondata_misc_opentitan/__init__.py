d2c5a1acf3554e675639e4ae223b6498731fa513
LiteX Robot <robot@timvideos.us>
2022-04-08 07:28:44 +0000

    Updating pythondata-misc-opentitan to 0.0.post11460
    Updated data to v0.0-11334-g0ef9e0469 based on 0ef9e04697ab2f484bd588795c1e6a49ec892a56 from https://github.com/lowRISC/opentitan.
    > commit 0ef9e04697ab2f484bd588795c1e6a49ec892a56
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Thu Apr 7 17:52:37 2022 +0100
    >
    >     [rom_ctrl,rtl] Tweak extraction of in_state_done to avoid lint error
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,38 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11459"
-version_tuple = (0, 0, 11459)
+version_str = "0.0.post11460"
+version_tuple = (0, 0, 11460)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11459")
+    pversion = V("0.0.post11460")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11333"
-data_version_tuple = (0, 0, 11333)
+data_version_str = "0.0.post11334"
+data_version_tuple = (0, 0, 11334)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11333")
+    pdata_version = V("0.0.post11334")
 except ImportError:
     pass
-data_git_hash = "86286403a900eacb6af89686a3ccde85a4404637"
-data_git_describe = "v0.0-11333-g86286403a"
+data_git_hash = "0ef9e04697ab2f484bd588795c1e6a49ec892a56"
+data_git_describe = "v0.0-11334-g0ef9e0469"
 data_git_msg = """\
-commit 86286403a900eacb6af89686a3ccde85a4404637
-Author: Cindy Chen <chencindy@opentitan.org>
-Date:   Tue Apr 5 10:20:19 2022 -0700
+commit 0ef9e04697ab2f484bd588795c1e6a49ec892a56
+Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
+Date:   Thu Apr 7 17:52:37 2022 +0100
 
-    [dv/otp] Increase FSM coverage
+    [rom_ctrl,rtl] Tweak extraction of in_state_done to avoid lint error
     
-    This PR increases the FSM cov by:
-    1). Add an enum to control random timing to issue reset or lc_esc.
-    2). Add some flags to mark specific FSM states: such as digests cal,
-      init, etc.
-    3). Enhance some randomizations in existing tests.
-    
-    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
+    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
 """
 
