b34fdedff185b5ede6de8f8058100448d56ad4f9
LiteX Robot <robot@timvideos.us>
2022-04-08 14:27:04 +0000

    Updating pythondata-misc-opentitan to 0.0.post11466
    Updated data to v0.0-11340-gc19face37 based on c19face3711d149f7befa6c8fdee08b90f8c172a from https://github.com/lowRISC/opentitan.
    > commit c19face3711d149f7befa6c8fdee08b90f8c172a
    > Author: Miguel Young de la Sota <mcyoung@google.com>
    > Date:   Wed Apr 6 11:06:01 2022 -0400
    >
    >     [mask_rom] Add a separate library for .static_critical symbols
    >
    >     Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11462"
-version_tuple = (0, 0, 11462)
+version_str = "0.0.post11466"
+version_tuple = (0, 0, 11466)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11462")
+    pversion = V("0.0.post11466")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11336"
-data_version_tuple = (0, 0, 11336)
+data_version_str = "0.0.post11340"
+data_version_tuple = (0, 0, 11340)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11336")
+    pdata_version = V("0.0.post11340")
 except ImportError:
     pass
-data_git_hash = "ef0063c1167897d722242ffc11a2890eb363b508"
-data_git_describe = "v0.0-11336-gef0063c11"
+data_git_hash = "c19face3711d149f7befa6c8fdee08b90f8c172a"
+data_git_describe = "v0.0-11340-gc19face37"
 data_git_msg = """\
-commit ef0063c1167897d722242ffc11a2890eb363b508
-Author: Douglas Reis <doreis@lowrisc.org>
-Date:   Mon Apr 4 16:56:16 2022 +0100
+commit c19face3711d149f7befa6c8fdee08b90f8c172a
+Author: Miguel Young de la Sota <mcyoung@google.com>
+Date:   Wed Apr 6 11:06:01 2022 -0400
 
-    [dif, aes] Refactor aes unittest
+    [mask_rom] Add a separate library for .static_critical symbols
     
-    Signed-off-by: Douglas Reis <doreis@lowrisc.org>
+    Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>
 
 """
 
