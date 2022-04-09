c5b345a73505f9f721fe89245a2b1f1a9c21ccb9
LiteX Robot <robot@timvideos.us>
2022-04-09 01:51:09 +0000

    Updating pythondata-misc-opentitan to 0.0.post11494
    Updated data to v0.0-11368-g43a24e646 based on 43a24e6461673730a30de1c66d7c997f89bd7597 from https://github.com/lowRISC/opentitan.
    > commit 43a24e6461673730a30de1c66d7c997f89bd7597
    > Author: Srikrishna Iyer <sriyer@google.com>
    > Date:   Thu Apr 7 00:37:07 2022 -0700
    >
    >     [cdc-rand] Enable CDC random delay injection
    >
    >     This commit builds upon the work by @udinator and instantiates the CDC random
    >     delay model within the prim_flop_2sync module. It injects delays into the incoming
    >     source data before it passes through the 2 flop synchronizer.
    >
    >     It also comes with its mini testbench, which currently lacks any checks, but
    >     serves as a way to view waves and check the behavior via visual inspection.
    >
    >     The delay insertion is currently disabled due to smoke regr failures.
    >
    >     Signed-off-by: Srikrishna Iyer <sriyer@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,41 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11492"
-version_tuple = (0, 0, 11492)
+version_str = "0.0.post11494"
+version_tuple = (0, 0, 11494)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11492")
+    pversion = V("0.0.post11494")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11366"
-data_version_tuple = (0, 0, 11366)
+data_version_str = "0.0.post11368"
+data_version_tuple = (0, 0, 11368)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11366")
+    pdata_version = V("0.0.post11368")
 except ImportError:
     pass
-data_git_hash = "88a4bcc87472a2e29d75faa2ee2756b6d5d19bec"
-data_git_describe = "v0.0-11366-g88a4bcc87"
+data_git_hash = "43a24e6461673730a30de1c66d7c997f89bd7597"
+data_git_describe = "v0.0-11368-g43a24e646"
 data_git_msg = """\
-commit 88a4bcc87472a2e29d75faa2ee2756b6d5d19bec
-Author: Steve Nelson <steve.nelson@wdc.com>
-Date:   Fri Apr 8 10:21:39 2022 -0700
+commit 43a24e6461673730a30de1c66d7c997f89bd7597
+Author: Srikrishna Iyer <sriyer@google.com>
+Date:   Thu Apr 7 00:37:07 2022 -0700
 
-    [edn/dv] Implement my V2 Review TODO Items listed in Issue #11489
+    [cdc-rand] Enable CDC random delay injection
     
-    Signed-off-by: Steve Nelson <steve.nelson@wdc.com>
+    This commit builds upon the work by @udinator and instantiates the CDC random
+    delay model within the prim_flop_2sync module. It injects delays into the incoming
+    source data before it passes through the 2 flop synchronizer.
+    
+    It also comes with its mini testbench, which currently lacks any checks, but
+    serves as a way to view waves and check the behavior via visual inspection.
+    
+    The delay insertion is currently disabled due to smoke regr failures.
+    
+    Signed-off-by: Srikrishna Iyer <sriyer@google.com>
 
 """
 
