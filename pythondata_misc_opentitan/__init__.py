f6d614e59d7b459ff1a21cb2e2189c0f89e92148
LiteX Robot <robot@timvideos.us>
2022-04-09 03:36:30 +0000

    Updating pythondata-misc-opentitan to 0.0.post11495
    Updated data to v0.0-11369-gef74184eb based on ef74184eb4a8dc7e48fa081cfb7671df30dcce77 from https://github.com/lowRISC/opentitan.
    > commit ef74184eb4a8dc7e48fa081cfb7671df30dcce77
    > Author: Michael Schaffner <msf@opentitan.org>
    > Date:   Wed Apr 6 22:13:35 2022 -0700
    >
    >     [lc_ctrl] Ensure inconsistent token mux triggers TRANSITION_ERROR
    >
    >     Signed-off-by: Michael Schaffner <msf@opentitan.org>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,41 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11494"
-version_tuple = (0, 0, 11494)
+version_str = "0.0.post11495"
+version_tuple = (0, 0, 11495)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11494")
+    pversion = V("0.0.post11495")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11368"
-data_version_tuple = (0, 0, 11368)
+data_version_str = "0.0.post11369"
+data_version_tuple = (0, 0, 11369)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11368")
+    pdata_version = V("0.0.post11369")
 except ImportError:
     pass
-data_git_hash = "43a24e6461673730a30de1c66d7c997f89bd7597"
-data_git_describe = "v0.0-11368-g43a24e646"
+data_git_hash = "ef74184eb4a8dc7e48fa081cfb7671df30dcce77"
+data_git_describe = "v0.0-11369-gef74184eb"
 data_git_msg = """\
-commit 43a24e6461673730a30de1c66d7c997f89bd7597
-Author: Srikrishna Iyer <sriyer@google.com>
-Date:   Thu Apr 7 00:37:07 2022 -0700
+commit ef74184eb4a8dc7e48fa081cfb7671df30dcce77
+Author: Michael Schaffner <msf@opentitan.org>
+Date:   Wed Apr 6 22:13:35 2022 -0700
 
-    [cdc-rand] Enable CDC random delay injection
+    [lc_ctrl] Ensure inconsistent token mux triggers TRANSITION_ERROR
     
-    This commit builds upon the work by @udinator and instantiates the CDC random
-    delay model within the prim_flop_2sync module. It injects delays into the incoming
-    source data before it passes through the 2 flop synchronizer.
-    
-    It also comes with its mini testbench, which currently lacks any checks, but
-    serves as a way to view waves and check the behavior via visual inspection.
-    
-    The delay insertion is currently disabled due to smoke regr failures.
-    
-    Signed-off-by: Srikrishna Iyer <sriyer@google.com>
+    Signed-off-by: Michael Schaffner <msf@opentitan.org>
 
 """
 
