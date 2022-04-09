293737275ddd239be16917dddc03dc2bf912a686
LiteX Robot <robot@timvideos.us>
2022-04-09 22:28:32 +0000

    Updating pythondata-misc-opentitan to 0.0.post11499
    Updated data to v0.0-11373-g8d83613c4 based on 8d83613c4cd516e0e619384984d0d62072ebadf8 from https://github.com/lowRISC/opentitan.
    > commit 8d83613c4cd516e0e619384984d0d62072ebadf8
    > Author: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
    > Date:   Fri Apr 8 12:59:04 2022 -0700
    >
    >     [entropy_src/rtl] Suppress re-assertion of interrupts
    >
    >     The entropy_data and observe FIFOs are cleared with the entropy_src
    >     is disabled, but any interrupts saying they have data must be
    >     cleared by FW.
    >
    >     As noted in issue #11951, there is also a delay between when the
    >     IP is disabled and the FIFO is cleared.  This means that
    >     even when FIFO interrupts are cleared after disabling, the interrupt
    >     can reassert itself in this brief delay period.
    >
    >     This PR therefore suppresses these interrupt events from
    >     retriggering when the IP is disabled.
    >
    >     Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,44 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11498"
-version_tuple = (0, 0, 11498)
+version_str = "0.0.post11499"
+version_tuple = (0, 0, 11499)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11498")
+    pversion = V("0.0.post11499")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11372"
-data_version_tuple = (0, 0, 11372)
+data_version_str = "0.0.post11373"
+data_version_tuple = (0, 0, 11373)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11372")
+    pdata_version = V("0.0.post11373")
 except ImportError:
     pass
-data_git_hash = "493fbbdf68d895b8a736236aa791e0e3bced6044"
-data_git_describe = "v0.0-11372-g493fbbdf6"
+data_git_hash = "8d83613c4cd516e0e619384984d0d62072ebadf8"
+data_git_describe = "v0.0-11373-g8d83613c4"
 data_git_msg = """\
-commit 493fbbdf68d895b8a736236aa791e0e3bced6044
-Author: Chris Frantz <cfrantz@google.com>
-Date:   Fri Apr 8 12:28:05 2022 -0700
+commit 8d83613c4cd516e0e619384984d0d62072ebadf8
+Author: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
+Date:   Fri Apr 8 12:59:04 2022 -0700
 
-    [bazel, clang] Update to the latest bazel-embedded
+    [entropy_src/rtl] Suppress re-assertion of interrupts
     
-    1. Downloads resources as http archives (for airgapped builds).
-    2. Fixes the compiler configuration to not find the system compiler.
+    The entropy_data and observe FIFOs are cleared with the entropy_src
+    is disabled, but any interrupts saying they have data must be
+    cleared by FW.
     
-    Signed-off-by: Chris Frantz <cfrantz@google.com>
+    As noted in issue #11951, there is also a delay between when the
+    IP is disabled and the FIFO is cleared.  This means that
+    even when FIFO interrupts are cleared after disabling, the interrupt
+    can reassert itself in this brief delay period.
+    
+    This PR therefore suppresses these interrupt events from
+    retriggering when the IP is disabled.
+    
+    Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
 
 """
 
