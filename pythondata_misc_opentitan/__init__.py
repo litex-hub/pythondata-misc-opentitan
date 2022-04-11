60eee3bd7a8763554da37ab4ded7c5e8669e0c85
LiteX Robot <robot@timvideos.us>
2022-04-11 22:28:34 +0000

    Updating pythondata-misc-opentitan to 0.0.post11513
    Updated data to v0.0-11387-g5fd9acdb6 based on 5fd9acdb61b2eb0d6ecce5037e33d4d59850c3dd from https://github.com/lowRISC/opentitan.
    > commit 5fd9acdb61b2eb0d6ecce5037e33d4d59850c3dd
    > Author: Jaedon Kim <jdonjdon@google.com>
    > Date:   Fri Apr 8 18:48:23 2022 +0000
    >
    >     pwrmgr,dv] v2s_review_ready
    >
    >     Signed-off-by: Jaedon Kim <jdonjdon@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,50 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11512"
-version_tuple = (0, 0, 11512)
+version_str = "0.0.post11513"
+version_tuple = (0, 0, 11513)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11512")
+    pversion = V("0.0.post11513")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11386"
-data_version_tuple = (0, 0, 11386)
+data_version_str = "0.0.post11387"
+data_version_tuple = (0, 0, 11387)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11386")
+    pdata_version = V("0.0.post11387")
 except ImportError:
     pass
-data_git_hash = "bee94fda2845061741afa6b85c448378e1be2b8f"
-data_git_describe = "v0.0-11386-gbee94fda2"
+data_git_hash = "5fd9acdb61b2eb0d6ecce5037e33d4d59850c3dd"
+data_git_describe = "v0.0-11387-g5fd9acdb6"
 data_git_msg = """\
-commit bee94fda2845061741afa6b85c448378e1be2b8f
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Mon Apr 11 12:52:24 2022 +0100
+commit 5fd9acdb61b2eb0d6ecce5037e33d4d59850c3dd
+Author: Jaedon Kim <jdonjdon@google.com>
+Date:   Fri Apr 8 18:48:23 2022 +0000
 
-    [rom_ctrl,rtl] Tighten up expression for counter_data_rdy
+    pwrmgr,dv] v2s_review_ready
     
-    This signal is used to tell the counter that it can increment on this
-    cycle. Usually, that would be because we'd managed to pass a packet of
-    data to KMAC (when state is ReadingLow).
-    
-    Once we've finished passing things to KMAC, we want to race through
-    the last 8 words with no back pressure. This was implemented by the
-    "state_q != ReadingLow" term. Unfortunately, an injected error on
-    state_q when we're in the ReadingLow state caused us to spuriously
-    increment the ROM index which, in turn, meant that the data we were
-    presenting to KMAC changed.
-    
-    This isn't a big deal (the chip is already going to deadlock), but it
-    causes an assertion failure. Rather than messing around with
-    assertions, let's just tighten up the expression so that we control
-    the "race ahead" behaviour more explicitly. ReadingHigh and KmacAhead
-    are the two FSM states where we've finished passing data to KMAC but
-    haven't yet finished reading the top of the ROM.
-    
-    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    Signed-off-by: Jaedon Kim <jdonjdon@google.com>
 
 """
 
