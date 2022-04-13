247239dc40588585fd4f69327cd728ea64a23af4
LiteX Robot <robot@timvideos.us>
2022-04-13 02:04:14 +0000

    Updating pythondata-misc-opentitan to 0.0.post11542
    Updated data to v0.0-11416-gc3279dce6 based on c3279dce64edb3c2507ea992f1756055c5080b77 from https://github.com/lowRISC/opentitan.
    > commit c3279dce64edb3c2507ea992f1756055c5080b77
    > Author: Alexander Williams <awill@google.com>
    > Date:   Tue Apr 12 16:11:39 2022 -0700
    >
    >     [build] Fix up test_rom / flash_ctrl deps
    >
    >     Signed-off-by: Alexander Williams <awill@google.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,38 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11539"
-version_tuple = (0, 0, 11539)
+version_str = "0.0.post11542"
+version_tuple = (0, 0, 11542)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11539")
+    pversion = V("0.0.post11542")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11413"
-data_version_tuple = (0, 0, 11413)
+data_version_str = "0.0.post11416"
+data_version_tuple = (0, 0, 11416)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11413")
+    pdata_version = V("0.0.post11416")
 except ImportError:
     pass
-data_git_hash = "6fbcf741bd43222abc75227dde7ef9e80483df80"
-data_git_describe = "v0.0-11413-g6fbcf741b"
+data_git_hash = "c3279dce64edb3c2507ea992f1756055c5080b77"
+data_git_describe = "v0.0-11416-gc3279dce6"
 data_git_msg = """\
-commit 6fbcf741bd43222abc75227dde7ef9e80483df80
-Author: Cindy Chen <chencindy@opentitan.org>
-Date:   Thu Apr 7 11:16:11 2022 -0700
+commit c3279dce64edb3c2507ea992f1756055c5080b77
+Author: Alexander Williams <awill@google.com>
+Date:   Tue Apr 12 16:11:39 2022 -0700
 
-    [dv/chip] Add alert_ping_timeout test
+    [build] Fix up test_rom / flash_ctrl deps
     
-    This PR adds an aler_ping_timeout test in chip-level.
-    As the testplan describes, this test will configure the alert ping
-    timeout to a very small value - 2 cycles, and expect the alert ping to
-    always timeout. Then we will check if the interrupt is triggered
-    correctly, and if the local alert cause is correct.
-    
-    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
+    Signed-off-by: Alexander Williams <awill@google.com>
 
 """
 
