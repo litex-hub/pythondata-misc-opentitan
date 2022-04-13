8c515e00f2e203007880c3be0783cf8e196e39fa
LiteX Robot <robot@timvideos.us>
2022-04-13 17:31:56 +0000

    Updating pythondata-misc-opentitan to 0.0.post11556
    Updated data to v0.0-11430-ge228d1257 based on e228d1257b7b83cadf3add722a695984c217f438 from https://github.com/lowRISC/opentitan.
    > commit e228d1257b7b83cadf3add722a695984c217f438
    > Author: Rasmus Madsen <rasmus.madsen@wdc.com>
    > Date:   Tue Apr 12 05:21:23 2022 -0700
    >
    >     [aes/dv] updated aes refines for V2
    >
    >     Signed-off-by: Rasmus Madsen <rasmus.madsen@wdc.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11555"
-version_tuple = (0, 0, 11555)
+version_str = "0.0.post11556"
+version_tuple = (0, 0, 11556)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11555")
+    pversion = V("0.0.post11556")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11429"
-data_version_tuple = (0, 0, 11429)
+data_version_str = "0.0.post11430"
+data_version_tuple = (0, 0, 11430)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11429")
+    pdata_version = V("0.0.post11430")
 except ImportError:
     pass
-data_git_hash = "937c6a83e84a6eaaf6eaff00c43a5b8ad429e986"
-data_git_describe = "v0.0-11429-g937c6a83e"
+data_git_hash = "e228d1257b7b83cadf3add722a695984c217f438"
+data_git_describe = "v0.0-11430-ge228d1257"
 data_git_msg = """\
-commit 937c6a83e84a6eaaf6eaff00c43a5b8ad429e986
-Author: Timothy Chen <timothytim@google.com>
-Date:   Tue Apr 12 15:05:43 2022 -0700
+commit e228d1257b7b83cadf3add722a695984c217f438
+Author: Rasmus Madsen <rasmus.madsen@wdc.com>
+Date:   Tue Apr 12 05:21:23 2022 -0700
 
-    [doc] Minor fix to sensor_ctrl sw markdown
+    [aes/dv] updated aes refines for V2
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Rasmus Madsen <rasmus.madsen@wdc.com>
 
 """
 
