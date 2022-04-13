4554847aab6e3ad2df88ab528a80804f2f65f0e2
LiteX Robot <robot@timvideos.us>
2022-04-13 04:36:48 +0000

    Updating pythondata-misc-opentitan to 0.0.post11543
    Updated data to v0.0-11417-g913a02d91 based on 913a02d91a7c51fc4b8397cc73affbce0ca3ad23 from https://github.com/lowRISC/opentitan.
    > commit 913a02d91a7c51fc4b8397cc73affbce0ca3ad23
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Tue Apr 12 14:40:44 2022 -0700
    >
    >     [dv/chip] Fix regression build-seed failure
    >
    >     This PR fixes regression top-level failure when using `--build-seed`
    >     option. This temp solution won't randomize OTP image until we found the
    >     issue.
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11542"
-version_tuple = (0, 0, 11542)
+version_str = "0.0.post11543"
+version_tuple = (0, 0, 11543)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11542")
+    pversion = V("0.0.post11543")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11416"
-data_version_tuple = (0, 0, 11416)
+data_version_str = "0.0.post11417"
+data_version_tuple = (0, 0, 11417)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11416")
+    pdata_version = V("0.0.post11417")
 except ImportError:
     pass
-data_git_hash = "c3279dce64edb3c2507ea992f1756055c5080b77"
-data_git_describe = "v0.0-11416-gc3279dce6"
+data_git_hash = "913a02d91a7c51fc4b8397cc73affbce0ca3ad23"
+data_git_describe = "v0.0-11417-g913a02d91"
 data_git_msg = """\
-commit c3279dce64edb3c2507ea992f1756055c5080b77
-Author: Alexander Williams <awill@google.com>
-Date:   Tue Apr 12 16:11:39 2022 -0700
+commit 913a02d91a7c51fc4b8397cc73affbce0ca3ad23
+Author: Cindy Chen <chencindy@opentitan.org>
+Date:   Tue Apr 12 14:40:44 2022 -0700
 
-    [build] Fix up test_rom / flash_ctrl deps
+    [dv/chip] Fix regression build-seed failure
     
-    Signed-off-by: Alexander Williams <awill@google.com>
+    This PR fixes regression top-level failure when using `--build-seed`
+    option. This temp solution won't randomize OTP image until we found the
+    issue.
+    
+    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
 """
 
