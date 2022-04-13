fe5c95178dc7b162659bf6c4190ae428294a5905
LiteX Robot <robot@timvideos.us>
2022-04-13 09:28:31 +0000

    Updating pythondata-misc-opentitan to 0.0.post11545
    Updated data to v0.0-11419-g79a3d0125 based on 79a3d01253a7e6d32bac88bd8096a0c2b0051ea6 from https://github.com/lowRISC/opentitan.
    > commit 79a3d01253a7e6d32bac88bd8096a0c2b0051ea6
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Fri Apr 8 15:52:55 2022 +0100
    >
    >     [otbn,rtl] Signal done on escalation when waiting for URND
    >
    >     As far as the external model of OTBN is concerned, an operation has
    >     started. So we should ping the "done" interrupt as we lock ourselves.
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11543"
-version_tuple = (0, 0, 11543)
+version_str = "0.0.post11545"
+version_tuple = (0, 0, 11545)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11543")
+    pversion = V("0.0.post11545")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11417"
-data_version_tuple = (0, 0, 11417)
+data_version_str = "0.0.post11419"
+data_version_tuple = (0, 0, 11419)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11417")
+    pdata_version = V("0.0.post11419")
 except ImportError:
     pass
-data_git_hash = "913a02d91a7c51fc4b8397cc73affbce0ca3ad23"
-data_git_describe = "v0.0-11417-g913a02d91"
+data_git_hash = "79a3d01253a7e6d32bac88bd8096a0c2b0051ea6"
+data_git_describe = "v0.0-11419-g79a3d0125"
 data_git_msg = """\
-commit 913a02d91a7c51fc4b8397cc73affbce0ca3ad23
-Author: Cindy Chen <chencindy@opentitan.org>
-Date:   Tue Apr 12 14:40:44 2022 -0700
+commit 79a3d01253a7e6d32bac88bd8096a0c2b0051ea6
+Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
+Date:   Fri Apr 8 15:52:55 2022 +0100
 
-    [dv/chip] Fix regression build-seed failure
+    [otbn,rtl] Signal done on escalation when waiting for URND
     
-    This PR fixes regression top-level failure when using `--build-seed`
-    option. This temp solution won't randomize OTP image until we found the
-    issue.
+    As far as the external model of OTBN is concerned, an operation has
+    started. So we should ping the "done" interrupt as we lock ourselves.
     
-    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
+    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
 """
 
