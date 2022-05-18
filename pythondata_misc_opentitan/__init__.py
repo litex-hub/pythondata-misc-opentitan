414ff519773e6b9d383909539a11ea645f53e571
LiteX Robot <robot@timvideos.us>
2022-05-18 18:16:40 +0000

    Updating pythondata-misc-opentitan to 0.0.post12213
    Updated data to v0.0-12085-gb9370006f based on b9370006f49956571000ec34c7f7ae84a08badc2 from https://github.com/lowRISC/opentitan.
    > commit b9370006f49956571000ec34c7f7ae84a08badc2
    > Author: Miguel Young de la Sota <mcyoung@google.com>
    > Date:   Thu May 12 13:34:36 2022 -0400
    >
    >     [dif/edn] Add unit tests
    >
    >     Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>
    >
    
    Updated using 0.0.post128 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,41 +4,41 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post11560"
-version_tuple = (0, 0, 11560)
+version_str = "0.0.post12213"
+version_tuple = (0, 0, 12213)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post11560")
+    pversion = V("0.0.post12213")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post11434"
-data_version_tuple = (0, 0, 11434)
+data_version_str = "0.0.post12085"
+data_version_tuple = (0, 0, 12085)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post11434")
+    pdata_version = V("0.0.post12085")
 except ImportError:
     pass
-data_git_hash = "9ab6a2dcb4005a3dcf15c65022377e1646238b13"
-data_git_describe = "v0.0-11434-g9ab6a2dcb"
+data_git_hash = "b9370006f49956571000ec34c7f7ae84a08badc2"
+data_git_describe = "v0.0-12085-gb9370006f"
 data_git_msg = """\
-commit 9ab6a2dcb4005a3dcf15c65022377e1646238b13
-Author: Alexander Williams <awill@google.com>
-Date:   Fri Apr 8 07:51:09 2022 -0700
+commit b9370006f49956571000ec34c7f7ae84a08badc2
+Author: Miguel Young de la Sota <mcyoung@google.com>
+Date:   Thu May 12 13:34:36 2022 -0400
 
-    [pinmux/dif] Update checklist for usage and unit tests
+    [dif/edn] Add unit tests
     
-    Signed-off-by: Alexander Williams <awill@google.com>
+    Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>
 
 """
 
 # Tool version info
-tool_version_str = "0.0.post126"
-tool_version_tuple = (0, 0, 126)
+tool_version_str = "0.0.post128"
+tool_version_tuple = (0, 0, 128)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post126")
+    ptool_version = V("0.0.post128")
 except ImportError:
     pass
 
