7d24829c0bfa8663a4e0cd5ee9347482a55062a5
LiteX Robot <robot@timvideos.us>
2022-05-19 22:12:52 +0000

    Updating pythondata-misc-opentitan to 0.0.post12246
    Updated data to v0.0-12118-geb25b5bb4 based on eb25b5bb4f583a0dcba970539cbfc0799413e8de from https://github.com/lowRISC/opentitan.
    > commit eb25b5bb4f583a0dcba970539cbfc0799413e8de
    > Author: Douglas Reis <doreis@lowrisc.org>
    > Date:   Tue May 17 14:59:48 2022 +0100
    >
    >     [dif, hmac] Add unittest for `...mode_hmac_start` function
    >
    >     Signed-off-by: Douglas Reis <doreis@lowrisc.org>
    >
    
    Updated using 0.0.post128 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12241"
-version_tuple = (0, 0, 12241)
+version_str = "0.0.post12246"
+version_tuple = (0, 0, 12246)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12241")
+    pversion = V("0.0.post12246")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12113"
-data_version_tuple = (0, 0, 12113)
+data_version_str = "0.0.post12118"
+data_version_tuple = (0, 0, 12118)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12113")
+    pdata_version = V("0.0.post12118")
 except ImportError:
     pass
-data_git_hash = "69766dd58d497e6bba429bb4110b45a66f7ce7ab"
-data_git_describe = "v0.0-12113-g69766dd58"
+data_git_hash = "eb25b5bb4f583a0dcba970539cbfc0799413e8de"
+data_git_describe = "v0.0-12118-geb25b5bb4"
 data_git_msg = """\
-commit 69766dd58d497e6bba429bb4110b45a66f7ce7ab
-Author: Madhuri Patel <madhuri.patel@ensilica.com>
-Date:   Thu May 19 13:38:50 2022 +0100
+commit eb25b5bb4f583a0dcba970539cbfc0799413e8de
+Author: Douglas Reis <doreis@lowrisc.org>
+Date:   Tue May 17 14:59:48 2022 +0100
 
-    [sysrst_ctrl,dv] Fix the combo detect seq name in stress all test
+    [dif, hmac] Add unittest for `...mode_hmac_start` function
     
-    Signed-off-by: Madhuri Patel <madhuri.patel@ensilica.com>
+    Signed-off-by: Douglas Reis <doreis@lowrisc.org>
 
 """
 
