0ce1b11bfaa1df94ed9c35aeda69a1bc04a27246
LiteX Robot <robot@timvideos.us>
2022-05-20 20:14:34 +0000

    Updating pythondata-misc-opentitan to 0.0.post12266
    Updated data to v0.0-12138-ga5093e820 based on a5093e820b4aeca7a6985430434356334d220f6d from https://github.com/lowRISC/opentitan.
    > commit a5093e820b4aeca7a6985430434356334d220f6d
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Fri May 20 10:22:56 2022 -0700
    >
    >     [dv/lc_ctrl] temp remove lc_ctrl stress_all_with_rand_reset test
    >
    >     This PR temp removes a test to unblock nightly regression hanging issue.
    >     An issue is filed to re-enable this test: #12807
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post128 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12265"
-version_tuple = (0, 0, 12265)
+version_str = "0.0.post12266"
+version_tuple = (0, 0, 12266)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12265")
+    pversion = V("0.0.post12266")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12137"
-data_version_tuple = (0, 0, 12137)
+data_version_str = "0.0.post12138"
+data_version_tuple = (0, 0, 12138)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12137")
+    pdata_version = V("0.0.post12138")
 except ImportError:
     pass
-data_git_hash = "421b9a95536da34e9efcc5b84ec6fbd555e02e39"
-data_git_describe = "v0.0-12137-g421b9a955"
+data_git_hash = "a5093e820b4aeca7a6985430434356334d220f6d"
+data_git_describe = "v0.0-12138-ga5093e820"
 data_git_msg = """\
-commit 421b9a95536da34e9efcc5b84ec6fbd555e02e39
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Fri May 20 12:15:58 2022 +0100
+commit a5093e820b4aeca7a6985430434356334d220f6d
+Author: Cindy Chen <chencindy@opentitan.org>
+Date:   Fri May 20 10:22:56 2022 -0700
 
-    [otbn,dv] Re-order definitions in otbn_model
+    [dv/lc_ctrl] temp remove lc_ctrl stress_all_with_rand_reset test
     
-    This is a little nitty, but the definitions in the header file, C++
-    file and both DPI headers were all in different orders. And, in some
-    cases, quite bizarre orders! Put them all the same way around.
+    This PR temp removes a test to unblock nightly regression hanging issue.
+    An issue is filed to re-enable this test: #12807
     
-    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
 """
 
