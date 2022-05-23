95a3b697f2fb3201ee84d801117e62245e68f614
LiteX Robot <robot@timvideos.us>
2022-05-23 08:15:36 +0000

    Updating pythondata-misc-opentitan to 0.0.post12271
    Updated data to v0.0-12143-g46e134fa9 based on 46e134fa9faf6652c6ee4a05fd5cb138d32508ae from https://github.com/lowRISC/opentitan.
    > commit 46e134fa9faf6652c6ee4a05fd5cb138d32508ae
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Fri May 20 17:26:23 2022 +0100
    >
    >     [otbn,doc] Reword documentation of the RND ISPR
    >
    >     This is mostly just a rejig to make it easier to understand. But it
    >     also documents the new cache-clearing behaviour implemented in
    >     8e97525.
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post128 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12270"
-version_tuple = (0, 0, 12270)
+version_str = "0.0.post12271"
+version_tuple = (0, 0, 12271)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12270")
+    pversion = V("0.0.post12271")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12142"
-data_version_tuple = (0, 0, 12142)
+data_version_str = "0.0.post12143"
+data_version_tuple = (0, 0, 12143)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12142")
+    pdata_version = V("0.0.post12143")
 except ImportError:
     pass
-data_git_hash = "2a6e9f0277edfdfd8c612c267d37fa0a74e253da"
-data_git_describe = "v0.0-12142-g2a6e9f027"
+data_git_hash = "46e134fa9faf6652c6ee4a05fd5cb138d32508ae"
+data_git_describe = "v0.0-12143-g46e134fa9"
 data_git_msg = """\
-commit 2a6e9f0277edfdfd8c612c267d37fa0a74e253da
-Author: Alphan Ulusoy <alphan@google.com>
-Date:   Fri May 20 13:08:12 2022 -0400
+commit 46e134fa9faf6652c6ee4a05fd5cb138d32508ae
+Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
+Date:   Fri May 20 17:26:23 2022 +0100
 
-    [sw/silicon_creator] Relax the default case in bootstrap_handle_program()
+    [otbn,doc] Reword documentation of the RND ISPR
     
-    We don't expect any other commands since the switch statement covers all
-    commands that are handled by SW. However, since we can potentially
-    get a 0x0 opcode due to glitches on SPI or strap lines. See #11871.
+    This is mostly just a rejig to make it easier to understand. But it
+    also documents the new cache-clearing behaviour implemented in
+    8e97525.
     
-    Signed-off-by: Alphan Ulusoy <alphan@google.com>
+    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
 """
 
