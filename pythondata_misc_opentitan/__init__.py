164c8ef7024483eefca74e846398624b3c19d8cf
LiteX Robot <robot@timvideos.us>
2022-05-23 22:13:05 +0000

    Updating pythondata-misc-opentitan to 0.0.post12287
    Updated data to v0.0-12159-g256f09bd6 based on 256f09bd6cfc255df5636ec75230392536bdc2f7 from https://github.com/lowRISC/opentitan.
    > commit 256f09bd6cfc255df5636ec75230392536bdc2f7
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Mon May 16 15:49:26 2022 -0700
    >
    >     [dv/chip] LC_walkthrough test [PART3]
    >
    >     Adding a transition to RMA state using the RMA token.
    >     This PR adds the following transitions:
    >     1). RAW -> TESTUNLOCK0 -> PROD -> RMA
    >     2). RAW -> TESTUNLOCK0 -> DEV -> RMA
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post128 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,37 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12286"
-version_tuple = (0, 0, 12286)
+version_str = "0.0.post12287"
+version_tuple = (0, 0, 12287)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12286")
+    pversion = V("0.0.post12287")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12158"
-data_version_tuple = (0, 0, 12158)
+data_version_str = "0.0.post12159"
+data_version_tuple = (0, 0, 12159)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12158")
+    pdata_version = V("0.0.post12159")
 except ImportError:
     pass
-data_git_hash = "b42dbc2d004358a7036ab4eee2c02b79e091bead"
-data_git_describe = "v0.0-12158-gb42dbc2d0"
+data_git_hash = "256f09bd6cfc255df5636ec75230392536bdc2f7"
+data_git_describe = "v0.0-12159-g256f09bd6"
 data_git_msg = """\
-commit b42dbc2d004358a7036ab4eee2c02b79e091bead
-Author: Timothy Trippel <ttrippel@google.com>
-Date:   Tue May 17 17:02:42 2022 -0700
+commit 256f09bd6cfc255df5636ec75230392536bdc2f7
+Author: Cindy Chen <chencindy@opentitan.org>
+Date:   Mon May 16 15:49:26 2022 -0700
 
-    [bazel,dvsim] update dvsim.py to use Bazel to build SW
+    [dv/chip] LC_walkthrough test [PART3]
     
-    This fixes #11563 by updating dvsim.py, and the chip-level testbench, to
-    build SW artifacts with Bazel (instead of meson).
+    Adding a transition to RMA state using the RMA token.
+    This PR adds the following transitions:
+    1). RAW -> TESTUNLOCK0 -> PROD -> RMA
+    2). RAW -> TESTUNLOCK0 -> DEV -> RMA
     
-    Signed-off-by: Timothy Trippel <ttrippel@google.com>
+    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
 """
 
