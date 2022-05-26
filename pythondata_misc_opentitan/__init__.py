77b1bcf95d599c34d2a586a44b82869c1d34cecd
LiteX Robot <robot@timvideos.us>
2022-05-26 17:05:54 +0000

    Updating pythondata-misc-opentitan to 0.0.post12354
    Updated data to v0.0-12216-ge1961043f based on e1961043f6308107ec570b5791b2c3606cf17b33 from https://github.com/lowRISC/opentitan.
    > commit e1961043f6308107ec570b5791b2c3606cf17b33
    > Author: Jaedon Kim <jdonjdon@google.com>
    > Date:   Wed May 25 01:19:34 2022 +0000
    >
    >     [adc_ctrl,dv] regression fix fsm_reset test
    >
    >     - trigger aon reset and main reset at the same time during the test
    >
    >     Signed-off-by: Jaedon Kim <jdonjdon@google.com>
    >
    
    Updated using 0.0.post138 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,44 +4,43 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12346"
-version_tuple = (0, 0, 12346)
+version_str = "0.0.post12354"
+version_tuple = (0, 0, 12354)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12346")
+    pversion = V("0.0.post12354")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12210"
-data_version_tuple = (0, 0, 12210)
+data_version_str = "0.0.post12216"
+data_version_tuple = (0, 0, 12216)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12210")
+    pdata_version = V("0.0.post12216")
 except ImportError:
     pass
-data_git_hash = "9b014610cfc4a99a77414314a154883f6f1e24c6"
-data_git_describe = "v0.0-12210-g9b014610c"
+data_git_hash = "e1961043f6308107ec570b5791b2c3606cf17b33"
+data_git_describe = "v0.0-12216-ge1961043f"
 data_git_msg = """\
-commit 9b014610cfc4a99a77414314a154883f6f1e24c6
-Author: Nikola Miladinovic <nikola.miladinovic@ensilica.com>
-Date:   Tue May 24 14:31:32 2022 +0100
+commit e1961043f6308107ec570b5791b2c3606cf17b33
+Author: Jaedon Kim <jdonjdon@google.com>
+Date:   Wed May 25 01:19:34 2022 +0000
 
-    [flash_ctrl] Add flash invalid operation test
+    [adc_ctrl,dv] regression fix fsm_reset test
     
-    Add flash invalid operation test. Check that invalid operation
-    does not affect content of memory. Check all partitions.
+    - trigger aon reset and main reset at the same time during the test
     
-    Signed-off-by: Nikola Miladinovic <nikola.miladinovic@ensilica.com>
+    Signed-off-by: Jaedon Kim <jdonjdon@google.com>
 
 """
 
 # Tool version info
-tool_version_str = "0.0.post136"
-tool_version_tuple = (0, 0, 136)
+tool_version_str = "0.0.post138"
+tool_version_tuple = (0, 0, 138)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post136")
+    ptool_version = V("0.0.post138")
 except ImportError:
     pass
 
