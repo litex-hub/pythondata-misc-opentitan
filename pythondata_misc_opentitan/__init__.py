10478558d74ffdfa5e9161a832df5c0af29a1ffb
LiteX Robot <robot@timvideos.us>
2022-05-26 16:40:20 +0000

    Updating pythondata-misc-opentitan to 0.0.post12346
    Updated data to v0.0-12210-g9b014610c based on 9b014610cfc4a99a77414314a154883f6f1e24c6 from https://github.com/lowRISC/opentitan.
    > commit 9b014610cfc4a99a77414314a154883f6f1e24c6
    > Author: Nikola Miladinovic <nikola.miladinovic@ensilica.com>
    > Date:   Tue May 24 14:31:32 2022 +0100
    >
    >     [flash_ctrl] Add flash invalid operation test
    >
    >     Add flash invalid operation test. Check that invalid operation
    >     does not affect content of memory. Check all partitions.
    >
    >     Signed-off-by: Nikola Miladinovic <nikola.miladinovic@ensilica.com>
    >
    
    Updated using 0.0.post136 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12344"
-version_tuple = (0, 0, 12344)
+version_str = "0.0.post12346"
+version_tuple = (0, 0, 12346)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12344")
+    pversion = V("0.0.post12346")
 except ImportError:
     pass
 
@@ -37,11 +37,11 @@ Date:   Tue May 24 14:31:32 2022 +0100
 """
 
 # Tool version info
-tool_version_str = "0.0.post134"
-tool_version_tuple = (0, 0, 134)
+tool_version_str = "0.0.post136"
+tool_version_tuple = (0, 0, 136)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post134")
+    ptool_version = V("0.0.post136")
 except ImportError:
     pass
 
