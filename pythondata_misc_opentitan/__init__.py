bfec3efd4b9439962f0f2753e6fc9c7380c4748b
LiteX Robot <robot@timvideos.us>
2022-05-27 15:12:51 +0000

    Updating pythondata-misc-opentitan to 0.0.post12378
    Updated data to v0.0-12238-g5f5017437 based on 5f50174370b6ffdca2f9c77a6e66ff40a370c9d6 from https://github.com/lowRISC/opentitan.
    > commit 5f50174370b6ffdca2f9c77a6e66ff40a370c9d6
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Thu May 26 15:40:40 2022 +0100
    >
    >     [otbn,dv] Adjust assertions in otbn_rnd_if to handle ignored values
    >
    >     The changes made in the RTL in 8e97525 and the documentation in
    >     46e134f allow a situation where data comes in from the EDN and is
    >     ignored.
    >
    >     This commit tweaks the FSM diagram and associated assertions
    >     accordingly. I've also made signals for the transition labels to try
    >     to decouple the assertions and RTL signal names a little.
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post140 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,40 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12377"
-version_tuple = (0, 0, 12377)
+version_str = "0.0.post12378"
+version_tuple = (0, 0, 12378)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12377")
+    pversion = V("0.0.post12378")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12237"
-data_version_tuple = (0, 0, 12237)
+data_version_str = "0.0.post12238"
+data_version_tuple = (0, 0, 12238)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12237")
+    pdata_version = V("0.0.post12238")
 except ImportError:
     pass
-data_git_hash = "538c36b83659cd5b9898e3f1b4c94b66a25d4e18"
-data_git_describe = "v0.0-12237-g538c36b83"
+data_git_hash = "5f50174370b6ffdca2f9c77a6e66ff40a370c9d6"
+data_git_describe = "v0.0-12238-g5f5017437"
 data_git_msg = """\
-commit 538c36b83659cd5b9898e3f1b4c94b66a25d4e18
-Author: Prajwala Puttappa <prajwalaputtappa@lowrisc.org>
-Date:   Fri May 20 17:24:24 2022 +0100
+commit 5f50174370b6ffdca2f9c77a6e66ff40a370c9d6
+Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
+Date:   Thu May 26 15:40:40 2022 +0100
 
-    [otbn, dv] Disabling end addr check when zero state error is injected
+    [otbn,dv] Adjust assertions in otbn_rnd_if to handle ignored values
     
-    This commit disables end addr check when zero state error is injected
+    The changes made in the RTL in 8e97525 and the documentation in
+    46e134f allow a situation where data comes in from the EDN and is
+    ignored.
     
-    Signed-off-by: Prajwala Puttappa <prajwalaputtappa@lowrisc.org>
+    This commit tweaks the FSM diagram and associated assertions
+    accordingly. I've also made signals for the transition labels to try
+    to decouple the assertions and RTL signal names a little.
+    
+    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
 """
 
