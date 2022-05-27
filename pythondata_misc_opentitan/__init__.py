0d6e7b674e7eb4afc2c6bca98269ad29766ea512
LiteX Robot <robot@timvideos.us>
2022-05-27 09:12:39 +0000

    Updating pythondata-misc-opentitan to 0.0.post12371
    Updated data to v0.0-12231-g757a768cc based on 757a768cc8d9395c49ec8479f5aed45a574d2c54 from https://github.com/lowRISC/opentitan.
    > commit 757a768cc8d9395c49ec8479f5aed45a574d2c54
    > Author: Greg Chadwick <gac@lowrisc.org>
    > Date:   Wed May 18 14:33:17 2022 +0100
    >
    >     [otbn, rtl] Add fatal error when prefetch isn't correct
    >
    >     By design the prefetch stage either prefetches the correct address or
    >     doesn't prefetch. If a prefetched instruction has a different address to
    >     the one required by the instruction fetch stage a fault has ocurred.
    >
    >     Signed-off-by: Greg Chadwick <gac@lowrisc.org>
    >
    
    Updated using 0.0.post140 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12370"
-version_tuple = (0, 0, 12370)
+version_str = "0.0.post12371"
+version_tuple = (0, 0, 12371)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12370")
+    pversion = V("0.0.post12371")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12230"
-data_version_tuple = (0, 0, 12230)
+data_version_str = "0.0.post12231"
+data_version_tuple = (0, 0, 12231)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12230")
+    pdata_version = V("0.0.post12231")
 except ImportError:
     pass
-data_git_hash = "c938c1833f7477e204f07e665584c87396ac3265"
-data_git_describe = "v0.0-12230-gc938c1833"
+data_git_hash = "757a768cc8d9395c49ec8479f5aed45a574d2c54"
+data_git_describe = "v0.0-12231-g757a768cc"
 data_git_msg = """\
-commit c938c1833f7477e204f07e665584c87396ac3265
-Author: Alphan Ulusoy <alphan@google.com>
-Date:   Thu May 26 17:51:39 2022 -0400
+commit 757a768cc8d9395c49ec8479f5aed45a574d2c54
+Author: Greg Chadwick <gac@lowrisc.org>
+Date:   Wed May 18 14:33:17 2022 +0100
 
-    Remove sw/host/vendor directory
+    [otbn, rtl] Add fatal error when prefetch isn't correct
     
-    Signed-off-by: Alphan Ulusoy <alphan@google.com>
+    By design the prefetch stage either prefetches the correct address or
+    doesn't prefetch. If a prefetched instruction has a different address to
+    the one required by the instruction fetch stage a fault has ocurred.
+    
+    Signed-off-by: Greg Chadwick <gac@lowrisc.org>
 
 """
 
