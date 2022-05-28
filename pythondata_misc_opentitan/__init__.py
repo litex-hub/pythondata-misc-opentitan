1aff9bb26c2605485d20b3a958d40a659ae4f93f
LiteX Robot <robot@timvideos.us>
2022-05-28 00:42:44 +0000

    Updating pythondata-misc-opentitan to 0.0.post12396
    Updated data to v0.0-12256-g2e9cc9f72 based on 2e9cc9f72d6d1d39d6c68e68f2d3ab7d0f7fba39 from https://github.com/lowRISC/opentitan.
    > commit 2e9cc9f72d6d1d39d6c68e68f2d3ab7d0f7fba39
    > Author: Timothy Trippel <ttrippel@google.com>
    > Date:   Fri May 27 12:41:15 2022 -0700
    >
    >     [sw/tests] update non-volatile flash region example
    >
    >     The non-volatile flash region example did not make it clear that any
    >     non-volatile flash data should be initialized to all 1s to achieve the
    >     desired behavior.
    >
    >     Signed-off-by: Timothy Trippel <ttrippel@google.com>
    >
    
    Updated using 0.0.post140 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,30 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12394"
-version_tuple = (0, 0, 12394)
+version_str = "0.0.post12396"
+version_tuple = (0, 0, 12396)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12394")
+    pversion = V("0.0.post12396")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12254"
-data_version_tuple = (0, 0, 12254)
+data_version_str = "0.0.post12256"
+data_version_tuple = (0, 0, 12256)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12254")
+    pdata_version = V("0.0.post12256")
 except ImportError:
     pass
-data_git_hash = "26b2cabc4da62c4b675149910d8db160a4ecad82"
-data_git_describe = "v0.0-12254-g26b2cabc4"
+data_git_hash = "2e9cc9f72d6d1d39d6c68e68f2d3ab7d0f7fba39"
+data_git_describe = "v0.0-12256-g2e9cc9f72"
 data_git_msg = """\
-commit 26b2cabc4da62c4b675149910d8db160a4ecad82
+commit 2e9cc9f72d6d1d39d6c68e68f2d3ab7d0f7fba39
 Author: Timothy Trippel <ttrippel@google.com>
-Date:   Fri May 27 10:44:45 2022 -0700
+Date:   Fri May 27 12:41:15 2022 -0700
 
-    [docs] remove newline in closed-source test hooks docs
+    [sw/tests] update non-volatile flash region example
+    
+    The non-volatile flash region example did not make it clear that any
+    non-volatile flash data should be initialized to all 1s to achieve the
+    desired behavior.
     
     Signed-off-by: Timothy Trippel <ttrippel@google.com>
 
