34bde23db001b39fea73c0928ef20cebd6e4370d
LiteX Robot <robot@timvideos.us>
2022-05-30 11:12:46 +0000

    Updating pythondata-misc-opentitan to 0.0.post12405
    Updated data to v0.0-12265-g510f1832b based on 510f1832bbd6179042e29884c765c82c41cce4b1 from https://github.com/lowRISC/opentitan.
    > commit 510f1832bbd6179042e29884c765c82c41cce4b1
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Sun May 29 23:19:08 2022 +0100
    >
    >     [otbn,dv] Correct bogus assertion in on_otp_cdc_done
    >
    >     I'd forgotten that we also rotate memory keys when locking on a fatal
    >     error.
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post140 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,47 +4,33 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12404"
-version_tuple = (0, 0, 12404)
+version_str = "0.0.post12405"
+version_tuple = (0, 0, 12405)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12404")
+    pversion = V("0.0.post12405")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12264"
-data_version_tuple = (0, 0, 12264)
+data_version_str = "0.0.post12265"
+data_version_tuple = (0, 0, 12265)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12264")
+    pdata_version = V("0.0.post12265")
 except ImportError:
     pass
-data_git_hash = "306c584d0fc0fa1a10e21c6a88753602420ada5b"
-data_git_describe = "v0.0-12264-g306c584d0"
+data_git_hash = "510f1832bbd6179042e29884c765c82c41cce4b1"
+data_git_describe = "v0.0-12265-g510f1832b"
 data_git_msg = """\
-commit 306c584d0fc0fa1a10e21c6a88753602420ada5b
+commit 510f1832bbd6179042e29884c765c82c41cce4b1
 Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Sat May 28 22:24:34 2022 +0100
+Date:   Sun May 29 23:19:08 2022 +0100
 
-    [otbn,rtl] Drop pending RND prefetch at end of operation
+    [otbn,dv] Correct bogus assertion in on_otp_cdc_done
     
-    Consider a situation where we have two operations, A and B, that run
-    back-to-back. Suppose operation A writes to RND_PREFETCH and then
-    finishes before the EDN replies. Now suppose that operation B also
-    writes to RND_PREFETCH. In this case, the RTL uses a signal called
-    rnd_req_queued_q to track the fact that it should send out another RND
-    request once the first (ignored) one comes back. But what if operation
-    B also finishes before the EDN has replied to the first request?
-    
-    Once the result comes in, there was a slight mismatch between how the
-    ISS and RTL handled things (depending on the timing of the *next*
-    operation, the ISS might or might not have sent out another request).
-    But this whole situation is silly! We know we don't care about the
-    result of the second prefetch, so we don't need to do it at all.
-    
-    This patch tweaks the RTL to drop the pending request in this
-    situation and adds a "forget" function in the ISS to do the same.
+    I'd forgotten that we also rotate memory keys when locking on a fatal
+    error.
     
     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
