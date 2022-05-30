5a7ccb281160717b941a9d89ba986daaec59fb7e
LiteX Robot <robot@timvideos.us>
2022-05-30 09:14:34 +0000

    Updating pythondata-misc-opentitan to 0.0.post12404
    Updated data to v0.0-12264-g306c584d0 based on 306c584d0fc0fa1a10e21c6a88753602420ada5b from https://github.com/lowRISC/opentitan.
    > commit 306c584d0fc0fa1a10e21c6a88753602420ada5b
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Sat May 28 22:24:34 2022 +0100
    >
    >     [otbn,rtl] Drop pending RND prefetch at end of operation
    >
    >     Consider a situation where we have two operations, A and B, that run
    >     back-to-back. Suppose operation A writes to RND_PREFETCH and then
    >     finishes before the EDN replies. Now suppose that operation B also
    >     writes to RND_PREFETCH. In this case, the RTL uses a signal called
    >     rnd_req_queued_q to track the fact that it should send out another RND
    >     request once the first (ignored) one comes back. But what if operation
    >     B also finishes before the EDN has replied to the first request?
    >
    >     Once the result comes in, there was a slight mismatch between how the
    >     ISS and RTL handled things (depending on the timing of the *next*
    >     operation, the ISS might or might not have sent out another request).
    >     But this whole situation is silly! We know we don't care about the
    >     result of the second prefetch, so we don't need to do it at all.
    >
    >     This patch tweaks the RTL to drop the pending request in this
    >     situation and adds a "forget" function in the ISS to do the same.
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post140 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,40 +4,49 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12401"
-version_tuple = (0, 0, 12401)
+version_str = "0.0.post12404"
+version_tuple = (0, 0, 12404)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12401")
+    pversion = V("0.0.post12404")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12261"
-data_version_tuple = (0, 0, 12261)
+data_version_str = "0.0.post12264"
+data_version_tuple = (0, 0, 12264)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12261")
+    pdata_version = V("0.0.post12264")
 except ImportError:
     pass
-data_git_hash = "84869a3ca0106b994a80c358d06a2fafc2f5ef18"
-data_git_describe = "v0.0-12261-g84869a3ca"
+data_git_hash = "306c584d0fc0fa1a10e21c6a88753602420ada5b"
+data_git_describe = "v0.0-12264-g306c584d0"
 data_git_msg = """\
-commit 84869a3ca0106b994a80c358d06a2fafc2f5ef18
-Author: Michael Munday <mike.munday@lowrisc.org>
-Date:   Fri May 27 16:35:54 2022 +0100
+commit 306c584d0fc0fa1a10e21c6a88753602420ada5b
+Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
+Date:   Sat May 28 22:24:34 2022 +0100
 
-    [COMMITTERS] Update committer list to add Douglas Reis
+    [otbn,rtl] Drop pending RND prefetch at end of operation
     
-    The Technical Committee has granted committer status to Douglas.
-    Congratulations Douglas, thank you for all your hard work on the
-    project so far.
+    Consider a situation where we have two operations, A and B, that run
+    back-to-back. Suppose operation A writes to RND_PREFETCH and then
+    finishes before the EDN replies. Now suppose that operation B also
+    writes to RND_PREFETCH. In this case, the RTL uses a signal called
+    rnd_req_queued_q to track the fact that it should send out another RND
+    request once the first (ignored) one comes back. But what if operation
+    B also finishes before the EDN has replied to the first request?
     
-    Also, remove Miguel Young de la Sota from the COMMITTERS and
-    CODEOWNERS lists as he is no longer working on the project. Thanks
-    for all your contributions over the years Miguel!
+    Once the result comes in, there was a slight mismatch between how the
+    ISS and RTL handled things (depending on the timing of the *next*
+    operation, the ISS might or might not have sent out another request).
+    But this whole situation is silly! We know we don't care about the
+    result of the second prefetch, so we don't need to do it at all.
     
-    Signed-off-by: Michael Munday <mike.munday@lowrisc.org>
+    This patch tweaks the RTL to drop the pending request in this
+    situation and adds a "forget" function in the ISS to do the same.
+    
+    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
 """
 
