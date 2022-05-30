e75855c32d32c9a013e395d8db5080a0e18e5bd0
LiteX Robot <robot@timvideos.us>
2022-05-30 12:20:55 +0000

    Updating pythondata-misc-opentitan to 0.0.post12406
    Updated data to v0.0-12266-g78e9223cd based on 78e9223cdcbe9921f22c8b95dc5e1ae41c01ccc7 from https://github.com/lowRISC/opentitan.
    > commit 78e9223cdcbe9921f22c8b95dc5e1ae41c01ccc7
    > Author: Rasmus Madsen <rasmus.madsen@wdc.com>
    > Date:   Thu Apr 21 03:32:22 2022 -0700
    >
    >     [aes/dv] implemented fault inject into aes_control_fsm
    >
    >     Signed-off-by: Rasmus Madsen <rasmus.madsen@wdc.com>
    >
    
    Updated using 0.0.post140 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12405"
-version_tuple = (0, 0, 12405)
+version_str = "0.0.post12406"
+version_tuple = (0, 0, 12406)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12405")
+    pversion = V("0.0.post12406")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12265"
-data_version_tuple = (0, 0, 12265)
+data_version_str = "0.0.post12266"
+data_version_tuple = (0, 0, 12266)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12265")
+    pdata_version = V("0.0.post12266")
 except ImportError:
     pass
-data_git_hash = "510f1832bbd6179042e29884c765c82c41cce4b1"
-data_git_describe = "v0.0-12265-g510f1832b"
+data_git_hash = "78e9223cdcbe9921f22c8b95dc5e1ae41c01ccc7"
+data_git_describe = "v0.0-12266-g78e9223cd"
 data_git_msg = """\
-commit 510f1832bbd6179042e29884c765c82c41cce4b1
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Sun May 29 23:19:08 2022 +0100
+commit 78e9223cdcbe9921f22c8b95dc5e1ae41c01ccc7
+Author: Rasmus Madsen <rasmus.madsen@wdc.com>
+Date:   Thu Apr 21 03:32:22 2022 -0700
 
-    [otbn,dv] Correct bogus assertion in on_otp_cdc_done
+    [aes/dv] implemented fault inject into aes_control_fsm
     
-    I'd forgotten that we also rotate memory keys when locking on a fatal
-    error.
-    
-    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    Signed-off-by: Rasmus Madsen <rasmus.madsen@wdc.com>
 
 """
 
