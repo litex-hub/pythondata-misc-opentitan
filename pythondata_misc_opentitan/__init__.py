c5509de92a6d74b57d7743390c0f1550c9cfefef
LiteX Robot <robot@timvideos.us>
2022-05-30 14:15:06 +0000

    Updating pythondata-misc-opentitan to 0.0.post12407
    Updated data to v0.0-12267-g50f8a6ed1 based on 50f8a6ed18ef91ca889a6ccc51c588bff0389b54 from https://github.com/lowRISC/opentitan.
    > commit 50f8a6ed18ef91ca889a6ccc51c588bff0389b54
    > Author: Pirmin Vogel <vogelpi@lowrisc.org>
    > Date:   Tue May 24 11:00:19 2022 +0200
    >
    >     [python-requirements.txt] Switch to chipwhisperer-minimal
    >
    >     So far, we haven't touched the chipwhisperer version but we will soon
    >     have to update in order to re-enable FTDI MPSSE emulation and OpenOCD
    >     debugging on the CW310. See lowRISC/OpenTitan#10625. However, this isn't
    >     straightforward as more recent ChipWhisperer versions have dependencies
    >     that require Python 3.7 or newer (e.g. numpy), whereas OpenTitan
    >     requires Python 3.6 or newer.
    >
    >     ChipWhisperer has now prepared a minimal ChipWhisperer version that only
    >     supports the CW310 but doesn't have these dependencies. As such
    >     chipwhisperer-minimal will allows us to track more recent versions
    >     ChipWhisperer while remaining on Python 3.6 for the moment (default for
    >     Ubuntu 18.04). This commit switches to using chipwhisperer-minimal.
    >
    >     Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
    >
    
    Updated using 0.0.post140 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,45 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12406"
-version_tuple = (0, 0, 12406)
+version_str = "0.0.post12407"
+version_tuple = (0, 0, 12407)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12406")
+    pversion = V("0.0.post12407")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12266"
-data_version_tuple = (0, 0, 12266)
+data_version_str = "0.0.post12267"
+data_version_tuple = (0, 0, 12267)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12266")
+    pdata_version = V("0.0.post12267")
 except ImportError:
     pass
-data_git_hash = "78e9223cdcbe9921f22c8b95dc5e1ae41c01ccc7"
-data_git_describe = "v0.0-12266-g78e9223cd"
+data_git_hash = "50f8a6ed18ef91ca889a6ccc51c588bff0389b54"
+data_git_describe = "v0.0-12267-g50f8a6ed1"
 data_git_msg = """\
-commit 78e9223cdcbe9921f22c8b95dc5e1ae41c01ccc7
-Author: Rasmus Madsen <rasmus.madsen@wdc.com>
-Date:   Thu Apr 21 03:32:22 2022 -0700
+commit 50f8a6ed18ef91ca889a6ccc51c588bff0389b54
+Author: Pirmin Vogel <vogelpi@lowrisc.org>
+Date:   Tue May 24 11:00:19 2022 +0200
 
-    [aes/dv] implemented fault inject into aes_control_fsm
+    [python-requirements.txt] Switch to chipwhisperer-minimal
     
-    Signed-off-by: Rasmus Madsen <rasmus.madsen@wdc.com>
+    So far, we haven't touched the chipwhisperer version but we will soon
+    have to update in order to re-enable FTDI MPSSE emulation and OpenOCD
+    debugging on the CW310. See lowRISC/OpenTitan#10625. However, this isn't
+    straightforward as more recent ChipWhisperer versions have dependencies
+    that require Python 3.7 or newer (e.g. numpy), whereas OpenTitan
+    requires Python 3.6 or newer.
+    
+    ChipWhisperer has now prepared a minimal ChipWhisperer version that only
+    supports the CW310 but doesn't have these dependencies. As such
+    chipwhisperer-minimal will allows us to track more recent versions
+    ChipWhisperer while remaining on Python 3.6 for the moment (default for
+    Ubuntu 18.04). This commit switches to using chipwhisperer-minimal.
+    
+    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
 
 """
 
