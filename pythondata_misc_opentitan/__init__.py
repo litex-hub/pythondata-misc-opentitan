e470d99f92df3b2a76fc925e01ff42f9257c53fe
LiteX Robot <robot@timvideos.us>
2022-06-01 13:45:22 +0000

    Updating pythondata-misc-opentitan to 0.0.post12432
    Updated data to v0.0-12290-g217a0168b based on 217a0168ba118503c166a9587819e3811eeb0c0c from https://github.com/lowRISC/opentitan.
    > commit 217a0168ba118503c166a9587819e3811eeb0c0c
    > Author: Douglas Reis <doreis@lowrisc.org>
    > Date:   Tue May 31 15:18:17 2022 +0100
    >
    >     [dif, rv_core_ibex] Add rnd api
    >
    >     Signed-off-by: Douglas Reis <doreis@lowrisc.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,44 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12431"
-version_tuple = (0, 0, 12431)
+version_str = "0.0.post12432"
+version_tuple = (0, 0, 12432)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12431")
+    pversion = V("0.0.post12432")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12289"
-data_version_tuple = (0, 0, 12289)
+data_version_str = "0.0.post12290"
+data_version_tuple = (0, 0, 12290)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12289")
+    pdata_version = V("0.0.post12290")
 except ImportError:
     pass
-data_git_hash = "e9cfd0f0f3de2b58fe472d5bcb8ad4a0a6e4e88a"
-data_git_describe = "v0.0-12289-ge9cfd0f0f"
+data_git_hash = "217a0168ba118503c166a9587819e3811eeb0c0c"
+data_git_describe = "v0.0-12290-g217a0168b"
 data_git_msg = """\
-commit e9cfd0f0f3de2b58fe472d5bcb8ad4a0a6e4e88a
-Author: Greg Chadwick <gac@lowrisc.org>
-Date:   Thu May 26 10:54:34 2022 +0100
+commit 217a0168ba118503c166a9587819e3811eeb0c0c
+Author: Douglas Reis <doreis@lowrisc.org>
+Date:   Tue May 31 15:18:17 2022 +0100
 
-    [otbn,rtl] Refactor ISPR read muxing
+    [dif, rv_core_ibex] Add rnd api
     
-    ISPR read data is muxed out in two stages in the bignum ALU. The first
-    stage muxes between all of the ISPRs that don't have integrity bits. The
-    result goes through an integrity encoder and is fed into the second
-    stage. The second stage chooses the final read data choosing between the
-    first stage with the calculated intergrity and the ISPRs that have
-    integrity bits.
-    
-    This refactoring makes this structure more explicit and should result in
-    area savings as the previous RTL fed the same signal into multiple
-    inputs of the second stage mux. As there is an optimization barrier in
-    the mux these would not have been optimized down to a single input.
-    
-    Signed-off-by: Greg Chadwick <gac@lowrisc.org>
+    Signed-off-by: Douglas Reis <doreis@lowrisc.org>
 
 """
 
