a0ff595d032c91eb444a3f9f3d492db764f393a8
LiteX Robot <robot@timvideos.us>
2022-06-01 07:15:17 +0000

    Updating pythondata-misc-opentitan to 0.0.post12430
    Updated data to v0.0-12288-g15bc9e352 based on 15bc9e3527d89d793d2ca740403adad262f708ac from https://github.com/lowRISC/opentitan.
    > commit 15bc9e3527d89d793d2ca740403adad262f708ac
    > Author: Michael Schaffner <msf@opentitan.org>
    > Date:   Tue May 31 06:22:00 2022 -0700
    >
    >     [adc_ctrl] Fix a few documentation / comment nits
    >
    >     Signed-off-by: Michael Schaffner <msf@opentitan.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,47 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12429"
-version_tuple = (0, 0, 12429)
+version_str = "0.0.post12430"
+version_tuple = (0, 0, 12430)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12429")
+    pversion = V("0.0.post12430")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12287"
-data_version_tuple = (0, 0, 12287)
+data_version_str = "0.0.post12288"
+data_version_tuple = (0, 0, 12288)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12287")
+    pdata_version = V("0.0.post12288")
 except ImportError:
     pass
-data_git_hash = "33ea5821037a0ee96f4b034c9758819b6d8b1fd8"
-data_git_describe = "v0.0-12287-g33ea58210"
+data_git_hash = "15bc9e3527d89d793d2ca740403adad262f708ac"
+data_git_describe = "v0.0-12288-g15bc9e352"
 data_git_msg = """\
-commit 33ea5821037a0ee96f4b034c9758819b6d8b1fd8
-Author: Eunchan Kim <eunchan@opentitan.org>
-Date:   Tue May 31 12:50:52 2022 -0700
+commit 15bc9e3527d89d793d2ca740403adad262f708ac
+Author: Michael Schaffner <msf@opentitan.org>
+Date:   Tue May 31 06:22:00 2022 -0700
 
-    fix(spi_device)!: Default to Flash Mode
+    [adc_ctrl] Fix a few documentation / comment nits
     
-    As discussed in #12808, SPI_DEVICE default mode is changed to Flash mode
-    in this commit. It removes three register writes in the Boot ROM.
-    
-    As Generic feature still lives in the SPI_DEVICE IP, SW can always
-    change back to Generic mode.
-    
-    Justification:
-    
-        BootROM has been switched to SPI Flash mode to download the ROM
-        Extension images. The usage of SPI_DEVICE is focused on Flash mode
-        and Passthrough mode. Changing between Generic mode and
-        (Flash/Passthrough) mode is not easy due to the clock change. SW
-        shall follow the steps in the doc. This change eliminates the
-        unnecessary steps in most cases.
-    
-    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>
+    Signed-off-by: Michael Schaffner <msf@opentitan.org>
 
 """
 
