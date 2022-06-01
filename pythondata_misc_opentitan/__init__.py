fc2d395353260ed669459b73a6d45e02d647883c
LiteX Robot <robot@timvideos.us>
2022-06-01 17:22:40 +0000

    Updating pythondata-misc-opentitan to 0.0.post12444
    Updated data to v0.0-12302-g4f6eadba9 based on 4f6eadba989c3712df79e23880d462192d48a24f from https://github.com/lowRISC/opentitan.
    > commit 4f6eadba989c3712df79e23880d462192d48a24f
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Fri May 27 17:56:24 2022 +0100
    >
    >     [otbn,rtl] Squash prefetch mismatch error when controller stops
    >
    >     Without this squashing behaviour, there's a problem if the controller
    >     stops on an instruction that doesn't look like a jump/branch/etc. This
    >     stalls the instruction stream (so insn_fetch_req_addr_i stops
    >     changing). In this case, the invalid instruction didn't look like a
    >     jump or similar, so the prefetcher has already been incremented by 4.
    >     We then generate an error (insn_addr_err_o) when we look at the
    >     results.
    >
    >     Hopefully, any situation like this would be caught by the existing
    >     testbench (by looking at ERR_BITS). But we definitely catch situations
    >     where the initial error is only supposed to send out a recoverable
    >     alert. In this case, the testbench notices that an unexpected fatal
    >     alert came out.
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,46 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12441"
-version_tuple = (0, 0, 12441)
+version_str = "0.0.post12444"
+version_tuple = (0, 0, 12444)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12441")
+    pversion = V("0.0.post12444")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12299"
-data_version_tuple = (0, 0, 12299)
+data_version_str = "0.0.post12302"
+data_version_tuple = (0, 0, 12302)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12299")
+    pdata_version = V("0.0.post12302")
 except ImportError:
     pass
-data_git_hash = "672f9167e3b7fc2593eb33a552038b1bceec5aac"
-data_git_describe = "v0.0-12299-g672f9167e"
+data_git_hash = "4f6eadba989c3712df79e23880d462192d48a24f"
+data_git_describe = "v0.0-12302-g4f6eadba9"
 data_git_msg = """\
-commit 672f9167e3b7fc2593eb33a552038b1bceec5aac
-Author: Nikola Miladinovic <nikola.miladinovic@ensilica.com>
-Date:   Mon May 30 12:15:47 2022 +0100
+commit 4f6eadba989c3712df79e23880d462192d48a24f
+Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
+Date:   Fri May 27 17:56:24 2022 +0100
 
-    [flash_ctrl] Add oob_err exclusions
+    [otbn,rtl] Squash prefetch mismatch error when controller stops
     
-    Add lines 45 and 49 of flash_ctrl_erase.sv file to exclusions due
-    to oob error.
+    Without this squashing behaviour, there's a problem if the controller
+    stops on an instruction that doesn't look like a jump/branch/etc. This
+    stalls the instruction stream (so insn_fetch_req_addr_i stops
+    changing). In this case, the invalid instruction didn't look like a
+    jump or similar, so the prefetcher has already been incremented by 4.
+    We then generate an error (insn_addr_err_o) when we look at the
+    results.
     
-    Signed-off-by: Nikola Miladinovic <nikola.miladinovic@ensilica.com>
+    Hopefully, any situation like this would be caught by the existing
+    testbench (by looking at ERR_BITS). But we definitely catch situations
+    where the initial error is only supposed to send out a recoverable
+    alert. In this case, the testbench notices that an unexpected fatal
+    alert came out.
+    
+    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
 
 """
 
