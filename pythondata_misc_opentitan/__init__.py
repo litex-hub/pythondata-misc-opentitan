f3598612a04be8d4f9715e26a231e4ecc4b6a205
LiteX Robot <robot@timvideos.us>
2022-06-01 15:15:16 +0000

    Updating pythondata-misc-opentitan to 0.0.post12440
    Updated data to v0.0-12298-g8fce9b07d based on 8fce9b07d969c484eac2af6e73587935c41ae3e9 from https://github.com/lowRISC/opentitan.
    > commit 8fce9b07d969c484eac2af6e73587935c41ae3e9
    > Author: Drew Macrae <drewmacrae@google.com>
    > Date:   Tue May 31 22:07:17 2022 -0400
    >
    >     [bazel] rename hw/ip/otp_ctrl/data:rma_image_verilator to img_rma
    >
    >     Signed-off-by: Drew Macrae <drewmacrae@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12434"
-version_tuple = (0, 0, 12434)
+version_str = "0.0.post12440"
+version_tuple = (0, 0, 12440)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12434")
+    pversion = V("0.0.post12440")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12292"
-data_version_tuple = (0, 0, 12292)
+data_version_str = "0.0.post12298"
+data_version_tuple = (0, 0, 12298)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12292")
+    pdata_version = V("0.0.post12298")
 except ImportError:
     pass
-data_git_hash = "cf8ea0c9502802b974c65c9f7a87e4d694ca2c74"
-data_git_describe = "v0.0-12292-gcf8ea0c95"
+data_git_hash = "8fce9b07d969c484eac2af6e73587935c41ae3e9"
+data_git_describe = "v0.0-12298-g8fce9b07d"
 data_git_msg = """\
-commit cf8ea0c9502802b974c65c9f7a87e4d694ca2c74
-Author: Alphan Ulusoy <alphan@google.com>
-Date:   Tue May 31 17:27:41 2022 -0400
+commit 8fce9b07d969c484eac2af6e73587935c41ae3e9
+Author: Drew Macrae <drewmacrae@google.com>
+Date:   Tue May 31 22:07:17 2022 -0400
 
-    [sw/silicon_creator] Minor updates to shutdown_unittest.cc
+    [bazel] rename hw/ip/otp_ctrl/data:rma_image_verilator to img_rma
     
-    The only remaining mock in shutdown_unittest.cc mocks the internal
-    functions of the shutdown module and thus it's appropriate to keep its
-    definition there.
-    
-    Signed-off-by: Alphan Ulusoy <alphan@google.com>
+    Signed-off-by: Drew Macrae <drewmacrae@google.com>
 
 """
 
