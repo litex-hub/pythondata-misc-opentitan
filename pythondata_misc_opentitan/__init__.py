d77f8b7291ede5e562e54600cc10bc7e597a3cb5
LiteX Robot <robot@timvideos.us>
2022-06-02 00:48:57 +0000

    Updating pythondata-misc-opentitan to 0.0.post12457
    Updated data to v0.0-12315-g1b4293480 based on 1b4293480568f9bb6ac97836fff952f1e6c63fba from https://github.com/lowRISC/opentitan.
    > commit 1b4293480568f9bb6ac97836fff952f1e6c63fba
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Wed Jun 1 13:44:54 2022 -0700
    >
    >     [dv/pattgen] Remove memory testplan entry
    >
    >     This PR removes testplan entries regarding memory. Pattgen does not
    >     contain any memory so this test is not needed.
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12454"
-version_tuple = (0, 0, 12454)
+version_str = "0.0.post12457"
+version_tuple = (0, 0, 12457)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12454")
+    pversion = V("0.0.post12457")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12312"
-data_version_tuple = (0, 0, 12312)
+data_version_str = "0.0.post12315"
+data_version_tuple = (0, 0, 12315)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12312")
+    pdata_version = V("0.0.post12315")
 except ImportError:
     pass
-data_git_hash = "780e05e7b64764ae36a5421eae5c418c5fe84565"
-data_git_describe = "v0.0-12312-g780e05e7b"
+data_git_hash = "1b4293480568f9bb6ac97836fff952f1e6c63fba"
+data_git_describe = "v0.0-12315-g1b4293480"
 data_git_msg = """\
-commit 780e05e7b64764ae36a5421eae5c418c5fe84565
-Author: Alphan Ulusoy <alphan@google.com>
-Date:   Wed Jun 1 15:06:02 2022 -0400
+commit 1b4293480568f9bb6ac97836fff952f1e6c63fba
+Author: Cindy Chen <chencindy@opentitan.org>
+Date:   Wed Jun 1 13:44:54 2022 -0700
 
-    [sw/silicon_creator] Remove fake_deps.c
+    [dv/pattgen] Remove memory testplan entry
     
-    Signed-off-by: Alphan Ulusoy <alphan@google.com>
+    This PR removes testplan entries regarding memory. Pattgen does not
+    contain any memory so this test is not needed.
+    
+    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
 """
 
