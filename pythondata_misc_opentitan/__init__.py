55a3ea7f6a1fda872ed3a0e8ce17b4db36f54bce
LiteX Robot <robot@timvideos.us>
2022-06-02 16:15:10 +0000

    Updating pythondata-misc-opentitan to 0.0.post12466
    Updated data to v0.0-12324-g5c1a7171c based on 5c1a7171c43ee59120e5b3896f2ca95282056bf3 from https://github.com/lowRISC/opentitan.
    > commit 5c1a7171c43ee59120e5b3896f2ca95282056bf3
    > Author: Eunchan Kim <eunchan@opentitan.org>
    > Date:   Wed Jun 1 14:33:24 2022 -0700
    >
    >     chore(cdc): Waive JTAG EN strap
    >
    >     JTAG en strap can be waived in CDC review
    >
    >     Signed-off-by: Eunchan Kim <eunchan@opentitan.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12462"
-version_tuple = (0, 0, 12462)
+version_str = "0.0.post12466"
+version_tuple = (0, 0, 12466)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12462")
+    pversion = V("0.0.post12466")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12320"
-data_version_tuple = (0, 0, 12320)
+data_version_str = "0.0.post12324"
+data_version_tuple = (0, 0, 12324)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12320")
+    pdata_version = V("0.0.post12324")
 except ImportError:
     pass
-data_git_hash = "f0e8fd418b5743a2906fde6a07f591bb16da01a7"
-data_git_describe = "v0.0-12320-gf0e8fd418"
+data_git_hash = "5c1a7171c43ee59120e5b3896f2ca95282056bf3"
+data_git_describe = "v0.0-12324-g5c1a7171c"
 data_git_msg = """\
-commit f0e8fd418b5743a2906fde6a07f591bb16da01a7
-Author: Alphan Ulusoy <alphan@google.com>
-Date:   Wed Jun 1 15:19:41 2022 -0400
+commit 5c1a7171c43ee59120e5b3896f2ca95282056bf3
+Author: Eunchan Kim <eunchan@opentitan.org>
+Date:   Wed Jun 1 14:33:24 2022 -0700
 
-    [sw/silicon_creator] Use sram_ctrl DIFs in mask_rom_epmp_test.c
+    chore(cdc): Waive JTAG EN strap
     
-    Signed-off-by: Alphan Ulusoy <alphan@google.com>
+    JTAG en strap can be waived in CDC review
+    
+    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>
 
 """
 
