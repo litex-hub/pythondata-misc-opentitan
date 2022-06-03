11aabc72d26cde2d51f6a99e0c0812a428c8c371
LiteX Robot <robot@timvideos.us>
2022-06-03 12:17:56 +0000

    Updating pythondata-misc-opentitan to 0.0.post12486
    Updated data to v0.0-12344-gb83afe85e based on b83afe85e411b68cd10da9f265994eede6597b78 from https://github.com/lowRISC/opentitan.
    > commit b83afe85e411b68cd10da9f265994eede6597b78
    > Author: Michael Schaffner <msf@opentitan.org>
    > Date:   Thu Jun 2 12:47:43 2022 +0200
    >
    >     [otbn] Minor lint fix
    >
    >     Signed-off-by: Michael Schaffner <msf@opentitan.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,38 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12485"
-version_tuple = (0, 0, 12485)
+version_str = "0.0.post12486"
+version_tuple = (0, 0, 12486)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12485")
+    pversion = V("0.0.post12486")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12343"
-data_version_tuple = (0, 0, 12343)
+data_version_str = "0.0.post12344"
+data_version_tuple = (0, 0, 12344)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12343")
+    pdata_version = V("0.0.post12344")
 except ImportError:
     pass
-data_git_hash = "c941279301af2774f8d64120e31466a55669ed25"
-data_git_describe = "v0.0-12343-gc94127930"
+data_git_hash = "b83afe85e411b68cd10da9f265994eede6597b78"
+data_git_describe = "v0.0-12344-gb83afe85e"
 data_git_msg = """\
-commit c941279301af2774f8d64120e31466a55669ed25
-Author: Timothy Chen <timothytim@google.com>
-Date:   Wed Jun 1 14:19:15 2022 -0700
+commit b83afe85e411b68cd10da9f265994eede6597b78
+Author: Michael Schaffner <msf@opentitan.org>
+Date:   Thu Jun 2 12:47:43 2022 +0200
 
-    [pwrmgr] Fix escalate request CDC
+    [otbn] Minor lint fix
     
-    - addresses #12981
-    - If the escalate request is not permanent, it is possible
-      for the pwrmgr to miss this request since it goes through
-      an always-on clock synchronization.
-    - capture and hold escalate request until the system resets.
-    
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Michael Schaffner <msf@opentitan.org>
 
 """
 
