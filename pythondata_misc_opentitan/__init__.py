16e9026c24a2ad3212a6956a43062284a29e32a8
LiteX Robot <robot@timvideos.us>
2022-06-06 18:13:47 +0000

    Updating pythondata-misc-opentitan to 0.0.post12518
    Updated data to v0.0-12376-g6d211c008 based on 6d211c008ce67c0ec0849d6c8724dc0146cf490b from https://github.com/lowRISC/opentitan.
    > commit 6d211c008ce67c0ec0849d6c8724dc0146cf490b
    > Author: Weicai Yang <weicai@google.com>
    > Date:   Fri Jun 3 17:02:02 2022 -0700
    >
    >     [dv] Add common test for reg write enable check
    >
    >     Address #12113
    >     - Inject a fault in the prim_onehot_check to trigger reg integrity error
    >     - Check status reg and fatal alert
    >
    >     Only add it for sram_ctrl. Will create a separate PR to enable this for
    >     all other blocks
    >
    >     Signed-off-by: Weicai Yang <weicai@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,38 +4,39 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12517"
-version_tuple = (0, 0, 12517)
+version_str = "0.0.post12518"
+version_tuple = (0, 0, 12518)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12517")
+    pversion = V("0.0.post12518")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12375"
-data_version_tuple = (0, 0, 12375)
+data_version_str = "0.0.post12376"
+data_version_tuple = (0, 0, 12376)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12375")
+    pdata_version = V("0.0.post12376")
 except ImportError:
     pass
-data_git_hash = "ce4d65282bb2122c0ef25768e3b9fea55a9837e5"
-data_git_describe = "v0.0-12375-gce4d65282"
+data_git_hash = "6d211c008ce67c0ec0849d6c8724dc0146cf490b"
+data_git_describe = "v0.0-12376-g6d211c008"
 data_git_msg = """\
-commit ce4d65282bb2122c0ef25768e3b9fea55a9837e5
-Author: Miles Dai <milesdai@google.com>
-Date:   Fri Jun 3 17:36:52 2022 -0400
+commit 6d211c008ce67c0ec0849d6c8724dc0146cf490b
+Author: Weicai Yang <weicai@google.com>
+Date:   Fri Jun 3 17:02:02 2022 -0700
 
-    [ci] Reduce the number of CW310 tests being run in CI
+    [dv] Add common test for reg write enable check
     
-    The communication with the CW310 board in CI fails intermittently. This
-    is happening more frequency after more tests were added during the Bazel
-    transition. This commit is a temporary measure to reduce the frequency
-    of these failures by running fewer tests (the same set that was
-    previously being run by systemtest).
+    Address #12113
+    - Inject a fault in the prim_onehot_check to trigger reg integrity error
+    - Check status reg and fatal alert
     
-    Signed-off-by: Miles Dai <milesdai@google.com>
+    Only add it for sram_ctrl. Will create a separate PR to enable this for
+    all other blocks
+    
+    Signed-off-by: Weicai Yang <weicai@google.com>
 
 """
 
