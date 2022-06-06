25c491f90e03bc3778397ccfba5d1d52e5903865
LiteX Robot <robot@timvideos.us>
2022-06-06 22:13:02 +0000

    Updating pythondata-misc-opentitan to 0.0.post12539
    Updated data to v0.0-12397-g93c8f594a based on 93c8f594a707b916dfac7f11bb83add3475295f5 from https://github.com/lowRISC/opentitan.
    > commit 93c8f594a707b916dfac7f11bb83add3475295f5
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Mon Jun 6 11:36:11 2022 -0700
    >
    >     [dv/alert_handler] Add common sec_cm test for alert_handler
    >
    >     Alert_handler does not have alerts, so sec_cm violation will directly
    >     fires escalations or causes a local alert.
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12524"
-version_tuple = (0, 0, 12524)
+version_str = "0.0.post12539"
+version_tuple = (0, 0, 12539)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12524")
+    pversion = V("0.0.post12539")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12382"
-data_version_tuple = (0, 0, 12382)
+data_version_str = "0.0.post12397"
+data_version_tuple = (0, 0, 12397)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12382")
+    pdata_version = V("0.0.post12397")
 except ImportError:
     pass
-data_git_hash = "1bd62be4b9fb64e6e7f314ebe081da0202d0bd6e"
-data_git_describe = "v0.0-12382-g1bd62be4b"
+data_git_hash = "93c8f594a707b916dfac7f11bb83add3475295f5"
+data_git_describe = "v0.0-12397-g93c8f594a"
 data_git_msg = """\
-commit 1bd62be4b9fb64e6e7f314ebe081da0202d0bd6e
-Author: Greg Chadwick <gac@lowrisc.org>
-Date:   Wed Jun 1 10:32:14 2022 +0100
+commit 93c8f594a707b916dfac7f11bb83add3475295f5
+Author: Cindy Chen <chencindy@opentitan.org>
+Date:   Mon Jun 6 11:36:11 2022 -0700
 
-    [otbn, rtl] Add prim_buf and prim_flop
+    [dv/alert_handler] Add common sec_cm test for alert_handler
     
-    These add needed optimization barriers around security hardening
-    features.
+    Alert_handler does not have alerts, so sec_cm violation will directly
+    fires escalations or causes a local alert.
     
-    Signed-off-by: Greg Chadwick <gac@lowrisc.org>
+    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
 """
 
