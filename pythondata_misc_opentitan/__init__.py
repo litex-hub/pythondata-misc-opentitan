48ad3adfee09f6e18061737d14851bbe4d84fdaa
LiteX Robot <robot@timvideos.us>
2022-06-08 00:42:04 +0000

    Updating pythondata-misc-opentitan to 0.0.post12564
    Updated data to v0.0-12422-g1995394f8 based on 1995394f8a8e9447e4f4a9b17753f3b1cd22ece8 from https://github.com/lowRISC/opentitan.
    > commit 1995394f8a8e9447e4f4a9b17753f3b1cd22ece8
    > Author: Miguel Osorio <miguelosorio@google.com>
    > Date:   Fri Jun 3 18:09:39 2022 -0700
    >
    >     [cryptolib/aes] Add NIST test vector.
    >
    >     Switch test case to use a NIST test vector to make the test case easier to
    >     audit. The test was previously not passing on Verilator/FPGA targets.
    >
    >     Signed-off-by: Miguel Osorio <miguelosorio@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12562"
-version_tuple = (0, 0, 12562)
+version_str = "0.0.post12564"
+version_tuple = (0, 0, 12564)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12562")
+    pversion = V("0.0.post12564")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12420"
-data_version_tuple = (0, 0, 12420)
+data_version_str = "0.0.post12422"
+data_version_tuple = (0, 0, 12422)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12420")
+    pdata_version = V("0.0.post12422")
 except ImportError:
     pass
-data_git_hash = "2f48edc13fec8fd3787988100028f2ee107e766d"
-data_git_describe = "v0.0-12420-g2f48edc13"
+data_git_hash = "1995394f8a8e9447e4f4a9b17753f3b1cd22ece8"
+data_git_describe = "v0.0-12422-g1995394f8"
 data_git_msg = """\
-commit 2f48edc13fec8fd3787988100028f2ee107e766d
-Author: Eunchan Kim <eunchan@opentitan.org>
-Date:   Tue Jun 7 11:50:00 2022 -0700
+commit 1995394f8a8e9447e4f4a9b17753f3b1cd22ece8
+Author: Miguel Osorio <miguelosorio@google.com>
+Date:   Fri Jun 3 18:09:39 2022 -0700
 
-    chore(ci): Skip FPGA , SW test for CDC changes
+    [cryptolib/aes] Add NIST test vector.
     
-    This commit let CI skip FPGA tests, SW tests when only CDC related
-    changes are made.
+    Switch test case to use a NIST test vector to make the test case easier to
+    audit. The test was previously not passing on Verilator/FPGA targets.
     
-    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>
+    Signed-off-by: Miguel Osorio <miguelosorio@google.com>
 
 """
 
