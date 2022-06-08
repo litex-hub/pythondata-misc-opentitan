55b69581a9a47e4348dbb6c0935f9561de86645c
LiteX Robot <robot@timvideos.us>
2022-06-08 18:13:43 +0000

    Updating pythondata-misc-opentitan to 0.0.post12571
    Updated data to v0.0-12429-gd957c8654 based on d957c8654e10f029263a1bf10776eb0c7c98f536 from https://github.com/lowRISC/opentitan.
    > commit d957c8654e10f029263a1bf10776eb0c7c98f536
    > Author: Weicai Yang <weicai@google.com>
    > Date:   Tue Jun 7 15:00:30 2022 -0700
    >
    >     [dv] Enable reg_wr_check test for all blocks
    >
    >     Signed-off-by: Weicai Yang <weicai@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,40 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12568"
-version_tuple = (0, 0, 12568)
+version_str = "0.0.post12571"
+version_tuple = (0, 0, 12571)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12568")
+    pversion = V("0.0.post12571")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12426"
-data_version_tuple = (0, 0, 12426)
+data_version_str = "0.0.post12429"
+data_version_tuple = (0, 0, 12429)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12426")
+    pdata_version = V("0.0.post12429")
 except ImportError:
     pass
-data_git_hash = "fdd3b8c5bae60cd9c998f79140632639c74e1dc1"
-data_git_describe = "v0.0-12426-gfdd3b8c5b"
+data_git_hash = "d957c8654e10f029263a1bf10776eb0c7c98f536"
+data_git_describe = "v0.0-12429-gd957c8654"
 data_git_msg = """\
-commit fdd3b8c5bae60cd9c998f79140632639c74e1dc1
-Author: Eunchan Kim <eunchan@opentitan.org>
-Date:   Tue Jun 7 15:18:48 2022 -0700
+commit d957c8654e10f029263a1bf10776eb0c7c98f536
+Author: Weicai Yang <weicai@google.com>
+Date:   Tue Jun 7 15:00:30 2022 -0700
 
-    fix(otp_ctrl): Keymgr Key valid glitch
+    [dv] Enable reg_wr_check test for all blocks
     
-    This PR fixes the CDC issue on otp_keymgr_key_t.valid signal. Also
-    there's chance the key_share0, key_share1 can be changed during the
-    operation but the chance is limited to the escalation stage, in which
-    case the CDC is not meaningful anymore.
-    
-    So rather than latching the key_shares, this commit adds assertion to
-    check the stability of the key_shares during the normal operation.
-    
-    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>
+    Signed-off-by: Weicai Yang <weicai@google.com>
 
 """
 
