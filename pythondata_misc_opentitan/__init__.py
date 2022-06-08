07fd58ae44ad55622bd061ad5398a1369affb50b
LiteX Robot <robot@timvideos.us>
2022-06-08 21:12:05 +0000

    Updating pythondata-misc-opentitan to 0.0.post12573
    Updated data to v0.0-12431-g2f640af93 based on 2f640af93a68580265ede307c6129e4e153a8116 from https://github.com/lowRISC/opentitan.
    > commit 2f640af93a68580265ede307c6129e4e153a8116
    > Author: Chris Frantz <cfrantz@google.com>
    > Date:   Wed Jun 8 08:20:41 2022 -0700
    >
    >     [cleanup] Make `bazelisk.sh` operate in the workspace
    >
    >     The `bazelisk.sh` script should chdir to its containing subdirectory
    >     so that it always operates in the project's workspace.  This will allow
    >     other projects (e.g. tock) to invoke bazelisk to build binaries or
    >     execute tools.
    >
    >     Signed-off-by: Chris Frantz <cfrantz@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,33 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12572"
-version_tuple = (0, 0, 12572)
+version_str = "0.0.post12573"
+version_tuple = (0, 0, 12573)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12572")
+    pversion = V("0.0.post12573")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12430"
-data_version_tuple = (0, 0, 12430)
+data_version_str = "0.0.post12431"
+data_version_tuple = (0, 0, 12431)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12430")
+    pdata_version = V("0.0.post12431")
 except ImportError:
     pass
-data_git_hash = "13babd7d1d9ebea34e97b15277ffb1be794ab376"
-data_git_describe = "v0.0-12430-g13babd7d1"
+data_git_hash = "2f640af93a68580265ede307c6129e4e153a8116"
+data_git_describe = "v0.0-12431-g2f640af93"
 data_git_msg = """\
-commit 13babd7d1d9ebea34e97b15277ffb1be794ab376
+commit 2f640af93a68580265ede307c6129e4e153a8116
 Author: Chris Frantz <cfrantz@google.com>
-Date:   Wed Jun 8 10:37:11 2022 -0700
+Date:   Wed Jun 8 08:20:41 2022 -0700
 
-    [cleanup] Write a newline into the `latest.txt` file
+    [cleanup] Make `bazelisk.sh` operate in the workspace
     
-    1. Write a newline into the `latest.txt` file.  This allows a user to
-       `cat` this file without it running into their shell prompt configuration.
+    The `bazelisk.sh` script should chdir to its containing subdirectory
+    so that it always operates in the project's workspace.  This will allow
+    other projects (e.g. tock) to invoke bazelisk to build binaries or
+    execute tools.
     
     Signed-off-by: Chris Frantz <cfrantz@google.com>
 
