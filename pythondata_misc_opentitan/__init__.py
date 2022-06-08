d1b6c68e46b2872c6805587ee83288fdd6f6031a
LiteX Robot <robot@timvideos.us>
2022-06-08 17:16:53 +0000

    Updating pythondata-misc-opentitan to 0.0.post12568
    Updated data to v0.0-12426-gfdd3b8c5b based on fdd3b8c5bae60cd9c998f79140632639c74e1dc1 from https://github.com/lowRISC/opentitan.
    > commit fdd3b8c5bae60cd9c998f79140632639c74e1dc1
    > Author: Eunchan Kim <eunchan@opentitan.org>
    > Date:   Tue Jun 7 15:18:48 2022 -0700
    >
    >     fix(otp_ctrl): Keymgr Key valid glitch
    >
    >     This PR fixes the CDC issue on otp_keymgr_key_t.valid signal. Also
    >     there's chance the key_share0, key_share1 can be changed during the
    >     operation but the chance is limited to the escalation stage, in which
    >     case the CDC is not meaningful anymore.
    >
    >     So rather than latching the key_shares, this commit adds assertion to
    >     check the stability of the key_shares during the normal operation.
    >
    >     Signed-off-by: Eunchan Kim <eunchan@opentitan.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,40 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12567"
-version_tuple = (0, 0, 12567)
+version_str = "0.0.post12568"
+version_tuple = (0, 0, 12568)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12567")
+    pversion = V("0.0.post12568")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12425"
-data_version_tuple = (0, 0, 12425)
+data_version_str = "0.0.post12426"
+data_version_tuple = (0, 0, 12426)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12425")
+    pdata_version = V("0.0.post12426")
 except ImportError:
     pass
-data_git_hash = "e7f754ffc8d592421981e7321aa254901df883f0"
-data_git_describe = "v0.0-12425-ge7f754ffc"
+data_git_hash = "fdd3b8c5bae60cd9c998f79140632639c74e1dc1"
+data_git_describe = "v0.0-12426-gfdd3b8c5b"
 data_git_msg = """\
-commit e7f754ffc8d592421981e7321aa254901df883f0
-Author: Rasmus Madsen <rasmus.madsen@wdc.com>
-Date:   Tue Jun 7 04:22:38 2022 -0700
+commit fdd3b8c5bae60cd9c998f79140632639c74e1dc1
+Author: Eunchan Kim <eunchan@opentitan.org>
+Date:   Tue Jun 7 15:18:48 2022 -0700
 
-    Refixed 12236 to a more rubust solution
+    fix(otp_ctrl): Keymgr Key valid glitch
     
-    Signed-off-by: Rasmus Madsen <rasmus.madsen@wdc.com>
+    This PR fixes the CDC issue on otp_keymgr_key_t.valid signal. Also
+    there's chance the key_share0, key_share1 can be changed during the
+    operation but the chance is limited to the escalation stage, in which
+    case the CDC is not meaningful anymore.
+    
+    So rather than latching the key_shares, this commit adds assertion to
+    check the stability of the key_shares during the normal operation.
+    
+    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>
 
 """
 
