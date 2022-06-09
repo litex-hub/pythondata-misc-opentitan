404bcfe205965bd37d54f864422156243feab37b
LiteX Robot <robot@timvideos.us>
2022-06-09 23:13:34 +0000

    Updating pythondata-misc-opentitan to 0.0.post12622
    Updated data to v0.0-12480-g24d368c57 based on 24d368c57633e0e9ee2e0f3f400988e7a3ab4629 from https://github.com/lowRISC/opentitan.
    > commit 24d368c57633e0e9ee2e0f3f400988e7a3ab4629
    > Author: Dave Williams <dave.williams@ensilica.com>
    > Date:   Mon May 30 18:20:58 2022 +0100
    >
    >     [sw,tests] Update SRAM scramble tests to handle ECC errors
    >
    >     For tests:
    >     chip_sw_sram_ctrl_main_scrambled_access
    >     chip_sw_sram_ctrl_main_scrambled_access_jitter_en
    >     chip_sw_sram_ctrl_ret_scrambled_access
    >
    >     The tests have been updated to handle ECC errors in the readback data. Previously,
    >     this was not needed as ECC checking was not enabled. It was sufficient to write
    >     data, scramble, read back and check for mismatches against the original data. ECC errors
    >     now trigger an internal interrupt in the CPU. An exception handler has been written for
    >     this. In addition to the read back check, a count of the number of exceptions handled
    >     is kept and checked against the expected number.
    >
    >     Signed-off-by: Dave Williams <dave.williams@ensilica.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,44 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12621"
-version_tuple = (0, 0, 12621)
+version_str = "0.0.post12622"
+version_tuple = (0, 0, 12622)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12621")
+    pversion = V("0.0.post12622")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12479"
-data_version_tuple = (0, 0, 12479)
+data_version_str = "0.0.post12480"
+data_version_tuple = (0, 0, 12480)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12479")
+    pdata_version = V("0.0.post12480")
 except ImportError:
     pass
-data_git_hash = "662e9dfcfe263d675ae1139831a56938b0240d83"
-data_git_describe = "v0.0-12479-g662e9dfcf"
+data_git_hash = "24d368c57633e0e9ee2e0f3f400988e7a3ab4629"
+data_git_describe = "v0.0-12480-g24d368c57"
 data_git_msg = """\
-commit 662e9dfcfe263d675ae1139831a56938b0240d83
-Author: Weicai Yang <weicai@google.com>
-Date:   Wed Jun 8 23:22:56 2022 -0700
+commit 24d368c57633e0e9ee2e0f3f400988e7a3ab4629
+Author: Dave Williams <dave.williams@ensilica.com>
+Date:   Mon May 30 18:20:58 2022 +0100
 
-    [dv] Update xcelium coverage config file
+    [sw,tests] Update SRAM scramble tests to handle ECC errors
     
-    Excluded statement coverage for tlul_assert, only enabled assertion
-    coverage.
-    Signed-off-by: Weicai Yang <weicai@google.com>
+    For tests:
+    chip_sw_sram_ctrl_main_scrambled_access
+    chip_sw_sram_ctrl_main_scrambled_access_jitter_en
+    chip_sw_sram_ctrl_ret_scrambled_access
+    
+    The tests have been updated to handle ECC errors in the readback data. Previously,
+    this was not needed as ECC checking was not enabled. It was sufficient to write
+    data, scramble, read back and check for mismatches against the original data. ECC errors
+    now trigger an internal interrupt in the CPU. An exception handler has been written for
+    this. In addition to the read back check, a count of the number of exceptions handled
+    is kept and checked against the expected number.
+    
+    Signed-off-by: Dave Williams <dave.williams@ensilica.com>
 
 """
 
