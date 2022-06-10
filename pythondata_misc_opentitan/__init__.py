8521b2b38c80a40945ede4a6a7f16cd56a0ced5f
LiteX Robot <robot@timvideos.us>
2022-06-10 17:18:34 +0000

    Updating pythondata-misc-opentitan to 0.0.post12635
    Updated data to v0.0-12493-g6084cfa93 based on 6084cfa936b25826bab0db21a60e925a33fd6b0e from https://github.com/lowRISC/opentitan.
    > commit 6084cfa936b25826bab0db21a60e925a33fd6b0e
    > Author: Canberk Topal <ctopal@lowrisc.org>
    > Date:   Thu Jun 9 17:52:23 2022 +0100
    >
    >     [otbn,dv] Illegal Bus Access fixup
    >
    >     This commit includes changes in the do_mem_acc task.
    >     Main idea is to poll TL bus requests for DMEM and IMEM and send relevant
    >     error code to the model right when it happens.
    >
    >     Signed-off-by: Canberk Topal <ctopal@lowrisc.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12633"
-version_tuple = (0, 0, 12633)
+version_str = "0.0.post12635"
+version_tuple = (0, 0, 12635)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12633")
+    pversion = V("0.0.post12635")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12491"
-data_version_tuple = (0, 0, 12491)
+data_version_str = "0.0.post12493"
+data_version_tuple = (0, 0, 12493)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12491")
+    pdata_version = V("0.0.post12493")
 except ImportError:
     pass
-data_git_hash = "895bb8a265dda4fb6b38e202118bc5d5c100bccb"
-data_git_describe = "v0.0-12491-g895bb8a26"
+data_git_hash = "6084cfa936b25826bab0db21a60e925a33fd6b0e"
+data_git_describe = "v0.0-12493-g6084cfa93"
 data_git_msg = """\
-commit 895bb8a265dda4fb6b38e202118bc5d5c100bccb
-Author: Andreas Kurth <adk@lowrisc.org>
-Date:   Thu Jun 9 22:29:42 2022 +0200
+commit 6084cfa936b25826bab0db21a60e925a33fd6b0e
+Author: Canberk Topal <ctopal@lowrisc.org>
+Date:   Thu Jun 9 17:52:23 2022 +0100
 
-    [doc] Remove trailing whitespace in Secure HW Design Guidelines
+    [otbn,dv] Illegal Bus Access fixup
     
-    This is required to pass CI.
+    This commit includes changes in the do_mem_acc task.
+    Main idea is to poll TL bus requests for DMEM and IMEM and send relevant
+    error code to the model right when it happens.
     
-    Signed-off-by: Andreas Kurth <adk@lowrisc.org>
+    Signed-off-by: Canberk Topal <ctopal@lowrisc.org>
 
 """
 
