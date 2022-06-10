602030057132f3189647dca2d56d294ca21e1350
LiteX Robot <robot@timvideos.us>
2022-06-10 15:15:07 +0000

    Updating pythondata-misc-opentitan to 0.0.post12630
    Updated data to v0.0-12488-gbd4040591 based on bd4040591221e5b850e946cb048e4c2c88d284bb from https://github.com/lowRISC/opentitan.
    > commit bd4040591221e5b850e946cb048e4c2c88d284bb
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Thu Jun 9 17:58:00 2022 -0700
    >
    >     [top] update testplan
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,38 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12628"
-version_tuple = (0, 0, 12628)
+version_str = "0.0.post12630"
+version_tuple = (0, 0, 12630)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12628")
+    pversion = V("0.0.post12630")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12486"
-data_version_tuple = (0, 0, 12486)
+data_version_str = "0.0.post12488"
+data_version_tuple = (0, 0, 12488)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12486")
+    pdata_version = V("0.0.post12488")
 except ImportError:
     pass
-data_git_hash = "ec88d27257f8eae66ef51783310a39633d373687"
-data_git_describe = "v0.0-12486-gec88d2725"
+data_git_hash = "bd4040591221e5b850e946cb048e4c2c88d284bb"
+data_git_describe = "v0.0-12488-gbd4040591"
 data_git_msg = """\
-commit ec88d27257f8eae66ef51783310a39633d373687
-Author: Luís Marques <luismarques@lowrisc.org>
-Date:   Tue Apr 19 12:00:37 2022 +0200
+commit bd4040591221e5b850e946cb048e4c2c88d284bb
+Author: Timothy Chen <timothytim@google.com>
+Date:   Thu Jun 9 17:58:00 2022 -0700
 
-    [sw] Fix function prototypes
+    [top] update testplan
     
-    Several functions without parameters were declared as `foo()` instead of
-    `foo(void)`. The former is considered a function without a prototype, not
-    a function without parameters. Newer versions of Clang implement a
-    stricter check of this condition, and the new warnings were breaking the
-    build. Fix this by adding the missing `void` in several places.
-    
-    Signed-off-by: Luís Marques <luismarques@lowrisc.org>
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
