802a38260f9614a6600bd83a4d59c5c737603170
LiteX Robot <robot@timvideos.us>
2022-06-13 17:20:08 +0000

    Updating pythondata-misc-opentitan to 0.0.post12662
    Updated data to v0.0-12520-g022b181f4 based on 022b181f4b5b4e0d771b5c6baef29df76d3f7063 from https://github.com/lowRISC/opentitan.
    > commit 022b181f4b5b4e0d771b5c6baef29df76d3f7063
    > Author: Alphan Ulusoy <alphan@google.com>
    > Date:   Mon Jun 13 10:49:20 2022 -0400
    >
    >     [util] Edit chip_info format to look better with LOG_INFO()
    >
    >     Signed-off-by: Alphan Ulusoy <alphan@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12657"
-version_tuple = (0, 0, 12657)
+version_str = "0.0.post12662"
+version_tuple = (0, 0, 12662)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12657")
+    pversion = V("0.0.post12662")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12515"
-data_version_tuple = (0, 0, 12515)
+data_version_str = "0.0.post12520"
+data_version_tuple = (0, 0, 12520)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12515")
+    pdata_version = V("0.0.post12520")
 except ImportError:
     pass
-data_git_hash = "5c33c1ab7ecd1ecf824549e3d49c9665d93cc970"
-data_git_describe = "v0.0-12515-g5c33c1ab7"
+data_git_hash = "022b181f4b5b4e0d771b5c6baef29df76d3f7063"
+data_git_describe = "v0.0-12520-g022b181f4"
 data_git_msg = """\
-commit 5c33c1ab7ecd1ecf824549e3d49c9665d93cc970
-Author: Eitan Shapira <eitanshapira89@gmail.com>
-Date:   Thu Jun 9 14:20:48 2022 +0300
+commit 022b181f4b5b4e0d771b5c6baef29df76d3f7063
+Author: Alphan Ulusoy <alphan@google.com>
+Date:   Mon Jun 13 10:49:20 2022 -0400
 
-    [flash_ctrl/tl/dv] Add timeout argument to tl_access methods
+    [util] Edit chip_info format to look better with LOG_INFO()
     
-    Signed-off-by: Eitan Shapira <eitanshapira89@gmail.com>
+    Signed-off-by: Alphan Ulusoy <alphan@google.com>
 
 """
 
