b6d4d224673b66efc8c3988197c48e26c8ae81dc
LiteX Robot <robot@timvideos.us>
2022-06-14 11:11:22 +0000

    Updating pythondata-misc-opentitan to 0.0.post12671
    Updated data to v0.0-12529-g5003ad289 based on 5003ad289b3cbb3fee7299db0a325d8749f163b0 from https://github.com/lowRISC/opentitan.
    > commit 5003ad289b3cbb3fee7299db0a325d8749f163b0
    > Author: Jade Philipoom <jadep@google.com>
    > Date:   Fri Jun 10 15:20:13 2022 +0100
    >
    >     [sigverify] Check OTBN instruction count.
    >
    >     As an extra protection against fault injection attacks, check that the
    >     instruction count from the OTBN modexp routine falls within a feasible
    >     range given the control-flow paths available in the program.
    >
    >     Signed-off-by: Jade Philipoom <jadep@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12670"
-version_tuple = (0, 0, 12670)
+version_str = "0.0.post12671"
+version_tuple = (0, 0, 12671)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12670")
+    pversion = V("0.0.post12671")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12528"
-data_version_tuple = (0, 0, 12528)
+data_version_str = "0.0.post12529"
+data_version_tuple = (0, 0, 12529)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12528")
+    pdata_version = V("0.0.post12529")
 except ImportError:
     pass
-data_git_hash = "71d4a19ac81f137a304a00a7c550d2ddc6c60fa9"
-data_git_describe = "v0.0-12528-g71d4a19ac"
+data_git_hash = "5003ad289b3cbb3fee7299db0a325d8749f163b0"
+data_git_describe = "v0.0-12529-g5003ad289"
 data_git_msg = """\
-commit 71d4a19ac81f137a304a00a7c550d2ddc6c60fa9
-Author: Pirmin Vogel <vogelpi@lowrisc.org>
-Date:   Mon Jun 13 16:58:31 2022 +0200
+commit 5003ad289b3cbb3fee7299db0a325d8749f163b0
+Author: Jade Philipoom <jadep@google.com>
+Date:   Fri Jun 10 15:20:13 2022 +0100
 
-    [fpga] Make `set_clock_sense` constraint more flexible using a wildcard
+    [sigverify] Check OTBN instruction count.
     
-    It seems that the name of the target LUT cell may change as the
-    design evolves. This results in Vivado not being able to apply the
-    constraint and triggering a critical warning instead.
+    As an extra protection against fault injection attacks, check that the
+    instruction count from the OTBN modexp routine falls within a feasible
+    range given the control-flow paths available in the program.
     
-    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
+    Signed-off-by: Jade Philipoom <jadep@google.com>
 
 """
 
