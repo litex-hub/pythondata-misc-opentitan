7210ce4655b4cbf987fccb2beec984a8ff17e81b
LiteX Robot <robot@timvideos.us>
2022-06-14 15:12:40 +0000

    Updating pythondata-misc-opentitan to 0.0.post12676
    Updated data to v0.0-12534-gedbd61c66 based on edbd61c6633f87650846d3f07053d24dbf535f90 from https://github.com/lowRISC/opentitan.
    > commit edbd61c6633f87650846d3f07053d24dbf535f90
    > Author: Michael Schaffner <msf@opentitan.org>
    > Date:   Mon Jun 13 14:33:17 2022 +0200
    >
    >     [otp_ctrl] Remove TODO from RTL
    >
    >     Signed-off-by: Michael Schaffner <msf@opentitan.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12674"
-version_tuple = (0, 0, 12674)
+version_str = "0.0.post12676"
+version_tuple = (0, 0, 12676)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12674")
+    pversion = V("0.0.post12676")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12532"
-data_version_tuple = (0, 0, 12532)
+data_version_str = "0.0.post12534"
+data_version_tuple = (0, 0, 12534)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12532")
+    pdata_version = V("0.0.post12534")
 except ImportError:
     pass
-data_git_hash = "eba99e7b938c28e375a4b195e39103f3b9b4b45d"
-data_git_describe = "v0.0-12532-geba99e7b9"
+data_git_hash = "edbd61c6633f87650846d3f07053d24dbf535f90"
+data_git_describe = "v0.0-12534-gedbd61c66"
 data_git_msg = """\
-commit eba99e7b938c28e375a4b195e39103f3b9b4b45d
-Author: Pirmin Vogel <vogelpi@lowrisc.org>
-Date:   Mon Jun 13 17:07:10 2022 +0200
+commit edbd61c6633f87650846d3f07053d24dbf535f90
+Author: Michael Schaffner <msf@opentitan.org>
+Date:   Mon Jun 13 14:33:17 2022 +0200
 
-    [clkmgr] Allow transactional clock groups with just 1 clock
+    [otp_ctrl] Remove TODO from RTL
     
-    This allows removing HMAC on English Breakfast to free some more FPGA
-    resources.
-    
-    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
+    Signed-off-by: Michael Schaffner <msf@opentitan.org>
 
 """
 
