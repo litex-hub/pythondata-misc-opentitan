b0d9537c4710fb392020cba09295d558799a11c6
LiteX Robot <robot@timvideos.us>
2022-06-17 15:16:36 +0000

    Updating pythondata-misc-opentitan to 0.0.post12730
    Updated data to v0.0-12588-g7de3ae517 based on 7de3ae517a990d6e1714779c5cb38e1b0dc35388 from https://github.com/lowRISC/opentitan.
    > commit 7de3ae517a990d6e1714779c5cb38e1b0dc35388
    > Author: Hugo McNally <hugo.mcnally@gmail.com>
    > Date:   Fri Jun 17 12:46:28 2022 +0100
    >
    >     [dif, hmac] add unit test dif_hmac_get_message_length
    >
    >     Signed-off-by: Hugo McNally <hugo.mcnally@gmail.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12729"
-version_tuple = (0, 0, 12729)
+version_str = "0.0.post12730"
+version_tuple = (0, 0, 12730)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12729")
+    pversion = V("0.0.post12730")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12587"
-data_version_tuple = (0, 0, 12587)
+data_version_str = "0.0.post12588"
+data_version_tuple = (0, 0, 12588)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12587")
+    pdata_version = V("0.0.post12588")
 except ImportError:
     pass
-data_git_hash = "87fc75187ad2a01b2e972ff7eb125cafadeaedde"
-data_git_describe = "v0.0-12587-g87fc75187"
+data_git_hash = "7de3ae517a990d6e1714779c5cb38e1b0dc35388"
+data_git_describe = "v0.0-12588-g7de3ae517"
 data_git_msg = """\
-commit 87fc75187ad2a01b2e972ff7eb125cafadeaedde
-Author: Greg Chadwick <gac@lowrisc.org>
-Date:   Thu Jun 16 17:32:55 2022 +0100
+commit 7de3ae517a990d6e1714779c5cb38e1b0dc35388
+Author: Hugo McNally <hugo.mcnally@gmail.com>
+Date:   Fri Jun 17 12:46:28 2022 +0100
 
-    [otbn, rtl] Clarify multiple base RF WEs in predecoder
+    [dif, hmac] add unit test dif_hmac_get_message_length
     
-    Signed-off-by: Greg Chadwick <gac@lowrisc.org>
+    Signed-off-by: Hugo McNally <hugo.mcnally@gmail.com>
 
 """
 
