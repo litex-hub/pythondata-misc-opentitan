87c39230af7e6ba1f13202d355dbeb263731de21
LiteX Robot <robot@timvideos.us>
2022-06-21 19:10:17 +0000

    Updating pythondata-misc-opentitan to 0.0.post12760
    Updated data to v0.0-12618-g7c8a371fad based on 7c8a371fadb1ec856842406c11d49892138b1518 from https://github.com/lowRISC/opentitan.
    > commit 7c8a371fadb1ec856842406c11d49892138b1518
    > Author: Weicai Yang <weicai@google.com>
    > Date:   Thu Jun 16 22:47:05 2022 -0700
    >
    >     [sram, dv] Update intg_test_err
    >
    >     Aligned with design that detecting fault injection gates all mem access.
    >     The access after FI won't be blocked but it causes a fatal alert
    >
    >     Signed-off-by: Weicai Yang <weicai@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12759"
-version_tuple = (0, 0, 12759)
+version_str = "0.0.post12760"
+version_tuple = (0, 0, 12760)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12759")
+    pversion = V("0.0.post12760")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12617"
-data_version_tuple = (0, 0, 12617)
+data_version_str = "0.0.post12618"
+data_version_tuple = (0, 0, 12618)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12617")
+    pdata_version = V("0.0.post12618")
 except ImportError:
     pass
-data_git_hash = "7022ad86cdd049cf0b2f6d202ef48d2b40db43b5"
-data_git_describe = "v0.0-12617-g7022ad86cd"
+data_git_hash = "7c8a371fadb1ec856842406c11d49892138b1518"
+data_git_describe = "v0.0-12618-g7c8a371fad"
 data_git_msg = """\
-commit 7022ad86cdd049cf0b2f6d202ef48d2b40db43b5
-Author: Joshua Park <jeoong@google.com>
-Date:   Thu Jun 9 15:08:14 2022 -0700
+commit 7c8a371fadb1ec856842406c11d49892138b1518
+Author: Weicai Yang <weicai@google.com>
+Date:   Thu Jun 16 22:47:05 2022 -0700
 
-    [PRIM] new clock mux to prevent a glitch
+    [sram, dv] Update intg_test_err
     
-    Signed-off-by: Joshua Park <jeoong@google.com>
+    Aligned with design that detecting fault injection gates all mem access.
+    The access after FI won't be blocked but it causes a fatal alert
+    
+    Signed-off-by: Weicai Yang <weicai@google.com>
 
 """
 
