d888bf49bbc602337b65b7c90aa92f626a947950
LiteX Robot <robot@timvideos.us>
2022-06-21 17:28:04 +0000

    Updating pythondata-misc-opentitan to 0.0.post12758
    Updated data to v0.0-12616-g6cb41fe6d2 based on 6cb41fe6d2d4cdcc941ca54c2f5e478e6a111869 from https://github.com/lowRISC/opentitan.
    > commit 6cb41fe6d2d4cdcc941ca54c2f5e478e6a111869
    > Author: Guillermo Maturana <maturana@google.com>
    > Date:   Sat Jun 18 05:40:30 2022 -0700
    >
    >     [dv,chip] Format time in microseconds
    >
    >     Format time in microseconds losing no precision. The added "." makes it
    >     easier to  determine the order of magnitude without counting digits, as
    >     is needed if it was formatted as ps or ns.
    >
    >     For example, time would be represented as "  333.333333 us" instead of
    >     "   333333333 ps".
    >
    >     Signed-off-by: Guillermo Maturana <maturana@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,51 +4,39 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12755"
-version_tuple = (0, 0, 12755)
+version_str = "0.0.post12758"
+version_tuple = (0, 0, 12758)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12755")
+    pversion = V("0.0.post12758")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12613"
-data_version_tuple = (0, 0, 12613)
+data_version_str = "0.0.post12616"
+data_version_tuple = (0, 0, 12616)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12613")
+    pdata_version = V("0.0.post12616")
 except ImportError:
     pass
-data_git_hash = "58d38c160e6de7fabd5cfdfe7ebd253611d497cd"
-data_git_describe = "v0.0-12613-g58d38c160e"
+data_git_hash = "6cb41fe6d2d4cdcc941ca54c2f5e478e6a111869"
+data_git_describe = "v0.0-12616-g6cb41fe6d2"
 data_git_msg = """\
-commit 58d38c160e6de7fabd5cfdfe7ebd253611d497cd
-Author: Andreas Kurth <adk@lowrisc.org>
-Date:   Thu Jun 16 07:50:33 2022 +0200
+commit 6cb41fe6d2d4cdcc941ca54c2f5e478e6a111869
+Author: Guillermo Maturana <maturana@google.com>
+Date:   Sat Jun 18 05:40:30 2022 -0700
 
-    [otbn,sw] Use app symbols for parameter addresses
+    [dv,chip] Format time in microseconds
     
-    `otbn_smoketest` runs the `barrett384` OTBN application.  Before and
-    after OTBN execution, data is copied to and from OTBN data memory,
-    respectively.  Prior to this commit, `otbn_smoketest` had hard-coded
-    offsets for the input and output data.  Instead of hard-coded offsets,
-    the OTBN DIF can extract the address of a symbol in data memory from an
-    OTBN object file.  This commit replaces the hard-coded offsets with
-    symbol-derived addresses.
+    Format time in microseconds losing no precision. The added "." makes it
+    easier to  determine the order of magnitude without counting digits, as
+    is needed if it was formatted as ps or ns.
     
-    The instruction count of `barrett384` increases by 10:  Before running
-    the actual algorithm, OTBN now loads four operands from symbol-defined
-    addresses instead of a hard-coded offset.  This code uses one `la` per
-    operand, which is a pseudo-instruction implemented as two real
-    instructions.  After running the algorithm, OTBN now stores one operand
-    using the same method.  That sums up to 8 additional instructions for
-    loading and 2 additional instructions for storing.  For repeated
-    invocations of `barrett384`, the code could be optimized so that the
-    additional instructions don't reduce the overall performance.
+    For example, time would be represented as "  333.333333 us" instead of
+    "   333333333 ps".
     
-    Co-authored-by: Jade Philipoom <jadep@google.com>
-    Signed-off-by: Andreas Kurth <adk@lowrisc.org>
+    Signed-off-by: Guillermo Maturana <maturana@google.com>
 
 """
 
