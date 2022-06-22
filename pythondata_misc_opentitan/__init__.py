b5d128a129d61bcc50c831aaef79e84ce4451616
LiteX Robot <robot@timvideos.us>
2022-06-22 06:16:15 +0000

    Updating pythondata-misc-opentitan to 0.0.post12781
    Updated data to v0.0-12639-g1013e5fa44 based on 1013e5fa4481e4b4fad1eef1202e2d7006925c57 from https://github.com/lowRISC/opentitan.
    > commit 1013e5fa4481e4b4fad1eef1202e2d7006925c57
    > Author: Jade Philipoom <jadep@google.com>
    > Date:   Mon Apr 4 18:37:17 2022 +0100
    >
    >     [sw,crypto] Ed25519 point addition implementation.
    >
    >     Implementation and a quick OTBN-simulator test for Ed25519 point
    >     addition using extended twisted Edwards coordinates (following RFC
    >     8032).
    >
    >     Signed-off-by: Jade Philipoom <jadep@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12780"
-version_tuple = (0, 0, 12780)
+version_str = "0.0.post12781"
+version_tuple = (0, 0, 12781)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12780")
+    pversion = V("0.0.post12781")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12638"
-data_version_tuple = (0, 0, 12638)
+data_version_str = "0.0.post12639"
+data_version_tuple = (0, 0, 12639)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12638")
+    pdata_version = V("0.0.post12639")
 except ImportError:
     pass
-data_git_hash = "87062e69f17ddc7a4f641796e2cc09d36e13176c"
-data_git_describe = "v0.0-12638-g87062e69f1"
+data_git_hash = "1013e5fa4481e4b4fad1eef1202e2d7006925c57"
+data_git_describe = "v0.0-12639-g1013e5fa44"
 data_git_msg = """\
-commit 87062e69f17ddc7a4f641796e2cc09d36e13176c
-Author: Dharanendra Kumar <dharanendra.kumar@ensilica.com>
-Date:   Tue Jun 21 14:30:34 2022 +0100
+commit 1013e5fa4481e4b4fad1eef1202e2d7006925c57
+Author: Jade Philipoom <jadep@google.com>
+Date:   Mon Apr 4 18:37:17 2022 +0100
 
-    [PWM/DV] Updated Checker For issue #13200
+    [sw,crypto] Ed25519 point addition implementation.
     
-      Updated pwm_scoreboard.sv to accomodate the changes in RTL
-      For the issue #13200
+    Implementation and a quick OTBN-simulator test for Ed25519 point
+    addition using extended twisted Edwards coordinates (following RFC
+    8032).
     
-    Signed-off-by: Dharanendra Kumar <dharanendra.kumar@ensilica.com>
+    Signed-off-by: Jade Philipoom <jadep@google.com>
 
 """
 
