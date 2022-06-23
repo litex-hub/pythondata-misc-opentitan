301fe7c1e72bfeca2b9f1ac7dfcc7ab30f323bb9
LiteX Robot <robot@timvideos.us>
2022-06-23 10:13:17 +0000

    Updating pythondata-misc-opentitan to 0.0.post12798
    Updated data to v0.0-12656-g0799e40007 based on 0799e400076f71fdcdab19bbb0da27240c8e5b83 from https://github.com/lowRISC/opentitan.
    > commit 0799e400076f71fdcdab19bbb0da27240c8e5b83
    > Author: Alphan Ulusoy <alphan@google.com>
    > Date:   Wed Jun 22 15:30:16 2022 -0400
    >
    >     [sw/silicon_creator] Move gp init earlier to eliminate several jalr instructions
    >
    >     This improves our resistance to FI since `jal` instructions use an
    >     immediate while `jalr` instructions use an additional register which can
    >     also be attacked.
    >
    >     This commit also removes the line that resets `gp` (`x3`) to 0 since
    >     `gp` is initialized earlier now.
    >
    >     Signed-off-by: Alphan Ulusoy <alphan@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,39 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12797"
-version_tuple = (0, 0, 12797)
+version_str = "0.0.post12798"
+version_tuple = (0, 0, 12798)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12797")
+    pversion = V("0.0.post12798")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12655"
-data_version_tuple = (0, 0, 12655)
+data_version_str = "0.0.post12656"
+data_version_tuple = (0, 0, 12656)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12655")
+    pdata_version = V("0.0.post12656")
 except ImportError:
     pass
-data_git_hash = "85d5126183fae9084c9626d890d76b7859d9d89e"
-data_git_describe = "v0.0-12655-g85d5126183"
+data_git_hash = "0799e400076f71fdcdab19bbb0da27240c8e5b83"
+data_git_describe = "v0.0-12656-g0799e40007"
 data_git_msg = """\
-commit 85d5126183fae9084c9626d890d76b7859d9d89e
-Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
-Date:   Fri Mar 11 12:19:58 2022 +0000
+commit 0799e400076f71fdcdab19bbb0da27240c8e5b83
+Author: Alphan Ulusoy <alphan@google.com>
+Date:   Wed Jun 22 15:30:16 2022 -0400
 
-    [otbn] Remove SecWipeEn configuration machinery
+    [sw/silicon_creator] Move gp init earlier to eliminate several jalr instructions
     
-    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    This improves our resistance to FI since `jal` instructions use an
+    immediate while `jalr` instructions use an additional register which can
+    also be attacked.
+    
+    This commit also removes the line that resets `gp` (`x3`) to 0 since
+    `gp` is initialized earlier now.
+    
+    Signed-off-by: Alphan Ulusoy <alphan@google.com>
 
 """
 
