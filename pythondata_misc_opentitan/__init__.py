a853283f4ee536aa52bc0ca78dced55f3a6c5539
LiteX Robot <robot@timvideos.us>
2022-06-24 13:38:02 +0000

    Updating pythondata-misc-opentitan to 0.0.post12832
    Updated data to v0.0-12690-g2bd39248e4 based on 2bd39248e4d9716740417886199c38a6138b01c5 from https://github.com/lowRISC/opentitan.
    > commit 2bd39248e4d9716740417886199c38a6138b01c5
    > Author: Pirmin Vogel <vogelpi@lowrisc.org>
    > Date:   Fri Jun 24 14:32:38 2022 +0200
    >
    >     [otbn/doc] Clarify the expected behavior around RND_REP/FIPS_CHK_FAIL
    >
    >     These are both recoverable errors that are only triggered when OTBN
    >     uses the random numbers during program execution by reading RND
    >     register and not when the entropy is received from EDN.
    >
    >     This resolves lowRISC/OpenTitan#13325.
    >
    >     Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,38 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12830"
-version_tuple = (0, 0, 12830)
+version_str = "0.0.post12832"
+version_tuple = (0, 0, 12832)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12830")
+    pversion = V("0.0.post12832")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12688"
-data_version_tuple = (0, 0, 12688)
+data_version_str = "0.0.post12690"
+data_version_tuple = (0, 0, 12690)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12688")
+    pdata_version = V("0.0.post12690")
 except ImportError:
     pass
-data_git_hash = "1b111f4e5060f3eb308f59761c85daebb373c01f"
-data_git_describe = "v0.0-12688-g1b111f4e50"
+data_git_hash = "2bd39248e4d9716740417886199c38a6138b01c5"
+data_git_describe = "v0.0-12690-g2bd39248e4"
 data_git_msg = """\
-commit 1b111f4e5060f3eb308f59761c85daebb373c01f
-Author: Canberk Topal <ctopal@lowrisc.org>
-Date:   Thu Jun 23 18:44:08 2022 +0100
+commit 2bd39248e4d9716740417886199c38a6138b01c5
+Author: Pirmin Vogel <vogelpi@lowrisc.org>
+Date:   Fri Jun 24 14:32:38 2022 +0200
 
-    [otbn,rtl] Connect insn addr mismatch to escalate
+    [otbn/doc] Clarify the expected behavior around RND_REP/FIPS_CHK_FAIL
     
-    Fixes #13323
+    These are both recoverable errors that are only triggered when OTBN
+    uses the random numbers during program execution by reading RND
+    register and not when the entropy is received from EDN.
     
-    Signed-off-by: Canberk Topal <ctopal@lowrisc.org>
+    This resolves lowRISC/OpenTitan#13325.
+    
+    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
 
 """
 
