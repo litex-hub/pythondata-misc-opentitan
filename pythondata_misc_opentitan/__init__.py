ec278059d05716680f50762608397d01cdeee807
LiteX Robot <robot@timvideos.us>
2022-06-24 18:17:26 +0000

    Updating pythondata-misc-opentitan to 0.0.post12838
    Updated data to v0.0-12696-gc1328144a0 based on c1328144a07ccd2120eb7b3159a636c4b16f27bd from https://github.com/lowRISC/opentitan.
    > commit c1328144a07ccd2120eb7b3159a636c4b16f27bd
    > Author: Dave Williams <dave.williams@ensilica.com>
    > Date:   Wed Jun 15 10:57:33 2022 +0100
    >
    >     [sw,tests] Verify sysrst_ctrl inputs can be read using CSRs
    >
    >     For test:
    >     chip_sw_sysrst_ctrl_inputs
    >
    >     Checks that known values driven at the chip inputs by the testbench
    >     can be read using the sysrst_ctrl CSRs in software. The values read
    >     are verified against the expected values.
    >
    >     Signed-off-by: Dave Williams <dave.williams@ensilica.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,39 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12835"
-version_tuple = (0, 0, 12835)
+version_str = "0.0.post12838"
+version_tuple = (0, 0, 12838)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12835")
+    pversion = V("0.0.post12838")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12693"
-data_version_tuple = (0, 0, 12693)
+data_version_str = "0.0.post12696"
+data_version_tuple = (0, 0, 12696)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12693")
+    pdata_version = V("0.0.post12696")
 except ImportError:
     pass
-data_git_hash = "aac02b4e727be002c40c4b7c07f04e3cd1ee43a8"
-data_git_describe = "v0.0-12693-gaac02b4e72"
+data_git_hash = "c1328144a07ccd2120eb7b3159a636c4b16f27bd"
+data_git_describe = "v0.0-12696-gc1328144a0"
 data_git_msg = """\
-commit aac02b4e727be002c40c4b7c07f04e3cd1ee43a8
-Author: Timothy Chen <timothytim@google.com>
-Date:   Wed Jun 22 23:27:31 2022 -0700
+commit c1328144a07ccd2120eb7b3159a636c4b16f27bd
+Author: Dave Williams <dave.williams@ensilica.com>
+Date:   Wed Jun 15 10:57:33 2022 +0100
 
-    [top] Correct xbar reset connections
+    [sw,tests] Verify sysrst_ctrl inputs can be read using CSRs
     
-    - fixes #13354
-    - isolate the xbar reset from the individual end-point resets
-    - scripting updates required to allow for different resets
+    For test:
+    chip_sw_sysrst_ctrl_inputs
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Checks that known values driven at the chip inputs by the testbench
+    can be read using the sysrst_ctrl CSRs in software. The values read
+    are verified against the expected values.
+    
+    Signed-off-by: Dave Williams <dave.williams@ensilica.com>
 
 """
 
