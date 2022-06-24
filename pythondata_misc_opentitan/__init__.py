914ccaa27628013417a64f3e464667c1f0cbf836
LiteX Robot <robot@timvideos.us>
2022-06-24 22:11:59 +0000

    Updating pythondata-misc-opentitan to 0.0.post12842
    Updated data to v0.0-12700-gd96a35a211 based on d96a35a211a718c031e679774c769c6056283b7a from https://github.com/lowRISC/opentitan.
    > commit d96a35a211a718c031e679774c769c6056283b7a
    > Author: Dan McArdle <dmcardle@google.com>
    > Date:   Fri Jun 24 11:41:07 2022 -0400
    >
    >     [util] Use correct working dir in generate_compilation_db.py
    >
    >     Specifically, this PR updates the script to emit bazel-opentitan/ as the
    >     working directory for each compile command.
    >
    >     I used `bazel build -s` to print out subcommands when generating a test
    >     target, and noticed that it was changing directory before each of the
    >     calls to gcc, whereas the generated compile_commands.json just points to
    >     the repo's root directory. Helpfully, Bazel already makes a symlink to
    >     the correct working directory: bazel-opentitan/.
    >
    >     Signed-off-by: Dan McArdle <dmcardle@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,41 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12839"
-version_tuple = (0, 0, 12839)
+version_str = "0.0.post12842"
+version_tuple = (0, 0, 12842)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12839")
+    pversion = V("0.0.post12842")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12697"
-data_version_tuple = (0, 0, 12697)
+data_version_str = "0.0.post12700"
+data_version_tuple = (0, 0, 12700)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12697")
+    pdata_version = V("0.0.post12700")
 except ImportError:
     pass
-data_git_hash = "4a9e9fbb6c91e25ddfe5612b740ccadbb53e61ba"
-data_git_describe = "v0.0-12697-g4a9e9fbb6c"
+data_git_hash = "d96a35a211a718c031e679774c769c6056283b7a"
+data_git_describe = "v0.0-12700-gd96a35a211"
 data_git_msg = """\
-commit 4a9e9fbb6c91e25ddfe5612b740ccadbb53e61ba
-Author: Cindy Chen <chencindy@opentitan.org>
-Date:   Fri Jun 24 11:38:32 2022 -0700
+commit d96a35a211a718c031e679774c769c6056283b7a
+Author: Dan McArdle <dmcardle@google.com>
+Date:   Fri Jun 24 11:41:07 2022 -0400
 
-    [formal/conn] Update chip level connectivity csvs
+    [util] Use correct working dir in generate_compilation_db.py
     
-    This PR updates two mismatches in connectivity test:
-    1). Fix an ibex memory path
-    2). Fix usb clock connection
+    Specifically, this PR updates the script to emit bazel-opentitan/ as the
+    working directory for each compile command.
     
-    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
+    I used `bazel build -s` to print out subcommands when generating a test
+    target, and noticed that it was changing directory before each of the
+    calls to gcc, whereas the generated compile_commands.json just points to
+    the repo's root directory. Helpfully, Bazel already makes a symlink to
+    the correct working directory: bazel-opentitan/.
+    
+    Signed-off-by: Dan McArdle <dmcardle@google.com>
 
 """
 
