d592986561122e74c0999385895dd7897cf1ce90
LiteX Robot <robot@timvideos.us>
2022-06-24 00:44:10 +0000

    Updating pythondata-misc-opentitan to 0.0.post12826
    Updated data to v0.0-12684-g3a97dbfaad based on 3a97dbfaad9847ca68614f46f1369298745c7a76 from https://github.com/lowRISC/opentitan.
    > commit 3a97dbfaad9847ca68614f46f1369298745c7a76
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Thu Jun 23 15:07:43 2022 -0700
    >
    >     [dv] Add build rule for sensor_ctrl_status
    >
    >     - also move to sim_dv directory since this test does not work
    >       for fpga / verilator natively
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,38 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12825"
-version_tuple = (0, 0, 12825)
+version_str = "0.0.post12826"
+version_tuple = (0, 0, 12826)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12825")
+    pversion = V("0.0.post12826")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12683"
-data_version_tuple = (0, 0, 12683)
+data_version_str = "0.0.post12684"
+data_version_tuple = (0, 0, 12684)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12683")
+    pdata_version = V("0.0.post12684")
 except ImportError:
     pass
-data_git_hash = "4bbb3434d86d75a4464c2a5e5e710ae8162dc9f6"
-data_git_describe = "v0.0-12683-g4bbb3434d8"
+data_git_hash = "3a97dbfaad9847ca68614f46f1369298745c7a76"
+data_git_describe = "v0.0-12684-g3a97dbfaad"
 data_git_msg = """\
-commit 4bbb3434d86d75a4464c2a5e5e710ae8162dc9f6
-Author: Alphan Ulusoy <alphan@google.com>
-Date:   Thu Jun 23 15:58:13 2022 -0400
+commit 3a97dbfaad9847ca68614f46f1369298745c7a76
+Author: Timothy Chen <timothytim@google.com>
+Date:   Thu Jun 23 15:07:43 2022 -0700
 
-    [rv_compliance] Update riscv compliance tests to 1.0
+    [dv] Add build rule for sensor_ctrl_status
     
-    This change updates the riscv compliances tests to 1.0 to fix the
-    access violation bugs in I-SB-01 and I-SH-01.
+    - also move to sim_dv directory since this test does not work
+      for fpga / verilator natively
     
-    See https://github.com/riscv-non-isa/riscv-arch-test/pull/104 for more
-    details.
-    
-    Signed-off-by: Alphan Ulusoy <alphan@google.com>
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
