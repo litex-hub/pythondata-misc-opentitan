885da9fe9e3e8b5def605cc55ec0cc57c094c928
LiteX Robot <robot@timvideos.us>
2022-06-25 03:06:45 +0000

    Updating pythondata-misc-opentitan to 0.0.post12850
    Updated data to v0.0-12708-ga7319457e0 based on a7319457e049f55bd628badcd48630d339363eff from https://github.com/lowRISC/opentitan.
    > commit a7319457e049f55bd628badcd48630d339363eff
    > Author: Alexander Williams <awill@google.com>
    > Date:   Fri Jun 24 10:24:56 2022 -0700
    >
    >     [usbdev] Fix OUT ZLP handling under FIFO errors
    >
    >     Previously, zero-length packets could elicit an ACK response even when
    >     the AV FIFO is empty or the RX FIFO is full. In the OUT transaction
    >     state machine, the state of the FIFOs was only checked when data would
    >     be written to a buffer. However, in usbdev_usbif, the transaction would
    >     be rejected, so the host would incorrectly think its packet reached the
    >     device.
    >
    >     Fix this up by having the state machine also check FIFO states upon
    >     reaching the decision point for a handshake phase. This will be the
    >     final gate that will reject ZLPs that can't get an entry for the RX
    >     FIFO.
    >
    >     Signed-off-by: Alexander Williams <awill@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,37 +4,44 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12849"
-version_tuple = (0, 0, 12849)
+version_str = "0.0.post12850"
+version_tuple = (0, 0, 12850)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12849")
+    pversion = V("0.0.post12850")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12707"
-data_version_tuple = (0, 0, 12707)
+data_version_str = "0.0.post12708"
+data_version_tuple = (0, 0, 12708)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12707")
+    pdata_version = V("0.0.post12708")
 except ImportError:
     pass
-data_git_hash = "5ecc7e1cc83abd761b736bafcf7332f40e070f58"
-data_git_describe = "v0.0-12707-g5ecc7e1cc8"
+data_git_hash = "a7319457e049f55bd628badcd48630d339363eff"
+data_git_describe = "v0.0-12708-ga7319457e0"
 data_git_msg = """\
-commit 5ecc7e1cc83abd761b736bafcf7332f40e070f58
-Author: Guillermo Maturana <maturana@google.com>
-Date:   Fri Jun 24 15:15:32 2022 -0700
+commit a7319457e049f55bd628badcd48630d339363eff
+Author: Alexander Williams <awill@google.com>
+Date:   Fri Jun 24 10:24:56 2022 -0700
 
-    [dv,rstmgr] Fix rstmgr leaf_rst tests
+    [usbdev] Fix OUT ZLP handling under FIFO errors
     
-    Update the list of rstmgr_leaf_rst instances.
-    Extend the time to wait on shadow_attack tests.
-    Disable cascading reset SVAs in shadow_attack tests since they violate
-    the reset protocol.
+    Previously, zero-length packets could elicit an ACK response even when
+    the AV FIFO is empty or the RX FIFO is full. In the OUT transaction
+    state machine, the state of the FIFOs was only checked when data would
+    be written to a buffer. However, in usbdev_usbif, the transaction would
+    be rejected, so the host would incorrectly think its packet reached the
+    device.
     
-    Signed-off-by: Guillermo Maturana <maturana@google.com>
+    Fix this up by having the state machine also check FIFO states upon
+    reaching the decision point for a handshake phase. This will be the
+    final gate that will reject ZLPs that can't get an entry for the RX
+    FIFO.
+    
+    Signed-off-by: Alexander Williams <awill@google.com>
 
 """
 
