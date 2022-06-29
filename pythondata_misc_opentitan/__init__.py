e3c9f93467d9d82ba67523e7ef3d7e17eb115a8c
LiteX Robot <robot@timvideos.us>
2022-06-29 16:31:36 +0000

    Updating pythondata-misc-opentitan to 0.0.post12886
    Updated data to v0.0-12744-g1c8f1e426f based on 1c8f1e426fb6a3df9c38cef5082896467b6340e3 from https://github.com/lowRISC/opentitan.
    > commit 1c8f1e426fb6a3df9c38cef5082896467b6340e3
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Thu Jun 9 16:36:00 2022 -0700
    >
    >     [dv/chip] Update chip-level LPG testplan
    >
    >     This PR adds an enhancement to alert's LPG testplan.
    >     It adds to scenarios where rst/clk mgrs can turn off IP's clock or
    >     toggle reset.
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12885"
-version_tuple = (0, 0, 12885)
+version_str = "0.0.post12886"
+version_tuple = (0, 0, 12886)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12885")
+    pversion = V("0.0.post12886")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12743"
-data_version_tuple = (0, 0, 12743)
+data_version_str = "0.0.post12744"
+data_version_tuple = (0, 0, 12744)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12743")
+    pdata_version = V("0.0.post12744")
 except ImportError:
     pass
-data_git_hash = "badbbac145fce9f37c822883e3faf6ba777104d1"
-data_git_describe = "v0.0-12743-gbadbbac145"
+data_git_hash = "1c8f1e426fb6a3df9c38cef5082896467b6340e3"
+data_git_describe = "v0.0-12744-g1c8f1e426f"
 data_git_msg = """\
-commit badbbac145fce9f37c822883e3faf6ba777104d1
-Author: Michael Schaffner <msf@opentitan.org>
-Date:   Tue Jun 28 15:41:41 2022 +0200
+commit 1c8f1e426fb6a3df9c38cef5082896467b6340e3
+Author: Cindy Chen <chencindy@opentitan.org>
+Date:   Thu Jun 9 16:36:00 2022 -0700
 
-    [alert_handler] Add some more assertions to check wait cycle mask
+    [dv/chip] Update chip-level LPG testplan
     
-    Signed-off-by: Michael Schaffner <msf@opentitan.org>
+    This PR adds an enhancement to alert's LPG testplan.
+    It adds to scenarios where rst/clk mgrs can turn off IP's clock or
+    toggle reset.
+    
+    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
 """
 
