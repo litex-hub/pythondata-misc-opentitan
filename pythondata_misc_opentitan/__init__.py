44292d636e9298a756a540e327f231a977fb8b6c
LiteX Robot <robot@timvideos.us>
2022-06-29 00:43:38 +0000

    Updating pythondata-misc-opentitan to 0.0.post12876
    Updated data to v0.0-12734-g67ba900f5e based on 67ba900f5e83c07dff0aa3c4acd95005b04d7bea from https://github.com/lowRISC/opentitan.
    > commit 67ba900f5e83c07dff0aa3c4acd95005b04d7bea
    > Author: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
    > Date:   Sat Jun 4 10:11:42 2022 -0700
    >
    >     [entropy_src/dv] Test support for SW_REGUPD and CSR autolock
    >
    >     - entropy_src_rng_vseq now randomly reconfigures the DUT while it is
    >       enabled or while SW_REGUPD is zero.  Such events should have no
    >       impact on the DUT in these states.
    >       - Adds a new timing parameter, mean_rand_reconfig_time
    >         to control the average frequency of these random events.
    >         Like the hard_mtbf, and soft_mtbf parameters, these
    >         events occur with an exponential distribution, meaning
    >         that on average the probability of such an update occuring
    >         in a period of duration dt is:
    >         P(t)dt = dt/mean_rand_reconfig_time
    >       - These update events are also scheduled to NOT happen
    >         during other maintaince events (DUT resets, FIFO reads,
    >         etc).
    >     - Updates the Scoreboard to properly support this feature
    >     - Adds support in the rng and base vseqs for generating new
    >       configurations, subject to the same original constraints.
    >     - In order to support the randomization of DUT configurations
    >       a new entropy_src_dut_cfg class is added to separate the
    >       config fields that describe DUT configs from more generic
    >       environment or sequence configurations.
    >
    >     Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,53 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12875"
-version_tuple = (0, 0, 12875)
+version_str = "0.0.post12876"
+version_tuple = (0, 0, 12876)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12875")
+    pversion = V("0.0.post12876")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12733"
-data_version_tuple = (0, 0, 12733)
+data_version_str = "0.0.post12734"
+data_version_tuple = (0, 0, 12734)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12733")
+    pdata_version = V("0.0.post12734")
 except ImportError:
     pass
-data_git_hash = "00d5622674f50d751d4aa0850e19d3149597db9c"
-data_git_describe = "v0.0-12733-g00d5622674"
+data_git_hash = "67ba900f5e83c07dff0aa3c4acd95005b04d7bea"
+data_git_describe = "v0.0-12734-g67ba900f5e"
 data_git_msg = """\
-commit 00d5622674f50d751d4aa0850e19d3149597db9c
-Author: Michael Schaffner <msf@opentitan.org>
-Date:   Tue Jun 14 22:39:01 2022 +0200
+commit 67ba900f5e83c07dff0aa3c4acd95005b04d7bea
+Author: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
+Date:   Sat Jun 4 10:11:42 2022 -0700
 
-    [topgen] Pass alias register paths into topgen for top RAL generation
+    [entropy_src/dv] Test support for SW_REGUPD and CSR autolock
     
-    Signed-off-by: Michael Schaffner <msf@opentitan.org>
+    - entropy_src_rng_vseq now randomly reconfigures the DUT while it is
+      enabled or while SW_REGUPD is zero.  Such events should have no
+      impact on the DUT in these states.
+      - Adds a new timing parameter, mean_rand_reconfig_time
+        to control the average frequency of these random events.
+        Like the hard_mtbf, and soft_mtbf parameters, these
+        events occur with an exponential distribution, meaning
+        that on average the probability of such an update occuring
+        in a period of duration dt is:
+        P(t)dt = dt/mean_rand_reconfig_time
+      - These update events are also scheduled to NOT happen
+        during other maintaince events (DUT resets, FIFO reads,
+        etc).
+    - Updates the Scoreboard to properly support this feature
+    - Adds support in the rng and base vseqs for generating new
+      configurations, subject to the same original constraints.
+    - In order to support the randomization of DUT configurations
+      a new entropy_src_dut_cfg class is added to separate the
+      config fields that describe DUT configs from more generic
+      environment or sequence configurations.
+    
+    Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
 
 """
 
