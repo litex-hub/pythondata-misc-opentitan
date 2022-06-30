48220e55ed9411f013fe15f3fec4e6b51ffbfe2f
LiteX Robot <robot@timvideos.us>
2022-06-30 18:16:46 +0000

    Updating pythondata-misc-opentitan to 0.0.post12897
    Updated data to v0.0-12755-g749ef4f591 based on 749ef4f591c2587c251cda3a1eafab7a7cd730e2 from https://github.com/lowRISC/opentitan.
    > commit 749ef4f591c2587c251cda3a1eafab7a7cd730e2
    > Author: Alphan Ulusoy <alphan@google.com>
    > Date:   Thu Jun 30 07:22:48 2022 -0400
    >
    >     [util] Make util/generate_compilation_db.py compatiable with python 3.6
    >
    >     Signed-off-by: Alphan Ulusoy <alphan@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12895"
-version_tuple = (0, 0, 12895)
+version_str = "0.0.post12897"
+version_tuple = (0, 0, 12897)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12895")
+    pversion = V("0.0.post12897")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12753"
-data_version_tuple = (0, 0, 12753)
+data_version_str = "0.0.post12755"
+data_version_tuple = (0, 0, 12755)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12753")
+    pdata_version = V("0.0.post12755")
 except ImportError:
     pass
-data_git_hash = "be5236c029f018715868d822a97407f6721f5582"
-data_git_describe = "v0.0-12753-gbe5236c029"
+data_git_hash = "749ef4f591c2587c251cda3a1eafab7a7cd730e2"
+data_git_describe = "v0.0-12755-g749ef4f591"
 data_git_msg = """\
-commit be5236c029f018715868d822a97407f6721f5582
-Author: Alexander Williams <awill@google.com>
-Date:   Mon Jun 27 14:39:11 2022 -0700
+commit 749ef4f591c2587c251cda3a1eafab7a7cd730e2
+Author: Alphan Ulusoy <alphan@google.com>
+Date:   Thu Jun 30 07:22:48 2022 -0400
 
-    [usbdev] Do not write to unallocated buffer
+    [util] Make util/generate_compilation_db.py compatiable with python 3.6
     
-    The "put" derived from the OUT state machine is not gated by whether a
-    buffer is available. Prevent writing to an unallocated buffer by taking
-    into consideration whether the AV FIFO is empty.
-    
-    Signed-off-by: Alexander Williams <awill@google.com>
+    Signed-off-by: Alphan Ulusoy <alphan@google.com>
 
 """
 
