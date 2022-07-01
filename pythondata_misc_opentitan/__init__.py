8081eb5abd43c488b0447c51a6c118c01fbd885e
LiteX Robot <robot@timvideos.us>
2022-07-01 14:12:25 +0000

    Updating pythondata-misc-opentitan to 0.0.post12912
    Updated data to v0.0-12770-gdbc470d2bf based on dbc470d2bfcb528a996db9060294207a9ffbdffd from https://github.com/lowRISC/opentitan.
    > commit dbc470d2bfcb528a996db9060294207a9ffbdffd
    > Author: Michał Mazurek <maz@semihalf.com>
    > Date:   Wed Jun 29 16:46:43 2022 +0200
    >
    >     [opentitanlib] Fix Ti50Emulator Uart Sub-process state handling.
    >
    >     Fix state handling due to changes introduced by PR#13277
    >     with function get_socket(..).
    >
    >     Signed-off-by: Michał Mazurek <maz@semihalf.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12911"
-version_tuple = (0, 0, 12911)
+version_str = "0.0.post12912"
+version_tuple = (0, 0, 12912)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12911")
+    pversion = V("0.0.post12912")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12769"
-data_version_tuple = (0, 0, 12769)
+data_version_str = "0.0.post12770"
+data_version_tuple = (0, 0, 12770)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12769")
+    pdata_version = V("0.0.post12770")
 except ImportError:
     pass
-data_git_hash = "cbb1d8754089353268897cedd3416fc590f6f1d9"
-data_git_describe = "v0.0-12769-gcbb1d87540"
+data_git_hash = "dbc470d2bfcb528a996db9060294207a9ffbdffd"
+data_git_describe = "v0.0-12770-gdbc470d2bf"
 data_git_msg = """\
-commit cbb1d8754089353268897cedd3416fc590f6f1d9
-Author: Steve Nelson <steve.nelson@wdc.com>
-Date:   Tue Jun 28 08:07:31 2022 -0700
+commit dbc470d2bfcb528a996db9060294207a9ffbdffd
+Author: Michał Mazurek <maz@semihalf.com>
+Date:   Wed Jun 29 16:46:43 2022 +0200
 
-    [tools/dv] Modify common.ccf file for proper expression coverage
+    [opentitanlib] Fix Ti50Emulator Uart Sub-process state handling.
     
-    Signed-off-by: Steve Nelson <steve.nelson@wdc.com>
+    Fix state handling due to changes introduced by PR#13277
+    with function get_socket(..).
+    
+    Signed-off-by: Michał Mazurek <maz@semihalf.com>
 
 """
 
