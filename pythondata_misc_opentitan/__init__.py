8b1fd6c3489a043a8db43e5962c42480b04aff99
LiteX Robot <robot@timvideos.us>
2022-07-01 17:22:31 +0000

    Updating pythondata-misc-opentitan to 0.0.post12925
    Updated data to v0.0-12783-g1697525a0b based on 1697525a0b9029af2a8601be5bfecb9449f3492d from https://github.com/lowRISC/opentitan.
    > commit 1697525a0b9029af2a8601be5bfecb9449f3492d
    > Author: Miguel Osorio <miguelosorio@google.com>
    > Date:   Fri Jul 1 10:09:49 2022 -0700
    >
    >     [doc] Remove sw/device/examples index page.
    >
    >     This document currently doesn't have any content, and all examples
    >     documentation are covered in README.md files.
    >
    >     Signed-off-by: Miguel Osorio <miguelosorio@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,30 +4,33 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12918"
-version_tuple = (0, 0, 12918)
+version_str = "0.0.post12925"
+version_tuple = (0, 0, 12925)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12918")
+    pversion = V("0.0.post12925")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12776"
-data_version_tuple = (0, 0, 12776)
+data_version_str = "0.0.post12783"
+data_version_tuple = (0, 0, 12783)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12776")
+    pdata_version = V("0.0.post12783")
 except ImportError:
     pass
-data_git_hash = "dd256fe3b7d5aed44574baf3b10278ef741fdc54"
-data_git_describe = "v0.0-12776-gdd256fe3b7"
+data_git_hash = "1697525a0b9029af2a8601be5bfecb9449f3492d"
+data_git_describe = "v0.0-12783-g1697525a0b"
 data_git_msg = """\
-commit dd256fe3b7d5aed44574baf3b10278ef741fdc54
+commit 1697525a0b9029af2a8601be5bfecb9449f3492d
 Author: Miguel Osorio <miguelosorio@google.com>
-Date:   Fri Jul 1 08:18:59 2022 -0700
+Date:   Fri Jul 1 10:09:49 2022 -0700
 
-    [doc] Update top earlgrey block diagram
+    [doc] Remove sw/device/examples index page.
+    
+    This document currently doesn't have any content, and all examples
+    documentation are covered in README.md files.
     
     Signed-off-by: Miguel Osorio <miguelosorio@google.com>
 
