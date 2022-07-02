12b4b8249e0d139e990be6ec83b992aca502669c
LiteX Robot <robot@timvideos.us>
2022-07-02 04:31:11 +0000

    Updating pythondata-misc-opentitan to 0.0.post12935
    Updated data to v0.0-12793-g6d81f51f41 based on 6d81f51f41d809e7bb030cc36291338ea567e870 from https://github.com/lowRISC/opentitan.
    > commit 6d81f51f41d809e7bb030cc36291338ea567e870
    > Author: Alphan Ulusoy <alphan@google.com>
    > Date:   Fri Jul 1 08:51:37 2022 -0400
    >
    >     [bazel] Enable e2e_bootup_success test
    >
    >     Signed-off-by: Alphan Ulusoy <alphan@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12931"
-version_tuple = (0, 0, 12931)
+version_str = "0.0.post12935"
+version_tuple = (0, 0, 12935)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12931")
+    pversion = V("0.0.post12935")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12789"
-data_version_tuple = (0, 0, 12789)
+data_version_str = "0.0.post12793"
+data_version_tuple = (0, 0, 12793)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12789")
+    pdata_version = V("0.0.post12793")
 except ImportError:
     pass
-data_git_hash = "0cc1e439b562ab1dffc5ce85ab4ad2a48b4245ee"
-data_git_describe = "v0.0-12789-g0cc1e439b5"
+data_git_hash = "6d81f51f41d809e7bb030cc36291338ea567e870"
+data_git_describe = "v0.0-12793-g6d81f51f41"
 data_git_msg = """\
-commit 0cc1e439b562ab1dffc5ce85ab4ad2a48b4245ee
-Author: Guillermo Maturana <maturana@google.com>
-Date:   Fri Jul 1 11:19:52 2022 -0700
+commit 6d81f51f41d809e7bb030cc36291338ea567e870
+Author: Alphan Ulusoy <alphan@google.com>
+Date:   Fri Jul 1 08:51:37 2022 -0400
 
-    [formal,conn] Add ast to entropy_src connectivity check
+    [bazel] Enable e2e_bootup_success test
     
-    Also fix path to usbdev ram.
-    
-    Fixes #13460
-    
-    Signed-off-by: Guillermo Maturana <maturana@google.com>
+    Signed-off-by: Alphan Ulusoy <alphan@google.com>
 
 """
 
