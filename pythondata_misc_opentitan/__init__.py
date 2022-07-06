6ffc94c9c3c55fb7b9f9fcf5f136fea5f11c09a0
LiteX Robot <robot@timvideos.us>
2022-07-06 19:10:25 +0000

    Updating pythondata-misc-opentitan to 0.0.post12943
    Updated data to v0.0-12801-g168cc3161d based on 168cc3161d3232403592ef6370d0993874d0a346 from https://github.com/lowRISC/opentitan.
    > commit 168cc3161d3232403592ef6370d0993874d0a346
    > Author: Michał Mazurek <maz@semihalf.com>
    > Date:   Wed Jun 15 10:55:07 2022 +0200
    >
    >     [opentitanlib] Add I2C for Ti50Emulator transport
    >
    >     The implementation of the I2C bus for the Ti50Emulator is based on the following assumptions.
    >     Connections between DUT and the Host are made directly (point-to-point).
    >     There are no other devices on the bus, only DUT and Host are connected.
    >     In order to perform the tests, the connections do not need to be simulated at the level of transmitted bits.
    >     The implementation does not simulate the ACK / NACK mechanism.
    >
    >     The emulated I2C BUS is implemented as a Unix Stream socket with a request/respons
    >     based protocol to describe read and write transactions.
    >
    >     Signed-off-by: Michał Mazurek <maz@semihalf.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,41 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12942"
-version_tuple = (0, 0, 12942)
+version_str = "0.0.post12943"
+version_tuple = (0, 0, 12943)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12942")
+    pversion = V("0.0.post12943")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12800"
-data_version_tuple = (0, 0, 12800)
+data_version_str = "0.0.post12801"
+data_version_tuple = (0, 0, 12801)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12800")
+    pdata_version = V("0.0.post12801")
 except ImportError:
     pass
-data_git_hash = "47e856187ae49dcf5e4976d52f4c9649b7eb8828"
-data_git_describe = "v0.0-12800-g47e856187a"
+data_git_hash = "168cc3161d3232403592ef6370d0993874d0a346"
+data_git_describe = "v0.0-12801-g168cc3161d"
 data_git_msg = """\
-commit 47e856187ae49dcf5e4976d52f4c9649b7eb8828
-Author: Drew Macrae <drewmacrae@google.com>
-Date:   Wed Jun 22 13:17:29 2022 -0400
+commit 168cc3161d3232403592ef6370d0993874d0a346
+Author: Michał Mazurek <maz@semihalf.com>
+Date:   Wed Jun 15 10:55:07 2022 +0200
 
-    [meson] remove meson.build file from sw/device/tests/sim_dv
+    [opentitanlib] Add I2C for Ti50Emulator transport
     
-    Signed-off-by: Drew Macrae <drewmacrae@google.com>
+    The implementation of the I2C bus for the Ti50Emulator is based on the following assumptions.
+    Connections between DUT and the Host are made directly (point-to-point).
+    There are no other devices on the bus, only DUT and Host are connected.
+    In order to perform the tests, the connections do not need to be simulated at the level of transmitted bits.
+    The implementation does not simulate the ACK / NACK mechanism.
+    
+    The emulated I2C BUS is implemented as a Unix Stream socket with a request/respons
+    based protocol to describe read and write transactions.
+    
+    Signed-off-by: Michał Mazurek <maz@semihalf.com>
 
 """
 
