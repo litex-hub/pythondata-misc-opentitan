243917bbd671ac0fb17403122673bf9b55279e31
LiteX Robot <robot@timvideos.us>
2022-07-06 21:12:11 +0000

    Updating pythondata-misc-opentitan to 0.0.post12944
    Updated data to v0.0-12802-g9591e1f25d based on 9591e1f25d02e10673e259d9c29b42bf0686bfcc from https://github.com/lowRISC/opentitan.
    > commit 9591e1f25d02e10673e259d9c29b42bf0686bfcc
    > Author: Muqing Liu <muqing.liu@wdc.com>
    > Date:   Thu Jun 30 13:25:11 2022 -0700
    >
    >     [pwm, rtl] PWM RTL fix for duty cycle overflow/underflow
    >
    >       - Fix the PWM RTL duty cycle overflow/underflow issue to match the spec
    >       - Fixes #13434
    >
    >     Signed-off-by: Muqing Liu <muqing.liu@wdc.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,41 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12943"
-version_tuple = (0, 0, 12943)
+version_str = "0.0.post12944"
+version_tuple = (0, 0, 12944)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12943")
+    pversion = V("0.0.post12944")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12801"
-data_version_tuple = (0, 0, 12801)
+data_version_str = "0.0.post12802"
+data_version_tuple = (0, 0, 12802)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12801")
+    pdata_version = V("0.0.post12802")
 except ImportError:
     pass
-data_git_hash = "168cc3161d3232403592ef6370d0993874d0a346"
-data_git_describe = "v0.0-12801-g168cc3161d"
+data_git_hash = "9591e1f25d02e10673e259d9c29b42bf0686bfcc"
+data_git_describe = "v0.0-12802-g9591e1f25d"
 data_git_msg = """\
-commit 168cc3161d3232403592ef6370d0993874d0a346
-Author: Michał Mazurek <maz@semihalf.com>
-Date:   Wed Jun 15 10:55:07 2022 +0200
+commit 9591e1f25d02e10673e259d9c29b42bf0686bfcc
+Author: Muqing Liu <muqing.liu@wdc.com>
+Date:   Thu Jun 30 13:25:11 2022 -0700
 
-    [opentitanlib] Add I2C for Ti50Emulator transport
+    [pwm, rtl] PWM RTL fix for duty cycle overflow/underflow
     
-    The implementation of the I2C bus for the Ti50Emulator is based on the following assumptions.
-    Connections between DUT and the Host are made directly (point-to-point).
-    There are no other devices on the bus, only DUT and Host are connected.
-    In order to perform the tests, the connections do not need to be simulated at the level of transmitted bits.
-    The implementation does not simulate the ACK / NACK mechanism.
+      - Fix the PWM RTL duty cycle overflow/underflow issue to match the spec
+      - Fixes #13434
     
-    The emulated I2C BUS is implemented as a Unix Stream socket with a request/respons
-    based protocol to describe read and write transactions.
-    
-    Signed-off-by: Michał Mazurek <maz@semihalf.com>
+    Signed-off-by: Muqing Liu <muqing.liu@wdc.com>
 
 """
 
