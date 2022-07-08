8517f59c5246de85b6a6ce2947095ba68a44f6c1
LiteX Robot <robot@timvideos.us>
2022-07-08 23:14:17 +0000

    Updating pythondata-misc-opentitan to 0.0.post13002
    Updated data to v0.0-12860-g4696b83b72 based on 4696b83b725c9a0cafd02d3f27e5687893fc1b1c from https://github.com/lowRISC/opentitan.
    > commit 4696b83b725c9a0cafd02d3f27e5687893fc1b1c
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Thu Jul 7 16:06:09 2022 -0700
    >
    >     [dv/alert_handler] Add more LPG checkings
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post13001"
-version_tuple = (0, 0, 13001)
+version_str = "0.0.post13002"
+version_tuple = (0, 0, 13002)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post13001")
+    pversion = V("0.0.post13002")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12859"
-data_version_tuple = (0, 0, 12859)
+data_version_str = "0.0.post12860"
+data_version_tuple = (0, 0, 12860)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12859")
+    pdata_version = V("0.0.post12860")
 except ImportError:
     pass
-data_git_hash = "1a180062f29790d404cf1d363fc27eca4af99c3e"
-data_git_describe = "v0.0-12859-g1a180062f2"
+data_git_hash = "4696b83b725c9a0cafd02d3f27e5687893fc1b1c"
+data_git_describe = "v0.0-12860-g4696b83b72"
 data_git_msg = """\
-commit 1a180062f29790d404cf1d363fc27eca4af99c3e
-Author: Michael Schaffner <msf@opentitan.org>
-Date:   Fri Jul 8 12:37:50 2022 -0700
+commit 4696b83b725c9a0cafd02d3f27e5687893fc1b1c
+Author: Cindy Chen <chencindy@opentitan.org>
+Date:   Thu Jul 7 16:06:09 2022 -0700
 
-    [alert_handler] Align docs for crashdump latching
+    [dv/alert_handler] Add more LPG checkings
     
-    Signed-off-by: Michael Schaffner <msf@opentitan.org>
+    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
 """
 
