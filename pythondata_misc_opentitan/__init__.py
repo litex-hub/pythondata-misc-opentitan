d157e65ae6a0c7354def4ce36a6dcc409251fcdf
LiteX Robot <robot@timvideos.us>
2022-07-08 15:12:15 +0000

    Updating pythondata-misc-opentitan to 0.0.post12975
    Updated data to v0.0-12833-g461b8f3c41 based on 461b8f3c41f9d1f3341f9a5582a51018daeb6243 from https://github.com/lowRISC/opentitan.
    > commit 461b8f3c41f9d1f3341f9a5582a51018daeb6243
    > Author: Prajwala Puttappa <prajwalaputtappa@lowrisc.org>
    > Date:   Wed Jun 22 11:29:45 2022 +0100
    >
    >     [otbn, dv] Adds otbn_pc_ctrl_flow_redun testcase
    >
    >     This commit adds a new testcase to verify OTBN.PC.CTRL_FLOW.REDUN
    >     countermeasure.
    >
    >     Signed-off-by: Prajwala Puttappa <prajwalaputtappa@lowrisc.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,37 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post12974"
-version_tuple = (0, 0, 12974)
+version_str = "0.0.post12975"
+version_tuple = (0, 0, 12975)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post12974")
+    pversion = V("0.0.post12975")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12832"
-data_version_tuple = (0, 0, 12832)
+data_version_str = "0.0.post12833"
+data_version_tuple = (0, 0, 12833)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12832")
+    pdata_version = V("0.0.post12833")
 except ImportError:
     pass
-data_git_hash = "c0a96f9b46e96aa83a2db0469383067c36fcb5e8"
-data_git_describe = "v0.0-12832-gc0a96f9b46"
+data_git_hash = "461b8f3c41f9d1f3341f9a5582a51018daeb6243"
+data_git_describe = "v0.0-12833-g461b8f3c41"
 data_git_msg = """\
-commit c0a96f9b46e96aa83a2db0469383067c36fcb5e8
-Author: Cindy Chen <chencindy@opentitan.org>
-Date:   Thu Jul 7 15:05:10 2022 -0700
+commit 461b8f3c41f9d1f3341f9a5582a51018daeb6243
+Author: Prajwala Puttappa <prajwalaputtappa@lowrisc.org>
+Date:   Wed Jun 22 11:29:45 2022 +0100
 
-    [dv/alert_handler] Change timeout from actual time to clock cycles
+    [otbn, dv] Adds otbn_pc_ctrl_flow_redun testcase
     
-    This PR changes the implementation of EDN request timeout check from
-    measuring actual time to clock cycles. This is becuase in LPG cases, we
-    will stub the clk_i, and it will trigger this timeout check even though
-    the clock is stopped.
+    This commit adds a new testcase to verify OTBN.PC.CTRL_FLOW.REDUN
+    countermeasure.
     
-    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
+    Signed-off-by: Prajwala Puttappa <prajwalaputtappa@lowrisc.org>
 
 """
 
