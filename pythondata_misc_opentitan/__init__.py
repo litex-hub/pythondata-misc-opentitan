6fe5e6d5897c8322ae24432a159c4c34e50afaed
LiteX Robot <robot@timvideos.us>
2022-07-12 16:14:33 +0000

    Updating pythondata-misc-opentitan to 0.0.post13042
    Updated data to v0.0-12900-gba51d069e9 based on ba51d069e96060409d83d770239748f4acc9ec88 from https://github.com/lowRISC/opentitan.
    > commit ba51d069e96060409d83d770239748f4acc9ec88
    > Author: Michael Schaffner <msf@opentitan.org>
    > Date:   Mon Jul 11 09:03:53 2022 -0700
    >
    >     [doc] Add more links pointing to secure hw design guidelines
    >
    >     Signed-off-by: Michael Schaffner <msf@opentitan.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post13037"
-version_tuple = (0, 0, 13037)
+version_str = "0.0.post13042"
+version_tuple = (0, 0, 13042)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post13037")
+    pversion = V("0.0.post13042")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12895"
-data_version_tuple = (0, 0, 12895)
+data_version_str = "0.0.post12900"
+data_version_tuple = (0, 0, 12900)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12895")
+    pdata_version = V("0.0.post12900")
 except ImportError:
     pass
-data_git_hash = "2ac1e40acfbf90d3ba8b1d688317355b873207ab"
-data_git_describe = "v0.0-12895-g2ac1e40acf"
+data_git_hash = "ba51d069e96060409d83d770239748f4acc9ec88"
+data_git_describe = "v0.0-12900-gba51d069e9"
 data_git_msg = """\
-commit 2ac1e40acfbf90d3ba8b1d688317355b873207ab
-Author: Canberk Topal <ctopal@lowrisc.org>
-Date:   Mon Jul 4 17:06:33 2022 +0100
+commit ba51d069e96060409d83d770239748f4acc9ec88
+Author: Michael Schaffner <msf@opentitan.org>
+Date:   Mon Jul 11 09:03:53 2022 -0700
 
-    [otbn,dv] Set same sideload key in standalone sim
+    [doc] Add more links pointing to secure hw design guidelines
     
-    Signed-off-by: Canberk Topal <ctopal@lowrisc.org>
+    Signed-off-by: Michael Schaffner <msf@opentitan.org>
 
 """
 
