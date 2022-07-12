cdc65acad610165deaca33ab91536bea76b9ea52
LiteX Robot <robot@timvideos.us>
2022-07-12 00:48:37 +0000

    Updating pythondata-misc-opentitan to 0.0.post13028
    Updated data to v0.0-12886-g85bb174a34 based on 85bb174a344bdc725fecb981c370c1af799a61a7 from https://github.com/lowRISC/opentitan.
    > commit 85bb174a344bdc725fecb981c370c1af799a61a7
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Fri Jun 24 17:27:18 2022 -0700
    >
    >     [flash_ctrl] Minor tweak of arbitration behavior
    >
    >     - make the arbitration count 5
    >     - only select new fill buffer when there is no match on request
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post13027"
-version_tuple = (0, 0, 13027)
+version_str = "0.0.post13028"
+version_tuple = (0, 0, 13028)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post13027")
+    pversion = V("0.0.post13028")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12885"
-data_version_tuple = (0, 0, 12885)
+data_version_str = "0.0.post12886"
+data_version_tuple = (0, 0, 12886)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12885")
+    pdata_version = V("0.0.post12886")
 except ImportError:
     pass
-data_git_hash = "a8e96a0213571207bea750f0e691e8d1521fe4c4"
-data_git_describe = "v0.0-12885-ga8e96a0213"
+data_git_hash = "85bb174a344bdc725fecb981c370c1af799a61a7"
+data_git_describe = "v0.0-12886-g85bb174a34"
 data_git_msg = """\
-commit a8e96a0213571207bea750f0e691e8d1521fe4c4
-Author: Cindy Chen <chencindy@opentitan.org>
-Date:   Fri Jul 8 15:14:28 2022 -0700
+commit 85bb174a344bdc725fecb981c370c1af799a61a7
+Author: Timothy Chen <timothytim@google.com>
+Date:   Fri Jun 24 17:27:18 2022 -0700
 
-    [util/dv] Fix sec_cm auto-gen in IP template file
+    [flash_ctrl] Minor tweak of arbitration behavior
     
-    This PR fixes issue #13560 regarding auto-generated ip_template file
-    cannot override sec_cm_testplan issue.
+    - make the arbitration count 5
+    - only select new fill buffer when there is no match on request
     
-    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
