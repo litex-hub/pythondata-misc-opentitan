e78388cdf5686f0702be6823e1850a24f308056f
LiteX Robot <robot@timvideos.us>
2022-07-12 17:18:57 +0000

    Updating pythondata-misc-opentitan to 0.0.post13045
    Updated data to v0.0-12903-g52f7da058f based on 52f7da058f43ce5d976841f48091366788496862 from https://github.com/lowRISC/opentitan.
    > commit 52f7da058f43ce5d976841f48091366788496862
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Thu Jun 30 17:38:14 2022 -0700
    >
    >     [clkmgr/prim] Make frequency measurement disable more robust
    >
    >     - handles part of #13489
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post13042"
-version_tuple = (0, 0, 13042)
+version_str = "0.0.post13045"
+version_tuple = (0, 0, 13045)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post13042")
+    pversion = V("0.0.post13045")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12900"
-data_version_tuple = (0, 0, 12900)
+data_version_str = "0.0.post12903"
+data_version_tuple = (0, 0, 12903)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12900")
+    pdata_version = V("0.0.post12903")
 except ImportError:
     pass
-data_git_hash = "ba51d069e96060409d83d770239748f4acc9ec88"
-data_git_describe = "v0.0-12900-gba51d069e9"
+data_git_hash = "52f7da058f43ce5d976841f48091366788496862"
+data_git_describe = "v0.0-12903-g52f7da058f"
 data_git_msg = """\
-commit ba51d069e96060409d83d770239748f4acc9ec88
-Author: Michael Schaffner <msf@opentitan.org>
-Date:   Mon Jul 11 09:03:53 2022 -0700
+commit 52f7da058f43ce5d976841f48091366788496862
+Author: Timothy Chen <timothytim@google.com>
+Date:   Thu Jun 30 17:38:14 2022 -0700
 
-    [doc] Add more links pointing to secure hw design guidelines
+    [clkmgr/prim] Make frequency measurement disable more robust
     
-    Signed-off-by: Michael Schaffner <msf@opentitan.org>
+    - handles part of #13489
+    
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
