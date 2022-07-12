67fa0474ba222e03b32a35ffb5ad8212d65eb44c
LiteX Robot <robot@timvideos.us>
2022-07-12 14:12:24 +0000

    Updating pythondata-misc-opentitan to 0.0.post13037
    Updated data to v0.0-12895-g2ac1e40acf based on 2ac1e40acfbf90d3ba8b1d688317355b873207ab from https://github.com/lowRISC/opentitan.
    > commit 2ac1e40acfbf90d3ba8b1d688317355b873207ab
    > Author: Canberk Topal <ctopal@lowrisc.org>
    > Date:   Mon Jul 4 17:06:33 2022 +0100
    >
    >     [otbn,dv] Set same sideload key in standalone sim
    >
    >     Signed-off-by: Canberk Topal <ctopal@lowrisc.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,49 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post13032"
-version_tuple = (0, 0, 13032)
+version_str = "0.0.post13037"
+version_tuple = (0, 0, 13037)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post13032")
+    pversion = V("0.0.post13037")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12890"
-data_version_tuple = (0, 0, 12890)
+data_version_str = "0.0.post12895"
+data_version_tuple = (0, 0, 12895)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12890")
+    pdata_version = V("0.0.post12895")
 except ImportError:
     pass
-data_git_hash = "db5ae4da3144f15ac8f5e3150502213b1491b054"
-data_git_describe = "v0.0-12890-gdb5ae4da31"
+data_git_hash = "2ac1e40acfbf90d3ba8b1d688317355b873207ab"
+data_git_describe = "v0.0-12895-g2ac1e40acf"
 data_git_msg = """\
-commit db5ae4da3144f15ac8f5e3150502213b1491b054
-Author: Chris Frantz <cfrantz@google.com>
-Date:   Tue Jun 21 18:32:47 2022 -0700
+commit 2ac1e40acfbf90d3ba8b1d688317355b873207ab
+Author: Canberk Topal <ctopal@lowrisc.org>
+Date:   Mon Jul 4 17:06:33 2022 +0100
 
-    [base] Add a `status_t` for use in user code
+    [otbn,dv] Set same sideload key in standalone sim
     
-    `status_t` is modelled after `absl::Status` and uses the same error
-    categories.
-    
-    - Ok status codes can carry any positive integer value from [0 .. INT_MAX].
-    - Error status codes carry the error code, the module id that
-      created the error (the first 3 letters of the filename) and a
-      parameter in the range [0 .. 2047] that, by default, is the
-      source line number in the module that created the error.
-    - A nonstandard printf specifier `%r` prints out a text representation
-      of the status value.
-    
-    The c++ standard is changed from `c++14` to `gnu++14`.  The preprocessor
-    macro magic used to create the error values is not turned on by
-    `-std=c++14` (ie: this change needs the comma elision behavior of
-    `##__VA_ARGS__`).  Lamentably, standard c++20 replacement `__VA_OPT__(,)`
-    does not work when `-std=c++14`.
-    
-    Signed-off-by: Chris Frantz <cfrantz@google.com>
+    Signed-off-by: Canberk Topal <ctopal@lowrisc.org>
 
 """
 
