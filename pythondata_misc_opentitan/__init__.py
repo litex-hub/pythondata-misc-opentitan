20b4aaeeb48ff338f52b9ba5ec2dae56b476a746
LiteX Robot <robot@timvideos.us>
2022-07-13 19:10:48 +0000

    Updating pythondata-misc-opentitan to 0.0.post13062
    Updated data to v0.0-12920-g170f37016e based on 170f37016ed77437f48b5648b89baa4a8ba1df2d from https://github.com/lowRISC/opentitan.
    > commit 170f37016ed77437f48b5648b89baa4a8ba1df2d
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Tue Jul 12 14:06:26 2022 -0700
    >
    >     [dv/alert_handler] Remove regwen related cov
    >
    >     Regwen cov is replaced by automated coverbins
    >     "regwen_val_when_new_value_written_cg" from dv_abse_reg_pkg.
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post13060"
-version_tuple = (0, 0, 13060)
+version_str = "0.0.post13062"
+version_tuple = (0, 0, 13062)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post13060")
+    pversion = V("0.0.post13062")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12918"
-data_version_tuple = (0, 0, 12918)
+data_version_str = "0.0.post12920"
+data_version_tuple = (0, 0, 12920)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12918")
+    pdata_version = V("0.0.post12920")
 except ImportError:
     pass
-data_git_hash = "7aad73af8d0e3c9f1df970428b12b5275adda2a0"
-data_git_describe = "v0.0-12918-g7aad73af8d"
+data_git_hash = "170f37016ed77437f48b5648b89baa4a8ba1df2d"
+data_git_describe = "v0.0-12920-g170f37016e"
 data_git_msg = """\
-commit 7aad73af8d0e3c9f1df970428b12b5275adda2a0
-Author: Jaedon Kim <jdonjdon@google.com>
-Date:   Tue Jul 12 03:48:48 2022 +0000
+commit 170f37016ed77437f48b5648b89baa4a8ba1df2d
+Author: Cindy Chen <chencindy@opentitan.org>
+Date:   Tue Jul 12 14:06:26 2022 -0700
 
-    [dv,flash_ctrl] regression fix : mp_region test
+    [dv/alert_handler] Remove regwen related cov
     
-    Signed-off-by: Jaedon Kim <jdonjdon@google.com>
+    Regwen cov is replaced by automated coverbins
+    "regwen_val_when_new_value_written_cg" from dv_abse_reg_pkg.
+    
+    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
 """
 
