88bed0a95680705b85f61f19ff2e22498de3d400
LiteX Robot <robot@timvideos.us>
2022-07-13 21:12:41 +0000

    Updating pythondata-misc-opentitan to 0.0.post13065
    Updated data to v0.0-12923-gbed33ef9b2 based on bed33ef9b2340cc6ab1873f515251af8297a4af0 from https://github.com/lowRISC/opentitan.
    > commit bed33ef9b2340cc6ab1873f515251af8297a4af0
    > Author: Dan McArdle <dmcardle@google.com>
    > Date:   Tue Jul 12 12:37:38 2022 -0400
    >
    >     [bazel] Add functest that uses OTP-spliced bitstream
    >
    >     Add a new CW310-specific functest:
    >     //sw/device/silicon_creator/mask_rom:e2e_bootup_success_otp_dev.
    >
    >     This new functest uses //hw/bitstream:mask_rom_otp_dev, which is the
    >     Mask ROM bitstream after splicing in the OTP Dev image, replacing the
    >     OTP RMA image.
    >
    >     Signed-off-by: Dan McArdle <dmcardle@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,39 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post13062"
-version_tuple = (0, 0, 13062)
+version_str = "0.0.post13065"
+version_tuple = (0, 0, 13065)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post13062")
+    pversion = V("0.0.post13065")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12920"
-data_version_tuple = (0, 0, 12920)
+data_version_str = "0.0.post12923"
+data_version_tuple = (0, 0, 12923)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12920")
+    pdata_version = V("0.0.post12923")
 except ImportError:
     pass
-data_git_hash = "170f37016ed77437f48b5648b89baa4a8ba1df2d"
-data_git_describe = "v0.0-12920-g170f37016e"
+data_git_hash = "bed33ef9b2340cc6ab1873f515251af8297a4af0"
+data_git_describe = "v0.0-12923-gbed33ef9b2"
 data_git_msg = """\
-commit 170f37016ed77437f48b5648b89baa4a8ba1df2d
-Author: Cindy Chen <chencindy@opentitan.org>
-Date:   Tue Jul 12 14:06:26 2022 -0700
+commit bed33ef9b2340cc6ab1873f515251af8297a4af0
+Author: Dan McArdle <dmcardle@google.com>
+Date:   Tue Jul 12 12:37:38 2022 -0400
 
-    [dv/alert_handler] Remove regwen related cov
+    [bazel] Add functest that uses OTP-spliced bitstream
     
-    Regwen cov is replaced by automated coverbins
-    "regwen_val_when_new_value_written_cg" from dv_abse_reg_pkg.
+    Add a new CW310-specific functest:
+    //sw/device/silicon_creator/mask_rom:e2e_bootup_success_otp_dev.
     
-    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
+    This new functest uses //hw/bitstream:mask_rom_otp_dev, which is the
+    Mask ROM bitstream after splicing in the OTP Dev image, replacing the
+    OTP RMA image.
+    
+    Signed-off-by: Dan McArdle <dmcardle@google.com>
 
 """
 
