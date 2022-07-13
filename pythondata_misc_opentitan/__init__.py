ca559210e26d7bb80b7e3d0d1f4a1bfd4a0e7983
LiteX Robot <robot@timvideos.us>
2022-07-13 22:13:38 +0000

    Updating pythondata-misc-opentitan to 0.0.post13067
    Updated data to v0.0-12925-gf353489e0d based on f353489e0d9a9645eeb4e07c3766640e20798af8 from https://github.com/lowRISC/opentitan.
    > commit f353489e0d9a9645eeb4e07c3766640e20798af8
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Wed Jul 13 12:19:52 2022 -0700
    >
    >     [chip/conn] Move a DV testplan entry to connectivity testplan
    >
    >     This PR removes a DV testplan entry (EDN clock and reset check in OTP)
    >     to a connectivity check. We discussed this topic in one of the DV
    >     meeting and found that there aren't any check points in simulation due
    >     to the clock and reset groups. So we move this to a connectivity check.
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,39 +4,37 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post13065"
-version_tuple = (0, 0, 13065)
+version_str = "0.0.post13067"
+version_tuple = (0, 0, 13067)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post13065")
+    pversion = V("0.0.post13067")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12923"
-data_version_tuple = (0, 0, 12923)
+data_version_str = "0.0.post12925"
+data_version_tuple = (0, 0, 12925)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12923")
+    pdata_version = V("0.0.post12925")
 except ImportError:
     pass
-data_git_hash = "bed33ef9b2340cc6ab1873f515251af8297a4af0"
-data_git_describe = "v0.0-12923-gbed33ef9b2"
+data_git_hash = "f353489e0d9a9645eeb4e07c3766640e20798af8"
+data_git_describe = "v0.0-12925-gf353489e0d"
 data_git_msg = """\
-commit bed33ef9b2340cc6ab1873f515251af8297a4af0
-Author: Dan McArdle <dmcardle@google.com>
-Date:   Tue Jul 12 12:37:38 2022 -0400
+commit f353489e0d9a9645eeb4e07c3766640e20798af8
+Author: Cindy Chen <chencindy@opentitan.org>
+Date:   Wed Jul 13 12:19:52 2022 -0700
 
-    [bazel] Add functest that uses OTP-spliced bitstream
+    [chip/conn] Move a DV testplan entry to connectivity testplan
     
-    Add a new CW310-specific functest:
-    //sw/device/silicon_creator/mask_rom:e2e_bootup_success_otp_dev.
+    This PR removes a DV testplan entry (EDN clock and reset check in OTP)
+    to a connectivity check. We discussed this topic in one of the DV
+    meeting and found that there aren't any check points in simulation due
+    to the clock and reset groups. So we move this to a connectivity check.
     
-    This new functest uses //hw/bitstream:mask_rom_otp_dev, which is the
-    Mask ROM bitstream after splicing in the OTP Dev image, replacing the
-    OTP RMA image.
-    
-    Signed-off-by: Dan McArdle <dmcardle@google.com>
+    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
 """
 
