35bc200d1691d7359411a4a712edcdc4324e86e6
LiteX Robot <robot@timvideos.us>
2022-07-14 09:12:34 +0000

    Updating pythondata-misc-opentitan to 0.0.post13072
    Updated data to v0.0-12930-gf3266dc363 based on f3266dc363ee3c5f0a8a2a65381dfb9797a4fc26 from https://github.com/lowRISC/opentitan.
    > commit f3266dc363ee3c5f0a8a2a65381dfb9797a4fc26
    > Author: Guillermo Maturana <maturana@google.com>
    > Date:   Wed Jul 13 12:21:49 2022 -0700
    >
    >     [dv/rstrmgr] Update list of rstmgr_leaf_rst
    >
    >     Update list per design change, and suggest a way to generate the list to
    >     ease updates. The incorrect list was causing all rstmgr_leaf_rst_cnsty
    >     tests to fail.
    >
    >     Signed-off-by: Guillermo Maturana <maturana@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,46 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post13071"
-version_tuple = (0, 0, 13071)
+version_str = "0.0.post13072"
+version_tuple = (0, 0, 13072)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post13071")
+    pversion = V("0.0.post13072")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12929"
-data_version_tuple = (0, 0, 12929)
+data_version_str = "0.0.post12930"
+data_version_tuple = (0, 0, 12930)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12929")
+    pdata_version = V("0.0.post12930")
 except ImportError:
     pass
-data_git_hash = "8c853f05a104abbb900caf22917be9f19e6da46e"
-data_git_describe = "v0.0-12929-g8c853f05a1"
+data_git_hash = "f3266dc363ee3c5f0a8a2a65381dfb9797a4fc26"
+data_git_describe = "v0.0-12930-gf3266dc363"
 data_git_msg = """\
-commit 8c853f05a104abbb900caf22917be9f19e6da46e
-Author: Jade Philipoom <jadep@google.com>
-Date:   Tue Jun 28 16:30:43 2022 +0100
+commit f3266dc363ee3c5f0a8a2a65381dfb9797a4fc26
+Author: Guillermo Maturana <maturana@google.com>
+Date:   Wed Jul 13 12:21:49 2022 -0700
 
-    [crypto] Change AES-GCM GHASH implementation to use 4-bit windows.
+    [dv/rstrmgr] Update list of rstmgr_leaf_rst
     
-    This provides significant space savings as well as speed savings for
-    small inputs, although large inputs will be substantially slower.
-    Specifically, the constant overhead per encryption is about 10x more
-    (17k vs 170k cycles) for 8-bit windows because of computing the product
-    table, but GHASH is about twice as fast (20k vs 10k cycles). We have to
-    compute 1 GHASH per plaintext or aad block, so eventually the speedup in
-    GHASH overtakes the higher overhead. However, this only overtaking
-    happens at about 256 bytes of plaintext + aad, which is already around
-    300k cycles. Since large inputs will need to be chunked to meet latency
-    requirements anyway, it seems like the space and speed for small inputs
-    are worth some slowdown in larger inputs.
+    Update list per design change, and suggest a way to generate the list to
+    ease updates. The incorrect list was causing all rstmgr_leaf_rst_cnsty
+    tests to fail.
     
-    Also includes a few code fixes from code review comments.
-    
-    Signed-off-by: Jade Philipoom <jadep@google.com>
+    Signed-off-by: Guillermo Maturana <maturana@google.com>
 
 """
 
