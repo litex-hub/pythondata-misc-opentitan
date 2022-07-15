5478b1189c4c476a9376a13a6ac7f0b22607f0e3
LiteX Robot <robot@timvideos.us>
2022-07-15 18:14:33 +0000

    Updating pythondata-misc-opentitan to 0.0.post13119
    Updated data to v0.0-12977-g0962269c70 based on 0962269c706650526c858a3522100c0364a75c6b from https://github.com/lowRISC/opentitan.
    > commit 0962269c706650526c858a3522100c0364a75c6b
    > Author: Joshua Park <jeoong@google.com>
    > Date:   Wed Jun 29 17:28:33 2022 -0700
    >
    >     [cdc] misc. waivers
    >
    >     AST clocks : waived S_GENCLK errors
    >     SPI_DEVICE :
    >     - Black-boxing dual port SRAM
    >     - Waived paths from TLUL error to u_reg
    >
    >     Signed-off-by: Joshua Park <jeoong@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,37 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post13117"
-version_tuple = (0, 0, 13117)
+version_str = "0.0.post13119"
+version_tuple = (0, 0, 13119)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post13117")
+    pversion = V("0.0.post13119")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12975"
-data_version_tuple = (0, 0, 12975)
+data_version_str = "0.0.post12977"
+data_version_tuple = (0, 0, 12977)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12975")
+    pdata_version = V("0.0.post12977")
 except ImportError:
     pass
-data_git_hash = "77d9624db87f02ba7b9d4e78d18d29b2b377c230"
-data_git_describe = "v0.0-12975-g77d9624db8"
+data_git_hash = "0962269c706650526c858a3522100c0364a75c6b"
+data_git_describe = "v0.0-12977-g0962269c70"
 data_git_msg = """\
-commit 77d9624db87f02ba7b9d4e78d18d29b2b377c230
-Author: Drew Macrae <drewmacrae@google.com>
-Date:   Tue Jul 12 09:58:52 2022 -0400
+commit 0962269c706650526c858a3522100c0364a75c6b
+Author: Joshua Park <jeoong@google.com>
+Date:   Wed Jun 29 17:28:33 2022 -0700
 
-    [english_breakfast] fake relevant drivers for test_rom reset reasons
+    [cdc] misc. waivers
     
-    Signed-off-by: Drew Macrae <drewmacrae@google.com>
+    AST clocks : waived S_GENCLK errors
+    SPI_DEVICE :
+    - Black-boxing dual port SRAM
+    - Waived paths from TLUL error to u_reg
+    
+    Signed-off-by: Joshua Park <jeoong@google.com>
 
 """
 
