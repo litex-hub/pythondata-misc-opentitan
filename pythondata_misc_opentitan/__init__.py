7b163c4b71b2c8b37f0951c2db27e8f701cf9bb6
LiteX Robot <robot@timvideos.us>
2022-07-15 13:44:21 +0000

    Updating pythondata-misc-opentitan to 0.0.post13101
    Updated data to v0.0-12959-gf4c69a028f based on f4c69a028fe09d6cb0e7a66fc7f79f8442c9a6ea from https://github.com/lowRISC/opentitan.
    > commit f4c69a028fe09d6cb0e7a66fc7f79f8442c9a6ea
    > Author: Andreas Kurth <adk@lowrisc.org>
    > Date:   Fri Jul 15 09:46:56 2022 +0200
    >
    >     [otbn,rtl] Fix `adder_y_res_used`
    >
    >     `adder_y_res_used` indicates whether `adder_y_res` is used in any way.
    >     This is relevant for determining if upstream integrity errors could
    >     possibly have any effect.  Prior to this commit, `adder_y_res_used` was
    >     `0` in one case where `adder_y_res` is used in an `if` condition.  This
    >     is wrong, and this commit fixes this flaw.
    >
    >     Signed-off-by: Andreas Kurth <adk@lowrisc.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,40 +4,38 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post13099"
-version_tuple = (0, 0, 13099)
+version_str = "0.0.post13101"
+version_tuple = (0, 0, 13101)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post13099")
+    pversion = V("0.0.post13101")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12957"
-data_version_tuple = (0, 0, 12957)
+data_version_str = "0.0.post12959"
+data_version_tuple = (0, 0, 12959)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12957")
+    pdata_version = V("0.0.post12959")
 except ImportError:
     pass
-data_git_hash = "db28d6d3b29bd99de8680dbd476ebdc88927186b"
-data_git_describe = "v0.0-12957-gdb28d6d3b2"
+data_git_hash = "f4c69a028fe09d6cb0e7a66fc7f79f8442c9a6ea"
+data_git_describe = "v0.0-12959-gf4c69a028f"
 data_git_msg = """\
-commit db28d6d3b29bd99de8680dbd476ebdc88927186b
-Author: Timothy Chen <timothytim@google.com>
-Date:   Mon Jul 11 15:41:53 2022 -0700
+commit f4c69a028fe09d6cb0e7a66fc7f79f8442c9a6ea
+Author: Andreas Kurth <adk@lowrisc.org>
+Date:   Fri Jul 15 09:46:56 2022 +0200
 
-    [top/cw310] Add cw310 to lint
+    [otbn,rtl] Fix `adder_y_res_used`
     
-    - this lint is mainly for integration checking, as it is as
-      assumed that top_earlgrey lint is done separately and that
-      ast also behaves correctly.
+    `adder_y_res_used` indicates whether `adder_y_res` is used in any way.
+    This is relevant for determining if upstream integrity errors could
+    possibly have any effect.  Prior to this commit, `adder_y_res_used` was
+    `0` in one case where `adder_y_res` is used in an `if` condition.  This
+    is wrong, and this commit fixes this flaw.
     
-    - we've been bitten multiple times by integration mistakes
-      that manifest as FPGA test failures, so hopefully this will
-      help us address some of the low hanging fruits.
-    
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Andreas Kurth <adk@lowrisc.org>
 
 """
 
