def921500e8572422749d33313c2f19d2afd5394
LiteX Robot <robot@timvideos.us>
2022-07-16 16:14:35 +0000

    Updating pythondata-misc-opentitan to 0.0.post13138
    Updated data to v0.0-12996-gb802eaad8b based on b802eaad8b300a1df13952ab77f328a13a025bc3 from https://github.com/lowRISC/opentitan.
    > commit b802eaad8b300a1df13952ab77f328a13a025bc3
    > Author: Guillermo Maturana <maturana@google.com>
    > Date:   Thu Jun 30 17:40:47 2022 -0700
    >
    >     [dv,chip,clkmgr] Add clkmgr_sleep_frequency test
    >
    >     This test is marked as broken for cw310 and verilator because ast_init
    >     is not done, which defeats the clock measurements.
    >
    >     Signed-off-by: Guillermo Maturana <maturana@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post13137"
-version_tuple = (0, 0, 13137)
+version_str = "0.0.post13138"
+version_tuple = (0, 0, 13138)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post13137")
+    pversion = V("0.0.post13138")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12995"
-data_version_tuple = (0, 0, 12995)
+data_version_str = "0.0.post12996"
+data_version_tuple = (0, 0, 12996)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12995")
+    pdata_version = V("0.0.post12996")
 except ImportError:
     pass
-data_git_hash = "4a949ee43da5ce26e1c8d338a1f1cbb370a0b82e"
-data_git_describe = "v0.0-12995-g4a949ee43d"
+data_git_hash = "b802eaad8b300a1df13952ab77f328a13a025bc3"
+data_git_describe = "v0.0-12996-gb802eaad8b"
 data_git_msg = """\
-commit 4a949ee43da5ce26e1c8d338a1f1cbb370a0b82e
-Author: Alphan Ulusoy <alphan@google.com>
-Date:   Sat Jul 2 00:49:16 2022 -0400
+commit b802eaad8b300a1df13952ab77f328a13a025bc3
+Author: Guillermo Maturana <maturana@google.com>
+Date:   Thu Jun 30 17:40:47 2022 -0700
 
-    [test] Add e2e bare_metal test
+    [dv,chip,clkmgr] Add clkmgr_sleep_frequency test
     
-    Signed-off-by: Alphan Ulusoy <alphan@google.com>
+    This test is marked as broken for cw310 and verilator because ast_init
+    is not done, which defeats the clock measurements.
+    
+    Signed-off-by: Guillermo Maturana <maturana@google.com>
 
 """
 
