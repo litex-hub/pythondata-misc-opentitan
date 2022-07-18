c17b3863a8eef5c76474f3b85d082a772f603cb7
LiteX Robot <robot@timvideos.us>
2022-07-18 19:12:06 +0000

    Updating pythondata-misc-opentitan to 0.0.post13150
    Updated data to v0.0-13008-gf0f9dbf27c based on f0f9dbf27c79145850fbe4352b4682e4c6481b47 from https://github.com/lowRISC/opentitan.
    > commit f0f9dbf27c79145850fbe4352b4682e4c6481b47
    > Author: Drew Macrae <drewmacrae@google.com>
    > Date:   Thu Jul 14 06:12:33 2022 -0400
    >
    >     [bazel/test_rom] use the dif for the rstmgr in test_rom
    >
    >     Some of the target specific sources are to do with bootstraps but some
    >     aren't specific anylonger so they were renamed target_test_rom_lib or
    >     with a specific platform substituted for "target"
    >
    >     Signed-off-by: Drew Macrae <drewmacrae@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,37 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post13149"
-version_tuple = (0, 0, 13149)
+version_str = "0.0.post13150"
+version_tuple = (0, 0, 13150)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post13149")
+    pversion = V("0.0.post13150")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post13007"
-data_version_tuple = (0, 0, 13007)
+data_version_str = "0.0.post13008"
+data_version_tuple = (0, 0, 13008)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post13007")
+    pdata_version = V("0.0.post13008")
 except ImportError:
     pass
-data_git_hash = "13baddf15c1520df1b569acec49c540fac12d8e8"
-data_git_describe = "v0.0-13007-g13baddf15c"
+data_git_hash = "f0f9dbf27c79145850fbe4352b4682e4c6481b47"
+data_git_describe = "v0.0-13008-gf0f9dbf27c"
 data_git_msg = """\
-commit 13baddf15c1520df1b569acec49c540fac12d8e8
-Author: Dan McArdle <dmcardle@google.com>
-Date:   Fri Jul 15 16:14:18 2022 -0400
+commit f0f9dbf27c79145850fbe4352b4682e4c6481b47
+Author: Drew Macrae <drewmacrae@google.com>
+Date:   Thu Jul 14 06:12:33 2022 -0400
 
-    [bazel] Refactor Bazel gen in bitstreams_workspace.py
+    [bazel/test_rom] use the dif for the rstmgr in test_rom
     
-    * Factor long triple-quote string literals into line lists that get
-      joined by '\n'.
+    Some of the target specific sources are to do with bootstraps but some
+    aren't specific anylonger so they were renamed target_test_rom_lib or
+    with a specific platform substituted for "target"
     
-    * Add helper function to generate `filegroup` blocks.
-    
-    Signed-off-by: Dan McArdle <dmcardle@google.com>
+    Signed-off-by: Drew Macrae <drewmacrae@google.com>
 
 """
 
