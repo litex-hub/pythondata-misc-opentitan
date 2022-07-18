f65395d66fc7dc40d001c8dcd954b2b427de94eb
LiteX Robot <robot@timvideos.us>
2022-07-18 00:44:23 +0000

    Updating pythondata-misc-opentitan to 0.0.post13141
    Updated data to v0.0-12999-g4a7932628d based on 4a7932628d9bca84c7d631528df7175886e2f551 from https://github.com/lowRISC/opentitan.
    > commit 4a7932628d9bca84c7d631528df7175886e2f551
    > Author: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
    > Date:   Sun Jul 10 13:38:16 2022 -0700
    >
    >     [entropy_src] Implement Error and Alert related covergroups
    >
    >     This PR updates the test plan to expands the err_test_cg in to several
    >     smaller covergroups that are easier to sample.
    >
    >     Since the original err_test_cg covers so many disparate classes of
    >     events that happen at different times, it would be hard to write a
    >     single sample function that covers them all.   Therefore this CG has
    >     been broken into 5 smaller coverpoints:
    >
    >      - `err_test_cg`: covers that writes to ERR_CODE_TEST generate the
    >         expected alerts for all bits
    >      - `mubi_err_cg`: covers that we've tested that writes to registers that
    >         expect redundancy in their values (including both MultiBit Booleans
    >         and the AlertThreshold numeric register), and that alerts are seen
    >         when bad values are inserted.
    >      - `sm_err_cg` covers that we've seen alerts when that the MAIN_SM and
    >         ACK_SM are forced into invalid values
    >      - `fifo_err_cg` covers that we've seen alerts when the FIFOs
    >         malfunction.
    >      - `cntr_err_cg` covers that all redundant counters have been forced to
    >         mismatching values.
    >
    >     NOTE: Alerts related to health tests (i.e. the main_sm _recoverable_
    >           alert) are covered by other HT related coverpoints.
    >
    >     In terms of implementation Sampling and scoreboarding has been updated
    >     to support sampling of the new err_test_cg.  entropy_src_rng_vseq
    >     has also been updated to generate ERR_CODE_TEST events.
    >
    >     Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,60 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post13139"
-version_tuple = (0, 0, 13139)
+version_str = "0.0.post13141"
+version_tuple = (0, 0, 13141)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post13139")
+    pversion = V("0.0.post13141")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post12997"
-data_version_tuple = (0, 0, 12997)
+data_version_str = "0.0.post12999"
+data_version_tuple = (0, 0, 12999)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post12997")
+    pdata_version = V("0.0.post12999")
 except ImportError:
     pass
-data_git_hash = "a9d8e2bdb023811f40309f52901e469be6c4bda3"
-data_git_describe = "v0.0-12997-ga9d8e2bdb0"
+data_git_hash = "4a7932628d9bca84c7d631528df7175886e2f551"
+data_git_describe = "v0.0-12999-g4a7932628d"
 data_git_msg = """\
-commit a9d8e2bdb023811f40309f52901e469be6c4bda3
-Author: Guillermo Maturana <maturana@google.com>
-Date:   Thu Jun 30 17:40:47 2022 -0700
+commit 4a7932628d9bca84c7d631528df7175886e2f551
+Author: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
+Date:   Sun Jul 10 13:38:16 2022 -0700
 
-    [dv,chip,clkmgr] Add clkmgr_sleep_frequency test
+    [entropy_src] Implement Error and Alert related covergroups
     
-    This test is marked as broken for cw310 and verilator because ast_init
-    is not done, which defeats the clock measurements.
+    This PR updates the test plan to expands the err_test_cg in to several
+    smaller covergroups that are easier to sample.
     
-    Signed-off-by: Guillermo Maturana <maturana@google.com>
+    Since the original err_test_cg covers so many disparate classes of
+    events that happen at different times, it would be hard to write a
+    single sample function that covers them all.   Therefore this CG has
+    been broken into 5 smaller coverpoints:
+    
+     - `err_test_cg`: covers that writes to ERR_CODE_TEST generate the
+        expected alerts for all bits
+     - `mubi_err_cg`: covers that we've tested that writes to registers that
+        expect redundancy in their values (including both MultiBit Booleans
+        and the AlertThreshold numeric register), and that alerts are seen
+        when bad values are inserted.
+     - `sm_err_cg` covers that we've seen alerts when that the MAIN_SM and
+        ACK_SM are forced into invalid values
+     - `fifo_err_cg` covers that we've seen alerts when the FIFOs
+        malfunction.
+     - `cntr_err_cg` covers that all redundant counters have been forced to
+        mismatching values.
+    
+    NOTE: Alerts related to health tests (i.e. the main_sm _recoverable_
+          alert) are covered by other HT related coverpoints.
+    
+    In terms of implementation Sampling and scoreboarding has been updated
+    to support sampling of the new err_test_cg.  entropy_src_rng_vseq
+    has also been updated to generate ERR_CODE_TEST events.
+    
+    Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
 
 """
 
