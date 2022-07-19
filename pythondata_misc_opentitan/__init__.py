02fedb5eb43ddec8104df184a60207574ee96ab0
LiteX Robot <robot@timvideos.us>
2022-07-19 19:10:19 +0000

    Updating pythondata-misc-opentitan to 0.0.post13176
    Updated data to v0.0-13034-g7365f25f56 based on 7365f25f56928f457e2d6a61fa96eaf8398730b9 from https://github.com/lowRISC/opentitan.
    > commit 7365f25f56928f457e2d6a61fa96eaf8398730b9
    > Author: Jaedon Kim <jdonjdon@google.com>
    > Date:   Thu Jul 14 16:35:38 2022 +0000
    >
    >     [flash_ctrl,dv] single bit error test
    >
    >     Signed-off-by: Jaedon Kim <jdonjdon@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post13175"
-version_tuple = (0, 0, 13175)
+version_str = "0.0.post13176"
+version_tuple = (0, 0, 13176)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post13175")
+    pversion = V("0.0.post13176")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post13033"
-data_version_tuple = (0, 0, 13033)
+data_version_str = "0.0.post13034"
+data_version_tuple = (0, 0, 13034)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post13033")
+    pdata_version = V("0.0.post13034")
 except ImportError:
     pass
-data_git_hash = "1e86ba2a238dc26c2111d325ee7645b0e65058e5"
-data_git_describe = "v0.0-13033-g1e86ba2a23"
+data_git_hash = "7365f25f56928f457e2d6a61fa96eaf8398730b9"
+data_git_describe = "v0.0-13034-g7365f25f56"
 data_git_msg = """\
-commit 1e86ba2a238dc26c2111d325ee7645b0e65058e5
-Author: Cindy Chen <chencindy@opentitan.org>
-Date:   Tue Jul 19 09:41:20 2022 -0700
+commit 7365f25f56928f457e2d6a61fa96eaf8398730b9
+Author: Jaedon Kim <jdonjdon@google.com>
+Date:   Thu Jul 14 16:35:38 2022 +0000
 
-    [doc/kmac] Fix a small typo
+    [flash_ctrl,dv] single bit error test
     
-    Fix a small typo in kmac doc regarding kmac_pkg.
-    
-    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
+    Signed-off-by: Jaedon Kim <jdonjdon@google.com>
 
 """
 
