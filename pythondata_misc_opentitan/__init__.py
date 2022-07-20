207aa48ec1aee18c6acd9bbff075ecf281565765
LiteX Robot <robot@timvideos.us>
2022-07-20 00:41:44 +0000

    Updating pythondata-misc-opentitan to 0.0.post13183
    Updated data to v0.0-13041-g23f837b7d0 based on 23f837b7d09068a1d293a9dedbe86e29932b6e84 from https://github.com/lowRISC/opentitan.
    > commit 23f837b7d09068a1d293a9dedbe86e29932b6e84
    > Author: Timothy Trippel <ttrippel@google.com>
    > Date:   Tue Jul 19 13:13:46 2022 -0700
    >
    >     [dif/entropy_src] move to S2
    >
    >     This updates the DIF checklists to move the SW to S2.
    >
    >     Signed-off-by: Timothy Trippel <ttrippel@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post13181"
-version_tuple = (0, 0, 13181)
+version_str = "0.0.post13183"
+version_tuple = (0, 0, 13183)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post13181")
+    pversion = V("0.0.post13183")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post13039"
-data_version_tuple = (0, 0, 13039)
+data_version_str = "0.0.post13041"
+data_version_tuple = (0, 0, 13041)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post13039")
+    pdata_version = V("0.0.post13041")
 except ImportError:
     pass
-data_git_hash = "9fac57c085f9d5910d758a983b7b78087274c96d"
-data_git_describe = "v0.0-13039-g9fac57c085"
+data_git_hash = "23f837b7d09068a1d293a9dedbe86e29932b6e84"
+data_git_describe = "v0.0-13041-g23f837b7d0"
 data_git_msg = """\
-commit 9fac57c085f9d5910d758a983b7b78087274c96d
-Author: Cindy Chen <chencindy@opentitan.org>
-Date:   Tue Jul 19 11:02:15 2022 -0700
+commit 23f837b7d09068a1d293a9dedbe86e29932b6e84
+Author: Timothy Trippel <ttrippel@google.com>
+Date:   Tue Jul 19 13:13:46 2022 -0700
 
-    [dv/kmac] improve app interface sequence
+    [dif/entropy_src] move to S2
     
-    Remove the constraint to configure kmac cfg_shadow register hash_mode
-    field. Kmac should be able to select the correct mode when using the app
-    interface.
+    This updates the DIF checklists to move the SW to S2.
     
-    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
+    Signed-off-by: Timothy Trippel <ttrippel@google.com>
 
 """
 
