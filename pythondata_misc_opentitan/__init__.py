705b75012468417b87803a1bb89c2d6b235c561b
LiteX Robot <robot@timvideos.us>
2022-07-20 23:13:25 +0000

    Updating pythondata-misc-opentitan to 0.0.post13197
    Updated data to v0.0-13055-gca7ed9cf6b based on ca7ed9cf6b733a425ab5333763e9a5fd518d89e8 from https://github.com/lowRISC/opentitan.
    > commit ca7ed9cf6b733a425ab5333763e9a5fd518d89e8
    > Author: Srikrishna Iyer <sriyer@google.com>
    > Date:   Tue Jul 19 10:13:54 2022 -0700
    >
    >     [dvsim] Make email.html filename more descriptive
    >
    >     This commit makes 2 changes to the generated reports:
    >     - Remove `latest` symlink in the reports area
    >     - Make email.html filename more descriptive, i.e. reflective of
    >       what hjson cfg was run, the tool flow and the tool.
    >
    >     Signed-off-by: Srikrishna Iyer <sriyer@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,37 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post13196"
-version_tuple = (0, 0, 13196)
+version_str = "0.0.post13197"
+version_tuple = (0, 0, 13197)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post13196")
+    pversion = V("0.0.post13197")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post13054"
-data_version_tuple = (0, 0, 13054)
+data_version_str = "0.0.post13055"
+data_version_tuple = (0, 0, 13055)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post13054")
+    pdata_version = V("0.0.post13055")
 except ImportError:
     pass
-data_git_hash = "9a9f97f35d0f0595e576869b6db0ad503ad359dd"
-data_git_describe = "v0.0-13054-g9a9f97f35d"
+data_git_hash = "ca7ed9cf6b733a425ab5333763e9a5fd518d89e8"
+data_git_describe = "v0.0-13055-gca7ed9cf6b"
 data_git_msg = """\
-commit 9a9f97f35d0f0595e576869b6db0ad503ad359dd
-Author: Rasmus Madsen <rasmus.madsen@wdc.com>
-Date:   Tue Jul 19 03:59:01 2022 -0700
+commit ca7ed9cf6b733a425ab5333763e9a5fd518d89e8
+Author: Srikrishna Iyer <sriyer@google.com>
+Date:   Tue Jul 19 10:13:54 2022 -0700
 
-    [aes/dv] added FI for ctr fsm
+    [dvsim] Make email.html filename more descriptive
     
-    Signed-off-by: Rasmus Madsen <rasmus.madsen@wdc.com>
+    This commit makes 2 changes to the generated reports:
+    - Remove `latest` symlink in the reports area
+    - Make email.html filename more descriptive, i.e. reflective of
+      what hjson cfg was run, the tool flow and the tool.
+    
+    Signed-off-by: Srikrishna Iyer <sriyer@google.com>
 
 """
 
