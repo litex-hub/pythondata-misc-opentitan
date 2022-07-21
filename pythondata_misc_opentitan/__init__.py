f766afc5c1cd98b8e01e6e90c81bb61376e65b28
LiteX Robot <robot@timvideos.us>
2022-07-21 04:39:16 +0000

    Updating pythondata-misc-opentitan to 0.0.post13202
    Updated data to v0.0-13060-g45f56546c5 based on 45f56546c54cae125a8c7bd077cdf596adb27940 from https://github.com/lowRISC/opentitan.
    > commit 45f56546c54cae125a8c7bd077cdf596adb27940
    > Author: Michael Schaffner <msf@google.com>
    > Date:   Wed Jul 20 16:54:53 2022 -0700
    >
    >     [tlul_adapter_sram] Update comment describing the ByteAccess parameter
    >
    >     Signed-off-by: Michael Schaffner <msf@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post13199"
-version_tuple = (0, 0, 13199)
+version_str = "0.0.post13202"
+version_tuple = (0, 0, 13202)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post13199")
+    pversion = V("0.0.post13202")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post13057"
-data_version_tuple = (0, 0, 13057)
+data_version_str = "0.0.post13060"
+data_version_tuple = (0, 0, 13060)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post13057")
+    pdata_version = V("0.0.post13060")
 except ImportError:
     pass
-data_git_hash = "7003d824ec9598c2b2ef537c8afaf4ae43526aed"
-data_git_describe = "v0.0-13057-g7003d824ec"
+data_git_hash = "45f56546c54cae125a8c7bd077cdf596adb27940"
+data_git_describe = "v0.0-13060-g45f56546c5"
 data_git_msg = """\
-commit 7003d824ec9598c2b2ef537c8afaf4ae43526aed
-Author: Guillermo Maturana <maturana@google.com>
-Date:   Fri Jul 15 15:14:31 2022 -0700
+commit 45f56546c54cae125a8c7bd077cdf596adb27940
+Author: Michael Schaffner <msf@google.com>
+Date:   Wed Jul 20 16:54:53 2022 -0700
 
-    [sw,sensor_ctrl] Create testutils function for ast_init_done
+    [tlul_adapter_sram] Update comment describing the ByteAccess parameter
     
-    Signed-off-by: Guillermo Maturana <maturana@google.com>
+    Signed-off-by: Michael Schaffner <msf@google.com>
 
 """
 
