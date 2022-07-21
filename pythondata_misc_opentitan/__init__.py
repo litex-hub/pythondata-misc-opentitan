b1a1b2f65168577832118470d397175647776d37
LiteX Robot <robot@timvideos.us>
2022-07-21 19:15:13 +0000

    Updating pythondata-misc-opentitan to 0.0.post13211
    Updated data to v0.0-13069-g737e1a4407 based on 737e1a4407f5a1f2de4da2311bcab48c7b686a9e from https://github.com/lowRISC/opentitan.
    > commit 737e1a4407f5a1f2de4da2311bcab48c7b686a9e
    > Author: Muqing Liu <muqing.liu@wdc.com>
    > Date:   Wed Jul 20 16:56:30 2022 -0700
    >
    >     [entropy_src, dv] Add covergroup sample functions for entropy_src forced errors
    >
    >       - Add covergroup sample functions in the err_vseq for the forced errors that can't be accessed in the scoreboard
    >       - Update alert_vseq to accommodate the updates in recov_alert_sts register
    >       - Fix some path reference bugs in the err_vseq
    >
    >     Signed-off-by: Muqing Liu <muqing.liu@wdc.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post13210"
-version_tuple = (0, 0, 13210)
+version_str = "0.0.post13211"
+version_tuple = (0, 0, 13211)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post13210")
+    pversion = V("0.0.post13211")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post13068"
-data_version_tuple = (0, 0, 13068)
+data_version_str = "0.0.post13069"
+data_version_tuple = (0, 0, 13069)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post13068")
+    pdata_version = V("0.0.post13069")
 except ImportError:
     pass
-data_git_hash = "7c4d32f80bfa74bcace17905b174cdba78efd314"
-data_git_describe = "v0.0-13068-g7c4d32f80b"
+data_git_hash = "737e1a4407f5a1f2de4da2311bcab48c7b686a9e"
+data_git_describe = "v0.0-13069-g737e1a4407"
 data_git_msg = """\
-commit 7c4d32f80bfa74bcace17905b174cdba78efd314
-Author: Jaedon Kim <jdonjdon@google.com>
-Date:   Mon Jul 18 12:32:24 2022 +0000
+commit 737e1a4407f5a1f2de4da2311bcab48c7b686a9e
+Author: Muqing Liu <muqing.liu@wdc.com>
+Date:   Wed Jul 20 16:56:30 2022 -0700
 
-    [flash_ctrl,dv] do_direct_read update
+    [entropy_src, dv] Add covergroup sample functions for entropy_src forced errors
     
-     - pass abort signal to caller
-     - subsequent update all tests uses do_direct_read with a new signature
+      - Add covergroup sample functions in the err_vseq for the forced errors that can't be accessed in the scoreboard
+      - Update alert_vseq to accommodate the updates in recov_alert_sts register
+      - Fix some path reference bugs in the err_vseq
     
-    Signed-off-by: Jaedon Kim <jdonjdon@google.com>
+    Signed-off-by: Muqing Liu <muqing.liu@wdc.com>
 
 """
 
