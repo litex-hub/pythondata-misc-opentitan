3a11d2f98ee6030996ff1a27b4653678e32ac91f
LiteX Robot <robot@timvideos.us>
2022-07-26 00:48:43 +0000

    Updating pythondata-misc-opentitan to 0.0.post13262
    Updated data to v0.0-13120-g2f044ef0e5 based on 2f044ef0e564a055f1a0d639eb646fc7af7be606 from https://github.com/lowRISC/opentitan.
    > commit 2f044ef0e564a055f1a0d639eb646fc7af7be606
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Mon Jul 25 14:29:35 2022 -0700
    >
    >     [dv/kmac] Fix EDN timeout assertion failures
    >
    >     This PR fixes edn_timeout test's assertion failures:
    >     1). Req should be asserted until ack -> Fix this assertion error by
    >       extending the disable check statement to sequence level.
    >     2). Fix a path within the disable assertion statement in DV.
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,33 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post13260"
-version_tuple = (0, 0, 13260)
+version_str = "0.0.post13262"
+version_tuple = (0, 0, 13262)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post13260")
+    pversion = V("0.0.post13262")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post13118"
-data_version_tuple = (0, 0, 13118)
+data_version_str = "0.0.post13120"
+data_version_tuple = (0, 0, 13120)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post13118")
+    pdata_version = V("0.0.post13120")
 except ImportError:
     pass
-data_git_hash = "612efc49a499ff83af2632616b041f7ba23da6ab"
-data_git_describe = "v0.0-13118-g612efc49a4"
+data_git_hash = "2f044ef0e564a055f1a0d639eb646fc7af7be606"
+data_git_describe = "v0.0-13120-g2f044ef0e5"
 data_git_msg = """\
-commit 612efc49a499ff83af2632616b041f7ba23da6ab
+commit 2f044ef0e564a055f1a0d639eb646fc7af7be606
 Author: Cindy Chen <chencindy@opentitan.org>
-Date:   Mon Jul 25 10:05:32 2022 -0700
+Date:   Mon Jul 25 14:29:35 2022 -0700
 
-    [dv/kmac] Fix unmapped testplan
+    [dv/kmac] Fix EDN timeout assertion failures
     
-    This PR fixes unmapped stress_all tests because they have not been
-    declared in the testplan.
+    This PR fixes edn_timeout test's assertion failures:
+    1). Req should be asserted until ack -> Fix this assertion error by
+      extending the disable check statement to sequence level.
+    2). Fix a path within the disable assertion statement in DV.
     
     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
