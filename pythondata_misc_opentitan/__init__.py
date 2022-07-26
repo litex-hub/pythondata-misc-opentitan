1b9439c4d88350eb1053d9fb6e3091d9efe9c774
LiteX Robot <robot@timvideos.us>
2022-07-26 18:13:58 +0000

    Updating pythondata-misc-opentitan to 0.0.post13270
    Updated data to v0.0-13128-g6f14afac37 based on 6f14afac37d1b9d1fb0d66ff4cbffae8e1bda895 from https://github.com/lowRISC/opentitan.
    > commit 6f14afac37d1b9d1fb0d66ff4cbffae8e1bda895
    > Author: Srikrishna Iyer <sriyer@google.com>
    > Date:   Mon Jul 25 17:45:56 2022 -0700
    >
    >     [dv, waves] Improve wave dumping
    >
    >     A minor non-functional change to waves.tcl improves the way
    >     wavedumpScope method is used to dump specific hierarchies
    >     rather than dump the tb top by default.
    >
    >     The code is rearranged a little, and several comments are added
    >     to make the usage clearer. the `fid` argument of `wavedumpScope
    >     proc is removed in favor of using the value internally by
    >     referencing a global variable instead of passing it as an arg.
    >
    >     Wavedumping has been tested with vcs:fsdb,vpd,vcd and
    >     xcelium:shm,fsdb and everything seeems to be working ok.
    >
    >     Signed-off-by: Srikrishna Iyer <sriyer@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,44 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post13265"
-version_tuple = (0, 0, 13265)
+version_str = "0.0.post13270"
+version_tuple = (0, 0, 13270)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post13265")
+    pversion = V("0.0.post13270")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post13123"
-data_version_tuple = (0, 0, 13123)
+data_version_str = "0.0.post13128"
+data_version_tuple = (0, 0, 13128)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post13123")
+    pdata_version = V("0.0.post13128")
 except ImportError:
     pass
-data_git_hash = "9595dea00edadf0c90c1b93d8ecfb1e42ffd930e"
-data_git_describe = "v0.0-13123-g9595dea00e"
+data_git_hash = "6f14afac37d1b9d1fb0d66ff4cbffae8e1bda895"
+data_git_describe = "v0.0-13128-g6f14afac37"
 data_git_msg = """\
-commit 9595dea00edadf0c90c1b93d8ecfb1e42ffd930e
-Author: Mark Branstad <mark.branstad@wdc.com>
-Date:   Mon Jul 25 12:11:04 2022 -0700
+commit 6f14afac37d1b9d1fb0d66ff4cbffae8e1bda895
+Author: Srikrishna Iyer <sriyer@google.com>
+Date:   Mon Jul 25 17:45:56 2022 -0700
 
-    [entropy_src/rtl] add recov alert for FW_OV_WR error
+    [dv, waves] Improve wave dumping
     
-    A recoverable alert has been added in the case where the packer FIFO
-    has been written but was full at the time.
+    A minor non-functional change to waves.tcl improves the way
+    wavedumpScope method is used to dump specific hierarchies
+    rather than dump the tb top by default.
     
-    Signed-off-by: Mark Branstad <mark.branstad@wdc.com>
+    The code is rearranged a little, and several comments are added
+    to make the usage clearer. the `fid` argument of `wavedumpScope
+    proc is removed in favor of using the value internally by
+    referencing a global variable instead of passing it as an arg.
+    
+    Wavedumping has been tested with vcs:fsdb,vpd,vcd and
+    xcelium:shm,fsdb and everything seeems to be working ok.
+    
+    Signed-off-by: Srikrishna Iyer <sriyer@google.com>
 
 """
 
