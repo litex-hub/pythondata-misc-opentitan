5c754561e248d123abdb1ada5024a1ff9d8c449b
LiteX Robot <robot@timvideos.us>
2022-07-27 00:48:57 +0000

    Updating pythondata-misc-opentitan to 0.0.post13278
    Updated data to v0.0-13136-ga04d745693 based on a04d74569382e62eb720d66909c8dedc1f6aeb93 from https://github.com/lowRISC/opentitan.
    > commit a04d74569382e62eb720d66909c8dedc1f6aeb93
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Mon Jul 25 18:04:14 2022 -0700
    >
    >     [dv/kmac] Fix assertion failures in shadow reg
    >
    >     This PR disable a few assertions when shadow_reg error fires.
    >     Because the kmac will go to terminal state.
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,38 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post13276"
-version_tuple = (0, 0, 13276)
+version_str = "0.0.post13278"
+version_tuple = (0, 0, 13278)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post13276")
+    pversion = V("0.0.post13278")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post13134"
-data_version_tuple = (0, 0, 13134)
+data_version_str = "0.0.post13136"
+data_version_tuple = (0, 0, 13136)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post13134")
+    pdata_version = V("0.0.post13136")
 except ImportError:
     pass
-data_git_hash = "19a8e33764bac07e0653d1d8720be7ffd0a02dd8"
-data_git_describe = "v0.0-13134-g19a8e33764"
+data_git_hash = "a04d74569382e62eb720d66909c8dedc1f6aeb93"
+data_git_describe = "v0.0-13136-ga04d745693"
 data_git_msg = """\
-commit 19a8e33764bac07e0653d1d8720be7ffd0a02dd8
-Author: Timothy Trippel <ttrippel@google.com>
-Date:   Tue Jul 26 10:12:44 2022 -0700
+commit a04d74569382e62eb720d66909c8dedc1f6aeb93
+Author: Cindy Chen <chencindy@opentitan.org>
+Date:   Mon Jul 25 18:04:14 2022 -0700
 
-    [dvsim] remove unecessary `sw_build_dir` parameter
+    [dv/kmac] Fix assertion failures in shadow reg
     
-    In the previous commit, SW image string formats were update to be
-    referred to by the Bazel label. Part of this update also involved
-    copying Bazel-built SW images into the simulation `run_dir`, which
-    removed the need to track the `sw_build_dir`. Therefore, this commit
-    cleans up dvsim.py to remove this parameter.
+    This PR disable a few assertions when shadow_reg error fires.
+    Because the kmac will go to terminal state.
     
-    Signed-off-by: Timothy Trippel <ttrippel@google.com>
+    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
 """
 
