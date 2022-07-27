69df6f00280ff4fe340b06daabd709d9fba11747
LiteX Robot <robot@timvideos.us>
2022-07-27 03:17:18 +0000

    Updating pythondata-misc-opentitan to 0.0.post13281
    Updated data to v0.0-13139-g542ef5653b based on 542ef5653be7bcc1e1af4e01e75641c089475cf5 from https://github.com/lowRISC/opentitan.
    > commit 542ef5653be7bcc1e1af4e01e75641c089475cf5
    > Author: Michael Schaffner <msf@opentitan.org>
    > Date:   Fri Jul 15 10:15:32 2022 -0700
    >
    >     [alert_handler] Update checklist and move to D3
    >
    >     Fix #13576
    >
    >     Signed-off-by: Michael Schaffner <msf@opentitan.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post13278"
-version_tuple = (0, 0, 13278)
+version_str = "0.0.post13281"
+version_tuple = (0, 0, 13281)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post13278")
+    pversion = V("0.0.post13281")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post13136"
-data_version_tuple = (0, 0, 13136)
+data_version_str = "0.0.post13139"
+data_version_tuple = (0, 0, 13139)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post13136")
+    pdata_version = V("0.0.post13139")
 except ImportError:
     pass
-data_git_hash = "a04d74569382e62eb720d66909c8dedc1f6aeb93"
-data_git_describe = "v0.0-13136-ga04d745693"
+data_git_hash = "542ef5653be7bcc1e1af4e01e75641c089475cf5"
+data_git_describe = "v0.0-13139-g542ef5653b"
 data_git_msg = """\
-commit a04d74569382e62eb720d66909c8dedc1f6aeb93
-Author: Cindy Chen <chencindy@opentitan.org>
-Date:   Mon Jul 25 18:04:14 2022 -0700
+commit 542ef5653be7bcc1e1af4e01e75641c089475cf5
+Author: Michael Schaffner <msf@opentitan.org>
+Date:   Fri Jul 15 10:15:32 2022 -0700
 
-    [dv/kmac] Fix assertion failures in shadow reg
+    [alert_handler] Update checklist and move to D3
     
-    This PR disable a few assertions when shadow_reg error fires.
-    Because the kmac will go to terminal state.
+    Fix #13576
     
-    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
+    Signed-off-by: Michael Schaffner <msf@opentitan.org>
 
 """
 
