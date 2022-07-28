2e71363a909649cbe81f81966f2e1466bd5e6025
LiteX Robot <robot@timvideos.us>
2022-07-28 09:12:26 +0000

    Updating pythondata-misc-opentitan to 0.0.post13302
    Updated data to v0.0-13160-gb009abd2b9 based on b009abd2b9734e4b7cd9df0cc02282512f30cc5d from https://github.com/lowRISC/opentitan.
    > commit b009abd2b9734e4b7cd9df0cc02282512f30cc5d
    > Author: Pirmin Vogel <vogelpi@lowrisc.org>
    > Date:   Fri Jun 10 16:43:07 2022 +0200
    >
    >     [otbn,rtl] Remove reset from FF-based regfiles
    >
    >     This removes the reset from the flip-flop based general-purpose
    >     registers (GPRs and wide data registers (WDRs) as well as the `ACC` and
    >     the `MOD` register.  This improves security:  If a register holds a
    >     random mask to be applied to some other register (remember that the
    >     masking is implemented in SW here) and an attacker manages to reset that
    >     register while OTBN is running, the masking is effectively switched off.
    >
    >     Co-authored-by: Andreas Kurth <adk@lowrisc.org>
    >     Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,40 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post13300"
-version_tuple = (0, 0, 13300)
+version_str = "0.0.post13302"
+version_tuple = (0, 0, 13302)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post13300")
+    pversion = V("0.0.post13302")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post13158"
-data_version_tuple = (0, 0, 13158)
+data_version_str = "0.0.post13160"
+data_version_tuple = (0, 0, 13160)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post13158")
+    pdata_version = V("0.0.post13160")
 except ImportError:
     pass
-data_git_hash = "b1f6dddfdf29165e8c5f62c1a630e8b88025db6d"
-data_git_describe = "v0.0-13158-gb1f6dddfdf"
+data_git_hash = "b009abd2b9734e4b7cd9df0cc02282512f30cc5d"
+data_git_describe = "v0.0-13160-gb009abd2b9"
 data_git_msg = """\
-commit b1f6dddfdf29165e8c5f62c1a630e8b88025db6d
-Author: Timothy Chen <timothytim@google.com>
-Date:   Wed Jul 27 16:47:38 2022 -0700
+commit b009abd2b9734e4b7cd9df0cc02282512f30cc5d
+Author: Pirmin Vogel <vogelpi@lowrisc.org>
+Date:   Fri Jun 10 16:43:07 2022 +0200
 
-    [dv/top] Correct test name in sim config
+    [otbn,rtl] Remove reset from FF-based regfiles
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    This removes the reset from the flip-flop based general-purpose
+    registers (GPRs and wide data registers (WDRs) as well as the `ACC` and
+    the `MOD` register.  This improves security:  If a register holds a
+    random mask to be applied to some other register (remember that the
+    masking is implemented in SW here) and an attacker manages to reset that
+    register while OTBN is running, the masking is effectively switched off.
+    
+    Co-authored-by: Andreas Kurth <adk@lowrisc.org>
+    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
 
 """
 
