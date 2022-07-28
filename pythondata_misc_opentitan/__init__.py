f087f1fdbe732ac4cfbdaeb0836a6588b0c4d3bb
LiteX Robot <robot@timvideos.us>
2022-07-28 12:22:01 +0000

    Updating pythondata-misc-opentitan to 0.0.post13307
    Updated data to v0.0-13165-g09115123e5 based on 09115123e5481699ef91c490e9206161290540cc from https://github.com/lowRISC/opentitan.
    > commit 09115123e5481699ef91c490e9206161290540cc
    > Author: Pirmin Vogel <vogelpi@lowrisc.org>
    > Date:   Mon Jul 18 12:09:56 2022 +0200
    >
    >     [top] Daisy chaining of LC RMA req/ack interface for Flash and OTBN
    >
    >     It has been decided that in addition to Flash also some non-reset
    >     registers inside OTBN should be securely wiped before LC can enter RMA.
    >     Since LC is already in D3, meaning we want to avoid changes to LC if
    >     possible, it has been decided to daisy chain the LC RMA req/ack
    >     interface. More precisely, LC will send the REQ to Flash as before,
    >     but Flash will send the ACK to the REQ input of OTBN, and OTBNs ACK is
    >     then connected to the LC ACK.
    >
    >     Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,38 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post13305"
-version_tuple = (0, 0, 13305)
+version_str = "0.0.post13307"
+version_tuple = (0, 0, 13307)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post13305")
+    pversion = V("0.0.post13307")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post13163"
-data_version_tuple = (0, 0, 13163)
+data_version_str = "0.0.post13165"
+data_version_tuple = (0, 0, 13165)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post13163")
+    pdata_version = V("0.0.post13165")
 except ImportError:
     pass
-data_git_hash = "51bbfc42375d721bea21728f88f49de5f3952f63"
-data_git_describe = "v0.0-13163-g51bbfc4237"
+data_git_hash = "09115123e5481699ef91c490e9206161290540cc"
+data_git_describe = "v0.0-13165-g09115123e5"
 data_git_msg = """\
-commit 51bbfc42375d721bea21728f88f49de5f3952f63
+commit 09115123e5481699ef91c490e9206161290540cc
 Author: Pirmin Vogel <vogelpi@lowrisc.org>
-Date:   Mon Jul 25 13:09:19 2022 +0200
+Date:   Mon Jul 18 12:09:56 2022 +0200
 
-    [kmac/doc] Clarify purpose of entropy mode configuration values
+    [top] Daisy chaining of LC RMA req/ack interface for Flash and OTBN
     
-    The idle_mode should really only be used for the very initial ROM_CTRL
-    phase. After that software should swtich to either sw_mode or edn_mode
-    if the entropy complex is available and importantly also set
-    entropy_ready.
+    It has been decided that in addition to Flash also some non-reset
+    registers inside OTBN should be securely wiped before LC can enter RMA.
+    Since LC is already in D3, meaning we want to avoid changes to LC if
+    possible, it has been decided to daisy chain the LC RMA req/ack
+    interface. More precisely, LC will send the REQ to Flash as before,
+    but Flash will send the ACK to the REQ input of OTBN, and OTBNs ACK is
+    then connected to the LC ACK.
     
     Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
 
