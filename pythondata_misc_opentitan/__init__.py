ba8a286203b5d27e33679e66743bda70b1b251a9
LiteX Robot <robot@timvideos.us>
2022-07-28 23:14:01 +0000

    Updating pythondata-misc-opentitan to 0.0.post13322
    Updated data to v0.0-13180-gc1a7954749 based on c1a79547494105265e2d04457a36659ab6142d62 from https://github.com/lowRISC/opentitan.
    > commit c1a79547494105265e2d04457a36659ab6142d62
    > Author: Guillermo Maturana <maturana@google.com>
    > Date:   Tue Jul 26 17:43:23 2022 -0700
    >
    >     [utils,dvsim] Add wall-clock timeout feature
    >
    >     Add a timeout for builds and runs. Set it in Modes, and enable overriding it
    >     via dvsim arguments.
    >
    >     Signed-off-by: Guillermo Maturana <maturana@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post13321"
-version_tuple = (0, 0, 13321)
+version_str = "0.0.post13322"
+version_tuple = (0, 0, 13322)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post13321")
+    pversion = V("0.0.post13322")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post13179"
-data_version_tuple = (0, 0, 13179)
+data_version_str = "0.0.post13180"
+data_version_tuple = (0, 0, 13180)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post13179")
+    pdata_version = V("0.0.post13180")
 except ImportError:
     pass
-data_git_hash = "0ca714ff4e77b12c0c74e94b7f13ed78b6127121"
-data_git_describe = "v0.0-13179-g0ca714ff4e"
+data_git_hash = "c1a79547494105265e2d04457a36659ab6142d62"
+data_git_describe = "v0.0-13180-gc1a7954749"
 data_git_msg = """\
-commit 0ca714ff4e77b12c0c74e94b7f13ed78b6127121
-Author: Timothy Chen <timothytim@google.com>
-Date:   Wed Jul 27 16:02:50 2022 -0700
+commit c1a79547494105265e2d04457a36659ab6142d62
+Author: Guillermo Maturana <maturana@google.com>
+Date:   Tue Jul 26 17:43:23 2022 -0700
 
-    [sw/flash_ctrl] remove software mention of flash macro error
+    [utils,dvsim] Add wall-clock timeout feature
     
-    - flash macro error is no longer part of the recoverable error
-      codes. Make corresponding changes in the dif and testutils
+    Add a timeout for builds and runs. Set it in Modes, and enable overriding it
+    via dvsim arguments.
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Guillermo Maturana <maturana@google.com>
 
 """
 
