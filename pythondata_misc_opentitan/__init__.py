4553792be04d6b6c4ec8eb7d946b6fd02c3549f9
LiteX Robot <robot@timvideos.us>
2022-07-29 11:11:33 +0000

    Updating pythondata-misc-opentitan to 0.0.post13329
    Updated data to v0.0-13187-g18563abecc based on 18563abecc18c4802476d48df61d8c477eb42ad1 from https://github.com/lowRISC/opentitan.
    > commit 18563abecc18c4802476d48df61d8c477eb42ad1
    > Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
    > Date:   Tue Feb 22 23:08:35 2022 +0000
    >
    >     [otbn] Update bus accessible size to 3kiB
    >
    >     After some discussion, this looks like the safest approach: the
    >     1kiB scratchpad should still be big enough to hold any key data but
    >     we've got a bit more headroom on the bus accessible part.
    >
    >     To make the RSA routines work with the reduced scratchpad, we need to
    >     reduce the software buffers accordingly:
    >     - m0d occupies always 32 bytes.
    >     - RR occupies N*32 bytes where N is the number of 256 limbs per bignum
    >       which is 16 for RSA-4096. This means RR needs at most 512 bytes.
    >     - work_buf occupies the remaining 480 bytes of the scratchpad.
    >
    >     Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    >     Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,44 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post13326"
-version_tuple = (0, 0, 13326)
+version_str = "0.0.post13329"
+version_tuple = (0, 0, 13329)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post13326")
+    pversion = V("0.0.post13329")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post13184"
-data_version_tuple = (0, 0, 13184)
+data_version_str = "0.0.post13187"
+data_version_tuple = (0, 0, 13187)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post13184")
+    pdata_version = V("0.0.post13187")
 except ImportError:
     pass
-data_git_hash = "3db58a50e3b8739cfde242bbf29079a4707c888d"
-data_git_describe = "v0.0-13184-g3db58a50e3"
+data_git_hash = "18563abecc18c4802476d48df61d8c477eb42ad1"
+data_git_describe = "v0.0-13187-g18563abecc"
 data_git_msg = """\
-commit 3db58a50e3b8739cfde242bbf29079a4707c888d
-Author: Alexander Williams <awill@google.com>
-Date:   Mon Jul 25 09:28:10 2022 -0700
+commit 18563abecc18c4802476d48df61d8c477eb42ad1
+Author: Rupert Swarbrick <rswarbrick@lowrisc.org>
+Date:   Tue Feb 22 23:08:35 2022 +0000
 
-    [fpga/cw310] Add hyperdebug variant of the CW310
+    [otbn] Update bus accessible size to 3kiB
     
-    Add a new fusesoc core that switches in a hyperdebug-specific XDC for a
-    CW310 bitstream build.
+    After some discussion, this looks like the safest approach: the
+    1kiB scratchpad should still be big enough to hold any key data but
+    we've got a bit more headroom on the bus accessible part.
     
-    Signed-off-by: Alexander Williams <awill@google.com>
+    To make the RSA routines work with the reduced scratchpad, we need to
+    reduce the software buffers accordingly:
+    - m0d occupies always 32 bytes.
+    - RR occupies N*32 bytes where N is the number of 256 limbs per bignum
+      which is 16 for RSA-4096. This means RR needs at most 512 bytes.
+    - work_buf occupies the remaining 480 bytes of the scratchpad.
+    
+    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
+    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
 
 """
 
