065174d270698a3e8353e29d5a0f94ef4b82a805
LiteX Robot <robot@timvideos.us>
2022-09-11 23:14:38 +0000

    Updating pythondata-misc-opentitan to 0.0.post14116
    Updated data to v0.0-13974-g3c54b1eb22 based on 3c54b1eb225f685fe528a88992a1b8de3e30cd4b from https://github.com/lowRISC/opentitan.
    > commit 3c54b1eb225f685fe528a88992a1b8de3e30cd4b
    > Author: Srikrishna Iyer <sriyer@google.com>
    > Date:   Sat Sep 10 01:01:26 2022 -0700
    >
    >     [util] Fix extract_sw_logs.py
    >
    >     Fixes #14636.
    >     Fixes issue encountered in #14858.
    >
    >     Rather than being told which ELF sections to parse and generate a database
    >     from, the script now reads ALL valid sections from the ELF and creates a
    >     database of address and string found at that address. This eliminates the
    >     need to pass sections using the `--rodata-sections` arg in the bazel rules.
    >     This takes care of string constants that reside in other sections (for
    >     example, the `chip_info` which resides in its own `.chip_info` section).
    >
    >     The other bug was in the `get_str_at_addr()` method, which returns
    >     a string (or substring) starting at a particular address. This computation
    >     requires the original length of the string to be known. Since the strings
    >     go through sanitization, the orignal length may change, leading to errors
    >     thrown when printing single whitespace characters, such as
    >     `LOG_INFO("\n")`. The change records the original length of the string
    >     along with the string (as a tuple) at each address.
    >
    >     Signed-off-by: Srikrishna Iyer <sriyer@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,50 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14115"
-version_tuple = (0, 0, 14115)
+version_str = "0.0.post14116"
+version_tuple = (0, 0, 14116)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14115")
+    pversion = V("0.0.post14116")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post13973"
-data_version_tuple = (0, 0, 13973)
+data_version_str = "0.0.post13974"
+data_version_tuple = (0, 0, 13974)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post13973")
+    pdata_version = V("0.0.post13974")
 except ImportError:
     pass
-data_git_hash = "c4d76f7d41a4072a5d0a8e1da5634af66209a4b3"
-data_git_describe = "v0.0-13973-gc4d76f7d41"
+data_git_hash = "3c54b1eb225f685fe528a88992a1b8de3e30cd4b"
+data_git_describe = "v0.0-13974-g3c54b1eb22"
 data_git_msg = """\
-commit c4d76f7d41a4072a5d0a8e1da5634af66209a4b3
-Author: Jaedon Kim <jdonjdon@google.com>
-Date:   Tue Sep 6 17:21:29 2022 +0000
+commit 3c54b1eb225f685fe528a88992a1b8de3e30cd4b
+Author: Srikrishna Iyer <sriyer@google.com>
+Date:   Sat Sep 10 01:01:26 2022 -0700
 
-    [chip,dv,i2c] add loopback mode to i2c agent
+    [util] Fix extract_sw_logs.py
     
-    Signed-off-by: Jaedon Kim <jdonjdon@google.com>
+    Fixes #14636.
+    Fixes issue encountered in #14858.
+    
+    Rather than being told which ELF sections to parse and generate a database
+    from, the script now reads ALL valid sections from the ELF and creates a
+    database of address and string found at that address. This eliminates the
+    need to pass sections using the `--rodata-sections` arg in the bazel rules.
+    This takes care of string constants that reside in other sections (for
+    example, the `chip_info` which resides in its own `.chip_info` section).
+    
+    The other bug was in the `get_str_at_addr()` method, which returns
+    a string (or substring) starting at a particular address. This computation
+    requires the original length of the string to be known. Since the strings
+    go through sanitization, the orignal length may change, leading to errors
+    thrown when printing single whitespace characters, such as
+    `LOG_INFO("\n")`. The change records the original length of the string
+    along with the string (as a tuple) at each address.
+    
+    Signed-off-by: Srikrishna Iyer <sriyer@google.com>
 
 """
 
