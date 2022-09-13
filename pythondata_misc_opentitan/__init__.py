4c8079d9a141d85f29a2b9a305d485b500917e7e
LiteX Robot <robot@timvideos.us>
2022-09-13 10:16:32 +0000

    Updating pythondata-misc-opentitan to 0.0.post14170
    Updated data to v0.0-14028-g6f12fbe44f based on 6f12fbe44f45962fff8c6ef5c1a43e88a65ba650 from https://github.com/lowRISC/opentitan.
    > commit 6f12fbe44f45962fff8c6ef5c1a43e88a65ba650
    > Author: Alphan Ulusoy <alphan@google.com>
    > Date:   Mon Sep 12 13:55:16 2022 -0400
    >
    >     [opentitantool] cargo raze --generate-lockfile
    >
    >     Signed-off-by: Alphan Ulusoy <alphan@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14163"
-version_tuple = (0, 0, 14163)
+version_str = "0.0.post14170"
+version_tuple = (0, 0, 14170)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14163")
+    pversion = V("0.0.post14170")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14021"
-data_version_tuple = (0, 0, 14021)
+data_version_str = "0.0.post14028"
+data_version_tuple = (0, 0, 14028)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14021")
+    pdata_version = V("0.0.post14028")
 except ImportError:
     pass
-data_git_hash = "85057ad934a7e13fd28ed4f142fc943259f328a6"
-data_git_describe = "v0.0-14021-g85057ad934"
+data_git_hash = "6f12fbe44f45962fff8c6ef5c1a43e88a65ba650"
+data_git_describe = "v0.0-14028-g6f12fbe44f"
 data_git_msg = """\
-commit 85057ad934a7e13fd28ed4f142fc943259f328a6
-Author: Prajwala Puttappa <prajwalaputtappa@lowrisc.org>
-Date:   Tue Aug 30 10:12:52 2022 +0100
+commit 6f12fbe44f45962fff8c6ef5c1a43e88a65ba650
+Author: Alphan Ulusoy <alphan@google.com>
+Date:   Mon Sep 12 13:55:16 2022 -0400
 
-    [otbn, dv] Added a testcase to verify *_STACK.ADDR.INTEGRITY
+    [opentitantool] cargo raze --generate-lockfile
     
-    This commit adds a new testcase called otbn_stack_addr_integ_chk to
-    verify OTBN.CALL_STACK.ADDR.INTEGRITY and OTBN.LOOP_STACK.ADDR.INTEGRITY
-    countermeasures.
-    
-    Signed-off-by: Prajwala Puttappa <prajwalaputtappa@lowrisc.org>
+    Signed-off-by: Alphan Ulusoy <alphan@google.com>
 
 """
 
