af632371100222ae7485fe4ab455c9e372bcdbd5
LiteX Robot <robot@timvideos.us>
2022-09-13 19:13:28 +0000

    Updating pythondata-misc-opentitan to 0.0.post14175
    Updated data to v0.0-14033-g1fb830b76a based on 1fb830b76ab75e1489857a67e7bf340fe493b23d from https://github.com/lowRISC/opentitan.
    > commit 1fb830b76ab75e1489857a67e7bf340fe493b23d
    > Author: Sharon Topaz <sharon.topaz@nuvoton.com>
    > Date:   Thu Jun 3 22:22:56 2021 +0300
    >
    >     SunGrid launcher support
    >
    >     Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
    >
    >     lint fixes
    >
    >     Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
    >
    >     fix lint issues
    >
    >     Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
    >
    >     add open source header
    >
    >     Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
    >
    >     update message in SGE.py and a comment in SgeLauncher.py
    >
    >     Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
    >
    >     closing the log file
    >
    >     Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
    >
    >     update SGE lower case and strings
    >
    >     Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
    >
    >     update SGE PR
    >
    >     Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
    >
    >     Resolving comments
    >
    >     Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
    >
    >     Fix lint issues
    >
    >     Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
    >
    >     Fix SGE lint issue
    >
    >     Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
    >
    >     Fix SGE lint indent issue
    >
    >     Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
    >
    >     Fix SGE lint indent issue2
    >
    >     Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
    >
    >     Fix SGE lint indent issue3
    >
    >     Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
    >
    >     Fix SGE comments 3-Aug
    >
    >     Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
    >
    >     Fix SgeLauncher
    >
    >     Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
    >
    >     Resolve SGE PR comments
    >
    >     Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
    >
    >     Remove trailing spaces
    >
    >     Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
    >
    >     Fix lint issues
    >
    >     Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
    >
    >     Fix latest comments
    >
    >     Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
    >
    >     Update exception description
    >
    >     Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,112 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14174"
-version_tuple = (0, 0, 14174)
+version_str = "0.0.post14175"
+version_tuple = (0, 0, 14175)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14174")
+    pversion = V("0.0.post14175")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14032"
-data_version_tuple = (0, 0, 14032)
+data_version_str = "0.0.post14033"
+data_version_tuple = (0, 0, 14033)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14032")
+    pdata_version = V("0.0.post14033")
 except ImportError:
     pass
-data_git_hash = "b1cc27a98e2c21dbe93aceca6f1c34e0da852ac7"
-data_git_describe = "v0.0-14032-gb1cc27a98e"
+data_git_hash = "1fb830b76ab75e1489857a67e7bf340fe493b23d"
+data_git_describe = "v0.0-14033-g1fb830b76a"
 data_git_msg = """\
-commit b1cc27a98e2c21dbe93aceca6f1c34e0da852ac7
-Author: Canberk Topal <ctopal@lowrisc.org>
-Date:   Tue Sep 13 14:26:06 2022 +0100
+commit 1fb830b76ab75e1489857a67e7bf340fe493b23d
+Author: Sharon Topaz <sharon.topaz@nuvoton.com>
+Date:   Thu Jun 3 22:22:56 2021 +0300
 
-    [otbn,dv] Fix OTBN block level tests for xlm
+    SunGrid launcher support
     
-    Signed-off-by: Canberk Topal <ctopal@lowrisc.org>
+    Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
+    
+    lint fixes
+    
+    Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
+    
+    fix lint issues
+    
+    Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
+    
+    add open source header
+    
+    Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
+    
+    update message in SGE.py and a comment in SgeLauncher.py
+    
+    Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
+    
+    closing the log file
+    
+    Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
+    
+    update SGE lower case and strings
+    
+    Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
+    
+    update SGE PR
+    
+    Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
+    
+    Resolving comments
+    
+    Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
+    
+    Fix lint issues
+    
+    Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
+    
+    Fix SGE lint issue
+    
+    Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
+    
+    Fix SGE lint indent issue
+    
+    Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
+    
+    Fix SGE lint indent issue2
+    
+    Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
+    
+    Fix SGE lint indent issue3
+    
+    Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
+    
+    Fix SGE comments 3-Aug
+    
+    Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
+    
+    Fix SgeLauncher
+    
+    Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
+    
+    Resolve SGE PR comments
+    
+    Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
+    
+    Remove trailing spaces
+    
+    Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
+    
+    Fix lint issues
+    
+    Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
+    
+    Fix latest comments
+    
+    Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
+    
+    Update exception description
+    
+    Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
 
 """
 
