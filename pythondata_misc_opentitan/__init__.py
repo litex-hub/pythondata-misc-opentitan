06ea70a27a52283f15a2834a2bab6117302e0051
LiteX Robot <robot@timvideos.us>
2022-09-14 20:17:33 +0000

    Updating pythondata-misc-opentitan to 0.0.post14199
    Updated data to v0.0-14057-ga6bc82ee68 based on a6bc82ee68ded1144c0dcf637a67e12eeec00294 from https://github.com/lowRISC/opentitan.
    > commit a6bc82ee68ded1144c0dcf637a67e12eeec00294
    > Author: Timothy Trippel <ttrippel@google.com>
    > Date:   Wed Sep 14 11:58:30 2022 -0700
    >
    >     [bazel] fix airgapped directory generation script
    >
    >     Turns out #14860 requires an update to this script.
    >
    >     Signed-off-by: Timothy Trippel <ttrippel@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,42 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14195"
-version_tuple = (0, 0, 14195)
+version_str = "0.0.post14199"
+version_tuple = (0, 0, 14199)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14195")
+    pversion = V("0.0.post14199")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14053"
-data_version_tuple = (0, 0, 14053)
+data_version_str = "0.0.post14057"
+data_version_tuple = (0, 0, 14057)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14053")
+    pdata_version = V("0.0.post14057")
 except ImportError:
     pass
-data_git_hash = "85600eec69b9c79423fbb93a3b186f3e953ca729"
-data_git_describe = "v0.0-14053-g85600eec69"
+data_git_hash = "a6bc82ee68ded1144c0dcf637a67e12eeec00294"
+data_git_describe = "v0.0-14057-ga6bc82ee68"
 data_git_msg = """\
-commit 85600eec69b9c79423fbb93a3b186f3e953ca729
-Author: Cindy Chen <chencindy@opentitan.org>
-Date:   Tue Sep 13 16:24:15 2022 -0700
+commit a6bc82ee68ded1144c0dcf637a67e12eeec00294
+Author: Timothy Trippel <ttrippel@google.com>
+Date:   Wed Sep 14 11:58:30 2022 -0700
 
-    [dv/otp_ctrl] Sec_cm test update
+    [bazel] fix airgapped directory generation script
     
-    This PR update otp_ctrl_sec_cm test after design moves prim_otp alerts
-    to the OTP_CTRL standard wrappers.
-    With this movement, we need to slightly change the testbench because:
-    1). prim_otp module output to a separate alerts.
-    2). prim_otp module's alert only bypass OTP_CTRL, it does not create any
-      actual error.
-    3). tl_integrity error coming from prim_otp module will only bypass
-      OTP_CTRL as well.
-    4). LC_gate module will output to a different alert output.
+    Turns out #14860 requires an update to this script.
     
-    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
+    Signed-off-by: Timothy Trippel <ttrippel@google.com>
 
 """
 
