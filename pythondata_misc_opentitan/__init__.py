1a6c53b7cb0fe3eed60044cb28e17962e9948872
LiteX Robot <robot@timvideos.us>
2022-09-14 19:14:44 +0000

    Updating pythondata-misc-opentitan to 0.0.post14195
    Updated data to v0.0-14053-g85600eec69 based on 85600eec69b9c79423fbb93a3b186f3e953ca729 from https://github.com/lowRISC/opentitan.
    > commit 85600eec69b9c79423fbb93a3b186f3e953ca729
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Tue Sep 13 16:24:15 2022 -0700
    >
    >     [dv/otp_ctrl] Sec_cm test update
    >
    >     This PR update otp_ctrl_sec_cm test after design moves prim_otp alerts
    >     to the OTP_CTRL standard wrappers.
    >     With this movement, we need to slightly change the testbench because:
    >     1). prim_otp module output to a separate alerts.
    >     2). prim_otp module's alert only bypass OTP_CTRL, it does not create any
    >       actual error.
    >     3). tl_integrity error coming from prim_otp module will only bypass
    >       OTP_CTRL as well.
    >     4). LC_gate module will output to a different alert output.
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,42 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14194"
-version_tuple = (0, 0, 14194)
+version_str = "0.0.post14195"
+version_tuple = (0, 0, 14195)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14194")
+    pversion = V("0.0.post14195")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14052"
-data_version_tuple = (0, 0, 14052)
+data_version_str = "0.0.post14053"
+data_version_tuple = (0, 0, 14053)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14052")
+    pdata_version = V("0.0.post14053")
 except ImportError:
     pass
-data_git_hash = "d4641339ed703904120639d1504804fb725535ce"
-data_git_describe = "v0.0-14052-gd4641339ed"
+data_git_hash = "85600eec69b9c79423fbb93a3b186f3e953ca729"
+data_git_describe = "v0.0-14053-g85600eec69"
 data_git_msg = """\
-commit d4641339ed703904120639d1504804fb725535ce
-Author: Jaedon Kim <jdonjdon@google.com>
-Date:   Wed Sep 14 05:46:30 2022 +0000
+commit 85600eec69b9c79423fbb93a3b186f3e953ca729
+Author: Cindy Chen <chencindy@opentitan.org>
+Date:   Tue Sep 13 16:24:15 2022 -0700
 
-    [flash_ctrl/dv] Sign off V2
+    [dv/otp_ctrl] Sec_cm test update
     
-    Signed-off-by: Jaedon Kim <jdonjdon@google.com>
+    This PR update otp_ctrl_sec_cm test after design moves prim_otp alerts
+    to the OTP_CTRL standard wrappers.
+    With this movement, we need to slightly change the testbench because:
+    1). prim_otp module output to a separate alerts.
+    2). prim_otp module's alert only bypass OTP_CTRL, it does not create any
+      actual error.
+    3). tl_integrity error coming from prim_otp module will only bypass
+      OTP_CTRL as well.
+    4). LC_gate module will output to a different alert output.
+    
+    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
 """
 
