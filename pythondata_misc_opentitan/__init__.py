0247d7109f0e77ed7bd75cccd9881fb24a586ce3
LiteX Robot <robot@timvideos.us>
2022-09-14 23:16:02 +0000

    Updating pythondata-misc-opentitan to 0.0.post14204
    Updated data to v0.0-14062-g228f0d4cea based on 228f0d4ceac5399086de337a122b89011556b776 from https://github.com/lowRISC/opentitan.
    > commit 228f0d4ceac5399086de337a122b89011556b776
    > Author: Miguel Osorio <miguelosorio@google.com>
    > Date:   Wed Sep 14 13:48:35 2022 -0700
    >
    >     [top-test] Increase reverse ping test timeout
    >
    >     Increase chip_sw_alert_handler_reverse_ping_in_deep_sleep test timeout
    >     to 0.300s. The previous calculations didn't take into account the time
    >     needed to increase the counters before going into deep sleep.
    >
    >     Signed-off-by: Miguel Osorio <miguelosorio@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14203"
-version_tuple = (0, 0, 14203)
+version_str = "0.0.post14204"
+version_tuple = (0, 0, 14204)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14203")
+    pversion = V("0.0.post14204")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14061"
-data_version_tuple = (0, 0, 14061)
+data_version_str = "0.0.post14062"
+data_version_tuple = (0, 0, 14062)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14061")
+    pdata_version = V("0.0.post14062")
 except ImportError:
     pass
-data_git_hash = "18419550510bc8327550cfb9a49beacbb66c535b"
-data_git_describe = "v0.0-14061-g1841955051"
+data_git_hash = "228f0d4ceac5399086de337a122b89011556b776"
+data_git_describe = "v0.0-14062-g228f0d4cea"
 data_git_msg = """\
-commit 18419550510bc8327550cfb9a49beacbb66c535b
-Author: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
-Date:   Sat Feb 19 17:19:53 2022 -0800
+commit 228f0d4ceac5399086de337a122b89011556b776
+Author: Miguel Osorio <miguelosorio@google.com>
+Date:   Wed Sep 14 13:48:35 2022 -0700
 
-    [entropy_src/rtl] Standardize Ext. Health Test Interface
+    [top-test] Increase reverse ping test timeout
     
-    In preparation for creating an agent for the external health test,
-    this commit updates the extht interface to make it more consistent
-    with the current implementation of the other health checks.
+    Increase chip_sw_alert_handler_reverse_ping_in_deep_sleep test timeout
+    to 0.300s. The previous calculations didn't take into account the time
+    needed to increase the counters before going into deep sleep.
     
-    Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
+    Signed-off-by: Miguel Osorio <miguelosorio@google.com>
 
 """
 
