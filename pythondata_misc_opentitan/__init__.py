fb76ffc3b1738a06465bc63b52a2f5dc2b7b80a2
LiteX Robot <robot@timvideos.us>
2022-09-15 23:17:57 +0000

    Updating pythondata-misc-opentitan to 0.0.post14247
    Updated data to v0.0-14105-gad0efd0714 based on ad0efd0714daf9b33ca1e1750f71e6978d12f519 from https://github.com/lowRISC/opentitan.
    > commit ad0efd0714daf9b33ca1e1750f71e6978d12f519
    > Author: Timothy Trippel <ttrippel@google.com>
    > Date:   Wed Sep 14 23:09:05 2022 -0700
    >
    >     [python] pin package version numbers
    >
    >     Having unpinned package versions reduces reproducibility and was causing
    >     unncessary Bazel airgapped image updates.
    >
    >     This fixes #14939.
    >
    >     Signed-off-by: Timothy Trippel <ttrippel@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14244"
-version_tuple = (0, 0, 14244)
+version_str = "0.0.post14247"
+version_tuple = (0, 0, 14247)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14244")
+    pversion = V("0.0.post14247")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14102"
-data_version_tuple = (0, 0, 14102)
+data_version_str = "0.0.post14105"
+data_version_tuple = (0, 0, 14105)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14102")
+    pdata_version = V("0.0.post14105")
 except ImportError:
     pass
-data_git_hash = "2abd786c664c619dab8a2538c479b3dcf03d9d54"
-data_git_describe = "v0.0-14102-g2abd786c66"
+data_git_hash = "ad0efd0714daf9b33ca1e1750f71e6978d12f519"
+data_git_describe = "v0.0-14105-gad0efd0714"
 data_git_msg = """\
-commit 2abd786c664c619dab8a2538c479b3dcf03d9d54
+commit ad0efd0714daf9b33ca1e1750f71e6978d12f519
 Author: Timothy Trippel <ttrippel@google.com>
-Date:   Wed Sep 14 19:13:19 2022 -0700
+Date:   Wed Sep 14 23:09:05 2022 -0700
 
-    [bazel] reduce number of bitstreams included in the airgapped image
+    [python] pin package version numbers
     
-    The airgapped image requires an "initialized" bistreams cache in order
-    for the bitstream cache "offline" mode to properly function. This
-    reduces the number of bistreams included to just one to reduce the size
-    fot he airgapped images.
+    Having unpinned package versions reduces reproducibility and was causing
+    unncessary Bazel airgapped image updates.
+    
+    This fixes #14939.
     
     Signed-off-by: Timothy Trippel <ttrippel@google.com>
 
