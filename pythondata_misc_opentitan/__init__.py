4323a9ffd3fa1b5bddd91d37508030ce3272ea61
LiteX Robot <robot@timvideos.us>
2022-09-15 06:40:33 +0000

    Updating pythondata-misc-opentitan to 0.0.post14222
    Updated data to v0.0-14080-g7200ca3677 based on 7200ca3677141d0f3c6d9a341c9bcf2d97685d58 from https://github.com/lowRISC/opentitan.
    > commit 7200ca3677141d0f3c6d9a341c9bcf2d97685d58
    > Author: Alphan Ulusoy <alphan@google.com>
    > Date:   Tue Sep 6 15:39:15 2022 -0400
    >
    >     [bazel] Add bazel rules for manual tests
    >
    >     Signed-off-by: Alphan Ulusoy <alphan@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14218"
-version_tuple = (0, 0, 14218)
+version_str = "0.0.post14222"
+version_tuple = (0, 0, 14222)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14218")
+    pversion = V("0.0.post14222")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14076"
-data_version_tuple = (0, 0, 14076)
+data_version_str = "0.0.post14080"
+data_version_tuple = (0, 0, 14080)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14076")
+    pdata_version = V("0.0.post14080")
 except ImportError:
     pass
-data_git_hash = "490b9d86df769ffb6d0efd8e637904c1a7aa792c"
-data_git_describe = "v0.0-14076-g490b9d86df"
+data_git_hash = "7200ca3677141d0f3c6d9a341c9bcf2d97685d58"
+data_git_describe = "v0.0-14080-g7200ca3677"
 data_git_msg = """\
-commit 490b9d86df769ffb6d0efd8e637904c1a7aa792c
-Author: Timothy Trippel <ttrippel@google.com>
-Date:   Wed Sep 14 17:59:32 2022 -0700
+commit 7200ca3677141d0f3c6d9a341c9bcf2d97685d58
+Author: Alphan Ulusoy <alphan@google.com>
+Date:   Tue Sep 6 15:39:15 2022 -0400
 
-    [bazel] rollback #14924 and #14927
+    [bazel] Add bazel rules for manual tests
     
-    Since #14860 was rolled back, #14924 and #14927 should also be rolled
-    back since they were fixes in response to #14860 to begin with.
-    
-    Signed-off-by: Timothy Trippel <ttrippel@google.com>
+    Signed-off-by: Alphan Ulusoy <alphan@google.com>
 
 """
 
