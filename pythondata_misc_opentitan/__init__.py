91fa50f428c321f282e5e3bd782b754eff8a1f96
LiteX Robot <robot@timvideos.us>
2022-09-15 03:49:12 +0000

    Updating pythondata-misc-opentitan to 0.0.post14218
    Updated data to v0.0-14076-g490b9d86df based on 490b9d86df769ffb6d0efd8e637904c1a7aa792c from https://github.com/lowRISC/opentitan.
    > commit 490b9d86df769ffb6d0efd8e637904c1a7aa792c
    > Author: Timothy Trippel <ttrippel@google.com>
    > Date:   Wed Sep 14 17:59:32 2022 -0700
    >
    >     [bazel] rollback #14924 and #14927
    >
    >     Since #14860 was rolled back, #14924 and #14927 should also be rolled
    >     back since they were fixes in response to #14860 to begin with.
    >
    >     Signed-off-by: Timothy Trippel <ttrippel@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,33 +4,33 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14209"
-version_tuple = (0, 0, 14209)
+version_str = "0.0.post14218"
+version_tuple = (0, 0, 14218)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14209")
+    pversion = V("0.0.post14218")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14067"
-data_version_tuple = (0, 0, 14067)
+data_version_str = "0.0.post14076"
+data_version_tuple = (0, 0, 14076)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14067")
+    pdata_version = V("0.0.post14076")
 except ImportError:
     pass
-data_git_hash = "28f79af3c25d17cee70597881b6b8ba0fa0fc434"
-data_git_describe = "v0.0-14067-g28f79af3c2"
+data_git_hash = "490b9d86df769ffb6d0efd8e637904c1a7aa792c"
+data_git_describe = "v0.0-14076-g490b9d86df"
 data_git_msg = """\
-commit 28f79af3c25d17cee70597881b6b8ba0fa0fc434
+commit 490b9d86df769ffb6d0efd8e637904c1a7aa792c
 Author: Timothy Trippel <ttrippel@google.com>
-Date:   Wed Sep 14 16:36:53 2022 -0700
+Date:   Wed Sep 14 17:59:32 2022 -0700
 
-    [bazel,ottool] rollback #14860
+    [bazel] rollback #14924 and #14927
     
-    PR #14860 is causing issues in the airgapped environment so temporarily
-    rolling back.
+    Since #14860 was rolled back, #14924 and #14927 should also be rolled
+    back since they were fixes in response to #14860 to begin with.
     
     Signed-off-by: Timothy Trippel <ttrippel@google.com>
 
