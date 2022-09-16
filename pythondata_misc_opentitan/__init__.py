14fa246e2ae0ba595937a66c7eaa8657fbc08478
LiteX Robot <robot@timvideos.us>
2022-09-16 17:34:22 +0000

    Updating pythondata-misc-opentitan to 0.0.post14260
    Updated data to v0.0-14118-g476fd52158 based on 476fd521585a806fcbce21a41e6e5dded2207c4b from https://github.com/lowRISC/opentitan.
    > commit 476fd521585a806fcbce21a41e6e5dded2207c4b
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Thu Sep 15 16:39:47 2022 -0700
    >
    >     [util/topgen] Update alert lpg generation
    >
    >     - fixes #14954
    >
    >     The alert lpg generation currently uses the clock group, clock domain
    >     and power domain of a particular clock to uniquely identify lpg groups.
    >
    >     As a result, it lumps all the transactional clocks of the same domains
    >     into one group.  This behavior however is not correct, as the
    >     transactional clocks are unique and can be individually gated.
    >
    >     This commit updates the lpg generation to take this into account.
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,43 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14258"
-version_tuple = (0, 0, 14258)
+version_str = "0.0.post14260"
+version_tuple = (0, 0, 14260)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14258")
+    pversion = V("0.0.post14260")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14116"
-data_version_tuple = (0, 0, 14116)
+data_version_str = "0.0.post14118"
+data_version_tuple = (0, 0, 14118)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14116")
+    pdata_version = V("0.0.post14118")
 except ImportError:
     pass
-data_git_hash = "80e3a6cb0ab922adb51cf7ccabeebd64394d74e2"
-data_git_describe = "v0.0-14116-g80e3a6cb0a"
+data_git_hash = "476fd521585a806fcbce21a41e6e5dded2207c4b"
+data_git_describe = "v0.0-14118-g476fd52158"
 data_git_msg = """\
-commit 80e3a6cb0ab922adb51cf7ccabeebd64394d74e2
-Author: Jon Flatley <jflat@google.com>
-Date:   Wed Sep 14 12:02:23 2022 -0400
+commit 476fd521585a806fcbce21a41e6e5dded2207c4b
+Author: Timothy Chen <timothytim@google.com>
+Date:   Thu Sep 15 16:39:47 2022 -0700
 
-    [opentitanlib] Add alert_handler_reg tests
+    [util/topgen] Update alert lpg generation
     
-    Signed-off-by: Jon Flatley <jflat@google.com>
+    - fixes #14954
+    
+    The alert lpg generation currently uses the clock group, clock domain
+    and power domain of a particular clock to uniquely identify lpg groups.
+    
+    As a result, it lumps all the transactional clocks of the same domains
+    into one group.  This behavior however is not correct, as the
+    transactional clocks are unique and can be individually gated.
+    
+    This commit updates the lpg generation to take this into account.
+    
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
