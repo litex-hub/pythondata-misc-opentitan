41a140a98fc0cd9d342d2e5d24bba2b2aad80f50
LiteX Robot <robot@timvideos.us>
2022-09-17 07:14:40 +0000

    Updating pythondata-misc-opentitan to 0.0.post14285
    Updated data to v0.0-14143-geaa2430855 based on eaa2430855297f50971c1fefe4d03e14a0febc60 from https://github.com/lowRISC/opentitan.
    > commit eaa2430855297f50971c1fefe4d03e14a0febc60
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Fri Sep 16 10:23:40 2022 -0700
    >
    >     [top/dv] Add some basic IO tests to the smoke regression
    >
    >     This ensures smoke functionality and also ensures that
    >     block level DV agent changes do not break top level
    >     test functionality.
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14283"
-version_tuple = (0, 0, 14283)
+version_str = "0.0.post14285"
+version_tuple = (0, 0, 14285)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14283")
+    pversion = V("0.0.post14285")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14141"
-data_version_tuple = (0, 0, 14141)
+data_version_str = "0.0.post14143"
+data_version_tuple = (0, 0, 14143)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14141")
+    pdata_version = V("0.0.post14143")
 except ImportError:
     pass
-data_git_hash = "d430668982f70873d7e58245de31da4ed3de51b1"
-data_git_describe = "v0.0-14141-gd430668982"
+data_git_hash = "eaa2430855297f50971c1fefe4d03e14a0febc60"
+data_git_describe = "v0.0-14143-geaa2430855"
 data_git_msg = """\
-commit d430668982f70873d7e58245de31da4ed3de51b1
-Author: Alphan Ulusoy <alphan@google.com>
-Date:   Tue Sep 13 17:53:44 2022 -0400
+commit eaa2430855297f50971c1fefe4d03e14a0febc60
+Author: Timothy Chen <timothytim@google.com>
+Date:   Fri Sep 16 10:23:40 2022 -0700
 
-    [bazel] Add update_usr_access parameter to bitstream_splice
+    [top/dv] Add some basic IO tests to the smoke regression
     
-    Signed-off-by: Alphan Ulusoy <alphan@google.com>
+    This ensures smoke functionality and also ensures that
+    block level DV agent changes do not break top level
+    test functionality.
+    
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
