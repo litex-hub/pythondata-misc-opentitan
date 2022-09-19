d8e877c42c451a834702e3819d1913c33f64476b
LiteX Robot <robot@timvideos.us>
2022-09-19 19:16:18 +0000

    Updating pythondata-misc-opentitan to 0.0.post14292
    Updated data to v0.0-14150-gb2267c8b3b based on b2267c8b3b30662107ce189ce6455973fe766034 from https://github.com/lowRISC/opentitan.
    > commit b2267c8b3b30662107ce189ce6455973fe766034
    > Author: Eli Kim <eli@opentitan.org>
    > Date:   Thu Sep 15 16:50:58 2022 -0700
    >
    >     feat(chip): PINMUX Retention mode configuration
    >
    >     This commit adds a test code configuring the PAD retention modes in
    >     PINMUX in `chip_sw_sleep_pin_mio_dio_val` test.
    >
    >     Signed-off-by: Eli Kim <eli@opentitan.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,39 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14288"
-version_tuple = (0, 0, 14288)
+version_str = "0.0.post14292"
+version_tuple = (0, 0, 14292)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14288")
+    pversion = V("0.0.post14292")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14146"
-data_version_tuple = (0, 0, 14146)
+data_version_str = "0.0.post14150"
+data_version_tuple = (0, 0, 14150)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14146")
+    pdata_version = V("0.0.post14150")
 except ImportError:
     pass
-data_git_hash = "fdbd80ef827b93d73480764b13c97e2153fafbed"
-data_git_describe = "v0.0-14146-gfdbd80ef82"
+data_git_hash = "b2267c8b3b30662107ce189ce6455973fe766034"
+data_git_describe = "v0.0-14150-gb2267c8b3b"
 data_git_msg = """\
-commit fdbd80ef827b93d73480764b13c97e2153fafbed
-Author: Weicai Yang <weicai@google.com>
-Date:   Fri Sep 16 22:30:49 2022 -0700
+commit b2267c8b3b30662107ce189ce6455973fe766034
+Author: Eli Kim <eli@opentitan.org>
+Date:   Thu Sep 15 16:50:58 2022 -0700
 
-    [spi_device/dv] Update spi_agent interface
+    feat(chip): PINMUX Retention mode configuration
     
-    1. Updated interface to take sio as inout ports
-    2. Fixed an issue caused by recent update that SPI host assigns z when it's idle, while
-    the sio[1] is assigned with a DUT output.
-    3. This enables chip-level to test dual/quad mode on SPI device
+    This commit adds a test code configuring the PAD retention modes in
+    PINMUX in `chip_sw_sleep_pin_mio_dio_val` test.
     
-    Also fixed failed test - chip_sw_spi_device_tx_rx
-    
-    Signed-off-by: Weicai Yang <weicai@google.com>
+    Signed-off-by: Eli Kim <eli@opentitan.org>
 
 """
 
