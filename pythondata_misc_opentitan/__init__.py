ca4d4fdf31729ee6266c76d2ad4d6d9f0589c804
LiteX Robot <robot@timvideos.us>
2022-09-19 20:15:39 +0000

    Updating pythondata-misc-opentitan to 0.0.post14293
    Updated data to v0.0-14151-g3d8aaefa4d based on 3d8aaefa4dd79b284cd65907b5728f5384399119 from https://github.com/lowRISC/opentitan.
    > commit 3d8aaefa4dd79b284cd65907b5728f5384399119
    > Author: Weicai Yang <weicai@google.com>
    > Date:   Sun Sep 18 23:05:00 2022 -0700
    >
    >     [spi_device/dv] Fix xcelium errors
    >
    >     2 places that kill the thread when csb is dropped, which caused errors in xcelium
    >
    >     Signed-off-by: Weicai Yang <weicai@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14292"
-version_tuple = (0, 0, 14292)
+version_str = "0.0.post14293"
+version_tuple = (0, 0, 14293)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14292")
+    pversion = V("0.0.post14293")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14150"
-data_version_tuple = (0, 0, 14150)
+data_version_str = "0.0.post14151"
+data_version_tuple = (0, 0, 14151)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14150")
+    pdata_version = V("0.0.post14151")
 except ImportError:
     pass
-data_git_hash = "b2267c8b3b30662107ce189ce6455973fe766034"
-data_git_describe = "v0.0-14150-gb2267c8b3b"
+data_git_hash = "3d8aaefa4dd79b284cd65907b5728f5384399119"
+data_git_describe = "v0.0-14151-g3d8aaefa4d"
 data_git_msg = """\
-commit b2267c8b3b30662107ce189ce6455973fe766034
-Author: Eli Kim <eli@opentitan.org>
-Date:   Thu Sep 15 16:50:58 2022 -0700
+commit 3d8aaefa4dd79b284cd65907b5728f5384399119
+Author: Weicai Yang <weicai@google.com>
+Date:   Sun Sep 18 23:05:00 2022 -0700
 
-    feat(chip): PINMUX Retention mode configuration
+    [spi_device/dv] Fix xcelium errors
     
-    This commit adds a test code configuring the PAD retention modes in
-    PINMUX in `chip_sw_sleep_pin_mio_dio_val` test.
+    2 places that kill the thread when csb is dropped, which caused errors in xcelium
     
-    Signed-off-by: Eli Kim <eli@opentitan.org>
+    Signed-off-by: Weicai Yang <weicai@google.com>
 
 """
 
