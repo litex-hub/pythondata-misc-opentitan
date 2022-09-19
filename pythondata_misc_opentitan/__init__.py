aae199908c463b86bacbc834a9368911f293ffe2
LiteX Robot <robot@timvideos.us>
2022-09-19 09:20:15 +0000

    Updating pythondata-misc-opentitan to 0.0.post14287
    Updated data to v0.0-14145-gc80a2e2e1b based on c80a2e2e1bc2cb727ae82cb5c00c666a677c191f from https://github.com/lowRISC/opentitan.
    > commit c80a2e2e1bc2cb727ae82cb5c00c666a677c191f
    > Author: Michael Schaffner <msf@google.com>
    > Date:   Fri Sep 16 14:54:43 2022 -0700
    >
    >     [test/rv_plic] Add a hart_id check to the test
    >
    >     This additional check makes sure that the configured PLIC target
    >     ID corresponds to the hart ID of the processor this is executed on.
    >     To be used in single-hart systems only.
    >
    >     Signed-off-by: Michael Schaffner <msf@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14285"
-version_tuple = (0, 0, 14285)
+version_str = "0.0.post14287"
+version_tuple = (0, 0, 14287)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14285")
+    pversion = V("0.0.post14287")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14143"
-data_version_tuple = (0, 0, 14143)
+data_version_str = "0.0.post14145"
+data_version_tuple = (0, 0, 14145)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14143")
+    pdata_version = V("0.0.post14145")
 except ImportError:
     pass
-data_git_hash = "eaa2430855297f50971c1fefe4d03e14a0febc60"
-data_git_describe = "v0.0-14143-geaa2430855"
+data_git_hash = "c80a2e2e1bc2cb727ae82cb5c00c666a677c191f"
+data_git_describe = "v0.0-14145-gc80a2e2e1b"
 data_git_msg = """\
-commit eaa2430855297f50971c1fefe4d03e14a0febc60
-Author: Timothy Chen <timothytim@google.com>
-Date:   Fri Sep 16 10:23:40 2022 -0700
+commit c80a2e2e1bc2cb727ae82cb5c00c666a677c191f
+Author: Michael Schaffner <msf@google.com>
+Date:   Fri Sep 16 14:54:43 2022 -0700
 
-    [top/dv] Add some basic IO tests to the smoke regression
+    [test/rv_plic] Add a hart_id check to the test
     
-    This ensures smoke functionality and also ensures that
-    block level DV agent changes do not break top level
-    test functionality.
+    This additional check makes sure that the configured PLIC target
+    ID corresponds to the hart ID of the processor this is executed on.
+    To be used in single-hart systems only.
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Michael Schaffner <msf@google.com>
 
 """
 
