3f247949fba3ddc1a53977ad7b5c88048ccd1bd6
LiteX Robot <robot@timvideos.us>
2022-09-20 18:18:54 +0000

    Updating pythondata-misc-opentitan to 0.0.post14311
    Updated data to v0.0-14169-g902ca28e32 based on 902ca28e32018f7fb4f3db9e71fdf395ced7ed09 from https://github.com/lowRISC/opentitan.
    > commit 902ca28e32018f7fb4f3db9e71fdf395ced7ed09
    > Author: Srikrishna Iyer <sriyer@google.com>
    > Date:   Mon Sep 19 18:04:51 2022 -0700
    >
    >     [chip dv] Fix JTAG interface enablement
    >
    >     THe previous code relied on the test bench setting the TAP
    >     strap pins to enable the JTAG interface, rather than adding
    >     a dedicated `enable_jtag` mux signal.
    >
    >     The existing code looked at the `tap_straps_if.pins`, which
    >     is just a wire which will reflect the value of the pins
    >     IOC5,8 which may have been wiggled for a different test with
    >     a different interface, and that would inadvertantly end up
    >     enabling JTAG, causing contention.
    >
    >     This commit makes it look at both, the `pins_o` and `pins_oe`
    >     values, so that the actual intent of setting the TAP straps
    >     is what causes the JTAG to be enabled.
    >
    >     Signed-off-by: Srikrishna Iyer <sriyer@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,46 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14309"
-version_tuple = (0, 0, 14309)
+version_str = "0.0.post14311"
+version_tuple = (0, 0, 14311)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14309")
+    pversion = V("0.0.post14311")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14167"
-data_version_tuple = (0, 0, 14167)
+data_version_str = "0.0.post14169"
+data_version_tuple = (0, 0, 14169)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14167")
+    pdata_version = V("0.0.post14169")
 except ImportError:
     pass
-data_git_hash = "f613046ab59dcf5ba23039e456cdc1740b29e030"
-data_git_describe = "v0.0-14167-gf613046ab5"
+data_git_hash = "902ca28e32018f7fb4f3db9e71fdf395ced7ed09"
+data_git_describe = "v0.0-14169-g902ca28e32"
 data_git_msg = """\
-commit f613046ab59dcf5ba23039e456cdc1740b29e030
-Author: Alexander Williams <awill@google.com>
-Date:   Mon Sep 19 20:24:06 2022 -0700
+commit 902ca28e32018f7fb4f3db9e71fdf395ced7ed09
+Author: Srikrishna Iyer <sriyer@google.com>
+Date:   Mon Sep 19 18:04:51 2022 -0700
 
-    [dv] Update testplan for chip_sw_spi_device_flash_mode
+    [chip dv] Fix JTAG interface enablement
     
-    Fill in the test used (chip_sw_uart_tx_rx_bootstrap).
+    THe previous code relied on the test bench setting the TAP
+    strap pins to enable the JTAG interface, rather than adding
+    a dedicated `enable_jtag` mux signal.
     
-    Signed-off-by: Alexander Williams <awill@google.com>
+    The existing code looked at the `tap_straps_if.pins`, which
+    is just a wire which will reflect the value of the pins
+    IOC5,8 which may have been wiggled for a different test with
+    a different interface, and that would inadvertantly end up
+    enabling JTAG, causing contention.
+    
+    This commit makes it look at both, the `pins_o` and `pins_oe`
+    values, so that the actual intent of setting the TAP straps
+    is what causes the JTAG to be enabled.
+    
+    Signed-off-by: Srikrishna Iyer <sriyer@google.com>
 
 """
 
