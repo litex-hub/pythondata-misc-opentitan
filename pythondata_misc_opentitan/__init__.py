3daf120b2a43c31d531395c8e41f52dc54cef97f
LiteX Robot <robot@timvideos.us>
2022-09-20 23:14:34 +0000

    Updating pythondata-misc-opentitan to 0.0.post14333
    Updated data to v0.0-14191-g3575ab2b6f based on 3575ab2b6f1fff58cdc5a3111632fe30ec59bfb8 from https://github.com/lowRISC/opentitan.
    > commit 3575ab2b6f1fff58cdc5a3111632fe30ec59bfb8
    > Author: Timothy Trippel <ttrippel@google.com>
    > Date:   Tue Sep 20 05:39:45 2022 -0700
    >
    >     [dif] update checklists to align with #14632
    >
    >     This updates the checklists of the following IPs:
    >     - sysrst_ctrl
    >     - uart
    >     - usbdev
    >
    >     This fixes #14632.
    >
    >     Signed-off-by: Timothy Trippel <ttrippel@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,39 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14323"
-version_tuple = (0, 0, 14323)
+version_str = "0.0.post14333"
+version_tuple = (0, 0, 14333)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14323")
+    pversion = V("0.0.post14333")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14181"
-data_version_tuple = (0, 0, 14181)
+data_version_str = "0.0.post14191"
+data_version_tuple = (0, 0, 14191)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14181")
+    pdata_version = V("0.0.post14191")
 except ImportError:
     pass
-data_git_hash = "93dc5862ed78e85df8205dc252a8eee9696e5d1b"
-data_git_describe = "v0.0-14181-g93dc5862ed"
+data_git_hash = "3575ab2b6f1fff58cdc5a3111632fe30ec59bfb8"
+data_git_describe = "v0.0-14191-g3575ab2b6f"
 data_git_msg = """\
-commit 93dc5862ed78e85df8205dc252a8eee9696e5d1b
-Author: Timothy Chen <timothytim@google.com>
-Date:   Tue Sep 20 12:31:07 2022 -0700
+commit 3575ab2b6f1fff58cdc5a3111632fe30ec59bfb8
+Author: Timothy Trippel <ttrippel@google.com>
+Date:   Tue Sep 20 05:39:45 2022 -0700
 
-    [top/dv] Increase reseed for wdog sleep pause
+    [dif] update checklists to align with #14632
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    This updates the checklists of the following IPs:
+    - sysrst_ctrl
+    - uart
+    - usbdev
+    
+    This fixes #14632.
+    
+    Signed-off-by: Timothy Trippel <ttrippel@google.com>
 
 """
 
