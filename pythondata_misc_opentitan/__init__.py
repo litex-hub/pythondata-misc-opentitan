9b5a24450fe60ae9b0c85ad32b6222bf1ddcd3bb
LiteX Robot <robot@timvideos.us>
2022-09-22 10:15:13 +0000

    Updating pythondata-misc-opentitan to 0.0.post14380
    Updated data to v0.0-14238-g3731187647 based on 3731187647ca2bed9df6bc9dd712645f9c01156a from https://github.com/lowRISC/opentitan.
    > commit 3731187647ca2bed9df6bc9dd712645f9c01156a
    > Author: Alphan Ulusoy <alphan@google.com>
    > Date:   Wed Aug 31 20:38:25 2022 -0400
    >
    >     [test] Make boot_data_functest compatible with rom
    >
    >     Signed-off-by: Alphan Ulusoy <alphan@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14374"
-version_tuple = (0, 0, 14374)
+version_str = "0.0.post14380"
+version_tuple = (0, 0, 14380)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14374")
+    pversion = V("0.0.post14380")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14232"
-data_version_tuple = (0, 0, 14232)
+data_version_str = "0.0.post14238"
+data_version_tuple = (0, 0, 14238)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14232")
+    pdata_version = V("0.0.post14238")
 except ImportError:
     pass
-data_git_hash = "0a3ba57c724eb7e842f74fd3bd8595939a02c432"
-data_git_describe = "v0.0-14232-g0a3ba57c72"
+data_git_hash = "3731187647ca2bed9df6bc9dd712645f9c01156a"
+data_git_describe = "v0.0-14238-g3731187647"
 data_git_msg = """\
-commit 0a3ba57c724eb7e842f74fd3bd8595939a02c432
-Author: Douglas Reis <doreis@lowrisc.org>
-Date:   Tue Sep 20 10:59:43 2022 +0100
+commit 3731187647ca2bed9df6bc9dd712645f9c01156a
+Author: Alphan Ulusoy <alphan@google.com>
+Date:   Wed Aug 31 20:38:25 2022 -0400
 
-    [silicon_creator] Refactor the epmp lib
+    [test] Make boot_data_functest compatible with rom
     
-     This commit refactors the epmp lib to use the epmp_state global
-     from the static_critical section rather than receiving the object.
-    
-    Signed-off-by: Douglas Reis <doreis@lowrisc.org>
+    Signed-off-by: Alphan Ulusoy <alphan@google.com>
 
 """
 
