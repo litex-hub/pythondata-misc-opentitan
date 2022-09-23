216ce048f6727253ed328ce843fcbd599278755a
LiteX Robot <robot@timvideos.us>
2022-09-23 12:26:04 +0000

    Updating pythondata-misc-opentitan to 0.0.post14397
    Updated data to v0.0-14255-gb72304ab6b based on b72304ab6bc403b819e54329ee8e153c6438f4e6 from https://github.com/lowRISC/opentitan.
    > commit b72304ab6bc403b819e54329ee8e153c6438f4e6
    > Author: Canberk Topal <ctopal@lowrisc.org>
    > Date:   Thu Sep 15 15:21:42 2022 +0100
    >
    >     [otbn,fcov] Add RND spurious ack error coverpoint
    >
    >     In the comment linked below, a spurious ack check feature said to
    >     include RND as well as URND. This commit extends bad_internal_cg to
    >     include that error as well.
    >
    >     https://github.com/lowRISC/opentitan/issues/11546#issuecomment-1172031776
    >
    >     Signed-off-by: Canberk Topal <ctopal@lowrisc.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14393"
-version_tuple = (0, 0, 14393)
+version_str = "0.0.post14397"
+version_tuple = (0, 0, 14397)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14393")
+    pversion = V("0.0.post14397")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14251"
-data_version_tuple = (0, 0, 14251)
+data_version_str = "0.0.post14255"
+data_version_tuple = (0, 0, 14255)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14251")
+    pdata_version = V("0.0.post14255")
 except ImportError:
     pass
-data_git_hash = "42313dc0381e53a01e6b251d6596089b9898afbf"
-data_git_describe = "v0.0-14251-g42313dc038"
+data_git_hash = "b72304ab6bc403b819e54329ee8e153c6438f4e6"
+data_git_describe = "v0.0-14255-gb72304ab6b"
 data_git_msg = """\
-commit 42313dc0381e53a01e6b251d6596089b9898afbf
+commit b72304ab6bc403b819e54329ee8e153c6438f4e6
 Author: Canberk Topal <ctopal@lowrisc.org>
-Date:   Fri Sep 16 09:26:38 2022 +0100
+Date:   Thu Sep 15 15:21:42 2022 +0100
 
-    [otbn,dv] Turn off some assertions in FI tests
+    [otbn,fcov] Add RND spurious ack error coverpoint
     
-    This commit groups blanking assertions and DMEM assertions with
-    DV_ASSERT_CTRL macro and turns them off in fault injection tests
-    since it is not guaranteed to be able to blank in those cases.
+    In the comment linked below, a spurious ack check feature said to
+    include RND as well as URND. This commit extends bad_internal_cg to
+    include that error as well.
+    
+    https://github.com/lowRISC/opentitan/issues/11546#issuecomment-1172031776
     
     Signed-off-by: Canberk Topal <ctopal@lowrisc.org>
 
