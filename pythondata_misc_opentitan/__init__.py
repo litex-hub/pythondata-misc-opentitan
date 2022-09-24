8b1b6916051135f52e7d0ad213353b4dff5f86e4
LiteX Robot <robot@timvideos.us>
2022-09-24 05:12:22 +0000

    Updating pythondata-misc-opentitan to 0.0.post14423
    Updated data to v0.0-14281-gf3b5cf8996 based on f3b5cf89960dc332e12fbe4cb1e04e1b8aa7aa67 from https://github.com/lowRISC/opentitan.
    > commit f3b5cf89960dc332e12fbe4cb1e04e1b8aa7aa67
    > Author: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
    > Date:   Fri Sep 23 16:06:55 2022 -0700
    >
    >     [entropy_src/dv] VCS compatibility
    >
    >     Minor VCS-related bug fix
    >
    >     Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14422"
-version_tuple = (0, 0, 14422)
+version_str = "0.0.post14423"
+version_tuple = (0, 0, 14423)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14422")
+    pversion = V("0.0.post14423")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14280"
-data_version_tuple = (0, 0, 14280)
+data_version_str = "0.0.post14281"
+data_version_tuple = (0, 0, 14281)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14280")
+    pdata_version = V("0.0.post14281")
 except ImportError:
     pass
-data_git_hash = "5ca06aa40bc864f0b903da1cebb0013a842ec08c"
-data_git_describe = "v0.0-14280-g5ca06aa40b"
+data_git_hash = "f3b5cf89960dc332e12fbe4cb1e04e1b8aa7aa67"
+data_git_describe = "v0.0-14281-gf3b5cf8996"
 data_git_msg = """\
-commit 5ca06aa40bc864f0b903da1cebb0013a842ec08c
-Author: Eli Kim <eli@opentitan.org>
-Date:   Thu Sep 22 09:14:27 2022 -0700
+commit f3b5cf89960dc332e12fbe4cb1e04e1b8aa7aa67
+Author: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
+Date:   Fri Sep 23 16:06:55 2022 -0700
 
-    doc(chip): Revise testplan to cover any sleep modes.
+    [entropy_src/dv] VCS compatibility
     
-    To cover `chip_sw_random_sleep_pin_wake` test, the
-    `chip_sw_sleep_pin_wake` covers normal sleep mode too. This commit
-    revises the testplan to mention the deep/ normal sleep states.
+    Minor VCS-related bug fix
     
-    Signed-off-by: Eli Kim <eli@opentitan.org>
+    Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
 
 """
 
