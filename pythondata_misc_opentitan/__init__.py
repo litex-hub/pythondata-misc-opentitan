92112dcc79fcf3b43e531ef076f3948082438eaa
LiteX Robot <robot@timvideos.us>
2022-09-26 17:30:56 +0000

    Updating pythondata-misc-opentitan to 0.0.post14438
    Updated data to v0.0-14296-gfcefd065d4 based on fcefd065d4c444f090b42534cd19d7511950f538 from https://github.com/lowRISC/opentitan.
    > commit fcefd065d4c444f090b42534cd19d7511950f538
    > Author: Weicai Yang <weicai@google.com>
    > Date:   Fri Sep 23 16:19:27 2022 -0700
    >
    >     [spi_device/dv] Update TPM sequences
    >
    >     1. Update locality sequence to use the new method
    >     2. Remove csb_consecutive and clean up used task/functions
    >
    >     Signed-off-by: Weicai Yang <weicai@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,37 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14434"
-version_tuple = (0, 0, 14434)
+version_str = "0.0.post14438"
+version_tuple = (0, 0, 14438)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14434")
+    pversion = V("0.0.post14438")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14292"
-data_version_tuple = (0, 0, 14292)
+data_version_str = "0.0.post14296"
+data_version_tuple = (0, 0, 14296)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14292")
+    pdata_version = V("0.0.post14296")
 except ImportError:
     pass
-data_git_hash = "955d57a70b66373c08cbe575de83c4754315bc8a"
-data_git_describe = "v0.0-14292-g955d57a70b"
+data_git_hash = "fcefd065d4c444f090b42534cd19d7511950f538"
+data_git_describe = "v0.0-14296-gfcefd065d4"
 data_git_msg = """\
-commit 955d57a70b66373c08cbe575de83c4754315bc8a
-Author: Jade Philipoom <jadep@google.com>
-Date:   Fri Sep 16 14:27:24 2022 +0200
+commit fcefd065d4c444f090b42534cd19d7511950f538
+Author: Weicai Yang <weicai@google.com>
+Date:   Fri Sep 23 16:19:27 2022 -0700
 
-    [otbn, util] Modify otbn_sim_test rule to use new wrapper.
+    [spi_device/dv] Update TPM sequences
     
-    Use the new OTBN simulator test wrapper in the otbn_sim_test Bazel rule.
-    For compatibility with existing tests, this version simply always sets
-    the expected values to "w0=0"; future updates will create specific
-    expected-values files for each test.
+    1. Update locality sequence to use the new method
+    2. Remove csb_consecutive and clean up used task/functions
     
-    Signed-off-by: Jade Philipoom <jadep@google.com>
+    Signed-off-by: Weicai Yang <weicai@google.com>
 
 """
 
