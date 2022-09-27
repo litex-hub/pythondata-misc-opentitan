929dc26efd66ea10df2f5d3b1a0572496f0bbdcb
LiteX Robot <robot@timvideos.us>
2022-09-27 01:03:11 +0000

    Updating pythondata-misc-opentitan to 0.0.post14440
    Updated data to v0.0-14298-g5b95d019b9 based on 5b95d019b95423b14e590a285475abf4d99c50f6 from https://github.com/lowRISC/opentitan.
    > commit 5b95d019b95423b14e590a285475abf4d99c50f6
    > Author: Guillermo Maturana <maturana@google.com>
    > Date:   Mon Sep 19 13:35:35 2022 -0700
    >
    >     [dv/rstmgr] Fix dv for recent changes in reset
    >
    >     The recent RTL changes required big changes in the dv environment.
    >     This change disables non-aon clocks upon reset, and improves the
    >     flow of reset stimulus.
    >
    >     Signed-off-by: Guillermo Maturana <maturana@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14439"
-version_tuple = (0, 0, 14439)
+version_str = "0.0.post14440"
+version_tuple = (0, 0, 14440)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14439")
+    pversion = V("0.0.post14440")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14297"
-data_version_tuple = (0, 0, 14297)
+data_version_str = "0.0.post14298"
+data_version_tuple = (0, 0, 14298)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14297")
+    pdata_version = V("0.0.post14298")
 except ImportError:
     pass
-data_git_hash = "c43e2906516442dec36022f101e109c050d2dd69"
-data_git_describe = "v0.0-14297-gc43e290651"
+data_git_hash = "5b95d019b95423b14e590a285475abf4d99c50f6"
+data_git_describe = "v0.0-14298-g5b95d019b9"
 data_git_msg = """\
-commit c43e2906516442dec36022f101e109c050d2dd69
-Author: Michael Schaffner <msf@google.com>
-Date:   Fri Sep 23 15:34:53 2022 +0200
+commit 5b95d019b95423b14e590a285475abf4d99c50f6
+Author: Guillermo Maturana <maturana@google.com>
+Date:   Mon Sep 19 13:35:35 2022 -0700
 
-    [rv_core_ibex] Add SVA to check PC is not advancing
+    [dv/rstmgr] Fix dv for recent changes in reset
     
-    The PC must not advance if the CPU is not enabled.
-    Addresses part of #11208.
+    The recent RTL changes required big changes in the dv environment.
+    This change disables non-aon clocks upon reset, and improves the
+    flow of reset stimulus.
     
-    Signed-off-by: Michael Schaffner <msf@google.com>
+    Signed-off-by: Guillermo Maturana <maturana@google.com>
 
 """
 
