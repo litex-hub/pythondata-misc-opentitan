7dc5e4975d4eb5b76829f00d4679058cbc8d3ce7
LiteX Robot <robot@timvideos.us>
2022-09-27 23:16:10 +0000

    Updating pythondata-misc-opentitan to 0.0.post14464
    Updated data to v0.0-14322-g97d123b67e based on 97d123b67eccfea340e56717afdbc1dfe5d8a510 from https://github.com/lowRISC/opentitan.
    > commit 97d123b67eccfea340e56717afdbc1dfe5d8a510
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Mon Sep 26 14:34:58 2022 -0700
    >
    >     address review comments
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14461"
-version_tuple = (0, 0, 14461)
+version_str = "0.0.post14464"
+version_tuple = (0, 0, 14464)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14461")
+    pversion = V("0.0.post14464")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14319"
-data_version_tuple = (0, 0, 14319)
+data_version_str = "0.0.post14322"
+data_version_tuple = (0, 0, 14322)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14319")
+    pdata_version = V("0.0.post14322")
 except ImportError:
     pass
-data_git_hash = "b40e28862c7f82b149d7f233bedb211366a58fa8"
-data_git_describe = "v0.0-14319-gb40e28862c"
+data_git_hash = "97d123b67eccfea340e56717afdbc1dfe5d8a510"
+data_git_describe = "v0.0-14322-g97d123b67e"
 data_git_msg = """\
-commit b40e28862c7f82b149d7f233bedb211366a58fa8
-Author: Weicai Yang <weicai@google.com>
-Date:   Mon Sep 26 16:54:29 2022 -0700
+commit 97d123b67eccfea340e56717afdbc1dfe5d8a510
+Author: Timothy Chen <timothytim@google.com>
+Date:   Mon Sep 26 14:34:58 2022 -0700
 
-    [spi_device/dv] Update monitor for TPM mode
+    address review comments
     
-    1. small refactor to reuse the part that kills threads after CSR drops
-    2. Completed `collect_tpm_trans` task
-    3. Renamed a few task as they can be reused for TPM mode
-    
-    Signed-off-by: Weicai Yang <weicai@google.com>
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
