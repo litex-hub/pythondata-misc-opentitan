17d0b2185b35dab31f8fbdab3c270384e3552980
LiteX Robot <robot@timvideos.us>
2022-09-28 06:49:49 +0000

    Updating pythondata-misc-opentitan to 0.0.post14467
    Updated data to v0.0-14325-gdb06938589 based on db069385899684ae5b736270cb5ed3d8dd745b1a from https://github.com/lowRISC/opentitan.
    > commit db069385899684ae5b736270cb5ed3d8dd745b1a
    > Author: Miles Dai <milesdai@google.com>
    > Date:   Mon Sep 26 16:02:08 2022 -0400
    >
    >     [lint] Make the flake8 check mandatory on CI
    >
    >     Signed-off-by: Miles Dai <milesdai@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14465"
-version_tuple = (0, 0, 14465)
+version_str = "0.0.post14467"
+version_tuple = (0, 0, 14467)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14465")
+    pversion = V("0.0.post14467")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14323"
-data_version_tuple = (0, 0, 14323)
+data_version_str = "0.0.post14325"
+data_version_tuple = (0, 0, 14325)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14323")
+    pdata_version = V("0.0.post14325")
 except ImportError:
     pass
-data_git_hash = "b358d54a6b796d6a70d9c59f1741e45e31ad0479"
-data_git_describe = "v0.0-14323-gb358d54a6b"
+data_git_hash = "db069385899684ae5b736270cb5ed3d8dd745b1a"
+data_git_describe = "v0.0-14325-gdb06938589"
 data_git_msg = """\
-commit b358d54a6b796d6a70d9c59f1741e45e31ad0479
-Author: Cindy <chencindy@google.com>
-Date:   Tue Sep 27 20:00:18 2022 +0000
+commit db069385899684ae5b736270cb5ed3d8dd745b1a
+Author: Miles Dai <milesdai@google.com>
+Date:   Mon Sep 26 16:02:08 2022 -0400
 
-    [fpv] fix random seed syntax error
+    [lint] Make the flake8 check mandatory on CI
     
-    FPV does not support rand dist and FPV_INIT with a non-constant.
-    So this PR fixes the syntax error by using the default LFSR seed.
-    
-    Signed-off-by: Cindy <chencindy@google.com>
+    Signed-off-by: Miles Dai <milesdai@google.com>
 
 """
 
