6461bc7e8981eebd53fb0bcfd970f3b1f9cb6359
LiteX Robot <robot@timvideos.us>
2022-09-28 05:27:12 +0000

    Updating pythondata-misc-opentitan to 0.0.post14465
    Updated data to v0.0-14323-gb358d54a6b based on b358d54a6b796d6a70d9c59f1741e45e31ad0479 from https://github.com/lowRISC/opentitan.
    > commit b358d54a6b796d6a70d9c59f1741e45e31ad0479
    > Author: Cindy <chencindy@google.com>
    > Date:   Tue Sep 27 20:00:18 2022 +0000
    >
    >     [fpv] fix random seed syntax error
    >
    >     FPV does not support rand dist and FPV_INIT with a non-constant.
    >     So this PR fixes the syntax error by using the default LFSR seed.
    >
    >     Signed-off-by: Cindy <chencindy@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14464"
-version_tuple = (0, 0, 14464)
+version_str = "0.0.post14465"
+version_tuple = (0, 0, 14465)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14464")
+    pversion = V("0.0.post14465")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14322"
-data_version_tuple = (0, 0, 14322)
+data_version_str = "0.0.post14323"
+data_version_tuple = (0, 0, 14323)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14322")
+    pdata_version = V("0.0.post14323")
 except ImportError:
     pass
-data_git_hash = "97d123b67eccfea340e56717afdbc1dfe5d8a510"
-data_git_describe = "v0.0-14322-g97d123b67e"
+data_git_hash = "b358d54a6b796d6a70d9c59f1741e45e31ad0479"
+data_git_describe = "v0.0-14323-gb358d54a6b"
 data_git_msg = """\
-commit 97d123b67eccfea340e56717afdbc1dfe5d8a510
-Author: Timothy Chen <timothytim@google.com>
-Date:   Mon Sep 26 14:34:58 2022 -0700
+commit b358d54a6b796d6a70d9c59f1741e45e31ad0479
+Author: Cindy <chencindy@google.com>
+Date:   Tue Sep 27 20:00:18 2022 +0000
 
-    address review comments
+    [fpv] fix random seed syntax error
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    FPV does not support rand dist and FPV_INIT with a non-constant.
+    So this PR fixes the syntax error by using the default LFSR seed.
+    
+    Signed-off-by: Cindy <chencindy@google.com>
 
 """
 
