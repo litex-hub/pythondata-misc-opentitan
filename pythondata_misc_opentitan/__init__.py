185e072ad71d3c0633eb3a71a78e0774f3c3b6ee
LiteX Robot <robot@timvideos.us>
2022-09-28 19:19:31 +0000

    Updating pythondata-misc-opentitan to 0.0.post14483
    Updated data to v0.0-14341-gbefa83b432 based on befa83b43297b2764af9d2be93a761b41e255286 from https://github.com/lowRISC/opentitan.
    > commit befa83b43297b2764af9d2be93a761b41e255286
    > Author: Jade Philipoom <jadep@google.com>
    > Date:   Tue Sep 27 13:36:05 2022 +0200
    >
    >     [doc] Cleanup and fixes for install guide.
    >
    >     Adjust the install guide pages based on feedback and on a read-through:
    >     - Reverses the order of the Verilator install and the software build,
    >       since Bazel tests essentially require Verilator
    >     - Changes the tl;dr section of the software build page to something that
    >       should succeed on the first try (the previous example required
    >     Verible)
    >     - Adds troubleshooting notes and information about RAM requirements
    >     - Adds notes and time estimates for long-running steps
    >     - Updates information about the CI setup (we now use Ubuntu 20.04, not
    >       18.04)
    >     - Removes "$" before console commands to make them easier to copy-paste
    >
    >     Signed-off-by: Jade Philipoom <jadep@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,44 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14482"
-version_tuple = (0, 0, 14482)
+version_str = "0.0.post14483"
+version_tuple = (0, 0, 14483)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14482")
+    pversion = V("0.0.post14483")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14340"
-data_version_tuple = (0, 0, 14340)
+data_version_str = "0.0.post14341"
+data_version_tuple = (0, 0, 14341)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14340")
+    pdata_version = V("0.0.post14341")
 except ImportError:
     pass
-data_git_hash = "c9f6c6e93c8f10904df3bf253a3206753e26b9be"
-data_git_describe = "v0.0-14340-gc9f6c6e93c"
+data_git_hash = "befa83b43297b2764af9d2be93a761b41e255286"
+data_git_describe = "v0.0-14341-gbefa83b432"
 data_git_msg = """\
-commit c9f6c6e93c8f10904df3bf253a3206753e26b9be
-Author: Michael Schaffner <msf@google.com>
-Date:   Fri Sep 23 16:30:34 2022 +0200
+commit befa83b43297b2764af9d2be93a761b41e255286
+Author: Jade Philipoom <jadep@google.com>
+Date:   Tue Sep 27 13:36:05 2022 +0200
 
-    [mubi/lc] Relax transient SVA checks
+    [doc] Cleanup and fixes for install guide.
     
-    Fix #14925.
+    Adjust the install guide pages based on feedback and on a read-through:
+    - Reverses the order of the Verilator install and the software build,
+      since Bazel tests essentially require Verilator
+    - Changes the tl;dr section of the software build page to something that
+      should succeed on the first try (the previous example required
+    Verible)
+    - Adds troubleshooting notes and information about RAM requirements
+    - Adds notes and time estimates for long-running steps
+    - Updates information about the CI setup (we now use Ubuntu 20.04, not
+      18.04)
+    - Removes "$" before console commands to make them easier to copy-paste
     
-    Signed-off-by: Michael Schaffner <msf@google.com>
+    Signed-off-by: Jade Philipoom <jadep@google.com>
 
 """
 
