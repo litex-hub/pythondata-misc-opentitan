0dc37bb6d93df1d5d700e9d6677532e2c9eab297
LiteX Robot <robot@timvideos.us>
2022-09-29 04:17:55 +0000

    Updating pythondata-misc-opentitan to 0.0.post14489
    Updated data to v0.0-14347-g6274743922 based on 62747439226d65a760ec82b90c2da13886e52958 from https://github.com/lowRISC/opentitan.
    > commit 62747439226d65a760ec82b90c2da13886e52958
    > Author: Alexander Williams <awill@google.com>
    > Date:   Wed Sep 28 16:46:12 2022 -0700
    >
    >     [dv] Adjust chip_sw_uart_tx_rx_bootstrap max sim time
    >
    >     It can go longer than 60 ms, so adjust to 80 ms.
    >
    >     Signed-off-by: Alexander Williams <awill@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14486"
-version_tuple = (0, 0, 14486)
+version_str = "0.0.post14489"
+version_tuple = (0, 0, 14489)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14486")
+    pversion = V("0.0.post14489")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14344"
-data_version_tuple = (0, 0, 14344)
+data_version_str = "0.0.post14347"
+data_version_tuple = (0, 0, 14347)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14344")
+    pdata_version = V("0.0.post14347")
 except ImportError:
     pass
-data_git_hash = "7550a3512290c3c4df978fcc70112b787c963076"
-data_git_describe = "v0.0-14344-g7550a35122"
+data_git_hash = "62747439226d65a760ec82b90c2da13886e52958"
+data_git_describe = "v0.0-14347-g6274743922"
 data_git_msg = """\
-commit 7550a3512290c3c4df978fcc70112b787c963076
-Author: Timothy Trippel <ttrippel@google.com>
-Date:   Tue Sep 27 17:51:44 2022 -0700
+commit 62747439226d65a760ec82b90c2da13886e52958
+Author: Alexander Williams <awill@google.com>
+Date:   Wed Sep 28 16:46:12 2022 -0700
 
-    [bazel] remove unused build targets
+    [dv] Adjust chip_sw_uart_tx_rx_bootstrap max sim time
     
-    It looks like these build targets are unused / unnecessary as they are
-    file groups that export a single target that could each be used
-    directly.
+    It can go longer than 60 ms, so adjust to 80 ms.
     
-    Signed-off-by: Timothy Trippel <ttrippel@google.com>
+    Signed-off-by: Alexander Williams <awill@google.com>
 
 """
 
