8eea9702ed5c887cec91ed5cebfaf0713a252f63
LiteX Robot <robot@timvideos.us>
2022-10-03 13:51:33 +0000

    Updating pythondata-misc-opentitan to 0.0.post14532
    Updated data to v0.0-14390-gcfe71b5140 based on cfe71b5140093f4a1aa95dce80b4d3b81d333de3 from https://github.com/lowRISC/opentitan.
    > commit cfe71b5140093f4a1aa95dce80b4d3b81d333de3
    > Author: Andreas Kurth <adk@lowrisc.org>
    > Date:   Tue Sep 27 15:53:17 2022 +0000
    >
    >     [doc] Update motivation and objectives of DIFs
    >
    >     The current DIF reference manual (RM) text emphasizes error checking and
    >     robustness of DIFs quite heavily ("making it difficult, or impossible,
    >     to misuse [hardware]." and "providing example code to aid the
    >     implementation of reference firmware including end-consumer
    >     applications").  However, DIFs are called in test code, some of which
    >     deliberately misuses a hardware module, so error checking and robustness
    >     are not a primary objective for DIFs and could hinder their use for
    >     tests.
    >
    >     This PR changes the wording of the DIF RM to de-emphasize error checking
    >     and robustness.  The revised text is aligned with the understanding of
    >     the Software WG at their meeting on 2022-09-27.  It also matches the
    >     description in `sw/device/lib/dif/_index.md` more closely.
    >
    >     Signed-off-by: Andreas Kurth <adk@lowrisc.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,39 +4,46 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14531"
-version_tuple = (0, 0, 14531)
+version_str = "0.0.post14532"
+version_tuple = (0, 0, 14532)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14531")
+    pversion = V("0.0.post14532")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14389"
-data_version_tuple = (0, 0, 14389)
+data_version_str = "0.0.post14390"
+data_version_tuple = (0, 0, 14390)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14389")
+    pdata_version = V("0.0.post14390")
 except ImportError:
     pass
-data_git_hash = "67a41e3bc3ff025554a024cdb15fbf892c4752b3"
-data_git_describe = "v0.0-14389-g67a41e3bc3"
+data_git_hash = "cfe71b5140093f4a1aa95dce80b4d3b81d333de3"
+data_git_describe = "v0.0-14390-gcfe71b5140"
 data_git_msg = """\
-commit 67a41e3bc3ff025554a024cdb15fbf892c4752b3
-Author: Timothy Chen <timothytim@google.com>
-Date:   Wed Sep 21 11:42:16 2022 -0700
+commit cfe71b5140093f4a1aa95dce80b4d3b81d333de3
+Author: Andreas Kurth <adk@lowrisc.org>
+Date:   Tue Sep 27 15:53:17 2022 +0000
 
-    [i2c] fix target mode start / stop handling
+    [doc] Update motivation and objectives of DIFs
     
-    - instead of looking for start / stop in specific states, jump directly
-      to AcquireSrP when in target mode.
+    The current DIF reference manual (RM) text emphasizes error checking and
+    robustness of DIFs quite heavily ("making it difficult, or impossible,
+    to misuse [hardware]." and "providing example code to aid the
+    implementation of reference firmware including end-consumer
+    applications").  However, DIFs are called in test code, some of which
+    deliberately misuses a hardware module, so error checking and robustness
+    are not a primary objective for DIFs and could hinder their use for
+    tests.
     
-    - stop detection is cleared in AcquireSrP, while start_detection is
-      only cleared once we cycle through start address decode handling
-      again.
+    This PR changes the wording of the DIF RM to de-emphasize error checking
+    and robustness.  The revised text is aligned with the understanding of
+    the Software WG at their meeting on 2022-09-27.  It also matches the
+    description in `sw/device/lib/dif/_index.md` more closely.
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Andreas Kurth <adk@lowrisc.org>
 
 """
 
