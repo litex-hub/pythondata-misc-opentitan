4bdc3c932605953779e013936758c94b66169645
LiteX Robot <robot@timvideos.us>
2022-10-03 21:17:17 +0000

    Updating pythondata-misc-opentitan to 0.0.post14536
    Updated data to v0.0-14394-g709af834e3 based on 709af834e3ba2b35f958f91e6c30508f73e0ce99 from https://github.com/lowRISC/opentitan.
    > commit 709af834e3ba2b35f958f91e6c30508f73e0ce99
    > Author: Jaedon Kim <jdonjdon@google.com>
    > Date:   Fri Sep 30 03:05:25 2022 +0000
    >
    >     [flash_ctrl,dv] Update filesystem support test
    >
    >     Update filesystem support test(#15037)
    >      - Shift focus from 'erase' to 'double write'
    >      - Improve access resolution from 'page' to '8bytes'
    >
    >     Signed-off-by: Jaedon Kim <jdonjdon@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,38 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14535"
-version_tuple = (0, 0, 14535)
+version_str = "0.0.post14536"
+version_tuple = (0, 0, 14536)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14535")
+    pversion = V("0.0.post14536")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14393"
-data_version_tuple = (0, 0, 14393)
+data_version_str = "0.0.post14394"
+data_version_tuple = (0, 0, 14394)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14393")
+    pdata_version = V("0.0.post14394")
 except ImportError:
     pass
-data_git_hash = "0ea3c9ccf15917b9e61bfdef6c0637c5c2a42cf0"
-data_git_describe = "v0.0-14393-g0ea3c9ccf1"
+data_git_hash = "709af834e3ba2b35f958f91e6c30508f73e0ce99"
+data_git_describe = "v0.0-14394-g709af834e3"
 data_git_msg = """\
-commit 0ea3c9ccf15917b9e61bfdef6c0637c5c2a42cf0
-Author: Timothy Trippel <ttrippel@google.com>
-Date:   Thu Sep 29 17:14:30 2022 -0700
+commit 709af834e3ba2b35f958f91e6c30508f73e0ce99
+Author: Jaedon Kim <jdonjdon@google.com>
+Date:   Fri Sep 30 03:05:25 2022 +0000
 
-    [top] compute virtual mem size from eflash size
+    [flash_ctrl,dv] Update filesystem support test
     
-    This updates the toplevel linker script template to compute the virtual
-    slot region size from the eflash size, since they size of eflash is
-    different between Earlgrey and English Breakfast toplevels, and cause
-    the English Breakfast builds to fail now that address translation is
-    added to test ROM.
+    Update filesystem support test(#15037)
+     - Shift focus from 'erase' to 'double write'
+     - Improve access resolution from 'page' to '8bytes'
     
-    Signed-off-by: Timothy Trippel <ttrippel@google.com>
+    Signed-off-by: Jaedon Kim <jdonjdon@google.com>
 
 """
 
