e382dcac7c46220e0cc5ae6bc8b25c319056cd6f
LiteX Robot <robot@timvideos.us>
2022-10-03 17:38:32 +0000

    Updating pythondata-misc-opentitan to 0.0.post14535
    Updated data to v0.0-14393-g0ea3c9ccf1 based on 0ea3c9ccf15917b9e61bfdef6c0637c5c2a42cf0 from https://github.com/lowRISC/opentitan.
    > commit 0ea3c9ccf15917b9e61bfdef6c0637c5c2a42cf0
    > Author: Timothy Trippel <ttrippel@google.com>
    > Date:   Thu Sep 29 17:14:30 2022 -0700
    >
    >     [top] compute virtual mem size from eflash size
    >
    >     This updates the toplevel linker script template to compute the virtual
    >     slot region size from the eflash size, since they size of eflash is
    >     different between Earlgrey and English Breakfast toplevels, and cause
    >     the English Breakfast builds to fail now that address translation is
    >     added to test ROM.
    >
    >     Signed-off-by: Timothy Trippel <ttrippel@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,46 +4,38 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14532"
-version_tuple = (0, 0, 14532)
+version_str = "0.0.post14535"
+version_tuple = (0, 0, 14535)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14532")
+    pversion = V("0.0.post14535")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14390"
-data_version_tuple = (0, 0, 14390)
+data_version_str = "0.0.post14393"
+data_version_tuple = (0, 0, 14393)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14390")
+    pdata_version = V("0.0.post14393")
 except ImportError:
     pass
-data_git_hash = "cfe71b5140093f4a1aa95dce80b4d3b81d333de3"
-data_git_describe = "v0.0-14390-gcfe71b5140"
+data_git_hash = "0ea3c9ccf15917b9e61bfdef6c0637c5c2a42cf0"
+data_git_describe = "v0.0-14393-g0ea3c9ccf1"
 data_git_msg = """\
-commit cfe71b5140093f4a1aa95dce80b4d3b81d333de3
-Author: Andreas Kurth <adk@lowrisc.org>
-Date:   Tue Sep 27 15:53:17 2022 +0000
+commit 0ea3c9ccf15917b9e61bfdef6c0637c5c2a42cf0
+Author: Timothy Trippel <ttrippel@google.com>
+Date:   Thu Sep 29 17:14:30 2022 -0700
 
-    [doc] Update motivation and objectives of DIFs
+    [top] compute virtual mem size from eflash size
     
-    The current DIF reference manual (RM) text emphasizes error checking and
-    robustness of DIFs quite heavily ("making it difficult, or impossible,
-    to misuse [hardware]." and "providing example code to aid the
-    implementation of reference firmware including end-consumer
-    applications").  However, DIFs are called in test code, some of which
-    deliberately misuses a hardware module, so error checking and robustness
-    are not a primary objective for DIFs and could hinder their use for
-    tests.
+    This updates the toplevel linker script template to compute the virtual
+    slot region size from the eflash size, since they size of eflash is
+    different between Earlgrey and English Breakfast toplevels, and cause
+    the English Breakfast builds to fail now that address translation is
+    added to test ROM.
     
-    This PR changes the wording of the DIF RM to de-emphasize error checking
-    and robustness.  The revised text is aligned with the understanding of
-    the Software WG at their meeting on 2022-09-27.  It also matches the
-    description in `sw/device/lib/dif/_index.md` more closely.
-    
-    Signed-off-by: Andreas Kurth <adk@lowrisc.org>
+    Signed-off-by: Timothy Trippel <ttrippel@google.com>
 
 """
 
