84795f49c7a5f7cb54ab40a1d0bfba99383767ec
LiteX Robot <robot@timvideos.us>
2022-10-04 23:15:39 +0000

    Updating pythondata-misc-opentitan to 0.0.post14553
    Updated data to v0.0-14411-g101063ebdf based on 101063ebdfd099f8eefaf2c80ae0cfbf5a5cb8f9 from https://github.com/lowRISC/opentitan.
    > commit 101063ebdfd099f8eefaf2c80ae0cfbf5a5cb8f9
    > Author: Timothy Trippel <ttrippel@google.com>
    > Date:   Mon Oct 3 21:48:37 2022 -0700
    >
    >     [tests] add example top-level concurrency test
    >
    >     This adds an example top-level concurrency test to showcase how to use
    >     the OTTF to launch, and context switch between several concurrent FreeRTOS
    >     tasks.
    >
    >     Signed-off-by: Timothy Trippel <ttrippel@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14549"
-version_tuple = (0, 0, 14549)
+version_str = "0.0.post14553"
+version_tuple = (0, 0, 14553)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14549")
+    pversion = V("0.0.post14553")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14407"
-data_version_tuple = (0, 0, 14407)
+data_version_str = "0.0.post14411"
+data_version_tuple = (0, 0, 14411)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14407")
+    pdata_version = V("0.0.post14411")
 except ImportError:
     pass
-data_git_hash = "2512ca0da80635a4677c522b1f8b8b8d5bc23789"
-data_git_describe = "v0.0-14407-g2512ca0da8"
+data_git_hash = "101063ebdfd099f8eefaf2c80ae0cfbf5a5cb8f9"
+data_git_describe = "v0.0-14411-g101063ebdf"
 data_git_msg = """\
-commit 2512ca0da80635a4677c522b1f8b8b8d5bc23789
-Author: Weicai Yang <weicai@google.com>
-Date:   Mon Oct 3 17:00:07 2022 -0700
+commit 101063ebdfd099f8eefaf2c80ae0cfbf5a5cb8f9
+Author: Timothy Trippel <ttrippel@google.com>
+Date:   Mon Oct 3 21:48:37 2022 -0700
 
-    [spi_device/dv] Add tpm_read_hw_reg test and update scb to check HW regs
+    [tests] add example top-level concurrency test
     
-    1. Create a test that has both SW and host access the HW regs to test #15134
-    2. Consolidate tpm init tasks
-    3. Update scb to allow SPI host to read back either the new value or the old one.
+    This adds an example top-level concurrency test to showcase how to use
+    the OTTF to launch, and context switch between several concurrent FreeRTOS
+    tasks.
     
-    Signed-off-by: Weicai Yang <weicai@google.com>
+    Signed-off-by: Timothy Trippel <ttrippel@google.com>
 
 """
 
