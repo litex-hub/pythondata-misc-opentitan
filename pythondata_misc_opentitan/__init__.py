82a677936901cd98b2a15b80e3c29a9b4f396c8c
LiteX Robot <robot@timvideos.us>
2022-10-04 20:19:11 +0000

    Updating pythondata-misc-opentitan to 0.0.post14544
    Updated data to v0.0-14402-g3cd03728a9 based on 3cd03728a90b2e5b6d55b9d79a27fd64555a2082 from https://github.com/lowRISC/opentitan.
    > commit 3cd03728a90b2e5b6d55b9d79a27fd64555a2082
    > Author: Weicai Yang <weicai@google.com>
    > Date:   Mon Oct 3 16:41:21 2022 -0700
    >
    >     [spi_device] Fix CmdAddrInfo_A assertion
    >
    >     Need to add a condition - is_tpm_reg before checking the addr
    >     Without is_tpm_reg, locality won't be latched
    >
    >     Signed-off-by: Weicai Yang <weicai@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,62 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14543"
-version_tuple = (0, 0, 14543)
+version_str = "0.0.post14544"
+version_tuple = (0, 0, 14544)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14543")
+    pversion = V("0.0.post14544")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14401"
-data_version_tuple = (0, 0, 14401)
+data_version_str = "0.0.post14402"
+data_version_tuple = (0, 0, 14402)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14401")
+    pdata_version = V("0.0.post14402")
 except ImportError:
     pass
-data_git_hash = "7aee16bd0c2cc7543a3ec719894830b8d88befc6"
-data_git_describe = "v0.0-14401-g7aee16bd0c"
+data_git_hash = "3cd03728a90b2e5b6d55b9d79a27fd64555a2082"
+data_git_describe = "v0.0-14402-g3cd03728a9"
 data_git_msg = """\
-commit 7aee16bd0c2cc7543a3ec719894830b8d88befc6
-Author: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
-Date:   Wed Aug 31 10:46:22 2022 -0700
+commit 3cd03728a90b2e5b6d55b9d79a27fd64555a2082
+Author: Weicai Yang <weicai@google.com>
+Date:   Mon Oct 3 16:41:21 2022 -0700
 
-    [entropy_src/rtl] Cleanup timing enable network
+    [spi_device] Fix CmdAddrInfo_A assertion
     
-    As discussed in PR #14660 there are many places where enable-clearing
-    RNG data or masking internal control signals can cause verification
-    challenges. This is a primary motivation for keeping delayed copies of
-    the enable signals, to apply delayed enable signals at the appropriate
-    points in the pipeline.
+    Need to add a condition - is_tpm_reg before checking the addr
+    Without is_tpm_reg, locality won't be latched
     
-    Such fine-tooth timing management of the enable/disable signals is not
-    good for maintainability.  This commit aims to simplify the network of
-    enable signals in the entropy_src core, while fixing a number of
-    regression failures.
-    
-    Data pathways that had previously been blocked the instance the module
-    was disabled have now been allowed to flow, letting data propagate until
-    it stalls at the SHA3 conditioner or gets dropped at the bypass FIFO.
-    
-    At the same time, this commit simplifies and documents the timing of the
-    module enable signals, both the `es_enable` level signal as well as the
-    `module_enable_pulse`.
-    
-    The module_enable_pulse is now one cycle long, and generated from an
-    explicit flop. (As opposed to using a prim_mubi4_sync),  AsyncOn=1. The
-    es_enable level signal starts in the cycle right after the
-    module_enable_pulse, but falls immediately when the module_enable
-    register is set to MuBi4False.
-    
-    Since both the enable pulse, and the enable level signal are derived
-    from the same Mubi field, they are fanned-out in MuBi representation.
-    
-    All of the prim_mubi_sync's now use AsyncOn(0).
-    
-    Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
+    Signed-off-by: Weicai Yang <weicai@google.com>
 
 """
 
