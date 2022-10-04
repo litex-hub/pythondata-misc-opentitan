6aaccb8c24df5f938f9a2ca1257831b297477e7f
LiteX Robot <robot@timvideos.us>
2022-10-04 17:36:42 +0000

    Updating pythondata-misc-opentitan to 0.0.post14542
    Updated data to v0.0-14400-gc9fece32a8 based on c9fece32a8e753c0d1975ec8034c11be0998a6c8 from https://github.com/lowRISC/opentitan.
    > commit c9fece32a8e753c0d1975ec8034c11be0998a6c8
    > Author: Jaedon Kim <jdonjdon@google.com>
    > Date:   Tue Oct 4 02:46:30 2022 +0000
    >
    >     [chip,dv] add run time limit for chip_sw_pwrmgr_b2b_sleep_reset_req
    >
    >     Signed-off-by: Jaedon Kim <jdonjdon@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,37 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14540"
-version_tuple = (0, 0, 14540)
+version_str = "0.0.post14542"
+version_tuple = (0, 0, 14542)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14540")
+    pversion = V("0.0.post14542")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14398"
-data_version_tuple = (0, 0, 14398)
+data_version_str = "0.0.post14400"
+data_version_tuple = (0, 0, 14400)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14398")
+    pdata_version = V("0.0.post14400")
 except ImportError:
     pass
-data_git_hash = "81b7f4049e18a077da549f860beeb201d0471bd2"
-data_git_describe = "v0.0-14398-g81b7f4049e"
+data_git_hash = "c9fece32a8e753c0d1975ec8034c11be0998a6c8"
+data_git_describe = "v0.0-14400-gc9fece32a8"
 data_git_msg = """\
-commit 81b7f4049e18a077da549f860beeb201d0471bd2
-Author: Cindy Chen <chencindy@opentitan.org>
-Date:   Mon Oct 3 15:42:14 2022 -0700
+commit c9fece32a8e753c0d1975ec8034c11be0998a6c8
+Author: Jaedon Kim <jdonjdon@google.com>
+Date:   Tue Oct 4 02:46:30 2022 +0000
 
-    [dv/kmac] Change kmac_fsm to terminal error to base exclusion file
+    [chip,dv] add run time limit for chip_sw_pwrmgr_b2b_sleep_reset_req
     
-    This PR changes the masked version of the kmac state to terminal error
-    state exclusion file to a base exclusion file.
-    Because both the masked and unmasked versions share the same exclusion
-    items.
-    
-    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
+    Signed-off-by: Jaedon Kim <jdonjdon@google.com>
 
 """
 
