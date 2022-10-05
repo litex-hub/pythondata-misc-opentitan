58179c54a9b278aa3ec58ffb2c3611da071827ca
LiteX Robot <robot@timvideos.us>
2022-10-05 21:14:37 +0000

    Updating pythondata-misc-opentitan to 0.0.post14569
    Updated data to v0.0-14427-gcf36305f59 based on cf36305f59649e5ebd9c7d023dd34df4c80b7cd8 from https://github.com/lowRISC/opentitan.
    > commit cf36305f59649e5ebd9c7d023dd34df4c80b7cd8
    > Author: Miles Dai <milesdai@google.com>
    > Date:   Mon Oct 3 13:18:09 2022 -0400
    >
    >     [lint] Fix buildifier linting issues
    >
    >     Signed-off-by: Miles Dai <milesdai@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14566"
-version_tuple = (0, 0, 14566)
+version_str = "0.0.post14569"
+version_tuple = (0, 0, 14569)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14566")
+    pversion = V("0.0.post14569")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14424"
-data_version_tuple = (0, 0, 14424)
+data_version_str = "0.0.post14427"
+data_version_tuple = (0, 0, 14427)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14424")
+    pdata_version = V("0.0.post14427")
 except ImportError:
     pass
-data_git_hash = "6c710842200064ace0bcf6da58605e6ce57fb1b4"
-data_git_describe = "v0.0-14424-g6c71084220"
+data_git_hash = "cf36305f59649e5ebd9c7d023dd34df4c80b7cd8"
+data_git_describe = "v0.0-14427-gcf36305f59"
 data_git_msg = """\
-commit 6c710842200064ace0bcf6da58605e6ce57fb1b4
-Author: Guillermo Maturana <maturana@google.com>
-Date:   Thu Sep 29 09:58:07 2022 -0700
+commit cf36305f59649e5ebd9c7d023dd34df4c80b7cd8
+Author: Miles Dai <milesdai@google.com>
+Date:   Mon Oct 3 13:18:09 2022 -0400
 
-    [rtl/prim] Fix some prim_esc_receiver SVAs
+    [lint] Fix buildifier linting issues
     
-    Delay the start of EscRespCheck_A by a cycle because it uses $past.
-    Minor formatting changes.
-    
-    Signed-off-by: Guillermo Maturana <maturana@google.com>
+    Signed-off-by: Miles Dai <milesdai@google.com>
 
 """
 
