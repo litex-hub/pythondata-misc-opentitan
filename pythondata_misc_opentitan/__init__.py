85cc243f20190c604f13a5d40f2f66aaf9df0791
LiteX Robot <robot@timvideos.us>
2022-10-05 01:07:32 +0000

    Updating pythondata-misc-opentitan to 0.0.post14554
    Updated data to v0.0-14412-g71f19edb3f based on 71f19edb3fd7331291abd6c2bbe5c51557faba39 from https://github.com/lowRISC/opentitan.
    > commit 71f19edb3fd7331291abd6c2bbe5c51557faba39
    > Author: Eli Kim <eli@opentitan.org>
    > Date:   Tue Oct 4 15:12:08 2022 -0700
    >
    >     fix(spid): Latch FIFOADDR at correct time
    >
    >     _Related Issue: https://github.com/lowRISC/opentitan/issues/15266_
    >
    >     This PR fixes a bug for unaligned access in TPM read commands.
    >
    >     fifoaddr is latched with the counter being `'h1F`. The counter, however,
    >     runs on SPI_CLK, while the latching logic is running on inverted SPI_CLK
    >     domain.
    >
    >     As a result, the latching logic captures the fifo address at a cycle
    >     earlier with prematured address. So it always assume address[1] as
    >     address[0].
    >
    >     This commit fixes the issue by:
    >
    >     1. Convert `sck_fifoaddr_latch` into iSCK domain.
    >     2. To not shift the address one bit more, use `sck_cmdaddr_wdata_q`
    >        rather than `sck_cmdaddr_wdata_d`.
    >
    >     Signed-off-by: Eli Kim <eli@opentitan.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,50 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14553"
-version_tuple = (0, 0, 14553)
+version_str = "0.0.post14554"
+version_tuple = (0, 0, 14554)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14553")
+    pversion = V("0.0.post14554")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14411"
-data_version_tuple = (0, 0, 14411)
+data_version_str = "0.0.post14412"
+data_version_tuple = (0, 0, 14412)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14411")
+    pdata_version = V("0.0.post14412")
 except ImportError:
     pass
-data_git_hash = "101063ebdfd099f8eefaf2c80ae0cfbf5a5cb8f9"
-data_git_describe = "v0.0-14411-g101063ebdf"
+data_git_hash = "71f19edb3fd7331291abd6c2bbe5c51557faba39"
+data_git_describe = "v0.0-14412-g71f19edb3f"
 data_git_msg = """\
-commit 101063ebdfd099f8eefaf2c80ae0cfbf5a5cb8f9
-Author: Timothy Trippel <ttrippel@google.com>
-Date:   Mon Oct 3 21:48:37 2022 -0700
+commit 71f19edb3fd7331291abd6c2bbe5c51557faba39
+Author: Eli Kim <eli@opentitan.org>
+Date:   Tue Oct 4 15:12:08 2022 -0700
 
-    [tests] add example top-level concurrency test
+    fix(spid): Latch FIFOADDR at correct time
     
-    This adds an example top-level concurrency test to showcase how to use
-    the OTTF to launch, and context switch between several concurrent FreeRTOS
-    tasks.
+    _Related Issue: https://github.com/lowRISC/opentitan/issues/15266_
     
-    Signed-off-by: Timothy Trippel <ttrippel@google.com>
+    This PR fixes a bug for unaligned access in TPM read commands.
+    
+    fifoaddr is latched with the counter being `'h1F`. The counter, however,
+    runs on SPI_CLK, while the latching logic is running on inverted SPI_CLK
+    domain.
+    
+    As a result, the latching logic captures the fifo address at a cycle
+    earlier with prematured address. So it always assume address[1] as
+    address[0].
+    
+    This commit fixes the issue by:
+    
+    1. Convert `sck_fifoaddr_latch` into iSCK domain.
+    2. To not shift the address one bit more, use `sck_cmdaddr_wdata_q`
+       rather than `sck_cmdaddr_wdata_d`.
+    
+    Signed-off-by: Eli Kim <eli@opentitan.org>
 
 """
 
