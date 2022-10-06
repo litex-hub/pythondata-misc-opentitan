16f6f9a35723ab833872663aaf44c9443c238725
LiteX Robot <robot@timvideos.us>
2022-10-06 23:18:38 +0000

    Updating pythondata-misc-opentitan to 0.0.post14604
    Updated data to v0.0-14462-g691ebe2235 based on 691ebe22351e94e566518f020330114451cdc6c6 from https://github.com/lowRISC/opentitan.
    > commit 691ebe22351e94e566518f020330114451cdc6c6
    > Author: Guillermo Maturana <maturana@google.com>
    > Date:   Wed Oct 5 14:27:43 2022 -0700
    >
    >     [dv,top] Share common code in pwrmgr_*sleep_all_wake_ups
    >
    >     This facilitates the creation of a random version of these tests,
    >     and removes redundant code.
    >
    >     Signed-off-by: Guillermo Maturana <maturana@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14603"
-version_tuple = (0, 0, 14603)
+version_str = "0.0.post14604"
+version_tuple = (0, 0, 14604)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14603")
+    pversion = V("0.0.post14604")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14461"
-data_version_tuple = (0, 0, 14461)
+data_version_str = "0.0.post14462"
+data_version_tuple = (0, 0, 14462)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14461")
+    pdata_version = V("0.0.post14462")
 except ImportError:
     pass
-data_git_hash = "a3f6f6bceae186b732a07ceec350f54af85ffda1"
-data_git_describe = "v0.0-14461-ga3f6f6bcea"
+data_git_hash = "691ebe22351e94e566518f020330114451cdc6c6"
+data_git_describe = "v0.0-14462-g691ebe2235"
 data_git_msg = """\
-commit a3f6f6bceae186b732a07ceec350f54af85ffda1
-Author: Miguel Osorio <miguelosorio@google.com>
-Date:   Thu Oct 6 12:52:24 2022 -0700
+commit 691ebe22351e94e566518f020330114451cdc6c6
+Author: Guillermo Maturana <maturana@google.com>
+Date:   Wed Oct 5 14:27:43 2022 -0700
 
-    [top-test] entropy_src_csrng adjust timeout
+    [dv,top] Share common code in pwrmgr_*sleep_all_wake_ups
     
-    Adjust test timeout to fix nightly regression failure.
+    This facilitates the creation of a random version of these tests,
+    and removes redundant code.
     
-    Signed-off-by: Miguel Osorio <miguelosorio@google.com>
+    Signed-off-by: Guillermo Maturana <maturana@google.com>
 
 """
 
