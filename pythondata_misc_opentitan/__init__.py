5d19e40ac07682216d596726547c64ab0c53ae31
LiteX Robot <robot@timvideos.us>
2022-10-07 01:11:40 +0000

    Updating pythondata-misc-opentitan to 0.0.post14606
    Updated data to v0.0-14464-g1bc7ba3536 based on 1bc7ba3536a2240ef209e9acd4b7fed1e32cac91 from https://github.com/lowRISC/opentitan.
    > commit 1bc7ba3536a2240ef209e9acd4b7fed1e32cac91
    > Author: Eli Kim <eli@opentitan.org>
    > Date:   Thu Oct 6 11:24:37 2022 -0700
    >
    >     feat(chip): Scaffolds SV test env for sleep_pin_retention
    >
    >     Signed-off-by: Eli Kim <eli@opentitan.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14604"
-version_tuple = (0, 0, 14604)
+version_str = "0.0.post14606"
+version_tuple = (0, 0, 14606)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14604")
+    pversion = V("0.0.post14606")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14462"
-data_version_tuple = (0, 0, 14462)
+data_version_str = "0.0.post14464"
+data_version_tuple = (0, 0, 14464)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14462")
+    pdata_version = V("0.0.post14464")
 except ImportError:
     pass
-data_git_hash = "691ebe22351e94e566518f020330114451cdc6c6"
-data_git_describe = "v0.0-14462-g691ebe2235"
+data_git_hash = "1bc7ba3536a2240ef209e9acd4b7fed1e32cac91"
+data_git_describe = "v0.0-14464-g1bc7ba3536"
 data_git_msg = """\
-commit 691ebe22351e94e566518f020330114451cdc6c6
-Author: Guillermo Maturana <maturana@google.com>
-Date:   Wed Oct 5 14:27:43 2022 -0700
+commit 1bc7ba3536a2240ef209e9acd4b7fed1e32cac91
+Author: Eli Kim <eli@opentitan.org>
+Date:   Thu Oct 6 11:24:37 2022 -0700
 
-    [dv,top] Share common code in pwrmgr_*sleep_all_wake_ups
+    feat(chip): Scaffolds SV test env for sleep_pin_retention
     
-    This facilitates the creation of a random version of these tests,
-    and removes redundant code.
-    
-    Signed-off-by: Guillermo Maturana <maturana@google.com>
+    Signed-off-by: Eli Kim <eli@opentitan.org>
 
 """
 
