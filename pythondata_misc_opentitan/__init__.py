d7d84ecbbbc426564ac27ed43e4da821c7175f36
LiteX Robot <robot@timvideos.us>
2022-10-07 19:19:14 +0000

    Updating pythondata-misc-opentitan to 0.0.post14619
    Updated data to v0.0-14477-gdf662bf3de based on df662bf3dee4df9b99296054a139daee867153d5 from https://github.com/lowRISC/opentitan.
    > commit df662bf3dee4df9b99296054a139daee867153d5
    > Author: Guillermo Maturana <maturana@google.com>
    > Date:   Wed Sep 28 21:44:38 2022 -0700
    >
    >     [dv/pwrmgr] Fixes for recent RTL changes
    >
    >     The change in handling of reset had a big impact on DV.
    >     Some test sequences need to be adjusted to stop during lc reset or clock
    >     stops, and reset handling needs to change.
    >     The pwrmgr_clk_ctrl_agent was out of sync with the counterpart in
    >     pwrmgr_base_vseq, so we are moving all functionality to the latter.
    >     This PR disables that agent and a subsequent one will remove it.
    >
    >     Signed-off-by: Guillermo Maturana <maturana@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,39 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14618"
-version_tuple = (0, 0, 14618)
+version_str = "0.0.post14619"
+version_tuple = (0, 0, 14619)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14618")
+    pversion = V("0.0.post14619")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14476"
-data_version_tuple = (0, 0, 14476)
+data_version_str = "0.0.post14477"
+data_version_tuple = (0, 0, 14477)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14476")
+    pdata_version = V("0.0.post14477")
 except ImportError:
     pass
-data_git_hash = "3db50d206f0289eb1c9e0c2d36d67c8b85bf1bc5"
-data_git_describe = "v0.0-14476-g3db50d206f"
+data_git_hash = "df662bf3dee4df9b99296054a139daee867153d5"
+data_git_describe = "v0.0-14477-gdf662bf3de"
 data_git_msg = """\
-commit 3db50d206f0289eb1c9e0c2d36d67c8b85bf1bc5
-Author: Timothy Chen <timothytim@google.com>
-Date:   Thu Oct 6 17:02:46 2022 -0700
+commit df662bf3dee4df9b99296054a139daee867153d5
+Author: Guillermo Maturana <maturana@google.com>
+Date:   Wed Sep 28 21:44:38 2022 -0700
 
-    [rv_core_ibex] Documentation fix
+    [dv/pwrmgr] Fixes for recent RTL changes
     
-    - addresses the documentation portion of #15052
+    The change in handling of reset had a big impact on DV.
+    Some test sequences need to be adjusted to stop during lc reset or clock
+    stops, and reset handling needs to change.
+    The pwrmgr_clk_ctrl_agent was out of sync with the counterpart in
+    pwrmgr_base_vseq, so we are moving all functionality to the latter.
+    This PR disables that agent and a subsequent one will remove it.
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Guillermo Maturana <maturana@google.com>
 
 """
 
