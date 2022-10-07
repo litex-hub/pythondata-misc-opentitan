872fd8d30cbbcf228bbd0c9df4f48ebdba62aef5
LiteX Robot <robot@timvideos.us>
2022-10-07 23:17:34 +0000

    Updating pythondata-misc-opentitan to 0.0.post14626
    Updated data to v0.0-14484-g760a0b635e based on 760a0b635e060d1b7a2087aede139fca9bd6da98 from https://github.com/lowRISC/opentitan.
    > commit 760a0b635e060d1b7a2087aede139fca9bd6da98
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Fri Oct 7 12:31:12 2022 -0700
    >
    >     [dv/chip] Remove uart agent coverage in chip level
    >
    >     Branch from PR #15325, this PR skips creating the uart agent functional
    >     coverage for chip level testing.
    >     Thanks @sri and @weicai for the suggetions!
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,42 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14625"
-version_tuple = (0, 0, 14625)
+version_str = "0.0.post14626"
+version_tuple = (0, 0, 14626)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14625")
+    pversion = V("0.0.post14626")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14483"
-data_version_tuple = (0, 0, 14483)
+data_version_str = "0.0.post14484"
+data_version_tuple = (0, 0, 14484)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14483")
+    pdata_version = V("0.0.post14484")
 except ImportError:
     pass
-data_git_hash = "8b107c473157f391d25654168b62911b02f3d205"
-data_git_describe = "v0.0-14483-g8b107c4731"
+data_git_hash = "760a0b635e060d1b7a2087aede139fca9bd6da98"
+data_git_describe = "v0.0-14484-g760a0b635e"
 data_git_msg = """\
-commit 8b107c473157f391d25654168b62911b02f3d205
-Author: Timothy Chen <timothytim@google.com>
-Date:   Wed Oct 5 14:37:36 2022 -0700
+commit 760a0b635e060d1b7a2087aede139fca9bd6da98
+Author: Cindy Chen <chencindy@opentitan.org>
+Date:   Fri Oct 7 12:31:12 2022 -0700
 
-    [top/dv] Add exit_test_unlock test.
+    [dv/chip] Remove uart agent coverage in chip level
     
-    The primary difference between this test and the existing unlock
-    test is that this sequence tries to simulate the liklihood system
-    usage.
+    Branch from PR #15325, this PR skips creating the uart agent functional
+    coverage for chip level testing.
+    Thanks @sri and @weicai for the suggetions!
     
-    Specifically, the ROM_EXEC_EN and the advance to PROD are done in
-    the same power cycle.  Upon reboot, an image is then bootstrap
-    into the device to ensure the behavior is correct.
-    
-    The bootstrap portion for this test is still missing.
-    
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
 """
 
