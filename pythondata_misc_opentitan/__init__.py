eaf37361fb415919ebe0d71debe05df67f3ee07e
LiteX Robot <robot@timvideos.us>
2022-10-11 20:18:15 +0000

    Updating pythondata-misc-opentitan to 0.0.post14678
    Updated data to v0.0-14536-gf0d65e65ae based on f0d65e65aeefdc0ca79097acf7c79e1fd683ac92 from https://github.com/lowRISC/opentitan.
    > commit f0d65e65aeefdc0ca79097acf7c79e1fd683ac92
    > Author: Andreas Kurth <adk@lowrisc.org>
    > Date:   Fri Sep 30 17:24:32 2022 +0000
    >
    >     [top/dv] Add test glitching outputs of Ibex or lockstep core
    >
    >     This commit contributes to resolving lowRISC/ibex#1757.  A subsequent
    >     commit shall extend this test to also glitch the inputs of the lockstep
    >     core.
    >
    >     Signed-off-by: Andreas Kurth <adk@lowrisc.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,41 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14673"
-version_tuple = (0, 0, 14673)
+version_str = "0.0.post14678"
+version_tuple = (0, 0, 14678)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14673")
+    pversion = V("0.0.post14678")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14531"
-data_version_tuple = (0, 0, 14531)
+data_version_str = "0.0.post14536"
+data_version_tuple = (0, 0, 14536)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14531")
+    pdata_version = V("0.0.post14536")
 except ImportError:
     pass
-data_git_hash = "08334590b8b26212fc4525f4b5cb14d3ec5fdec3"
-data_git_describe = "v0.0-14531-g08334590b8"
+data_git_hash = "f0d65e65aeefdc0ca79097acf7c79e1fd683ac92"
+data_git_describe = "v0.0-14536-gf0d65e65ae"
 data_git_msg = """\
-commit 08334590b8b26212fc4525f4b5cb14d3ec5fdec3
-Author: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
-Date:   Mon Oct 10 09:56:47 2022 -0700
+commit f0d65e65aeefdc0ca79097acf7c79e1fd683ac92
+Author: Andreas Kurth <adk@lowrisc.org>
+Date:   Fri Sep 30 17:24:32 2022 +0000
 
-    [entropy_src/dv] Refactor scoreboarding collect_entropy
+    [top/dv] Add test glitching outputs of Ibex or lockstep core
     
-    When scoreboarding results from the ExtHT agent the scoreboard needs to
-    wait for the ExtHT agent to report back.  Since the agent is slightly
-    delayed by the DUT, this means that the health test scoring must also
-    be delayed.
+    This commit contributes to resolving lowRISC/ibex#1757.  A subsequent
+    commit shall extend this test to also glitch the inputs of the lockstep
+    core.
     
-    This commit breaks entropy collection and health test scoring into
-    separate threads so that the health test scoring can be done at the
-    appropriate time.
-    
-    Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
+    Signed-off-by: Andreas Kurth <adk@lowrisc.org>
 
 """
 
