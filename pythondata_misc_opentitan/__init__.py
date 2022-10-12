44abb42b68fe90598066a5439e0a328592dc59c6
LiteX Robot <robot@timvideos.us>
2022-10-12 23:15:42 +0000

    Updating pythondata-misc-opentitan to 0.0.post14713
    Updated data to v0.0-14571-gd072ac505f based on d072ac505f82152678d6e04be95c72b728a347b8 from https://github.com/lowRISC/opentitan.
    > commit d072ac505f82152678d6e04be95c72b728a347b8
    > Author: Jes B. Klinke <jbk@chromium.org>
    > Date:   Fri Oct 7 14:54:34 2022 -0700
    >
    >     [opentitantool] Support multi-layer alias conf
    >
    >     We have an agreed way of connecting HyperDebug to CW310, which should be
    >     declared in a JSON conf file, such that opentitan tool understands a
    >     name such as A3 to be an alias of the HyperDebug native pin CN7_18.
    >
    >     Separately, the ChromeOS team has a particular idea of how to use the
    >     OpenTitan chip pins, and wishes to use the name AP_FLASH_SEL as an alias
    >     for A3.  This is mostly independent of CW310 and HyperDebug, and would
    >     apply even to future ASIC development board.
    >
    >     Because of the above, I foresee that we would want to simultaneously
    >     provide a CW310/HyperDebug configuration file, and a CromeOS/EarlGrey
    >     configuration file to OpenTitan tool.
    >
    >     This PR allows the --conf option to take multiple arguments, and it
    >     adapts the alias resolution logic to be transitive.
    >
    >     Change-Id: I6f8a328f269dfee246a19c4d3bae9fd60f056aec
    >     Signed-off-by: Jes B. Klinke <jbk@chromium.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,49 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14707"
-version_tuple = (0, 0, 14707)
+version_str = "0.0.post14713"
+version_tuple = (0, 0, 14713)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14707")
+    pversion = V("0.0.post14713")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14565"
-data_version_tuple = (0, 0, 14565)
+data_version_str = "0.0.post14571"
+data_version_tuple = (0, 0, 14571)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14565")
+    pdata_version = V("0.0.post14571")
 except ImportError:
     pass
-data_git_hash = "a1ae9ba979aa1b47f978d8f5cd9bca6501f8e891"
-data_git_describe = "v0.0-14565-ga1ae9ba979"
+data_git_hash = "d072ac505f82152678d6e04be95c72b728a347b8"
+data_git_describe = "v0.0-14571-gd072ac505f"
 data_git_msg = """\
-commit a1ae9ba979aa1b47f978d8f5cd9bca6501f8e891
-Author: Jaedon Kim <jdonjdon@google.com>
-Date:   Tue Oct 11 23:40:48 2022 +0000
+commit d072ac505f82152678d6e04be95c72b728a347b8
+Author: Jes B. Klinke <jbk@chromium.org>
+Date:   Fri Oct 7 14:54:34 2022 -0700
 
-    [flash_ctrl,dv] Add clocking block and input output slack
+    [opentitantool] Support multi-layer alias conf
     
-    Signed-off-by: Jaedon Kim <jdonjdon@google.com>
+    We have an agreed way of connecting HyperDebug to CW310, which should be
+    declared in a JSON conf file, such that opentitan tool understands a
+    name such as A3 to be an alias of the HyperDebug native pin CN7_18.
+    
+    Separately, the ChromeOS team has a particular idea of how to use the
+    OpenTitan chip pins, and wishes to use the name AP_FLASH_SEL as an alias
+    for A3.  This is mostly independent of CW310 and HyperDebug, and would
+    apply even to future ASIC development board.
+    
+    Because of the above, I foresee that we would want to simultaneously
+    provide a CW310/HyperDebug configuration file, and a CromeOS/EarlGrey
+    configuration file to OpenTitan tool.
+    
+    This PR allows the --conf option to take multiple arguments, and it
+    adapts the alias resolution logic to be transitive.
+    
+    Change-Id: I6f8a328f269dfee246a19c4d3bae9fd60f056aec
+    Signed-off-by: Jes B. Klinke <jbk@chromium.org>
 
 """
 
