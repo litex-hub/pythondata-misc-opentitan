e063573e11c4304ea87930ba48cc6d901ea966a9
LiteX Robot <robot@timvideos.us>
2022-10-12 05:13:32 +0000

    Updating pythondata-misc-opentitan to 0.0.post14692
    Updated data to v0.0-14550-g1752a8bd60 based on 1752a8bd6035ff367837cb6bb9ec69a6098e8c62 from https://github.com/lowRISC/opentitan.
    > commit 1752a8bd6035ff367837cb6bb9ec69a6098e8c62
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Tue Oct 11 16:18:46 2022 -0700
    >
    >     [dv/otp_ctrl] Fix tl_intg_error nightly failure
    >
    >     This PR fixes the TL_intg_error failure in nightly regression.
    >     The issue is that: the prim_otp_tl interface does not set any err_code,
    >     only triggers a prim_otp_alert.
    >     And the tl_intg_err task is share for sec_cm one_hot check and
    >     tl_intg_error check.
    >
    >     So we need to distinguish which tlul interface has integrity and sec_cm
    >     error.
    >     Because these two tests have different input: ral_name and proxy_path,
    >     so otp_ctrl_common_vseq create an internal flag to store which tlul
    >     interface is used.
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,41 +4,44 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14690"
-version_tuple = (0, 0, 14690)
+version_str = "0.0.post14692"
+version_tuple = (0, 0, 14692)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14690")
+    pversion = V("0.0.post14692")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14548"
-data_version_tuple = (0, 0, 14548)
+data_version_str = "0.0.post14550"
+data_version_tuple = (0, 0, 14550)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14548")
+    pdata_version = V("0.0.post14550")
 except ImportError:
     pass
-data_git_hash = "dfb2e5306a82128dc6305d9f4940e44ce14129ed"
-data_git_describe = "v0.0-14548-gdfb2e5306a"
+data_git_hash = "1752a8bd6035ff367837cb6bb9ec69a6098e8c62"
+data_git_describe = "v0.0-14550-g1752a8bd60"
 data_git_msg = """\
-commit dfb2e5306a82128dc6305d9f4940e44ce14129ed
-Author: Michael Schaffner <msf@google.com>
-Date:   Mon Oct 10 21:30:04 2022 -0700
+commit 1752a8bd6035ff367837cb6bb9ec69a6098e8c62
+Author: Cindy Chen <chencindy@opentitan.org>
+Date:   Tue Oct 11 16:18:46 2022 -0700
 
-    [test] Add test for LC access control signal going to OTP
+    [dv/otp_ctrl] Fix tl_intg_error nightly failure
     
-    This test checks the following life cycle signals going to OTP:
-    lc_creator_seed_sw_rw_en
-    lc_seed_hw_rd_en
+    This PR fixes the TL_intg_error failure in nightly regression.
+    The issue is that: the prim_otp_tl interface does not set any err_code,
+    only triggers a prim_otp_alert.
+    And the tl_intg_err task is share for sec_cm one_hot check and
+    tl_intg_error check.
     
-    Since this uses the keymgr, we also test the effects of the
-    lc_keymgr_en signal (enable/disable).
+    So we need to distinguish which tlul interface has integrity and sec_cm
+    error.
+    Because these two tests have different input: ral_name and proxy_path,
+    so otp_ctrl_common_vseq create an internal flag to store which tlul
+    interface is used.
     
-    Fixes #15272 and #14139.
-    
-    Signed-off-by: Michael Schaffner <msf@google.com>
+    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
 """
 
