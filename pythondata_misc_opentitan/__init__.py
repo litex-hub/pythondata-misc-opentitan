c2435c5e80ec6047b26661dfff4e9637c6d0f362
LiteX Robot <robot@timvideos.us>
2022-10-13 03:39:05 +0000

    Updating pythondata-misc-opentitan to 0.0.post14722
    Updated data to v0.0-14580-gbc758e2cd2 based on bc758e2cd2b1624a22370dbe1acdff701ad9fc91 from https://github.com/lowRISC/opentitan.
    > commit bc758e2cd2b1624a22370dbe1acdff701ad9fc91
    > Author: Miguel Osorio <miguelosorio@google.com>
    > Date:   Wed Oct 12 16:18:41 2022 -0700
    >
    >     [top-test] xref csrng_lc_hw_debug_en to test point
    >
    >     Signed-off-by: Miguel Osorio <miguelosorio@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,46 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14718"
-version_tuple = (0, 0, 14718)
+version_str = "0.0.post14722"
+version_tuple = (0, 0, 14722)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14718")
+    pversion = V("0.0.post14722")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14576"
-data_version_tuple = (0, 0, 14576)
+data_version_str = "0.0.post14580"
+data_version_tuple = (0, 0, 14580)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14576")
+    pdata_version = V("0.0.post14580")
 except ImportError:
     pass
-data_git_hash = "dd3d4e46aa33d10ca2d89312345d158fe1a0c6fa"
-data_git_describe = "v0.0-14576-gdd3d4e46aa"
+data_git_hash = "bc758e2cd2b1624a22370dbe1acdff701ad9fc91"
+data_git_describe = "v0.0-14580-gbc758e2cd2"
 data_git_msg = """\
-commit dd3d4e46aa33d10ca2d89312345d158fe1a0c6fa
-Author: Eli Kim <eli@opentitan.org>
-Date:   Wed Oct 12 09:45:33 2022 -0700
+commit bc758e2cd2b1624a22370dbe1acdff701ad9fc91
+Author: Miguel Osorio <miguelosorio@google.com>
+Date:   Wed Oct 12 16:18:41 2022 -0700
 
-    refactor(spid): Change `tpm_header_not_empty` to level
+    [top-test] xref csrng_lc_hw_debug_en to test point
     
-    _Related Issue: https://github.com/lowRISC/opentitan/issues/15282_
-    
-    The current interrupt scheme assumes the input is an event (pulse). If
-    the input is a status, it was recommented to add an edge detector to
-    create a pulse.
-    
-    In certain cases, this may create unwanted (phantom) interrupts or
-    missing interrupts depending on the SW approach to handling the
-    interrupts.
-    
-    To remove the race condition, HW now connects the status signal to the
-    interrupt in this commit. With the change, now SW is responsible to mask
-    the interrupt while the SW queues the interrupt to event queues.
-    
-    Signed-off-by: Eli Kim <eli@opentitan.org>
+    Signed-off-by: Miguel Osorio <miguelosorio@google.com>
 
 """
 
