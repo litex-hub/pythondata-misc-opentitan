b01cdb121372e63bfb910f993b6753b278b7ce34
LiteX Robot <robot@timvideos.us>
2022-10-13 11:16:38 +0000

    Updating pythondata-misc-opentitan to 0.0.post14726
    Updated data to v0.0-14584-gd5f561baac based on d5f561baac6ab140eb76679020bdbb591a3d80d6 from https://github.com/lowRISC/opentitan.
    > commit d5f561baac6ab140eb76679020bdbb591a3d80d6
    > Author: Vladimir Rozic <vrozic@lowrisc.org>
    > Date:   Wed Sep 28 17:14:15 2022 +0100
    >
    >     [topgen] Strong random class.
    >
    >     A class for generating random numbers using an entropy pool.
    >     The intention is to use this class to generate random netlist
    >     constants and permutations.
    >
    >     This commit contains:
    >         1. strong_random.py containing class strong_random()
    >             -Constructor
    >             -load() function for loading the entropy buffer from an
    >             external entropy buffer file.
    >             -getrandbits(k) - a function for fetching k random bits
    >             -randbelow(n) - a function for generating a random
    >             integer smaller than n
    >             -shuffle(x) - for generating a random permutation of x
    >             -bookkeeping and debugging functions isempty() and
    >             printstatus().
    >         In case that the buffer runs out of entropy the class issues
    >         an error and exits.
    >
    >         2. entropy_buffer_generator.py - a script for generating an
    >         entropy file using random.py
    >
    >     Signed-off-by: Vladimir Rozic <vrozic@lowrisc.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,38 +4,53 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14725"
-version_tuple = (0, 0, 14725)
+version_str = "0.0.post14726"
+version_tuple = (0, 0, 14726)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14725")
+    pversion = V("0.0.post14726")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14583"
-data_version_tuple = (0, 0, 14583)
+data_version_str = "0.0.post14584"
+data_version_tuple = (0, 0, 14584)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14583")
+    pdata_version = V("0.0.post14584")
 except ImportError:
     pass
-data_git_hash = "e5de5869cfd24ec6d23d19b2f75e89f7d9a61b7d"
-data_git_describe = "v0.0-14583-ge5de5869cf"
+data_git_hash = "d5f561baac6ab140eb76679020bdbb591a3d80d6"
+data_git_describe = "v0.0-14584-gd5f561baac"
 data_git_msg = """\
-commit e5de5869cfd24ec6d23d19b2f75e89f7d9a61b7d
-Author: Timothy Trippel <ttrippel@google.com>
-Date:   Wed Oct 12 18:41:55 2022 -0700
+commit d5f561baac6ab140eb76679020bdbb591a3d80d6
+Author: Vladimir Rozic <vrozic@lowrisc.org>
+Date:   Wed Sep 28 17:14:15 2022 +0100
 
-    [test_rom] enable icache / Ibex security features
+    [topgen] Strong random class.
     
-    This updates the test ROM to enable the icache (and other cpuctrl
-    controlled Ibex security features) based on the OTP-stored configuration
-    word.
+    A class for generating random numbers using an entropy pool.
+    The intention is to use this class to generate random netlist
+    constants and permutations.
     
-    This fixes #12883.
+    This commit contains:
+        1. strong_random.py containing class strong_random()
+            -Constructor
+            -load() function for loading the entropy buffer from an
+            external entropy buffer file.
+            -getrandbits(k) - a function for fetching k random bits
+            -randbelow(n) - a function for generating a random
+            integer smaller than n
+            -shuffle(x) - for generating a random permutation of x
+            -bookkeeping and debugging functions isempty() and
+            printstatus().
+        In case that the buffer runs out of entropy the class issues
+        an error and exits.
     
-    Signed-off-by: Timothy Trippel <ttrippel@google.com>
+        2. entropy_buffer_generator.py - a script for generating an
+        entropy file using random.py
+    
+    Signed-off-by: Vladimir Rozic <vrozic@lowrisc.org>
 
 """
 
