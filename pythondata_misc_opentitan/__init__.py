bcb53146a23233b1737f85ee5eedef9a576b32e0
LiteX Robot <robot@timvideos.us>
2022-10-14 04:20:53 +0000

    Updating pythondata-misc-opentitan to 0.0.post14739
    Updated data to v0.0-14597-gb3f3b128c7 based on b3f3b128c77846c8cd3d094f51138d9ebc1e0423 from https://github.com/lowRISC/opentitan.
    > commit b3f3b128c77846c8cd3d094f51138d9ebc1e0423
    > Author: Guillermo Maturana <maturana@google.com>
    > Date:   Wed Oct 12 11:31:03 2022 -0700
    >
    >     [dv,chip] Add chip_sw_random_sleep_all_wake_ups
    >
    >     Fixes #14166
    >
    >     Signed-off-by: Guillermo Maturana <maturana@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,38 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14738"
-version_tuple = (0, 0, 14738)
+version_str = "0.0.post14739"
+version_tuple = (0, 0, 14739)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14738")
+    pversion = V("0.0.post14739")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14596"
-data_version_tuple = (0, 0, 14596)
+data_version_str = "0.0.post14597"
+data_version_tuple = (0, 0, 14597)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14596")
+    pdata_version = V("0.0.post14597")
 except ImportError:
     pass
-data_git_hash = "f1288c8633be5f0459e12a3557a4a25dab4eb3d1"
-data_git_describe = "v0.0-14596-gf1288c8633"
+data_git_hash = "b3f3b128c77846c8cd3d094f51138d9ebc1e0423"
+data_git_describe = "v0.0-14597-gb3f3b128c7"
 data_git_msg = """\
-commit f1288c8633be5f0459e12a3557a4a25dab4eb3d1
-Author: Eli Kim <eli@opentitan.org>
-Date:   Thu Oct 13 10:49:07 2022 -0700
+commit b3f3b128c77846c8cd3d094f51138d9ebc1e0423
+Author: Guillermo Maturana <maturana@google.com>
+Date:   Wed Oct 12 11:31:03 2022 -0700
 
-    doc(spid): TPM_CMDADDR_NOTEMPTY interrupt
+    [dv,chip] Add chip_sw_random_sleep_all_wake_ups
     
-    * _Related PR: https://github.com/lowRISC/opentitan/pull/15442_
-    * _Related Issue: https://github.com/lowRISC/opentitan/issues/15282_
+    Fixes #14166
     
-    This commit describes how-to process the `tpm_cmdaddr_notempty`
-    interrupt.
-    
-    Signed-off-by: Eli Kim <eli@opentitan.org>
+    Signed-off-by: Guillermo Maturana <maturana@google.com>
 
 """
 
