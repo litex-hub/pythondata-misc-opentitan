c19df7f94efc7483987e77527d29c5a971a6a74c
LiteX Robot <robot@timvideos.us>
2022-10-14 16:20:36 +0000

    Updating pythondata-misc-opentitan to 0.0.post14746
    Updated data to v0.0-14604-g0f9276d84d based on 0f9276d84df7cd3286914a12ad45d8df186289ff from https://github.com/lowRISC/opentitan.
    > commit 0f9276d84df7cd3286914a12ad45d8df186289ff
    > Author: Chris Frantz <cfrantz@google.com>
    > Date:   Wed Oct 12 11:16:54 2022 -0700
    >
    >     [quality] Add a rust formatting check
    >
    >     Signed-off-by: Chris Frantz <cfrantz@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,51 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14745"
-version_tuple = (0, 0, 14745)
+version_str = "0.0.post14746"
+version_tuple = (0, 0, 14746)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14745")
+    pversion = V("0.0.post14746")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14603"
-data_version_tuple = (0, 0, 14603)
+data_version_str = "0.0.post14604"
+data_version_tuple = (0, 0, 14604)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14603")
+    pdata_version = V("0.0.post14604")
 except ImportError:
     pass
-data_git_hash = "5e4f0a35fc1013616566157d0a8a5fc164391b54"
-data_git_describe = "v0.0-14603-g5e4f0a35fc"
+data_git_hash = "0f9276d84df7cd3286914a12ad45d8df186289ff"
+data_git_describe = "v0.0-14604-g0f9276d84d"
 data_git_msg = """\
-commit 5e4f0a35fc1013616566157d0a8a5fc164391b54
-Author: Pirmin Vogel <vogelpi@lowrisc.org>
-Date:   Mon Oct 10 23:18:36 2022 +0200
+commit 0f9276d84df7cd3286914a12ad45d8df186289ff
+Author: Chris Frantz <cfrantz@google.com>
+Date:   Wed Oct 12 11:16:54 2022 -0700
 
-    [edn, dif] Rework/enable additional data for INS, RES and GEN commands
+    [quality] Add a rust formatting check
     
-    Previously, the different commands had varying and at most incomplete
-    support for sending additional data/seed material to CSRNG, which didn't
-    allow making use of many of the features provided by the hardware:
-    - INS didn't support additional data at all.
-    - RES and GEN were overloading the seed data structure for storing the
-      actual application interface command header. As a result, they were
-      supporting up to 11 words of additional data/seed material only where
-      the hardware allows up to twelve.
-    
-    The main change of this commit is to add a new dif_edn_cmt_t data
-    structure holding the actual application interface command header
-    as well as the existing dif_edn_seed_material_t structure
-    containing the seed material, and then rework the
-    dif_edn_set_auto_mode() function using these structures. The underlying
-    dif_edn_instantiate/reseed/generate() functions are left untouched.
-    
-    With this commit, three commands can take up to 12 words of additional
-    data thereby making full use of the hardware.
-    
-    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
+    Signed-off-by: Chris Frantz <cfrantz@google.com>
 
 """
 
