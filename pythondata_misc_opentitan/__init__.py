de13fb1d1f9a9abac716083bd6efbdb136527d66
LiteX Robot <robot@timvideos.us>
2022-10-15 23:15:20 +0000

    Updating pythondata-misc-opentitan to 0.0.post14768
    Updated data to v0.0-14626-g84fdd3bf37 based on 84fdd3bf374f168367ba7ffcb07cfa4d79c54875 from https://github.com/lowRISC/opentitan.
    > commit 84fdd3bf374f168367ba7ffcb07cfa4d79c54875
    > Author: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
    > Date:   Fri Oct 14 10:08:40 2022 -0700
    >
    >     [entropy_src/rtl] Clear window_cntr on disable
    >
    >     This commit reverts the window_cntr_clr behavior to the original design
    >     where this counter is cleared on disable.   When health_test_clr was
    >     changed to a pulse emitted on enable, this counter was left uncleared
    >     after disable.
    >
    >     This has been observed to cause spurious `window_wrap_pulse`'s when
    >     reconfiguring the module, particularly if the window_size is shrunk
    >     while reconfiguring the DUT.
    >
    >     Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,41 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14767"
-version_tuple = (0, 0, 14767)
+version_str = "0.0.post14768"
+version_tuple = (0, 0, 14768)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14767")
+    pversion = V("0.0.post14768")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14625"
-data_version_tuple = (0, 0, 14625)
+data_version_str = "0.0.post14626"
+data_version_tuple = (0, 0, 14626)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14625")
+    pdata_version = V("0.0.post14626")
 except ImportError:
     pass
-data_git_hash = "8c8a3a83645ee23ef762234371ce77f58dea6190"
-data_git_describe = "v0.0-14625-g8c8a3a8364"
+data_git_hash = "84fdd3bf374f168367ba7ffcb07cfa4d79c54875"
+data_git_describe = "v0.0-14626-g84fdd3bf37"
 data_git_msg = """\
-commit 8c8a3a83645ee23ef762234371ce77f58dea6190
-Author: Jaedon Kim <jdonjdon@google.com>
-Date:   Fri Oct 14 02:42:46 2022 +0000
+commit 84fdd3bf374f168367ba7ffcb07cfa4d79c54875
+Author: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
+Date:   Fri Oct 14 10:08:40 2022 -0700
 
-    [flash_ctrl,dv] hw fault_status coverage
+    [entropy_src/rtl] Clear window_cntr on disable
     
-    - Add a few new tests to hit covergropu 'hw_error_cg'
-    - Add flip_2bits function to accomodate asserting 2 bit error
-    - Reactor flash_ctrl_err_base_vseq and childrun sequences to be more reusable
+    This commit reverts the window_cntr_clr behavior to the original design
+    where this counter is cleared on disable.   When health_test_clr was
+    changed to a pulse emitted on enable, this counter was left uncleared
+    after disable.
     
-    Signed-off-by: Jaedon Kim <jdonjdon@google.com>
+    This has been observed to cause spurious `window_wrap_pulse`'s when
+    reconfiguring the module, particularly if the window_size is shrunk
+    while reconfiguring the DUT.
+    
+    Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>
 
 """
 
