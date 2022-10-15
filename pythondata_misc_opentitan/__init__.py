38626f3b52eeef3dc82cd7e8a84126b3a0ac3216
LiteX Robot <robot@timvideos.us>
2022-10-15 16:18:17 +0000

    Updating pythondata-misc-opentitan to 0.0.post14767
    Updated data to v0.0-14625-g8c8a3a8364 based on 8c8a3a83645ee23ef762234371ce77f58dea6190 from https://github.com/lowRISC/opentitan.
    > commit 8c8a3a83645ee23ef762234371ce77f58dea6190
    > Author: Jaedon Kim <jdonjdon@google.com>
    > Date:   Fri Oct 14 02:42:46 2022 +0000
    >
    >     [flash_ctrl,dv] hw fault_status coverage
    >
    >     - Add a few new tests to hit covergropu 'hw_error_cg'
    >     - Add flip_2bits function to accomodate asserting 2 bit error
    >     - Reactor flash_ctrl_err_base_vseq and childrun sequences to be more reusable
    >
    >     Signed-off-by: Jaedon Kim <jdonjdon@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14766"
-version_tuple = (0, 0, 14766)
+version_str = "0.0.post14767"
+version_tuple = (0, 0, 14767)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14766")
+    pversion = V("0.0.post14767")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14624"
-data_version_tuple = (0, 0, 14624)
+data_version_str = "0.0.post14625"
+data_version_tuple = (0, 0, 14625)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14624")
+    pdata_version = V("0.0.post14625")
 except ImportError:
     pass
-data_git_hash = "c5f7bfefb64eee68205111b5b7abff7f086a4e0d"
-data_git_describe = "v0.0-14624-gc5f7bfefb6"
+data_git_hash = "8c8a3a83645ee23ef762234371ce77f58dea6190"
+data_git_describe = "v0.0-14625-g8c8a3a8364"
 data_git_msg = """\
-commit c5f7bfefb64eee68205111b5b7abff7f086a4e0d
-Author: Cindy Chen <chencindy@opentitan.org>
-Date:   Fri Oct 14 16:17:10 2022 -0700
+commit 8c8a3a83645ee23ef762234371ce77f58dea6190
+Author: Jaedon Kim <jdonjdon@google.com>
+Date:   Fri Oct 14 02:42:46 2022 +0000
 
-    [dv/otp_ctrl] Fix scb X value
+    [flash_ctrl,dv] hw fault_status coverage
     
-    This PR fixes scb X value because I missed the part to assign the value
-    from interface.
-    Thanks Sri for finding these.
+    - Add a few new tests to hit covergropu 'hw_error_cg'
+    - Add flip_2bits function to accomodate asserting 2 bit error
+    - Reactor flash_ctrl_err_base_vseq and childrun sequences to be more reusable
     
-    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
+    Signed-off-by: Jaedon Kim <jdonjdon@google.com>
 
 """
 
