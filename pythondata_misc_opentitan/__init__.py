dc932e4de98ab44e6e76ab08b040ce78bba99caa
LiteX Robot <robot@timvideos.us>
2022-10-17 16:26:49 +0000

    Updating pythondata-misc-opentitan to 0.0.post14782
    Updated data to v0.0-14640-g8a6b83dbfd based on 8a6b83dbfd0fab3d725f66a509294be11352d9e0 from https://github.com/lowRISC/opentitan.
    > commit 8a6b83dbfd0fab3d725f66a509294be11352d9e0
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Thu Oct 13 15:58:49 2022 -0700
    >
    >     [dv/lc_ctrl] Update coverage exclusion file
    >
    >     This PR updates lc_ctrl's coverage exclusion file:
    >     1). Removed the V2 only exclusion file
    >     2). Add exclusion file to exclude any non-idle state to InvalidSt.
    >       Because they are fully covered by FPV testbench.
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,37 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14779"
-version_tuple = (0, 0, 14779)
+version_str = "0.0.post14782"
+version_tuple = (0, 0, 14782)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14779")
+    pversion = V("0.0.post14782")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14637"
-data_version_tuple = (0, 0, 14637)
+data_version_str = "0.0.post14640"
+data_version_tuple = (0, 0, 14640)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14637")
+    pdata_version = V("0.0.post14640")
 except ImportError:
     pass
-data_git_hash = "1045061e816476b70008606739960a481e1c29d5"
-data_git_describe = "v0.0-14637-g1045061e81"
+data_git_hash = "8a6b83dbfd0fab3d725f66a509294be11352d9e0"
+data_git_describe = "v0.0-14640-g8a6b83dbfd"
 data_git_msg = """\
-commit 1045061e816476b70008606739960a481e1c29d5
-Author: Chris Frantz <cfrantz@google.com>
-Date:   Wed Oct 12 10:03:48 2022 -0700
+commit 8a6b83dbfd0fab3d725f66a509294be11352d9e0
+Author: Cindy Chen <chencindy@opentitan.org>
+Date:   Thu Oct 13 15:58:49 2022 -0700
 
-    [rust] Turn on clippy
+    [dv/lc_ctrl] Update coverage exclusion file
     
-    Signed-off-by: Chris Frantz <cfrantz@google.com>
+    This PR updates lc_ctrl's coverage exclusion file:
+    1). Removed the V2 only exclusion file
+    2). Add exclusion file to exclude any non-idle state to InvalidSt.
+      Because they are fully covered by FPV testbench.
+    
+    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
 """
 
