1867e48b4b0a0437055a7d68eee62e1575c0eaa2
LiteX Robot <robot@timvideos.us>
2022-10-17 21:15:25 +0000

    Updating pythondata-misc-opentitan to 0.0.post14788
    Updated data to v0.0-14646-g559bcc276a based on 559bcc276afc56be90e696337f0d4f1b297e4773 from https://github.com/lowRISC/opentitan.
    > commit 559bcc276afc56be90e696337f0d4f1b297e4773
    > Author: Eli Kim <eli@opentitan.org>
    > Date:   Tue Oct 11 18:25:41 2022 -0700
    >
    >     fix(chip): check `logic` compare and pins connection
    >
    >     In the prev codes there were three bugs that falsely passes the test.
    >
    >     1. The sample_gpio() should sample `ios_if.pins` not `ios_if.pins_o`.
    >     2. The DV checker should use `===` rather than `==`. The PADs are
    >        `logic` so the checker strictly compares all four types.
    >     3. The SW did not clear pinmux status after waking up from the sleep.
    >
    >     Signed-off-by: Eli Kim <eli@opentitan.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,40 +4,39 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14784"
-version_tuple = (0, 0, 14784)
+version_str = "0.0.post14788"
+version_tuple = (0, 0, 14788)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14784")
+    pversion = V("0.0.post14788")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14642"
-data_version_tuple = (0, 0, 14642)
+data_version_str = "0.0.post14646"
+data_version_tuple = (0, 0, 14646)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14642")
+    pdata_version = V("0.0.post14646")
 except ImportError:
     pass
-data_git_hash = "8f985b9f2af6271bb5bf2e5f567bc57662989f7d"
-data_git_describe = "v0.0-14642-g8f985b9f2a"
+data_git_hash = "559bcc276afc56be90e696337f0d4f1b297e4773"
+data_git_describe = "v0.0-14646-g559bcc276a"
 data_git_msg = """\
-commit 8f985b9f2af6271bb5bf2e5f567bc57662989f7d
-Author: Cindy Chen <chencindy@opentitan.org>
-Date:   Fri Oct 14 14:25:18 2022 -0700
+commit 559bcc276afc56be90e696337f0d4f1b297e4773
+Author: Eli Kim <eli@opentitan.org>
+Date:   Tue Oct 11 18:25:41 2022 -0700
 
-    [dv/adc_ctrl] Fix adc_ctrl regression error
+    fix(chip): check `logic` compare and pins connection
     
-    This PR fixes adc_ctrl regression error due to reset_aon_ni and reset_ni
-    not issuing at the same time.
-    Because the reset_aon_ni has very small frequency compares to reset_ni
-    clock, so issuing reset with normal `apply_reset` task would cause the
-    following issue:
-    The reset_ni will finsh toggle but reset_aon_ni has not start activation
-    yet.
+    In the prev codes there were three bugs that falsely passes the test.
     
-    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
+    1. The sample_gpio() should sample `ios_if.pins` not `ios_if.pins_o`.
+    2. The DV checker should use `===` rather than `==`. The PADs are
+       `logic` so the checker strictly compares all four types.
+    3. The SW did not clear pinmux status after waking up from the sleep.
+    
+    Signed-off-by: Eli Kim <eli@opentitan.org>
 
 """
 
