8cf3258ad1376c6f34b4fbc610a7f4f7e2f990a7
LiteX Robot <robot@timvideos.us>
2022-10-18 22:16:17 +0000

    Updating pythondata-misc-opentitan to 0.0.post14811
    Updated data to v0.0-14669-g9ca43a9f2d based on 9ca43a9f2dbfa676a6c5b38857eb024704bafe65 from https://github.com/lowRISC/opentitan.
    > commit 9ca43a9f2dbfa676a6c5b38857eb024704bafe65
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Tue Oct 18 10:50:43 2022 -0700
    >
    >     [dv/jtag_riscv_agent] Fix rv_dm activation error
    >
    >     This PR fixes the nightly regression failure due to rv_dm activation
    >     failure.
    >     Because rv_dm activation consists a series of steps, so initially I
    >     forget to assign the final status to return to the activation sequence.
    >     This error is caught by `DV_CHECK` macro updates.
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,38 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14810"
-version_tuple = (0, 0, 14810)
+version_str = "0.0.post14811"
+version_tuple = (0, 0, 14811)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14810")
+    pversion = V("0.0.post14811")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14668"
-data_version_tuple = (0, 0, 14668)
+data_version_str = "0.0.post14669"
+data_version_tuple = (0, 0, 14669)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14668")
+    pdata_version = V("0.0.post14669")
 except ImportError:
     pass
-data_git_hash = "54fa34484ef4ac6cfcf1f054bb4f283e08a211b1"
-data_git_describe = "v0.0-14668-g54fa34484e"
+data_git_hash = "9ca43a9f2dbfa676a6c5b38857eb024704bafe65"
+data_git_describe = "v0.0-14669-g9ca43a9f2d"
 data_git_msg = """\
-commit 54fa34484ef4ac6cfcf1f054bb4f283e08a211b1
-Author: Weicai Yang <weicai@google.com>
-Date:   Fri Oct 14 12:14:38 2022 -0700
+commit 9ca43a9f2dbfa676a6c5b38857eb024704bafe65
+Author: Cindy Chen <chencindy@opentitan.org>
+Date:   Tue Oct 18 10:50:43 2022 -0700
 
-    [spi_device/dv] Update CSR exclusions
+    [dv/jtag_riscv_agent] Fix rv_dm activation error
     
-    Removed some unnecessary exclusions
+    This PR fixes the nightly regression failure due to rv_dm activation
+    failure.
+    Because rv_dm activation consists a series of steps, so initially I
+    forget to assign the final status to return to the activation sequence.
+    This error is caught by `DV_CHECK` macro updates.
     
-    Signed-off-by: Weicai Yang <weicai@google.com>
+    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
 """
 
