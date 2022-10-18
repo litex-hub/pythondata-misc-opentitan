f761de9423a679a6b25a23cbb646e299ed051c7f
LiteX Robot <robot@timvideos.us>
2022-10-18 01:03:43 +0000

    Updating pythondata-misc-opentitan to 0.0.post14791
    Updated data to v0.0-14649-gd336486bcf based on d336486bcfc32a009f8f20d621c6ceac930ffdd9 from https://github.com/lowRISC/opentitan.
    > commit d336486bcfc32a009f8f20d621c6ceac930ffdd9
    > Author: Michael Schaffner <msf@google.com>
    > Date:   Mon Oct 17 15:32:10 2022 -0700
    >
    >     [test] Increase SW timeout for ping_timeout test
    >
    >     This is a fix for addressing #15280.
    >
    >     Signed-off-by: Michael Schaffner <msf@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14790"
-version_tuple = (0, 0, 14790)
+version_str = "0.0.post14791"
+version_tuple = (0, 0, 14791)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14790")
+    pversion = V("0.0.post14791")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14648"
-data_version_tuple = (0, 0, 14648)
+data_version_str = "0.0.post14649"
+data_version_tuple = (0, 0, 14649)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14648")
+    pdata_version = V("0.0.post14649")
 except ImportError:
     pass
-data_git_hash = "d698f0375c7d23919b20826f3cd269f8f7d9bf5d"
-data_git_describe = "v0.0-14648-gd698f0375c"
+data_git_hash = "d336486bcfc32a009f8f20d621c6ceac930ffdd9"
+data_git_describe = "v0.0-14649-gd336486bcf"
 data_git_msg = """\
-commit d698f0375c7d23919b20826f3cd269f8f7d9bf5d
-Author: Weicai Yang <weicai@google.com>
-Date:   Fri Oct 14 17:09:40 2022 -0700
+commit d336486bcfc32a009f8f20d621c6ceac930ffdd9
+Author: Michael Schaffner <msf@google.com>
+Date:   Mon Oct 17 15:32:10 2022 -0700
 
-    [spi_device/dv] Update spi_device_driver to collect payload
+    [test] Increase SW timeout for ping_timeout test
     
-    Updated spi_device_driver to collect payload so that vseq can have the completed
-    payload to check.
+    This is a fix for addressing #15280.
     
-    Thanks Alex for pointing this out.
-    Signed-off-by: Weicai Yang <weicai@google.com>
+    Signed-off-by: Michael Schaffner <msf@google.com>
 
 """
 
