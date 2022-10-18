55017cf123fb7d24c4e512c6a63e4530eb3ce9e5
LiteX Robot <robot@timvideos.us>
2022-10-18 08:25:42 +0000

    Updating pythondata-misc-opentitan to 0.0.post14794
    Updated data to v0.0-14652-g484086eec0 based on 484086eec0115826ca5ee26ab7bef956bd1eb190 from https://github.com/lowRISC/opentitan.
    > commit 484086eec0115826ca5ee26ab7bef956bd1eb190
    > Author: Jade Philipoom <jadep@google.com>
    > Date:   Fri Oct 7 17:28:06 2022 +0200
    >
    >     [crypto] Safely generate the P-256 secret scalar and random keys.
    >
    >     Previously, the secret scalar k was simply fetched from RND, but when
    >     reduced modulo the curve order the value will be biased. For this
    >     reason, the FIPS spec requires generating the secret scalar from a
    >     larger (320-bit) seed, and additionally ensuring it is nonzero.
    >     Generation of the secret key from a random seed is very similar.
    >
    >     Signed-off-by: Jade Philipoom <jadep@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,38 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14793"
-version_tuple = (0, 0, 14793)
+version_str = "0.0.post14794"
+version_tuple = (0, 0, 14794)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14793")
+    pversion = V("0.0.post14794")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14651"
-data_version_tuple = (0, 0, 14651)
+data_version_str = "0.0.post14652"
+data_version_tuple = (0, 0, 14652)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14651")
+    pdata_version = V("0.0.post14652")
 except ImportError:
     pass
-data_git_hash = "4b867c56f4dc553d78485e6b4192d48ef4c72892"
-data_git_describe = "v0.0-14651-g4b867c56f4"
+data_git_hash = "484086eec0115826ca5ee26ab7bef956bd1eb190"
+data_git_describe = "v0.0-14652-g484086eec0"
 data_git_msg = """\
-commit 4b867c56f4dc553d78485e6b4192d48ef4c72892
-Author: Timothy Chen <timothytim@google.com>
-Date:   Mon Oct 17 12:45:02 2022 -0700
+commit 484086eec0115826ca5ee26ab7bef956bd1eb190
+Author: Jade Philipoom <jadep@google.com>
+Date:   Fri Oct 7 17:28:06 2022 +0200
 
-    [top/dv] Automatically backdoor load ROM_EXEC_EN
+    [crypto] Safely generate the P-256 secret scalar and random keys.
     
-    This is the same fix as the one applied to lc_walkthrough.
+    Previously, the secret scalar k was simply fetched from RND, but when
+    reduced modulo the curve order the value will be biased. For this
+    reason, the FIPS spec requires generating the secret scalar from a
+    larger (320-bit) seed, and additionally ensuring it is nonzero.
+    Generation of the secret key from a random seed is very similar.
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Jade Philipoom <jadep@google.com>
 
 """
 
