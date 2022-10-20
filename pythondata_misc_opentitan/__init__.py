d7abe348713e15a504821c31246e021a86bf59f5
LiteX Robot <robot@timvideos.us>
2022-10-20 00:59:12 +0000

    Updating pythondata-misc-opentitan to 0.0.post14848
    Updated data to v0.0-14706-g9bbff6f01e based on 9bbff6f01e352180891e7de2d35cffad07278133 from https://github.com/lowRISC/opentitan.
    > commit 9bbff6f01e352180891e7de2d35cffad07278133
    > Author: Guillermo Maturana <maturana@google.com>
    > Date:   Tue Oct 18 18:04:19 2022 -0700
    >
    >     [dv,top-level] Fix all_escalation_resets test for flash_ctrl
    >
    >     Capture the alert info dump to confirm the alert after the escalation reset.
    >     For flash_ctrl onlyh, ignore the check for ISRs having run, since flash
    >     accesses are blocked for flash_ctrl integrity errors.
    >     Provide a partial solution to #8945.
    >     Fixes a TODO in rstmgr_alert_info_test.c related to the creation of the
    >     C struct equivalent to the hardware bit stream for alert_info dump.
    >     Replaces some code that computes cycle counts from time durations by the
    >     corresponding code in alert_handler_testutils.
    >
    >     Fixes #14899
    >
    >     Signed-off-by: Guillermo Maturana <maturana@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,43 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14846"
-version_tuple = (0, 0, 14846)
+version_str = "0.0.post14848"
+version_tuple = (0, 0, 14848)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14846")
+    pversion = V("0.0.post14848")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14704"
-data_version_tuple = (0, 0, 14704)
+data_version_str = "0.0.post14706"
+data_version_tuple = (0, 0, 14706)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14704")
+    pdata_version = V("0.0.post14706")
 except ImportError:
     pass
-data_git_hash = "68ec61ba0e02859f16ff8a9a4c95124d2ad9fb48"
-data_git_describe = "v0.0-14704-g68ec61ba0e"
+data_git_hash = "9bbff6f01e352180891e7de2d35cffad07278133"
+data_git_describe = "v0.0-14706-g9bbff6f01e"
 data_git_msg = """\
-commit 68ec61ba0e02859f16ff8a9a4c95124d2ad9fb48
-Author: Chris Frantz <cfrantz@google.com>
-Date:   Tue Oct 18 08:00:55 2022 -0700
+commit 9bbff6f01e352180891e7de2d35cffad07278133
+Author: Guillermo Maturana <maturana@google.com>
+Date:   Tue Oct 18 18:04:19 2022 -0700
 
-    [rom-e2e] Update sfdp test to check more fields
+    [dv,top-level] Fix all_escalation_resets test for flash_ctrl
     
-    Signed-off-by: Chris Frantz <cfrantz@google.com>
+    Capture the alert info dump to confirm the alert after the escalation reset.
+    For flash_ctrl onlyh, ignore the check for ISRs having run, since flash
+    accesses are blocked for flash_ctrl integrity errors.
+    Provide a partial solution to #8945.
+    Fixes a TODO in rstmgr_alert_info_test.c related to the creation of the
+    C struct equivalent to the hardware bit stream for alert_info dump.
+    Replaces some code that computes cycle counts from time durations by the
+    corresponding code in alert_handler_testutils.
+    
+    Fixes #14899
+    
+    Signed-off-by: Guillermo Maturana <maturana@google.com>
 
 """
 
