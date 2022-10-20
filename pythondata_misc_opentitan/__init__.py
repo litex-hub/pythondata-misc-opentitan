1570c498047fd140afebe035f661719c783145f6
LiteX Robot <robot@timvideos.us>
2022-10-20 18:32:10 +0000

    Updating pythondata-misc-opentitan to 0.0.post14858
    Updated data to v0.0-14716-gf17833bf0e based on f17833bf0ea781f2f6bc0e975075720143bccee3 from https://github.com/lowRISC/opentitan.
    > commit f17833bf0ea781f2f6bc0e975075720143bccee3
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Tue Oct 18 13:30:21 2022 -0700
    >
    >     [top/dv] Adjust setup for floating pin.
    >
    >     - Previously, the test would enable the UART RX pins without
    >       a pull-up.  This meant that until the test bench environment
    >       actively drove, the pin was left floating and cause x propagation.
    >
    >     - This update just sets the pull-up at the same time as enabling the
    >       pin to ensure this situation does not come up.  The pull-up setting
    >       is platform dependent since verilator / fpga do not have pull-up
    >       capabilities and must be excluded.
    >
    >     - Similarly, the pull-up must be excluded for englishbreakfast since
    >       certain registers do not exist in that space.
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,44 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14856"
-version_tuple = (0, 0, 14856)
+version_str = "0.0.post14858"
+version_tuple = (0, 0, 14858)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14856")
+    pversion = V("0.0.post14858")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14714"
-data_version_tuple = (0, 0, 14714)
+data_version_str = "0.0.post14716"
+data_version_tuple = (0, 0, 14716)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14714")
+    pdata_version = V("0.0.post14716")
 except ImportError:
     pass
-data_git_hash = "be7318d6859548c47238b89a3920c73da316abf7"
-data_git_describe = "v0.0-14714-gbe7318d685"
+data_git_hash = "f17833bf0ea781f2f6bc0e975075720143bccee3"
+data_git_describe = "v0.0-14716-gf17833bf0e"
 data_git_msg = """\
-commit be7318d6859548c47238b89a3920c73da316abf7
-Author: Alphan Ulusoy <alphan@google.com>
-Date:   Wed Oct 19 14:56:06 2022 -0400
+commit f17833bf0ea781f2f6bc0e975075720143bccee3
+Author: Timothy Chen <timothytim@google.com>
+Date:   Tue Oct 18 13:30:21 2022 -0700
 
-    [test] Set pmpaddr0 to match the entire address space
+    [top/dv] Adjust setup for floating pin.
     
-    Signed-off-by: Alphan Ulusoy <alphan@google.com>
+    - Previously, the test would enable the UART RX pins without
+      a pull-up.  This meant that until the test bench environment
+      actively drove, the pin was left floating and cause x propagation.
+    
+    - This update just sets the pull-up at the same time as enabling the
+      pin to ensure this situation does not come up.  The pull-up setting
+      is platform dependent since verilator / fpga do not have pull-up
+      capabilities and must be excluded.
+    
+    - Similarly, the pull-up must be excluded for englishbreakfast since
+      certain registers do not exist in that space.
+    
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
