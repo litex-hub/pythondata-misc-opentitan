e90a54dfc632ea077d2bef0c8b094509dc58237a
LiteX Robot <robot@timvideos.us>
2022-10-20 03:35:16 +0000

    Updating pythondata-misc-opentitan to 0.0.post14849
    Updated data to v0.0-14707-g47a8e09c9b based on 47a8e09c9b519069615bb32a0c976a92302c9bb8 from https://github.com/lowRISC/opentitan.
    > commit 47a8e09c9b519069615bb32a0c976a92302c9bb8
    > Author: Michael Munday <mike.munday@lowrisc.org>
    > Date:   Tue Oct 18 21:21:10 2022 -0400
    >
    >     [site] Update lowRISC registered office address
    >
    >     Signed-off-by: Michael Munday <mike.munday@lowrisc.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,43 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14848"
-version_tuple = (0, 0, 14848)
+version_str = "0.0.post14849"
+version_tuple = (0, 0, 14849)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14848")
+    pversion = V("0.0.post14849")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14706"
-data_version_tuple = (0, 0, 14706)
+data_version_str = "0.0.post14707"
+data_version_tuple = (0, 0, 14707)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14706")
+    pdata_version = V("0.0.post14707")
 except ImportError:
     pass
-data_git_hash = "9bbff6f01e352180891e7de2d35cffad07278133"
-data_git_describe = "v0.0-14706-g9bbff6f01e"
+data_git_hash = "47a8e09c9b519069615bb32a0c976a92302c9bb8"
+data_git_describe = "v0.0-14707-g47a8e09c9b"
 data_git_msg = """\
-commit 9bbff6f01e352180891e7de2d35cffad07278133
-Author: Guillermo Maturana <maturana@google.com>
-Date:   Tue Oct 18 18:04:19 2022 -0700
+commit 47a8e09c9b519069615bb32a0c976a92302c9bb8
+Author: Michael Munday <mike.munday@lowrisc.org>
+Date:   Tue Oct 18 21:21:10 2022 -0400
 
-    [dv,top-level] Fix all_escalation_resets test for flash_ctrl
+    [site] Update lowRISC registered office address
     
-    Capture the alert info dump to confirm the alert after the escalation reset.
-    For flash_ctrl onlyh, ignore the check for ISRs having run, since flash
-    accesses are blocked for flash_ctrl integrity errors.
-    Provide a partial solution to #8945.
-    Fixes a TODO in rstmgr_alert_info_test.c related to the creation of the
-    C struct equivalent to the hardware bit stream for alert_info dump.
-    Replaces some code that computes cycle counts from time durations by the
-    corresponding code in alert_handler_testutils.
-    
-    Fixes #14899
-    
-    Signed-off-by: Guillermo Maturana <maturana@google.com>
+    Signed-off-by: Michael Munday <mike.munday@lowrisc.org>
 
 """
 
