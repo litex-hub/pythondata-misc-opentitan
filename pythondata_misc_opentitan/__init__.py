41c27da4e2c12435fa8818df8e37190deca6a9e8
LiteX Robot <robot@timvideos.us>
2022-10-21 08:20:58 +0000

    Updating pythondata-misc-opentitan to 0.0.post14873
    Updated data to v0.0-14731-g7d9b5775cb based on 7d9b5775cb6c9de18e13a9b6b2be73051f25d658 from https://github.com/lowRISC/opentitan.
    > commit 7d9b5775cb6c9de18e13a9b6b2be73051f25d658
    > Author: Greg Chadwick <gac@lowrisc.org>
    > Date:   Thu Oct 20 12:01:17 2022 +0100
    >
    >     [otbn,rtl] Latch various internal state errors
    >
    >     FPV has revealed it was possible for some internal state errors to be
    >     ignored, so no alert was triggered, if they occurred the same cycle or
    >     the cycle before a start command was issues to OTBN. This was due to
    >     some errors only appearing for a single cycle and others getting reset
    >     by the start command.
    >
    >     This adds latching behaviour to various internal errors, so once seen
    >     they will remain asserted until reset.
    >
    >     Signed-off-by: Greg Chadwick <gac@lowrisc.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,41 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14872"
-version_tuple = (0, 0, 14872)
+version_str = "0.0.post14873"
+version_tuple = (0, 0, 14873)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14872")
+    pversion = V("0.0.post14873")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14730"
-data_version_tuple = (0, 0, 14730)
+data_version_str = "0.0.post14731"
+data_version_tuple = (0, 0, 14731)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14730")
+    pdata_version = V("0.0.post14731")
 except ImportError:
     pass
-data_git_hash = "1c6f03666f77ee3d907d6daa3e9bfb19663e6b51"
-data_git_describe = "v0.0-14730-g1c6f03666f"
+data_git_hash = "7d9b5775cb6c9de18e13a9b6b2be73051f25d658"
+data_git_describe = "v0.0-14731-g7d9b5775cb"
 data_git_msg = """\
-commit 1c6f03666f77ee3d907d6daa3e9bfb19663e6b51
-Author: Pirmin Vogel <vogelpi@lowrisc.org>
-Date:   Thu Oct 20 13:22:37 2022 +0200
+commit 7d9b5775cb6c9de18e13a9b6b2be73051f25d658
+Author: Greg Chadwick <gac@lowrisc.org>
+Date:   Thu Oct 20 12:01:17 2022 +0100
 
-    [sw, sca] Rename aes_serial_encrypt() function for disambiguation
+    [otbn,rtl] Latch various internal state errors
     
-    This is the underlying function called by several simple serial commands
-    but it is itself not a simple serial command.
+    FPV has revealed it was possible for some internal state errors to be
+    ignored, so no alert was triggered, if they occurred the same cycle or
+    the cycle before a start command was issues to OTBN. This was due to
+    some errors only appearing for a single cycle and others getting reset
+    by the start command.
     
-    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
+    This adds latching behaviour to various internal errors, so once seen
+    they will remain asserted until reset.
+    
+    Signed-off-by: Greg Chadwick <gac@lowrisc.org>
 
 """
 
