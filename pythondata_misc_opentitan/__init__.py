3bdc0f59d4d08d9c39ee11b20e8ee30d5e811512
LiteX Robot <robot@timvideos.us>
2022-10-21 05:50:03 +0000

    Updating pythondata-misc-opentitan to 0.0.post14867
    Updated data to v0.0-14725-gaad7e8f8ed based on aad7e8f8ed303374f9eff94ca75fae95f60f0fa9 from https://github.com/lowRISC/opentitan.
    > commit aad7e8f8ed303374f9eff94ca75fae95f60f0fa9
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Thu Oct 20 11:44:25 2022 -0700
    >
    >     [doc/edn] Minor update on the EDN doc
    >
    >     This PR updates two parts on EDN doc:
    >     1). Update the statement saying this version does not support alert
    >     2). Minor fix on a redundant word "only".
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14864"
-version_tuple = (0, 0, 14864)
+version_str = "0.0.post14867"
+version_tuple = (0, 0, 14867)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14864")
+    pversion = V("0.0.post14867")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14722"
-data_version_tuple = (0, 0, 14722)
+data_version_str = "0.0.post14725"
+data_version_tuple = (0, 0, 14725)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14722")
+    pdata_version = V("0.0.post14725")
 except ImportError:
     pass
-data_git_hash = "2f668bcd9da6aaa3771b1bc9f3b05f20a6bf7167"
-data_git_describe = "v0.0-14722-g2f668bcd9d"
+data_git_hash = "aad7e8f8ed303374f9eff94ca75fae95f60f0fa9"
+data_git_describe = "v0.0-14725-gaad7e8f8ed"
 data_git_msg = """\
-commit 2f668bcd9da6aaa3771b1bc9f3b05f20a6bf7167
-Author: Michael Schaffner <msf@google.com>
-Date:   Thu Oct 20 11:38:53 2022 -0700
+commit aad7e8f8ed303374f9eff94ca75fae95f60f0fa9
+Author: Cindy Chen <chencindy@opentitan.org>
+Date:   Thu Oct 20 11:44:25 2022 -0700
 
-    [top] Remove defunct synthesis dashboard
+    [doc/edn] Minor update on the EDN doc
     
-    The synthesis dashboard is currently not working,
-    hence we remove it for the time being.
+    This PR updates two parts on EDN doc:
+    1). Update the statement saying this version does not support alert
+    2). Minor fix on a redundant word "only".
     
-    Signed-off-by: Michael Schaffner <msf@google.com>
+    Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
 """
 
