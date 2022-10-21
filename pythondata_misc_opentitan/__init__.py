6f208ebc5c474a4506396f11dd0c14c428e8f014
LiteX Robot <robot@timvideos.us>
2022-10-21 16:26:10 +0000

    Updating pythondata-misc-opentitan to 0.0.post14879
    Updated data to v0.0-14737-g0f7b76f809 based on 0f7b76f809315d42c6240cec6e72598b1c2efbdb from https://github.com/lowRISC/opentitan.
    > commit 0f7b76f809315d42c6240cec6e72598b1c2efbdb
    > Author: Michał Mazurek <maz@semihalf.com>
    > Date:   Mon Oct 17 17:21:38 2022 +0200
    >
    >     [utils,gpio] Add new hjson key 'auto_split' as hint for regtool
    >
    >     Introduces a new optional key 'auto_split' to serve as a hint
    >     for register map generator script. Adds support up to 'auto_split'
    >     in the TockOS register map generator.
    >
    >     If the bitfield or signal has key 'auto_split' set to value
    >     true generator will split this filed to multiple one-bit fields.
    >     The name of each generated bitfiled will consist of the
    >     original bitfield name and an offset. If the parameter is set to false,
    >     the current behavior of the regtool is not replaced. The default value of the
    >     new key is set to false.
    >
    >     Signed-off-by: Michał Mazurek <maz@semihalf.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,37 +4,43 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14875"
-version_tuple = (0, 0, 14875)
+version_str = "0.0.post14879"
+version_tuple = (0, 0, 14879)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14875")
+    pversion = V("0.0.post14879")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14733"
-data_version_tuple = (0, 0, 14733)
+data_version_str = "0.0.post14737"
+data_version_tuple = (0, 0, 14737)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14733")
+    pdata_version = V("0.0.post14737")
 except ImportError:
     pass
-data_git_hash = "938687bba3afcf236488299fb31ed036df450102"
-data_git_describe = "v0.0-14733-g938687bba3"
+data_git_hash = "0f7b76f809315d42c6240cec6e72598b1c2efbdb"
+data_git_describe = "v0.0-14737-g0f7b76f809"
 data_git_msg = """\
-commit 938687bba3afcf236488299fb31ed036df450102
-Author: Greg Chadwick <gac@lowrisc.org>
-Date:   Tue Oct 18 17:50:23 2022 +0100
+commit 0f7b76f809315d42c6240cec6e72598b1c2efbdb
+Author: Michał Mazurek <maz@semihalf.com>
+Date:   Mon Oct 17 17:21:38 2022 +0200
 
-    [dv] Fix race condition in otbn_escalate_if
+    [utils,gpio] Add new hjson key 'auto_split' as hint for regtool
     
-    Previously this interface used blocking assignment to set these. This
-    led to the updates happening in the wrong place in the simulation
-    scheduler. In particular it led to the failure of the `OutputDelay_A`
-    assertion in prim_lc_sync in some cases.
+    Introduces a new optional key 'auto_split' to serve as a hint
+    for register map generator script. Adds support up to 'auto_split'
+    in the TockOS register map generator.
     
-    Signed-off-by: Greg Chadwick <gac@lowrisc.org>
+    If the bitfield or signal has key 'auto_split' set to value
+    true generator will split this filed to multiple one-bit fields.
+    The name of each generated bitfiled will consist of the
+    original bitfield name and an offset. If the parameter is set to false,
+    the current behavior of the regtool is not replaced. The default value of the
+    new key is set to false.
+    
+    Signed-off-by: Michał Mazurek <maz@semihalf.com>
 
 """
 
