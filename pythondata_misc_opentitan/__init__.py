883d1de0b7a05648480aba1ec04f336839e1362e
LiteX Robot <robot@timvideos.us>
2022-10-21 15:21:52 +0000

    Updating pythondata-misc-opentitan to 0.0.post14875
    Updated data to v0.0-14733-g938687bba3 based on 938687bba3afcf236488299fb31ed036df450102 from https://github.com/lowRISC/opentitan.
    > commit 938687bba3afcf236488299fb31ed036df450102
    > Author: Greg Chadwick <gac@lowrisc.org>
    > Date:   Tue Oct 18 17:50:23 2022 +0100
    >
    >     [dv] Fix race condition in otbn_escalate_if
    >
    >     Previously this interface used blocking assignment to set these. This
    >     led to the updates happening in the wrong place in the simulation
    >     scheduler. In particular it led to the failure of the `OutputDelay_A`
    >     assertion in prim_lc_sync in some cases.
    >
    >     Signed-off-by: Greg Chadwick <gac@lowrisc.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,40 +4,37 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14874"
-version_tuple = (0, 0, 14874)
+version_str = "0.0.post14875"
+version_tuple = (0, 0, 14875)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14874")
+    pversion = V("0.0.post14875")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14732"
-data_version_tuple = (0, 0, 14732)
+data_version_str = "0.0.post14733"
+data_version_tuple = (0, 0, 14733)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14732")
+    pdata_version = V("0.0.post14733")
 except ImportError:
     pass
-data_git_hash = "7c5837b7ada5c32280c9c18b473388354766f807"
-data_git_describe = "v0.0-14732-g7c5837b7ad"
+data_git_hash = "938687bba3afcf236488299fb31ed036df450102"
+data_git_describe = "v0.0-14733-g938687bba3"
 data_git_msg = """\
-commit 7c5837b7ada5c32280c9c18b473388354766f807
-Author: Jade Philipoom <jadep@google.com>
-Date:   Tue Oct 18 12:55:45 2022 +0200
+commit 938687bba3afcf236488299fb31ed036df450102
+Author: Greg Chadwick <gac@lowrisc.org>
+Date:   Tue Oct 18 17:50:23 2022 +0100
 
-    [crypto] Reorder AES driver checks to avoid race condition.
+    [dv] Fix race condition in otbn_escalate_if
     
-    The AES driver documentation specifies that the `dest` pointer should
-    accept the output from the *previous* input block (if any), not the
-    current one. However, the implementation actually passed the next block
-    of input before reading the output, which could lead to a race condition
-    where the AES block overwrites the output data before it is copied to
-    `dest`. This has not been triggered by any tests but it seems better to
-    be on the safe side and fix it preemptively.
+    Previously this interface used blocking assignment to set these. This
+    led to the updates happening in the wrong place in the simulation
+    scheduler. In particular it led to the failure of the `OutputDelay_A`
+    assertion in prim_lc_sync in some cases.
     
-    Signed-off-by: Jade Philipoom <jadep@google.com>
+    Signed-off-by: Greg Chadwick <gac@lowrisc.org>
 
 """
 
