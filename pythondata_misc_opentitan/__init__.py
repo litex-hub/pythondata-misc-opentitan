6fb378d10e5cf82c35c71664fb327d29be13b42d
LiteX Robot <robot@timvideos.us>
2022-10-24 15:20:57 +0000

    Updating pythondata-misc-opentitan to 0.0.post14905
    Updated data to v0.0-14763-g4ad3f30781 based on 4ad3f30781a95dff7e161bc0ed865e3972b2446b from https://github.com/lowRISC/opentitan.
    > commit 4ad3f30781a95dff7e161bc0ed865e3972b2446b
    > Author: Miles Dai <milesdai@google.com>
    > Date:   Wed Oct 19 23:13:47 2022 -0400
    >
    >     [bazel] Add support for pagination when querying the bitstream cache
    >
    >     Signed-off-by: Miles Dai <milesdai@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14903"
-version_tuple = (0, 0, 14903)
+version_str = "0.0.post14905"
+version_tuple = (0, 0, 14905)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14903")
+    pversion = V("0.0.post14905")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14761"
-data_version_tuple = (0, 0, 14761)
+data_version_str = "0.0.post14763"
+data_version_tuple = (0, 0, 14763)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14761")
+    pdata_version = V("0.0.post14763")
 except ImportError:
     pass
-data_git_hash = "23dc40e81e83aa575650d76bccf6d53959278dcb"
-data_git_describe = "v0.0-14761-g23dc40e81e"
+data_git_hash = "4ad3f30781a95dff7e161bc0ed865e3972b2446b"
+data_git_describe = "v0.0-14763-g4ad3f30781"
 data_git_msg = """\
-commit 23dc40e81e83aa575650d76bccf6d53959278dcb
-Author: Pirmin Vogel <vogelpi@lowrisc.org>
-Date:   Fri Oct 21 10:46:17 2022 +0200
+commit 4ad3f30781a95dff7e161bc0ed865e3972b2446b
+Author: Miles Dai <milesdai@google.com>
+Date:   Wed Oct 19 23:13:47 2022 -0400
 
-    [top] Enable SecAesAllowForcingMasks parameter for top_earlgrey ES
+    [bazel] Add support for pagination when querying the bitstream cache
     
-    It has been decided to enable this feature for ES. For reference,
-    see lowRISC/OpenTitan#14240.
-    
-    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
+    Signed-off-by: Miles Dai <milesdai@google.com>
 
 """
 
