ea68f0bad0203f32a1b79094af51e957e909e5b9
LiteX Robot <robot@timvideos.us>
2022-10-25 22:17:31 +0000

    Updating pythondata-misc-opentitan to 0.0.post14944
    Updated data to v0.0-14802-g6923df040f based on 6923df040fc87f664a1307e56ba5ba9f65301a8a from https://github.com/lowRISC/opentitan.
    > commit 6923df040fc87f664a1307e56ba5ba9f65301a8a
    > Author: Guillermo Maturana <maturana@google.com>
    > Date:   Fri Oct 21 15:26:35 2022 -0700
    >
    >     [dv/rstmgr] Exclude sw_rst_ctrl_n CSR from unit csr tests
    >
    >     Rapid flips of these CSR bits can occasionally trigger alerts, which
    >     can unpredictably update err_code CSR bits.
    >
    >     Signed-off-by: Guillermo Maturana <maturana@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14941"
-version_tuple = (0, 0, 14941)
+version_str = "0.0.post14944"
+version_tuple = (0, 0, 14944)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14941")
+    pversion = V("0.0.post14944")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14799"
-data_version_tuple = (0, 0, 14799)
+data_version_str = "0.0.post14802"
+data_version_tuple = (0, 0, 14802)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14799")
+    pdata_version = V("0.0.post14802")
 except ImportError:
     pass
-data_git_hash = "4580a1765e7b5bd9a5f96c71f89e746e6c27c95d"
-data_git_describe = "v0.0-14799-g4580a1765e"
+data_git_hash = "6923df040fc87f664a1307e56ba5ba9f65301a8a"
+data_git_describe = "v0.0-14802-g6923df040f"
 data_git_msg = """\
-commit 4580a1765e7b5bd9a5f96c71f89e746e6c27c95d
-Author: Miles Dai <milesdai@google.com>
-Date:   Thu Oct 20 17:27:01 2022 -0400
+commit 6923df040fc87f664a1307e56ba5ba9f65301a8a
+Author: Guillermo Maturana <maturana@google.com>
+Date:   Fri Oct 21 15:26:35 2022 -0700
 
-    [bazel] Allow opentitan_functest to accept custom bitstreams
+    [dv/rstmgr] Exclude sw_rst_ctrl_n CSR from unit csr tests
     
-    Signed-off-by: Miles Dai <milesdai@google.com>
+    Rapid flips of these CSR bits can occasionally trigger alerts, which
+    can unpredictably update err_code CSR bits.
+    
+    Signed-off-by: Guillermo Maturana <maturana@google.com>
 
 """
 
