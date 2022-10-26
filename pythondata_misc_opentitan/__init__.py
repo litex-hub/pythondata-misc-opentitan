0da850a67eb20a50139023fb1f420e05f20c96e7
LiteX Robot <robot@timvideos.us>
2022-10-26 00:55:51 +0000

    Updating pythondata-misc-opentitan to 0.0.post14950
    Updated data to v0.0-14808-g0573dd72b4 based on 0573dd72b4b63bd528c52a489bf8fc24b097b486 from https://github.com/lowRISC/opentitan.
    > commit 0573dd72b4b63bd528c52a489bf8fc24b097b486
    > Author: Alexander Williams <awill@google.com>
    > Date:   Mon Oct 24 08:39:47 2022 -0700
    >
    >     [spi_host] Adjust passthrough not-X assertions
    >
    >     The assertions for the passthrough-related I/Os were overzealous and did
    >     not account for conditions where X is allowable. Adjust them to focus on
    >     SCK and CSB only and have the SCK assertion activate only when CSB is
    >     asserted.
    >
    >     Signed-off-by: Alexander Williams <awill@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,33 +4,37 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14945"
-version_tuple = (0, 0, 14945)
+version_str = "0.0.post14950"
+version_tuple = (0, 0, 14950)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14945")
+    pversion = V("0.0.post14950")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14803"
-data_version_tuple = (0, 0, 14803)
+data_version_str = "0.0.post14808"
+data_version_tuple = (0, 0, 14808)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14803")
+    pdata_version = V("0.0.post14808")
 except ImportError:
     pass
-data_git_hash = "e01285f81810f08fa1c41988cff9ac75d901d7ea"
-data_git_describe = "v0.0-14803-ge01285f818"
+data_git_hash = "0573dd72b4b63bd528c52a489bf8fc24b097b486"
+data_git_describe = "v0.0-14808-g0573dd72b4"
 data_git_msg = """\
-commit e01285f81810f08fa1c41988cff9ac75d901d7ea
-Author: Weicai Yang <weicai@google.com>
-Date:   Tue Oct 25 12:24:27 2022 -0700
+commit 0573dd72b4b63bd528c52a489bf8fc24b097b486
+Author: Alexander Williams <awill@google.com>
+Date:   Mon Oct 24 08:39:47 2022 -0700
 
-    [spi_device/dv] Add test - spi_device_flash_and_tpm_min_idle
+    [spi_host] Adjust passthrough not-X assertions
     
-    It inherits from spi_device_flash_and_tpm_vseq and configure the min idle period btw 2 SPI items.
-    Signed-off-by: Weicai Yang <weicai@google.com>
+    The assertions for the passthrough-related I/Os were overzealous and did
+    not account for conditions where X is allowable. Adjust them to focus on
+    SCK and CSB only and have the SCK assertion activate only when CSB is
+    asserted.
+    
+    Signed-off-by: Alexander Williams <awill@google.com>
 
 """
 
