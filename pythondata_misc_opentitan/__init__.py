a305d455cc725b862cd87ea3615bfff02211ca77
LiteX Robot <robot@timvideos.us>
2022-10-27 10:15:28 +0000

    Updating pythondata-misc-opentitan to 0.0.post14979
    Updated data to v0.0-14837-gf46226d1f3 based on f46226d1f3149b49aaf89963b7f7bd02e0eb6ab8 from https://github.com/lowRISC/opentitan.
    > commit f46226d1f3149b49aaf89963b7f7bd02e0eb6ab8
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Wed Oct 26 16:30:39 2022 -0700
    >
    >     [top] Reduce async fifo depth
    >
    >     Fix issue identified in #14204
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,47 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14978"
-version_tuple = (0, 0, 14978)
+version_str = "0.0.post14979"
+version_tuple = (0, 0, 14979)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14978")
+    pversion = V("0.0.post14979")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14836"
-data_version_tuple = (0, 0, 14836)
+data_version_str = "0.0.post14837"
+data_version_tuple = (0, 0, 14837)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14836")
+    pdata_version = V("0.0.post14837")
 except ImportError:
     pass
-data_git_hash = "f58cda5ab437a3a2a5b60427c2963f97e7336c18"
-data_git_describe = "v0.0-14836-gf58cda5ab4"
+data_git_hash = "f46226d1f3149b49aaf89963b7f7bd02e0eb6ab8"
+data_git_describe = "v0.0-14837-gf46226d1f3"
 data_git_msg = """\
-commit f58cda5ab437a3a2a5b60427c2963f97e7336c18
-Author: Pirmin Vogel <vogelpi@lowrisc.org>
-Date:   Mon Jun 13 22:41:48 2022 +0200
+commit f46226d1f3149b49aaf89963b7f7bd02e0eb6ab8
+Author: Timothy Chen <timothytim@google.com>
+Date:   Wed Oct 26 16:30:39 2022 -0700
 
-    [aes] Rework SecAllowForcingMasks parameter
+    [top] Reduce async fifo depth
     
-    Previously, if enabled this parameter would allow forcing the output of
-    the masking PRNG to 0 requiring 160 MUXs. This was okay for FPGA but not
-    for ES.
+    Fix issue identified in #14204
     
-    With this commit, the design is changed as follows:
-    - Upon enabling the parameter and setting the FORCE_MASKS bit in the
-      auxiliary control register (protected by both a shadow register and
-      REGWEN register), the PRNG is no longer advanced. It's output is kept
-      constant.
-    - To switch off the masking, EDN/CSRNG have further to be configured to
-      produce a special seed. Only with this seed and the PRNG output being
-      held constant, the masking is off.
-    
-    This is related to lowRISC/OpenTitan#14240.
-    
-    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
