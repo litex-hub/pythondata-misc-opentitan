0a98ac711285342215fec4cd3b4c44b0a0266eb1
LiteX Robot <robot@timvideos.us>
2022-10-27 11:14:37 +0000

    Updating pythondata-misc-opentitan to 0.0.post14980
    Updated data to v0.0-14838-gd850f024af based on d850f024af6cfc0d9a7877f394374775d2e848c0 from https://github.com/lowRISC/opentitan.
    > commit d850f024af6cfc0d9a7877f394374775d2e848c0
    > Author: Pirmin Vogel <vogelpi@lowrisc.org>
    > Date:   Mon Oct 24 23:27:04 2022 +0200
    >
    >     [test] Add DV component for AES masking off test, add it to the testplan
    >
    >     This commit adds the AES masking off test to the testplan. As the
    >     masking itself is transparent to software, a DV component is added to
    >     probe into AES and verify that 1) all prerequisites are met to switch
    >     off the masking and 2) that the second share of the state as well as
    >     mask input and output of SubBytes are all zero, i.e., masking is indeed
    >     switched off.
    >
    >     This is related to lowRISC/OpenTitan#14240.
    >
    >     Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,41 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post14979"
-version_tuple = (0, 0, 14979)
+version_str = "0.0.post14980"
+version_tuple = (0, 0, 14980)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post14979")
+    pversion = V("0.0.post14980")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14837"
-data_version_tuple = (0, 0, 14837)
+data_version_str = "0.0.post14838"
+data_version_tuple = (0, 0, 14838)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14837")
+    pdata_version = V("0.0.post14838")
 except ImportError:
     pass
-data_git_hash = "f46226d1f3149b49aaf89963b7f7bd02e0eb6ab8"
-data_git_describe = "v0.0-14837-gf46226d1f3"
+data_git_hash = "d850f024af6cfc0d9a7877f394374775d2e848c0"
+data_git_describe = "v0.0-14838-gd850f024af"
 data_git_msg = """\
-commit f46226d1f3149b49aaf89963b7f7bd02e0eb6ab8
-Author: Timothy Chen <timothytim@google.com>
-Date:   Wed Oct 26 16:30:39 2022 -0700
+commit d850f024af6cfc0d9a7877f394374775d2e848c0
+Author: Pirmin Vogel <vogelpi@lowrisc.org>
+Date:   Mon Oct 24 23:27:04 2022 +0200
 
-    [top] Reduce async fifo depth
+    [test] Add DV component for AES masking off test, add it to the testplan
     
-    Fix issue identified in #14204
+    This commit adds the AES masking off test to the testplan. As the
+    masking itself is transparent to software, a DV component is added to
+    probe into AES and verify that 1) all prerequisites are met to switch
+    off the masking and 2) that the second share of the state as well as
+    mask input and output of SubBytes are all zero, i.e., masking is indeed
+    switched off.
     
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    This is related to lowRISC/OpenTitan#14240.
+    
+    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
 
 """
 
