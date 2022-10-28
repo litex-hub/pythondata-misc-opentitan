3b37e2f1eb890bb3fcff95373fa7b2c3213a4147
LiteX Robot <robot@timvideos.us>
2022-10-28 04:43:52 +0000

    Updating pythondata-misc-opentitan to 0.0.post15015
    Updated data to v0.0-14873-g5a4e508f3e based on 5a4e508f3e81e0a17eb7a040f8b439e5de7b5d31 from https://github.com/lowRISC/opentitan.
    > commit 5a4e508f3e81e0a17eb7a040f8b439e5de7b5d31
    > Author: Weicai Yang <weicai@google.com>
    > Date:   Thu Oct 27 13:28:04 2022 -0700
    >
    >     [spi_device/dv] Fix regression issues
    >
    >     This will make most of tests 100% pass, except stress_all
    >     1. Update constrain for flash_mode, need to check mailbox_en before pick an address
    >     2. Update scb to fix prediction of flash_status for a corner case that dummy item involves
    >     3. Fix some regression timeout
    >     4. Remove dummy item in stress_all as it's added in subseq already
    >
    >     Signed-off-by: Weicai Yang <weicai@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,38 +4,38 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post15014"
-version_tuple = (0, 0, 15014)
+version_str = "0.0.post15015"
+version_tuple = (0, 0, 15015)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post15014")
+    pversion = V("0.0.post15015")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14872"
-data_version_tuple = (0, 0, 14872)
+data_version_str = "0.0.post14873"
+data_version_tuple = (0, 0, 14873)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14872")
+    pdata_version = V("0.0.post14873")
 except ImportError:
     pass
-data_git_hash = "92440a24af6e09e52551aa112c85dd0b5588acef"
-data_git_describe = "v0.0-14872-g92440a24af"
+data_git_hash = "5a4e508f3e81e0a17eb7a040f8b439e5de7b5d31"
+data_git_describe = "v0.0-14873-g5a4e508f3e"
 data_git_msg = """\
-commit 92440a24af6e09e52551aa112c85dd0b5588acef
-Author: Timothy Chen <timothytim@google.com>
-Date:   Thu Oct 27 12:26:23 2022 -0700
+commit 5a4e508f3e81e0a17eb7a040f8b439e5de7b5d31
+Author: Weicai Yang <weicai@google.com>
+Date:   Thu Oct 27 13:28:04 2022 -0700
 
-    [top/dv] Add lc_ctrl wait to wait_rom_check_done
+    [spi_device/dv] Fix regression issues
     
-    After the reset domain change, the rom_ctrl checks happen
-    in parallel with life cycle control initilization.
+    This will make most of tests 100% pass, except stress_all
+    1. Update constrain for flash_mode, need to check mailbox_en before pick an address
+    2. Update scb to fix prediction of flash_status for a corner case that dummy item involves
+    3. Fix some regression timeout
+    4. Remove dummy item in stress_all as it's added in subseq already
     
-    This means waiting for rom_ctrl alone to be done is insufficient
-    and we need to wait for life cycle control to be done as well.
-    
-    Signed-off-by: Timothy Chen <timothytim@google.com>
+    Signed-off-by: Weicai Yang <weicai@google.com>
 
 """
 
