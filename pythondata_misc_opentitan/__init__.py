760b07bf132cca27b64974b65a4d36448ced7d00
LiteX Robot <robot@timvideos.us>
2022-10-28 12:26:11 +0000

    Updating pythondata-misc-opentitan to 0.0.post15019
    Updated data to v0.0-14877-g64ea3bd268 based on 64ea3bd268b0ad00e771aa65d735a3a89a7febcb from https://github.com/lowRISC/opentitan.
    > commit 64ea3bd268b0ad00e771aa65d735a3a89a7febcb
    > Author: Pirmin Vogel <vogelpi@lowrisc.org>
    > Date:   Mon Sep 26 18:08:11 2022 +0200
    >
    >     [top_englishbreakfast] Update dir list to delete before running topgen
    >
    >     Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,30 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post15018"
-version_tuple = (0, 0, 15018)
+version_str = "0.0.post15019"
+version_tuple = (0, 0, 15019)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post15018")
+    pversion = V("0.0.post15019")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14876"
-data_version_tuple = (0, 0, 14876)
+data_version_str = "0.0.post14877"
+data_version_tuple = (0, 0, 14877)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14876")
+    pdata_version = V("0.0.post14877")
 except ImportError:
     pass
-data_git_hash = "39eb0210e1bc6a378f052cbd43c41d502e1bac98"
-data_git_describe = "v0.0-14876-g39eb0210e1"
+data_git_hash = "64ea3bd268b0ad00e771aa65d735a3a89a7febcb"
+data_git_describe = "v0.0-14877-g64ea3bd268"
 data_git_msg = """\
-commit 39eb0210e1bc6a378f052cbd43c41d502e1bac98
+commit 64ea3bd268b0ad00e771aa65d735a3a89a7febcb
 Author: Pirmin Vogel <vogelpi@lowrisc.org>
-Date:   Thu Oct 27 22:49:58 2022 +0200
+Date:   Mon Sep 26 18:08:11 2022 +0200
 
-    [aes, dv] Align and fix FI test sequences
-    
-    This commit aligns the different FI test sequences for the AES main FSM,
-    the cipher core FSM and the CTR mode FSM. Thanks to this code
-    re-alignment a bug inside aes_control_fi_vseq.sv has been identified and
-    fixed which caused about 20% of these tests to fail.
+    [top_englishbreakfast] Update dir list to delete before running topgen
     
     Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
 
