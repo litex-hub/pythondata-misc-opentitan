9613bd7ce2dc265334bf3e2d7922317489d5fade
LiteX Robot <robot@timvideos.us>
2022-10-31 08:19:45 +0000

    Updating pythondata-misc-opentitan to 0.0.post15050
    Updated data to v0.0-14908-gd3cdb0dd5e based on d3cdb0dd5ebe4e5addb53b2e12c34a4695228a14 from https://github.com/lowRISC/opentitan.
    > commit d3cdb0dd5ebe4e5addb53b2e12c34a4695228a14
    > Author: Felix Miller <felix@opentitan.org>
    > Date:   Thu Jul 21 03:18:18 2022 +0200
    >
    >     [sw,crypto] add SHA-512 implementation for OTBN
    >
    >     Adds a OTBN SHA-512 implementation based on FIPS 180-4. This
    >     implementation is partially unrolled and favors performance over
    >     code size.
    >
    >     Signed-off-by: Felix Miller <felix@opentitan.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,36 +4,36 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post15047"
-version_tuple = (0, 0, 15047)
+version_str = "0.0.post15050"
+version_tuple = (0, 0, 15050)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post15047")
+    pversion = V("0.0.post15050")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14905"
-data_version_tuple = (0, 0, 14905)
+data_version_str = "0.0.post14908"
+data_version_tuple = (0, 0, 14908)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14905")
+    pdata_version = V("0.0.post14908")
 except ImportError:
     pass
-data_git_hash = "3f88b0938fc839b6c3d4b153b04072b6d1d9029b"
-data_git_describe = "v0.0-14905-g3f88b0938f"
+data_git_hash = "d3cdb0dd5ebe4e5addb53b2e12c34a4695228a14"
+data_git_describe = "v0.0-14908-gd3cdb0dd5e"
 data_git_msg = """\
-commit 3f88b0938fc839b6c3d4b153b04072b6d1d9029b
-Author: Weicai Yang <weicai@google.com>
-Date:   Sun Oct 30 21:50:54 2022 -0700
+commit d3cdb0dd5ebe4e5addb53b2e12c34a4695228a14
+Author: Felix Miller <felix@opentitan.org>
+Date:   Thu Jul 21 03:18:18 2022 +0200
 
-    [spi_device/dv] Fix a constraint error
+    [sw,crypto] add SHA-512 implementation for OTBN
     
-    Recent constraint update affected the direct test - read_buffer_direct
-    Changed to call a uvm_reg_field randomize instead of vseq randomize,
-    since this direct test doesn't need to the complex constraint in base_vseq.
+    Adds a OTBN SHA-512 implementation based on FIPS 180-4. This
+    implementation is partially unrolled and favors performance over
+    code size.
     
-    Signed-off-by: Weicai Yang <weicai@google.com>
+    Signed-off-by: Felix Miller <felix@opentitan.org>
 
 """
 
