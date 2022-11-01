26abe1cb2edeba335d0911702905fa68bfd1a41c
LiteX Robot <robot@timvideos.us>
2022-11-01 18:18:32 +0000

    Updating pythondata-misc-opentitan to 0.0.post15091
    Updated data to v0.0-14949-g8aff88ccc1 based on 8aff88ccc12afd90179361987c611f07409e8410 from https://github.com/lowRISC/opentitan.
    > commit 8aff88ccc12afd90179361987c611f07409e8410
    > Author: Miles Dai <milesdai@google.com>
    > Date:   Tue Nov 1 11:50:42 2022 -0400
    >
    >     Revert "[edn] Tweak fifo clear logic"
    >
    >     This reverts commit 3ad51c3b4fa9a10c865d9e9ea0b6b87266ebd392.
    >
    >     Signed-off-by: Miles Dai <milesdai@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,30 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post15088"
-version_tuple = (0, 0, 15088)
+version_str = "0.0.post15091"
+version_tuple = (0, 0, 15091)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post15088")
+    pversion = V("0.0.post15091")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14946"
-data_version_tuple = (0, 0, 14946)
+data_version_str = "0.0.post14949"
+data_version_tuple = (0, 0, 14949)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14946")
+    pdata_version = V("0.0.post14949")
 except ImportError:
     pass
-data_git_hash = "0871f2865370a2ab572448115f4d4ecb7758b391"
-data_git_describe = "v0.0-14946-g0871f28653"
+data_git_hash = "8aff88ccc12afd90179361987c611f07409e8410"
+data_git_describe = "v0.0-14949-g8aff88ccc1"
 data_git_msg = """\
-commit 0871f2865370a2ab572448115f4d4ecb7758b391
+commit 8aff88ccc12afd90179361987c611f07409e8410
 Author: Miles Dai <milesdai@google.com>
-Date:   Wed Oct 19 14:56:31 2022 -0400
+Date:   Tue Nov 1 11:50:42 2022 -0400
 
-    [test] Add shutdown redaction tests
+    Revert "[edn] Tweak fifo clear logic"
+    
+    This reverts commit 3ad51c3b4fa9a10c865d9e9ea0b6b87266ebd392.
     
     Signed-off-by: Miles Dai <milesdai@google.com>
 
