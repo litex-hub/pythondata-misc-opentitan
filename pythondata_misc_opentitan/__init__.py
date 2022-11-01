d1b3f151173acc25c9c3543fe50087f60f6e5e6d
LiteX Robot <robot@timvideos.us>
2022-11-01 21:16:40 +0000

    Updating pythondata-misc-opentitan to 0.0.post15096
    Updated data to v0.0-14954-g27c13f0951 based on 27c13f095112b2621e2a9e7fb784dc23b7c73485 from https://github.com/lowRISC/opentitan.
    > commit 27c13f095112b2621e2a9e7fb784dc23b7c73485
    > Author: Vladimir Rozic <vrozic@lowrisc.org>
    > Date:   Tue Nov 1 15:47:34 2022 +0000
    >
    >     [python] Removing numpy from the python requirements
    >
    >     Removing numpy from python-requirements.txt as it is not currently
    >     used.
    >
    >     Signed-off-by: Vladimir Rozic <vrozic@lowrisc.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post15092"
-version_tuple = (0, 0, 15092)
+version_str = "0.0.post15096"
+version_tuple = (0, 0, 15096)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post15092")
+    pversion = V("0.0.post15096")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14950"
-data_version_tuple = (0, 0, 14950)
+data_version_str = "0.0.post14954"
+data_version_tuple = (0, 0, 14954)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14950")
+    pdata_version = V("0.0.post14954")
 except ImportError:
     pass
-data_git_hash = "6f00d58c2028dcd984d0c56ac6bc9a3a6b530880"
-data_git_describe = "v0.0-14950-g6f00d58c20"
+data_git_hash = "27c13f095112b2621e2a9e7fb784dc23b7c73485"
+data_git_describe = "v0.0-14954-g27c13f0951"
 data_git_msg = """\
-commit 6f00d58c2028dcd984d0c56ac6bc9a3a6b530880
-Author: Srikrishna Iyer <sriyer@google.com>
-Date:   Mon Oct 31 10:57:43 2022 -0700
+commit 27c13f095112b2621e2a9e7fb784dc23b7c73485
+Author: Vladimir Rozic <vrozic@lowrisc.org>
+Date:   Tue Nov 1 15:47:34 2022 +0000
 
-    [prim-lfsr] Fix DefaultSeedLocal compile scope
+    [python] Removing numpy from the python requirements
     
-    Use `ifdef SIMULATION` compilation scope for DefaultSeedLocal`
-    setting in prim_lfsr / prim_double_lfsr.
+    Removing numpy from python-requirements.txt as it is not currently
+    used.
     
-    Signed-off-by: Srikrishna Iyer <sriyer@google.com>
+    Signed-off-by: Vladimir Rozic <vrozic@lowrisc.org>
 
 """
 
