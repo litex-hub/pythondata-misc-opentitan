9ffd69072601f4a4eeb7fb1cbdb35f3e0aad14c4
LiteX Robot <robot@timvideos.us>
2022-11-02 16:19:40 +0000

    Updating pythondata-misc-opentitan to 0.0.post15137
    Updated data to v0.0-14995-gdb5905ee18 based on db5905ee18fd9573a705d6802424433f660bc0cb from https://github.com/lowRISC/opentitan.
    > commit db5905ee18fd9573a705d6802424433f660bc0cb
    > Author: Michael Schaffner <msf@google.com>
    > Date:   Tue Nov 1 19:23:06 2022 -0700
    >
    >     [doc] Minor tweaks to test triage issue template
    >
    >     Signed-off-by: Michael Schaffner <msf@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post15136"
-version_tuple = (0, 0, 15136)
+version_str = "0.0.post15137"
+version_tuple = (0, 0, 15137)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post15136")
+    pversion = V("0.0.post15137")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14994"
-data_version_tuple = (0, 0, 14994)
+data_version_str = "0.0.post14995"
+data_version_tuple = (0, 0, 14995)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14994")
+    pdata_version = V("0.0.post14995")
 except ImportError:
     pass
-data_git_hash = "8e7f2778a4277a5854a0843850b812e8363e0b45"
-data_git_describe = "v0.0-14994-g8e7f2778a4"
+data_git_hash = "db5905ee18fd9573a705d6802424433f660bc0cb"
+data_git_describe = "v0.0-14995-gdb5905ee18"
 data_git_msg = """\
-commit 8e7f2778a4277a5854a0843850b812e8363e0b45
-Author: Chris Frantz <cfrantz@google.com>
-Date:   Tue Nov 1 17:10:13 2022 -0700
+commit db5905ee18fd9573a705d6802424433f660bc0cb
+Author: Michael Schaffner <msf@google.com>
+Date:   Tue Nov 1 19:23:06 2022 -0700
 
-    [rust] Update to serde-annotate v0.0.5
+    [doc] Minor tweaks to test triage issue template
     
-    Signed-off-by: Chris Frantz <cfrantz@google.com>
+    Signed-off-by: Michael Schaffner <msf@google.com>
 
 """
 
