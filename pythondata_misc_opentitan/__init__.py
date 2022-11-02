ade9a9d0067b22d144482a22520d746a9f6dc0d4
LiteX Robot <robot@timvideos.us>
2022-11-02 05:43:48 +0000

    Updating pythondata-misc-opentitan to 0.0.post15118
    Updated data to v0.0-14976-ga40885552d based on a40885552d0776e909a41cdb71095b76cd3bbcbf from https://github.com/lowRISC/opentitan.
    > commit a40885552d0776e909a41cdb71095b76cd3bbcbf
    > Author: Srikrishna Iyer <sriyer@google.com>
    > Date:   Fri Oct 28 11:09:31 2022 -0700
    >
    >     [rtl, chip dv] Coverage exclusions for pinmux / padring
    >
    >     This commit adds a large number of exclusions for pinmux / padctrl
    >     / top_earlgrey, related to the tieoffs in `*attr*` signals.
    >
    >     It also marks unused signals in `prim_generic_pad_wrapper` with
    >     inline coverage off pragmas, and excludes `prim_generic_pad_attr`
    >     entirely from coverage collection (LOW_RISK).
    >
    >     Signed-off-by: Srikrishna Iyer <sriyer@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,37 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post15110"
-version_tuple = (0, 0, 15110)
+version_str = "0.0.post15118"
+version_tuple = (0, 0, 15118)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post15110")
+    pversion = V("0.0.post15118")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14968"
-data_version_tuple = (0, 0, 14968)
+data_version_str = "0.0.post14976"
+data_version_tuple = (0, 0, 14976)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14968")
+    pdata_version = V("0.0.post14976")
 except ImportError:
     pass
-data_git_hash = "b9bffe464791584200251aea1039e9e4fa028d94"
-data_git_describe = "v0.0-14968-gb9bffe4647"
+data_git_hash = "a40885552d0776e909a41cdb71095b76cd3bbcbf"
+data_git_describe = "v0.0-14976-ga40885552d"
 data_git_msg = """\
-commit b9bffe464791584200251aea1039e9e4fa028d94
+commit a40885552d0776e909a41cdb71095b76cd3bbcbf
 Author: Srikrishna Iyer <sriyer@google.com>
-Date:   Tue Nov 1 18:31:30 2022 -0700
+Date:   Fri Oct 28 11:09:31 2022 -0700
 
-    [chip dv] Minor enhance,ent to adc_ctrl debug cable test
+    [rtl, chip dv] Coverage exclusions for pinmux / padring
     
-    Check interrupt_expected to be true in the ISR.
+    This commit adds a large number of exclusions for pinmux / padctrl
+    / top_earlgrey, related to the tieoffs in `*attr*` signals.
+    
+    It also marks unused signals in `prim_generic_pad_wrapper` with
+    inline coverage off pragmas, and excludes `prim_generic_pad_attr`
+    entirely from coverage collection (LOW_RISK).
     
     Signed-off-by: Srikrishna Iyer <sriyer@google.com>
 
