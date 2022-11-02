96482179fe2a52b9c6ab0ff52d5fc0690608302f
LiteX Robot <robot@timvideos.us>
2022-11-02 05:00:10 +0000

    Updating pythondata-misc-opentitan to 0.0.post15110
    Updated data to v0.0-14968-gb9bffe4647 based on b9bffe464791584200251aea1039e9e4fa028d94 from https://github.com/lowRISC/opentitan.
    > commit b9bffe464791584200251aea1039e9e4fa028d94
    > Author: Srikrishna Iyer <sriyer@google.com>
    > Date:   Tue Nov 1 18:31:30 2022 -0700
    >
    >     [chip dv] Minor enhance,ent to adc_ctrl debug cable test
    >
    >     Check interrupt_expected to be true in the ISR.
    >
    >     Signed-off-by: Srikrishna Iyer <sriyer@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post15103"
-version_tuple = (0, 0, 15103)
+version_str = "0.0.post15110"
+version_tuple = (0, 0, 15110)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post15103")
+    pversion = V("0.0.post15110")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14961"
-data_version_tuple = (0, 0, 14961)
+data_version_str = "0.0.post14968"
+data_version_tuple = (0, 0, 14968)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14961")
+    pdata_version = V("0.0.post14968")
 except ImportError:
     pass
-data_git_hash = "eb0bd40a0b48e8c9cb111f2aef4125cda34fd5e8"
-data_git_describe = "v0.0-14961-geb0bd40a0b"
+data_git_hash = "b9bffe464791584200251aea1039e9e4fa028d94"
+data_git_describe = "v0.0-14968-gb9bffe4647"
 data_git_msg = """\
-commit eb0bd40a0b48e8c9cb111f2aef4125cda34fd5e8
-Author: Miles Dai <milesdai@google.com>
-Date:   Tue Nov 1 17:04:10 2022 -0400
+commit b9bffe464791584200251aea1039e9e4fa028d94
+Author: Srikrishna Iyer <sriyer@google.com>
+Date:   Tue Nov 1 18:31:30 2022 -0700
 
-    [ci] Enable Vivado on FPGA runners
+    [chip dv] Minor enhance,ent to adc_ctrl debug cable test
     
-    Signed-off-by: Miles Dai <milesdai@google.com>
+    Check interrupt_expected to be true in the ISR.
+    
+    Signed-off-by: Srikrishna Iyer <sriyer@google.com>
 
 """
 
