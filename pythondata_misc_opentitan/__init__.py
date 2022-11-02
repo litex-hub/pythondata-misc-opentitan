e2d2f9c4970aae57cb9d922ff17abbb8328d1e4e
LiteX Robot <robot@timvideos.us>
2022-11-02 17:29:41 +0000

    Updating pythondata-misc-opentitan to 0.0.post15139
    Updated data to v0.0-14997-g3a7c11f8b3 based on 3a7c11f8b3fbabd45ce31e9fd4ed1a2fb0b6eb86 from https://github.com/lowRISC/opentitan.
    > commit 3a7c11f8b3fbabd45ce31e9fd4ed1a2fb0b6eb86
    > Author: Timothy Chen <timothytim@google.com>
    > Date:   Tue Nov 1 23:19:42 2022 -0700
    >
    >     [top/dv] Add missing queue constraint
    >
    >     - fixes #15895
    >
    >     Signed-off-by: Timothy Chen <timothytim@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post15137"
-version_tuple = (0, 0, 15137)
+version_str = "0.0.post15139"
+version_tuple = (0, 0, 15139)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post15137")
+    pversion = V("0.0.post15139")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14995"
-data_version_tuple = (0, 0, 14995)
+data_version_str = "0.0.post14997"
+data_version_tuple = (0, 0, 14997)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14995")
+    pdata_version = V("0.0.post14997")
 except ImportError:
     pass
-data_git_hash = "db5905ee18fd9573a705d6802424433f660bc0cb"
-data_git_describe = "v0.0-14995-gdb5905ee18"
+data_git_hash = "3a7c11f8b3fbabd45ce31e9fd4ed1a2fb0b6eb86"
+data_git_describe = "v0.0-14997-g3a7c11f8b3"
 data_git_msg = """\
-commit db5905ee18fd9573a705d6802424433f660bc0cb
-Author: Michael Schaffner <msf@google.com>
-Date:   Tue Nov 1 19:23:06 2022 -0700
+commit 3a7c11f8b3fbabd45ce31e9fd4ed1a2fb0b6eb86
+Author: Timothy Chen <timothytim@google.com>
+Date:   Tue Nov 1 23:19:42 2022 -0700
 
-    [doc] Minor tweaks to test triage issue template
+    [top/dv] Add missing queue constraint
     
-    Signed-off-by: Michael Schaffner <msf@google.com>
+    - fixes #15895
+    
+    Signed-off-by: Timothy Chen <timothytim@google.com>
 
 """
 
