0ef6c082865ffe1d15875a9328317e535885eaea
LiteX Robot <robot@timvideos.us>
2022-11-02 03:21:32 +0000

    Updating pythondata-misc-opentitan to 0.0.post15103
    Updated data to v0.0-14961-geb0bd40a0b based on eb0bd40a0b48e8c9cb111f2aef4125cda34fd5e8 from https://github.com/lowRISC/opentitan.
    > commit eb0bd40a0b48e8c9cb111f2aef4125cda34fd5e8
    > Author: Miles Dai <milesdai@google.com>
    > Date:   Tue Nov 1 17:04:10 2022 -0400
    >
    >     [ci] Enable Vivado on FPGA runners
    >
    >     Signed-off-by: Miles Dai <milesdai@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,32 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post15102"
-version_tuple = (0, 0, 15102)
+version_str = "0.0.post15103"
+version_tuple = (0, 0, 15103)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post15102")
+    pversion = V("0.0.post15103")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14960"
-data_version_tuple = (0, 0, 14960)
+data_version_str = "0.0.post14961"
+data_version_tuple = (0, 0, 14961)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14960")
+    pdata_version = V("0.0.post14961")
 except ImportError:
     pass
-data_git_hash = "f3e95c6d0bc66483c94bc07fed6aab8b9d38b25c"
-data_git_describe = "v0.0-14960-gf3e95c6d0b"
+data_git_hash = "eb0bd40a0b48e8c9cb111f2aef4125cda34fd5e8"
+data_git_describe = "v0.0-14961-geb0bd40a0b"
 data_git_msg = """\
-commit f3e95c6d0bc66483c94bc07fed6aab8b9d38b25c
-Author: Michael Schaffner <msf@google.com>
-Date:   Tue Nov 1 11:13:51 2022 -0700
+commit eb0bd40a0b48e8c9cb111f2aef4125cda34fd5e8
+Author: Miles Dai <milesdai@google.com>
+Date:   Tue Nov 1 17:04:10 2022 -0400
 
-    [prim_diff_decode] Update SVAs to make them compatible with sim CDC
+    [ci] Enable Vivado on FPGA runners
     
-    This is done in preparation for CDC instrumentation in simulation,
-    see here: #15863
-    
-    Signed-off-by: Michael Schaffner <msf@google.com>
+    Signed-off-by: Miles Dai <milesdai@google.com>
 
 """
 
