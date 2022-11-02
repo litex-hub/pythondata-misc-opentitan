410d868aa82b5c38dcf694c98f00fc586e0d7070
LiteX Robot <robot@timvideos.us>
2022-11-02 11:14:19 +0000

    Updating pythondata-misc-opentitan to 0.0.post15129
    Updated data to v0.0-14987-g3b4b30e487 based on 3b4b30e4875994e1c91619e99bcc6789e9b8cedc from https://github.com/lowRISC/opentitan.
    > commit 3b4b30e4875994e1c91619e99bcc6789e9b8cedc
    > Author: Alphan Ulusoy <alphan@google.com>
    > Date:   Tue Nov 1 15:31:18 2022 -0400
    >
    >     [test] Run rom_e2e_shutdown_output for remaining life cycle states
    >
    >     Fixes #14270
    >
    >     Signed-off-by: Alphan Ulusoy <alphan@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post15122"
-version_tuple = (0, 0, 15122)
+version_str = "0.0.post15129"
+version_tuple = (0, 0, 15129)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post15122")
+    pversion = V("0.0.post15129")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post14980"
-data_version_tuple = (0, 0, 14980)
+data_version_str = "0.0.post14987"
+data_version_tuple = (0, 0, 14987)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post14980")
+    pdata_version = V("0.0.post14987")
 except ImportError:
     pass
-data_git_hash = "971782fde76e9ae79cd4d4b44fadd770f94b6b82"
-data_git_describe = "v0.0-14980-g971782fde7"
+data_git_hash = "3b4b30e4875994e1c91619e99bcc6789e9b8cedc"
+data_git_describe = "v0.0-14987-g3b4b30e487"
 data_git_msg = """\
-commit 971782fde76e9ae79cd4d4b44fadd770f94b6b82
-Author: Chris Frantz <cfrantz@google.com>
-Date:   Fri Oct 28 15:28:41 2022 -0700
+commit 3b4b30e4875994e1c91619e99bcc6789e9b8cedc
+Author: Alphan Ulusoy <alphan@google.com>
+Date:   Tue Nov 1 15:31:18 2022 -0400
 
-    Update rustfmt rule
+    [test] Run rom_e2e_shutdown_output for remaining life cycle states
     
-    Signed-off-by: Chris Frantz <cfrantz@google.com>
+    Fixes #14270
+    
+    Signed-off-by: Alphan Ulusoy <alphan@google.com>
 
 """
 
