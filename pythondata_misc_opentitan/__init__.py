daf3b108765abc0cab33fa1ced252cf9f5731826
LiteX Robot <robot@timvideos.us>
2022-11-03 11:12:39 +0000

    Updating pythondata-misc-opentitan to 0.0.post15167
    Updated data to v0.0-15025-gcbba276294 based on cbba276294efa163b5c5be029a9487c9908d9dd2 from https://github.com/lowRISC/opentitan.
    > commit cbba276294efa163b5c5be029a9487c9908d9dd2
    > Author: James Wainwright <james.wainwright@lowrisc.org>
    > Date:   Thu Oct 20 16:39:18 2022 +0100
    >
    >     [docs] Add styleguide example for documented struct
    >
    >     Clarifies that block comments should be used for structs and struct members as
    >     well as functions.
    >
    >     Signed-off-by: James Wainwright <james.wainwright@lowrisc.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,47 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post15166"
-version_tuple = (0, 0, 15166)
+version_str = "0.0.post15167"
+version_tuple = (0, 0, 15167)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post15166")
+    pversion = V("0.0.post15167")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post15024"
-data_version_tuple = (0, 0, 15024)
+data_version_str = "0.0.post15025"
+data_version_tuple = (0, 0, 15025)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post15024")
+    pdata_version = V("0.0.post15025")
 except ImportError:
     pass
-data_git_hash = "3e32a31a2ba47298a1041913daa27e827b5068b0"
-data_git_describe = "v0.0-15024-g3e32a31a2b"
+data_git_hash = "cbba276294efa163b5c5be029a9487c9908d9dd2"
+data_git_describe = "v0.0-15025-gcbba276294"
 data_git_msg = """\
-commit 3e32a31a2ba47298a1041913daa27e827b5068b0
-Author: Srikrishna Iyer <sriyer@google.com>
-Date:   Wed Nov 2 22:03:03 2022 -0700
+commit cbba276294efa163b5c5be029a9487c9908d9dd2
+Author: James Wainwright <james.wainwright@lowrisc.org>
+Date:   Thu Oct 20 16:39:18 2022 +0100
 
-    [chip dv] Fixes for failing CSR tests
+    [docs] Add styleguide example for documented struct
     
-    Fixes chip_csr_hw_reset test with failure signature:
-    ```
-      UVM_ERROR @ 2823.589889 us: (chip_if.sv:146) [tb.dut.chip_if] Detected an X on MioPadIor13
-    ```
-    - Added weak pulls on DIOs for this test, since it randomly writes pinmux pad attr
-      and wake up registers and inadvertently creates a combination that ends up spewing Xs.
+    Clarifies that block comments should be used for structs and struct members as
+    well as functions.
     
-    Fixes chip_csr_bit_bash test with failure signature:
-    ```
-    Job chip_earlgrey_asic-sim-vcs_run_cover_reg_top killed due to: Exit reason: User job exceeded
-    runlimit: User job timed out has 1 failures:
-    ```
-    - Limited the number of CSRs tested for bit bash to 200 at a time, increased
-      test timeout to 180 minutes.
-    
-    Signed-off-by: Srikrishna Iyer <sriyer@google.com>
+    Signed-off-by: James Wainwright <james.wainwright@lowrisc.org>
 
 """
 
