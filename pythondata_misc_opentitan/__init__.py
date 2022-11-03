0640af03902e9406bdc83e4f318c10053991b034
LiteX Robot <robot@timvideos.us>
2022-11-03 07:14:32 +0000

    Updating pythondata-misc-opentitan to 0.0.post15166
    Updated data to v0.0-15024-g3e32a31a2b based on 3e32a31a2ba47298a1041913daa27e827b5068b0 from https://github.com/lowRISC/opentitan.
    > commit 3e32a31a2ba47298a1041913daa27e827b5068b0
    > Author: Srikrishna Iyer <sriyer@google.com>
    > Date:   Wed Nov 2 22:03:03 2022 -0700
    >
    >     [chip dv] Fixes for failing CSR tests
    >
    >     Fixes chip_csr_hw_reset test with failure signature:
    >     ```
    >       UVM_ERROR @ 2823.589889 us: (chip_if.sv:146) [tb.dut.chip_if] Detected an X on MioPadIor13
    >     ```
    >     - Added weak pulls on DIOs for this test, since it randomly writes pinmux pad attr
    >       and wake up registers and inadvertently creates a combination that ends up spewing Xs.
    >
    >     Fixes chip_csr_bit_bash test with failure signature:
    >     ```
    >     Job chip_earlgrey_asic-sim-vcs_run_cover_reg_top killed due to: Exit reason: User job exceeded
    >     runlimit: User job timed out has 1 failures:
    >     ```
    >     - Limited the number of CSRs tested for bit bash to 200 at a time, increased
    >       test timeout to 180 minutes.
    >
    >     Signed-off-by: Srikrishna Iyer <sriyer@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,37 +4,47 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post15165"
-version_tuple = (0, 0, 15165)
+version_str = "0.0.post15166"
+version_tuple = (0, 0, 15166)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post15165")
+    pversion = V("0.0.post15166")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post15023"
-data_version_tuple = (0, 0, 15023)
+data_version_str = "0.0.post15024"
+data_version_tuple = (0, 0, 15024)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post15023")
+    pdata_version = V("0.0.post15024")
 except ImportError:
     pass
-data_git_hash = "d3ed1298de886116adcab66f0c3609fd0dcd9f82"
-data_git_describe = "v0.0-15023-gd3ed1298de"
+data_git_hash = "3e32a31a2ba47298a1041913daa27e827b5068b0"
+data_git_describe = "v0.0-15024-g3e32a31a2b"
 data_git_msg = """\
-commit d3ed1298de886116adcab66f0c3609fd0dcd9f82
-Author: Miguel Osorio <miguelosorio@google.com>
-Date:   Wed Nov 2 19:05:31 2022 -0700
+commit 3e32a31a2ba47298a1041913daa27e827b5068b0
+Author: Srikrishna Iyer <sriyer@google.com>
+Date:   Wed Nov 2 22:03:03 2022 -0700
 
-    [top-test] Update flash_ctrl_test buffer sizes
+    [chip dv] Fixes for failing CSR tests
     
-    Update buffer sizes to cover the entire page based on
-    `flash_info.bytes_per_page`.
+    Fixes chip_csr_hw_reset test with failure signature:
+    ```
+      UVM_ERROR @ 2823.589889 us: (chip_if.sv:146) [tb.dut.chip_if] Detected an X on MioPadIor13
+    ```
+    - Added weak pulls on DIOs for this test, since it randomly writes pinmux pad attr
+      and wake up registers and inadvertently creates a combination that ends up spewing Xs.
     
-    Fixes #10848.
+    Fixes chip_csr_bit_bash test with failure signature:
+    ```
+    Job chip_earlgrey_asic-sim-vcs_run_cover_reg_top killed due to: Exit reason: User job exceeded
+    runlimit: User job timed out has 1 failures:
+    ```
+    - Limited the number of CSRs tested for bit bash to 200 at a time, increased
+      test timeout to 180 minutes.
     
-    Signed-off-by: Miguel Osorio <miguelosorio@google.com>
+    Signed-off-by: Srikrishna Iyer <sriyer@google.com>
 
 """
 
