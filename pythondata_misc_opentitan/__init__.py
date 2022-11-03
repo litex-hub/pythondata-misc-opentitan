6c015a77ec85f0966690783dede7c7543f8a84d6
LiteX Robot <robot@timvideos.us>
2022-11-03 22:13:46 +0000

    Updating pythondata-misc-opentitan to 0.0.post15189
    Updated data to v0.0-15047-g1a0c43025e based on 1a0c43025eb0a475db5cf1ae5944e2063736629c from https://github.com/lowRISC/opentitan.
    > commit 1a0c43025eb0a475db5cf1ae5944e2063736629c
    > Author: Cindy Chen <chencindy@opentitan.org>
    > Date:   Thu Nov 3 10:34:02 2022 -0700
    >
    >     [dv/csrng_agent] Update coverage collection
    >
    >     This PR splits CSRNG command coverage to two parts:
    >     On device mode, some acmd values are illegal,
    >     but in host mode, we want to check how csrng response to these illegal
    >     cmds.
    >
    >     Related issue: #15947
    >
    >     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,33 +4,37 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post15176"
-version_tuple = (0, 0, 15176)
+version_str = "0.0.post15189"
+version_tuple = (0, 0, 15189)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post15176")
+    pversion = V("0.0.post15189")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post15034"
-data_version_tuple = (0, 0, 15034)
+data_version_str = "0.0.post15047"
+data_version_tuple = (0, 0, 15047)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post15034")
+    pdata_version = V("0.0.post15047")
 except ImportError:
     pass
-data_git_hash = "22b25ce20cb22e3e5f95e010b60ec2e5e26ab971"
-data_git_describe = "v0.0-15034-g22b25ce20c"
+data_git_hash = "1a0c43025eb0a475db5cf1ae5944e2063736629c"
+data_git_describe = "v0.0-15047-g1a0c43025e"
 data_git_msg = """\
-commit 22b25ce20cb22e3e5f95e010b60ec2e5e26ab971
+commit 1a0c43025eb0a475db5cf1ae5944e2063736629c
 Author: Cindy Chen <chencindy@opentitan.org>
-Date:   Thu Nov 3 10:40:39 2022 -0700
+Date:   Thu Nov 3 10:34:02 2022 -0700
 
-    [edn/doc] Update rw0c register description
+    [dv/csrng_agent] Update coverage collection
     
-    Fix a small typo in regwen. The RW0C should be write value zero to set
-    the regwen.
+    This PR splits CSRNG command coverage to two parts:
+    On device mode, some acmd values are illegal,
+    but in host mode, we want to check how csrng response to these illegal
+    cmds.
+    
+    Related issue: #15947
     
     Signed-off-by: Cindy Chen <chencindy@opentitan.org>
 
