f251586f84f5a22156b891fd6710e37777814966
LiteX Robot <robot@timvideos.us>
2022-11-03 03:57:09 +0000

    Updating pythondata-misc-opentitan to 0.0.post15159
    Updated data to v0.0-15017-gba9eccf37e based on ba9eccf37e2f6b5f44d84ad02f2c5d8f9a6154e8 from https://github.com/lowRISC/opentitan.
    > commit ba9eccf37e2f6b5f44d84ad02f2c5d8f9a6154e8
    > Author: Michael Schaffner <msf@google.com>
    > Date:   Wed Nov 2 18:46:24 2022 -0700
    >
    >     [pinmux] use JTAG TAP input assumptions only in FPV
    >
    >     Simulations may fail if the TAP inputs are not fully stable (which
    >     cannot be guaranteed within the pinmux).
    >
    >     Signed-off-by: Michael Schaffner <msf@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,42 +4,33 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post15157"
-version_tuple = (0, 0, 15157)
+version_str = "0.0.post15159"
+version_tuple = (0, 0, 15159)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post15157")
+    pversion = V("0.0.post15159")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post15015"
-data_version_tuple = (0, 0, 15015)
+data_version_str = "0.0.post15017"
+data_version_tuple = (0, 0, 15017)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post15015")
+    pdata_version = V("0.0.post15017")
 except ImportError:
     pass
-data_git_hash = "f3da3f66ef03e2485f51e7626d5dc46a93273ac5"
-data_git_describe = "v0.0-15015-gf3da3f66ef"
+data_git_hash = "ba9eccf37e2f6b5f44d84ad02f2c5d8f9a6154e8"
+data_git_describe = "v0.0-15017-gba9eccf37e"
 data_git_msg = """\
-commit f3da3f66ef03e2485f51e7626d5dc46a93273ac5
+commit ba9eccf37e2f6b5f44d84ad02f2c5d8f9a6154e8
 Author: Michael Schaffner <msf@google.com>
-Date:   Wed Nov 2 13:13:41 2022 -0700
+Date:   Wed Nov 2 18:46:24 2022 -0700
 
-    [dv] Always preload ROM in chip_base_vseq
+    [pinmux] use JTAG TAP input assumptions only in FPV
     
-    chip_stub_cpu_base_vseq which caused the ROM init done check in
-    dut_init() of the chip_rv_dm_lc_disabled_vseq to hang forever.
-    
-    This PR removes the ROM init check from chip_rv_dm_lc_disabled_vseq
-    to fix that. In addition, it always pre-loads a valid ROM image
-    during the pre_start() phase of chip_base_vseq to make sure that
-    the ROM check can succeed successfully. Note that the real hardware
-    will always contain a valid ROM image, so this is not a simulation-only
-    hack and bring simulations closer to the behavior of the real hardware.
-    
-    This fixes #15893.
+    Simulations may fail if the TAP inputs are not fully stable (which
+    cannot be guaranteed within the pinmux).
     
     Signed-off-by: Michael Schaffner <msf@google.com>
 
