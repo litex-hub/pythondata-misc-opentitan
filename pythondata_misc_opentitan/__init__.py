87c5efc3e3b324a2ed1e383a0a2d8c7c6b0df0f4
LiteX Robot <robot@timvideos.us>
2022-11-04 14:13:36 +0000

    Updating pythondata-misc-opentitan to 0.0.post15238
    Updated data to v0.0-15096-g024279c574 based on 024279c574b6be805ed33286801e62a2ed16fa13 from https://github.com/lowRISC/opentitan.
    > commit 024279c574b6be805ed33286801e62a2ed16fa13
    > Author: Dan McArdle <dmcardle@google.com>
    > Date:   Wed Nov 2 15:32:12 2022 -0400
    >
    >     [bazel] Use GDB from Bazel instead of /tools
    >
    >     Fixes #15935
    >
    >     Signed-off-by: Dan McArdle <dmcardle@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,44 +4,34 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post15237"
-version_tuple = (0, 0, 15237)
+version_str = "0.0.post15238"
+version_tuple = (0, 0, 15238)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post15237")
+    pversion = V("0.0.post15238")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post15095"
-data_version_tuple = (0, 0, 15095)
+data_version_str = "0.0.post15096"
+data_version_tuple = (0, 0, 15096)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post15095")
+    pdata_version = V("0.0.post15096")
 except ImportError:
     pass
-data_git_hash = "a09d5ca30df75679de69d417dbe40e2d3d899601"
-data_git_describe = "v0.0-15095-ga09d5ca30d"
+data_git_hash = "024279c574b6be805ed33286801e62a2ed16fa13"
+data_git_describe = "v0.0-15096-g024279c574"
 data_git_msg = """\
-commit a09d5ca30df75679de69d417dbe40e2d3d899601
-Author: Pirmin Vogel <vogelpi@lowrisc.org>
-Date:   Fri Nov 4 01:53:34 2022 +0100
+commit 024279c574b6be805ed33286801e62a2ed16fa13
+Author: Dan McArdle <dmcardle@google.com>
+Date:   Wed Nov 2 15:32:12 2022 -0400
 
-    [aes, dv] Depending on FI target signal, force different values
+    [bazel] Use GDB from Bazel instead of /tools
     
-    For some FI target signals, forcing certain values doesn't have any
-    impact on the functionality of the design. For example:
-    - Forcing a 1 on a handshake signal that is only read if the FSM is
-      anyway ready to proceed, or if the handshake is backed up by an
-      additional local counter.
-    - Forcing a 0 where multiple copies of the signal are OR-comibined
-      together.
-    This commit changes the FI tests to force values depending on the target
-    signal.
+    Fixes #15935
     
-    This resolves lowRISC/OpenTitan#13572.
-    
-    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>
+    Signed-off-by: Dan McArdle <dmcardle@google.com>
 
 """
 
