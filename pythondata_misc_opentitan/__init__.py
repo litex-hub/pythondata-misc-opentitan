2a4efb0e650f8571f4314a3adae11a140375b381
LiteX Robot <robot@timvideos.us>
2022-11-04 19:14:00 +0000

    Updating pythondata-misc-opentitan to 0.0.post15252
    Updated data to v0.0-15110-ga1284fc01b based on a1284fc01b5a8d6c081d458cdcb37ebb48202ef7 from https://github.com/lowRISC/opentitan.
    > commit a1284fc01b5a8d6c081d458cdcb37ebb48202ef7
    > Author: Chris Frantz <cfrantz@google.com>
    > Date:   Wed Oct 19 12:19:22 2022 -0700
    >
    >     [rom-e2e] Complete the asm-init test
    >
    >     1. Update the ChipStartup message exchanged between the riscv core and
    >        the test harness to include all of the information needed to evaluate
    >        correct initialization of the chip.
    >     2. Update the chip_specific_startup DUT program to gather all of the
    >        required information: lifecycle state, OTP config, ast done bit, clock
    >        jitter config, entropy config, ePMP, interrupt config and SRAM config.
    >     3. Evaluate the data from the DUT against the OTP configuration and
    >        lifecycle state.
    >
    >     Signed-off-by: Chris Frantz <cfrantz@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,41 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post15247"
-version_tuple = (0, 0, 15247)
+version_str = "0.0.post15252"
+version_tuple = (0, 0, 15252)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post15247")
+    pversion = V("0.0.post15252")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post15105"
-data_version_tuple = (0, 0, 15105)
+data_version_str = "0.0.post15110"
+data_version_tuple = (0, 0, 15110)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post15105")
+    pdata_version = V("0.0.post15110")
 except ImportError:
     pass
-data_git_hash = "fb80b7ab8909c8f9d45683c77258624e986a28b3"
-data_git_describe = "v0.0-15105-gfb80b7ab89"
+data_git_hash = "a1284fc01b5a8d6c081d458cdcb37ebb48202ef7"
+data_git_describe = "v0.0-15110-ga1284fc01b"
 data_git_msg = """\
-commit fb80b7ab8909c8f9d45683c77258624e986a28b3
-Author: Weicai Yang <weicai@google.com>
-Date:   Thu Nov 3 14:55:40 2022 -0700
+commit a1284fc01b5a8d6c081d458cdcb37ebb48202ef7
+Author: Chris Frantz <cfrantz@google.com>
+Date:   Wed Oct 19 12:19:22 2022 -0700
 
-    [dv] Skip checking alert if scb is disabled
+    [rom-e2e] Complete the asm-init test
     
-    scb may be disabled in the middle of sim. Check if scb is off before firing an error.
+    1. Update the ChipStartup message exchanged between the riscv core and
+       the test harness to include all of the information needed to evaluate
+       correct initialization of the chip.
+    2. Update the chip_specific_startup DUT program to gather all of the
+       required information: lifecycle state, OTP config, ast done bit, clock
+       jitter config, entropy config, ePMP, interrupt config and SRAM config.
+    3. Evaluate the data from the DUT against the OTP configuration and
+       lifecycle state.
     
-    Signed-off-by: Weicai Yang <weicai@google.com>
+    Signed-off-by: Chris Frantz <cfrantz@google.com>
 
 """
 
