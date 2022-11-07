f42db83337a3e3cdd34920d4b4a8d7791c93ac12
LiteX Robot <robot@timvideos.us>
2022-11-07 05:27:04 +0000

    Updating pythondata-misc-opentitan to 0.0.post15264
    Updated data to v0.0-15122-ga029fa5c6c based on a029fa5c6c48ddb5e23d473c793f1f279a943fe9 from https://github.com/lowRISC/opentitan.
    > commit a029fa5c6c48ddb5e23d473c793f1f279a943fe9
    > Author: Weicai Yang <weicai@google.com>
    > Date:   Fri Nov 4 16:27:48 2022 -0700
    >
    >     [keymgr/dv] Test fsm_consistency
    >
    >     Test one more custom countermeasure
    >      - Set `ral.control_shadowed` to OpDisable, so that no Advance or Generate operation
    >       is selected.
    >      - Force internal `tb.dut.u_ctrl.adv_en_o` or `tb.dut.u_ctrl.gen_en_o` to 1
    >
    >     Signed-off-by: Weicai Yang <weicai@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,32 +4,37 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post15262"
-version_tuple = (0, 0, 15262)
+version_str = "0.0.post15264"
+version_tuple = (0, 0, 15264)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post15262")
+    pversion = V("0.0.post15264")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post15120"
-data_version_tuple = (0, 0, 15120)
+data_version_str = "0.0.post15122"
+data_version_tuple = (0, 0, 15122)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post15120")
+    pdata_version = V("0.0.post15122")
 except ImportError:
     pass
-data_git_hash = "2bf1fea0ba7a2dc0122deaee34ce63614356bfa4"
-data_git_describe = "v0.0-15120-g2bf1fea0ba"
+data_git_hash = "a029fa5c6c48ddb5e23d473c793f1f279a943fe9"
+data_git_describe = "v0.0-15122-ga029fa5c6c"
 data_git_msg = """\
-commit 2bf1fea0ba7a2dc0122deaee34ce63614356bfa4
-Author: Cindy Liu <hcindyl@google.com>
-Date:   Fri Nov 4 15:56:03 2022 -0700
+commit a029fa5c6c48ddb5e23d473c793f1f279a943fe9
+Author: Weicai Yang <weicai@google.com>
+Date:   Fri Nov 4 16:27:48 2022 -0700
 
-    [bazel, reggen] Fix gen_rtl python library
+    [keymgr/dv] Test fsm_consistency
     
-    Signed-off-by: Cindy Liu <hcindyl@google.com>
+    Test one more custom countermeasure
+     - Set `ral.control_shadowed` to OpDisable, so that no Advance or Generate operation
+      is selected.
+     - Force internal `tb.dut.u_ctrl.adv_en_o` or `tb.dut.u_ctrl.gen_en_o` to 1
+    
+    Signed-off-by: Weicai Yang <weicai@google.com>
 
 """
 
