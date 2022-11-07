bd5be9f7d6662b53add09dc4026078d7ccfb40c5
LiteX Robot <robot@timvideos.us>
2022-11-07 18:17:55 +0000

    Updating pythondata-misc-opentitan to 0.0.post15281
    Updated data to v0.0-15139-g72211c306c based on 72211c306ccacc11da7f4f2059c291690abcf9de from https://github.com/lowRISC/opentitan.
    > commit 72211c306ccacc11da7f4f2059c291690abcf9de
    > Author: Srikrishna Iyer <sriyer@google.com>
    > Date:   Sat Nov 5 09:24:02 2022 -0700
    >
    >     [chip dv] Coverage exclusions
    >
    >     - Updated pinmux / padctrl el file with more exclusions due to tie offs.
    >     - Added exclusion for entropy source extended health test interface (unconnected)
    >     - Added exclusion for ROM ctrl -> kmac app interface (tied off data and strobe)
    >     - Updated chip coverage collection criteria to only dump port toggle coverage
    >       on ibex_top instance (skip port toggles on its submodules)
    >
    >     Signed-off-by: Srikrishna Iyer <sriyer@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,38 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post15277"
-version_tuple = (0, 0, 15277)
+version_str = "0.0.post15281"
+version_tuple = (0, 0, 15281)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post15277")
+    pversion = V("0.0.post15281")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post15135"
-data_version_tuple = (0, 0, 15135)
+data_version_str = "0.0.post15139"
+data_version_tuple = (0, 0, 15139)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post15135")
+    pdata_version = V("0.0.post15139")
 except ImportError:
     pass
-data_git_hash = "2fda8fa3159893257d289e93047565d0ec301af5"
-data_git_describe = "v0.0-15135-g2fda8fa315"
+data_git_hash = "72211c306ccacc11da7f4f2059c291690abcf9de"
+data_git_describe = "v0.0-15139-g72211c306c"
 data_git_msg = """\
-commit 2fda8fa3159893257d289e93047565d0ec301af5
-Author: Michael Schaffner <msf@google.com>
-Date:   Fri Nov 4 18:12:01 2022 -0700
+commit 72211c306ccacc11da7f4f2059c291690abcf9de
+Author: Srikrishna Iyer <sriyer@google.com>
+Date:   Sat Nov 5 09:24:02 2022 -0700
 
-    [test] Minor updates to sysrst_ctrl_ec_rst_l test
+    [chip dv] Coverage exclusions
     
-    Aligns some names, adds more comments, checks and debug printouts.
+    - Updated pinmux / padctrl el file with more exclusions due to tie offs.
+    - Added exclusion for entropy source extended health test interface (unconnected)
+    - Added exclusion for ROM ctrl -> kmac app interface (tied off data and strobe)
+    - Updated chip coverage collection criteria to only dump port toggle coverage
+      on ibex_top instance (skip port toggles on its submodules)
     
-    Signed-off-by: Michael Schaffner <msf@google.com>
+    Signed-off-by: Srikrishna Iyer <sriyer@google.com>
 
 """
 
