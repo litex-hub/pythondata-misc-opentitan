d5a1b092a66387bd459cd82d2cd528aadac2a1ba
LiteX Robot <robot@timvideos.us>
2022-11-08 13:41:50 +0000

    Updating pythondata-misc-opentitan to 0.0.post15335
    Updated data to v0.0-15193-g8d99a2acf5 based on 8d99a2acf58b997811072fa2066a7acd45bfefde from https://github.com/lowRISC/opentitan.
    > commit 8d99a2acf58b997811072fa2066a7acd45bfefde
    > Author: Dan McArdle <dmcardle@google.com>
    > Date:   Fri Oct 28 17:31:53 2022 -0400
    >
    >     [test] Implement rom_e2e_asm_watchdog bite tests
    >
    >     Test that the watchdog's bite resets the core. This is achieved outside
    >     of GDB, by inspecting its output.
    >
    >     This commit adds the --gdb-expect-output-sequence flag to the GDB test
    >     coordinator script. When the script sees GDB print each of the given
    >     lines, in sequence, it will terminate GDB on the spot and consider it a
    >     success.
    >
    >     This enables us to assert that GDB reaches an echo line from the GDB
    >     script and *then* sees the core reset.
    >
    >     Signed-off-by: Dan McArdle <dmcardle@google.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,43 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post15331"
-version_tuple = (0, 0, 15331)
+version_str = "0.0.post15335"
+version_tuple = (0, 0, 15335)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post15331")
+    pversion = V("0.0.post15335")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post15189"
-data_version_tuple = (0, 0, 15189)
+data_version_str = "0.0.post15193"
+data_version_tuple = (0, 0, 15193)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post15189")
+    pdata_version = V("0.0.post15193")
 except ImportError:
     pass
-data_git_hash = "ce22188696373f4f1712624f08ced2b489b80b30"
-data_git_describe = "v0.0-15189-gce22188696"
+data_git_hash = "8d99a2acf58b997811072fa2066a7acd45bfefde"
+data_git_describe = "v0.0-15193-g8d99a2acf5"
 data_git_msg = """\
-commit ce22188696373f4f1712624f08ced2b489b80b30
-Author: James Wainwright <james.wainwright@lowrisc.org>
-Date:   Mon Oct 31 17:31:51 2022 +0000
+commit 8d99a2acf58b997811072fa2066a7acd45bfefde
+Author: Dan McArdle <dmcardle@google.com>
+Date:   Fri Oct 28 17:31:53 2022 -0400
 
-    [sw/silicon_creator] Add test cases for otbn_copy_data_{to,from}_otbn
+    [test] Implement rom_e2e_asm_watchdog bite tests
     
-    These functions are just wrappers around `otbn_{write,read}_data`, so
-    these new tests were modified from those functions' tests.
+    Test that the watchdog's bite resets the core. This is achieved outside
+    of GDB, by inspecting its output.
     
-    Signed-off-by: James Wainwright <james.wainwright@lowrisc.org>
+    This commit adds the --gdb-expect-output-sequence flag to the GDB test
+    coordinator script. When the script sees GDB print each of the given
+    lines, in sequence, it will terminate GDB on the spot and consider it a
+    success.
+    
+    This enables us to assert that GDB reaches an echo line from the GDB
+    script and *then* sees the core reset.
+    
+    Signed-off-by: Dan McArdle <dmcardle@google.com>
 
 """
 
