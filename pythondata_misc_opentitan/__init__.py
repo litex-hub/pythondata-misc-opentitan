88275f8d867de45d2f2a1cad9e1ac86925a7878c
LiteX Robot <robot@timvideos.us>
2022-11-08 10:15:16 +0000

    Updating pythondata-misc-opentitan to 0.0.post15331
    Updated data to v0.0-15189-gce22188696 based on ce22188696373f4f1712624f08ced2b489b80b30 from https://github.com/lowRISC/opentitan.
    > commit ce22188696373f4f1712624f08ced2b489b80b30
    > Author: James Wainwright <james.wainwright@lowrisc.org>
    > Date:   Mon Oct 31 17:31:51 2022 +0000
    >
    >     [sw/silicon_creator] Add test cases for otbn_copy_data_{to,from}_otbn
    >
    >     These functions are just wrappers around `otbn_{write,read}_data`, so
    >     these new tests were modified from those functions' tests.
    >
    >     Signed-off-by: James Wainwright <james.wainwright@lowrisc.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,35 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post15326"
-version_tuple = (0, 0, 15326)
+version_str = "0.0.post15331"
+version_tuple = (0, 0, 15331)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post15326")
+    pversion = V("0.0.post15331")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post15184"
-data_version_tuple = (0, 0, 15184)
+data_version_str = "0.0.post15189"
+data_version_tuple = (0, 0, 15189)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post15184")
+    pdata_version = V("0.0.post15189")
 except ImportError:
     pass
-data_git_hash = "32d4a1eefdbc2d6c440385e8153a48f6227ad6a2"
-data_git_describe = "v0.0-15184-g32d4a1eefd"
+data_git_hash = "ce22188696373f4f1712624f08ced2b489b80b30"
+data_git_describe = "v0.0-15189-gce22188696"
 data_git_msg = """\
-commit 32d4a1eefdbc2d6c440385e8153a48f6227ad6a2
-Author: Weicai Yang <weicai@google.com>
-Date:   Mon Nov 7 16:45:17 2022 -0800
+commit ce22188696373f4f1712624f08ced2b489b80b30
+Author: James Wainwright <james.wainwright@lowrisc.org>
+Date:   Mon Oct 31 17:31:51 2022 +0000
 
-    [keymgr/dv] Fix 2 regression failures
+    [sw/silicon_creator] Add test cases for otbn_copy_data_{to,from}_otbn
     
-    1. Fixed scb for direct_to_disable test
-    2. Disabled EDN SVA for CSR tests
+    These functions are just wrappers around `otbn_{write,read}_data`, so
+    these new tests were modified from those functions' tests.
     
-    Signed-off-by: Weicai Yang <weicai@google.com>
+    Signed-off-by: James Wainwright <james.wainwright@lowrisc.org>
 
 """
 
