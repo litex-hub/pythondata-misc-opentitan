abdbe316232acec87d7828a701ca7b5fae3ffcd1
LiteX Robot <robot@timvideos.us>
2022-11-09 00:51:17 +0000

    Updating pythondata-misc-opentitan to 0.0.post15347
    Updated data to v0.0-15205-g9ef6ee45d7 based on 9ef6ee45d730ac82b6eb5eef44f87251a491c24a from https://github.com/lowRISC/opentitan.
    > commit 9ef6ee45d730ac82b6eb5eef44f87251a491c24a
    > Author: Eli Kim <eli@opentitan.org>
    > Date:   Wed Nov 2 15:55:01 2022 -0700
    >
    >     [rdc/spid] Waive CSR to PAD retention
    >
    >     When SwRstRsq occurs, chip is in active mode. So no retention functions
    >     are used. The error can be waived.
    >
    >     Signed-off-by: Eli Kim <eli@opentitan.org>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_opentitan/__init__.py
+++ pythondata_misc_opentitan/__init__.py
@@ -4,34 +4,35 @@ data_location = os.path.join(__dir__, "resources")
 src = "https://github.com/lowRISC/opentitan"
 
 # Module version
-version_str = "0.0.post15342"
-version_tuple = (0, 0, 15342)
+version_str = "0.0.post15347"
+version_tuple = (0, 0, 15347)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post15342")
+    pversion = V("0.0.post15347")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post15200"
-data_version_tuple = (0, 0, 15200)
+data_version_str = "0.0.post15205"
+data_version_tuple = (0, 0, 15205)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post15200")
+    pdata_version = V("0.0.post15205")
 except ImportError:
     pass
-data_git_hash = "c2caa251d82030b456669e9c04afd5586a127743"
-data_git_describe = "v0.0-15200-gc2caa251d8"
+data_git_hash = "9ef6ee45d730ac82b6eb5eef44f87251a491c24a"
+data_git_describe = "v0.0-15205-g9ef6ee45d7"
 data_git_msg = """\
-commit c2caa251d82030b456669e9c04afd5586a127743
-Author: Johnathan Van Why <jrvanwhy@google.com>
-Date:   Wed Nov 2 12:59:26 2022 -0700
+commit 9ef6ee45d730ac82b6eb5eef44f87251a491c24a
+Author: Eli Kim <eli@opentitan.org>
+Date:   Wed Nov 2 15:55:01 2022 -0700
 
-    Add rom_e2e_watchdog_reconfig tests.
+    [rdc/spid] Waive CSR to PAD retention
     
-    These tests verify the ROM correctly configures the watchdog timer. Tests are run with OTP configurations that disable and enable the watchdog for each life cycle state.
+    When SwRstRsq occurs, chip is in active mode. So no retention functions
+    are used. The error can be waived.
     
-    Signed-off-by: Johnathan Van Why <jrvanwhy@google.com>
+    Signed-off-by: Eli Kim <eli@opentitan.org>
 
 """
 
